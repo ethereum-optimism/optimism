d6cc1cec9452fb65ac4d22e0c8594c605a8ee892
Kelvin Fichter <kelvinfichter@gmail.com>
2019-04-13 20:59:40 -0400

    Added merkle tree node algorithms

--- packages/core/src/app/pg-core/db/chain-db.ts
+++ packages/core/src/app/pg-core/db/chain-db.ts
@@ -13,13 +13,21 @@ import {
   encode,
   decode,
   bnToUint256,
-  getTransactionRangeEnd
+  getTransactionRangeEnd,
 } from '../../common'
 
+// TODO: Where should this stuff sit? Should it be customizeable?
+const TREE_DEPTH = 16
+const computeParent = (leftChild: string, rightChild: string): string => {
+  return
+}
+
+// TODO: Where should this sit? Probably OK to be here.
 const KEYS = {
   BLOCKS: new BaseKey('b', ['uint32']),
   DEPOSITS: new BaseKey('d', ['uint256']),
   TRANSACTIONS: new BaseKey('t', ['uint32', 'uint256']),
+  TREE_NODES: new BaseKey('n', ['uint32', 'uint32']),
 }
 
 /**
@@ -88,6 +96,74 @@ export class PGChainDB implements ChainDB {
     const key = KEYS.BLOCKS.encode([blockNumber])
     const value = await this.db.get(key)
     return value.toString()
+  }
+
+  /**
+   * Queries a specific stored Merkle tree node. Attempts to compute the node
+   * from its children if the node doesn't exist.
+   * @param blockNumber Block number to pull the node from.
+   * @param nodeIndex Index of the specific node to query.
+   * @returns the node or `null` if the node cannot be found.
+   */
+  public async getMerkleTreeNode(
+    blockNumber: number,
+    nodeIndex: number
+  ): Promise<string> {
+    const key = KEYS.TREE_NODES.encode([blockNumber, nodeIndex])
+    const value = await this.db.get(key)
+
+    // We have this node, return it.
+    if (value !== null) {
+      return value.toString()
+    }
+
+    // We're at a leaf node so we can't go any deeper.
+    if (nodeIndex > 2 ** TREE_DEPTH) {
+      return null
+    }
+
+    // Compute left and right children (full binary tree).
+    const leftChildIndex = 2 * nodeIndex + 1
+    const rightChildIndex = leftChildIndex + 1
+
+    // Recursively pull the left and right children.
+    const leftChild = await this.getMerkleTreeNode(blockNumber, leftChildIndex)
+    const rightChild = await this.getMerkleTreeNode(
+      blockNumber,
+      rightChildIndex
+    )
+
+    // We can't create a parent if either child is node.
+    if (leftChild === null || rightChild === null) {
+      return null
+    }
+
+    // We have both children so we can compute the parent!
+    return computeParent(leftChild, rightChild)
+  }
+
+  /**
+   * Adds a Merkle tree node for a specific block. Won't add the node if it
+   * already exists in the database or can be computed from other nodes that
+   * exist.
+   * @param blockNumber Block to add the node for.
+   * @param nodeIndex Index of the node to add.
+   * @param nodeHash Hash of the node to add.
+   */
+  public async addMerkleTreeNode(
+    blockNumber: number,
+    nodeIndex: number,
+    nodeHash: string
+  ): Promise<void> {
+    // Don't do anything if we already have the node.
+    if ((await this.getMerkleTreeNode(blockNumber, nodeIndex)) !== null) {
+      return
+    }
+
+    // We don't have the node, add it.
+    const key = KEYS.TREE_NODES.encode([blockNumber, nodeIndex])
+    const value = Buffer.from(nodeHash)
+    await this.db.put(key, value)
   }
 
   /**
