9ce72c2afb0f90beb45570d694a118d0ee51c76d
Karl Floersch <karl@karlfloersch.com>
2019-05-08 01:55:38 -0400

    Fix get so it returns surrounded range

--- packages/operator/src/state-manager/range-db.ts
+++ packages/operator/src/state-manager/range-db.ts
@@ -7,9 +7,13 @@ import { itNext, itEnd } from '../utils'
 
 /* Logging */
 import debug from 'debug'
-const log = debug('test:info:range-db')
+const log = debug('info:range-db')
 
 type Endianness = 'le' | 'be'
+
+const lte = (buf1: Buffer, buf2: Buffer): boolean => {
+  return Buffer.compare(buf1, buf2) <= 0
+}
 
 export class RangeDB {
   constructor (readonly db: level, readonly prefix: Buffer, readonly keyLength: number=16, readonly endianness: Endianness='be') {
@@ -24,6 +28,14 @@ export class RangeDB {
     return Buffer.concat([this.prefix, buf])
   }
 
+  private getStartFromValue(value: Buffer) {
+    return value.slice(0, this.keyLength)
+  }
+
+  private getDataFromValue(value: Buffer) {
+    return value.slice(this.keyLength)
+  }
+
   private isCorrectPrefix(key: Buffer): boolean {
     if (typeof key === 'undefined') {
       return false
@@ -35,7 +47,17 @@ export class RangeDB {
     log('Putting range:', start.toString('hex'), end.toString('hex'), value)
     const endBuffer = end.toBuffer(this.endianness, this.keyLength)
     const keyWithPrefix = this.addPrefix(endBuffer)
-    await this.db.put(keyWithPrefix, Buffer.concat([this.bnToKey(start), value]))
+    const valueThing = Buffer.concat([start.toBuffer(this.endianness, this.keyLength), value])
+    await this.db.put(keyWithPrefix, valueThing)
+  }
+
+  private resultToRange(result) {
+    // Helper function which gets the start and end position from a DB seek result
+    return {
+      start: new BigNum(this.getStartFromValue(result.value)),
+      end: new BigNum(result.key.slice(this.prefix.length)),
+      value: this.getDataFromValue(result.value)
+    }
   }
 
   public async get(start: BigNum, end: BigNum) {
@@ -46,22 +68,30 @@ export class RangeDB {
     })
     const ranges = []
     let result = await itNext(it)
+    // First make sure that the resulting value has the correct prefix.
+    if (!this.isCorrectPrefix(result.key)) {
+      // If not return because this means there are no values yet put in this RangeDB
+      await itEnd(it)
+      return []
+    }
+    // Next check if the resulting value surrounds our query. If so we want to return just that value.
+    if (lte(this.getStartFromValue(result.value), this.bnToKey(start)) && // result start <= query start
+        lte(this.bnToKey(end), result.key)                                // query end <= result end
+    ) {
+      // Add the first result to our return values if it surrounds our query and return the ranges
+      ranges.push(this.resultToRange(result))
+      await itEnd(it)
+      return ranges
+    }
+    log('these are the ranges', ranges)
     while (
       this.isCorrectPrefix(result.key) &&
-      Buffer.compare(result.key, this.bnToKey(end)) < 0
+      Buffer.compare(result.key, this.bnToKey(end)) <= 0
     ) {
-      ranges.push({
-        start: result.value.slice(0, this.keyLength),
-        end: result.key,
-        value: result.value.slice(this.keyLength)
-      })
+      ranges.push(this.resultToRange(result))
       result = await itNext(it)
     }
     await itEnd(it)
     return ranges
-  }
-
-  public async getContiguous(start: BigNum, end: BigNum) {
-    log('Getting range:', start, end)
   }
 }
--- packages/operator/test/unit/range-db.spec.ts
+++ packages/operator/test/unit/range-db.spec.ts
@@ -18,11 +18,65 @@ describe.only('RangeDB', () => {
     rangeDB = new RangeDB(db, Buffer.from([prefixCounter++]))
   })
 
-  it('should allow puts on a range', async() => {
-    const start = new BigNum('10', 'hex')
-    const end = new BigNum('100', 'hex')
-    rangeDB.put(start, end, Buffer.from('hello!'))
-    rangeDB.get(start, end.addn(10))
-    log('success!')
+  it('should allow puts on a range & get should return the range value which was put', async() => {
+    const start = 0
+    const end = 10
+    await rangeDB.put(new BigNum(start), new BigNum(end), Buffer.from('hello!'))
+    const res = await rangeDB.get(new BigNum(start), new BigNum(end))
+    new BigNum(res[0].start, 'hex').toNumber().should.equal(start)
+    new BigNum(res[0].end, 'hex').toNumber().should.equal(end)
+  })
+
+  it('should return an empty array if the db is empty', async() => {
+    const getStart = 4
+    const getEnd = 8
+    const res = await rangeDB.get(new BigNum(getStart), new BigNum(getEnd))
+    res.length.should.equal(0)
+  })
+
+  it('should return a range which surrounds the range which you are getting', async() => {
+    // This covers the case where the DB has one element of range 0-10, and you get 3-4, then it
+    // should return the entire element which "surrounds" your get query.
+    const start = 0
+    const end = 10
+    const getStart = 4
+    const getEnd = 8
+    await rangeDB.put(new BigNum(start), new BigNum(end), Buffer.from('hello!'))
+    const res = await rangeDB.get(new BigNum(getStart), new BigNum(getEnd))
+    new BigNum(res[0].start, 'hex').toNumber().should.equal(start)
+    new BigNum(res[0].end, 'hex').toNumber().should.equal(end)
+    res.length.should.equal(1)
+  })
+
+  it('should allow puts on multiple ranges', async() => {
+    // Generate some ranges
+    const ranges = []
+    for (let i = 0; i < 10; i++) {
+      const start = new BigNum('' + i*10, 'hex')
+      const end = new BigNum('' + (i + 1)*10, 'hex')
+      ranges.push({
+        start,
+        end
+      })
+    }
+    // Put them in our DB
+    for (const range of ranges) {
+      log(range.start.toString(16))
+      rangeDB.put(range.start, range.end, Buffer.from('hello!'))
+    }
+    // Get them from our DB
+    const gottenRanges = await rangeDB.get(ranges[0].start, ranges[ranges.length-1].end)
+    // Compare them to the ranges we put & got and make sure they are equal
+    for (let i = 0; i < ranges.length; i++) {
+      const start = ranges[i].start.toString(16)
+      const end = ranges[i].end.toString(16)
+      const gottenStart = ranges[i].start.toString(16)
+      const gottenEnd = ranges[i].end.toString(16)
+      log('Put start:', start, ' -- Got start:', gottenStart)
+      log('Put end:', end, ' -- Got end:', gottenEnd)
+      gottenStart.should.equal(start)
+      gottenEnd.should.equal(end)
+    }
+    gottenRanges.length.should.equal(ranges.length)
   })
 })
