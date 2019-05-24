76328a42e31aa4a558b07b4b6e4a78e8d0fca189
Alexander Attar <alexanderattar@gmail.com>
2019-05-24 16:04:13 -0400

    Add initial Deposit contract

--- packages/contracts/contracts/Commitment.sol
+++ packages/contracts/contracts/Commitment.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.5.0;
 pragma experimental ABIEncoderV2;
 
-import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+import "openzeppelin-solidity/contracts/math/Math.sol";
 
 contract Commitment {
    struct Range {
