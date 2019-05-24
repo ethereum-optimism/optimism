41d7fcfa34154590013781b90cbdce772f077b86
Alexander Attar <alexanderattar@gmail.com>
2019-05-24 16:02:05 -0400

    Add commitment contract

---
+++ packages/contracts/contracts/Commitment.sol
@@ -0,0 +1,24 @@
+pragma solidity ^0.5.0;
+pragma experimental ABIEncoderV2;
+
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+
+contract Commitment {
+   struct Range {
+    uint256 start;
+    uint256 end;
+  }
+
+  struct StateObject {
+    address predicateAddress;
+    bytes data;
+  }
+
+  struct StateUpdate {
+    Range range;
+    StateObject stateObject;
+    address plasmaContract;
+    uint256 plasmaBlockNumber;
+  }
+    function verifyInclusion(StateUpdate memory _stateUpdate, bytes memory _inclusionProof) public returns (bool) {}
+}
