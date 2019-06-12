d5d21ace5184fa6775d03648321bbdb1a951808b
Will Meister <william.meister@consensys.net>
2019-06-12 13:58:30 -0500

    Fixing code quality issues

--- packages/core/src/app/client/state-db.ts
+++ packages/core/src/app/client/state-db.ts
@@ -9,11 +9,11 @@ import {VerifiedStateUpdate} from "../../interfaces/common/utils";
  * See: http://spec.plasma.group/en/latest/src/05-client-architecture/state-db.html for more details.
  */
 export class DefaultStateDB implements StateDB {
-  async getVerifiedStateUpdates(start: BigNum, end: BigNum): Promise<VerifiedStateUpdate[]> {
+  public async getVerifiedStateUpdates(start: BigNum, end: BigNum): Promise<VerifiedStateUpdate[]> {
     throw Error("DefaultStateDB.getVerifiedStateUpdates is not implemented.")
   }
 
-  async putVerifiedStateUpdate(verifiedStateUpdate: VerifiedStateUpdate): Promise<void> {
+  public async putVerifiedStateUpdate(verifiedStateUpdate: VerifiedStateUpdate): Promise<void> {
     throw Error("DefaultStateDB.putVerifiedStateUpdate is not implemented.")
   }
 
