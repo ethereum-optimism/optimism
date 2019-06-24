bc0106d225808a382e031fa908ab8a8ed2a6f675
ben-chain <ben@pseudonym.party>
2019-06-24 19:41:50 -0400

    basic parent check semi-test

--- packages/contracts/contracts/CommitmentChain.sol
+++ packages/contracts/contracts/CommitmentChain.sol
@@ -10,15 +10,34 @@ contract CommitmentChain {
         return true;
     }
 
-    function stateSubtreeParent(
-            dt.StateSubtreeNode memory _leftSibling,
-            dt.StateSubtreeNode memory _rightSibling
-        ) public pure returns (dt.StateSubtreeNode memory) {
-            bytes32 memory computedHash = keccak256(
+    function getAbiPacked(
+        dt.StateSubtreeNode memory _leftSibling,
+        dt.StateSubtreeNode memory _rightSibling
+    ) public pure returns (bytes memory) {
+        bytes memory packed =
+            abi.encodePacked(
                 _leftSibling.hashValue,
                 _leftSibling.start,
                 _rightSibling.hashValue,
                 _rightSibling.start
             );
+        return packed;
+    }
+    function stateSubtreeParent(
+        dt.StateSubtreeNode memory _leftSibling,
+        dt.StateSubtreeNode memory _rightSibling
+    ) public pure returns (dt.StateSubtreeNode memory) {
+        dt.StateSubtreeNode memory parent;
+        bytes32 computedHash = keccak256(
+            abi.encodePacked(
+                _leftSibling.hashValue,
+                _leftSibling.start,
+                _rightSibling.hashValue,
+                _rightSibling.start
+            )
+        );
+        parent.hashValue = computedHash;
+        parent.start = _leftSibling.start;
+        return parent;
     }
 }
--- packages/contracts/test/Commitment.spec.ts
+++ packages/contracts/test/Commitment.spec.ts
@@ -1,20 +1,14 @@
 /* External Imports */
 import {
-    abi,
-    AbiStateObject,
-    AbiRange,
-    hexStringify,
-    AbiOwnershipParameters,
-    AbiOwnershipTransaction,
+    MerkleIntervalTree,
+    MerkleIntervalTreeNode,
+    MerkleStateIntervalTree
   } from '@pigi/core'
   import BigNum = require('bn.js')
+
   /* Contract Imports */
   import { createMockProvider, deployContract, getWallets } from 'ethereum-waffle'
-  import * as BasicTokenMock from '../build/BasicTokenMock.json'
-  import * as Deposit from '../build/Deposit.json'
   import * as Commitment from '../build/CommitmentChain.json'
-  import * as TransactionPredicate from '../build/TransactionPredicate.json'
-  import * as OwnershipTransactionPredicate from '../build/OwnershipTransactionPredicate.json'
   /* Logging */
   import debug from 'debug'
   import { check } from 'ethers/utils/wordlist'
@@ -23,19 +17,38 @@ import {
   import chai = require('chai')
   export const should = chai.should()
   
-  describe('Deposit with Ownership', () => {
+  describe.only('Deposit with Ownership', () => {
     const provider = createMockProvider()
     const [wallet, walletTo] = getWallets(provider)
-    let depositContract
     let commitmentContract
-    let ownershipPredicate
   
     beforeEach(async () => {
       commitmentContract = await deployContract(wallet, Commitment, [])
+    })
   
     describe('Commitment Contract', () => {
-      it('does not throw when deposit is called after approving erc20 movement', async () => {
+      it('correctly calculates the parent of two state subtree nodes', async () => {
+        const leftSibling = {
+            hashValue: '0x1111111111111111111111111111111111111111111111111111111111111111',
+            start: '0x00'
+        }
+        const rightSibling = {
+            hashValue: '0x1111111111111111111111111111111111111111111111111111111111111111',
+            start: '0xaa'
+        }
+        const abiPacked = await commitmentContract.getAbiPacked(leftSibling, rightSibling)
+        console.log('abipacked from solidity is: ', abiPacked)
+        const contractParent = await commitmentContract.stateSubtreeParent(leftSibling, rightSibling)
+        console.log('contract parent is: ', contractParent)
 
+        const tsLeftSibling = new MerkleIntervalTreeNode(Buffer.from(leftSibling.hashValue.slice(2), 'hex'), new BigNum(leftSibling.start.slice(2), "hex").toBuffer('be', 16))
+        console.log('typescript left sibling is ', tsLeftSibling)
+        const tsRightSibling = new MerkleIntervalTreeNode(Buffer.from(rightSibling.hashValue.slice(2), 'hex'), new BigNum(rightSibling.start.slice(2), "hex").toBuffer('be', 16))
+        console.log('typescript right sibling is ', tsRightSibling)
+
+
+        const tsParent = MerkleIntervalTree.parent(tsLeftSibling, tsRightSibling)
+        console.log('typescript parent is: ', tsParent)
       })
   })
-  
+})
