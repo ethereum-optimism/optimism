72750e4213d399b6db4f3d6f057e6c255ee4878b
ben-chain <ben@pseudonym.party>
2019-06-27 13:39:24 -0400

    improve commenting

--- packages/core/src/types/block-production.types.ts
+++ packages/core/src/types/block-production.types.ts
@@ -29,11 +29,13 @@ export interface MerkleIntervalTree {
   getInclusionProof(leafposition: number): MerkleIntervalInclusionProof
 }
 
+// Array of state updates for which a subtree is created in our block structure.
 export interface SubtreeContents {
-  assetId: Buffer
-  stateUpdates: AbiStateUpdate[]
+  assetId: Buffer // Each subtree of state updates will be given a leaf.lowerBound = assetId in the top-level tree.
+  stateUpdates: AbiStateUpdate[] // The state updates from which to build our subtree, whose root will become the leaf.hash = root() in the top-level tree.
 }
 
+// Our plasma blocks are a nested merkle interval tree.  See http://spec.plasma.group/en/latest/src/01-core/double-layer-tree.html
 export interface DoubleMerkleIntervalTree extends MerkleIntervalTree {
   dataBlocks: SubtreeContents
   subtrees: MerkleIntervalTree[]
