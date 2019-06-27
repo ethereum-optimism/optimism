c9bf617dd6cfea67758a4ba9583634ed4849c741
ben-chain <ben@pseudonym.party>
2019-06-27 13:27:11 -0400

    improve comments on tree interfaces

--- packages/core/src/types/block-production.types.ts
+++ packages/core/src/types/block-production.types.ts
@@ -5,24 +5,26 @@ import BigNumber = require('bn.js')
 import { AbiStateUpdate } from '../app'
 
 export interface MerkleIntervalTreeNode {
-  hash: Buffer // Hash of the sibling or leaf data
-  start: Buffer // The start interval value for this node
-  data: Buffer // concatenation of (hash, index)
+  hash: Buffer // Hash of the sibling or leaf data.
+  start: Buffer // The start interval value for this node.
+  data: Buffer // Concatenation of (hash, index)
 }
 
 export interface MerkleIntervalInclusionProof {
-  siblings: MerkleIntervalTreeNode[]
-  leafPosition: BigNumber
+  siblings: MerkleIntervalTreeNode[] // The siblings along the merkle path leading from the leaf to the root.
+  leafPosition: BigNumber // The index of the leaf we are proving inclusion of.
 }
 
 export interface MerkleIntervalProofOutput {
-  root: MerkleIntervalTreeNode
-  maxEnd: BigNumber
+  root: MerkleIntervalTreeNode // the root node resulting from a merkle index tree inclusion proof
+  maxEnd: BigNumber // The upper bound that an inclusion proof is valid for.  
+  // For a single MerkleIntervalTree, it is mathematically impossible for two branches to exist 
+  // such that their [leaf.lowerBound, proofOutput.upperBound) ranges intersect.
 }
 
 export interface MerkleIntervalTree {
-  dataBlocks: any
-  levels: MerkleIntervalTreeNode[][]
+  dataBlocks: any // The blocks of data we are constructing a merkle interval tree for.
+  levels: MerkleIntervalTreeNode[][] // The 'MerkleIntervalTreeNode's which make up the tree. levels[0].length == 
   root(): MerkleIntervalTreeNode
   getInclusionProof(leafposition: number): MerkleIntervalInclusionProof
 }
