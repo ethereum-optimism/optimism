afbc102acf850ef5981b0ced5ec24cbd01d1e0ab
Akhila Raju <akhila@berkeley.edu>
2019-07-10 10:57:35 -0400

    Aggregator fields added

--- packages/contracts/contracts/Aggregator.sol
+++ packages/contracts/contracts/Aggregator.sol
@@ -2,7 +2,16 @@ pragma solidity ^0.5.1;
 
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 
+/* Internal Imports */
+import { CommitmentChain } from "./CommitmentChain.sol";
+import { Deposit } from "./Deposit.sol";
+
 contract Aggregator {
+  address public authenticationAddress;
+  CommitmentChain public commitmentContract;
+  mapping(address => Deposit) depositContracts;
+  uint public id;
+  mapping(string => string) metadata;
 
   constructor() public {
   }
