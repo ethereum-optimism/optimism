ea88ee70c29e6a71090c1c35195736d6d4419fe3
ben-chain <ben@pseudonym.party>
2019-10-03 10:49:36 -0400

    improve error logging

--- packages/wallet/src/rollup-state-validator.ts
+++ packages/wallet/src/rollup-state-validator.ts
@@ -187,9 +187,7 @@ export class DefaultRollupStateValidator implements RollupStateValidator {
           fraudTransition: nextTransition,
         }
       } else {
-        log.info(
-          'The transaction threw an error--but for a reason unrelated to the transition itself not passing the state machine.  Uh oh!'
-        )
+        log.error(log, 'Transaction ingestion threw an error--but for a reason unrelated to the transition itself not passing the state machine. Uh oh!', error)
         throw new LocalMachineError()
       }
     }
