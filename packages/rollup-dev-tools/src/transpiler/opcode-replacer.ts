37b35698996a47a529a9c52f13384112b068c05e
ben-chain <ben@pseudonym.party>
2019-12-17 20:08:03 -0500

    clean commented code

--- packages/rollup-dev-tools/src/transpiler/opcode-replacer.ts
+++ packages/rollup-dev-tools/src/transpiler/opcode-replacer.ts
@@ -28,9 +28,6 @@ const DefaultOpcodeReplacements = {
   PUSH2: ['ADD', 'PUSH2', '0x0000'],
 }
 
-/**
- * Interface defining the set of transpiled opcodes, and what bytecode to replace with.
- */
 export class OpcodeReplacerImpl implements OpcodeReplacer {
   private readonly opcodeReplacementBytecodes: Map<
     EVMOpcode,
@@ -46,103 +43,6 @@ export class OpcodeReplacerImpl implements OpcodeReplacer {
       )
     }
   }
-  //   log.debug(
-  //     `Opcode replacer will be using the following opcode replacement JSON config: ${JSON.stringify(
-  //       OpcodeReplacementsJSON
-  //     )}`
-  //   )
-  //   for (const opcodeToReplaceStr in OpcodeReplacementsJSON) { // tslint:disable-line
-  //     const opcodeToReplace: EVMOpcode = Opcode.parseByName(opcodeToReplaceStr)
-  //     log.debug(
-  //       `Parsing bytecode replacement for opcode [${opcodeToReplace.name}].`
-  //     )
-
-  //     // get the specified replacement array from the JSON config
-  //     const replacementArray: string[] =
-  //       OpcodeReplacementsJSON[opcodeToReplaceStr]
-  //     log.info(
-  //       `The JSON specified a replacement array as: ${JSON.stringify(
-  //         replacementArray
-  //       )}`
-  //     )
-
-  //     // We want to replace all PUSH_STATE_MGR_ADDR with PUSH20 + the configured address.  Currently only works with max P_S_M_A per replacement.
-  //     // Find where it is, if anywhere
-  //     const indexToReplaceAddressPush = replacementArray.indexOf(
-  //       PUSH_STATE_MGR_ADDR
-  //     )
-  //     if (indexToReplaceAddressPush >= 0) {
-  //       log.debug(
-  //         `Found a PUSH_STATE_MGR_ADDR at index ${indexToReplaceAddressPush}.  Splicing in ['PUSH20', '${stateManagerAddress.toString()}'].`
-  //       )
-  //       // replace free var with PUSH20, 20-byte State Mgr Address
-  //       replacementArray.splice(
-  //         indexToReplaceAddressPush,
-  //         1,
-  //         'PUSH20',
-  //         stateManagerAddress
-  //       )
-  //     }
-
-  //     // now convert everything to bytcode, making sure PUSHN is always followed by length-N buffer
-  //     const replacementAsBytecode: EVMBytecode = []
-  //     for (let i = 0; i < replacementArray.length; i++) {
-  //       const op: EVMOpcode = Opcode.parseByName(replacementArray[i])
-  //       if (op === undefined) {
-  //         log.error(
-  //           `Opcode replacement config JSON specified: [${replacementArray[i]}], which could not be parsed into an EVM Opcode.`
-  //         )
-  //         process.exit(1)
-  //       }
-  //       log.debug(
-  //         `Parsing the ${i}th opcode in the replacement for ${opcodeToReplaceStr}, this opcode is: ${op.name}`
-  //       )
-  //       replacementAsBytecode[i] = op
-
-  //       // sanitize/typecheck PUSHes
-  //       const bytesToConsume: number = op.programBytesConsumed
-  //       if (bytesToConsume > 0) {
-  //         log.debug(
-  //           `Parsed the ${i}th bytcode replacement element for ${opcodeToReplaceStr} to be ${op.name}-- which is expected to consume ${bytesToConsume}.`
-  //         )
-  //         const consumedValueBuffer: Buffer = hexStrToBuf(
-  //           replacementArray[i + 1]
-  //         )
-  //         if (consumedValueBuffer === undefined) {
-  //           log.error(
-  //             `Final opcode in replacement array for ${opcodeToReplaceStr} was ${op.name}, but was not proceeded by any bytes to consume.`
-  //           )
-  //           process.exit(1)
-  //         }
-
-  //         if (consumedValueBuffer.length !== bytesToConsume) {
-  //           log.error(
-  //             `The hex sring following the PUSH operation was 0x[${consumedValueBuffer.toString(
-  //               'hex'
-  //             )}], but was expecting ${bytesToConsume} bytes to consume.`
-  //           )
-  //         }
-  //         log.debug(
-  //           `The proceeding hex string was found to be the right length for this [${op.name}].  Continuing...`
-  //         )
-  //         replacementAsBytecode[i + 1] = consumedValueBuffer
-  //         i++
-  //       }
-  //       log.info(
-  //         `Storing replacement Bytecode for [${
-  //           opcodeToReplace.name
-  //         }] as: ${JSON.stringify(replacementAsBytecode)}.`
-  //       )
-  //       // Store that we are replacing this one
-  //       this.replacedOpcodes.push(opcodeToReplace)
-  //       // Store its replacement
-  //       this.opcodeReplacementBytecodes.set(
-  //         opcodeToReplace,
-  //         replacementAsBytecode
-  //       )
-  //     }
-  //   }
-  // }
 
   public getOpcodeReplacement(opcodeAndBytes: EVMOpcodeAndBytes): EVMBytecode {
     log.debug(
@@ -169,23 +69,6 @@ export class OpcodeReplacerImpl implements OpcodeReplacer {
 
     // init replacement bytecode
     const replacementBytecode: EVMBytecode = []
-
-    // We want to replace all PUSH_STATE_MGR_ADDR with PUSH20 + the configured address.  Currently only works with max P_S_M_A per replacement.
-    // Find where it is, if anywhere
-    // const indexToReplaceAddressPush = cfgReplacmentArray.indexOf(
-    //   PUSH_STATE_MGR_ADDR
-    // )
-    // if (indexToReplaceAddressPush >= 0) {
-    //   log.debug(
-    //     `Found a PUSH_STATE_MGR_ADDR at index ${indexToReplaceAddressPush}.  Splicing in a PUSH20 with SM address '${stateManagerAddress.toString()}'].`
-    //   )
-    //   // replace free var with PUSH20, 20-byte State Mgr Address
-    //   replacementBytecode.push({
-    //     opcode: Ops.PUSH20,
-    //     consumedBytes: hexStrToBuf(this.stateManagerAddress)
-    //   })
-    //   continue
-    // }
 
     for (let i = 0; i < cfgReplacmentArray.length; i++) {
       if (cfgReplacmentArray[i] === PUSH_STATE_MGR_ADDR) {
