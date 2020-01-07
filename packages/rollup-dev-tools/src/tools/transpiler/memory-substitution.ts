e2ec73d57b0f43e09ea2e1925169b412b6139593
ben-chain <ben@pseudonym.party>
2020-01-07 16:38:02 -0500

    add comments, clean code

--- packages/rollup-dev-tools/src/tools/transpiler/memory-substitution.ts
+++ packages/rollup-dev-tools/src/tools/transpiler/memory-substitution.ts
@@ -20,26 +20,43 @@ import { POINT_CONVERSION_HYBRID } from 'constants'
 
 const log = getLogger(`memory-substitution-gen`)
 
-// assumes the first element on the stack is the memory offset to start pulling from
-export const dynamicStashMemoryInStack = function(
+/**
+ * Returns a piece of bytecode which dynamically stashes a specified number of words from memory onto the stack.
+ * Assumes that the first element of the stack is the memory index to load from.
+ *
+ * Stack before this operation: index, X, Y, Z
+ * Stack after this operation: index, memory[(wordsToStash - 1)*32 + index], memory[(wordsToStash - 2)*32 + index], ..., memory[index] X, Y, Z
+ * (Note that each MLOAD pulls 32 byte words from memory, each stack element above is a word)
+ *
+ * Memory after this operation: unaffected
+ *
+ * @param wordsToStash The number of 32-byte words from the memory to stash
+ * @returns Btyecode which results in the stash operation described above.
+ */
+export const dynamicStashMemoryInStack = (
   wordsToStash: number
-): EVMBytecode {
+): EVMBytecode => {
   let stashOperation: EVMBytecode = []
+  // For each word to stash...
   for (let i = 0; i < wordsToStash; i++) {
     stashOperation = stashOperation.concat([
+      // duplicate the mmory index which is expected as first element on stack
       {
         opcode: Opcode.DUP1,
         consumedBytes: undefined,
       },
-      getPUSHIntegerOp(i * 32),
+      // ADD the word number to the memory index to get index for this word
+      getPUSHIntegerOp(i * 32), // 32 because memory is byte indexed but loaded in 32 byte words
       {
         opcode: Opcode.ADD,
         consumedBytes: undefined,
       },
+      // MLOAD the word from memory
       {
         opcode: Opcode.MLOAD,
         consumedBytes: undefined,
       },
+      // Swap the loaded word so that memory index is first on stack for next iteration
       {
         opcode: Opcode.SWAP1,
         consumedBytes: undefined,
@@ -49,22 +66,41 @@ export const dynamicStashMemoryInStack = function(
   return stashOperation
 }
 
-// assumes the first element on the stack is the memory offset to start pushing to
-export const dynamicUnstashMemoryFromStack = function(
+/**
+ * Returns a piece of bytecode which dynamically unstashes a specified number of words from the stack back into memory.
+ * Assumes that the first element of the stack is the memory index to load from.
+ *
+ * Stack before this operation: [index, M_wordsToUnstash, M_(wordsToUnstash-1), ... M_1, X, Y, Z, ...]
+ * Stack after this operation: [index, X, Y, Z, ...]
+ * (Note that each MSTORE puts 32 byte words from memory, each M_* stack element above is a word)
+ *
+ * Memory after this operation: memory[index + 32 * n] = M_n (for n = 0 through wordsToUnstash)
+ *
+ * @param wordsToStash The number of 32-byte words from the stack to unstash
+ * @returns Btyecode which results in the unstash operation described above.
+ */
+export const dynamicUnstashMemoryFromStack = (
   wordsToUnstash: number
-): EVMBytecode {
+): EVMBytecode => {
   let unstashOperation: EVMBytecode = []
+  // The only trickiness here is that the stash operation for memory = A, B, C --> stack = C, B, A
+  // So we store in reeverse order, starting with index + wordsToUnstash and work back to index + 0.
+
+  // For each word to unstash...
   for (let i = 0; i < wordsToUnstash; i++) {
     unstashOperation = unstashOperation.concat([
+      // duplicate the memory index, expected as first thing on the stack.
       {
         opcode: Opcode.DUP1,
         consumedBytes: undefined,
       },
+      // ADD the max words to unstash
       getPUSHIntegerOp((wordsToUnstash - 1) * 32),
       {
         opcode: Opcode.ADD,
         consumedBytes: undefined,
       },
+      // SUBtract the current word we're going to unstash
       getPUSHIntegerOp(i * 32),
       {
         opcode: Opcode.SWAP1,
@@ -74,24 +110,55 @@ export const dynamicUnstashMemoryFromStack = function(
         opcode: Opcode.SUB,
         consumedBytes: undefined,
       },
+      // DUP the word we're going to unstash from the stack.
+      // Stack looks like: [index + numWords - i, index, C, B, A, ...]
+      // So the index of C is 3, index of B is 4, etc...
       getDUPNOp(3 + i),
+      // Swap so stack is now [index, wordToUnstash]
       {
         opcode: Opcode.SWAP1,
         consumedBytes: undefined,
       },
+      // Store
       {
         opcode: Opcode.MSTORE,
         consumedBytes: undefined,
       },
     ])
   }
+  // Now all that's left is to CLEANUP THE STACK
+  // For dynamic index, we don't want to delete the index, in case it's needed for other operations.
+  // Stack should look like it started, [index, C, B, A, ...]  so this SWAP makes it [A, C, B, index, ...]
+  unstashOperation.push(getSWAPNOp(wordsToUnstash))
+  // Now that the unstashed words are at the front of the stack, POP them all (numWords times)
+  unstashOperation = unstashOperation.concat(
+    new Array<EVMOpcodeAndBytes>(wordsToUnstash).fill({
+      opcode: Opcode.POP,
+      consumedBytes: undefined,
+    })
+  )
   return unstashOperation
 }
 
-export const staticStashMemoryInStack = function(
+/**
+ * Returns a piece of bytecode which stashes a specified number of words from memory, at the specified byte index, onto the stack.
+ *
+ * Stack before this operation: X, Y, Z
+ * Stack after this operation: memory[(wordsToStash - 1)*32 + memoryIndex], memory[(wordsToStash - 2)*32 + mmoryIndex], ..., memory[memoryIndex] X, Y, Z
+ * (Note that each MLOAD pulls 32 byte words from memory, each stack element above is a word)
+ *
+ * Memory after this operation: unaffected
+ *
+ * @param memoryIndex The byte index in the memory to stash
+ * @param numWords The number of 32-byte words from the memory to stash
+ * @returns Btyecode which results in the stash operation described above.
+ */
+
+export const staticStashMemoryInStack = (
   memoryIndex: number,
   numWords: number
-): EVMBytecode {
+): EVMBytecode => {
+  // we just use the dynamic operation, PUSHing the memoryIndex to the stack beforehand, and POPing the memoryIndex once the operation is complete.
   return [
     getPUSHIntegerOp(memoryIndex),
     ...dynamicStashMemoryInStack(numWords),
@@ -102,10 +169,25 @@ export const staticStashMemoryInStack = function(
   ]
 }
 
-export const staticUnstashMemoryFromStack = function(
+/**
+ * Returns a piece of bytecode which unstashes a specified number of words from the stack back into memory at the specified byte index.
+ * Assumes that the first element of the stack is the memory index to load from.
+ *
+ * Stack before this operation: [M_wordsToUnstash, M_(wordsToUnstash-1), ... M_1, X, Y, Z, ...]
+ * Stack after this operation: [X, Y, Z, ...]
+ * (Note that each MSTORE puts 32 byte words from memory, each M_* stack element above is a word)
+ *
+ * Memory after this operation: memory[memoryIndex + 32 * n] = M_n (for n = 0 through numWords)
+ *
+ * @param memoryIndex The byte index in the memory to unstash
+ * @param numWords The number of 32-byte words from the stack to unstash to memory
+ * @returns Btyecode which results in the unstash operation described above.
+ */
+export const staticUnstashMemoryFromStack = (
   memoryIndex: number,
   numWords: number
-): EVMBytecode {
+): EVMBytecode => {
+  // we just use the dynamic operation, PUSHing the memoryIndex to the stack beforehand, and POPing the memoryIndex once operation is complete.
   return [
     getPUSHIntegerOp(memoryIndex),
     ...dynamicUnstashMemoryFromStack(numWords),
@@ -116,20 +198,29 @@ export const staticUnstashMemoryFromStack = function(
   ]
 }
 
-export const getPUSHIntegerOp = function(theInt: number): EVMOpcodeAndBytes {
+// constructs an operation which PUSHes the given integer to the stack.
+export const getPUSHIntegerOp = (theInt: number): EVMOpcodeAndBytes => {
   const intAsBuffer: Buffer = new BigNumber(theInt).toBuffer()
   const numBytesToPush = intAsBuffer.length
-  // todo error if length exceeds 32
+  // TODO: error if length exceeds 32
   return {
     opcode: Opcode.parseByNumber(96 + numBytesToPush - 1), // PUSH1 is 96 in decimal
     consumedBytes: intAsBuffer,
   }
 }
-
-export const getDUPNOp = function(indexToDUP: number): EVMOpcodeAndBytes {
-  // todo error if index is too big
+// returns DUPN operation for the specified N.
+export const getDUPNOp = (indexToDUP: number): EVMOpcodeAndBytes => {
+  // TODO: error if index is too big
   return {
     opcode: Opcode.parseByNumber(128 + indexToDUP - 1),
+    consumedBytes: undefined,
+  }
+}
+// returns SWAPN operation for the specified N.
+export const getSWAPNOp = (indexToSWAP: number): EVMOpcodeAndBytes => {
+  // TODO: error if index is too big
+  return {
+    opcode: Opcode.parseByNumber(144 + indexToSWAP - 1),
     consumedBytes: undefined,
   }
 }
--- packages/rollup-dev-tools/test/transpiler/memory-substitution.spec.ts
+++ packages/rollup-dev-tools/test/transpiler/memory-substitution.spec.ts
@@ -37,18 +37,24 @@ import {
   staticUnstashMemoryFromStack,
   getPUSHIntegerOp,
 } from '../../src/tools/transpiler'
-import {
-  assertExecutionEqual,
-  stateManagerAddress,
-  whitelistedOpcodes,
-} from '../helpers'
+import { stateManagerAddress, whitelistedOpcodes } from '../helpers'
 import { EvmIntrospectionUtil } from '../../src/types/vm'
 import { EvmIntrospectionUtilImpl } from '../../src/tools/vm'
-import { exec } from 'child_process'
 
 const log: Logger = getLogger('test-memory-sub')
 
-const storeNWordsInMemorySequential = function(numWords: number): EVMBytecode {
+const pointlessOperation: EVMBytecode = [
+  {
+    opcode: Opcode.PUSH1,
+    consumedBytes: hexStrToBuf('0xab'),
+  },
+  {
+    opcode: Opcode.POP,
+    consumedBytes: undefined,
+  },
+]
+
+const storeNWordsInMemorySequential = (numWords: number): EVMBytecode => {
   let storageBytecode: EVMBytecode = []
   for (let i = 0; i < numWords; i++) {
     storageBytecode = storageBytecode.concat([
@@ -69,10 +75,10 @@ const storeNWordsInMemorySequential = function(numWords: number): EVMBytecode {
   return storageBytecode
 }
 
-const overwriteNWordsInMemoryWithOffset = function(
+const overwriteNWordsInMemoryWithOffset = (
   numWords: number,
   offset: number
-): EVMBytecode {
+): EVMBytecode => {
   let overwriteBytecode: EVMBytecode = []
   for (let i = 0; i < numWords; i++) {
     overwriteBytecode = overwriteBytecode.concat([
@@ -93,38 +99,95 @@ const overwriteNWordsInMemoryWithOffset = function(
 }
 
 describe.only('Memory Replacement Operations', () => {
-  let opcodeWhitelist: OpcodeWhitelist
-  let transpiler: Transpiler
-  let replacer: OpcodeReplacer
   let evmUtil: EvmIntrospectionUtil
-  let storeSomeValsInMemory: EVMBytecode = []
-
   before(async () => {
-    storeSomeValsInMemory = storeNWordsInMemorySequential(3)
+    evmUtil = await EvmIntrospectionUtilImpl.create()
+  })
 
-    const pointlessOperation: EVMBytecode = [
-      {
-        opcode: Opcode.PUSH1,
-        consumedBytes: hexStrToBuf('0xaa'),
-      },
-      {
-        opcode: Opcode.POP,
-        consumedBytes: undefined,
-      },
+  describe('Testing Memory Utils', () => {
+    it('should correctly storeNWordsInMemorySequential', async () => {
+      const numSequentialWordsToStore: number = 10
+      const operationBytecode: EVMBytecode = [
+        ...storeNWordsInMemorySequential(numSequentialWordsToStore),
+        { opcode: Opcode.RETURN, consumedBytes: undefined },
+      ]
+      const operationBuffer: Buffer = bytecodeToBuffer(operationBytecode)
+
+      const memoryStoredResult = await evmUtil.getStepContextBeforeStep(
+        operationBuffer,
+        670 // hardcoded PC val, found via debug log
+      )
+      memoryStoredResult.stackDepth.should.equal(0)
+      memoryStoredResult.memoryWordCount.should.equal(numSequentialWordsToStore)
+
+      let expectedMemory: number[] = []
+      for (let i = 0; i < numSequentialWordsToStore; i++) {
+        expectedMemory = expectedMemory.concat(new Array(32).fill(i, 0, 32))
+      }
+      memoryStoredResult.memory.should.deep.equal(Buffer.from(expectedMemory))
+    })
+    it('should correctly overwriteNWordsInMemoryWithOffset', async () => {
+      const numSequentialWordsToStore: number = 10
+      const numSequentialWordsToOverwrite: number = 3
+      const byteOffsetToOverwrite: number = 15
+      const operationBytecode: EVMBytecode = [
+        ...storeNWordsInMemorySequential(numSequentialWordsToStore),
+        ...overwriteNWordsInMemoryWithOffset(
+          numSequentialWordsToOverwrite,
+          byteOffsetToOverwrite
+        ),
+        { opcode: Opcode.RETURN, consumedBytes: undefined },
+      ]
+      const operationBuffer: Buffer = bytecodeToBuffer(operationBytecode)
+
+      const memoryModifiedResult = await evmUtil.getStepContextBeforeStep(
+        operationBuffer,
+        778 // hardcoded  PC val, found via debug log
+      )
+      memoryModifiedResult.stackDepth.should.equal(0)
+      memoryModifiedResult.memoryWordCount.should.equal(
+        numSequentialWordsToStore
+      )
+
+      let expectedMemory: number[] = []
+      for (let i = 0; i < numSequentialWordsToStore; i++) {
+        expectedMemory = expectedMemory.concat(new Array(32).fill(i, 0, 32))
+      }
+      const numBytesOverWritten = 32 * numSequentialWordsToOverwrite
+      expectedMemory.splice(
+        byteOffsetToOverwrite,
+        numBytesOverWritten,
+        ...new Array(numBytesOverWritten).fill(105)
+      ) // 105 is 0x69 in decimal
+
+      memoryModifiedResult.memory.should.deep.equal(Buffer.from(expectedMemory))
+    })
+  })
+
+  it.only('Memory operations between a stash and unstash operation should not have any effect', async () => {
+    const numWordsToStore = 10
+    const memoryModifyingBytecode: EVMBytecode = [
+      ...storeNWordsInMemorySequential(numWordsToStore),
+      ...pointlessOperation, // to be transpiled
+      { opcode: Opcode.RETURN, consumedBytes: undefined },
     ]
-
-    storeSomeValsInMemory.splice(3 * 4, 0, ...pointlessOperation) // 3 is num elements per iteration of MSTOREing, 3*N == insert opp after N memory elements have been written
+    const memoryModifyingBytecodeBuf: Buffer = bytecodeToBuffer(
+      memoryModifyingBytecode
+    )
 
     const memoryIndexToModify: number = 2
     const numWordsToModify: number = 2
-
-    let stashModifyUnstash: EVMBytecode = [
+    // stash memory to stack, overwrite memory, unstash memory
+    const stashModifyUnstash: EVMBytecode = [
       ...staticStashMemoryInStack(memoryIndexToModify, numWordsToModify),
-      ...overwriteNWordsInMemoryWithOffset(2, 2),
+      ...overwriteNWordsInMemoryWithOffset(
+        numWordsToModify,
+        memoryIndexToModify
+      ),
       ...staticUnstashMemoryFromStack(memoryIndexToModify, numWordsToModify),
     ]
 
-    let replaceMap: Map<EVMOpcode, EVMBytecode> = new Map<
+    const replaceMap: Map<EVMOpcode, EVMBytecode> = new Map<
       EVMOpcode,
       EVMBytecode
     >().set(Opcode.POP, [
@@ -136,92 +199,34 @@ describe.only('Memory Replacement Operations', () => {
       ...stashModifyUnstash,
     ])
 
-    opcodeWhitelist = new OpcodeWhitelistImpl(whitelistedOpcodes)
-    replacer = new OpcodeReplacerImpl(stateManagerAddress, replaceMap)
-    transpiler = new TranspilerImpl(opcodeWhitelist, replacer)
-    evmUtil = await EvmIntrospectionUtilImpl.create()
-  })
-  describe('Testing Memory Utils', () => {
-    it('should correctly storeNWordsInMemorySequential', async () => {
-        const numSequentialWordsToStore: number = 10
-        const operationBytecode: EVMBytecode = [
-          ...storeNWordsInMemorySequential(numSequentialWordsToStore),
-          { opcode: Opcode.RETURN, consumedBytes: undefined },
-        ]
-        const operationBuffer: Buffer = bytecodeToBuffer(operationBytecode)
-    
-        const memoryStoredResult = await evmUtil.getStepContextBeforeStep(
-          operationBuffer,
-          670 // hardcoded PC val, found via debug log
-        )
-        memoryStoredResult.stackDepth.should.equal(0)
-        memoryStoredResult.memoryWordCount.should.equal(numSequentialWordsToStore)
-    
-        let expectedMemory: Array<number> =  []
-        for (let i = 0; i < numSequentialWordsToStore; i++) {
-          expectedMemory = expectedMemory.concat(new Array(32).fill(i, 0, 32))
-        }
-        memoryStoredResult.memory.should.deep.equal(Buffer.from(expectedMemory))
-      })
-    it('should correctly overwriteNWordsInMemoryWithOffset', async () => {
-        const numSequentialWordsToStore: number = 10
-        const numSequentialWordsToOverwrite: number = 3
-        const byteOffsetToOverwrite: number = 15
-        const operationBytecode: EVMBytecode = [
-          ...storeNWordsInMemorySequential(numSequentialWordsToStore),
-          ...overwriteNWordsInMemoryWithOffset(numSequentialWordsToOverwrite, byteOffsetToOverwrite),
-          { opcode: Opcode.RETURN, consumedBytes: undefined },
-        ]
-        const operationBuffer: Buffer = bytecodeToBuffer(operationBytecode)
-    
-        const memoryModifiedResult = await evmUtil.getStepContextBeforeStep(
-          operationBuffer,
-          778 // hardcoded  PC val, found via debug log
-        )
-        memoryModifiedResult.stackDepth.should.equal(0)
-        memoryModifiedResult.memoryWordCount.should.equal(numSequentialWordsToStore)
-    
-        let expectedMemory: Array<number> = []
-        for (let i = 0; i < numSequentialWordsToStore; i++) {
-          expectedMemory = expectedMemory.concat(new Array(32).fill(i, 0, 32))
-        }
-        const numBytesOverWritten = 32 * numSequentialWordsToOverwrite
-        expectedMemory.splice(byteOffsetToOverwrite, numBytesOverWritten, ...new Array(numBytesOverWritten).fill(105)) // 105 is 0x69 in decimal
-
-        memoryModifiedResult.memory.should.deep.equal(Buffer.from(expectedMemory))
-      })  
-  })
-  
-  it('Memory operations between a stash and unstash operation should not have any effect', async () => {
-    const memoryOperatingBytecodeBuf: Buffer = bytecodeToBuffer(
-      storeSomeValsInMemory
-    )
-    log.debug(
-      `Running the memory modifying non transpiled code first, it is: \n${formatBytecode(
-        storeSomeValsInMemory
-      )}`
-    )
-    const preTransResult = await evmUtil.getExecutionResult(
-      memoryOperatingBytecodeBuf
-    )
-    log.debug(
-      `pre transpilation execution result is: ${JSON.stringify(preTransResult)}`
-    )
-
+    const opcodeWhitelist = new OpcodeWhitelistImpl(whitelistedOpcodes)
+    const replacer = new OpcodeReplacerImpl(stateManagerAddress, replaceMap)
+    const transpiler = new TranspilerImpl(opcodeWhitelist, replacer)
     const transpilation = transpiler.transpile(
-      memoryOperatingBytecodeBuf
+      memoryModifyingBytecodeBuf
     ) as SuccessfulTranspilation
-    const transpiledMemoryOperations: Buffer = transpilation.bytecode
+    const transpiledMemoryModifyingBytecodeBuf: Buffer = transpilation.bytecode
+
     log.debug(
-      `Running the memory modifying TRANSPILED code second (now), it is: \n${formatBytecode(
-        bufferToBytecode(transpilation.bytecode)
+      `The memory modifying untranspiled bytecode is as follows: \n${formatBytecode(
+        memoryModifyingBytecode
+      )}\nAnd the transpiled version is: \n${formatBytecode(
+        bufferToBytecode(transpiledMemoryModifyingBytecodeBuf)
       )}`
     )
-    const postTransResult = await evmUtil.getExecutionResult(
-      transpiledMemoryOperations
+
+    const comparisonBeforeReturns = await evmUtil.getExecutionComparisonBeforeStep(
+      memoryModifyingBytecodeBuf,
+      673,
+      transpiledMemoryModifyingBytecodeBuf,
+      788
     )
 
-    // const executionResults = await evmUtil.getExecutionResultComparison(memoryOperatingBytecodeBuf, transpiledMemoryOperations)
-    // executionResults.resultsDiffer.should.be.false
+    comparisonBeforeReturns.firstContext.memory.should.deep.equal(
+      comparisonBeforeReturns.secondContext.memory
+    )
+    comparisonBeforeReturns.firstContext.stack.should.deep.equal(
+      comparisonBeforeReturns.secondContext.stack
+    )
   })
 })
