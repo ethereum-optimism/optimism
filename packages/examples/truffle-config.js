ab126f6919505fce1b7566de2994461b425d03ae
Will Meister <william.k.meister@gmail.com>
2020-02-14 08:47:44 -0600

    Configuring OVM in separate truffle config as an easy example of side-by-side standard testing & ovm testing (#72)

--- packages/examples/truffle-config.js
+++ packages/examples/truffle-config.js
@@ -1,8 +1,28 @@
-const HDWalletProvider = require('truffle-hdwallet-provider');
+/**
+ * Use this file to configure your truffle project. It's seeded with some
+ * common settings for different networks and features like migrations,
+ * compilation and testing. Uncomment the ones you need or modify
+ * them to suit your project as necessary.
+ *
+ * More information about configuration can be found at:
+ *
+ * truffleframework.com/docs/advanced/configuration
+ *
+ * To deploy via Infura you'll need a wallet provider (like truffle-hdwallet-provider)
+ * to sign your transactions before they're sent to a remote public node. Infura accounts
+ * are available for free at: infura.io/register.
+ *
+ * You'll also need a mnemonic - the twelve word phrase the wallet uses to generate
+ * public/private key pairs. If you're publishing your code to GitHub make sure you load this
+ * phrase from a file you've .gitignored so it doesn't accidentally become public.
+ *
+ */
+
+// const HDWalletProvider = require('truffle-hdwallet-provider');
 // const infuraKey = "fj4jll3k.....";
 //
 // const fs = require('fs');
-const mnemonic = 'candy maple cake sugar pudding cream honey rich smooth crumble sweet treat'; // fs.readFileSync(".secret").toString().trim();
+// const mnemonic = fs.readFileSync(".secret").toString().trim();
 
 module.exports = {
   /**
@@ -12,44 +32,68 @@ module.exports = {
    * run `develop` or `test`. You can ask a truffle command to use a specific
    * network from the command line, e.g
    *
-   * $ truffle test --network fullnode ./truffle-tests/test-erc20.js
+   * $ truffle test --network <network-name>
    */
 
   networks: {
-    // Note: Requires running the rollup-full-node locally.
-    fullnode: {
-      network_id: 108,
-      provider: function() {
-        const wallet = new HDWalletProvider(mnemonic, "http://127.0.0.1:8545/", 0, 10);
+    // Useful for testing. The `development` name is special - truffle uses it by default
+    // if it's defined here and no other network is specified at the command line.
+    // You should run a client (like ganache-cli, geth or parity) in a separate terminal
+    // tab if you use this network and you must also set the `host`, `port` and `network_id`
+    // options below to some value.
+    //
+    // development: {
+    //  host: "127.0.0.1",     // Localhost (default: none)
+    //  port: 8545,            // Standard Ethereum port (default: none)
+    //  network_id: "*",       // Any network (default: none)
+    // },
 
-        const sendAsync = wallet.sendAsync
+    // Another network with more advanced options...
+    // advanced: {
+    // port: 8777,             // Custom port
+    // network_id: 1342,       // Custom network
+    // gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
+    // gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
+    // from: <address>,        // Account to send txs from (default: accounts[0])
+    // websockets: true        // Enable EventEmitter interface for web3 (default: false)
+    // },
 
-        wallet.sendAsync = function (...args) {
-          if (args[0].method === 'eth_sendTransaction') {
-            // HACK TO PROPERLY SET CHAIN ID
-            args[0].params[0].chainId = 108
-          }
-          sendAsync.apply(this, args)
-        };
+    // Useful for deploying to a public network.
+    // NB: It's important to wrap the provider as a function.
+    // ropsten: {
+    // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
+    // network_id: 3,       // Ropsten's id
+    // gas: 5500000,        // Ropsten has a lower block limit than mainnet
+    // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+    // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+    // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    // },
 
-        return wallet;
-      },
-      gasPrice: 0,
-      gas: 9000000,
-    },
+    // Useful for private networks
+    // private: {
+    // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
+    // network_id: 2111,   // This network is yours, in the cloud.
+    // production: true    // Treats this network as if it was a public net. (default: false)
+    // }
   },
 
   // Set default mocha options here, use special reporters etc.
   mocha: {
-    timeout: 100000
+    // timeout: 100000
   },
 
-
+  // Configure your compilers
   compilers: {
     solc: {
-      // Add path to the solc-transpiler
-      // TODO: will need EXECUTION_MANAGER_ADDRESS environment variable set.
-      version: "../../node_modules/@eth-optimism/solc-transpiler",
+      // version: "0.5.1",    // Fetch exact version from solc-bin (default: truffle's version)
+      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+      // settings: {          // See the solidity docs for advice about optimization and evmVersion
+      //  optimizer: {
+      //    enabled: false,
+      //    runs: 200
+      //  },
+      //  evmVersion: "byzantium"
+      // }
     }
   }
 }
--- packages/examples/truffle-tests/test-erc20.js
+++ packages/examples/truffle-tests/test-erc20.js
@@ -1,6 +1,5 @@
 const TestERC20 = artifacts.require("TestERC20");
 
-// TODO: Run `yarn server:fullnode` in rollup-full-node before executing this test
 contract('TestERC20', accounts => {
     const account = accounts[0];
     const account2 = accounts[1];
@@ -11,22 +10,22 @@ contract('TestERC20', accounts => {
     beforeEach(async () => {
         // Deploy a fresh contract every test
         erc20 = await TestERC20.new(initialValue, 'Test Token', 100, 'TEST',  {from: account});
-    })
+    });
 
     it('gets total supply', async () => {
         const totalSupply = await erc20.totalSupply.call();
         assert.equal(totalSupply, initialValue, 'Total supply mismatch!');
-    })
+    });
 
     it('gets balance of main account', async () => {
         const balance = await erc20.balanceOf.call(account);
         assert.equal(balance, initialValue, 'Sender balance mismatch!');
-    })
+    });
 
     it('gets 0 balance of second account', async () => {
         const balance = await erc20.balanceOf.call(account2);
         assert.equal(balance, 0, 'Second account balance mismatch!');
-    })
+    });
 
     it('transfers from account 1 to account 2', async () => {
         await erc20.transfer(account2, transferValue, {from: account});
@@ -36,7 +35,7 @@ contract('TestERC20', accounts => {
 
         const account2Balance = await erc20.balanceOf.call(account2);
         assert.equal(account2Balance, transferValue, 'Account balance mismatch after transfer!');
-    })
+    });
 
     it('transfers from account 1 to account 2 and then back', async () => {
         await erc20.transfer(account2, transferValue, {from: account});
@@ -47,5 +46,5 @@ contract('TestERC20', accounts => {
 
         const account2Balance = await erc20.balanceOf.call(account2);
         assert.equal(account2Balance, 0, 'Account balance mismatch after transfer!');
-    })
+    });
 });
