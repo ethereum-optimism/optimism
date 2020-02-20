9973526455ee10873226be9fc5ae6d105acd31cd
Will Meister <william.k.meister@gmail.com>
2020-02-20 11:01:20 -0600

    Adding ovm-truffle-provider-wrapper (#12)
    Adding ovm-truffle-provider-wrapper

--- packages/examples/truffle-config-ovm.js
+++ packages/examples/truffle-config-ovm.js
@@ -1,12 +1,12 @@
-const HDWalletProvider = require('truffle-hdwallet-provider');
-
-const mnemonic = 'candy maple cake sugar pudding cream honey rich smooth crumble sweet treat';
+const HDWalletProvider = require("truffle-hdwallet-provider");
+const wrapProvider = require("@eth-optimism/ovm-truffle-provider-wrapper");
+const mnemonic = "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat";
 
 // Set this to the desired Execution Manager Address -- required for the transpiler
 process.env.EXECUTION_MANAGER_ADDRESS = process.env.EXECUTION_MANAGER_ADDRESS || "0xA193E42526F1FEA8C99AF609dcEabf30C1c29fAA";
 const gasPrice = process.env.OVM_DEFAULT_GAS_PRICE || 0;
 const gas = process.env.OVM_DEFAULT_GAS || 9000000;
-const chainId = process.env.OVM_CHAIN_ID || 108;
+
 
 module.exports = {
   /**
@@ -21,17 +21,7 @@ module.exports = {
     test: {
       network_id: 108,
       provider: function() {
-        const wallet = new HDWalletProvider(mnemonic, "http://127.0.0.1:8545/", 0, 10);
-        const sendAsync = wallet.sendAsync
-
-        wallet.sendAsync = function (...args) {
-          if (args[0].method === 'eth_sendTransaction') {
-            // HACK TO PROPERLY SET CHAIN ID
-            args[0].params[0].chainId = chainId
-          }
-          sendAsync.apply(this, args)
-        };
-        return wallet;
+        return wrapProvider(new HDWalletProvider(mnemonic, "http://127.0.0.1:8545/", 0, 10));
       },
       gasPrice: gasPrice,
       gas: gas,
