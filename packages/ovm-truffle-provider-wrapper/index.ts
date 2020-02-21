efe0b36b32bd2139d04aeb74d85c617f092240d8
Will Meister <william.k.meister@gmail.com>
2020-02-21 13:48:27 -0600

    Adding a truffle provider that starts a local node

--- packages/ovm-truffle-provider-wrapper/index.ts
+++ packages/ovm-truffle-provider-wrapper/index.ts
@@ -1,4 +1,25 @@
-module.exports = (provider: any) => {
+import {execSync, spawn} from 'child_process'
+
+const startLocalNode = () => {
+  const runText = `(async function(){ const {runFullnode} = require('@eth-optimism/rollup-full-node');runFullnode();})();`
+
+  // Assumes this process was kicked off with node, but that's true for `truffle test` and `yarn ...`
+  const sub = spawn(process.argv[0], [`-e`, `${runText}`])
+
+  sub.on('error', (e) => {
+    console.error(`Local server could not be started. Error details: ${e.message}, Stack: ${e.stack}`)
+  })
+
+  // This reliably stops the local server when the current process exits.
+  process.on('exit', () => {
+    try {sub.kill()} catch (e) {/*swallow any errors */}
+  });
+
+  // TODO: This is hacky. If host / port become configurable, spawn a node process to ping it or something better.
+  execSync(`sleep 3`)
+}
+
+const wrapProvider = (provider: any) => {
   if (typeof provider !== 'object' || !provider['sendAsync']) {
     throw Error(
       'Invalid provider. Exepcted provider to conform to Truffle provider interface!'
@@ -16,4 +37,19 @@ module.exports = (provider: any) => {
     sendAsync.apply(this, args)
   }
   return provider
+}
+
+let nodeStarted = false
+const wrapProviderAndStartLocalNode = (provider: any) => {
+  if (!nodeStarted) {
+    nodeStarted = true
+    startLocalNode()
+  }
+
+  return wrapProvider(provider)
+}
+
+module.exports = {
+  wrapProvider,
+  wrapProviderAndStartLocalNode
 }
--- packages/rollup-full-node/exec/stopFullnode.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-pkill -f *fullnode.js &> derp.log
