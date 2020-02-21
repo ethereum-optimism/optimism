b451e8abfec1d1478f38e471a7420527fd20bef9
Cesar Rodriguez <cesar@immutability.io>
2020-02-20 22:38:48 -0500

    adds beta provider

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,8 +1,13 @@
 /*
- * GCP Provider - https://www.terraform.io/docs/providers/google/index.html
+ * GCP Provider - https://www.terraform.io/docs/providers/google/guides/provider_reference.html
  * Required for provisioning infrastructure in GCP
  */
 provider "google" {
+  project = var.gcp_project
+  region  = var.gcp_region
+}
+
+provider "google-beta" {
   project = var.gcp_project
   region  = var.gcp_region
 }
