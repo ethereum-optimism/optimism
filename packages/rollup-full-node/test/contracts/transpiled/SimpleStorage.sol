e08149c9461d6e0d982351200232489b2e38e29d
Mason Fischer <mason@kissr.co>
2020-03-05 14:54:52 -0500

    Remove unnecessary changes

---
+++ packages/rollup-full-node/test/contracts/transpiled/SimpleStorage.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.5.0;
+
+contract SimpleStorage {
+    mapping(bytes32 => bytes32) public builtInStorage;
+    function setStorage(bytes32 key, bytes32 value) public {
+        builtInStorage[key] = value;
+    }
+
+    function getStorage(bytes32 key) public view returns (bytes32) {
+        return builtInStorage[key];
+    }
+}
