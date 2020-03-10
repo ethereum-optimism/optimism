0ce64443594b0f54281543ddebe2cd72893eb138
cypherhat <jeff@immutability.io>
2020-03-10 13:01:52 -0400

    populate the unsealer vault with keys

--- unsealer/configure.sh
+++ unsealer/configure.sh
@@ -12,6 +12,8 @@ set -e
 # Usage:
 #   configure.sh
 
+OVPN_FILE="$(pwd)/../infrastructure/unsealer.ovpn"
+
 if [ -z "$VAULT_TOKEN" ]; then
 	echo "Missing VAULT_TOKEN"
 	exit 1
@@ -39,6 +41,12 @@ function secrets {
     vault secrets enable -version=1 kv
     vault write kv/consul_gossip_key value=$(consul keygen)
     vault write kv/unseal_token value=$(vault token create -field=token -ttl="8760h" -policy=autounseal)
+    vault write kv/vault_cacert value=$(cat $HOME/vault.unsealer/root.crt | base64)
+    vault write kv/vault_key value=$(cat $HOME/vault.unsealer/vault.key | base64)
+    vault write kv/vault_crt value=$(cat $HOME/vault.unsealer/vault.crt | base64)
+    if [ -f "$OVPN_FILE" ]; then
+        vault write kv/ovpn_file value=$(cat $OVPN_FILE | base64)
+    fi    
 }
 
 transit
