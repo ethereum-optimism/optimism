6c0b1ca6957cc23cc6e70bd901322d8931085bca
ben-chain <ben@pseudonym.party>
2020-03-16 10:01:30 -0400

    update naming

--- packages/rollup-contracts/contracts/L1ToL2MessagePasser.sol
+++ packages/rollup-contracts/contracts/L1ToL2MessagePasser.sol
@@ -4,14 +4,14 @@ pragma experimental ABIEncoderV2;
 /* Internal Imports */
 import {DataTypes as dt} from "./DataTypes.sol";
 
-contract RollupMessagePasser {
+contract L1ToL2MessagePasser {
     event L1ToL2Message(
         address _sender,
         address _target,
         bytes callData
     );
 
-    function passMessageToRollup(address ovmEntrypoint, bytes memory ovmCalldata) public {
+    function passMessageToL2(address ovmEntrypoint, bytes memory ovmCalldata) public {
         // TODO: Actually create/enqueue a rollup block with this message.  We are simply mocking this functionality for now.
         emit L1ToL2Message(
             msg.sender,
--- packages/rollup-contracts/test/bridge/L1ToL2MessagePasser.spec.ts
+++ packages/rollup-contracts/test/bridge/L1ToL2MessagePasser.spec.ts
@@ -1,7 +1,6 @@
 import '../setup'
 
 /* External Imports */
-import chai = require('chai')
 import {
   createMockProvider,
   deployContract,
@@ -9,26 +8,24 @@ import {
   solidity,
 } from 'ethereum-waffle'
 
-chai.use(solidity)
-
 /* Contract Imports */
-import * as RollupMessagePasser from '../../build/RollupMessagePasser.json'
+import * as L1ToL2MessagePasser from '../../build/L1ToL2MessagePasser.json'
 
-describe('Rollup Message Passer', () => {
+describe('L1 -> L2 Message Passer', () => {
   let provider
   let wallet
-  let rollupMessagePasser
+  let l1ToL2MessagePasser
   beforeEach(async () => {
     provider = createMockProvider()
     wallet = getWallets(provider)[0]
-    rollupMessagePasser = await deployContract(wallet, RollupMessagePasser)
+    l1ToL2MessagePasser = await deployContract(wallet, L1ToL2MessagePasser)
   })
   const entryPoint = '0x0000000000000000000000000000000000001234'
   const callData = '0xdeadbeefee5555'
   it('should emit the correct L1ToL2Message L1 event when an L1->L2 message is sent', async () => {
-    await rollupMessagePasser
-      .passMessageToRollup(entryPoint, callData)
-      .should.emit(rollupMessagePasser, 'L1ToL2Message')
+    await l1ToL2MessagePasser
+      .passMessageToL2(entryPoint, callData)
+      .should.emit(l1ToL2MessagePasser, 'L1ToL2Message')
       .withArgs(wallet.address, entryPoint, callData)
   })
 })
