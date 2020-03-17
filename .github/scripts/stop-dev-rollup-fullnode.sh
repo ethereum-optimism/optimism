c4da8e7b0aa740812ef00453cda03a98114edcc0
Will Meister <william.k.meister@gmail.com>
2020-03-17 18:02:03 -0500

    fixing deploy script

--- .github/scripts/stop-dev-rollup-fullnode.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-TASKS=$(aws ecs list-tasks --cluster dev-rollup-full-node --service-name rollup-full-node)
-
-TASK_ARN=$(echo $TASKS | awk -F\[ '{print $2}' | awk -F\" '{print $2}')
-
-if [ ! -z "${TASK_ARN}" ]; then
-  aws ecs stop-task --cluster dev-rollup-full-node --task $TASK_ARN
-fi
