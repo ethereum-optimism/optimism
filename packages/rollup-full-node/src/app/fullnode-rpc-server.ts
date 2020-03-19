837b39a54c435e57657990ffc337d2498f9d1022
Will Meister <william.k.meister@gmail.com>
2020-03-19 13:00:53 -0500

    handling batch rpc requests

--- packages/rollup-full-node/src/app/fullnode-rpc-server.ts
+++ packages/rollup-full-node/src/app/fullnode-rpc-server.ts
@@ -49,51 +49,62 @@ export class FullnodeRpcServer extends ExpressHttpServer {
    */
   protected initRoutes(): void {
     this.app.post('/', async (req, res) => {
-      let request: JsonRpcRequest
-      try {
-        request = req.body
-        if (!isJsonRpcRequest(request)) {
-          log.debug(`Received request of unsupported format: [${request}]`)
-          return res.json(buildJsonRpcError('INVALID_REQUEST', null))
-        }
-
-        const result = await this.fullnodeHandler.handleRequest(
-          request.method,
-          request.params
-        )
-        return res.json({
-          id: request.id,
-          jsonrpc: request.jsonrpc,
-          result,
-        })
-      } catch (err) {
-        if (err instanceof RevertError) {
-          log.debug(`Request reverted. Request: ${JSON.stringify(request)}`)
-          const errorResponse: JsonRpcErrorResponse = buildJsonRpcError(
-            'REVERT_ERROR',
-            request.id
-          )
-          errorResponse.error.message = err.message
-          return res.json(errorResponse)
-        }
-        if (err instanceof UnsupportedMethodError) {
-          log.debug(
-            `Received request with unsupported method: [${JSON.stringify(
-              request
-            )}]`
-          )
-          return res.json(buildJsonRpcError('METHOD_NOT_FOUND', request.id))
-        } else if (err instanceof InvalidParametersError) {
-          log.debug(
-            `Received request with valid method but invalid parameters: [${JSON.stringify(
-              request
-            )}]`
-          )
-          return res.json(buildJsonRpcError('INVALID_PARAMS', request.id))
-        }
-        logError(log, `Uncaught exception at endpoint-level`, err)
-        return res.json(buildJsonRpcError('INTERNAL_ERROR', request.id))
-      }
+      return res.json(await this.handleRequest(req))
     })
+  }
+
+  protected async handleRequest(req: any): Promise<any> {
+    let request: JsonRpcRequest
+    try {
+      request = req.body
+      if (Array.isArray(request)) {
+        const requestArray: any[] = request as any[]
+        return Promise.all(
+          requestArray.map((x) => this.handleRequest({ body: x }))
+        )
+      }
+
+      if (!isJsonRpcRequest(request)) {
+        log.debug(`Received request of unsupported format: [${request}]`)
+        return buildJsonRpcError('INVALID_REQUEST', null)
+      }
+
+      const result = await this.fullnodeHandler.handleRequest(
+        request.method,
+        request.params
+      )
+      return {
+        id: request.id,
+        jsonrpc: request.jsonrpc,
+        result,
+      }
+    } catch (err) {
+      if (err instanceof RevertError) {
+        log.debug(`Request reverted. Request: ${JSON.stringify(request)}`)
+        const errorResponse: JsonRpcErrorResponse = buildJsonRpcError(
+          'REVERT_ERROR',
+          request.id
+        )
+        errorResponse.error.message = err.message
+        return errorResponse
+      }
+      if (err instanceof UnsupportedMethodError) {
+        log.debug(
+          `Received request with unsupported method: [${JSON.stringify(
+            request
+          )}]`
+        )
+        return buildJsonRpcError('METHOD_NOT_FOUND', request.id)
+      } else if (err instanceof InvalidParametersError) {
+        log.debug(
+          `Received request with valid method but invalid parameters: [${JSON.stringify(
+            request
+          )}]`
+        )
+        return buildJsonRpcError('INVALID_PARAMS', request.id)
+      }
+      logError(log, `Uncaught exception at endpoint-level`, err)
+      return buildJsonRpcError('INTERNAL_ERROR', request.id)
+    }
   }
 }
