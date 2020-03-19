217454f592d604a43143ad912f688ce2c4e00615
Matthew Callens <callensmatt@gmail.com>
2020-03-19 16:18:12 -0400

    working vault/consul sidecar helm chart installation and unsealing

--- unsealer/initialize.sh
+++ unsealer/initialize.sh
@@ -99,6 +99,8 @@ function gencerts {
 	consul tls cert create \
 		-server \
 		-days=120 \
+		-additional-ipaddress="10.8.0.2" \
+		-additional-ipaddress="192.168.64.1" \
 		-additional-dnsname="omisego-consul-server" \
 		-additional-dnsname="*.omisego-consul-server" \
 		-additional-dnsname="*.omisego-consul-server.default" \
@@ -110,50 +112,6 @@ function gencerts {
 	mv consul-agent-ca-key.pem $VAULT_DIR/ca-key.pem
 	mv dc1-server-consul-0.pem $VAULT_DIR/services.pem
 	mv dc1-server-consul-0-key.pem $VAULT_DIR/services-key.pem
-
-	# openssl req \
-	# 		-new \
-	# 		-sha256 \
-	# 		-newkey rsa:2048 \
-	# 		-days 120 \
-	# 		-nodes \
-	# 		-x509 \
-	# 		-subj "/C=TH/ST=Bangkok/L=OmiseGO/O=Unsealer" \
-	# 		-keyout "$VAULT_DIR/ca-key.pem" \
-	# 		-out "$VAULT_DIR/ca.pem"
-
-	# # Generate the private key for the service. 
-	# openssl genrsa -out "$VAULT_DIR/services-key.pem" 2048
-
-	# # Generate a CSR using the configuration and the key just generated. We will
-	# # give this CSR to our CA to sign.
-	# openssl req \
-	# 		-new -key "$VAULT_DIR/services-key.pem" \
-	# 		-out "$VAULT_DIR/services.csr" \
-	# 		-config "config/openssl.cnf"
-
-	# # Sign the CSR with our CA. This will generate a new certificate that is signed
-	# # by our CA.
-	# openssl x509 \
-	# 		-req \
-	# 		-days 120 \
-	# 		-in "$VAULT_DIR/services.csr" \
-	# 		-CA "$VAULT_DIR/ca.pem" \
-	# 		-CAkey "$VAULT_DIR/ca-key.pem" \
-	# 		-CAcreateserial \
-	# 		-sha256 \
-	# 		-extensions v3_req \
-	# 		-extfile "config/openssl.cnf" \
-	# 		-out "$VAULT_DIR/services.pem"
-
-	# openssl x509 -in "$VAULT_DIR/services.pem" -noout -text
-	# cfssl gencert -initca config/ca-csr.json | cfssljson -bare $VAULT_DIR/ca
-	# cfssl gencert \
-	# 	-ca=$VAULT_DIR/ca.pem \
-	# 	-ca-key=$VAULT_DIR/ca-key.pem \
-	# 	-config=config/cfssl.json \
-	# 	config/services-csr.json | cfssljson -bare $VAULT_DIR/services  
-
 }
 
 mkdir -p $VAULT_DIR
