e0345d6e7fa459692d6f477b6ac81962e86817f7
Matthew Callens <callensmatt@gmail.com>
2020-03-24 13:14:49 -0400

    fix vault seal config with tls disabled

--- k8s/terraform/outputs.tf
+++ k8s/terraform/outputs.tf
@@ -9,3 +9,7 @@ output "helm_vault_status" {
 output "vault_service_data" {
   value = jsonencode(data.kubernetes_service.vault)
 }
+
+output "vault_service_ip" {
+  value = data.kubernetes_service.vault.spec[0].type == "ClusterIP" ? data.kubernetes_service.vault.spec[0].cluster_ip : data.kubernetes_service.vault.spec[0].load_balancer_ip
+}
