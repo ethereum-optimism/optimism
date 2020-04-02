239e967cbc3374294ce53c2e0f94432610ef2031
cypherhat <jeff@immutability.io>
2020-04-02 15:00:26 -0400

    configure plugin directory and turn off UI.

--- unsealer/plugin.sh
+++ unsealer/plugin.sh
@@ -3,26 +3,23 @@
 set -ex
 
 function configure_plugin {
-	plugin_file="immutability-eth-plugin"
+	PLUGIN_FILE="immutability-eth-plugin"
+	SHA256SUM="9cafb169cfa7be7574fa0986a15de0119785855b3978ab632f226f4c3509c385"
+	echo "ADDING TO CATALOG: sys/plugins/catalog/secret/$PLUGIN_FILE"
 
-	echo "ADDING TO CATALOG: sys/plugins/catalog/secret/${plugin_file}"
-
-	# just testing for now
-	plugin_file="${plugin_file}"
-	sha256sum=`cat SHA256SUMS | awk '{print $1}'`
-	vault write sys/plugins/catalog/secret/${plugin_file} \
-		  sha_256="$sha256sum" \
-		  command="$plugin_file --ca-cert=/etc/tls/ca/tls.crt --client-cert=/etc/tls/service/tls.crt --client-key=/etc/tls/service/tls.key"
+	vault write sys/plugins/catalog/secret/$PLUGIN_FILE \
+		  sha_256="$SHA256SUM" \
+		  command="$PLUGIN_FILE --ca-cert=/etc/tls/ca/tls.crt --client-cert=/etc/tls/service/tls.crt --client-key=/etc/tls/service/tls.key"
 
 	if [[ $? -eq 2 ]] ; then
 	  echo "Vault Catalog update failed!"
 	  exit 2
 	fi
 
-	echo "MOUNTING: ${plugin_file}"
-	vault secrets enable -path=${plugin_file} -plugin-name=${plugin_file} -description="OmiseGo Plasma Authority Wallet" plugin
+	echo "MOUNTING: $PLUGIN_FILE"
+	vault secrets enable -path=$PLUGIN_FILE -plugin-name=$PLUGIN_FILE -description="OmiseGo Plasma Authority Wallet" plugin
 	if [[ $? -eq 2 ]] ; then
-	  echo "Failed to mount ${plugin_file} plugin for test!"
+	  echo "Failed to mount $PLUGIN_FILE plugin for test!"
 	  exit 2
 	fi
 }
