01259596070989a991b4cc242c774e93955ad323
Will Meister <william.k.meister@gmail.com>
2020-04-22 14:12:46 -0500

    adding locking back into sendRawTransaction to improve latency

--- packages/test-ovm-full-node/src/simple-storage-stress-test.ts
+++ packages/test-ovm-full-node/src/simple-storage-stress-test.ts
@@ -57,4 +57,4 @@ class SimpleStorageStressTest extends FullNodeStressTest {
   }
 }
 
-new SimpleStorageStressTest(10, 'http://0.0.0.0:8545').runBatches(1000)
+new SimpleStorageStressTest(100, 'http://3.14.246.203:8545').runBatches(100)
