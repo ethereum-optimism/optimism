95d9449ce3daf08ca672378d1034dbcc47817e0d
Will Meister <william.k.meister@gmail.com>
2020-04-29 15:30:53 -0500

    Ability to clear The Graph data (#121)
    * Adding ability to clear The Graph data by setting CLEAR_DATA_KEY environment variable

--- docker/the-graph/wait-for-ovm.sh
+++ docker/the-graph/wait-for-ovm.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 # wait-for-ovm.sh <ovm url with port>
+# NOTE: set the CLEAR_DATA_KEY environment variable to clear the /data directory on startup.
+# Directory will only be cleared if CLEAR_DATA_KEY is set AND different from last start.
 
 set -e
 
@@ -28,6 +30,23 @@ wait_for_server_to_be_reachable()
 
 
 }
+
+clear_data_if_necessary()
+{
+  DATA_DIRECTORY=${DATA_DIRECTORY:-/data}
+  CLEAR_DATA_FILE_PATH="$DATA_DIRECTORY/.clear_data_key_$CLEAR_DATA_KEY"
+
+  if [[ -n "$CLEAR_DATA_KEY" && ! -f "$CLEAR_DATA_FILE_PATH" ]]; then
+    echo "Detected change in CLEAR_DATA_KEY. Purging data."
+    rm -rf ${DATA_DIRECTORY}/*
+    rm -rf ${DATA_DIRECTORY}/.clear_data_key_*
+    echo "Local data cleared from '${DATA_DIRECTORY}/*'"
+    echo "Contents of data dir: $(ls -alh $DATA_DIRECTORY)"
+    touch $CLEAR_DATA_FILE_PATH
+  fi
+}
+
+clear_data_if_necessary
 
 wait_for_server_to_be_reachable $OVM_URL_WITH_PORT
 
