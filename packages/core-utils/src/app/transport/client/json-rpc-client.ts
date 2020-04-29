df25629d772a0b598324b257f46acf1c5b3f558b
Will Meister <william.k.meister@gmail.com>
2020-04-29 10:45:13 -0500

    Making Router handle JSON RPC Errors (#117)
    * Making it so the router does not throw on properly-formatted JSON errors and instead passes them through

--- packages/core-utils/src/app/transport/client/json-rpc-client.ts
+++ packages/core-utils/src/app/transport/client/json-rpc-client.ts
@@ -8,6 +8,7 @@ import {
   JsonRpcRequest,
   Client,
   isJsonRpcErrorResponse,
+  JsonRpcResponse,
 } from '../../../types'
 
 /**
@@ -23,11 +24,34 @@ export class JsonRpcClient<TransportRequest, TransportResponse>
   /**
    * Handles a method call by making a JSON-RPC
    * request to some server.
+   *
+   * @param method Name of the method to call.
+   * @param [params] Parameters to send with the method call.
+   * @returns the `result` field of the response to the method call.
+   * @throws Error if there is any error, including a properly-formatted JsonRpcResponse error.
+   */
+  public async handle<T>(method: string, params?: any): Promise<T> {
+    const response: JsonRpcResponse = await this.makeRpcCall(method, params)
+
+    if (isJsonRpcErrorResponse(response)) {
+      throw new Error(`${JSON.stringify(response.error)}`)
+    }
+    return response.result
+  }
+
+  /**
+   * Makes an RPC call and returns the full JsonRpcResponse.
+   * Notably, this differs from handle<T>(...) because it does not throw on error
+   * or just return the `result` field on success.
+   *
    * @param method Name of the method to call.
    * @param [params] Parameters to send with the method call.
    * @returns the result of the method call.
    */
-  public async handle<T>(method: string, params?: any): Promise<T> {
+  public async makeRpcCall(
+    method: string,
+    params?: any
+  ): Promise<JsonRpcResponse> {
     const request: JsonRpcRequest = {
       jsonrpc: '2.0',
       method,
@@ -37,11 +61,6 @@ export class JsonRpcClient<TransportRequest, TransportResponse>
 
     const encodedRequest = this.adapter.encodeRequest(request)
     const encodedResponse = await this.client.request(encodedRequest)
-    const response = this.adapter.decodeResponse(encodedResponse)
-
-    if (isJsonRpcErrorResponse(response)) {
-      throw new Error(`${JSON.stringify(response.error)}`)
-    }
-    return response.result
+    return this.adapter.decodeResponse(encodedResponse)
   }
 }
