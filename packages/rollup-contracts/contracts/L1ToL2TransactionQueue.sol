f42661dbee149f55b76716f71b4ebe942c187126
Kevin Ho <kevinjho1996@gmail.com>
2020-05-13 17:23:54 -0400

    add L1ToL2TransactionQueue, tests

---
+++ packages/rollup-contracts/contracts/L1ToL2TransactionQueue.sol
@@ -0,0 +1,29 @@
+pragma solidity ^0.5.0;
+pragma experimental ABIEncoderV2;
+
+/* Internal Imports */
+import {DataTypes as dt} from "./DataTypes.sol";
+import {RollupMerkleUtils} from "./RollupMerkleUtils.sol";
+import {RollupList} from "./RollupList.sol";
+
+contract L1ToL2TransactionQueue is RollupList {
+  address public l1ToL2TransactionPasser;
+  address public canonicalTransactionChain;
+
+  constructor(
+    address _rollupMerkleUtilsAddress,
+    address _l1ToL2TransactionPasser,
+    address _canonicalTransactionChain
+  ) RollupList(_rollupMerkleUtilsAddress) public {
+    l1ToL2TransactionPasser = _l1ToL2TransactionPasser;
+    canonicalTransactionChain = _canonicalTransactionChain;
+  }
+
+  function authenticateEnqueue(address _sender) public view returns (bool) {
+    return _sender == l1ToL2TransactionPasser;
+  }
+  function authenticateDequeue(address _sender) public view returns (bool) {
+    return _sender == canonicalTransactionChain;
+  }
+  function authenticateDelete(address _sender) public view returns (bool) { return false; }
+}
