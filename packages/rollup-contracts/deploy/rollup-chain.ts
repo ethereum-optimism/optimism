5676abaf5be9cffa017e9869e969087f092f1ec2
Kevin Ho <kevinjho1996@gmail.com>
2020-06-04 18:06:45 -0400

    full working deployment script

--- packages/rollup-contracts/deploy/rollup-chain.ts
+++ packages/rollup-contracts/deploy/rollup-chain.ts
@@ -3,38 +3,81 @@ import { deploy, deployContract } from '@eth-optimism/core-utils'
 import { Wallet } from 'ethers'
 
 /* Internal Imports */
-import * as RollupMerkleUtils from '../build/contracts/RollupMerkleUtils.json'
-import * as CanonicalTransactionChain from '../build/contracts/CanonicalTransactionChain.json'
+import * as RollupMerkleUtils from '../build/RollupMerkleUtils.json'
+import * as CanonicalTransactionChain from '../build/CanonicalTransactionChain.json'
+import * as StateCommitmentChain from '../build/StateCommitmentChain.json'
+import * as SequencerBatchSubmitter from '../build/SequencerBatchSubmitter.json'
+
 import { resolve } from 'path'
 
 const rollupChainDeploymentFunction = async (
   wallet: Wallet
 ): Promise<string> => {
   console.log(`\nDeploying Rollup Chain!\n`)
-  //TODO fix this 
-  const l1ToL2TransactionPasser = wallet
-  const FORCE_INCLUSION_PERIOD = 600
 
+  const l1ToL2TransactionPasser = wallet // TODO actually deploy l1ToL2TransactionPasser
+  const FORCE_INCLUSION_PERIOD = 600 // TODO set this in the config
+  const fraudVerifier = wallet // TODO actually deploy Fraud Verifier
+  const sequencer = wallet // TODO set the sequencer address in the config
+
+  console.log(
+    `Deploying RollupMerkleUtils...`
+  )
   const rollupMerkleUtils = await deployContract(RollupMerkleUtils, wallet)
 
+  console.log(
+    `Deploying SequencerBatchSubmitter...`
+  )
+  const sequencerBatchSubmitter = await deployContract(
+    SequencerBatchSubmitter,
+    wallet,
+    sequencer.address
+  )
+
+  console.log(
+    `Deploying CanonicalTransactionChain...`
+  )
   const canonicalTxChain = await deployContract(
     CanonicalTransactionChain,
     wallet,
     rollupMerkleUtils.address,
-    wallet.address,
+    sequencerBatchSubmitter.address,
     l1ToL2TransactionPasser.address,
-    FORCE_INCLUSION_PERIOD,
+    FORCE_INCLUSION_PERIOD
   )
 
   const l1ToL2QueueAddress = await canonicalTxChain.l1ToL2Queue()
-  
+
   const safetyQueueAddress = await canonicalTxChain.safetyQueue()
 
-  console.log(`Canonical Transaction Chain deployed to ${canonicalTxChain.address}!\n\n`)
-  console.log(`Rollup Merkle Utils deployed to ${rollupMerkleUtils.address}!\n\n`)
-  console.log(`L1-to-L2 Transaction Queue deployed to ${l1ToL2QueueAddress}!\n\n`)
-  console.log(`Safety Transaction Queue deployed to ${safetyQueueAddress}!\n\n`)
+  console.log(
+    `Deploying StateCommitmentChain...`
+  )
+  const stateChain = await deployContract(
+    StateCommitmentChain,
+    wallet,
+    rollupMerkleUtils.address,
+    canonicalTxChain.address,
+    fraudVerifier.address
+  )
 
+  console.log(
+    `Initializing SequencerBatchSubmitter with chain addresses...`
+  )
+  await sequencerBatchSubmitter.initialize(canonicalTxChain.address, stateChain.address)
+
+  console.log(
+    `\nRollup Merkle Utils deployed to ${rollupMerkleUtils.address}!\n`
+  )
+  console.log(
+    `Canonical Transaction Chain deployed to ${canonicalTxChain.address}!\n`
+  )
+  console.log(
+    `L1-to-L2 Transaction Queue deployed to ${l1ToL2QueueAddress}!\n`
+  )
+  console.log(`Safety Transaction Queue deployed to ${safetyQueueAddress}!\n`)
+  console.log(`State Commitment Chain deployed to ${stateChain.address}!\n`)
+  console.log(`Sequencer Batch Submitter deployed to ${sequencerBatchSubmitter.address}!\n`)
   return canonicalTxChain.address
 }
 
