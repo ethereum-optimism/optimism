f810f3babdcfba215ead756ca3501f67dfd04ab1
Kelvin Fichter <kelvinfichter@gmail.com>
2020-06-24 14:20:09 -0400

    Adds (unoptimized) functions to MerkleTrie.sol (#160)
    * Added MerkleTrie.sol and deps
    
    * Added various randomly generated tests
    
    * Removed an unused logging line
    
    * Removed dummy package script
    
    * Removed unused variable
    
    * Various stylistic fixes
    
    * Adding some temp changes
    
    * Fixed various bugs
    
    * Removed old merkle trie file
    
    * Added initial comments
    
    * Linted and added comments to tests
    
    * Added more comments

--- packages/rollup-contracts/test/setup.ts
+++ packages/rollup-contracts/test/setup.ts
@@ -1,10 +1,13 @@
 /* External Imports */
 import chai = require('chai')
+import chaiAsPromised = require('chai-as-promised')
 import bignum = require('chai-bignumber')
 import { solidity } from 'ethereum-waffle'
 
 chai.use(bignum())
+chai.use(chaiAsPromised)
 chai.use(solidity)
 const should = chai.should()
+const expect = chai.expect
 
-export { should }
+export { should, expect }
