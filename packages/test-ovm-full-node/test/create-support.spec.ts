197700b0e0ec6b2d21316a621e0c83aa93ec8957
Kevin Ho <kevinjho1996@gmail.com>
2020-06-25 21:16:24 -0400

    lint

--- packages/test-ovm-full-node/test/create-support.spec.ts
+++ packages/test-ovm-full-node/test/create-support.spec.ts
@@ -5,11 +5,7 @@ import { add0x } from '@eth-optimism/core-utils'
 import { createMockProvider, deployContract, getWallets } from 'ethereum-waffle'
 import { addHandlerToProvider } from '@eth-optimism/rollup-full-node'
 import { Contract, Wallet } from 'ethers'
-import {
-  getAddress,
-  keccak256,
-  solidityPack
-} from 'ethers/utils'
+import { getAddress, keccak256, solidityPack } from 'ethers/utils'
 
 /* Contract Imports */
 import * as SimpleCreate2 from '../build/SimpleCreate2.json'
@@ -20,21 +16,17 @@ const getCreate2Address = (
   salt: string,
   bytecode: string
 ): string => {
-  const create2Inputs = [
-    '0xff',
-    factoryAddress,
-    salt,
-    keccak256(bytecode)
-  ]
-  const sanitizedInputs = `0x${create2Inputs.map(i => i.slice(2)).join('')}`
+  const create2Inputs = ['0xff', factoryAddress, salt, keccak256(bytecode)]
+  const sanitizedInputs = `0x${create2Inputs.map((i) => i.slice(2)).join('')}`
   return getAddress(`0x${keccak256(sanitizedInputs).slice(-40)}`)
 }
 
 describe('Create2', () => {
   let wallet
   let simpleCreate2: Contract
-  let provider 
-  const DEFAULT_SALT = '0x1234123412341234123412341234123412341234123412341234123412341234'
+  let provider
+  const DEFAULT_SALT =
+    '0x1234123412341234123412341234123412341234123412341234123412341234'
 
   beforeEach(async () => {
     provider = await createMockProvider()
@@ -42,29 +34,34 @@ describe('Create2', () => {
       provider = await addHandlerToProvider(provider)
     }
     const wallets = await getWallets(provider)
-    const wallet = wallets[0]
+    wallet = wallets[0]
     simpleCreate2 = await deployContract(wallet, SimpleCreate2, [])
   })
 
-  // TODO uncomment this once ovmCREATE2 is fixed!
-  // it('should calculate address correctly for invalid bytecode', async () => {
-  //   const bytecode = '0x00'
-  //   const salt = DEFAULT_SALT
-  //   const tx = await simpleCreate2.create2(bytecode, salt)
-  //   const receipt = await provider.getTransactionReceipt(tx.hash)
-  //   const address = await simpleCreate2.contractAddress()
-  //   const expectedAddress = getCreate2Address(simpleCreate2.address, salt, bytecode)
-  //   address.should.equal(expectedAddress)
-  // })
+  // TODO unskip this once ovmCREATE2 is fixed with YAS-473!
+  it.skip('should calculate address correctly for invalid bytecode', async () => {
+    const bytecode = '0x00'
+    const salt = DEFAULT_SALT
+    await simpleCreate2.create2(bytecode, salt)
+    const address = await simpleCreate2.contractAddress()
+    const expectedAddress = getCreate2Address(
+      simpleCreate2.address,
+      salt,
+      bytecode
+    )
+    address.should.equal(expectedAddress)
+  })
 
   it('should calculate address correctly for valid OVM bytecode', async () => {
     const bytecode = add0x(SimpleStorage.bytecode)
     const salt = DEFAULT_SALT
-    const tx = await simpleCreate2.create2(bytecode, salt)
-    const receipt = await provider.getTransactionReceipt(tx.hash)
+    await simpleCreate2.create2(bytecode, salt)
     const address = await simpleCreate2.contractAddress()
-    const expectedAddress = getCreate2Address(simpleCreate2.address, salt, bytecode)
+    const expectedAddress = getCreate2Address(
+      simpleCreate2.address,
+      salt,
+      bytecode
+    )
     address.should.equal(expectedAddress)
   })
 })
-
