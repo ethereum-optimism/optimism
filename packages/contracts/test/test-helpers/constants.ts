53d8b032d27153510d30232237e97da7b7f59c1b
Kevin Ho <kevinjho1996@gmail.com>
2020-07-12 22:12:45 -0400

    Add CHAINID (#182)
    * add CHAINID to transpiler
    
    * add ovmCHAINID to execution manager
    
    * remove comments, add CHAINID to unsafe opcodes
    
    * update call outsize
    
    * increase message passer sleep
    
    * increase message passer sleep MORE
    
    * update return sizes

--- packages/contracts/test/test-helpers/constants.ts
+++ packages/contracts/test/test-helpers/constants.ts
@@ -21,6 +21,7 @@ export const DEFAULT_UNSAFE_OPCODES: EVMOpcode[] = [
   Opcode.BLOCKHASH,
   Opcode.CALLCODE,
   Opcode.CALLER,
+  Opcode.CHAINID,
   Opcode.COINBASE,
   Opcode.CREATE,
   Opcode.CREATE2,
@@ -43,7 +44,8 @@ export const DEFAULT_UNSAFE_OPCODES: EVMOpcode[] = [
 
 export const GAS_LIMIT = 1_000_000_000
 export const DEFAULT_OPCODE_WHITELIST_MASK =
-  '0x600a0000000000000000001fffffffffffffffff0fcf004063f000013fff0fff'
+  '0x600a0000000000000000001fffffffffffffffff0fcf000063f000013fff0fff'
+
 export const L2_TO_L1_MESSAGE_PASSER_OVM_ADDRESS =
   '0x4200000000000000000000000000000000000000'
 
