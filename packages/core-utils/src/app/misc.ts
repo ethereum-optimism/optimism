ea5cd804fcd62f429abda84f4e83869697896216
Will Meister <william.k.meister@gmail.com>
2020-07-13 14:01:15 -0500

    fixing regression

--- packages/core-utils/src/app/misc.ts
+++ packages/core-utils/src/app/misc.ts
@@ -80,7 +80,7 @@ export const sleep = (ms: number): Promise<void> => {
  * @returns the string without "0x".
  */
 export const remove0x = (str: string): string => {
-  if (!str) {
+  if (str === undefined) {
     return str
   }
   return str.startsWith('0x') ? str.slice(2) : str
@@ -92,7 +92,7 @@ export const remove0x = (str: string): string => {
  * @returns the string with "0x".
  */
 export const add0x = (str: string): string => {
-  if (!str) {
+  if (str === undefined) {
     return str
   }
   return str.startsWith('0x') ? str : '0x' + str
