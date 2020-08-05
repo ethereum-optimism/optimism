7dbfe753059994a616191fdae5e784e3c5edc7cd
ben-chain <ben@pseudonym.party>
2020-08-05 14:00:24 -0400

    util for exact gas consumption

--- packages/contracts/contracts/optimistic-ethereum/utils/GasConsumer.sol
+++ packages/contracts/contracts/optimistic-ethereum/utils/GasConsumer.sol
@@ -1,12 +1,32 @@
 pragma solidity ^0.5.0;
 
 contract GasConsumer {
+
+    // default fallback should consume all allotted gas
+    function() external {
+        uint i;
+        while (true) {
+            i++;
+        }
+    }
+
     function consumeGasExceeding(uint _amount) public view {
         uint startGas = gasleft();
         while (true) {
             if (startGas - gasleft() > _amount) {
                 return;
             }
+        }
+    }
+
+    // overhead for checking methodId etc in this function before the actual call().
+    // This was figured out empirically during testing.
+    uint constant constantOverhead = 969;
+    function consumeGasExact(uint _amount) external {
+        uint gasToAlloc = _amount - constantOverhead;
+        // call our fallback which consumes all allocated gas
+        assembly {
+            pop(call(gasToAlloc, address, 0, 0, 0, 0, 0))
         }
     }
 }
