f7a8abe76279ab2e5dc15ad24b939c21dcf0f4d4
ben-chain <ben@pseudonym.party>
2020-08-06 12:36:15 -0400

    bump timing out test

--- packages/rollup-dev-tools/test/transpiler/jump-integration.spec.ts
+++ packages/rollup-dev-tools/test/transpiler/jump-integration.spec.ts
@@ -138,7 +138,7 @@ describe('JUMP table solidity integration', () => {
       paramTypes,
       callParams
     )
-  }).timeout(25000)
+  }).timeout(35000)
 })
 
 const assertCallsProduceSameResult = async (
