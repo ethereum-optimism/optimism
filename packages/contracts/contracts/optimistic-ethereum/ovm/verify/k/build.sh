e6b96baa40dffa45851340c70249d657eb30ab65
George Hotz <geohot@gmail.com>
2020-08-14 17:07:21 -0400

    klab build almost works

--- packages/contracts/contracts/optimistic-ethereum/ovm/verify/k/build.sh
+++ packages/contracts/contracts/optimistic-ethereum/ovm/verify/k/build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-CDD=$PWD
-cd ../../../
-solc --allow-paths . --combined-json=abi,bin,bin-runtime,srcmap,srcmap-runtime,ast ovm/SafetyChecker.sol > $CDD/dapp/out/SafetyChecker.sol.json
+cd dapp
+solc --allow-paths . --combined-json=abi,bin,bin-runtime,srcmap,srcmap-runtime,ast src/SafetyChecker.sol > out/dapp.sol.json
 
 
