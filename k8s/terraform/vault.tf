73ca11253c20429ce09c97b8e2ac25627fec787e
Matthew Callens <callensmatt@gmail.com>
2020-08-16 19:44:45 -0400

    remove unused terraform

--- k8s/terraform/vault.tf
+++
@@ -1,9 +0,0 @@
-/*
- * Vault Secret - https://www.terraform.io/docs/providers/vault/d/generic_secret.html
- * Loads the unseal Vault token from the running Vault node
- * to be injected into the K8S Vault configuration to unseal
- * themselves once them come online and are ready
- */
-data "vault_generic_secret" "unseal_token" {
-  path = "kv/unseal_token"
-}
