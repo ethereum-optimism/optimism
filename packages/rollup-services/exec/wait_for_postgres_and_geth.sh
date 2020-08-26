343f7c1845b73f43358459543801b66d8fe807ea
Will Meister <william.k.meister@gmail.com>
2020-08-26 17:51:53 -0500

    reverting change in wait_for_postgres_and_geth.sh

--- packages/rollup-services/exec/wait_for_postgres_and_geth.sh
+++ packages/rollup-services/exec/wait_for_postgres_and_geth.sh
@@ -8,7 +8,7 @@ RETRIES=30
 
 echo "Connection info: ${POSTGRES_HOST}:${POSTGRES_PORT}, user=${POSTGRES_USER}, postgres db=${POSTGRES_DATABASE}"
 
-until PGPASSWORD=$POSTGRES_PASSWORD psql -h $POSTGRES_HOST -p $POSTGRES_PORT -U $POSTGRES_USER -d $POSTGRES_DATABASE -c "select 1" >&1 || [ $RETRIES -eq 0 ]; do
+until PGPASSWORD=$POSTGRES_PASSWORD psql -h $POSTGRES_HOST -p $POSTGRES_PORT -U $POSTGRES_USER -d $POSTGRES_DATABASE -c "select 1" > /dev/null 2>&1 || [ $RETRIES -eq 0 ]; do
   echo "Waiting for Postgres server, $((RETRIES--)) remaining attempts..."
   sleep 1
 done
