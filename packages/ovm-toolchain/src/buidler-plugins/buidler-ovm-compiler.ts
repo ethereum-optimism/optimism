a4fead5b272252bdc679a68235956f065dfad4c1
Kevin Ho <kevinjho1996@gmail.com>
2020-09-01 12:16:50 -0400

    add native solc option

--- packages/ovm-toolchain/src/buidler-plugins/buidler-ovm-compiler.ts
+++ packages/ovm-toolchain/src/buidler-plugins/buidler-ovm-compiler.ts
@@ -4,6 +4,7 @@ import { internalTask } from '@nomiclabs/buidler/config'
 import { SolcInput } from '@nomiclabs/buidler/types'
 import { Compiler } from '@nomiclabs/buidler/internal/solidity/compiler'
 import { TASK_COMPILE_RUN_COMPILER } from '@nomiclabs/buidler/builtin-tasks/task-names'
+import { execSync } from 'child_process'
 
 internalTask(TASK_COMPILE_RUN_COMPILER).setAction(
   async ({ input }: { input: SolcInput }, { config }) => {
@@ -22,7 +23,14 @@ internalTask(TASK_COMPILE_RUN_COMPILER).setAction(
         return customCompiler
       }
     }
-
-    return compiler.compile(input)
+    if ((config as any).solc.native === true) {
+      console.log(`Using native solidity compiler`)
+      const output = execSync(`solc --standard-json`, {
+        input: JSON.stringify(input, undefined, 2),
+      })
+      return JSON.parse(output.toString('utf8'))
+    } else {
+      return compiler.compile(input)
+    }
   }
 )
