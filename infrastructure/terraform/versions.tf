ba71bc64abd0251dcdd1d680404435d7748abcc1
Matthew Callens <callensmatt@gmail.com>
2020-09-17 20:35:35 -0400

    clean out provider versions file for terraform

--- infrastructure/terraform/versions.tf
+++ infrastructure/terraform/versions.tf
@@ -1,16 +1,10 @@
 terraform {
   required_providers {
-    datadog = {
-      source = "terraform-providers/datadog"
-    }
     google = {
       source = "hashicorp/google"
     }
     google-beta = {
       source = "hashicorp/google-beta"
-    }
-    local = {
-      source = "hashicorp/local"
     }
   }
   required_version = ">= 0.13"
