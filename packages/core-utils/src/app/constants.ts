b04a1db964f8c5938cc02a459d8dc4cdb0a79d75
Mark Tyneway <mark.tyneway@gmail.com>
2020-09-22 16:49:08 -0700

    constants: add chainid

--- packages/core-utils/src/app/constants.ts
+++ packages/core-utils/src/app/constants.ts
@@ -1,3 +1,3 @@
 export const ZERO_ADDRESS = '0x' + '00'.repeat(20)
 export const INVALID_ADDRESS = '0xdeaDDeADDEaDdeaDdEAddEADDEAdDeadDEADDEaD'
-export const CHAIN_ID = 108
+export const CHAIN_ID = 420
