1496b3ee33f254f812571b84d4489c87847e973c
Will Meister <william.k.meister@gmail.com>
2020-09-24 22:41:36 -0500

    fixing tests

--- packages/rollup-core/src/app/constants.ts
+++ packages/rollup-core/src/app/constants.ts
@@ -7,7 +7,7 @@ export const L1ToL2TransactionBatchEventName = 'NewTransactionBatchAdded'
 export const CREATOR_CONTRACT_ADDRESS = ZERO_ADDRESS
 export const GAS_LIMIT = 1_000_000_000
 
-export const CHAIN_ID = 108
+export const CHAIN_ID = 420
 
 export const DEFAULT_UNSAFE_OPCODES: EVMOpcode[] = [
   Opcode.ADDRESS,
