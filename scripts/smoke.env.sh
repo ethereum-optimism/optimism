f28e8df84b39a895d6bfcd4f1b347fc66bd62b1f
Ino Murko <ino.murko@outlook.com>
2020-10-02 17:12:03 +0200

    sequential nonce

--- scripts/smoke.env.sh
+++ scripts/smoke.env.sh
@@ -8,4 +8,3 @@ export CONTRACTS_PATH="/home/vault/contracts/erc20/build/"
 export PLASMA_CONTRACT=`cat /truffleshuffle/plasma_framework_addr.out`
 export GAS_PRICE_LOW="1"
 export GAS_PRICE_HIGH="37000000000"
-export NONCE="111"
