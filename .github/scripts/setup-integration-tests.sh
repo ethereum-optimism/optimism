ba36b5f0b85abf738d5a06fdefe660902a7d518a
Mark Tyneway <mark.tyneway@gmail.com>
2020-10-07 06:46:31 -0700

    github actions: more debugging

--- .github/scripts/setup-integration-tests.sh
+++ .github/scripts/setup-integration-tests.sh
@@ -8,14 +8,11 @@ REPO=$(echo $GITHUB_REPOSITORY | cut -d '/' -f2)
 
 cd $HOME/optimism-integration/$REPO
 
-echo "GITHUB_SHA"
-echo $GITHUB_SHA
-
-echo "pwd $PWD"
-
 # TODO: this will not work for outside contributors
 git fetch origin $GITHUB_SHA
 git checkout $GITHUB_SHA
 
-$HOME/optimism-integration/build.sh
-$HOME/optimism-integration/test.sh
+cd $HOME/optimism-integration
+
+./build.sh
+./test.sh
