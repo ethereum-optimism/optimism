64b5f6d37591f3d13e3f20154c9fe9937deec6fe
Karl Floersch <karl@karlfloersch.com>
2020-10-20 13:03:28 -0400

    Add initial batch-submitter structure

---
+++ packages/batch-submitter/test/batch-submitter/batch-submitter.spec.ts
@@ -0,0 +1,14 @@
+import '../setup'
+
+/* Internal Imports */
+import { BatchSubmitter } from '../../src/batch-submitter'
+
+describe('BatchSubmitter', () => {
+  describe('Submit', () => {
+    it('should print', () => {
+        console.log('Hello there!')
+        const test = new BatchSubmitter()
+    })
+  })
+})
+
---
+++ packages/batch-submitter/test/setup.ts
@@ -0,0 +1,8 @@
+/* External Imports */
+import chai = require('chai')
+import Mocha from 'mocha'
+
+const should = chai.should()
+const expect = chai.expect
+
+export { should, expect, Mocha }
