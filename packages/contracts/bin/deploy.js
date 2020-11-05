0bb89f9bab2ad8af5187e4118cd2fca357fe3578
Mark Tyneway <mark.tyneway@gmail.com>
2020-11-05 09:43:21 -0800

    deployer: ledger support, no sequencer private key (#50)

--- packages/contracts/bin/deploy.js
+++ packages/contracts/bin/deploy.js
@@ -1,12 +1,14 @@
 #!/usr/bin/env node
 
 const contracts = require('../build/src/contract-deployment/deploy');
-const { providers, Wallet } = require('ethers');
+const { providers, Wallet, utils } = require('ethers');
+const { LedgerSigner } = require('@ethersproject/hardware-wallets');
 const { JsonRpcProvider } = providers;
 
 const env = process.env;
 const key = env.DEPLOYER_PRIVATE_KEY;
 const sequencerKey = env.SEQUENCER_PRIVATE_KEY;
+let SEQUENCER_ADDRESS = env.SEQUENCER_ADDRESS;
 const web3Url = env.L1_NODE_WEB3_URL || 'http://127.0.0.1:8545';
 const MIN_TRANSACTION_GAS_LIMIT = env.MIN_TRANSACTION_GAS_LIMIT || 0;
 const MAX_TRANSACTION_GAS_LIMIT = env.MAX_TRANSACTION_GAS_LIMIT || 1000000000;
@@ -16,17 +18,30 @@ let WHITELIST_OWNER = env.WHITELIST_OWNER;
 const WHITELIST_ALLOW_ARBITRARY_CONTRACT_DEPLOYMENT = env.WHITELIST_ALLOW_ARBITRARY_CONTRACT_DEPLOYMENT || true;
 const FORCE_INCLUSION_PERIOD_SECONDS = env.FORCE_INCLUSION_PERIOD_SECONDS || (30 * 60);
 const CHAIN_ID = env.CHAIN_ID || 420; // layer 2 chainid
+const USE_LEDGER = env.USE_LEDGER || false;
+const HD_PATH = env.HD_PATH || utils.defaultPath;
 
 (async () => {
-  if (typeof key === 'undefined')
-    throw new Error('Must pass deployer key as DEPLOYER_PRIVATE_KEY');
-
-  if (typeof sequencerKey === 'undefined')
-    throw new Error('Must pass sequencer key as SEQUENCER_PRIVATE_KEY');
-
   const provider = new JsonRpcProvider(web3Url);
-  const signer = new Wallet(key, provider);
-  const sequencer = new Wallet(sequencerKey, provider);
+  let signer;
+
+  if (USE_LEDGER) {
+    signer = new LedgerSigner(provider, 'default', HD_PATH);
+  } else  {
+    if (typeof key === 'undefined')
+      throw new Error('Must pass deployer key as DEPLOYER_PRIVATE_KEY');
+    signer = new Wallet(key, provider);
+  }
+
+  if (SEQUENCER_ADDRESS) {
+    if (!utils.isAddress(SEQUENCER_ADDRESS))
+      throw new Error(`Invalid Sequencer Address: ${SEQUENCER_ADDRESS}`)
+  } else {
+    if (!sequencerKey)
+      throw new Error('Must pass sequencer key as SEQUENCER_PRIVATE_KEY');
+    const sequencer = new Wallet(sequencerKey, provider);
+    SEQUENCER_ADDRESS = await sequencer.getAddress()
+  }
 
   if (typeof WHITELIST_OWNER === 'undefined')
     WHITELIST_OWNER = signer;
@@ -35,7 +50,7 @@ const CHAIN_ID = env.CHAIN_ID || 420; // layer 2 chainid
     deploymentSigner: signer,
     transactionChainConfig: {
       forceInclusionPeriodSeconds: FORCE_INCLUSION_PERIOD_SECONDS,
-      sequencer,
+      sequencer: SEQUENCER_ADDRESS,
     },
     ovmGlobalContext: {
       ovmCHAINID: CHAIN_ID
@@ -58,7 +73,7 @@ const CHAIN_ID = env.CHAIN_ID || 420; // layer 2 chainid
 
   const out = {};
   out.AddressManager = AddressManager.address;
-  out.OVM_Sequencer = await sequencer.getAddress()
+  out.OVM_Sequencer = SEQUENCER_ADDRESS;
   out.Deployer = await signer.getAddress()
   for (const [name, contract] of Object.entries(result.contracts)) {
     out[name] = contract.address;
