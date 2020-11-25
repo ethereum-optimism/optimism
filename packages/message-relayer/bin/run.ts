2a086a56362b67f139302fc79d357c62c74f2ff3
Mark Tyneway <mark.tyneway@gmail.com>
2020-11-24 16:30:47 -0800

    run: add env var parsing

--- packages/message-relayer/bin/run.ts
+++ packages/message-relayer/bin/run.ts
@@ -2,26 +2,52 @@ import { Wallet } from 'ethers'
 import { JsonRpcProvider } from '@ethersproject/providers'
 import { MessageRelayerService } from '../src/service/message-relayer.service'
 
-const main = async () => {
-  const l2Provider = new JsonRpcProvider('')
-  const l1Provider = new JsonRpcProvider('')
+const env = process.env
+const L2_NODE_WEB3_URL = env.L2_NODE_WEB3_URL
+const L1_NODE_WEB3_URL = env.L1_NODE_WEB3_URL
+const STATE_COMMITMENT_CHAIN_ADDRESS = env.STATE_COMMITMENT_CHAIN_ADDRESS
+const L1_CROSS_DOMAIN_MESSENGER_ADDRESS = env.L1_CROSS_DOMAIN_MESSENGER_ADDRESS
+const L2_CROSS_DOMAIN_MESSENGER_ADDRESS = env.L2_CROSS_DOMAIN_MESSENGER_ADDRESS
+const L2_TO_L1_MESSAGE_PASSER_ADDRESS =
+  env.L2_TO_L1_MESSAGE_PASSER_ADDRESS || '0x4200000000000000000000000000000000000000'
+const POLLING_INTERVAL = env.POLLING_INTERVAL || '5000'
+const RELAY_SIGNER = env.RELAY_SIGNER
+const BLOCK_OFFSET = env.BLOCK_OFFSET || '1'
 
-  const wallet = new Wallet('0x' + 'd0'.repeat(64), l1Provider)
+const main = async () => {
+  if (!STATE_COMMITMENT_CHAIN_ADDRESS) {
+    throw new Error('Must pass STATE_COMMITMENT_CHAIN_ADDRESS')
+  }
+  if (!L1_CROSS_DOMAIN_MESSENGER_ADDRESS) {
+    throw new Error('Must pass L1_CROSS_DOMAIN_MESSENGER_ADDRESS')
+  }
+  if (!L2_CROSS_DOMAIN_MESSENGER_ADDRESS) {
+    throw new Error('Must pass L2_CROSS_DOMAIN_MESSENGER_ADDRESS')
+  }
+
+  const l2Provider = new JsonRpcProvider(L2_NODE_WEB3_URL)
+  const l1Provider = new JsonRpcProvider(L1_NODE_WEB3_URL)
+
+  const wallet = new Wallet(RELAY_SIGNER, l1Provider)
 
   const service = new MessageRelayerService({
     l1RpcProvider: l1Provider,
     l2RpcProvider: l2Provider,
-    stateCommitmentChainAddress: '0xcdf217480D0802DE475Bd531093c53033F2CDCfE',
-    l1CrossDomainMessengerAddress: '0x1e3aa06079fDa5F395E663474ec5f7207A131bD2',
-    l2CrossDomainMessengerAddress: '0x39405fcB4fcc295DcFD16b08bD6f0bAd9D34E438',
-    l2ToL1MessagePasserAddress: '0x4200000000000000000000000000000000000000',
-    pollingInterval: 5000,
+    stateCommitmentChainAddress: STATE_COMMITMENT_CHAIN_ADDRESS,
+    l1CrossDomainMessengerAddress: L1_CROSS_DOMAIN_MESSENGER_ADDRESS,
+    l2CrossDomainMessengerAddress: L2_CROSS_DOMAIN_MESSENGER_ADDRESS,
+    l2ToL1MessagePasserAddress: L2_TO_L1_MESSAGE_PASSER_ADDRESS,
+    pollingInterval: parseInt(POLLING_INTERVAL, 10),
     relaySigner: wallet,
-    l2ChainStartingHeight: 63,
-    blockOffset: 1,
+    blockOffset: parseInt(BLOCK_OFFSET, 10),
   })
 
   await service.start()
 }
 
-main()
+(async () => {
+  await main()
+})().catch(err => {
+  console.log(err)
+  process.exit(1)
+})
