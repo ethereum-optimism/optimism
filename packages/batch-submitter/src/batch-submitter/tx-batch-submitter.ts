5537c26da2d721321602ed4e500e7d185b536efa
Kevin Ho <kevinjho1996@gmail.com>
2020-12-18 16:52:46 -0800

    add log

--- packages/batch-submitter/src/batch-submitter/tx-batch-submitter.ts
+++ packages/batch-submitter/src/batch-submitter/tx-batch-submitter.ts
@@ -228,6 +228,7 @@ export class TransactionBatchSubmitter extends BatchSubmitter {
       if (groupedBlock.sequenced.length === 0 && groupedBlock.queued.length === 0) {
         throw new Error('Attempted to generate batch context with 0 queued and 0 sequenced txs!')
       }
+      console.log('pushing context!')
       contexts.push({
         numSequencedTransactions: groupedBlock.sequenced.length,
         numSubsequentQueueTransactions: groupedBlock.queued.length,
