df4edec57823f521b37fbf49f39a40e1afa0290d
Maurelian <23033765+maurelian@users.noreply.github.com>
2021-01-11 21:41:07 -0500

    Add reentry protection to ExecutionManager.run() (#175)
    * Add nonreentrant to ExecutionManager.run()
    
    * Cheaper check using uninit'd tx context
    
    * Remove extraneous import

--- packages/contracts/contracts/optimistic-ethereum/OVM/execution/OVM_ExecutionManager.sol
+++ packages/contracts/contracts/optimistic-ethereum/OVM/execution/OVM_ExecutionManager.sol
@@ -144,6 +144,7 @@ contract OVM_ExecutionManager is iOVM_ExecutionManager, Lib_AddressResolver {
         override
         public
     {
+        require(transactionContext.ovmNUMBER == 0, "Only be callable at the start of a transaction");
         // Store our OVM_StateManager instance (significantly easier than attempting to pass the
         // address around in calldata).
         ovmStateManager = iOVM_StateManager(_ovmStateManager);
