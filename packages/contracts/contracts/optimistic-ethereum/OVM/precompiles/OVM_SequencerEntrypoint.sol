6d2f118133b8963981a97393eb230b4cb8f5c801
Maurelian <maurelian@protonmail.ch>
2021-01-26 09:47:51 -0500

    Add comment to document v argument (#199)

--- packages/contracts/contracts/optimistic-ethereum/OVM/precompiles/OVM_SequencerEntrypoint.sol
+++ packages/contracts/contracts/optimistic-ethereum/OVM/precompiles/OVM_SequencerEntrypoint.sol
@@ -30,6 +30,7 @@ contract OVM_SequencerEntrypoint {
      * calldata[00:01]: transaction type (0 == EIP 155, 2 == Eth Sign Message)
      * calldata[01:33]: signature "r" parameter
      * calldata[33:65]: signature "s" parameter
+     * calldata[65:66]: signature "v" parameter
      * calldata[66:69]: transaction gas limit
      * calldata[69:72]: transaction gas price
      * calldata[72:75]: transaction nonce
