279d58818a60f93768e242ac64b87659ff2fb462
Mark Tyneway <mark.tyneway@gmail.com>
2021-02-10 16:10:40 -0800

    core-utils: ethsign signature hashing

--- packages/core-utils/src/utils.ts
+++ packages/core-utils/src/utils.ts
@@ -1,5 +1,7 @@
 /* External Imports */
-import { BigNumber } from 'ethers'
+import { BigNumber, utils } from 'ethers'
+import { AbiCoder } from '@ethersproject/abi'
+import { keccak256 } from '@ethersproject/keccak256'
 
 export const getLen = (pos: { start; end }) => (pos.end - pos.start) * 2
 
@@ -26,4 +28,31 @@ export const add0x = (str: string): string => {
     return `0x${str}`
   }
   return str
+}
+
+export const serializeEthSignTransaction = (transaction): string => {
+  const abi = new AbiCoder()
+  return abi.encode(
+    ['uint256', 'uint256', 'uint256', 'uint256', 'address', 'bytes'],
+    [
+      transaction.nonce,
+      transaction.gasLimit,
+      transaction.gasPrice,
+      transaction.chainId,
+      transaction.to,
+      transaction.data,
+    ]
+  )
+}
+
+// Use this function to compute the ETH_SIGN signature hash. The prefix
+// boolean set to true prepend `\x19Ethereum Signed Message:` before hashing.
+// Set to false if using metamask
+export const sighashEthSign = (transaction, prefix?: boolean): string => {
+  const serialized = serializeEthSignTransaction(transaction)
+  const hash = keccak256(utils.arrayify(serialized))
+  if (prefix) {
+    return utils.hashMessage(utils.arrayify(hash))
+  }
+  return hash
 }
