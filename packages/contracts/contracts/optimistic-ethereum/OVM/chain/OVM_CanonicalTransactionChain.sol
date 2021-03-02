b316341b9deb8d7b2811bc074248fde20592e209
Tony Sosa <tony.sosa@tonysosa.com>
2021-03-02 13:46:05 -0600

    Update OVM_CanonicalTransactionChain.sol (#270)

--- packages/contracts/contracts/optimistic-ethereum/OVM/chain/OVM_CanonicalTransactionChain.sol
+++ packages/contracts/contracts/optimistic-ethereum/OVM/chain/OVM_CanonicalTransactionChain.sol
@@ -355,7 +355,7 @@ contract OVM_CanonicalTransactionChain is iOVM_CanonicalTransactionChain, Lib_Ad
         override
         public
     {
-        // Disable `appendQueueBatch` for minnet
+        // Disable `appendQueueBatch` for mainnet
         revert("appendQueueBatch is currently disabled.");
 
         _numQueuedTransactions = Lib_Math.min(_numQueuedTransactions, getNumPendingQueueElements());
