0a4b8bbb624ce9910618bdbd19302d3c699b1319
Kelvin Fichter <kelvinfichter@gmail.com>
2021-03-03 12:41:05 -0800

    dev: Use eth-optimism/dev (#279)
    * Use dev package
    
    * Add chai and mocha back in
    
    Co-authored-by: Mark Tyneway <mark.tyneway@gmail.com>

--- packages/contracts/bin/take-dump.ts
+++ packages/contracts/bin/take-dump.ts
@@ -3,14 +3,12 @@ import * as fs from 'fs'
 import * as path from 'path'
 import * as mkdirp from 'mkdirp'
 
-
 const env = process.env
 const CHAIN_ID = env.CHAIN_ID || '420'
 
 /* Internal Imports */
 import { makeStateDump } from '../src/contract-dumps'
 import { RollupDeployConfig } from '../src/contract-deployment'
-
 ;(async () => {
   const outdir = path.resolve(__dirname, '../build/dumps')
   const outfile = path.join(outdir, 'state-dump.latest.json')
