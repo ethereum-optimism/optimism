f13f2e7b52711714904c446f7d34cfc35826cf03
Kelvin Fichter <kelvinfichter@gmail.com>
2021-03-04 12:05:59 -0800

    cleanup: Bytes32Utils (#293)

--- packages/contracts/contracts/optimistic-ethereum/libraries/utils/Lib_Bytes32Utils.sol
+++ packages/contracts/contracts/optimistic-ethereum/libraries/utils/Lib_Bytes32Utils.sol
@@ -10,61 +10,86 @@ library Lib_Bytes32Utils {
      * Internal Functions *
      **********************/
 
+    /**
+     * Converts a bytes32 value to a boolean. Anything non-zero will be converted to "true."
+     * @param _in Input bytes32 value.
+     * @return Bytes32 as a boolean.
+     */
     function toBool(
         bytes32 _in
     )
         internal
         pure
         returns (
-            bool _out
+            bool
         )
     {
         return _in != 0;
     }
 
+    /**
+     * Converts a boolean to a bytes32 value.
+     * @param _in Input boolean value.
+     * @return Boolean as a bytes32.
+     */
     function fromBool(
         bool _in
     )
         internal
         pure
         returns (
-            bytes32 _out
+            bytes32
         )
     {
         return bytes32(uint256(_in ? 1 : 0));
     }
 
+    /**
+     * Converts a bytes32 value to an address. Takes the *last* 20 bytes.
+     * @param _in Input bytes32 value.
+     * @return Bytes32 as an address.
+     */
     function toAddress(
         bytes32 _in
     )
         internal
         pure
         returns (
-            address _out
+            address
         )
     {
         return address(uint160(uint256(_in)));
     }
 
+    /**
+     * Converts an address to a bytes32.
+     * @param _in Input address value.
+     * @return Address as a bytes32.
+     */
     function fromAddress(
         address _in
     )
         internal
         pure
         returns (
-            bytes32 _out
+            bytes32
         )
     {
         return bytes32(uint256(_in));
     }
 
+    /**
+     * Removes the leading zeros from a bytes32 value and returns a new (smaller) bytes value.
+     * @param _in Input bytes32 value.
+     * @return Bytes32 without any leading zeros.
+     */
     function removeLeadingZeros(
         bytes32 _in
     )
         internal
         pure
         returns (
-            bytes memory _out
+            bytes memory
         )
     {
         bytes memory out;
