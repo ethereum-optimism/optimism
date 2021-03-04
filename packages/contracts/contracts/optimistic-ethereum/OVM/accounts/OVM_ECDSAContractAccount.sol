497a8bbd781555d42be9dbf19d192e88e2d288ba
Kelvin Fichter <kelvinfichter@gmail.com>
2021-03-04 12:06:41 -0800

    cleanup: ECDSAContractAccount (#292)

--- packages/contracts/contracts/optimistic-ethereum/OVM/accounts/OVM_ECDSAContractAccount.sol
+++ packages/contracts/contracts/optimistic-ethereum/OVM/accounts/OVM_ECDSAContractAccount.sol
@@ -22,8 +22,15 @@ import { Lib_SafeMathWrapper } from "../../libraries/wrappers/Lib_SafeMathWrappe
  */
 contract OVM_ECDSAContractAccount is iOVM_ECDSAContractAccount {
 
+    /*************
+     * Constants *
+     *************/
+
+    // TODO: should be the amount sufficient to cover the gas costs of all of the transactions up
+    // to and including the CALL/CREATE which forms the entrypoint of the transaction.
+    uint256 constant EXECUTION_VALIDATION_GAS_OVERHEAD = 25000;
     address constant ETH_ERC20_ADDRESS = 0x4200000000000000000000000000000000000006;
-    uint256 constant EXECUTION_VALIDATION_GAS_OVERHEAD = 25000; // TODO: should be the amount sufficient to cover the gas costs of all of the transactions up to and including the CALL/CREATE which forms the entrypoint of the transaction.
+
 
     /********************
      * Public Functions *
@@ -36,8 +43,8 @@ contract OVM_ECDSAContractAccount is iOVM_ECDSAContractAccount {
      * @param _v Signature `v` parameter.
      * @param _r Signature `r` parameter.
      * @param _s Signature `s` parameter.
-     * @return _success Whether or not the call returned (rather than reverted).
-     * @return _returndata Data returned by the call.
+     * @return Whether or not the call returned (rather than reverted).
+     * @return Data returned by the call.
      */
     function execute(
         bytes memory _transaction,
@@ -49,8 +56,8 @@ contract OVM_ECDSAContractAccount is iOVM_ECDSAContractAccount {
         override
         public
         returns (
-            bool _success,
-            bytes memory _returndata
+            bool,
+            bytes memory
         )
     {
         bool isEthSign = _signatureType == Lib_OVMCodec.EOASignatureType.ETH_SIGNED_MESSAGE;
