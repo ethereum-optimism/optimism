89a08920ad010e8ac5a840b0afe4e359c88914bc
Mark Tyneway <mark.tyneway@gmail.com>
2021-03-10 17:09:15 -0800

    provider: delete

--- packages/provider/index.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Copyright 2020, Optimism PBC
- * MIT License
- * https://github.com/ethereum-optimism
- */
-
-export * from './src/app'
--- packages/provider/src/app/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Copyright 2020, Optimism PBC
- * MIT License
- * https://github.com/ethereum-optimism
- */
-
-export * from './provider'
-export * from './signer'
-export * from './utils'
