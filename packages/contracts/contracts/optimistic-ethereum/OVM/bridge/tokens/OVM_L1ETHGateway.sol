156a12d33f9ffb73fec68a2e06ff3881e5d75d1b
Maurelian <maurelian@protonmail.ch>
2021-03-12 18:31:42 -0500

    Add receive() to OVM_L1ETHGateway so you can send ETH to it (fixed) (#311)
    * remove immutable
    
    * add fallback function to deposit eth into l2
    
    * Revert "remove immutable"
    
    This reverts commit 6e8bf7b1871b56085ce96ee73b5acbd4bd04b96c.
    
    * remove conflicting receive function
    
    Co-authored-by: George Hotz <geohot@gmail.com>
    Co-authored-by: smartcontracts <kelvinfichter@gmail.com>

--- packages/contracts/contracts/optimistic-ethereum/OVM/bridge/tokens/OVM_L1ETHGateway.sol
+++ packages/contracts/contracts/optimistic-ethereum/OVM/bridge/tokens/OVM_L1ETHGateway.sol
@@ -56,6 +56,13 @@ contract OVM_L1ETHGateway is iOVM_L1ETHGateway, OVM_CrossDomainEnabled, Lib_Addr
      * Depositing *
      **************/
 
+    receive()
+        external
+        payable
+    {
+        _initiateDeposit(msg.sender, msg.sender);
+    }
+
     /**
      * @dev deposit an amount of the ERC20 to the caller's balance on L2
      */
@@ -154,15 +161,5 @@ contract OVM_L1ETHGateway is iOVM_L1ETHGateway, OVM_CrossDomainEnabled, Lib_Addr
     {
         (bool success, ) = _to.call{value: _value}(new bytes(0));
         require(success, 'TransferHelper::safeTransferETH: ETH transfer failed');
-    }
-
-    /**
-     * @dev Prevent users from sending ETH directly to this contract without calling deposit
-     */
-    receive()
-        external
-        payable
-    {
-        revert("Deposits must be initiated via deposit() or depositTo()");
     }
 }
