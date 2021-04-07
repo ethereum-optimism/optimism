7db47df59d63f4deb92a5902117aed5a1a75a8f7
ben-chain <ben@pseudonym.party>
2021-04-07 12:18:07 -0700

    fix: increase MAX_ROLLUP_TX_SIZE (#367)
    * bump MAX_ROLLUP_TX_SIZE
    
    * fix test gasLimit
    
    * lint

--- packages/contracts/contracts/optimistic-ethereum/OVM/chain/OVM_CanonicalTransactionChain.sol
+++ packages/contracts/contracts/optimistic-ethereum/OVM/chain/OVM_CanonicalTransactionChain.sol
@@ -38,7 +38,7 @@ contract OVM_CanonicalTransactionChain is iOVM_CanonicalTransactionChain, Lib_Ad
 
     // L2 tx gas-related
     uint256 constant public MIN_ROLLUP_TX_GAS = 100000;
-    uint256 constant public MAX_ROLLUP_TX_SIZE = 10000;
+    uint256 constant public MAX_ROLLUP_TX_SIZE = 50000;
     uint256 constant public L2_GAS_DISCOUNT_DIVISOR = 32;
 
     // Encoding-related (all in bytes)
