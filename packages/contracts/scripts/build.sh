161c2ac360a03b8b582dc8842a746eafc160f4ea
Georgios Konstantopoulos <me@gakonst.com>
2021-04-08 20:05:23 +0300

    fix: try fixing flaky ci (#42)

--- packages/contracts/scripts/build.sh
+++ packages/contracts/scripts/build.sh
@@ -1,12 +1,2 @@
-yarn run build:typescript &
-yarn run build:contracts &
-# avoid race condition between the 2 concurrent hardhat instances
-sleep 2
-yarn run build:contracts:ovm &
-
-wait
-
-yarn run build:copy:artifacts &
-yarn run build:copy:artifacts:ovm &
-yarn run build:copy:contracts &
-wait
+yarn run build:typescript & yarn run build:contracts & yarn run build:contracts:ovm
+yarn run build:copy:artifacts & yarn run build:copy:artifacts:ovm & yarn run build:copy:contracts
