45e297f5b8f10cef571692c9ade3b332cd5b21a2
Mark Tyneway <mark.tyneway@gmail.com>
2021-04-12 17:53:28 -0700

    docker: have geth wait for dtl to be up before starting

--- ops/scripts/geth.sh
+++ ops/scripts/geth.sh
@@ -19,4 +19,6 @@ if [ $ETH1_L1_ETH_GATEWAY_ADDRESS == null ]; then
     envSet ETH1_L1_ETH_GATEWAY_ADDRESS OVM_L1ETHGateway
 fi
 
+curl --retry-connrefused --retry $RETRIES --retry-delay 1 $ROLLUP_CLIENT_HTTP
+
 exec geth --verbosity=6
