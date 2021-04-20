a9035f69627622d140d98c1ce236c257c8f364f0
Georgios Konstantopoulos <me@gakonst.com>
2021-04-20 11:20:52 +0300

    ci: add 90s timeout when waiting for sequencer to be up

--- ops/scripts/wait-for-sequencer.sh
+++ ops/scripts/wait-for-sequencer.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 CONTAINER=l2geth
 
+RETRIES=30
+i=0
 until docker-compose logs l2geth | grep -q "Starting Sequencer Loop";
 do
     sleep 3
+    if [ $i -eq $RETRIES ]; then
+        echo 'Timed out waiting for sequencer'
+        break
+    fi
     echo 'Waiting for sequencer...'
+    ((i=i+1))
 done
