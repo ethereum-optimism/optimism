0566f2405c303fe5fc4416d183ebe24ef02316fb
Georgios Konstantopoulos <me@gakonst.com>
2021-04-22 17:31:07 +0300

    feat(l2geth/rollup-gpo): allow execution price to be specified

--- l2geth/eth/gasprice/rollup_gasprice.go
+++ l2geth/eth/gasprice/rollup_gasprice.go
@@ -6,11 +6,12 @@ import (
 )
 
 type RollupOracle struct {
-	dataPrice *big.Int
+	dataPrice      *big.Int
+	executionPrice *big.Int
 }
 
-func NewRollupOracle(dataPrice *big.Int) *RollupOracle {
-	return &RollupOracle{dataPrice}
+func NewRollupOracle(dataPrice *big.Int, executionPrice *big.Int) *RollupOracle {
+	return &RollupOracle{dataPrice, executionPrice}
 }
 
 /// SuggestDataPrice returns the gas price which should be charged per byte of published
@@ -21,4 +22,14 @@ func (gpo *RollupOracle) SuggestDataPrice(ctx context.Context) (*big.Int, error)
 
 func (gpo *RollupOracle) SetDataPrice(dataPrice *big.Int) {
 	gpo.dataPrice = dataPrice
+}
+
+/// SuggestExecutionPrice returns the gas price which should be charged per unit of gas
+/// set manually by the sequencer depending on congestion
+func (gpo *RollupOracle) SuggestExecutionPrice(ctx context.Context) (*big.Int, error) {
+	return gpo.executionPrice, nil
+}
+
+func (gpo *RollupOracle) SetExecutionPrice(executionPrice *big.Int) {
+	gpo.executionPrice = executionPrice
 }
