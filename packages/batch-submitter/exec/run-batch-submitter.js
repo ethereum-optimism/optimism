a3dc5539ad68718274501a923d35c2a4309e9078
Liam Horne <liam@lihorne.com>
2021-04-22 12:08:42 -0700

    build: add changeset

--- packages/batch-submitter/exec/run-batch-submitter.js
+++ packages/batch-submitter/exec/run-batch-submitter.js
@@ -1,5 +1,8 @@
 #!/usr/bin/env node
 
+console.log(process.env.npm_package_version);
+process.exit(1)
+
 const batchSubmitter = require("../dist/src/exec/run-batch-submitter")
 
 batchSubmitter.run()
