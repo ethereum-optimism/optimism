eebead8fa3cb83032c6c66c9a9697f0191a4b91b
Ben Jones <ben@pseudonym.party>
2021-04-22 16:59:47 -0700

    feat: add OVM Upgrade Functions with stub executor

---
+++ packages/contracts/contracts/optimistic-ethereum/OVM/predeploys/OVM_UpgradeExecutor.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+// @unsupported: evm
+pragma solidity >0.5.0 <0.8.0;
+
+/* Library Imports */
+import { Lib_ExecutionManagerWrapper } from "../../libraries/wrappers/Lib_ExecutionManagerWrapper.sol";
+
+/**
+ * @title OVM_UpgradeExecutor
+ * @dev The OVM_UpgradeExecutor is the contract which authenticates and executes (i.e.
+ * calls the relevant Execution Manager upgrade functions) upgrades to the OVM State.
+ * This enables us to update the predeploy and execution contracts directly from within
+ * L2 when there is a new release.
+ * 
+ * Compiler used: optimistic-solc
+ * Runtime target: OVM
+ */
+contract OVM_UpgradeExecutor {
+    fallback()
+        external
+    {
+        revert("Not yet implemented.");
+    }
+}
