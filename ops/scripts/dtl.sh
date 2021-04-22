9649a62738a2df088f00ef07a06bf4df3c16b3b7
Mark Tyneway <mark.tyneway@gmail.com>
2021-04-22 16:05:36 -0700

    ops: check for URL before trying to query it

--- ops/scripts/dtl.sh
+++ ops/scripts/dtl.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 RETRIES=${RETRIES:-60}
 
-# get the addrs from the URL provided
-ADDRESSES=$(curl --silent --retry-connrefused --retry $RETRIES --retry-delay 5 $URL)
-# set the env
-export DATA_TRANSPORT_LAYER__ADDRESS_MANAGER=$(echo $ADDRESSES | jq -r '.AddressManager')
+if [[ ! -z "$URL" ]]; then
+    # get the addrs from the URL provided
+    ADDRESSES=$(curl --silent --retry-connrefused --retry $RETRIES --retry-delay 5 $URL)
+    # set the env
+    export DATA_TRANSPORT_LAYER__ADDRESS_MANAGER=$(echo $ADDRESSES | jq -r '.AddressManager')
+fi
 
 # go
 exec node dist/src/services/run.js
