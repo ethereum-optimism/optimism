ethereum-optimism/optimism

commit 933880d443538d413f8bac6488e34b3339078ad5

build: add changeset

packages/batch-submitter/exec/run-batch-submitter.js
@@ -1,5 +1,8 @@
 #!/usr/bin/env node
 
+console.log(process.env.npm_package_version);
+process.exit(1)
+
 const batchSubmitter = require("../dist/src/exec/run-batch-submitter")
 
 batchSubmitter.run()

commit 586d387f2b637ba42ec1c49b1679333cfcc8f45c

fix: start the batch submitter properly
fixes bug introduced in https://github.com/ethereum-optimism/optimism/pull/538/commits/933880d443538d413f8bac6488e34b3339078ad5

packages/batch-submitter/exec/run-batch-submitter.js
@@ -1,8 +1,5 @@
 #!/usr/bin/env node
 
-console.log(process.env.npm_package_version);
-process.exit(1)
-
 const batchSubmitter = require("../dist/src/exec/run-batch-submitter")
 
 batchSubmitter.run()