fe09da8c088af2faf39c86367af9dcb7a447c46f
Kelvin Fichter <kelvinfichter@gmail.com>
2021-04-23 14:41:32 -0400

    fix[contracts]: Fix bug introduced by indirect constant

--- packages/contracts/contracts/optimistic-ethereum/OVM/accounts/OVM_ProxyEOA.sol
+++ packages/contracts/contracts/optimistic-ethereum/OVM/accounts/OVM_ProxyEOA.sol
@@ -30,7 +30,7 @@ contract OVM_ProxyEOA {
      *************/
 
     address constant DEFAULT_IMPLEMENTATION = 0x4200000000000000000000000000000000000003;
-    bytes32 constant IMPLEMENTATION_KEY = bytes32(uint256(keccak256('eip1967.proxy.implementation')) - 1);
+    bytes32 constant IMPLEMENTATION_KEY = 0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc; //bytes32(uint256(keccak256('eip1967.proxy.implementation')) - 1);
 
 
     /*********************
