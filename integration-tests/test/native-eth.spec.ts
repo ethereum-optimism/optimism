54ef2f59f8f872ee79d8e5ced790b459e4cefc99
smartcontracts <kelvinfichter@gmail.com>
2021-04-23 18:47:21 -0400

    fix[integration-tests]: Use fixed address in gas estimation test (#598)

--- integration-tests/test/native-eth.spec.ts
+++ integration-tests/test/native-eth.spec.ts
@@ -43,7 +43,7 @@ describe('Native ETH Integration Tests', async () => {
   describe('estimateGas', () => {
     it('Should estimate gas for ETH transfer', async () => {
       const amount = utils.parseEther('0.5')
-      const addr = await l2Bob.getAddress()
+      const addr = '0x' + '1234'.repeat(10)
       const gas = await env.ovmEth.estimateGas.transfer(addr, amount)
       expect(gas).to.be.deep.eq(BigNumber.from(213546))
     })
