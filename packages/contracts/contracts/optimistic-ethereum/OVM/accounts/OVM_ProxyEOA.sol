c20b2c5bb8ed4392d2fdce541adcd89988d0f083
smartcontracts <kelvinfichter@gmail.com>
2021-04-25 04:09:39 -0400

    feat[contracts]: Make ProxyEOA compatible with eip1967 (#592)
    * feat[contracts]: Make ProxyEOA compatible with eip1967
    
    * fix[contracts]: Fix bug introduced by indirect constant
    
    * chore[contracts]: Add changeset
    
    * Update .changeset/old-cycles-invite.md
    
    Co-authored-by: Georgios Konstantopoulos <me@gakonst.com>

--- packages/contracts/contracts/optimistic-ethereum/OVM/accounts/OVM_ProxyEOA.sol
+++ packages/contracts/contracts/optimistic-ethereum/OVM/accounts/OVM_ProxyEOA.sol
@@ -16,12 +16,21 @@ import { Lib_Bytes32Utils } from "../../libraries/utils/Lib_Bytes32Utils.sol";
  */
 contract OVM_ProxyEOA {
 
+    /**********
+     * Events *
+     **********/
+    
+    event Upgraded(
+        address indexed implementation
+    );
+
+
     /*************
      * Constants *
      *************/
 
     address constant DEFAULT_IMPLEMENTATION = 0x4200000000000000000000000000000000000003;
-    bytes32 constant IMPLEMENTATION_KEY = 0xdeaddeaddeaddeaddeaddeaddeaddeaddeaddeaddeaddeaddeaddeaddeaddead;
+    bytes32 constant IMPLEMENTATION_KEY = 0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc; //bytes32(uint256(keccak256('eip1967.proxy.implementation')) - 1);
 
 
     /*********************
@@ -68,6 +77,7 @@ contract OVM_ProxyEOA {
         );
 
         _setImplementation(_implementation);
+        emit Upgraded(_implementation);
     }
 
     /**
