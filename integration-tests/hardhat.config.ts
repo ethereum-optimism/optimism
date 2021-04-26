02ee5e58551beed0e35f9f671c6b12135f717e97
elenadimitrova <elena@arenabg.com>
2021-04-26 21:06:25 +0300

    Add gas reports to integration tests

--- integration-tests/hardhat.config.ts
+++ integration-tests/hardhat.config.ts
@@ -3,6 +3,9 @@ import { HardhatUserConfig } from 'hardhat/types'
 // Hardhat plugins
 import '@nomiclabs/hardhat-ethers'
 import '@eth-optimism/hardhat-ovm'
+import 'hardhat-gas-reporter'
+
+const enableGasReport = !!process.env.ENABLE_GAS_REPORT
 
 const config: HardhatUserConfig = {
   mocha: {
@@ -17,6 +20,12 @@ const config: HardhatUserConfig = {
   solidity: '0.7.6',
   ovm: {
     solcVersion: '0.7.6',
+  },
+  gasReporter: {
+    enabled: enableGasReport,
+    currency: 'USD',
+    gasPrice: 100,
+    outputFile: process.env.CI ? 'gas-report.txt' : undefined,
   },
 }
 
