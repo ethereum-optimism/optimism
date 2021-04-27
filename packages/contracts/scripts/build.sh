ec74a21aef5a3243ba5cd887dac026e2414d02aa
Mark Tyneway <mark.tyneway@gmail.com>
2021-04-26 17:13:19 -0700

    build: parallel

--- packages/contracts/scripts/build.sh
+++ packages/contracts/scripts/build.sh
@@ -2,6 +2,7 @@
 
 set -e
 
-yarn run build:typescript &
-yarn run build:contracts
-yarn run build:contracts:ovm
+yarn run build:typescript&
+yarn run build:contracts&
+yarn run build:contracts:ovm&
+wait
