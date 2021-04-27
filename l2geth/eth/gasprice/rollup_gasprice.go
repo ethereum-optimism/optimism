97a1df99989d7456c30e0b0f1d49537816adb1f1
Georgios Konstantopoulos <me@gakonst.com>
2021-04-27 14:38:26 +0300

    feat(rollup-gasprice): make Rollup GPO methods thread safe

--- l2geth/eth/gasprice/rollup_gasprice.go
+++ l2geth/eth/gasprice/rollup_gasprice.go
@@ -3,33 +3,49 @@ package gasprice
 import (
 	"context"
 	"math/big"
+	"sync"
 )
 
 type RollupOracle struct {
-	dataPrice      *big.Int
-	executionPrice *big.Int
+	dataPrice          *big.Int
+	executionPrice     *big.Int
+	dataPriceLock      sync.RWMutex
+	executionPriceLock sync.RWMutex
 }
 
 func NewRollupOracle(dataPrice *big.Int, executionPrice *big.Int) *RollupOracle {
-	return &RollupOracle{dataPrice, executionPrice}
+	return &RollupOracle{
+		dataPrice:      dataPrice,
+		executionPrice: executionPrice,
+	}
 }
 
 /// SuggestDataPrice returns the gas price which should be charged per byte of published
 /// data by the sequencer.
 func (gpo *RollupOracle) SuggestDataPrice(ctx context.Context) (*big.Int, error) {
-	return gpo.dataPrice, nil
+	gpo.dataPriceLock.RLock()
+	price := gpo.dataPrice
+	gpo.dataPriceLock.RUnlock()
+	return price, nil
 }
 
 func (gpo *RollupOracle) SetDataPrice(dataPrice *big.Int) {
+	gpo.dataPriceLock.Lock()
 	gpo.dataPrice = dataPrice
+	gpo.dataPriceLock.Unlock()
 }
 
 /// SuggestExecutionPrice returns the gas price which should be charged per unit of gas
 /// set manually by the sequencer depending on congestion
 func (gpo *RollupOracle) SuggestExecutionPrice(ctx context.Context) (*big.Int, error) {
-	return gpo.executionPrice, nil
+	gpo.executionPriceLock.RLock()
+	price := gpo.executionPrice
+	gpo.executionPriceLock.RUnlock()
+	return price, nil
 }
 
 func (gpo *RollupOracle) SetExecutionPrice(executionPrice *big.Int) {
+	gpo.executionPriceLock.Lock()
 	gpo.executionPrice = executionPrice
+	gpo.executionPriceLock.Unlock()
 }
