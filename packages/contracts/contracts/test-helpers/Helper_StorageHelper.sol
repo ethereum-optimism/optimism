98f83f90692ef864ea319ce89fb42ec78932de07
Kelvin Fichter <kelvinfichter@gmail.com>
2021-05-03 17:27:41 -0400

    Update testing for storage library

--- packages/contracts/contracts/test-helpers/Helper_StorageHelper.sol
+++ packages/contracts/contracts/test-helpers/Helper_StorageHelper.sol
@@ -2,48 +2,59 @@
 pragma solidity >0.5.0 <0.8.0;
 
 contract Helper_StorageHelper {
+    function setStorage(
+        bytes32 _key,
+        bytes32 _val
+    )
+        public
+    {
+        assembly {
+            sstore(_key, _val)
+        }
+    }
+
     struct BasicStruct {
         uint256 _structUint256;
         address _structAddress;
         bytes32 _structBytes32;
     }
 
-    uint8 _uint8;     // slot 0
-    bytes32 _spacer1; // to avoid slot packing, unused
+    uint8 public _uint8;     // slot 0
+    bytes32 _spacer1;        // to avoid slot packing, unused
 
-    uint64 _uint64;   // slot 2
-    bytes32 _spacer2; // to avoid slot packing, unused
+    uint64 public _uint64;   // slot 2
+    bytes32 _spacer2;        // to avoid slot packing, unused
 
-    uint256 _uint256; // slot 4
-    bytes32 _spacer3; // to avoid slot packing, unused
+    uint256 public _uint256; // slot 4
+    bytes32 _spacer3;        // to avoid slot packing, unused
 
-    bytes1 _bytes1;   // slot 6
-    bytes32 _spacer4; // to avoid slot packing, unused
+    bytes1 public _bytes1;   // slot 6
+    bytes32 _spacer4;        // to avoid slot packing, unused
 
-    bytes8 _bytes8;   // slot 8
-    bytes32 _spacer5; // to avoid slot packing, unused
+    bytes8 public _bytes8;   // slot 8
+    bytes32 _spacer5;        // to avoid slot packing, unused
 
-    bytes32 _bytes32; // slot 10
-    bytes32 _spacer6; // to avoid slot packing, unused
+    bytes32 public _bytes32; // slot 10
+    bytes32 _spacer6;        // to avoid slot packing, unused
 
-    bool _bool;       // slot 12
-    bytes32 _spacer7; // to avoid slot packing, unused
+    bool public _bool;       // slot 12
+    bytes32 _spacer7;        // to avoid slot packing, unused
 
-    address _address; // slot 14
-    bytes32 _spacer8; // to avoid slot packing, unused
+    address public _address; // slot 14
+    bytes32 _spacer8;        // to avoid slot packing, unused
 
-    bytes _bytes;     // slot 16
-    string _string;   // slot 17
+    bytes public _bytes;     // slot 16
+    string public _string;   // slot 17
 
-    BasicStruct _struct; // slot 18,19,20
+    BasicStruct public _struct; // slot 18,19,20
 
     // Pack into (bytes11,bool,address)
-    address _packedAddress; // slot 21
-    bool _packedBool;       // slot 21
-    bytes11 _packedBytes11; // slot 21
+    address public _packedAddress; // slot 21
+    bool public _packedBool;       // slot 21
+    bytes11 public _packedBytes11; // slot 21
 
     // Pack into (address,bool,bytes11)
-    bytes11 _otherPackedBytes11; // slot 22
-    bool _otherPackedBool;       // slot 22
-    address _otherPackedAddress; // slot 22
+    bytes11 public _otherPackedBytes11; // slot 22
+    bool public _otherPackedBool;       // slot 22
+    address public _otherPackedAddress; // slot 22
 }
--- packages/contracts/test/chugsplash/storage.spec.ts
+++ packages/contracts/test/chugsplash/storage.spec.ts
@@ -1,11 +1,14 @@
 import { expect } from '../setup'
 
-import hre from 'hardhat'
+import hre, { ethers } from 'hardhat'
+import { Contract } from 'ethers'
+
 import {
   computeStorageSlots,
   getStorageLayout,
   SolidityStorageLayout,
 } from '../../src'
+import { isObject, toPlainObject } from 'lodash'
 
 describe('ChugSplash storage layout parsing', () => {
   let layout: SolidityStorageLayout
@@ -13,240 +16,120 @@ describe('ChugSplash storage layout parsing', () => {
     layout = await getStorageLayout(hre, 'Helper_StorageHelper')
   })
 
+  let Helper_StorageHelper: Contract
+  beforeEach(async () => {
+    Helper_StorageHelper = await (
+      await hre.ethers.getContractFactory('Helper_StorageHelper')
+    ).deploy()
+  })
+
+  const computeAndVerifyStorageSlots = async (
+    variables: any
+  ): Promise<void> => {
+    const slots = computeStorageSlots(layout, variables)
+    for (const slot of slots) {
+      await Helper_StorageHelper.setStorage(slot.key, slot.val)
+    }
+
+    for (const variable of Object.keys(variables)) {
+      const valA = await Helper_StorageHelper[variable]()
+      const valB = variables[variable]
+      if (Array.isArray(valA) && isObject(valB)) {
+        expect(toPlainObject(valA)).to.deep.include(valB)
+      } else {
+        expect(valA).to.equal(valB)
+      }
+    }
+  }
+
   describe('computeStorageSlots', () => {
-    it('compute slots for uint8', () => {
-      expect(
-        computeStorageSlots(layout, {
-          _uint8: 123,
-        })
-      ).to.deep.equal([
-        {
-          key:
-            '0x0000000000000000000000000000000000000000000000000000000000000000',
-          val:
-            '0x000000000000000000000000000000000000000000000000000000000000007b',
-        },
-      ])
+    it('compute slots for uint8', async () => {
+      await computeAndVerifyStorageSlots({
+        _uint8: 123,
+      })
     })
 
-    it('compute slots for uint64', () => {
-      expect(
-        computeStorageSlots(layout, {
-          _uint64: 1234,
-        })
-      ).to.deep.equal([
-        {
-          key:
-            '0x0000000000000000000000000000000000000000000000000000000000000002',
-          val:
-            '0x00000000000000000000000000000000000000000000000000000000000004d2',
-        },
-      ])
+    it('compute slots for uint64', async () => {
+      await computeAndVerifyStorageSlots({
+        _uint64: 1234,
+      })
     })
 
-    it('compute slots for uint256', () => {
-      expect(
-        computeStorageSlots(layout, {
-          _uint256: 12345,
-        })
-      ).to.deep.equal([
-        {
-          key:
-            '0x0000000000000000000000000000000000000000000000000000000000000004',
-          val:
-            '0x0000000000000000000000000000000000000000000000000000000000003039',
-        },
-      ])
+    it('compute slots for uint256', async () => {
+      await computeAndVerifyStorageSlots({
+        _uint256: 12345,
+      })
     })
 
-    it('compute slots for bytes1', () => {
-      expect(
-        computeStorageSlots(layout, {
-          _bytes1: '0x11',
-        })
-      ).to.deep.equal([
-        {
-          key:
-            '0x0000000000000000000000000000000000000000000000000000000000000006',
-          val:
-            '0x0000000000000000000000000000000000000000000000000000000000000011',
-        },
-      ])
+    it('compute slots for bytes1', async () => {
+      await computeAndVerifyStorageSlots({
+        _bytes1: '0x11',
+      })
     })
 
-    it('compute slots for bytes8', () => {
-      expect(
-        computeStorageSlots(layout, {
-          _bytes8: '0x1212121212121212',
-        })
-      ).to.deep.equal([
-        {
-          key:
-            '0x0000000000000000000000000000000000000000000000000000000000000008',
-          val:
-            '0x0000000000000000000000000000000000000000000000001212121212121212',
-        },
-      ])
+    it('compute slots for bytes8', async () => {
+      await computeAndVerifyStorageSlots({
+        _bytes8: '0x1212121212121212',
+      })
     })
 
-    it('compute slots for bytes32', () => {
-      expect(
-        computeStorageSlots(layout, {
-          _bytes32:
-            '0x2222222222222222222222222222222222222222222222222222222222222222',
-        })
-      ).to.deep.equal([
-        {
-          key:
-            '0x000000000000000000000000000000000000000000000000000000000000000a',
-          val:
-            '0x2222222222222222222222222222222222222222222222222222222222222222',
-        },
-      ])
+    it('compute slots for bytes32', async () => {
+      await computeAndVerifyStorageSlots({
+        _bytes32:
+          '0x2222222222222222222222222222222222222222222222222222222222222222',
+      })
     })
 
-    it('compute slots for bool', () => {
-      expect(
-        computeStorageSlots(layout, {
-          _bool: true,
-        })
-      ).to.deep.equal([
-        {
-          key:
-            '0x000000000000000000000000000000000000000000000000000000000000000c',
-          val:
-            '0x0000000000000000000000000000000000000000000000000000000000000001',
-        },
-      ])
+    it('compute slots for bool', async () => {
+      await computeAndVerifyStorageSlots({
+        _bool: true,
+      })
     })
 
-    it('compute slots for address', () => {
-      expect(
-        computeStorageSlots(layout, {
-          _address: '0x5A0b54D5dc17e0AadC383d2db43B0a0D3E029c4c',
-        })
-      ).to.deep.equal([
-        {
-          key:
-            '0x000000000000000000000000000000000000000000000000000000000000000e',
-          val:
-            '0x0000000000000000000000005a0b54d5dc17e0aadc383d2db43b0a0d3e029c4c',
-        },
-      ])
+    it('compute slots for address', async () => {
+      await computeAndVerifyStorageSlots({
+        _address: '0x5A0b54D5dc17e0AadC383d2db43B0a0D3E029c4c',
+      })
     })
 
-    it('compute slots for bytes (<32 bytes long)', () => {
-      expect(
-        computeStorageSlots(layout, {
-          _bytes:
-            '0x12121212121212121212121212121212121212121212121212121212121212', // only 31 bytes
-        })
-      ).to.deep.equal([
-        {
-          key:
-            '0x0000000000000000000000000000000000000000000000000000000000000010',
-          val:
-            '0x121212121212121212121212121212121212121212121212121212121212123e', // last byte contains byte length * 2
-        },
-      ])
+    it('compute slots for bytes (<32 bytes long)', async () => {
+      await computeAndVerifyStorageSlots({
+        _bytes:
+          '0x12121212121212121212121212121212121212121212121212121212121212', // only 31 bytes
+      })
     })
 
-    it('compute slots for string (<32 bytes long)', () => {
-      expect(
-        computeStorageSlots(layout, {
-          _string: 'hello i am a string', // 19 bytes
-        })
-      ).to.deep.equal([
-        {
-          key:
-            '0x0000000000000000000000000000000000000000000000000000000000000011',
-          val:
-            '0x68656c6c6f206920616d206120737472696e6700000000000000000000000026', // 19 * 2 = 38 = 0x26
-        },
-      ])
+    it('compute slots for string (<32 bytes long)', async () => {
+      await computeAndVerifyStorageSlots({
+        _string: 'hello i am a string', // 19 bytes
+      })
     })
 
-    it('compute slots for a simple (complete) struct', () => {
-      expect(
-        computeStorageSlots(layout, {
-          _struct: {
-            _structUint256: 1234,
-            _structAddress: '0x5A0b54D5dc17e0AadC383d2db43B0a0D3E029c4c',
-            _structBytes32:
-              '0x1212121212121212121212121212121212121212121212121212121212121212',
-          },
-        })
-      ).to.deep.equal([
-        {
-          key:
-            '0x0000000000000000000000000000000000000000000000000000000000000012',
-          val:
-            '0x00000000000000000000000000000000000000000000000000000000000004d2',
-        },
-        {
-          key:
-            '0x0000000000000000000000000000000000000000000000000000000000000013',
-          val:
-            '0x0000000000000000000000005a0b54d5dc17e0aadc383d2db43b0a0d3e029c4c',
-        },
-        {
-          key:
-            '0x0000000000000000000000000000000000000000000000000000000000000014',
-          val:
+    it('compute slots for a simple (complete) struct', async () => {
+      await computeAndVerifyStorageSlots({
+        _struct: {
+          _structUint256: ethers.BigNumber.from(1234),
+          _structAddress: '0x5A0b54D5dc17e0AadC383d2db43B0a0D3E029c4c',
+          _structBytes32:
             '0x1212121212121212121212121212121212121212121212121212121212121212',
         },
-      ])
+      })
     })
 
-    it('compute slots for a simple (partial) struct', () => {
-      expect(
-        computeStorageSlots(layout, {
-          _struct: {
-            _structAddress: '0x5A0b54D5dc17e0AadC383d2db43B0a0D3E029c4c',
-          },
-        })
-      ).to.deep.equal([
-        {
-          key:
-            '0x0000000000000000000000000000000000000000000000000000000000000013',
-          val:
-            '0x0000000000000000000000005a0b54d5dc17e0aadc383d2db43b0a0d3e029c4c',
-        },
-      ])
+    it('compute slots for packed variables', async () => {
+      await computeAndVerifyStorageSlots({
+        _packedAddress: '0x5A0b54D5dc17e0AadC383d2db43B0a0D3E029c4c',
+        _packedBool: true,
+        _packedBytes11: '0x1212121212121212121212',
+      })
     })
 
-    it('compute slots for packed variables', () => {
-      expect(
-        computeStorageSlots(layout, {
-          _packedAddress: '0x5A0b54D5dc17e0AadC383d2db43B0a0D3E029c4c',
-          _packedBool: true,
-          _packedBytes11: '0x1212121212121212121212',
-        })
-      ).to.deep.equal([
-        {
-          key:
-            '0x0000000000000000000000000000000000000000000000000000000000000015',
-          val:
-            '0x1212121212121212121212015a0b54d5dc17e0aadc383d2db43b0a0d3e029c4c',
-        },
-      ])
-    })
-
-    it('compute slots for packed variables (2)', () => {
-      expect(
-        computeStorageSlots(layout, {
-          _otherPackedBytes11: '0x1212121212121212121212',
-          _otherPackedBool: true,
-          _otherPackedAddress: '0x5A0b54D5dc17e0AadC383d2db43B0a0D3E029c4c',
-        })
-      ).to.deep.equal([
-        {
-          key:
-            '0x0000000000000000000000000000000000000000000000000000000000000016',
-          val:
-            '0x5a0b54d5dc17e0aadc383d2db43b0a0d3e029c4c011212121212121212121212',
-        },
-      ])
+    it('compute slots for packed variables (2)', async () => {
+      await computeAndVerifyStorageSlots({
+        _otherPackedBytes11: '0x1212121212121212121212',
+        _otherPackedBool: true,
+        _otherPackedAddress: '0x5A0b54D5dc17e0AadC383d2db43B0a0D3E029c4c',
+      })
     })
   })
 })
