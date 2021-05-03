7f8cfae3e959bc440b8fe2f174996d7b2b3599ff
Georgios Konstantopoulos <me@gakonst.com>
2021-05-03 14:22:42 +0300

    chore: add --fail --show-error to curl

--- ops/scripts/integration-tests.sh
+++ ops/scripts/integration-tests.sh
@@ -6,13 +6,15 @@ JSON='{"jsonrpc":"2.0","id":0,"method":"rollup_getInfo","params":[]}'
 
 if [[ ! -z "$URL" ]]; then
     # get the addrs from the URL provided
-    ADDRESSES=$(curl --silent --retry-connrefused --retry $RETRIES --retry-delay 5 $URL)
+    ADDRESSES=$(curl --fail --show-error --silent --retry-connrefused --retry $RETRIES --retry-delay 5 $URL)
     export ADDRESS_MANAGER=$(echo $ADDRESSES | jq -r '.AddressManager')
 fi
 
 # wait for the sequencer to be up
 curl \
     --silent \
+    --fail \
+    --show-error \
     -H "Content-Type: application/json" \
     --retry-connrefused \
     --retry $RETRIES \
