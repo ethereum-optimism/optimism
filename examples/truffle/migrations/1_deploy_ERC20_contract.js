072d548c1870eff91994797b3c59103bec867af0
platocrat <jack.abe.winfield@gmail.com>
2021-05-04 18:21:04 -0500

    fix: add gasPrice: 0 to all ctc calls and deploys

--- examples/truffle/migrations/1_deploy_ERC20_contract.js
+++ examples/truffle/migrations/1_deploy_ERC20_contract.js
@@ -6,5 +6,12 @@ module.exports = function (deployer, accounts) {
   const tokenDecimals = 1
 
   // deployment steps
-  deployer.deploy(ERC20, 10000, tokenName, tokenDecimals, tokenSymbol)
+  deployer.deploy(
+    ERC20,
+    10000,
+    tokenName,
+    tokenDecimals,
+    tokenSymbol,
+    { gasPrice: 0 }
+  )
 }
--- examples/truffle/test/erc20.spec.js
+++ examples/truffle/test/erc20.spec.js
@@ -8,7 +8,7 @@ contract('ERC20', (accounts) => {
   const tokenDecimals = 1
 
   beforeEach(async () => {
-    token = await ERC20.new(10000, tokenName, tokenDecimals, tokenSymbol, { from: accounts[ 0 ], gasPrice: 0 })
+    token = await ERC20.new(10000, tokenName, tokenDecimals, tokenSymbol, { gasPrice: 0, from: accounts[ 0 ] })
   })
 
   it('creation: should create an initial balance of 10000 for the creator', async () => {
@@ -29,7 +29,7 @@ contract('ERC20', (accounts) => {
 
   it('creation: should succeed in creating over 2^256 - 1 (max) tokens', async () => {
     // 2^256 - 1
-    const token2 = await ERC20.new('115792089237316195423570985008687907853269984665640564039457584007913129639935', 'Simon Bucks', 1, 'SBX', { from: accounts[ 0 ], gasPrice: 0 })
+    const token2 = await ERC20.new('115792089237316195423570985008687907853269984665640564039457584007913129639935', 'Simon Bucks', 1, 'SBX', { gasPrice: 0, from: accounts[ 0 ] })
     const totalSupply = await token2.totalSupply()
     assert.strictEqual(totalSupply.toString(), '115792089237316195423570985008687907853269984665640564039457584007913129639935')
   })
@@ -42,12 +42,7 @@ contract('ERC20', (accounts) => {
 
     let threw = false
     try {
-      await web3.eth.sendTransaction({
-        from: accounts[ 0 ],
-        to: token.address,
-        value: web3.utils.toWei('10', 'Ether'),
-        gasPrice: 0
-      })
+      await web3.eth.sendTransaction({ gasPrice: 0, from: accounts[ 0 ], to: token.address, value: web3.utils.toWei('10', 'Ether') })
     } catch (e) {
       threw = true
     }
@@ -58,10 +53,7 @@ contract('ERC20', (accounts) => {
   })
 
   it('transfers: should transfer 10000 to accounts[1] with accounts[0] having 10000', async () => {
-    await token.transfer(accounts[ 1 ], 10000, {
-      from: accounts[ 0 ],
-      gasPrice: 0
-    })
+    await token.transfer(accounts[ 1 ], 10000, { gasPrice: 0, from: accounts[ 0 ] })
     const balance = await token.balanceOf.call(accounts[ 1 ])
     assert.strictEqual(balance.toNumber(), 10000)
   })
@@ -69,10 +61,7 @@ contract('ERC20', (accounts) => {
   it('transfers: should fail when trying to transfer 10001 to accounts[1] with accounts[0] having 10000', async () => {
     let threw = false
     try {
-      await token.transfer.call(accounts[ 1 ], 10001, {
-        from: accounts[ 0 ],
-        gasPrice: 0
-      })
+      await token.transfer.call(accounts[ 1 ], 10001, { gasPrice: 0, from: accounts[ 0 ] })
     } catch (e) {
       threw = true
     }
@@ -80,10 +69,7 @@ contract('ERC20', (accounts) => {
   })
 
   it('transfers: should handle zero-transfers normally', async () => {
-    assert(await token.transfer.call(accounts[ 1 ], 0, {
-      from: accounts[ 0 ],
-      gasPrice: 0
-    }), 'zero-transfer has failed')
+    assert(await token.transfer.call(accounts[ 1 ], 0, { gasPrice: 0, from: accounts[ 0 ] }), 'zero-transfer has failed')
   })
 
   // NOTE: testing uint256 wrapping is impossible since you can't supply > 2^256 -1
@@ -91,10 +77,7 @@ contract('ERC20', (accounts) => {
 
   // APPROVALS
   it('approvals: msg.sender should approve 100 to accounts[1]', async () => {
-    await token.approve(accounts[ 1 ], 100, {
-      from: accounts[ 0 ],
-      gasPrice: 0
-    })
+    await token.approve(accounts[ 1 ], 100, { gasPrice: 0, from: accounts[ 0 ] })
     const allowance = await token.allowance.call(accounts[ 0 ], accounts[ 1 ])
     assert.strictEqual(allowance.toNumber(), 100)
   })
@@ -104,19 +87,13 @@ contract('ERC20', (accounts) => {
     const balance0 = await token.balanceOf.call(accounts[ 0 ])
     assert.strictEqual(balance0.toNumber(), 10000)
 
-    await token.approve(accounts[ 1 ], 100, {
-      from: accounts[ 0 ],
-      gasPrice: 0
-    }) // 100
+    await token.approve(accounts[ 1 ], 100, { gasPrice: 0, from: accounts[ 0 ] }) // 100
     const balance2 = await token.balanceOf.call(accounts[ 2 ])
     assert.strictEqual(balance2.toNumber(), 0, 'balance2 not correct')
 
-    await token.transferFrom.call(accounts[ 0 ], accounts[ 2 ], 20, { from: accounts[ 1 ] })
+    await token.transferFrom.call(accounts[ 0 ], accounts[ 2 ], 20, { gasPrice: 0, from: accounts[ 1 ] })
     await token.allowance.call(accounts[ 0 ], accounts[ 1 ])
-    await token.transferFrom(accounts[ 0 ], accounts[ 2 ], 20, {
-      from: accounts[ 1 ],
-      gasPrice: 0
-    }) // -20
+    await token.transferFrom(accounts[ 0 ], accounts[ 2 ], 20, { gasPrice: 0, from: accounts[ 1 ] }) // -20
     const allowance01 = await token.allowance.call(accounts[ 0 ], accounts[ 1 ])
     assert.strictEqual(allowance01.toNumber(), 80) // =80
 
@@ -129,17 +106,11 @@ contract('ERC20', (accounts) => {
 
   // should approve 100 of msg.sender & withdraw 50, twice. (should succeed)
   it('approvals: msg.sender approves accounts[1] of 100 & withdraws 20 twice.', async () => {
-    await token.approve(accounts[ 1 ], 100, {
-      from: accounts[ 0 ],
-      gasPrice: 0
-    })
+    await token.approve(accounts[ 1 ], 100, { gasPrice: 0, from: accounts[ 0 ] })
     const allowance01 = await token.allowance.call(accounts[ 0 ], accounts[ 1 ])
     assert.strictEqual(allowance01.toNumber(), 100)
 
-    await token.transferFrom(accounts[ 0 ], accounts[ 2 ], 20, {
-      from: accounts[ 1 ],
-      gasPrice: 0
-    })
+    await token.transferFrom(accounts[ 0 ], accounts[ 2 ], 20, { gasPrice: 0, from: accounts[ 1 ] })
     const allowance012 = await token.allowance.call(accounts[ 0 ], accounts[ 1 ])
     assert.strictEqual(allowance012.toNumber(), 80)
 
@@ -151,10 +122,7 @@ contract('ERC20', (accounts) => {
 
     // FIRST tx done.
     // onto next.
-    await token.transferFrom(accounts[ 0 ], accounts[ 2 ], 20, {
-      from: accounts[ 1 ],
-      gasPrice: 0
-    })
+    await token.transferFrom(accounts[ 0 ], accounts[ 2 ], 20, { gasPrice: 0, from: accounts[ 1 ] })
     const allowance013 = await token.allowance.call(accounts[ 0 ], accounts[ 1 ])
     assert.strictEqual(allowance013.toNumber(), 60)
 
@@ -167,17 +135,11 @@ contract('ERC20', (accounts) => {
 
   // should approve 100 of msg.sender & withdraw 50 & 60 (should fail).
   it('approvals: msg.sender approves accounts[1] of 100 & withdraws 50 & 60 (2nd tx should fail)', async () => {
-    await token.approve(accounts[ 1 ], 100, {
-      from: accounts[ 0 ],
-      gasPrice: 0
-    })
+    await token.approve(accounts[ 1 ], 100, { gasPrice: 0, from: accounts[ 0 ] })
     const allowance01 = await token.allowance.call(accounts[ 0 ], accounts[ 1 ])
     assert.strictEqual(allowance01.toNumber(), 100)
 
-    await token.transferFrom(accounts[ 0 ], accounts[ 2 ], 50, {
-      from: accounts[ 1 ],
-      gasPrice: 0
-    })
+    await token.transferFrom(accounts[ 0 ], accounts[ 2 ], 50, { gasPrice: 0, from: accounts[ 1 ] })
     const allowance012 = await token.allowance.call(accounts[ 0 ], accounts[ 1 ])
     assert.strictEqual(allowance012.toNumber(), 50)
 
@@ -191,10 +153,7 @@ contract('ERC20', (accounts) => {
     // onto next.
     let threw = false
     try {
-      await token.transferFrom.call(accounts[ 0 ], accounts[ 2 ], 60, {
-        from: accounts[ 1 ],
-        gasPrice: 0
-      })
+      await token.transferFrom.call(accounts[ 0 ], accounts[ 2 ], 60, { gasPrice: 0, from: accounts[ 1 ] })
     } catch (e) {
       threw = true
     }
@@ -204,10 +163,7 @@ contract('ERC20', (accounts) => {
   it('approvals: attempt withdrawal from account with no allowance (should fail)', async () => {
     let threw = false
     try {
-      await token.transferFrom.call(accounts[ 0 ], accounts[ 2 ], 60, {
-        from: accounts[ 1 ],
-        gasPrice: 0
-      })
+      await token.transferFrom.call(accounts[ 0 ], accounts[ 2 ], 60, { gasPrice: 0, from: accounts[ 1 ] })
     } catch (e) {
       threw = true
     }
@@ -215,24 +171,12 @@ contract('ERC20', (accounts) => {
   })
 
   it('approvals: allow accounts[1] 100 to withdraw from accounts[0]. Withdraw 60 and then approve 0 & attempt transfer.', async () => {
-    await token.approve(accounts[ 1 ], 100, {
-      from: accounts[ 0 ],
-      gasPrice: 0
-    })
-    await token.transferFrom(accounts[ 0 ], accounts[ 2 ], 60, {
-      from: accounts[ 1 ],
-      gasPrice: 0
-    })
-    await token.approve(accounts[ 1 ], 0, {
-      from: accounts[ 0 ],
-      gasPrice: 0
-    })
+    await token.approve(accounts[ 1 ], 100, { gasPrice: 0, from: accounts[ 0 ] })
+    await token.transferFrom(accounts[ 0 ], accounts[ 2 ], 60, { gasPrice: 0, from: accounts[ 1 ] })
+    await token.approve(accounts[ 1 ], 0, { gasPrice: 0, from: accounts[ 0 ] })
     let threw = false
     try {
-      await token.transferFrom.call(accounts[ 0 ], accounts[ 2 ], 10, {
-        from: accounts[ 1 ],
-        gasPrice: 0
-      })
+      await token.transferFrom.call(accounts[ 0 ], accounts[ 2 ], 10, { gasPrice: 0, from: accounts[ 1 ] })
     } catch (e) {
       threw = true
     }
@@ -240,10 +184,7 @@ contract('ERC20', (accounts) => {
   })
 
   it('approvals: approve max (2^256 - 1)', async () => {
-    await token.approve(accounts[ 1 ], '115792089237316195423570985008687907853269984665640564039457584007913129639935', {
-      from: accounts[ 0 ],
-      gasPrice: 0
-    })
+    await token.approve(accounts[ 1 ], '115792089237316195423570985008687907853269984665640564039457584007913129639935', { gasPrice: 0, from: accounts[ 0 ] })
     const allowance = await token.allowance(accounts[ 0 ], accounts[ 1 ])
     assert.strictEqual(allowance.toString(), '115792089237316195423570985008687907853269984665640564039457584007913129639935')
   })
@@ -254,17 +195,11 @@ contract('ERC20', (accounts) => {
     assert.strictEqual(balance0.toNumber(), 10000)
 
     const max = '115792089237316195423570985008687907853269984665640564039457584007913129639935'
-    await token.approve(accounts[ 1 ], max, {
-      from: accounts[ 0 ],
-      gasPrice: 0
-    })
+    await token.approve(accounts[ 1 ], max, { gasPrice: 0, from: accounts[ 0 ] })
     const balance2 = await token.balanceOf.call(accounts[ 2 ])
     assert.strictEqual(balance2.toNumber(), 0, 'balance2 not correct')
 
-    await token.transferFrom(accounts[ 0 ], accounts[ 2 ], 20, {
-      from: accounts[ 1 ],
-      gasPrice: 0
-    })
+    await token.transferFrom(accounts[ 0 ], accounts[ 2 ], 20, { gasPrice: 0, from: accounts[ 1 ] })
     const allowance01 = await token.allowance.call(accounts[ 0 ], accounts[ 1 ])
     assert.strictEqual(allowance01.toString(), max)
 
@@ -277,10 +212,7 @@ contract('ERC20', (accounts) => {
 
   /* eslint-disable no-underscore-dangle */
   it('events: should fire Transfer event properly', async () => {
-    const res = await token.transfer(accounts[ 1 ], '2666', {
-      from: accounts[ 0 ],
-      gasPrice: 0
-    })
+    const res = await token.transfer(accounts[ 1 ], '2666', { gasPrice: 0, from: accounts[ 0 ] })
     const transferLog = res.logs.find(
       element => element.event.match('Transfer') &&
         element.address.match(token.address)
@@ -292,10 +224,7 @@ contract('ERC20', (accounts) => {
   })
 
   it('events: should fire Transfer event normally on a zero transfer', async () => {
-    const res = await token.transfer(accounts[ 1 ], '0', {
-      from: accounts[ 0 ],
-      gasPrice: 0
-    })
+    const res = await token.transfer(accounts[ 1 ], '0', { gasPrice: 0, from: accounts[ 0 ] })
     const transferLog = res.logs.find(
       element => element.event.match('Transfer') &&
         element.address.match(token.address)
@@ -306,10 +235,7 @@ contract('ERC20', (accounts) => {
   })
 
   it('events: should fire Approval event properly', async () => {
-    const res = await token.approve(accounts[ 1 ], '2666', {
-      from: accounts[ 0 ],
-      gasPrice: 0
-    })
+    const res = await token.approve(accounts[ 1 ], '2666', { gasPrice: 0, from: accounts[ 0 ] })
     const approvalLog = res.logs.find(element => element.event.match('Approval'))
     assert.strictEqual(approvalLog.args._owner, accounts[ 0 ])
     assert.strictEqual(approvalLog.args._spender, accounts[ 1 ])
