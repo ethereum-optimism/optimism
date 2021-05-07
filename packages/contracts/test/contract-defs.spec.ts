6a50ac99cc00770832a8b3ac3ebd4a9a4c6dc4b1
Matt Masurka <m.masurka@gmail.com>
2021-05-07 16:23:15 -0700

    Adds contrat-defs test file

---
+++ packages/contracts/test/contract-defs.spec.ts
@@ -0,0 +1,8 @@
+import { ethers, waffle } from 'hardhat'
+import { ContractFactory, Contract, Wallet } from 'ethers'
+
+import { getContractInterface } from '../src/contract-defs'
+
+describe('getContractInterface returns a a contract interface for all contrats', () => {
+  // TODO
+})
