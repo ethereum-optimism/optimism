a42bfa49dd9ac972f38570716357c308d65eecf7
Kelvin Fichter <kelvinfichter@gmail.com>
2021-05-10 14:27:05 -0400

    feat[contracts]: have deploy register multi message relayer on L1

--- packages/contracts/deploy/014-OVM_L1MultiMessageRelayer.deploy.ts
+++ packages/contracts/deploy/014-OVM_L1MultiMessageRelayer.deploy.ts
@@ -5,6 +5,7 @@ import { DeployFunction } from 'hardhat-deploy/dist/types'
 import {
   deployAndRegister,
   getDeployedContract,
+  registerAddress,
 } from '../src/hardhat-deploy-ethers'
 
 const deployFn: DeployFunction = async (hre) => {
@@ -13,11 +14,20 @@ const deployFn: DeployFunction = async (hre) => {
     'Lib_AddressManager'
   )
 
-  await deployAndRegister({
+  const OVM_L1MultiMessageRelayer = await deployAndRegister({
     hre,
     name: 'OVM_L1MultiMessageRelayer',
     args: [Lib_AddressManager.address],
   })
+
+  // OVM_L2MessageRelayer *must* be set to multi message relayer address on L1.
+  if (hre.network.name.includes('mainnet')) {
+    await registerAddress({
+      hre,
+      name: 'OVM_L2MessageRelayer',
+      address: OVM_L1MultiMessageRelayer.address
+    })
+  }
 }
 
 deployFn.dependencies = ['Lib_AddressManager']
