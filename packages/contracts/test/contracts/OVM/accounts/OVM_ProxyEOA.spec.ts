d2c7489a0f397f5e35439190bfce59d27c6b7062
smartcontracts <kelvinfichter@gmail.com>
2021-05-13 17:10:50 -0400

    feat[contracts]: temporarily disable EOA upgrades (#857)
    * feat[contracts]: disable eoa upgrades temporarily
    
    * chore: add changeset
    
    * Update OVM_ProxyEOA.sol
    
    * fix: turn upgrade into a noop
    
    * lint: fix

--- packages/contracts/test/contracts/OVM/accounts/OVM_ProxyEOA.spec.ts
+++ packages/contracts/test/contracts/OVM/accounts/OVM_ProxyEOA.spec.ts
@@ -44,7 +44,9 @@ describe('OVM_ProxyEOA', () => {
     })
   })
 
-  describe('upgrade()', () => {
+  // NOTE: Upgrades are disabled for now but will be re-enabled at a later point in time. See
+  // comment in OVM_ProxyEOA.sol for additional information.
+  describe.skip('upgrade()', () => {
     it(`should upgrade the proxy implementation`, async () => {
       const newImpl = `0x${'81'.repeat(20)}`
       Mock__OVM_ExecutionManager.smocked.ovmADDRESS.will.return.with(
