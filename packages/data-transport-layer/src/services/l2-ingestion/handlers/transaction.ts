5d41ede123a3540ccc948ec608b311a92b257aa6
Mark Tyneway <mark.tyneway@gmail.com>
2021-05-13 17:04:46 -0700

    dtl: fix replica tx indexing (#872)
    * dtl: update constant to 9 million gaslimit
    
    * dtl: parse as rlp without top level type
    
    * chore: add changeset
    
    * test: delete dead code
    
    * lint: fix
    
    * deps: add etherproject transactions
    
    * dtl: better handler for more number types
    
    * tests: fix chainid

--- packages/data-transport-layer/src/services/l2-ingestion/handlers/transaction.ts
+++ packages/data-transport-layer/src/services/l2-ingestion/handlers/transaction.ts
@@ -1,6 +1,6 @@
 /* Imports: External */
-import { ctcCoder } from '@eth-optimism/core-utils'
 import { BigNumber, constants, ethers } from 'ethers'
+import { serialize } from '@ethersproject/transactions'
 
 /* Imports: Internal */
 import { TransportDB } from '../../../db/transport-db'
@@ -37,7 +37,6 @@ export const handleSequencerBlock = {
       blockNumber: BigNumber.from(transaction.l1BlockNumber).toNumber(),
       timestamp: BigNumber.from(transaction.l1Timestamp).toNumber(),
       queueOrigin: transaction.queueOrigin,
-      type: parseTxType(transaction.txType),
       confirmed: false,
     }
 
@@ -54,7 +53,6 @@ export const handleSequencerBlock = {
         nonce: BigNumber.from(transaction.nonce).toNumber(),
         target: transaction.to || constants.AddressZero, // ?
         data: transaction.input,
-        type: transaction.txType,
       }
 
       transactionEntry = {
@@ -62,9 +60,21 @@ export const handleSequencerBlock = {
         gasLimit: SEQUENCER_GAS_LIMIT, // ?
         target: SEQUENCER_ENTRYPOINT_ADDRESS,
         origin: null,
-        data: maybeEncodeSequencerBatchTransaction(
-          decodedTransaction,
-          transaction.txType
+        data: serialize(
+          {
+            value: transaction.value,
+            gasLimit: transaction.gas,
+            gasPrice: transaction.gasPrice,
+            nonce: transaction.nonce,
+            to: transaction.to || constants.AddressZero,
+            data: transaction.input,
+            chainId,
+          },
+          {
+            v: BigNumber.from(transaction.v).toNumber(),
+            r: padHexString(transaction.r, 32),
+            s: padHexString(transaction.s, 32),
+          }
         ),
         decoded: decodedTransaction,
         queueIndex: null,
@@ -110,38 +120,4 @@ export const handleSequencerBlock = {
     await db.putUnconfirmedTransactionEntries([entry.transactionEntry])
     await db.putUnconfirmedStateRootEntries([entry.stateRootEntry])
   },
-}
-
-/**
- * Attempts to encode a sequencer batch transaction.
- * @param transaction Transaction to encode.
- * @param type Transaction type.
- */
-const maybeEncodeSequencerBatchTransaction = (
-  transaction: DecodedSequencerBatchTransaction,
-  type: 'EIP155' | 'EthSign' | null
-): string => {
-  if (type === 'EIP155') {
-    return ctcCoder.eip155TxData.encode(transaction).toLowerCase()
-  } else if (type === 'EthSign') {
-    return ctcCoder.ethSignTxData.encode(transaction).toLowerCase()
-  } else {
-    // Throw?
-    return
-  }
-}
-
-/**
- * Handles differences between the sequencer's enum strings and our own.
- * Will probably want to move this into core-utils eventually.
- * @param type Sequencer transaction type to parse.
- */
-const parseTxType = (
-  type: 'EIP155' | 'EthSign' | null
-): 'EIP155' | 'ETH_SIGN' | null => {
-  if (type === 'EthSign') {
-    return 'ETH_SIGN'
-  } else {
-    return type
-  }
 }
