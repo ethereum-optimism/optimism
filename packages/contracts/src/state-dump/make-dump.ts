1ea5c44813f4140a597d4711cc46c3eb03cae011
Mark Tyneway <mark.tyneway@gmail.com>
2021-05-19 10:00:49 -0700

    optimism: bump gaslimit to 10 million #870 (#871)
    * optimism: bump gaslimit to 10million
    
    * chore: add changeset
    
    * optimism: bump max gaslimit to 10 mil
    
    * chore: add changeset
    
    * chore: remove unnecessary changeset
    
    * chore: remove unnecessary changeset
    
    Co-authored-by: Georgios Konstantopoulos <me@gakonst.com>

--- packages/contracts/src/state-dump/make-dump.ts
+++ packages/contracts/src/state-dump/make-dump.ts
@@ -101,7 +101,7 @@ export const makeStateDump = async (cfg: RollupDeployConfig): Promise<any> => {
     deploymentSigner: signer,
     ovmGasMeteringConfig: {
       minTransactionGasLimit: 0,
-      maxTransactionGasLimit: 9_000_000,
+      maxTransactionGasLimit: 10_000_000,
       maxGasPerQueuePerEpoch: 1_000_000_000_000,
       secondsPerEpoch: 0,
     },
