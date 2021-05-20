1849cc74cf2927867716cf86998c41d6f8aa6dc7
Georgios Konstantopoulos <me@gakonst.com>
2021-05-20 14:03:08 +0300

    fix: configure max gas limit to 11m
    Uniswap V3 contracts require that, otherwise they throw call exceptions at deployment

--- packages/data-transport-layer/src/utils/constants.ts
+++ packages/data-transport-layer/src/utils/constants.ts
@@ -1,3 +1,3 @@
-export const SEQUENCER_GAS_LIMIT = 10_000_000 // TODO: Remove and use value from event.
+export const SEQUENCER_GAS_LIMIT = 11_000_000 // TODO: Remove and use value from event.
 export const SEQUENCER_ENTRYPOINT_ADDRESS =
   '0x4200000000000000000000000000000000000005'
