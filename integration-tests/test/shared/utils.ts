9c804abd26f2767c1cf159a61d455a12b87a5cc5
Maurelian <maurelian@protonmail.ch>
2021-05-21 21:46:00 -0400

    fix: increase gas on fundUser

--- integration-tests/test/shared/utils.ts
+++ integration-tests/test/shared/utils.ts
@@ -101,8 +101,8 @@ export const fundUser = async (
 ) => {
   const value = BigNumber.from(amount)
   const tx = recipient
-    ? gateway.depositTo(recipient, 150_000, '0xFFFF', { value, gasLimit: 150_000 })
-    : gateway.deposit(150_000, '0xFFFF', { value, gasLimit: 150_000 })
+    ? gateway.depositTo(recipient, 1_000_000, '0xFFFF', { value })
+    : gateway.deposit(1_000_000, '0xFFFF', { value })
 
   await waitForXDomainTransaction(watcher, tx, Direction.L1ToL2)
 }
