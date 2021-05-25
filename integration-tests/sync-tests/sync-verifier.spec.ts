2f6f7a192ee67f4315b89e91702449d8fd4e35bd
Annie Ke <annieke8@gmail.com>
2021-05-25 16:25:38 -0700

    cleanup tests and finalize sync test workflow

--- integration-tests/sync-tests/sync-verifier.spec.ts
+++ integration-tests/sync-tests/sync-verifier.spec.ts
@@ -1,6 +1,5 @@
 import chai, { expect } from 'chai'
-import { Wallet, BigNumber, Contract, ContractFactory, providers } from 'ethers'
-import { ethers } from 'hardhat'
+import { Wallet, BigNumber, providers } from 'ethers'
 import { injectL2Context } from '@eth-optimism/core-utils'
 
 import { sleep, l2Provider } from '../test/shared/utils'
@@ -21,11 +20,6 @@ describe('Syncing a verifier', () => {
     let totalElementsAfter = (await env.ctc.getTotalElements()) as BigNumber
     while (totalElementsBefore.eq(totalElementsAfter)) {
       await sleep(500)
-      console.log(
-        `still equal`,
-        totalElementsBefore.toNumber(),
-        totalElementsAfter.toNumber()
-      )
       totalElementsAfter = (await env.ctc.getTotalElements()) as BigNumber
     }
     return totalElementsAfter
@@ -39,7 +33,6 @@ describe('Syncing a verifier', () => {
     // Wait for verifier to be looping
     let logs = await verifier.logs()
     while (!logs.out.includes('Starting Sequencer Loop')) {
-      console.log('Retrieving more logs')
       await sleep(500)
       logs = await verifier.logs()
     }
@@ -47,12 +40,10 @@ describe('Syncing a verifier', () => {
     const verifierProvider = injectL2Context(
       new providers.JsonRpcProvider('http://localhost:8547')
     )
-    console.log(await verifierProvider.getBlock('latest'))
 
     // Wait until verifier has caught up to the sequencer
     let latestVerifierBlock = (await verifierProvider.getBlock('latest')) as any
     while (latestVerifierBlock.number < sequencerBlockNumber) {
-      console.log('waiting for new verifier blocks')
       await sleep(500)
       latestVerifierBlock = (await verifierProvider.getBlock('latest')) as any
     }
@@ -65,21 +56,7 @@ describe('Syncing a verifier', () => {
     wallet = env.l2Wallet
   })
 
-  describe('Basic transactions and ERC20s', () => {
-    const initialAmount = 1000
-    const tokenName = 'OVM Test'
-    const tokenDecimals = 8
-    const TokenSymbol = 'OVM'
-
-    let other: Wallet
-    let Factory__ERC20: ContractFactory
-    let ERC20: Contract
-
-    before(async () => {
-      other = Wallet.createRandom().connect(ethers.provider)
-      Factory__ERC20 = await ethers.getContractFactory('ERC20', wallet)
-    })
-
+  describe('Basic transactions', () => {
     afterEach(async () => {
       await verifier.stop('verifier')
       await verifier.rm()
@@ -104,42 +81,12 @@ describe('Syncing a verifier', () => {
       expect(totalElementsAfter.gt(totalElementsAfter))
 
       const latestSequencerBlock = (await provider.getBlock('latest')) as any
-      console.log(latestSequencerBlock)
 
-      const latestVerifierBlock = await startAndSyncVerifier(
+      const matchingVerifierBlock = await startAndSyncVerifier(
         latestSequencerBlock.number
       )
 
-      expect(latestVerifierBlock.stateRoot).to.eq(
-        latestSequencerBlock.stateRoot
-      )
-    })
-
-    it('should sync ERC20 deployment and transfer', async () => {
-      const totalElementsBefore = (await env.ctc.getTotalElements()) as BigNumber
-      ERC20 = await Factory__ERC20.deploy(
-        initialAmount,
-        tokenName,
-        tokenDecimals,
-        TokenSymbol
-      )
-
-      const transfer = await ERC20.transfer(other.address, 100)
-      await transfer.wait()
-
-      const totalElementsAfter = await waitForBatchSubmission(
-        totalElementsBefore
-      )
-      expect(totalElementsAfter.gt(totalElementsAfter))
-
-      const latestSequencerBlock = (await provider.getBlock('latest')) as any
-      console.log(latestSequencerBlock)
-
-      const latestVerifierBlock = await startAndSyncVerifier(
-        latestSequencerBlock.number
-      )
-
-      expect(latestVerifierBlock.stateRoot).to.eq(
+      expect(matchingVerifierBlock.stateRoot).to.eq(
         latestSequencerBlock.stateRoot
       )
     })
