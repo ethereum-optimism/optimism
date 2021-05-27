e1f8ea180aa7ef6651ef56715192f9064b3a825f
Ben Wilson <bwilson@optimism.io>
2021-05-27 08:17:32 -0400

    Check ETH_CALLS_ALLOWED is set, clean up comments, remove old Dockerfile

--- ops/docker/rpc-proxy/docker-entrypoint.sh
+++ ops/docker/rpc-proxy/docker-entrypoint.sh
@@ -7,12 +7,11 @@ if [ -z "$SEQUENCER" ];then
   exit 1
 fi
 
-# if [ -z "$REPLICAS" ];then
-#   echo "REPLICAS env must be set, exiting"
-#   exit 1
-# fi
+if [ -z "$ETH_CALLS_ALLOWED" ];then
+  echo "ETH_CALLS_ALLOWED env must be set, exiting"
+  exit 1
+fi
 
-# REPLICAS=$REPLICAS \
 gomplate -f /docker-entrypoint.d/nginx.template.conf > /usr/local/openresty/nginx/conf/nginx.conf
 
 cat /usr/local/openresty/nginx/conf/nginx.conf
