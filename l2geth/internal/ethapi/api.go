a64f8161514faee540a9ed90f0c460ff5597cb95
Mark Tyneway <mark.tyneway@gmail.com>
2021-05-28 19:43:41 -0700

    feat: fees v2 (#976)
    * l2 geth: new fee logic
    
    * l2 geth: migrate to fees package
    
    * core-utils: new fee scheme
    
    * chore: add changeset
    
    * l2geth: delete dead code
    
    * integration-tests: fix typo
    
    * integration-tests: fixes
    
    * fees: use fee scalar
    
    * lint: fix
    
    * rollup: correct gas payment comparison
    
    * fix(integration-tests): do not hardcode gas price
    
    * core-utils: update with new scheme
    
    * l2geth: refactor rollup oracle
    
    * l2geth: clean up DoEstimateGas
    
    * l2geth: implement latest scheme
    
    * tests: fix up
    
    * lint: fix
    
    * l2geth: better sycn service test
    
    * optimism: rename to TxGasLimit
    
    * fee: fix docstring
    
    * tests: fix
    
    * variables: rename
    
    * l2geth: prevent users from sending txs with too high of a fee
    
    * integration-tests: fix import
    
    * integration-tests: fix type
    
    * integration-tests: fix gas limits
    
    * lint: fix
    
    * l2geth: log error
    
    Co-authored-by: Georgios Konstantopoulos <me@gakonst.com>

--- l2geth/internal/ethapi/api.go
+++ l2geth/internal/ethapi/api.go
@@ -45,15 +45,17 @@ import (
 	"github.com/ethereum/go-ethereum/p2p"
 	"github.com/ethereum/go-ethereum/params"
 	"github.com/ethereum/go-ethereum/rlp"
+	"github.com/ethereum/go-ethereum/rollup/fees"
 	"github.com/ethereum/go-ethereum/rpc"
 	"github.com/tyler-smith/go-bip39"
 )
 
 const (
-	defaultGasPrice = params.Wei
+	defaultGasPrice = params.Wei * fees.TxGasPrice
 )
 
 var errOVMUnsupported = errors.New("OVM: Unsupported RPC Method")
+var bigDefaultGasPrice = new(big.Int).SetUint64(defaultGasPrice)
 
 // PublicEthereumAPI provides an API to access Ethereum related information.
 // It offers only methods that operate on public data that is freely available to anyone.
@@ -68,7 +70,7 @@ func NewPublicEthereumAPI(b Backend) *PublicEthereumAPI {
 
 // GasPrice always returns 1 gwei. See `DoEstimateGas` below for context.
 func (s *PublicEthereumAPI) GasPrice(ctx context.Context) (*hexutil.Big, error) {
-	return (*hexutil.Big)(big.NewInt(defaultGasPrice)), nil
+	return (*hexutil.Big)(bigDefaultGasPrice), nil
 }
 
 // ProtocolVersion returns the current Ethereum protocol version this node supports
@@ -1037,31 +1039,27 @@ func DoEstimateGas(ctx context.Context, b Backend, args CallArgs, blockNrOrHash 
 	if err != nil {
 		return 0, err
 	}
-
 	// 2a. fetch the data price, depends on how the sequencer has chosen to update their values based on the
 	// l1 gas prices
 	l1GasPrice, err := b.SuggestL1GasPrice(ctx)
 	if err != nil {
 		return 0, err
 	}
-
 	// 2b. fetch the execution gas price, by the typical mempool dynamics
 	l2GasPrice, err := b.SuggestL2GasPrice(ctx)
 	if err != nil {
 		return 0, err
 	}
-
-	var data []byte
-	if args.Data == nil {
-		data = []byte{}
-	} else {
+	data := []byte{}
+	if args.Data != nil {
 		data = *args.Data
 	}
-	// 3. calculate the fee
+	// 3. calculate the fee using just the calldata. The additional overhead of
+	// RLP encoding is covered inside of EncodeL2GasLimit
 	l2GasLimit := new(big.Int).SetUint64(uint64(gasUsed))
-	fee, err := core.CalculateRollupFee(data, l1GasPrice, l2GasLimit, l2GasPrice)
-	if err != nil {
-		return 0, err
+	fee := fees.EncodeTxGasLimit(data, l1GasPrice, l2GasLimit, l2GasPrice)
+	if !fee.IsUint64() {
+		return 0, fmt.Errorf("estimate gas overflow: %s", fee)
 	}
 	return (hexutil.Uint64)(fee.Uint64()), nil
 }
