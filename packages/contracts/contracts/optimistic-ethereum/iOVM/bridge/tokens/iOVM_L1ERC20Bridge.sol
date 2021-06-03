11d5b95876d9b9166992b25976ae611c6b9b9d12
elenadimitrova <elena@arenabg.com>
2021-06-03 07:00:37 +0300

    Split off ERC20Bridge interface for the purposes of being reused in custom ERC20 bridges

--- packages/contracts/contracts/optimistic-ethereum/iOVM/bridge/tokens/iOVM_L1ERC20Bridge.sol
+++ packages/contracts/contracts/optimistic-ethereum/iOVM/bridge/tokens/iOVM_L1ERC20Bridge.sol
@@ -2,57 +2,70 @@
 pragma solidity >0.5.0;
 pragma experimental ABIEncoderV2;
 
-import './iOVM_L1ERC20Bridge.sol';
-
 /**
- * @title iOVM_L1StandardBridge
+ * @title iOVM_L1ERC20Bridge
  */
-interface iOVM_L1StandardBridge is iOVM_L1ERC20Bridge {
+interface iOVM_L1ERC20Bridge {
 
     /**********
      * Events *
      **********/
-    event ETHDepositInitiated(
+
+    event ERC20DepositInitiated(
+        address indexed _l1Token,
+        address indexed _l2Token,
         address indexed _from,
-        address indexed _to,
+        address _to,
         uint256 _amount,
         bytes _data
     );
 
-    event ETHWithdrawalFinalized(
+    event ERC20WithdrawalFinalized(
+        address indexed _l1Token,
+        address indexed _l2Token,
         address indexed _from,
-        address indexed _to,
+        address _to,
         uint256 _amount,
         bytes _data
     );
+
 
     /********************
      * Public Functions *
      ********************/
 
-    function depositETH(
+    function depositERC20(
+        address _l1Token,
+		address _l2Token,
+        uint _amount,
         uint32 _l2Gas,
         bytes calldata _data
     )
-        external
-        payable;
+        external;
 
-    function depositETHTo(
+    function depositERC20To(
+        address _l1Token,
+		address _l2Token,
         address _to,
+        uint _amount,
         uint32 _l2Gas,
         bytes calldata _data
     )
-        external
-        payable;
+        external;
+
 
     /*************************
      * Cross-chain Functions *
      *************************/
-    function finalizeETHWithdrawal(
+
+    function finalizeERC20Withdrawal(
+        address _l1Token,
+		address _l2Token,
         address _from,
         address _to,
         uint _amount,
         bytes calldata _data
     )
         external;
+
 }
