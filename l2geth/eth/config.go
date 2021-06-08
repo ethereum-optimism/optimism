469f52fc78349be7890e072bc811842e3a923f1c
Mark Tyneway <mark.tyneway@gmail.com>
2021-06-08 13:28:16 -0700

    l2geth: more stop using extra config options

--- l2geth/eth/config.go
+++ l2geth/eth/config.go
@@ -81,8 +81,6 @@ var DefaultConfig = Config{
 		// is additional overhead that is unaccounted. Round down to 127000 for
 		// safety.
 		MaxCallDataSize: 127000,
-		DataPrice:       big.NewInt(100 * params.GWei),
-		ExecutionPrice:  big.NewInt(0),
 	},
 	DiffDbCache: 256,
 }
