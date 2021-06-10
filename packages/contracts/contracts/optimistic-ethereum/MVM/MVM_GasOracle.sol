80f4bf6322f264b5e990f821eafdc1a17860a35d
Ubuntu <ubuntu@ip-10-0-0-209.us-east-2.compute.internal>
2021-06-10 04:45:24 +0000

    update gasoracle

--- packages/contracts/contracts/optimistic-ethereum/MVM/MVM_GasOracle.sol
+++ packages/contracts/contracts/optimistic-ethereum/MVM/MVM_GasOracle.sol
@@ -1,20 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >0.5.0 <0.8.0;
 
-contract MVM_GasOracle
+contract MVM_GasOracle{
     uint _l2price;
     address _setter;
-    constructor(uint price, address setter) {
-      _l2price = price;
-      _setter = setter;
+    constructor() {
+      _l2price = 1_000_000_000_000_000_001;
+      _setter = address(0);
     }
 
-    function setPrice(uint price) {
+    function setPrice(uint price) public {
        require (msg.sender == _setter, "NOT ALLOWED");
        _l2price = price;
     }
 
-    function transferSetter(address newsetter) {
+    function transferSetter(address newsetter) public {
        require (msg.sender == _setter, "NOT ALLOWED");
        _setter = newsetter;
     }
