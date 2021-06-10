f50068c0263f5eea9bed32cc1da89890fa83b3c9
Maurelian <maurelian@protonmail.ch>
2021-06-10 12:44:37 -0400

    fix(contracts): deploy with chugsplash proxy

--- packages/contracts/deploy/015-Proxy__OVM_L1StandardBridge.deploy.ts
+++ packages/contracts/deploy/015-Proxy__OVM_L1StandardBridge.deploy.ts
@@ -7,7 +7,7 @@ import { predeploys } from '../src/predeploys'
 import { NON_ZERO_ADDRESS } from '../test/helpers/constants'
 import { getContractFactory } from '../src/contract-defs'
 
-import l1StandardBridgeJson from '../artifacts/contracts/optimistic-ethereum/OVM/bridge/tokens/OVM_L1StandardBridge.sol/OVM_L1StandardBridge.json
+import l1StandardBridgeJson from '../artifacts/contracts/optimistic-ethereum/OVM/bridge/tokens/OVM_L1StandardBridge.sol/OVM_L1StandardBridge.json'
 
 const deployFn: DeployFunction = async (hre) => {
   const { deploy } = hre.deployments
@@ -20,14 +20,14 @@ const deployFn: DeployFunction = async (hre) => {
       signerOrProvider: deployer,
     }
   )
-  const addressManagerOwner = '0x9BA6e03D8B90dE867373Db8cF1A58d2F7F006b3A'
 
   const result = await deploy('Proxy__OVM_L1StandardBridge', {
     contract: 'L1ChugSplashProxy',
     from: deployer,
-    args: [addressManagerOwner],
+    args: [deployer],
     log: true,
   })
+
 
   if (!result.newlyDeployed) {
     return
@@ -41,39 +41,47 @@ const deployFn: DeployFunction = async (hre) => {
       iface: 'L1ChugSplashProxy',
     }
   )
-
+  console.log('deployer:', deployer)
+  console.log("Proxy__OVM_L1StandardBridge.signer.address", await Proxy__OVM_L1StandardBridge.signer.getAddress())
   // Set the implementation code
   const bridgeCode = l1StandardBridgeJson.deployedBytecode
   await Proxy__OVM_L1StandardBridge.setCode(bridgeCode)
+
 
   // Set slot 0 to the L1 Messenger Address
   const l1MessengerAddress = await Lib_AddressManager.getAddress('Proxy__OVM_L1CrossDomainMessenger')
   await Proxy__OVM_L1StandardBridge.setStorage(
     hre.ethers.constants.HashZero,
     hre.ethers.utils.hexZeroPad(l1MessengerAddress, 32)
-  )
+    )
 
-  // Set Slot 1 to the L2 Standard Bridge Address
-  await Proxy__OVM_L1StandardBridge.setStorage(
-    hre.ethers.utils.hexZeroPad("0x01", 32),
-    hre.ethers.utils.hexZeroPad(predeploys.OVM_L2StandardBridge, 32)
-  )
+    // Set Slot 1 to the L2 Standard Bridge Address
+    await Proxy__OVM_L1StandardBridge.setStorage(
+      hre.ethers.utils.hexZeroPad("0x01", 32),
+      hre.ethers.utils.hexZeroPad(predeploys.OVM_L2StandardBridge, 32)
+      )
 
   // Get and initialize the implementation to disable it.
+  const bridgeImplAddress = await Proxy__OVM_L1StandardBridge.getImplementation()
+  console.log('bridgeImplAddress:', bridgeImplAddress)
 
-  const bridgeImplAddress = Proxy__OVM_L1StandardBridge.connect(
-    // connect to a provider to make an eth_call
-    hre.ethers.getDefaultProvider()
-  ).getImplementation()
-  const bridgeImplementation = await getContractFactory(
-    'OVM_L1StandardBridge',
-    deployer
-  ).attach(bridgeImplAddress)
+  // const bridgeImplAddress = await Proxy__OVM_L1StandardBridge.connect(
+  //   hre.ethers.getDefaultProvider() // connect to a provider to make an eth_call
+  // ).getImplementation()
+  // const bridgeImplementation = await getContractFactory(
+  //   'OVM_L1StandardBridge',
+  //   // deploy/015-Proxy__OVM_L1StandardBridge.deploy.ts(70,5): error TS2345: Argument of type 'string' is not assignable to parameter of type 'Signer'.
+  //   deployer
+  // ).attach(bridgeImplAddress)
 
-  await bridgeImplementation.initialize(
-    NON_ZERO_ADDRESS,
-    constants.AddressZero
-  )
+  // await bridgeImplementation.initialize(
+  //   NON_ZERO_ADDRESS,
+  //   NON_ZERO_ADDRESS
+  // )
+
+  // transfer ownership to Address Manager owner
+  const addressManagerOwner = '0x9BA6e03D8B90dE867373Db8cF1A58d2F7F006b3A'
+  await Proxy__OVM_L1StandardBridge.setOwner(addressManagerOwner)
 
   // Todo: remove this after adding chugsplash proxy
   await Lib_AddressManager.setAddress('Proxy__OVM_L1StandardBridge', result.address)
