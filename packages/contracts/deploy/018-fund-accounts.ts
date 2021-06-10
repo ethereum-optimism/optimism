29798d9fa5891214d659f60eb6cbefca8df28801
Kelvin Fichter <kelvinfichter@gmail.com>
2021-06-10 12:19:51 -0400

    feat[contracts]: better account funding for hardhat accounts

---
+++ packages/contracts/deploy/018-fund-accounts.ts
@@ -0,0 +1,61 @@
+/* Imports: External */
+import { DeployFunction } from 'hardhat-deploy/dist/types'
+import {
+  defaultHardhatNetworkHdAccountsConfigParams,
+  defaultHardhatNetworkParams,
+} from 'hardhat/internal/core/config/default-config'
+import { normalizeHardhatNetworkAccountsConfig } from 'hardhat/internal/core/providers/util'
+
+/* Imports: Internal */
+import { getDeployedContract } from '../src/hardhat-deploy-ethers'
+
+// This is a TEMPORARY way to fund the default hardhat accounts on L2. The better way to do this is
+// to make a modification to hardhat-ovm. However, I don't have the time right now to figure the
+// details of how to make that work cleanly. This is fine in the meantime.
+const deployFn: DeployFunction = async (hre) => {
+  // Only execute this step if we're on the hardhat chain ID.
+  const { chainId } = await hre.ethers.provider.getNetwork()
+  if (chainId === defaultHardhatNetworkParams.chainId) {
+    const Proxy__OVM_L1ETHGateway = await getDeployedContract(
+      hre,
+      'Proxy__OVM_L1ETHGateway',
+      {
+        iface: 'OVM_L1ETHGateway',
+      }
+    )
+
+    const accounts = normalizeHardhatNetworkAccountsConfig(
+      defaultHardhatNetworkHdAccountsConfigParams
+    )
+
+    // Fund the accounts in parallel to speed things up.
+    await Promise.all(
+      accounts.map(async (account) => {
+        const wallet = new hre.ethers.Wallet(
+          account.privateKey,
+          hre.ethers.provider
+        )
+        const balance = await wallet.getBalance()
+        const depositAmount = balance.div(2) // Deposit half of the wallet's balance into L2.
+        await Proxy__OVM_L1ETHGateway.connect(wallet).depositTo(
+          wallet.address,
+          8_000_000,
+          '0x',
+          {
+            value: depositAmount,
+          }
+        )
+        console.log(
+          `✓ Funded ${wallet.address} on L2 with ${hre.ethers.utils.formatEther(
+            depositAmount
+          )} ETH`
+        )
+      })
+    )
+  }
+}
+
+deployFn.dependencies = ['Proxy__OVM_L1ETHGateway']
+deployFn.tags = ['fund-accounts']
+
+export default deployFn
