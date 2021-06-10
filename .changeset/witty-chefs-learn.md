c2a0489372c2e2b34f779574c4b875d0e39b6e83
Mark Tyneway <mark.tyneway@gmail.com>
2021-06-10 15:06:30 -0700

    fix: abi encoded tx (#1049)
    * contracts: don't double rlp decode
    
    * chore: add changeset
    
    * lint: fix
    
    * deps: update
    
    * linting: cleanup
    
    * feat: contracts: use selector
    
    * fix: contracts: use typescript
    
    * contracts: use interface

---
+++ .changeset/witty-chefs-learn.md
@@ -0,0 +1,5 @@
+---
+'@eth-optimism/contracts': patch
+---
+
+Do not RLP decode the transaction in the OVM_ECDSAContractAccount
--- packages/contracts/test/helpers/types/ovm-types.ts
+++ packages/contracts/test/helpers/types/ovm-types.ts
@@ -1,5 +1,6 @@
 /* External Imports */
-import { BigNumber } from 'ethers'
+import { BigNumber, constants } from 'ethers'
+import { parse, Transaction } from '@ethersproject/transactions'
 
 export interface OVMAccount {
   nonce: number | BigNumber
@@ -7,4 +8,25 @@ export interface OVMAccount {
   storageRoot: string
   codeHash: string
   ethAddress: string
+}
+
+export const LibEIP155TxStruct = (tx: Transaction | string): Array<any> => {
+  if (typeof tx === 'string') {
+    tx = parse(tx)
+  }
+  const values = [
+    tx.nonce,
+    tx.gasPrice,
+    tx.gasLimit,
+    tx.to ? tx.to : constants.AddressZero,
+    tx.value,
+    tx.data,
+    tx.v % 256,
+    tx.r,
+    tx.s,
+    tx.chainId,
+    tx.v === 0 ? 0 : tx.v - 2 * tx.chainId - 35,
+    tx.to === null,
+  ]
+  return values
 }
