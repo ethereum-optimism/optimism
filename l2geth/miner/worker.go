c05909a7c3cd821809a033aa7ff88cb0bfa0ecfd
Kelvin Fichter <kelvinfichter@gmail.com>
2021-06-14 15:21:39 -0400

    fix[l2geth]: off-by-one sometimes breaking replica sync

--- l2geth/miner/worker.go
+++ l2geth/miner/worker.go
@@ -196,7 +196,7 @@ func newWorker(config *Config, chainConfig *params.ChainConfig, engine consensus
 		unconfirmed:        newUnconfirmedBlocks(eth.BlockChain(), miningLogAtDepth),
 		pendingTasks:       make(map[common.Hash]*task),
 		txsCh:              make(chan core.NewTxsEvent, txChanSize),
-		rollupCh:           make(chan core.NewTxsEvent, txChanSize),
+		rollupCh:           make(chan core.NewTxsEvent, 1),
 		chainHeadCh:        make(chan core.ChainHeadEvent, chainHeadChanSize),
 		chainSideCh:        make(chan core.ChainSideEvent, chainSideChanSize),
 		newWorkCh:          make(chan *newWorkReq),
