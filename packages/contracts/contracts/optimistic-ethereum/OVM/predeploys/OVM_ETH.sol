c43b33ecfe367b4f0b2006ab55073c7138bfa3c1
Kevin Ho <kevinjho1996@gmail.com>
2021-06-15 10:50:34 -0400

    WETH deposit and withdraw on OVM_ETH (#1083)
    * feat(contracts): add no-op WETH9 functionality to OVM_ETH
    
    * working WETH deposit and withdraw + tests
    
    * add changeset
    
    * address PR feedback
    
    * update WETH9 contract implementation
    
    * add fallback to WETH9
    
    * add fallback and revert withdraw test
    
    * update nit comment
    
    Co-authored-by: ben <ben@pseudonym.party>

--- packages/contracts/contracts/optimistic-ethereum/OVM/predeploys/OVM_ETH.sol
+++ packages/contracts/contracts/optimistic-ethereum/OVM/predeploys/OVM_ETH.sol
@@ -6,6 +6,7 @@ import { Lib_PredeployAddresses } from "../../libraries/constants/Lib_PredeployA
 
 /* Contract Imports */
 import { L2StandardERC20 } from "../../libraries/standards/L2StandardERC20.sol";
+import { IWETH9 } from "../../libraries/standards/IWETH9.sol";
 
 /**
  * @title OVM_ETH
@@ -15,7 +16,7 @@ import { L2StandardERC20 } from "../../libraries/standards/L2StandardERC20.sol";
  * Compiler used: optimistic-solc
  * Runtime target: OVM
  */
-contract OVM_ETH is L2StandardERC20 {
+contract OVM_ETH is L2StandardERC20, IWETH9 {
 
     /***************
      * Constructor *
@@ -29,4 +30,50 @@ contract OVM_ETH is L2StandardERC20 {
             "ETH"
         )
     {}
+
+
+    /******************************
+     * Custom WETH9 Functionality *
+     ******************************/
+    fallback() external payable {
+        deposit();
+    }
+
+    /**
+     * Implements the WETH9 deposit() function as a no-op.
+     * WARNING: this function does NOT have to do with cross-chain asset bridging. The
+     * relevant deposit and withdraw functions for that use case can be found at L2StandardBridge.sol.
+     * This function allows developers to treat OVM_ETH as WETH without any modifications to their code.
+     */
+    function deposit()
+        public
+        payable
+        override
+    {
+        // Calling deposit() with nonzero value will send the ETH to this contract address.  Once recieved here,
+        // We transfer it back by sending to the msg.sender.
+        _transfer(address(this), msg.sender, msg.value);
+
+        emit Deposit(msg.sender, msg.value);
+    }
+
+    /**
+     * Implements the WETH9 withdraw() function as a no-op.
+     * WARNING: this function does NOT have to do with cross-chain asset bridging. The
+     * relevant deposit and withdraw functions for that use case can be found at L2StandardBridge.sol.
+     * This function allows developers to treat OVM_ETH as WETH without any modifications to their code.
+     * @param _wad Amount being withdrawn
+     */
+    function withdraw(
+        uint256 _wad
+    )
+        external
+        override
+    {
+        // Calling withdraw() with value exceeding the withdrawer's ovmBALANCE should revert, as in WETH9.
+        require(balanceOf(msg.sender) >= _wad);
+
+        // Other than emitting an event, OVM_ETH already is native ETH, so we don't need to do anything else.
+        emit Withdrawal(msg.sender, _wad);
+    }
 }
