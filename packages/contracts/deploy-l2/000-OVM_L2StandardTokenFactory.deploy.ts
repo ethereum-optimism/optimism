4a0b5f0463f824709b80453d04f282eae0ceabde
elenadimitrova <elena@arenabg.com>
2021-06-25 16:02:25 +0300

    Add deployment script for the L2 token factory

---
+++ packages/contracts/deploy-l2/000-OVM_L2StandardTokenFactory.deploy.ts
@@ -0,0 +1,31 @@
+/* Imports: External */
+import { DeployFunction } from 'hardhat-deploy/dist/types'
+
+/* Imports: Internal */
+import { getContractDefinition } from '../src'
+
+const deployFn: DeployFunction = async (hre: any) => {
+  const { deployments, getNamedAccounts } = hre
+  const { deploy } = deployments
+  const { deployer } = await getNamedAccounts()
+
+  const l2TokenFactory = getContractDefinition('OVM_L2StandardTokenFactory', true)
+
+  const factoryOwner = (hre as any).deployConfig.ovmSequencerAddress
+  const initialGasPrice = (hre as any).deployConfig.initialGasPriceOracleGasPrice
+
+  if (!factoryOwner || !initialGasPrice) {
+    throw new Error('initialGasPrice & ovmSequencerAddress required to deploy gas price oracle')
+  }
+
+  await deploy('OVM_L2StandardTokenFactory', {
+    contract: l2TokenFactory,
+    from: deployer,
+    args: [factoryOwner, initialGasPrice],
+    log: true,
+  });
+}
+
+deployFn.tags = ['OVM_L2StandardTokenFactory']
+
+export default deployFn
