a3fab659595ecb7cf933b13ad0068b9de401362e
Kevin Ho <kevinjho1996@gmail.com>
2021-07-12 13:50:20 -0400

    Add dual integration tests to SNX test suite
    Adds "dual" integration tests (deposits and withdraws) to the external SNX test suite

--- integration-tests/ext-test/snx.sh
+++ integration-tests/ext-test/snx.sh
@@ -3,3 +3,4 @@ git clone --depth=1 --branch develop https://github.com/Synthetixio/synthetix.gi
 cd synthetix
 npm install
 npx hardhat --config ./hardhat.config.js test:integration:l2 --compile --deploy
+npx hardhat --config ./hardhat.config.js test:integration:dual
