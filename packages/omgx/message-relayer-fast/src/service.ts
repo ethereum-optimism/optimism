7b2b426a643a74c8e7e241ed8bce6fd9e0c75f7c
cby3149 <46272347+cby3149@users.noreply.github.com>
2021-08-12 16:29:34 -0400

    Remove eventCache (#279)
    Co-authored-by: CAPtheorem <79423264+CAPtheorem@users.noreply.github.com>

--- packages/omgx/message-relayer-fast/src/service.ts
+++ packages/omgx/message-relayer-fast/src/service.ts
@@ -160,6 +160,9 @@ export class MessageRelayerService extends BaseService<MessageRelayerOptions> {
       await this._getfilter();
 
       try {
+        // The message is relayed directly, no need to keep cache
+        this.state.eventCache = [];
+
         // Check that the correct address is set in the address manager
         const relayer = await this.state.Lib_AddressManager.getAddress(
           'OVM_L2MessageRelayer'
