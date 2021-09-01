0e14855c11e5a8304938684d585861199b68c031
Mark Tyneway <mark.tyneway@gmail.com>
2021-09-01 13:04:26 -0700

    l2geth: configurable min L2 gas limit
    This commit adds a new config flag `--rollup.minl2gaslimit` that
    defaults to `0` and is used to prevent transactions from being ingested
    that have a L2 gas limit smaller than the value configured. This
    config flag can also be controlled with the env var
    `ROLLUP_MIN_L2_GAS_LIMIT`.

---
+++ .changeset/popular-tools-jog.md
@@ -0,0 +1,5 @@
+---
+'@eth-optimism/l2geth': patch
+---
+
+Add in min accepted L2 gas limit config flag
--- l2geth/cmd/geth/usage.go
+++ l2geth/cmd/geth/usage.go
@@ -81,6 +81,7 @@ var AppHelpFlagGroups = []flagGroup{
 			utils.RollupMaxCalldataSizeFlag,
 			utils.RollupBackendFlag,
 			utils.RollupEnforceFeesFlag,
+			utils.RollupMinL2GasLimitFlag,
 			utils.RollupFeeThresholdDownFlag,
 			utils.RollupFeeThresholdUpFlag,
 			utils.GasPriceOracleOwnerAddress,
