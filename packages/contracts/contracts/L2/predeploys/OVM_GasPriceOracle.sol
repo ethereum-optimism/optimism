d9c875f54d65417d301dd0b1779ca15d39db4b23
Annie Ke <annieke8@gmail.com>
2021-09-03 15:10:41 -0700

    Switch pragma to 0.8.4 on modules

--- packages/contracts/contracts/L2/predeploys/OVM_GasPriceOracle.sol
+++ packages/contracts/contracts/L2/predeploys/OVM_GasPriceOracle.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >0.5.0 <0.8.0;
+pragma solidity ^0.8.4;
 
 /* Internal Imports */
 import { iOVM_GasPriceOracle } from "./iOVM_GasPriceOracle.sol";
--- packages/contracts/contracts/L2/predeploys/OVM_SequencerFeeVault.sol
+++ packages/contracts/contracts/L2/predeploys/OVM_SequencerFeeVault.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >0.5.0 <0.8.0;
+pragma solidity ^0.8.4;
 
 /* Library Imports */
 import { Lib_PredeployAddresses } from "../../libraries/constants/Lib_PredeployAddresses.sol";
