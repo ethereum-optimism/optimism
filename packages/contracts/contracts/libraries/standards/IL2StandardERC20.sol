012dcb7b1679847e855ad4ef172d6afd93208fb1
Annie Ke <annieke8@gmail.com>
2021-09-03 15:10:41 -0700

    Switch pragma to 0.8.4 on modules

--- packages/contracts/contracts/libraries/standards/IL2StandardERC20.sol
+++ packages/contracts/contracts/libraries/standards/IL2StandardERC20.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.5.16 <0.8.0;
+pragma solidity ^0.8.4;
 
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { IERC165 } from "@openzeppelin/contracts/introspection/IERC165.sol";
