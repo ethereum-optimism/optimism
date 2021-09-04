9d315ae423d0ae9ccf41ed8fb661690992d69b38
kf <kelvinfichter@gmail.com>
2021-09-04 06:35:52 +0000

    refactor: remove custom genesis logic

--- l2geth/rollup/sync_service.go
+++ l2geth/rollup/sync_service.go
@@ -63,7 +63,6 @@ type SyncService struct {
 	txLock                         sync.Mutex
 	loopLock                       sync.Mutex
 	enable                         bool
-	eth1ChainId                    uint64
 	bc                             *core.BlockChain
 	txpool                         *core.TxPool
 	RollupGpo                      *gasprice.RollupOracle
@@ -156,7 +155,6 @@ func NewSyncService(ctx context.Context, cfg Config, txpool *core.TxPool, bc *co
 		bc:                             bc,
 		txpool:                         txpool,
 		chainHeadCh:                    make(chan core.ChainHeadEvent, 1),
-		eth1ChainId:                    cfg.Eth1ChainId,
 		client:                         client,
 		db:                             db,
 		pollInterval:                   pollInterval,
@@ -263,7 +261,7 @@ func (s *SyncService) Start() error {
 		log.Info("Running without syncing enabled")
 		return nil
 	}
-	log.Info("Initializing Sync Service", "eth1-chainid", s.eth1ChainId)
+	log.Info("Initializing Sync Service")
 	if err := s.updateGasPriceOracleCache(nil); err != nil {
 		return err
 	}
