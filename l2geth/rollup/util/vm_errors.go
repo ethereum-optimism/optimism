b4bf74aaf8ff63cabb3928fd34bdffb3263844b9
Kelvin Fichter <kelvinfichter@gmail.com>
2021-09-08 20:39:28 -0700

    feat: reintroduce whitelist

---
+++ l2geth/rollup/util/vm_errors.go
@@ -0,0 +1,40 @@
+package util
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/ethereum/go-ethereum/accounts/abi"
+)
+
+var codec abi.ABI
+
+func init() {
+	const abidata = `
+	[
+		{
+			"type": "function",
+			"name": "Error",
+			"constant": true,
+			"inputs": [
+				{
+					"name": "msg",
+					"type": "string"
+				}
+      ],
+			"outputs": []
+		}
+	]
+`
+
+	var err error
+	codec, err = abi.JSON(strings.NewReader(abidata))
+	if err != nil {
+		panic(fmt.Errorf("unable to create abi decoder: %v", err))
+	}
+}
+
+// EncodeSolidityError generates an abi-encoded error message.
+func EncodeSolidityError(err error) ([]byte, error) {
+	return codec.Pack("Error", err.Error())
+}
