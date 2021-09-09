cd15141504906ab939507172e4ceb534db7e2f34
CAPtheorem <79423264+CAPtheorem@users.noreply.github.com>
2021-09-09 10:38:20 -0700

    Update LayerSwitcher.js

--- packages/omgx/wallet-frontend/src/components/mainMenu/layerSwitcher/LayerSwitcher.js
+++ packages/omgx/wallet-frontend/src/components/mainMenu/layerSwitcher/LayerSwitcher.js
@@ -1,4 +1,21 @@
-import React, { useState, useCallback } from 'react'
+
+ /*
+ Copyright 2019-present OmiseGO Pte Ltd
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License. */
+
+ import React, { useCallback } from 'react'
+
 import { Box } from '@material-ui/system'
 import { useSelector, useDispatch } from 'react-redux'
 import * as S from './LayerSwitcher.styles.js'
@@ -6,14 +23,14 @@ import { selectLayer } from 'selectors/setupSelector'
 import { setLayer } from 'actions/setupAction'
 import { Typography } from '@material-ui/core'
 import networkService from 'services/networkService'
-import Button from 'components/button/Button';
+import Button from 'components/button/Button'
 
-import LayerIcon from 'components/icons/LayerIcon';
+import LayerIcon from 'components/icons/LayerIcon'
 
 function LayerSwitcher({ walletEnabled, isButton = false }) {
 
   const dispatch = useDispatch()
-  const [ showAllLayers, setShowAllLayers ] = useState(false)
+
   let layer = useSelector(selectLayer())
 
   if (networkService.L1orL2 !== layer) {
@@ -33,7 +50,7 @@ function LayerSwitcher({ walletEnabled, isButton = false }) {
     dispatch(setLayer(layer))
     networkService.switchChain(layer)
     setShowAllLayers(false)
-  }, [ dispatch, setShowAllLayers ])
+  }, [ dispatch ])
 
   if (!!isButton) {
     return (<>
