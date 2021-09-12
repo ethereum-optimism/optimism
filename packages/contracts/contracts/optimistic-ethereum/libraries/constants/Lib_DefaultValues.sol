18b4198e33bb32f8fafdbadb5768618c309de83c
Davis Shaver <davisshaver@gmail.com>
2021-09-12 05:26:51 -0400

    chore(contracts): move xdomain to defaults lib

--- packages/contracts/contracts/optimistic-ethereum/libraries/constants/Lib_DefaultValues.sol
+++ packages/contracts/contracts/optimistic-ethereum/libraries/constants/Lib_DefaultValues.sol
@@ -7,9 +7,14 @@ pragma experimental ABIEncoderV2;
  */
 library Lib_DefaultValues {
 
-  address internal constant DEFAULT_ADDRESS = 0xdEfa017defA017DeFA017DEfa017DeFA017DeFa0;
+    address internal constant DEFAULT_ADDRESS = 0xdEfa017defA017DeFA017DEfa017DeFA017DeFa0;
 
-  uint256 internal constant DEFAULT_UINT256 =
+    uint256 internal constant DEFAULT_UINT256 =
     0xdefa017defa017defa017defa017defa017defa017defa017defa017defa017d;
+
+    // The default x-domain message sender being set to a non-zero value makes
+    // deployment a bit more expensive, but in exchange the refund on every call to
+    // `relayMessage` by the L1 and L2 messengers will be higher.
+    address internal constant DEFAULT_XDOMAIN_SENDER = 0x000000000000000000000000000000000000dEaD;
 
 }
