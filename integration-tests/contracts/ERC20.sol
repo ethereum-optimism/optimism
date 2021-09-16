b8f5056d7dfb044724b67888d3633add4dbea687
elenadimitrova <elena@arenabg.com>
2021-09-16 13:29:50 +0300

    Switch pragma to 0.8.4 for all contracts in integration-tests and batch-submitter packages

--- integration-tests/contracts/ERC20.sol
+++ integration-tests/contracts/ERC20.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.7.0 <0.8.0;
+pragma solidity ^0.8.4;
 
 contract ERC20 {
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
