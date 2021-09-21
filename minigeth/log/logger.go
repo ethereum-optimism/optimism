d01b4d7b7b10c8754a381d77933ac024b3c07f39
George Hotz <geohot@gmail.com>
2021-09-20 21:16:41 -0700

    add logger

---
+++ minigeth/log/logger.go
@@ -0,0 +1,21 @@
+package log
+
+import (
+	"fmt"
+)
+
+func Debug(msg string, ctx ...interface{}) {
+	fmt.Println(msg, ctx)
+}
+
+func Info(msg string, ctx ...interface{}) {
+	fmt.Println(msg, ctx)
+}
+
+func Warn(msg string, ctx ...interface{}) {
+	fmt.Println(msg, ctx)
+}
+
+func Error(msg string, ctx ...interface{}) {
+	fmt.Println(msg, ctx)
+}
