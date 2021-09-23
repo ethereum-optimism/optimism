dc0c393c4ae4b93e57227ddbdcaba14454036cf3
Maurelian <maurelian@protonmail.ch>
2021-09-23 10:51:23 -0400

    test(contracts): Assert upper bound on CTC gas costs.
    Previously our gas specs have only printed the values they record,
    without ensuring that the cost doesn't drift up over time. Adding expectApprox will allow these costs to decrease, but not increase.

--- packages/contracts/test/helpers/gas/gas.ts
+++ packages/contracts/test/helpers/gas/gas.ts
@@ -1,5 +1,6 @@
+import { expect } from 'chai'
 import { ethers } from 'hardhat'
-import { Contract, Signer } from 'ethers'
+import { BigNumber, Contract, Signer } from 'ethers'
 
 export class GasMeasurement {
   GasMeasurementContract: Contract
@@ -23,4 +24,40 @@ export class GasMeasurement {
 
     return gasCost
   }
+}
+
+interface percentDeviationRange {
+  upperPercentDeviation: number
+  lowerPercentDeviation?: number
+}
+
+export const expectApprox = (
+  actual: BigNumber | number,
+  target: BigNumber | number,
+  { upperPercentDeviation, lowerPercentDeviation = 100 }: percentDeviationRange
+): void => {
+  actual = BigNumber.from(actual)
+  target = BigNumber.from(target)
+
+  const validDeviations =
+    upperPercentDeviation >= 0 &&
+    upperPercentDeviation <= 100 &&
+    lowerPercentDeviation >= 0 &&
+    lowerPercentDeviation <= 100
+  if (!validDeviations) {
+    throw new Error(
+      'Upper and lower deviation percentage arguments should be between 0 and 100'
+    )
+  }
+  const upper = target.mul(100 + upperPercentDeviation).div(100)
+  const lower = target.mul(100 - lowerPercentDeviation).div(100)
+
+  expect(
+    actual.lte(upper),
+    `Actual value (${actual}) is more than ${upperPercentDeviation}% greater than target (${target})`
+  ).to.be.true
+  expect(
+    actual.gte(lower),
+    `Actual value (${actual}) is more than ${lowerPercentDeviation}% less than target (${target})`
+  ).to.be.true
 }
