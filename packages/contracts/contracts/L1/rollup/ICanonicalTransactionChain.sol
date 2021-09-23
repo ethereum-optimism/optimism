bc173bd425d3915a6f9c26c1136534e4ce7587e7
elenadimitrova <elena@arenabg.com>
2021-09-23 15:37:22 +0300

    Switch interfaces back to solidity pragma 0.5 to 0.9 as these serve as integration points for projects

--- packages/contracts/contracts/L1/rollup/ICanonicalTransactionChain.sol
+++ packages/contracts/contracts/L1/rollup/ICanonicalTransactionChain.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.7;
+pragma solidity >0.5.0 <0.9.0;
 
 /* Library Imports */
 import { Lib_OVMCodec } from "../../libraries/codec/Lib_OVMCodec.sol";
