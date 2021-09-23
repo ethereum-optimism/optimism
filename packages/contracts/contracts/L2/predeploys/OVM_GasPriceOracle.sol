6a7698d7cf727987b403400cd623726d4c4e9ef2
Mark Tyneway <mark.tyneway@gmail.com>
2021-09-23 14:56:22 -0700

    contracts: update `OVM_GasPriceOracle`
    Add a setter for L1 base fee. The trusted oracle can update it over
    time. Eventually this will become trustless, but this is a quick and
    easy approach for now.
    
    Also add a setter/getter for the per batch overhead and the scalar.
    
    Also emit events for when the values are updated. This will make it much
    easier to track historical gas prices over time.
    
    Add tests for new functionality. L2 geth will consume the new value in
    the `OVM_GasPriceOracle`
    
    Add getters that make it easy for users to know the L1 costs.
    - `getL1Fee` returns the L1 fee given the current L1 base fee known by
      the L2 node
    - `getL1GasUsed` counts the bytes and creates a sum for the gas cost of
      submitting the data to L1

--- packages/contracts/contracts/L2/predeploys/OVM_GasPriceOracle.sol
+++ packages/contracts/contracts/L2/predeploys/OVM_GasPriceOracle.sol
@@ -1,8 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >0.5.0 <0.8.0;
 
+/* Internal Imports */
+import { iOVM_GasPriceOracle } from "./iOVM_GasPriceOracle.sol";
+
 /* External Imports */
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import { SafeMath } from "@openzeppelin/contracts/math/SafeMath.sol";
 
 /**
  * @title OVM_GasPriceOracle
@@ -10,15 +14,28 @@ import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
  * currently is. This measure is used by the Sequencer to determine what fee to charge for
  * transactions. When the system is more congested, the l2 gas price will increase and fees
  * will also increase as a result.
+ *
+ * All public variables are set while generating the initial L2 state. The
+ * constructor doesn't run in practice as the L2 state generation script uses
+ * the deployed bytecode instead of running the initcode.
  */
-contract OVM_GasPriceOracle is Ownable {
+contract OVM_GasPriceOracle is Ownable, iOVM_GasPriceOracle {
 
     /*************
      * Variables *
      *************/
 
-    // Current l2 gas price
+
+    // Current L2 gas price
     uint256 public gasPrice;
+    // Current L1 base fee
+    uint256 public l1BaseFee;
+    // Amortized cost of batch submission per transaction
+    uint256 public overhead;
+    // Value to scale the fee up by
+    uint256 public scalar;
+    // Number of decimals of the scalar
+    uint256 public decimals;
 
     /***************
      * Constructor *
@@ -28,12 +45,10 @@ contract OVM_GasPriceOracle is Ownable {
      * @param _owner Address that will initially own this contract.
      */
     constructor(
-        address _owner,
-        uint256 _initialGasPrice
+        address _owner
     )
         Ownable()
     {
-        setGasPrice(_initialGasPrice);
         transferOwnership(_owner);
     }
 
@@ -50,8 +65,135 @@ contract OVM_GasPriceOracle is Ownable {
         uint256 _gasPrice
     )
         public
+        override
         onlyOwner
     {
         gasPrice = _gasPrice;
+        emit GasPriceUpdated(_gasPrice);
+    }
+
+    /**
+     * Allows the owner to modify the l1 base fee.
+     * @param _baseFee New l1 base fee
+     */
+    function setL1BaseFee(
+        uint256 _baseFee
+    )
+        public
+        override
+        onlyOwner
+    {
+        l1BaseFee = _baseFee;
+        emit L1BaseFeeUpdated(_baseFee);
+    }
+
+    /**
+     * Allows the owner to modify the overhead.
+     * @param _overhead New overhead
+     */
+    function setOverhead(
+        uint256 _overhead
+    )
+        public
+        override
+        onlyOwner
+    {
+        overhead = _overhead;
+        emit OverheadUpdated(_overhead);
+    }
+
+    /**
+     * Allows the owner to modify the scalar.
+     * @param _scalar New scalar
+     */
+    function setScalar(
+        uint256 _scalar
+    )
+        public
+        override
+        onlyOwner
+    {
+        scalar = _scalar;
+        emit ScalarUpdated(_scalar);
+    }
+
+    /**
+     * Allows the owner to modify the decimals.
+     * @param _decimals New decimals
+     */
+    function setDecimals(
+        uint256 _decimals
+    )
+        public
+        override
+        onlyOwner
+    {
+        decimals = _decimals;
+        emit DecimalsUpdated(_decimals);
+    }
+
+    /**
+     * Computes the L1 portion of the fee
+     * based on the size of the RLP encoded tx
+     * and the current l1BaseFee
+     * @param _data Unsigned RLP encoded tx, 6 elements
+     * @return L1 fee that should be paid for the tx
+     */
+    function getL1Fee(bytes memory _data)
+        public
+        view
+        override
+        returns (
+            uint256
+        )
+    {
+        uint256 l1GasUsed = getL1GasUsed(_data);
+        uint256 l1Fee = SafeMath.mul(l1GasUsed, l1BaseFee);
+        uint256 divisor = 10**decimals;
+        uint256 unscaled = SafeMath.mul(l1Fee, scalar);
+        uint256 scaled = SafeMath.div(unscaled, divisor);
+        return scaled;
+    }
+
+    /**
+     * Computes the amount of L1 gas used for a transaction
+     * The overhead represents the per batch gas overhead of
+     * posting both transaction and state roots to L1 given larger
+     * batch sizes.
+     * 4 gas for 0 byte
+     * https://github.com/ethereum/go-ethereum/blob/9ada4a2e2c415e6b0b51c50e901336872e028872/params/protocol_params.go#L33
+     * 16 gas for non zero byte
+     * https://github.com/ethereum/go-ethereum/blob/9ada4a2e2c415e6b0b51c50e901336872e028872/params/protocol_params.go#L87
+     * This will need to be updated if calldata gas prices change
+     * Account for the transaction being unsigned
+     * Padding is added to account for lack of signature on transaction
+     * 1 byte for RLP V prefix
+     * 1 byte for V
+     * 1 byte for RLP R prefix
+     * 32 bytes for R
+     * 1 byte for RLP S prefix
+     * 32 bytes for S
+     * Total: 68 bytes of padding
+     * @param _data Unsigned RLP encoded tx, 6 elements
+     * @return Amount of L1 gas used for a transaction
+     */
+    function getL1GasUsed(bytes memory _data)
+        public
+        view
+        override
+        returns (
+            uint256
+        )
+    {
+        uint256 total = 0;
+        for (uint256 i = 0; i < _data.length; i++) {
+            if (_data[i] == 0) {
+                total += 4;
+            } else {
+                total += 16;
+            }
+        }
+        uint256 unsigned = SafeMath.add(total, overhead);
+        return SafeMath.add(unsigned, 68 * 16);
     }
 }
