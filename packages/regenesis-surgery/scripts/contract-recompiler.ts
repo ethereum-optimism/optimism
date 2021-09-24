8b3a774ebc3437e8b603ffc7c8aaae76501793b2
Annie Ke <annieke8@gmail.com>
2021-09-24 09:23:33 -0700

    wip continued: use local solc and collect errors from etherscan source

--- packages/regenesis-surgery/scripts/contract-recompiler.ts
+++ packages/regenesis-surgery/scripts/contract-recompiler.ts
@@ -1,8 +1,9 @@
-import { providers, ethers, Contract, BigNumber, utils } from 'ethers'
 import solc from 'solc'
+import linker from 'solc/linker'
 import { createReadStream } from 'fs'
 import { parseChunked } from '@discoveryjs/json-ext'
 import dotenv from 'dotenv'
+import path from 'path'
 
 dotenv.config()
 
@@ -17,23 +18,12 @@ interface EtherscanContract {
   sourceCode: string
   creationCode: string
   contractFileName: string
+  contractName: string
   compilerVersion: string
   optimizationUsed: string
   runs: string
   constructorArguments: string
   library: string
-}
-
-const loadRemoteSolc = async (version: string) => {
-  return new Promise((resolve, reject) => {
-    solc.loadRemoteVersion(version, (err, snapshot) => {
-      if (err) {
-        console.error('error!', version, err)
-      } else {
-        resolve(snapshot)
-      }
-    })
-  })
 }
 
 ;(async () => {
@@ -42,8 +32,9 @@ const loadRemoteSolc = async (version: string) => {
   )
 
   // Corresponds to vanilla solidity
-  const compilerVersions = {
+  const compilerVersionsToSolc = {
     'v0.5.16': 'v0.5.16+commit.9c3226ce',
+    'v0.5.16-alpha.7': 'v0.5.16+commit.9c3226ce',
     'v0.6.12': 'v0.6.12+commit.27d51765',
     'v0.7.6': 'v0.7.6+commit.7338295f',
     'v0.7.6+commit.3b061308': 'v0.7.6+commit.7338295f', // what vanilla solidity should this be?
@@ -51,10 +42,15 @@ const loadRemoteSolc = async (version: string) => {
     'v0.8.4': 'v0.8.4+commit.c7e474f2',
   }
 
+  const noContractsCompiled = {}
+  const noContractName = []
+  const contractFileNameMismatch = {}
+  const hasImmutables = {}
+  const libraries = []
   for (const contract of etherscanContracts) {
     if (contract.sourceCode) {
       console.log(contract)
-      const input = {
+      let input = {
         language: 'Solidity',
         sources: {
           // this is a .sol filename in the example
@@ -65,7 +61,8 @@ const loadRemoteSolc = async (version: string) => {
         settings: {
           outputSelection: {
             '*': {
-              '*': ['evm.bytecode', 'evm.deployedBytecode', 'abi'],
+              // '*': ['evm.bytecode', 'evm.deployedBytecode', 'abi'],
+              '*': ['*'],
             },
           },
           optimizer: {
@@ -74,29 +71,103 @@ const loadRemoteSolc = async (version: string) => {
           },
         },
       }
+      let sourceCode = contract.sourceCode
+      // This is a whole input and Etherscan wraps it around a bracket
+      if (sourceCode.substr(0, 2) === '{{') {
+        // Trim the first and last bracket
+        sourceCode = sourceCode.slice(1, -1)
+      }
+      try {
+        const contractJson = JSON.parse(sourceCode)
+        console.log('got json', contractJson)
+        if (contractJson.language) {
+          console.log('seems like multifile input', contractJson)
+          input = contractJson
+        } else {
+          console.error('seems like just the source', contract.sourceCode)
+          input.sources = contractJson
+        }
+      } catch (e) {
+        console.error('got error trying json', e)
+      }
 
-      const version = compilerVersions[contract.compilerVersion]
+      const version = compilerVersionsToSolc[contract.compilerVersion]
       console.log('version', version)
       /* eslint @typescript-eslint/no-var-requires: "off" */
       const currSolc = solc.setupMethods(
         require(`../solc-bin/soljson-${version}.js`)
       )
-      // Fetching takes a long time
-      // const solcSnapshot: any = await loadRemoteSolc(version)
       const output = JSON.parse(currSolc.compile(JSON.stringify(input)))
       console.log('output!', contract.contractAddress, output)
+      if (!output.contracts) {
+        // There was an error compiling this contract
+        noContractsCompiled[contract.contractAddress] = output
+        continue
+      }
 
-      // How the example called remote
-      // solc.loadRemoteVersion(version, (err, solcSnapshot) => {
-      //   if (err) {
-      //     console.error('error!', version, err) // some have undefined versions!
-      //   } else {
-      //     const output = JSON.parse(solcSnapshot.compile(JSON.stringify(input)))
-      //     console.log('output!', contract.contractAddress, output)
-      //   }
-      // })
+      // Log those without file names
+      if (!contract.contractName) {
+        noContractName.push(contract.contractAddress)
+        continue
+      }
+      // const mainFile = path.parse(contract.contractFileName).name
+      console.log('contractName', contract.contractName)
+      // Contract name does not correspond with what's compiled from Etherscan sourcecode
+      let mainOutput
+      // there's a name for this multi-file address
+      if (contract.contractFileName) {
+        mainOutput =
+          output.contracts[contract.contractFileName][contract.contractName]
+      } else {
+        mainOutput = output.contracts.file[contract.contractName]
+      }
+      if (!mainOutput) {
+        contractFileNameMismatch[contract.contractAddress] = contract
+        continue
+      }
+
+      const immutableRefs = mainOutput.evm.deployedBytecode.immutableReferences
+      if (immutableRefs && Object.keys(immutableRefs).length !== 0) {
+        console.warn('this contract has immutables!', contract.contractAddress)
+        hasImmutables[contract.contractAddress] =
+          mainOutput.evm.deployedBytecode.immutableReferences
+      }
+      // Link libraries
+      if (contract.library) {
+        const deployedBytecode = mainOutput.evm.deployedBytecode.object
+        console.log('library!', contract.library)
+        libraries.push(contract.library)
+        const LibToAddress = contract.library.split(':')
+        // TEST: just to see output
+        console.log(
+          'link references!',
+          linker.findLinkReferences(deployedBytecode)
+        )
+        // TODO: empty object should be all the LibToAddressPairs
+        // const finalDeployedBytecode = linker.linkBytecode(deployedBytecode, {})
+        // use this finalDeployedBytecode to replace in state dump
+      }
     }
   }
+  console.log('had compiler errors', noContractsCompiled)
+  for (const [address, output] of Object.entries(noContractsCompiled)) {
+    console.log('error at address', address)
+    console.log(output)
+  }
+  console.log('filename missing from etherscan file', noContractName)
+  console.log(
+    'filename not found in compiled contracts',
+    contractFileNameMismatch
+  )
+  // TODO: handle immutables lmao
+  console.log('has immutables', hasImmutables)
+  console.log('all libraries from etherscan file', libraries)
+
+  // TODO: Uniswap: use their published contracts or libraries and just
+  // replace with bytecode from there
+  // Some contracts will just need to be wiped (those split specifically for OVM)
+  // See https://github.com/ethereum-optimism/optimism/pull/1481/files#diff-de41f93baec1842678463433ac56cf5ca6f669d64046729dfbf03dc6b3f03dfeR310-R312
+  // for accessing uniswap compiler output
 })().catch((err) => {
   console.log(err)
   process.exit(1)
