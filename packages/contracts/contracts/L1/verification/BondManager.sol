fd2a834462d47c9139724eff5de5edf8cac9979d
Kelvin Fichter <kelvin@optimism.io>
2021-09-28 23:19:30 -0400

    refactor: remove unused bond manager functions

--- packages/contracts/contracts/L1/verification/BondManager.sol
+++ packages/contracts/contracts/L1/verification/BondManager.sol
@@ -16,47 +16,21 @@ import { Lib_AddressResolver } from "../../libraries/resolver/Lib_AddressResolve
  */
 contract BondManager is IBondManager, Lib_AddressResolver {
 
+    /**
+     * @param _libAddressManager Address of the Address Manager.
+     */
     constructor(
         address _libAddressManager
     )
         Lib_AddressResolver(_libAddressManager)
     {}
 
-    function recordGasSpent(
-        bytes32 _preStateRoot,
-        bytes32 _txHash,
-        address _who,
-        uint256 _gasSpent
-    )
-        public
-    {}
-
-    function finalize(
-        bytes32 _preStateRoot,
-        address _publisher,
-        uint256 _timestamp
-    )
-        public
-    {}
-
-    function deposit()
-        public
-    {}
-
-    function startWithdrawal()
-        public
-    {}
-
-    function finalizeWithdrawal()
-        public
-    {}
-
-    function claim(
-        address _who
-    )
-        public
-    {}
-
+    /**
+     * Checks whether a given address is properly collateralized and can perform actions within
+     * the system.
+     * @param _who Address to check.
+     * @return true if the address is properly collateralized, false otherwise.
+     */
     function isCollateralized(
         address _who
     )
@@ -68,18 +42,5 @@ contract BondManager is IBondManager, Lib_AddressResolver {
     {
         // Only authenticate sequencer to submit state root batches.
         return _who == resolve("OVM_Proposer");
-    }
-
-    function getGasSpent(
-        bytes32 _preStateRoot,
-        address _who
-    )
-        public
-        pure
-        returns (
-            uint256
-        )
-    {
-        return 0;
     }
 }
