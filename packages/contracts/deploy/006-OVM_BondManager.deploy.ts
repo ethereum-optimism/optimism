4c23575687cf2aed1c8f7764316c812a9baf157b
Kelvin Fichter <kelvin@optimism.io>
2021-10-03 18:02:06 -0400

    feat: cleanup deployment process

--- packages/contracts/deploy/006-OVM_BondManager.deploy.ts
+++ packages/contracts/deploy/006-OVM_BondManager.deploy.ts
@@ -2,31 +2,22 @@
 import { DeployFunction } from 'hardhat-deploy/dist/types'
 
 /* Imports: Internal */
-import { getDeployedContract } from '../src/hardhat-deploy-ethers'
+import {
+  deployAndRegister,
+  getDeployedContract,
+} from '../src/hardhat-deploy-ethers'
 
 const deployFn: DeployFunction = async (hre) => {
-  const { deploy } = hre.deployments
-  const { deployer } = await hre.getNamedAccounts()
-
   const Lib_AddressManager = await getDeployedContract(
     hre,
-    'Lib_AddressManager',
-    {
-      signerOrProvider: deployer,
-    }
+    'Lib_AddressManager'
   )
 
-  const result = await deploy('BondManager', {
-    from: deployer,
+  await deployAndRegister({
+    hre,
+    name: 'BondManager',
     args: [Lib_AddressManager.address],
-    log: true,
   })
-
-  if (!result.newlyDeployed) {
-    return
-  }
-
-  await Lib_AddressManager.setAddress('BondManager', result.address)
 }
 
 deployFn.dependencies = ['Lib_AddressManager']
