6cda2cd4bf38f8e916e1ca60ff5003cd443a8b4d
Matthew Slipper <me@matthewslipper.com>
2021-10-09 22:48:13 -0600

    Introduce Docker layer caching

--- ops/scripts/build-ci.sh
+++ ops/scripts/build-ci.sh
@@ -1,15 +1,35 @@
-# build in 2 steps
-function build_images() {
-    docker-compose build --parallel -- builder l2geth l1_chain
-    docker-compose build --parallel -- deployer dtl batch_submitter relayer integration_tests
+# Builds an image using Buildx. Usage:
+# build <name> <tag> <dockerfile> <context>
+function build() {
+    echo "Building $1."
+    echo "Tag: $2"
+    echo "Dockerfile: $3"
+    echo "Context: $4"
+    docker buildx build --tag "$2" --build-arg LOCAL_REGISTRY=localhost:5000 --cache-from "type=local,src=/tmp/.buildx-cache/$1" --cache-to="type=local,dest=/tmp/.buildx-cache-new/$1" --file "$3" --load "$4" &
 }
 
-function build_dependencies() {
-    yarn
-    yarn build
-}
+mkdir -p /tmp/.buildx-cache-new
+docker buildx build --tag "localhost:5000/ethereumoptimism/builder:latest" --cache-from "type=local,src=/tmp/.buildx-cache/builder" --cache-to="type=local,mode=max,dest=/tmp/.buildx-cache-new/builder" --file "./ops/docker/Dockerfile.monorepo" --push . &
+build l2geth "ethereumoptimism/l2geth:latest" "./ops/docker/Dockerfile.geth" .
+build l1chain "ethereumoptimism/hardhat:latest" "./ops/docker/hardhat/Dockerfile" ./ops/docker/hardhat
 
-build_images &
-build_dependencies &
+wait
+
+# BuildX builds everything in a container when docker-container is selected as
+# the backend. Unfortunately, this means that the built image must be pushed
+# then re-pulled in order to make the container accessible to the Docker daemon.
+# We have to use the docker-container backend since the the docker backend does
+# not support cache-from and cache-to.
+docker pull localhost:5000/ethereumoptimism/builder:latest
+
+# Re-tag the local registry version of the builder so that docker-compose and
+# friends can see it.
+docker tag localhost:5000/ethereumoptimism/builder:latest ethereumoptimism/builder:latest
+
+build deployer "ethereumoptimism/deployer:latest" "./ops/docker/Dockerfile.deployer" .
+build dtl "ethereumoptimism/data-transport-layer:latest" "./ops/docker/Dockerfile.data-transport-layer" .
+build batch_submitter "ethereumoptimism/batch-submitter:latest" "./ops/docker/Dockerfile.batch-submitter" .
+build relayer "ethereumoptimism/message-relayer:latest" "./ops/docker/Dockerfile.message-relayer" .
+build integration-tests "ethereumoptimism/integration-tests:latest" "./ops/docker/Dockerfile.integration-tests" .
 
 wait
