71c33cebe7cc2bef3679cf0924a7b25e482d77f9
George Hotz <geohot@gmail.com>
2021-10-12 11:38:27 -0500

    comments to explain trie changes

--- minigeth/trie/stacktrie.go
+++ minigeth/trie/stacktrie.go
@@ -28,8 +28,6 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethdb"
 	"github.com/ethereum/go-ethereum/log"
-
-	//"github.com/ethereum/go-ethereum/log"
 	"github.com/ethereum/go-ethereum/rlp"
 )
 
