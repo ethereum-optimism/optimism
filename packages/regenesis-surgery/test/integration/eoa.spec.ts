662c0a9b5cc9203275f66a54bc48558799b768fa
Kelvin Fichter <kelvinfichter@gmail.com>
2021-10-12 17:20:07 -0400

    wip: continue filling out shell

--- packages/regenesis-surgery/test/integration/eoa.spec.ts
+++ packages/regenesis-surgery/test/integration/eoa.spec.ts
@@ -1,13 +1,27 @@
 import { expect } from '../setup'
 
 describe('EOAs', () => {
-  it('should not have any code', async () => {})
+  describe('standard EOA', () => {
+    it('should not have any code', async () => {})
 
-  it('should have the null code hash', async () => {})
+    it('should have the null code hash', async () => {})
 
-  it('should have the null storage root', async () => {})
+    it('should have the null storage root', async () => {})
 
-  it('should have the same balance as it had before', async () => {})
+    it('should have the same balance as it had before', async () => {})
 
-  it('should have the same nonce as it had before', async () => {})
+    it('should have the same nonce as it had before', async () => {})
+  })
+
+  describe('1inch deployer', () => {
+    it('should not have any code', async () => {})
+
+    it('should have the null code hash', async () => {})
+
+    it('should have the null storage root', async () => {})
+
+    it('should have the same balance as it had before', async () => {})
+
+    it('should have a nonce equal to zero', async () => {})
+  })
 })
---
+++ packages/regenesis-surgery/test/integration/uniswap.spec.ts
@@ -0,0 +1,27 @@
+describe('uniswap contracts', () => {
+  describe('V3 factory', () => {
+    it('should have the same owner', async () => {})
+
+    it('should have the same feeAmountTickSpacing map values', async () => {})
+
+    it('should have the right pool addresses', async () => {})
+  })
+
+  describe('V3 NFPM', () => {
+    // TODO
+  })
+
+  describe('V3 pools', () => {
+    it('should have the same code as on testnet', async () => {})
+
+    it('should have the same immutable values', async () => {})
+
+    it('should have the same token balances as before', async () => {})
+
+    // TODO: add a test for minting positions?
+  })
+
+  describe('other', () => {
+    it('should have the same code as on mainnet', async () => {})
+  })
+})
