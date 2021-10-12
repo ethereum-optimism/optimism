b476b1371c7654f06687d5ab98432b628ab05f13
George Hotz <geohot@gmail.com>
2021-10-12 11:09:28 -0500

    copy files for minigeth

---
+++ sync_minigeth.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+FILES=$(cat files_minigeth)
+MINIGETH=$PWD/minigeth
+git clone https://github.com/ethereum/go-ethereum.git
+cd go-ethereum
+git checkout 26675454bf93bf904be7a43cce6b3f550115ff90
+rsync -Rv $FILES $MINIGETH
