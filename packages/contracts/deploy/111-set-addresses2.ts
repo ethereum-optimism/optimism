47b95979b08c8805ee9f58fc104396a021cc4f16
Maurelian <maurelian@protonmail.ch>
2021-10-21 20:27:53 -0400

    refactor(contracts): Improve deploy script numbering scheme
    Discrete deploy scripts are prefixed with multiples of 10, allowing for related scripts
    to be place between them without renaming all subsquent scripts

--- packages/contracts/deploy/111-set-addresses2.ts
+++ packages/contracts/deploy/111-set-addresses2.ts
@@ -12,28 +12,10 @@ const deployFn: DeployFunction = async (hre) => {
   // L2CrossDomainMessenger is the address of the predeploy on L2. We can refactor off-chain
   // services such that we can remove the need to set this address, but for now it's easier
   // to simply keep setting the address.
-  await registerAddress({
-    hre,
-    name: 'L2CrossDomainMessenger',
-    address: predeploys.L2CrossDomainMessenger,
-  })
-
   // OVM_Sequencer is the address allowed to submit "Sequencer" blocks to the
   // CanonicalTransactionChain.
-  await registerAddress({
-    hre,
-    name: 'OVM_Sequencer',
-    address: (hre as any).deployConfig.ovmSequencerAddress,
-  })
-
   // OVM_Proposer is the address allowed to submit state roots (transaction results) to the
   // StateCommitmentChain.
-  await registerAddress({
-    hre,
-    name: 'OVM_Proposer',
-    address: (hre as any).deployConfig.ovmProposerAddress,
-  })
-
   const names = [
     'ChainStorageContainer-CTC-batches',
     'ChainStorageContainer-SCC-batches',
