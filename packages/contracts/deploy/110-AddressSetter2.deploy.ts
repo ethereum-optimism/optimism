3c7d3ace31eb633b687690fcaf95bccf678a2184
Maurelian <maurelian@protonmail.ch>
2021-10-21 20:27:53 -0400

    refactor(contracts): Improve deploy script numbering scheme
    Discrete deploy scripts are prefixed with multiples of 10, allowing for related scripts
    to be place between them without renaming all subsquent scripts

--- packages/contracts/deploy/110-AddressSetter2.deploy.ts
+++ packages/contracts/deploy/110-AddressSetter2.deploy.ts
@@ -15,32 +15,13 @@ const deployFn: DeployFunction = async (hre) => {
     'Lib_AddressManager'
   )
 
-  // ToDo: Clean up the method of mapping names to addresses esp.
-  // There's probably a more functional way to generate an object or something.
-  const names = [
-    'ChainStorageContainer-CTC-batches',
-    'ChainStorageContainer-SCC-batches',
-    'CanonicalTransactionChain',
-    'StateCommitmentChain',
-    'BondManager',
-    'OVM_L1CrossDomainMessenger',
-    'Proxy__L1CrossDomainMessenger',
-    'Proxy__L1StandardBridge',
-  ]
+  const names = ['Proxy__L1CrossDomainMessenger', 'Proxy__L1StandardBridge']
 
   const addresses = await Promise.all(
     names.map(async (n) => {
       return (await getDeployedContract(hre, n)).address
     })
   )
-
-  // Add non-deployed addresses to the arrays
-  names.push('L2CrossDomainMessenger')
-  addresses.push(predeploys.L2CrossDomainMessenger)
-  names.push('OVM_Sequencer')
-  addresses.push((hre as any).deployConfig.ovmSequencerAddress)
-  names.push('OVM_Proposer')
-  addresses.push((hre as any).deployConfig.ovmProposerAddress)
 
   await deployAndPostDeploy({
     hre,
