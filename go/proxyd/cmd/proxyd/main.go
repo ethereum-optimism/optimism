3ec03a23b2ce4306bdd6e6d5c7af59d622cac4d1
Matthew Slipper <me@matthewslipper.com>
2021-10-27 00:42:38 -0600

    Add proxy RPC daemon

---
+++ go/proxyd/cmd/proxyd/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"github.com/BurntSushi/toml"
+	"github.com/ethereum-optimism/optimism/go/proxyd"
+	"github.com/ethereum/go-ethereum/log"
+	"os"
+)
+
+var (
+	GitVersion = ""
+	GitCommit  = ""
+	GitDate    = ""
+)
+
+func main() {
+	// Set up logger with a default INFO level in case we fail to parse flags.
+	// Otherwise the final critical log won't show what the parsing error was.
+	log.Root().SetHandler(
+		log.LvlFilterHandler(
+			log.LvlInfo,
+			log.StreamHandler(os.Stdout, log.TerminalFormat(true)),
+		),
+	)
+
+	log.Info("starting proxyd", "version", GitVersion, "commit", GitCommit, "date", GitDate)
+
+	if len(os.Args) < 2 {
+		log.Crit("must specify a config file on the command line")
+	}
+
+	config := new(proxyd.Config)
+	if _, err := toml.DecodeFile(os.Args[1], config); err != nil {
+		log.Crit("error reading config file", "err", err)
+	}
+
+	if err := proxyd.Start(config); err != nil {
+		log.Crit("error starting proxyd", "err", err)
+	}
+}
