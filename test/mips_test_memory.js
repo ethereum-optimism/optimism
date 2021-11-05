0978b25170a16f27e827eb8235f234776b5d7a43
George Hotz <geohot@gmail.com>
2021-11-05 10:42:24 -0700

    found bug from fuzzer

--- test/mips_test_memory.js
+++ test/mips_test_memory.js
@@ -8,6 +8,7 @@ async function write(mm, root, addr, data) {
       root = l.data
     }
   }
+  console.log("new hash", root)
   return root
 }
 
@@ -19,15 +20,13 @@ describe("MIPSMemory contract", function () {
   beforeEach(async function () {
     const MIPSMemory = await ethers.getContractFactory("MIPSMemory")
     mm = await MIPSMemory.deploy()
+    await mm.AddTrieNode(new Uint8Array([0x80]))
   })
   it("write from new should work", async function() {
-    await mm.AddTrieNode(new Uint8Array([0x80]))
     let root = "0x56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421"
 
     root = await write(mm, root, 0, 1)
-    console.log("new hash", root)
     root = await write(mm, root, 4, 2)
-    console.log("new hash", root)
 
     expect(await mm.ReadMemory(root, 0)).to.equal(1)
     expect(await mm.ReadMemory(root, 4)).to.equal(2)
@@ -37,60 +36,59 @@ describe("MIPSMemory contract", function () {
     let root = "0x56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421"
 
     root = await write(mm, root, 0, 1)
-    console.log("new hash", root)
     root = await write(mm, root, 4, 2)
-    console.log("new hash", root)
     root = await write(mm, root, 0x40, 3)
-    console.log("new hash", root)
 
     expect(await mm.ReadMemory(root, 0)).to.equal(1)
     expect(await mm.ReadMemory(root, 4)).to.equal(2)
     expect(await mm.ReadMemory(root, 0x40)).to.equal(3)
   })
   it("write other three should work", async function() {
-    await mm.AddTrieNode(new Uint8Array([0x80]))
     let root = "0x56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421"
 
     root = await write(mm, root, 0x7fffd00c, 1)
-    console.log("new hash", root)
     root = await write(mm, root, 0x7fffd010, 2)
-    console.log("new hash", root)
     root = await write(mm, root, 0x7fffcffc, 3)
-    console.log("new hash", root)
 
     expect(await mm.ReadMemory(root, 0x7fffd00c)).to.equal(1)
     expect(await mm.ReadMemory(root, 0x7fffd010)).to.equal(2)
     expect(await mm.ReadMemory(root, 0x7fffcffc)).to.equal(3)
   })
-  it("fuzzing should be okay", async function() {
-    await mm.AddTrieNode(new Uint8Array([0x80]))
+  it("bug found fuzzing 1", async function() {
     let root = "0x56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421"
-    let keys = []
-    let values = []
+    root = await write(mm, root, 208, 3560962548)
+    root = await write(mm, root, 208, 1247044781)
+    root = await write(mm, root, 208, 688075632)
+  })
+  /*it("fuzzing should be okay", async function() {
+    let root = "0x56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421"
+    let kv = {}
 
     for (var i = 0; i < 100; i++) {
+      const keys = Object.keys(kv)
       const choice = Math.random()
       if (choice < 0.5 || keys.length == 0) {
         // write new key
-        const key = randint(0x4000)*4
+        const key = randint(0x100)*4
         const value = randint(0x100000000)
+        console.log("writing", key, value)
         root = await write(mm, root, key, value)
-        keys.push(key)
-        values.push(value)
+        kv[key] = value
       } else if (choice > 0.7) {
         // read old key
         const idx = randint(keys.length)
         const key = keys[idx]
-        const value = values[idx]
-        expect(await mm.ReadMemory(root, key)).to.equal(value)
+        console.log("reading", key)
+        expect(await mm.ReadMemory(root, key)).to.equal(kv[key])
       } else {
         // rewrite old key
         const idx = randint(keys.length)
         const key = keys[idx]
         const value = randint(0x100000000)
+        console.log("writing", key, value)
         root = await write(mm, root, key, value)
-        values[idx] = value
+        kv[key] = value
       }
     }
-  })
+  })*/
 })
