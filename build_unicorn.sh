4dd42bebfe19672ee621516fa6aa09fc2eb68992
George Hotz <geohot@gmail.com>
2021-11-07 09:49:38 -0800

    add unicorn crash test

--- build_unicorn.sh
+++ build_unicorn.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 git clone https://github.com/geohot/unicorn.git -b dev unicorn2
+#git clone https://github.com/unicorn-engine/unicorn.git -b dev unicorn2
 cd unicorn2
 #cmake . -DUNICORN_ARCH=mips -DCMAKE_BUILD_TYPE=Debug
 cmake . -DUNICORN_ARCH=mips -DCMAKE_BUILD_TYPE=Release
---
+++ mipsevm/unicorn_crash_test.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"encoding/binary"
+	"fmt"
+	"io/ioutil"
+	"testing"
+)
+
+func TestUnicornCrash(t *testing.T) {
+	fn := "../mipigo/test/test.bin"
+	ram := make(map[uint32](uint32))
+
+	root := "/tmp/cannon/0_13284469"
+	mu := GetHookedUnicorn(root, ram, nil)
+
+	// program
+	dat, _ := ioutil.ReadFile(fn)
+	mu.MemWrite(0, dat)
+
+	// inputs
+	inputs, _ := ioutil.ReadFile(fmt.Sprintf("%s/input", root))
+	mu.MemWrite(0x30000000, inputs[0:0xc0])
+
+	// load into ram
+	for i := 0; i < len(dat); i += 4 {
+		value := binary.BigEndian.Uint32(dat[i : i+4])
+		if value != 0 {
+			ram[uint32(i)] = value
+		}
+	}
+
+	mu.Start(0, 0x5ead0004)
+}
