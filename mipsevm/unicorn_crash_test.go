c5483b02d7a20d7b7d33eac16b0deff84a400d01
George Hotz <geohot@gmail.com>
2021-11-07 09:56:26 -0800

    simpler repro

--- mipsevm/unicorn_crash_test.go
+++ mipsevm/unicorn_crash_test.go
@@ -10,22 +10,22 @@ import (
 )
 
 func TestUnicornCrash(t *testing.T) {
-	fn := "../mipigo/test/test.bin"
-	ram := make(map[uint32](uint32))
-
 	root := "/tmp/cannon/0_13284469"
 	mu, err := uc.NewUnicorn(uc.ARCH_MIPS, uc.MODE_32|uc.MODE_BIG_ENDIAN)
 	check(err)
 
 	// program
-	dat, _ := ioutil.ReadFile(fn)
+	fn := "../mipigo/test/test.bin"
+	dat, err := ioutil.ReadFile(fn)
+	check(err)
 	mu.MemWrite(0, dat)
 
 	// inputs
 	inputs, _ := ioutil.ReadFile(fmt.Sprintf("%s/input", root))
 	mu.MemWrite(0x30000000, inputs[0:0xc0])
 
-	// load into ram
+	// load into ram, without this (completely unrelated), it doesn't crash
+	ram := make(map[uint32](uint32))
 	for i := 0; i < len(dat); i += 4 {
 		value := binary.BigEndian.Uint32(dat[i : i+4])
 		if value != 0 {
