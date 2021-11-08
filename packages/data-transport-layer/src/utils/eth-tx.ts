128e0f84629d0302a9688cd427f3f02be4bd689c
Mark Tyneway <mark.tyneway@gmail.com>
2021-11-08 13:10:53 -0800

    dtl: support unprotected txs
    When syncing from L2, all transactions were assumed to be
    protected. To prevent a network split for replicas,
    the DTL needs to check for transactions that are not
    protected and handle parsing the `v` parameter
    appropriately.

--- packages/data-transport-layer/src/utils/eth-tx.ts
+++ packages/data-transport-layer/src/utils/eth-tx.ts
@@ -2,8 +2,18 @@
 import { ethers } from 'ethers'
 
 export const parseSignatureVParam = (
-  v: number | ethers.BigNumber,
+  v: number | ethers.BigNumber | string,
   chainId: number
 ): number => {
-  return ethers.BigNumber.from(v).toNumber() - 2 * chainId - 35
+  v = ethers.BigNumber.from(v).toNumber()
+  // Handle normalized v
+  if (v === 0 || v === 1) {
+    return v
+  }
+  // Handle unprotected transactions
+  if (v === 27 || v === 28) {
+    return v - 27
+  }
+  // Handle EIP155 transactions
+  return v - 2 * chainId - 35
 }
