3223ad2117ad02a7bbe8e4527ba6f95454d82b42
Mark Tyneway <mark.tyneway@gmail.com>
2021-11-09 11:03:38 -0800

    regenesis-surgery: initial predeploy spec
    Adds a skeleton for the initial predeploy tests

--- packages/regenesis-surgery/test/predeploy.spec.ts
+++ packages/regenesis-surgery/test/predeploy.spec.ts
@@ -1,34 +1,56 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
-describe.skip('predeploys', () => {
-  describe('new predeploys that are not ETH', () => {
-    it('should have the exact state specified in the base genesis file', async () => {})
+import { expect, env } from './setup'
+import { AccountType, Account } from '../scripts/types'
+
+describe('predeploys', () => {
+  let predeploys = {
+    eth: [],
+    newNotEth: [],
+    noWipe: [],
+    wipe: [],
+    weth: [],
+  }
+
+  before(async () => {
+    await env.init()
+    predeploys.eth = env.getAccountsByType(AccountType.PREDEPLOY_ETH)
+    predeploys.newNotEth = env.getAccountsByType(AccountType.PREDEPLOY_NEW_NOT_ETH)
+    predeploys.noWipe = env.getAccountsByType(AccountType.PREDEPLOY_NO_WIPE)
+    predeploys.wipe = env.getAccountsByType(AccountType.PREDEPLOY_WIPE)
+    predeploys.weth = env.getAccountsByType(AccountType.PREDEPLOY_WETH)
   })
 
-  describe('predeploys where the old state should be wiped', () => {
-    it('should have the code and storage of the base genesis file', async () => {})
+  it('predeploy tests', () => {
+    describe.skip('new predeploys that are not ETH', () => {
+      it('should have the exact state specified in the base genesis file', async () => {})
+    })
 
-    it('should have the same nonce and balance as before', async () => {})
-  })
+    describe.skip('predeploys where the old state should be wiped', () => {
+      it('should have the code and storage of the base genesis file', async () => {})
 
-  describe('predeploys where the old state should be preserved', () => {
-    it('should have the code of the base genesis file', async () => {})
+      it('should have the same nonce and balance as before', async () => {})
+    })
 
-    it('should have the combined storage of the old and new state', async () => {})
+    describe.skip('predeploys where the old state should be preserved', () => {
+      it('should have the code of the base genesis file', async () => {})
 
-    it('should have the same nonce and balance as before', async () => {})
-  })
+      it('should have the combined storage of the old and new state', async () => {})
 
-  describe('OVM_ETH', () => {
-    it('should have disabled ERC20 features', async () => {})
+      it('should have the same nonce and balance as before', async () => {})
+    })
 
-    it('should no recorded balance for the contracts that move to WETH9', async () => {})
+    describe.skip('OVM_ETH', () => {
+      it('should have disabled ERC20 features', async () => {})
 
-    it('should have a new balance for WETH9 equal to the sum of the moved contract balances', async () => {})
-  })
+      it('should no recorded balance for the contracts that move to WETH9', async () => {})
 
-  describe('WETH9', () => {
-    it('should have balances for each contract that should move', async () => {})
+      it('should have a new balance for WETH9 equal to the sum of the moved contract balances', async () => {})
+    })
 
-    it('should have a balance equal to the sum of all moved balances', async () => {})
+    describe.skip('WETH9', () => {
+      it('should have balances for each contract that should move', async () => {})
+
+      it('should have a balance equal to the sum of all moved balances', async () => {})
+    })
   })
 })
