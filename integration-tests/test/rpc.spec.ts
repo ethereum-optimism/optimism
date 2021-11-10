fe7070cef3ece2cb04094c95780e317350575dfc
Mark Tyneway <mark.tyneway@gmail.com>
2021-11-09 16:49:48 -0800

    integration-tests: enable rollup_gasPrices test
    Enable the RPC test for `rollup_gasPrices` that
    was previously disabled due to how the gas pricing
    scheme was changed in `v0.5.0`.

--- integration-tests/test/rpc.spec.ts
+++ integration-tests/test/rpc.spec.ts
@@ -424,11 +424,10 @@ describe('Basic RPC tests', () => {
     })
   })
 
-  // SKIP: ?
-  describe.skip('rollup_gasPrices', () => {
+  describe('rollup_gasPrices', () => {
     it('should return the L1 and L2 gas prices', async () => {
       const result = await provider.send('rollup_gasPrices', [])
-      const l1GasPrice = await env.l1Wallet.provider.getGasPrice()
+      const l1GasPrice = await env.gasPriceOracle.l1BaseFee()
       const l2GasPrice = await env.gasPriceOracle.gasPrice()
 
       expect(BigNumber.from(result.l1GasPrice)).to.deep.eq(l1GasPrice)
