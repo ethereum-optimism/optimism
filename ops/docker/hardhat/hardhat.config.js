3a85b1b3dd6e2e133a1e493e5bbde25c13707de6
Krzysztof Kaczor <krzkaczor@gmail.com>
2021-12-03 13:28:52 +0100

    Add support for running L1 hardhat node in a fork mode

--- ops/docker/hardhat/hardhat.config.js
+++ ops/docker/hardhat/hardhat.config.js
@@ -1,8 +1,29 @@
+const isForkModeEnabled = !!process.env.FORK_URL
+const forkUrl = process.env.FORK_URL
+const forkStartingBlock = process.env.FORK_STARTING_BLOCK || undefined
+
+if (isForkModeEnabled) {
+  console.log(`Running hardhat in a fork mode! URL: ${forkUrl}`)
+  if (forkStartingBlock) {
+    console.log(`Starting block: ${forkStartingBlock}`)
+  }
+} else {
+  console.log('Running with a fresh state...')
+}
+
 module.exports = {
   networks: {
     hardhat: {
       gasPrice: 0,
-      initialBaseFeePerGas: 0
+      initialBaseFeePerGas: 0,
+      ...(isForkModeEnabled
+        ? {
+            forking: {
+              url: forkUrl,
+              blockNumber: forkStartingBlock,
+            },
+          }
+        : {}),
     },
   },
   analytics: { enabled: false },
