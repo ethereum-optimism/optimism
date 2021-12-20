d6e0de5ac4c1c417d293cad759ce9abc1e1aa3c2
Matthew Slipper <me@matthewslipper.com>
2021-12-19 20:57:40 -0700

    go/batch-submitter: Fix metrics server
    The metrics server wasn't running because of a space between the host and port.

--- go/batch-submitter/batch_submitter.go
+++ go/batch-submitter/batch_submitter.go
@@ -293,7 +293,7 @@ func parseWalletPrivKeyAndContractAddr(
 // NOTE: This method MUST be run as a goroutine.
 func runMetricsServer(hostname string, port uint64) {
 	metricsPortStr := strconv.FormatUint(port, 10)
-	metricsAddr := fmt.Sprintf("%s: %s", hostname, metricsPortStr)
+	metricsAddr := fmt.Sprintf("%s:%s", hostname, metricsPortStr)
 
 	http.Handle("/metrics", promhttp.Handler())
 	_ = http.ListenAndServe(metricsAddr, nil)
