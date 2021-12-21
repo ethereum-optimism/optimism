45ff4b6caaf18c0c8dc1936d1765905dab43c1a9
Maurelian <maurelian@protonmail.ch>
2021-12-20 22:11:22 -0500

    Add L1Block values contract

---
+++ packages/contracts/contracts/L2/L1Block.sol
@@ -0,0 +1,31 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.10;
+
+/**
+ * @title L1Block
+ */
+contract L1Block {
+  address constant depositorAccount = 0xDeaDDEaDDeAdDeAdDEAdDEaddeAddEAdDEAd0001;
+
+  uint256 public number;
+  uint256 public timestamp;
+  uint256 public basefee;
+  bytes32 public hash;
+
+  function setL1BlockValues(
+    uint256 _number,
+    uint256 _timestamp,
+    uint256 _basefee,
+    bytes32 _hash
+  ) external {
+    require(
+      msg.sender == depositorAccount,
+      "Only callable by L1 Attributes Depositor Account"
+    );
+
+    number = _number;
+    timestamp = _timestamp;
+    basefee = _basefee;
+    hash = _hash;
+  }
+}
