b60e07d445ca22fbe3235b3a849ec6cfa2155a15
Conner Fromknecht <conner@alum.mit.edu>
2022-01-28 15:20:16 -0800

    feat: use require for test assertions

--- opnode/test/system_test.go
+++ opnode/test/system_test.go
@@ -10,6 +10,7 @@ import (
 	"github.com/ethereum-optimism/optimistic-specs/opnode/contracts/deposit"
 	"github.com/ethereum-optimism/optimistic-specs/opnode/internal/testlog"
 	rollupNode "github.com/ethereum-optimism/optimistic-specs/opnode/node"
+	"github.com/stretchr/testify/require"
 
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/accounts/keystore"
@@ -78,38 +79,29 @@ func TestSystemE2E(t *testing.T) {
 
 	// Start L1
 	l1Node, l1Backend, err := l1Geth(cfg)
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.Nil(t, err)
 	defer l1Node.Close()
+
 	err = l1Node.Start()
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.Nil(t, err)
+
 	err = l1Backend.StartMining(1)
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.Nil(t, err)
+
 	l1Client, err := ethclient.Dial(endpoint(cfg.l1.nodeConfig))
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.Nil(t, err)
 	l1GenesisHash := getGenesisHash(l1Client)
 
 	// Start L2
 	l2Node, _, err := l2Geth(cfg)
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.Nil(t, err)
 	defer l2Node.Close()
+
 	err = l2Node.Start()
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.Nil(t, err)
+
 	l2Client, err := ethclient.Dial(endpoint(cfg.l2.nodeConfig))
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.Nil(t, err)
 	l2GenesisHash := getGenesisHash(l2Client)
 
 	// Rollup Node
@@ -121,14 +113,11 @@ func TestSystemE2E(t *testing.T) {
 		L2EngineAddrs: []string{endpoint(cfg.l2.nodeConfig)},
 	}
 	node, err := rollupNode.New(context.Background(), nodeCfg, testlog.Logger(t, log.LvlTrace))
-	if err != nil {
-		t.Fatalf("Failed to create the new node: %v", err)
-	}
+	require.Nil(t, err)
+
 	err = node.Start(context.Background())
+	require.Nil(t, err)
 	defer node.Stop()
-	if err != nil {
-		t.Fatal(err)
-	}
 
 	// Send Transaction & wait for success
 	contractAddr := common.HexToAddress(cfg.depositContractAddress)
@@ -138,45 +127,33 @@ func TestSystemE2E(t *testing.T) {
 	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
 	defer cancel()
 	startBalance, err := l2Client.BalanceAt(ctx, fromAddr, nil)
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.Nil(t, err)
 
 	// Contract
 	depositContract, err := deposit.NewDeposit(contractAddr, l1Client)
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.Nil(t, err)
 
 	// Signer
 	ks := l1Node.AccountManager().Backends(keystore.KeyStoreType)[0].(*keystore.KeyStore)
 	opts, err := bind.NewKeyStoreTransactorWithChainID(ks, ks.Accounts()[0], big.NewInt(int64(cfg.l1.ethConfig.NetworkId)))
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.Nil(t, err)
 
 	// Setup for L1 Confirmation
 	watchChan := make(chan *deposit.DepositTransactionDeposited)
 	watcher, err := depositContract.WatchTransactionDeposited(&bind.WatchOpts{}, watchChan, []common.Address{fromAddr}, []common.Address{fromAddr})
-	if err != nil {
-		t.Fatalf("with watcher: %v", err)
-	}
+	require.Nil(t, err, "with watcher")
 	defer watcher.Unsubscribe()
 
 	// Setup for L2 Confirmation
 	headChan := make(chan *types.Header, 100)
 	l2HeadSub, err := l2Client.SubscribeNewHead(context.Background(), headChan)
-	if err != nil {
-		t.Fatalf("with l2 head sub: %v", err)
-	}
+	require.Nil(t, err, "with l2 head sub")
 	defer l2HeadSub.Unsubscribe()
 
 	// Finally send TX
 	mintAmount := big.NewInt(1_000_000_000_000)
 	tx, err := depositContract.DepositTransaction(opts, fromAddr, mintAmount, big.NewInt(1_000_000), false, nil)
-	if err != nil {
-		t.Fatalf("with deposit txt: %v", err)
-	}
+	require.Nil(t, err, "with deposit tx")
 
 	// Wait for tx to be mined on L1 (or timeout)
 	select {
@@ -193,9 +170,7 @@ func TestSystemE2E(t *testing.T) {
 	ctx, cancel = context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 	receipt, err := l1Client.TransactionReceipt(ctx, tx.Hash())
-	if err != nil {
-		t.Fatalf("Could not get tranaction receipt: %v", err)
-	}
+	require.Nil(t, err, "Could not get transaction receipt")
 
 	// Wait (or timeout) for that block to show up on L2
 	timeoutCh := time.After(3 * time.Second)
@@ -217,14 +192,9 @@ loop:
 	ctx, cancel = context.WithTimeout(context.Background(), 1*time.Second)
 	defer cancel()
 	endBalance, err := l2Client.BalanceAt(ctx, fromAddr, nil)
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.Nil(t, err)
 
 	diff := new(big.Int)
 	diff = diff.Sub(endBalance, startBalance)
-	if diff.Cmp(mintAmount) != 0 {
-		t.Fatalf("Did not get expected balance change. start: %v, end: %v, mint: %v", startBalance, endBalance, mintAmount)
-	}
-
+	require.Equal(t, diff, mintAmount, "Did not get expected balance change")
 }
