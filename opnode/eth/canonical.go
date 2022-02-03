4224e0fbb916a49eb472521482ee35e2f75a964b
Joshua Gutow <jgutow@optimism.io>
2022-02-03 15:03:46 -0800

    Remove function adapter for CanonicalChain

--- opnode/eth/canonical.go
+++ opnode/eth/canonical.go
@@ -12,26 +12,26 @@ type BlockLinkByNumber interface {
 	BlockLinkByNumber(ctx context.Context, num uint64) (self BlockID, parent BlockID, err error)
 }
 
-// BlockLinkByNumberFn implements BlockLinkByNumber to implement the interface as anonymous function
-type BlockLinkByNumberFn func(ctx context.Context, num uint64) (self BlockID, parent BlockID, err error)
+type blockLink struct {
+	l1Src HeaderByNumberSource
+}
 
-func (fn BlockLinkByNumberFn) BlockLinkByNumber(ctx context.Context, num uint64) (self BlockID, parent BlockID, err error) {
-	return fn(ctx, num)
+func (b *blockLink) BlockLinkByNumber(ctx context.Context, num uint64) (self BlockID, parent BlockID, err error) {
+	header, err := b.l1Src.HeaderByNumber(ctx, big.NewInt(int64(num)))
+	if err != nil {
+		// w%: wrap the error, we still need to detect if a canonical block is not found, a.k.a. end of chain.
+		return BlockID{}, BlockID{}, fmt.Errorf("failed to determine block-hash of height %d, could not get header: %w", num, err)
+	}
+	parentNum := num
+	if parentNum > 0 {
+		parentNum -= 1
+	}
+	return BlockID{Hash: header.Hash(), Number: num}, BlockID{Hash: header.ParentHash, Number: parentNum}, nil
+
 }
 
 // CanonicalChain presents the block-hashes by height by wrapping a header-source
 // (useful due to lack of a direct JSON RPC endpoint)
 func CanonicalChain(l1Src HeaderByNumberSource) BlockLinkByNumber {
-	return BlockLinkByNumberFn(func(ctx context.Context, num uint64) (BlockID, BlockID, error) {
-		header, err := l1Src.HeaderByNumber(ctx, big.NewInt(int64(num)))
-		if err != nil {
-			// w%: wrap the error, we still need to detect if a canonical block is not found, a.k.a. end of chain.
-			return BlockID{}, BlockID{}, fmt.Errorf("failed to determine block-hash of height %d, could not get header: %w", num, err)
-		}
-		parentNum := num
-		if parentNum > 0 {
-			parentNum -= 1
-		}
-		return BlockID{Hash: header.Hash(), Number: num}, BlockID{Hash: header.ParentHash, Number: parentNum}, nil
-	})
+	return &blockLink{l1Src: l1Src}
 }
