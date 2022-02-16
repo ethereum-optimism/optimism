f6399795bd069a2000cf8a6816678d8d95b58323
Conner Fromknecht <conner@alum.mit.edu>
2022-02-16 10:15:47 -0700

    feat: add NatSpec comments and tests to TeleportrDeposit

--- packages/contracts/contracts/L1/teleportr/TeleportrDeposit.sol
+++ packages/contracts/contracts/L1/teleportr/TeleportrDeposit.sol
@@ -10,18 +10,57 @@ import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
  * https://github.com/0xclem/teleportr/blob/main/contracts/BridgeDeposit.sol
  */
 contract TeleportrDeposit is Ownable {
+    /// The minimum amount that be deposited in a receive.
     uint256 public minDepositAmount;
+    /// The maximum amount that be deposited in a receive.
     uint256 public maxDepositAmount;
+    /// The maximum balance the contract can hold after a receive.
     uint256 public maxBalance;
+    /// The total number of successful deposits received.
     uint256 public totalDeposits;
 
-    // Events
+    /**
+     * @notice Emitted any time the minimum deposit amount is set.
+     * @param previousAmount The previous minimum deposit amount.
+     * @param newAmount The new minimum deposit amount.
+     */
     event MinDepositAmountSet(uint256 previousAmount, uint256 newAmount);
+
+    /**
+     * @notice Emitted any time the maximum deposit amount is set.
+     * @param previousAmount The previous maximum deposit amount.
+     * @param newAmount The new maximum deposit amount.
+     */
     event MaxDepositAmountSet(uint256 previousAmount, uint256 newAmount);
+
+    /**
+     * @notice Emitted any time the contract maximum balance is set.
+     * @param previousBalance The previous maximum contract balance.
+     * @param newBalance The new maximum contract balance.
+     */
     event MaxBalanceSet(uint256 previousBalance, uint256 newBalance);
+
+    /**
+     * @notice Emitted any time the balance is withdrawn by the owner.
+     * @param owner The current owner and recipient of the funds.
+     * @param balance The current contract balance paid to the owner.
+     */
     event BalanceWithdrawn(address indexed owner, uint256 balance);
+
+    /**
+     * @notice Emitted any time a successful deposit is received.
+     * @param depositId A unique sequencer number identifying the deposit.
+     * @param emitter The sending address of the payer.
+     * @param amount The amount deposited by the payer.
+     */
     event EtherReceived(uint256 indexed depositId, address indexed emitter, uint256 indexed amount);
 
+    /**
+     * @notice Initializes a new TeleportrDeposit contract.
+     * @param _minDepositAmount The initial minimum deposit amount.
+     * @param _maxDepositAmount The initial maximum deposit amount.
+     * @param _maxBalance The initial maximum contract balance.
+     */
     constructor(
         uint256 _minDepositAmount,
         uint256 _maxDepositAmount,
@@ -36,9 +75,13 @@ contract TeleportrDeposit is Ownable {
         emit MaxBalanceSet(0, _maxBalance);
     }
 
-    // Receive function which reverts if the amount is outside the range
-    // [minDepositAmount, maxDepositAmount], or the amount would put the
-    // contract over its maxBalance.
+    /**
+     * @notice Accepts deposits that will be disbursed to the sender's address on L2.
+     * The method reverts if the amount is less than the current
+     * minDepositAmount, the amount is greater than the current
+     * maxDepositAmount, or the amount causes the contract to exceed its maximum
+     * allowed balance.
+     */
     receive() external payable {
         require(msg.value >= minDepositAmount, "Deposit amount is too small");
         require(msg.value <= maxDepositAmount, "Deposit amount is too big");
@@ -50,7 +93,9 @@ contract TeleportrDeposit is Ownable {
         }
     }
 
-    // Send the contract's balance to the owner
+    /**
+     * @notice Sends the contract's current balance to the owner.
+     */
     function withdrawBalance() external onlyOwner {
         address _owner = owner();
         uint256 _balance = address(this).balance;
@@ -58,17 +103,28 @@ contract TeleportrDeposit is Ownable {
         payable(_owner).transfer(_balance);
     }
 
-    // Setters
+    /**
+     * @notice Sets the minimum amount that can be deposited in a receive.
+     * @param _minDepositAmount The new minimum deposit amount.
+     */
     function setMinAmount(uint256 _minDepositAmount) external onlyOwner {
         emit MinDepositAmountSet(minDepositAmount, _minDepositAmount);
         minDepositAmount = _minDepositAmount;
     }
 
+    /**
+     * @notice Sets the maximum amount that can be deposited in a receive.
+     * @param _maxDepositAmount The new maximum deposit amount.
+     */
     function setMaxAmount(uint256 _maxDepositAmount) external onlyOwner {
         emit MaxDepositAmountSet(maxDepositAmount, _maxDepositAmount);
         maxDepositAmount = _maxDepositAmount;
     }
 
+    /**
+     * @notice Sets the maximum balance the contract can hold after a receive.
+     * @param _maxBalance The new maximum contract balance.
+     */
     function setMaxBalance(uint256 _maxBalance) external onlyOwner {
         emit MaxBalanceSet(maxBalance, _maxBalance);
         maxBalance = _maxBalance;
