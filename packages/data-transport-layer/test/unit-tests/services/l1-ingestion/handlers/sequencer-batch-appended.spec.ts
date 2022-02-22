403fb8292b9b26c405bc796e226cdf655593fa54
Mark Tyneway <mark.tyneway@gmail.com>
2022-02-22 15:34:54 -0800

    dtl: update unit tests

--- packages/data-transport-layer/test/unit-tests/services/l1-ingestion/handlers/sequencer-batch-appended.spec.ts
+++ packages/data-transport-layer/test/unit-tests/services/l1-ingestion/handlers/sequencer-batch-appended.spec.ts
@@ -1,4 +1,42 @@
+import fs from 'fs'
+import path from 'path'
+
 import { BigNumber, ethers } from 'ethers'
+import { compressBatchWithZlib } from '../../../../../src/utils'
+
+const readMockData = () => {
+  const mockDataPath = path.join(__dirname, '..', '..', '..', 'examples')
+  const paths = fs.readdirSync(mockDataPath)
+  const files = []
+  for (const filename of paths) {
+    // Skip non .txt files
+    if (!filename.endsWith('.txt')) {
+      continue
+    }
+    const filePath = path.join(mockDataPath, filename)
+    const file = fs.readFileSync(filePath)
+    const obj = JSON.parse(file.toString())
+    // Reserialize the BigNumbers
+    obj.input.extraData.prevTotalElements = BigNumber.from(
+      obj.input.extraData.prevTotalElements
+    )
+    obj.input.extraData.batchIndex = BigNumber.from(
+      obj.input.extraData.batchIndex
+    )
+    if (obj.input.event.args.length !== 3) {
+      throw new Error(`ABI mismatch`)
+    }
+    obj.input.event.args = obj.input.event.args.map(BigNumber.from)
+    obj.input.event.args._startingQueueIndex = obj.input.event.args[0]
+    obj.input.event.args._numQueueElements = obj.input.event.args[1]
+    obj.input.event.args._totalElements = obj.input.event.args[2]
+    obj.input.extraData.batchSize = BigNumber.from(
+      obj.input.extraData.batchSize
+    )
+    files.push(obj)
+  }
+  return files
+}
 
 /* Imports: Internal */
 import { expect } from '../../../../setup'
@@ -6,6 +44,8 @@ import { handleEventsSequencerBatchAppended } from '../../../../../src/services/
 import { SequencerBatchAppendedExtraData } from '../../../../../src/types'
 
 describe('Event Handlers: CanonicalTransactionChain.SequencerBatchAppended', () => {
+  const mockData = readMockData()
+
   describe('handleEventsSequencerBatchAppended.parseEvent', () => {
     // This tests the behavior of parsing a real mainnet transaction,
     // so it will break if the encoding scheme changes.
@@ -49,6 +89,45 @@ describe('Event Handlers: CanonicalTransactionChain.SequencerBatchAppended', () 
         `Block ${input1[1].blockNumber} transaction data is invalid for decoding: ${input1[1].l1TransactionData} , ` +
           `converted buffer length is < 12.`
       )
+    })
+
+    describe('mainnet transactions', () => {
+      for (const mock of mockData) {
+        const { input, output } = mock
+        const { event, extraData, l2ChainId } = input
+        const hash = mock.input.extraData.l1TransactionHash
+
+        it(`uncompressed: ${hash}`, () => {
+          const res = handleEventsSequencerBatchAppended.parseEvent(
+            event,
+            extraData,
+            l2ChainId
+          )
+          // Check all of the transaction entries individually
+          for (const [i, got] of res.transactionEntries.entries()) {
+            const expected = output.transactionEntries[i]
+            expect(got).to.deep.eq(expected, `case ${i}`)
+          }
+          expect(res).to.deep.eq(output)
+        })
+
+        it(`compressed: ${hash}`, () => {
+          const compressed = compressBatchWithZlib(
+            input.extraData.l1TransactionData
+          )
+
+          const copy = { ...extraData }
+          copy.l1TransactionData = compressed
+
+          const res = handleEventsSequencerBatchAppended.parseEvent(
+            event,
+            copy,
+            l2ChainId
+          )
+
+          expect(res).to.deep.eq(output)
+        })
+      }
     })
   })
 })
