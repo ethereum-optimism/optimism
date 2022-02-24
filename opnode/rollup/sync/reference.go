33fa47cc55b342dae10a69c2085ff4b02e9b67a0
Joshua Gutow <jgutow@optimism.io>
2022-02-23 16:10:30 -0800

    ref impl: Add SyncReferenceV2 interface
    This is a nicer interface to work with the sync start algorithm.

--- opnode/rollup/sync/reference.go
+++ opnode/rollup/sync/reference.go
@@ -13,6 +13,11 @@ import (
 	"github.com/ethereum/go-ethereum/core/types"
 )
 
+// Sync Source Interface TODOs:
+// Relax block by hash???  would have to use by number interface though
+// Merge v1 and v2
+// Put node definitions in eth?
+
 // SyncSource implements SyncReference with a L2 block sources and L1 hash-by-number source
 type SyncSource struct {
 	L1 interface {
@@ -70,4 +75,60 @@ type SyncReference interface {
 
 	// RefByL2Hash fetches the L1 and L2 block IDs from the engine for the given L2 block hash.
 	RefByL2Hash(ctx context.Context, l2Hash common.Hash, genesis *rollup.Genesis) (refL1 eth.BlockID, refL2 eth.BlockID, parentL2 common.Hash, err error)
+}
+
+// SyncReferenceV2 wraps the return types of SyncReference to be easier to work with.
+type SyncReferenceV2 interface {
+	L1NodeByNumber(ctx context.Context, l1Num uint64) (L1Node, error)
+	L1HeadNode(ctx context.Context) (L1Node, error)
+	L2NodeByNumber(ctx context.Context, l2Num *big.Int, genesis *rollup.Genesis) (L2Node, error)
+	L2NodeByHash(ctx context.Context, l2Hash common.Hash, genesis *rollup.Genesis) (L2Node, error)
+}
+
+type SyncSourceV2 struct {
+	SyncReference
+}
+
+func (src SyncSourceV2) L1NodeByNumber(ctx context.Context, l1Num uint64) (L1Node, error) {
+	self, parent, err := src.RefByL1Num(ctx, l1Num)
+	if err != nil {
+		return L1Node{}, err
+	}
+	return L1Node{self: self, parent: parent}, nil
+
+}
+
+func (src SyncSourceV2) L1HeadNode(ctx context.Context) (L1Node, error) {
+	self, parent, err := src.L1HeadRef(ctx)
+	if err != nil {
+		return L1Node{}, err
+	}
+	return L1Node{self: self, parent: parent}, nil
+}
+
+func (src SyncSourceV2) L2NodeByNumber(ctx context.Context, l2Num *big.Int, genesis *rollup.Genesis) (L2Node, error) {
+	l1Ref, l2ref, l2ParentHash, err := src.RefByL2Num(ctx, l2Num, genesis)
+	if err != nil {
+		return L2Node{}, err
+	}
+	return L2Node{self: l2ref, l1parent: l1Ref, l2parent: eth.BlockID{Hash: l2ParentHash, Number: l2ref.Number - 1}}, nil
+}
+
+func (src SyncSourceV2) L2NodeByHash(ctx context.Context, l2Hash common.Hash, genesis *rollup.Genesis) (L2Node, error) {
+	l1Ref, l2ref, l2ParentHash, err := src.RefByL2Hash(ctx, l2Hash, genesis)
+	if err != nil {
+		return L2Node{}, err
+	}
+	return L2Node{self: l2ref, l1parent: l1Ref, l2parent: eth.BlockID{Hash: l2ParentHash, Number: l2ref.Number - 1}}, nil
+}
+
+type L2Node struct {
+	self     eth.BlockID
+	l2parent eth.BlockID
+	l1parent eth.BlockID
+}
+
+type L1Node struct {
+	self   eth.BlockID
+	parent eth.BlockID
 }
