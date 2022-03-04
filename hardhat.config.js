a22c36eb3937471a1287f8a4f764da67d3ac8266
Owleksiy <owleksiy@gmail.com>
2022-03-04 07:13:44 -0800

    challenge_fault: attempt to fix timeout
     * use IP instead of hostname for HH node -> takes DNS out of the picture
     * increase the timeout

--- hardhat.config.js
+++ hardhat.config.js
@@ -18,8 +18,9 @@ module.exports = {
   //defaultNetwork: "hosthat",
   networks: {
     hosthat: {
-      url: "http://localhost:8545/",
-      accounts: ["0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"]
+      url: "http://127.0.0.1:8545/",
+      accounts: ["0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"],
+      timeout: 600_000,
     },
     cheapeth: {
       url: "https://rpc.cheapeth.org/rpc",
