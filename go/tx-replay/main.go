ab1f8e4ef1be9259219e90f30d294b4ba562f126
inphi <mlaw2501@gmail.com>
2022-03-11 11:16:36 -0500

    do not replay existing tx

--- go/tx-replay/main.go
+++ go/tx-replay/main.go
@@ -7,6 +7,7 @@ import (
 
 	"cloud.google.com/go/pubsub"
 
+	ethereum "github.com/ethereum-optimism/optimism/l2geth"
 	"github.com/ethereum-optimism/optimism/l2geth/core/types"
 	"github.com/ethereum-optimism/optimism/l2geth/ethclient"
 	"github.com/ethereum-optimism/optimism/l2geth/rlp"
@@ -17,6 +18,11 @@ var (
 	topic        = flag.String("topic", "", "PUB/SUB topic name to subscribe to")
 	sequencerURL = flag.String("sequencer-url", "http://0.0.0.0:8545", "sequencer URL to replay txs")
 	subscription = flag.String("sub", "", "subscription ID")
+
+	SubscriptionReceiveSettings = pubsub.ReceiveSettings{
+		MaxOutstandingMessages: 100000,
+		MaxOutstandingBytes:    1e9,
+	}
 )
 
 func init() {
@@ -32,22 +38,48 @@ func main() {
 	defer client.Close()
 
 	sub := client.Subscription(*subscription)
+	sub.ReceiveSettings = SubscriptionReceiveSettings
 
 	eclient, err := ethclient.DialContext(ctx, *sequencerURL)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// sanity check sub configs
+	subConfig, err := sub.Config(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if !subConfig.EnableMessageOrdering {
+		log.Fatal("invalid sub config: message ordering is not enabled")
+	}
+
 	cb := func(ctx context.Context, msg *pubsub.Message) {
 		var tx types.Transaction
 		if err := rlp.DecodeBytes(msg.Data, &tx); err != nil {
+			msg.Nack()
 			log.Fatalf("invalid transaction in queue: %v", err)
 		}
+
+		txHash := tx.Hash()
+		rtx, _, err := eclient.TransactionByHash(ctx, txHash)
+		if err != nil && err != ethereum.NotFound {
+			log.Printf("ERROR: unable to retrieve transaction hash %s: %v", txHash.String(), err)
+			msg.Nack()
+			return // retry later
+		}
+		if rtx != nil || err == ethereum.NotFound {
+			log.Printf("Skipping transaction hash %s", txHash.String())
+			msg.Ack()
+			return
+		}
+
 		jason, _ := tx.MarshalJSON()
-		log.Printf("Replaying transaction: %s\n", string(jason))
+		log.Printf("Replaying transaction %s: %s\n", txHash.String(), string(jason))
 
 		if err := eclient.SendTransaction(ctx, &tx); err != nil {
-			log.Fatalf("Failed to resend transaction: %v", err)
+			msg.Nack()
+			log.Fatalf("Failed to replay transaction %s: %v", txHash.String(), err)
 		}
 		msg.Ack()
 	}
