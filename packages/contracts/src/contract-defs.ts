eb66747a7231f907545c7c41727cc919fb0167db
Olubusayo Amowe <amoweolubusayo@gmail.com>
2022-03-16 18:26:41 +0100

    fix: yarn lock merge conflict

--- packages/contracts/src/contract-defs.ts
+++ packages/contracts/src/contract-defs.ts
@@ -1,10 +1,10 @@
 import { ethers } from 'ethers'
-/** 
+/**
  * Gets the contract's artifact.
- * 
- * @param name Given contract's name. 
- * @returns Hardhat's artifact for the given contract. 
- */ 
+ *
+ * @param name Given contract's name.
+ * @returns Hardhat's artifact for the given contract.
+ */
 export const getContractDefinition = (name: string): any => {
   // We import this using `require` because hardhat tries to build this file when compiling
   // the contracts, but we need the contracts to be compiled before the contract-artifacts.ts
@@ -18,24 +18,24 @@ export const getContractDefinition = (name: string): any => {
   return artifact
 }
 
-/** 
+/**
  * Gets the contract's interface.
- * 
- * @param name Given contract's name. 
- * @returns Interface for the given contract. 
- */ 
+ *
+ * @param name Given contract's name.
+ * @returns Interface for the given contract.
+ */
 export const getContractInterface = (name: string): ethers.utils.Interface => {
   const definition = getContractDefinition(name)
   return new ethers.utils.Interface(definition.abi)
 }
 
-/** 
+/**
  * Gets the contract to deploy.
- * 
- * @param name Given contract's name. 
- * @param signer Signer of contract. 
+ *
+ * @param name Given contract's name.
+ * @param signer Signer of contract.
  * @returns Contract's Factory.
- */ 
+ */
 export const getContractFactory = (
   name: string,
   signer?: ethers.Signer
