44fb405c64a954e24a3d77997b9b10adb6190e51
Olubusayo Amowe <amoweolubusayo@gmail.com>
2022-03-16 17:19:57 +0100

    fix: global variable getContractDefinition

--- packages/contracts/src/contract-defs.ts
+++ packages/contracts/src/contract-defs.ts
@@ -5,11 +5,12 @@ import { ethers } from 'ethers'
  * @param name Given contract's name. 
  * @returns Hardhat's artifact for the given contract. 
  */ 
-export const getContractArtifact = (name: string): any => {
+export const getContractDefinition = (name: string): any => {
   // We import this using `require` because hardhat tries to build this file when compiling
   // the contracts, but we need the contracts to be compiled before the contract-artifacts.ts
   // file can be generated.
   // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const { getContractArtifact } = require('./contract-artifacts')
   const artifact = getContractArtifact(name)
   if (artifact === undefined) {
     throw new Error(`Unable to find artifact for contract: ${name}`)
@@ -24,7 +25,7 @@ export const getContractArtifact = (name: string): any => {
  * @returns Interface for the given contract. 
  */ 
 export const getContractInterface = (name: string): ethers.utils.Interface => {
-  const definition = getContractArtifact(name)
+  const definition = getContractDefinition(name)
   return new ethers.utils.Interface(definition.abi)
 }
 
@@ -39,7 +40,7 @@ export const getContractFactory = (
   name: string,
   signer?: ethers.Signer
 ): ethers.ContractFactory => {
-  const definition = getContractArtifact(name)
+  const definition = getContractDefinition(name)
   const contractInterface = getContractInterface(name)
   return new ethers.ContractFactory(
     contractInterface,
