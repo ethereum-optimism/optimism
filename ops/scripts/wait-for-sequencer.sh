2f8f9921b3c174757eae8b320d45ef30e2fdcb64
Matthew Slipper <me@matthewslipper.com>
2022-03-18 14:09:30 -0600

    wip

--- ops/scripts/wait-for-sequencer.sh
+++ ops/scripts/wait-for-sequencer.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 CONTAINER=l2geth
 
-RETRIES=30
+RETRIES=90
 i=0
 until docker-compose logs l2geth | grep -q "Starting Sequencer Loop";
 do
-    sleep 3
+    sleep 1
     if [ $i -eq $RETRIES ]; then
         echo 'Timed out waiting for sequencer'
         break
