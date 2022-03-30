1f02e0aa93697b594e7fca55b0ae3ad6df0e51b1
inphi <mlaw2501@gmail.com>
2022-03-30 01:19:28 -0400

    Migrate DepositFeed spec to forge test contract
    This is nice because:
    * it's consistent with the existing contract testing
    * smaller test spec - easier to read and maintain
    * no need for the DummyContract to simulate contract calls

---
+++ packages/contracts/contracts/test/DepositFeed.t.sol
@@ -0,0 +1,126 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.10;
+
+import { DSTest } from "../../lib/ds-test/src/test.sol";
+import { Vm } from "../../lib/forge-std/src/Vm.sol";
+import { DepositFeed } from "../L1/DepositFeed.sol";
+
+contract DepositFeedTest is DSTest {
+    Vm vm = Vm(HEVM_ADDRESS);
+    address immutable ZERO_ADDRESS = address(0);
+    address immutable NON_ZERO_ADDRESS = address(1);
+    uint256 immutable NON_ZERO_VALUE = 100;
+    uint256 immutable ZERO_VALUE = 0;
+    uint256 immutable NON_ZERO_GASLIMIT = 50000;
+    bytes NON_ZERO_DATA = "0x1111";
+
+    DepositFeed df;
+
+    event TransactionDeposited(
+        address indexed from,
+        address indexed to,
+        uint256 mint,
+        uint256 value,
+        uint256 gasLimit,
+        bool isCreation,
+        bytes data
+    );
+
+    function setUp() external {
+        df = new DepositFeed();
+    }
+
+    function addressAlias() internal view returns(address) {
+        return address(uint160(address(this)) + uint160(0x1111000000000000000000000000000000001111));
+    }
+
+    // Test: depositTransaction fails when contract creation has a non-zero destination address
+    function test_depositTransaction_ContractCreationReverts() external {
+        vm.expectRevert(abi.encodeWithSignature("NonZeroCreationTarget()"));
+        df.depositTransaction(NON_ZERO_ADDRESS, NON_ZERO_VALUE, NON_ZERO_GASLIMIT, true, '0x');
+    }
+
+    // Test: depositTransaction should emit the correct log when an EOA deposits a tx with 0 value
+    function test_depositTransaction_NoValueEOA() external {
+        // EOA emulation
+        vm.prank(address(this), address(this));
+        vm.expectEmit(true, true, false, true);
+        emit TransactionDeposited(address(this), NON_ZERO_ADDRESS, ZERO_VALUE, ZERO_VALUE, NON_ZERO_GASLIMIT, false, NON_ZERO_DATA);
+
+        df.depositTransaction(NON_ZERO_ADDRESS, ZERO_VALUE, NON_ZERO_GASLIMIT, false, NON_ZERO_DATA);
+    }
+
+   // Test: depositTransaction should emit the correct log when a contract deposits a tx with 0 value
+   function test_depositTransaction_NoValueContract() external {
+        vm.expectEmit(true, true, false, true);
+        emit TransactionDeposited(addressAlias(), NON_ZERO_ADDRESS, ZERO_VALUE, ZERO_VALUE, NON_ZERO_GASLIMIT, false, NON_ZERO_DATA);
+
+        df.depositTransaction(NON_ZERO_ADDRESS, ZERO_VALUE, NON_ZERO_GASLIMIT, false, NON_ZERO_DATA);
+    }
+
+    // Test: depositTransaction should emit the correct log when an EOA deposits a contract creation with 0 value
+    function test_depositTransaction_createWithZeroValueForEOA() external {
+        // EOA emulation
+        vm.prank(address(this), address(this));
+ 
+        vm.expectEmit(true, true, false, true);
+        emit TransactionDeposited(address(this), ZERO_ADDRESS, ZERO_VALUE, ZERO_VALUE, NON_ZERO_GASLIMIT, true, NON_ZERO_DATA);
+
+        df.depositTransaction(ZERO_ADDRESS, ZERO_VALUE, NON_ZERO_GASLIMIT, true, NON_ZERO_DATA);
+    }
+
+    // Test: depositTransaction should emit the correct log when a contract deposits a contract creation with 0 value
+    function test_depositTransaction_createWithZeroValueForContract() external {
+        vm.expectEmit(true, true, false, true);
+        emit TransactionDeposited(addressAlias(), ZERO_ADDRESS, ZERO_VALUE, ZERO_VALUE, NON_ZERO_GASLIMIT, true, NON_ZERO_DATA);
+
+        df.depositTransaction(ZERO_ADDRESS, ZERO_VALUE, NON_ZERO_GASLIMIT, true, NON_ZERO_DATA);
+    }
+
+    // Test: depositTransaction should increase its eth balance when an EOA deposits a transaction with ETH
+    function test_depositTransaction_withEthValueFromEOA() external {
+        // EOA emulation
+        vm.prank(address(this), address(this));
+
+        vm.expectEmit(true, true, false, true);
+        emit TransactionDeposited(address(this), NON_ZERO_ADDRESS, NON_ZERO_VALUE, ZERO_VALUE, NON_ZERO_GASLIMIT, false, '0x');
+
+        uint256 balanceBefore = address(df).balance;
+        df.depositTransaction{value: NON_ZERO_VALUE}(NON_ZERO_ADDRESS, ZERO_VALUE, NON_ZERO_GASLIMIT, false, '0x');
+        uint256 balanceAfter = address(df).balance;
+        assertEq(balanceAfter - balanceBefore, NON_ZERO_VALUE);
+    }
+
+    // Test: depositTransaction should increase its eth balance when a contract deposits a transaction with ETH
+    function test_depositTransaction_withEthValueFromContract() external {
+        vm.expectEmit(true, true, false, true);
+        emit TransactionDeposited(addressAlias(), NON_ZERO_ADDRESS, NON_ZERO_VALUE, ZERO_VALUE, NON_ZERO_GASLIMIT, false, NON_ZERO_DATA);
+
+        df.depositTransaction{value: NON_ZERO_VALUE}(NON_ZERO_ADDRESS, ZERO_VALUE, NON_ZERO_GASLIMIT, false, NON_ZERO_DATA);
+    }
+
+    // Test: depositTransaction should increase its eth balance when an EOA deposits a contract creation with ETH
+    function test_depositTransaction_withEthValueAndEOAContractCreation() external {
+        // EOA emulation
+        vm.prank(address(this), address(this));
+
+        vm.expectEmit(true, true, false, true);
+        emit TransactionDeposited(address(this), ZERO_ADDRESS, NON_ZERO_VALUE, ZERO_VALUE, NON_ZERO_GASLIMIT, true, '0x');
+
+        uint256 balanceBefore = address(df).balance;
+        df.depositTransaction{value: NON_ZERO_VALUE}(ZERO_ADDRESS, ZERO_VALUE, NON_ZERO_GASLIMIT, true, '0x');
+        uint256 balanceAfter = address(df).balance;
+        assertEq(balanceAfter - balanceBefore, NON_ZERO_VALUE);
+    }
+
+    // Test: depositTransaction should increase its eth balance when a contract deposits a contract creation with ETH
+    function test_depositTransaction_withEthValueAndContractContractCreation() external {
+        vm.expectEmit(true, true, false, true);
+        emit TransactionDeposited(addressAlias(), ZERO_ADDRESS, NON_ZERO_VALUE, ZERO_VALUE, NON_ZERO_GASLIMIT, true, NON_ZERO_DATA);
+
+        uint256 balanceBefore = address(df).balance;
+        df.depositTransaction{value: NON_ZERO_VALUE}(ZERO_ADDRESS, ZERO_VALUE, NON_ZERO_GASLIMIT, true, NON_ZERO_DATA);
+        uint256 balanceAfter = address(df).balance;
+        assertEq(balanceAfter - balanceBefore, NON_ZERO_VALUE);
+    }
+}
