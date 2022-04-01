4611489de0d52f3ff30e1c2d0028335158470c50
Maurelian <maurelian@protonmail.ch>
2022-04-01 08:46:59 -0400

    Add output root inclusion proof
    Adds a check to ensure that withdrawal root is included in the
    l2 output root.

--- packages/contracts/contracts/L1/WithdrawalVerifier.sol
+++ packages/contracts/contracts/L1/WithdrawalVerifier.sol
@@ -7,21 +7,59 @@ import { L2OutputOracle } from "./L2OutputOracle.sol";
  * @title WithdrawalVerifier
  */
 contract WithdrawalVerifier {
-    L2OutputOracle public immutable l2oracle;
+    L2OutputOracle public immutable l2Oracle;
     address public immutable withdrawalsPredeploy;
+    // todo: add an immutable finalization window var here
+
+    struct OutputRootProof {
+        uint256 timestamp;
+        bytes32 version;
+        bytes32 stateRoot;
+        bytes32 withdrawerRoot;
+        bytes32 latestBlockhash;
+    }
+
+    // struct WithdrawalProof {
+    //     ;
+    // }
 
     constructor(L2OutputOracle _l2Oracle, address _withdrawalsPredeploy) {
-        l2oracle = _l2Oracle;
+        l2Oracle = _l2Oracle;
         withdrawalsPredeploy = _withdrawalsPredeploy;
     }
 
-    // function verifyWithdrawal(// WithdrawalMessage message,
-    // L2OutputTimestamp timestamp,
-    // WithdrawalsRootInclusionProof storageRootProof,
-    // WithdrawalMessageInclusionProof messageProof
-    // )
-    // {
+    function verifyWithdrawal(
+        uint256 nonce,
+        address _sender,
+        address _target,
+        uint256 _value,
+        uint256 _gasLimit,
+        bytes calldata _data,
+        OutputRootProof calldata _outputRootProof
+    )
+        external
+        returns (
+            // WithdrawalProof _withdrawalProof
+            bool
+        )
+    {
+        // check that the timestamp is 7 days old
+        // hash _outputRootProof and compare with the outputOracle's value
+        // how do I get the withdrawal root itself?
+        require(_outputRootProof.timestamp <= block.timestamp - 7 days, "Too soon");
 
-    // }
-
+        bytes32 outputRoot = l2Oracle.getL2Output(_outputRootProof.timestamp);
+        require(
+            outputRoot ==
+                keccak256(
+                    abi.encode(
+                        _outputRootProof.version,
+                        _outputRootProof.stateRoot,
+                        _outputRootProof.withdrawerRoot,
+                        _outputRootProof.latestBlockhash
+                    )
+                ),
+            "Calculated output root does not match expected value"
+        );
+    }
 }
