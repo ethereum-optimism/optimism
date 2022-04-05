784b390730cac98b8550dcbc6373272b52e670d1
Maurelian <maurelian@protonmail.ch>
2022-04-05 13:42:07 -0400

    contracts: Add burn function to the Withdrawer contract

--- packages/contracts/contracts/L2/Withdrawer.sol
+++ packages/contracts/contracts/L2/Withdrawer.sol
@@ -36,4 +36,29 @@ contract Withdrawer {
 
         emit WithdrawalInitiated(nonce, msg.sender, _target, msg.value, _gasLimit, _data);
     }
+
+    /**
+     * @notice Removes all ETH held in this contract from the state, by deploying a contract which
+     * immediately self destructs.
+     * For simplicity, this call is not incentivized as it costs very little to run.
+     * Inspired by https://etherscan.io/address/0xb69fba56b2e67e7dda61c8aa057886a8d1468575#code
+     */
+    function burn() external {
+        assembly {
+            // Put this code into memory at the scratch space (first word).
+            // 30 - address(this)
+            // ff - selfdestruct
+            mstore(0, 0x30ff)
+
+            // Transfer all funds to a new contract that will selfdestruct
+            // and destroy all the ether it holds in the process.
+            pop(
+                create(
+                    balance(address()), // Fund the new contract with the balance of this one.
+                    30, // offset
+                    2 // size
+                )
+            )
+        }
+    }
 }
