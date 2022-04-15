0f1d0e4e719b4c9f8f6a75a59444266cdddf5e9e
Will Cory <willcory@Wills-MacBook-Pro.local>
2022-04-15 02:47:27 -0700

    add specfile
    fix tests
    
    fix test
    
    fix test

---
+++ packages/sdk/test/utils/chain.spec.ts
@@ -0,0 +1,45 @@
+import { expect } from 'chai'
+import sinon from 'sinon'
+
+import {
+  addOptimismNetworkToMetamask,
+  Optimism,
+  toggleLayer,
+} from '../../src/utils/chain'
+
+describe('chain utils', () => {
+  describe('addOptimismNetworkToMetamask', () => {
+    it('should issue an rpc request for wallet_addEthereumChain', async () => {
+      const provider = { request: sinon.spy() }
+      await addOptimismNetworkToMetamask(provider)
+      expect(provider.request.getCalls()[0].firstArg.params[0]).to.eq(Optimism)
+      expect(provider.request.getCalls()[0].firstArg.method).to.eql(
+        'wallet_addEthereumChain'
+      )
+    })
+  })
+  describe('toggleLayer', () => {
+    it('should issue an rpc request for wallet_switchEthereumChain', async () => {
+      const provider = {
+        request: sinon.spy(({ method }: { method: string; params?: any[] }) => {
+          if (method === 'eth_chainId') {
+            return Promise.resolve(1)
+          }
+          return Promise.resolve()
+        }),
+      }
+      await toggleLayer(provider)
+      expect(
+        provider.request.calledWith({
+          method: 'eth_chainId',
+        })
+      ).to.eql(true)
+      expect(provider.request.getCalls()[1].firstArg.params[0].chainId).to.eq(
+        '0xa'
+      )
+      expect(provider.request.getCalls()[1].firstArg.method).to.eql(
+        'wallet_switchEthereumChain'
+      )
+    })
+  })
+})
