2d62a30b8f53b2cd6f3d41d7c80dba66325c55b4
protolambda <proto@protolambda.com>
2022-04-17 12:02:29 +0200

    opnode/p2p: test with require instead of assert

--- opnode/p2p/host_test.go
+++ opnode/p2p/host_test.go
@@ -18,14 +18,14 @@ import (
 	yamux "github.com/libp2p/go-libp2p-yamux"
 	lconf "github.com/libp2p/go-libp2p/config"
 	mocknet "github.com/libp2p/go-libp2p/p2p/net/mock"
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestingConfig(t *testing.T) *Config {
 	p, _, err := crypto.GenerateSecp256k1Key(rand.Reader)
-	assert.NoError(t, err, "failed to generate new p2p priv key")
+	require.NoError(t, err, "failed to generate new p2p priv key")
 	mtpt, err := lconf.MuxerConstructor(yamux.DefaultTransport)
-	assert.NoError(t, err)
+	require.NoError(t, err)
 	mux := lconf.MsMuxC{MuxC: mtpt, ID: "/yamux/1.0.0"}
 
 	return &Config{
@@ -58,31 +58,31 @@ func TestP2PSimple(t *testing.T) {
 	confA := TestingConfig(t)
 	confB := TestingConfig(t)
 	hostA, err := confA.Host()
-	assert.NoError(t, err, "failed to launch host A")
+	require.NoError(t, err, "failed to launch host A")
 	defer hostA.Close()
 	hostB, err := confB.Host()
-	assert.NoError(t, err, "failed to launch host B")
+	require.NoError(t, err, "failed to launch host B")
 	defer hostB.Close()
 	err = hostA.Connect(context.Background(), peer.AddrInfo{ID: hostB.ID(), Addrs: hostB.Addrs()})
-	assert.NoError(t, err, "failed to connect to peer B from peer A")
-	assert.Equal(t, hostB.Network().Connectedness(hostA.ID()), network.Connected)
+	require.NoError(t, err, "failed to connect to peer B from peer A")
+	require.Equal(t, hostB.Network().Connectedness(hostA.ID()), network.Connected)
 }
 
 // Full setup, using negotiated transport security and muxes
 func TestP2PFull(t *testing.T) {
 	pA, _, err := crypto.GenerateSecp256k1Key(rand.Reader)
-	assert.NoError(t, err, "failed to generate new p2p priv key")
+	require.NoError(t, err, "failed to generate new p2p priv key")
 	pB, _, err := crypto.GenerateSecp256k1Key(rand.Reader)
-	assert.NoError(t, err, "failed to generate new p2p priv key")
+	require.NoError(t, err, "failed to generate new p2p priv key")
 
 	mplexC, err := mplexC()
-	assert.NoError(t, err)
+	require.NoError(t, err)
 	yamuxC, err := yamuxC()
-	assert.NoError(t, err)
+	require.NoError(t, err)
 	noiseC, err := noiseC()
-	assert.NoError(t, err)
+	require.NoError(t, err)
 	tlsC, err := tlsC()
-	assert.NoError(t, err)
+	require.NoError(t, err)
 
 	confA := Config{
 		Priv:                (*ecdsa.PrivateKey)((pA).(*crypto.Secp256k1PrivateKey)),
@@ -113,24 +113,24 @@ func TestP2PFull(t *testing.T) {
 	// TODO: maybe swap the order of sec/mux preferences, to test that negotiation works
 
 	hostA, err := confA.Host()
-	assert.NoError(t, err)
+	require.NoError(t, err)
 	defer hostA.Close()
 	hostB, err := confB.Host()
-	assert.NoError(t, err)
+	require.NoError(t, err)
 	defer hostB.Close()
 	err = hostA.Connect(context.Background(), peer.AddrInfo{ID: hostB.ID(), Addrs: hostB.Addrs()})
-	assert.NoError(t, err, "failed to connect to peer B from peer A")
-	assert.Equal(t, hostB.Network().Connectedness(hostA.ID()), network.Connected)
+	require.NoError(t, err, "failed to connect to peer B from peer A")
+	require.Equal(t, hostB.Network().Connectedness(hostA.ID()), network.Connected)
 }
 
 // Most tests should use mocknets instead of using the actual local host network
 func TestP2PMocknet(t *testing.T) {
 	mnet, err := mocknet.FullMeshConnected(3)
-	assert.NoError(t, err, "failed to setup mocknet")
+	require.NoError(t, err, "failed to setup mocknet")
 	defer mnet.Close()
 	hosts := mnet.Hosts()
 	hostA, hostB, hostC := hosts[0], hosts[1], hosts[2]
-	assert.Equal(t, hostA.Network().Connectedness(hostB.ID()), network.Connected)
-	assert.Equal(t, hostA.Network().Connectedness(hostC.ID()), network.Connected)
-	assert.Equal(t, hostB.Network().Connectedness(hostC.ID()), network.Connected)
+	require.Equal(t, hostA.Network().Connectedness(hostB.ID()), network.Connected)
+	require.Equal(t, hostA.Network().Connectedness(hostC.ID()), network.Connected)
+	require.Equal(t, hostB.Network().Connectedness(hostC.ID()), network.Connected)
 }
