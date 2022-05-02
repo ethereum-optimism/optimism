fba10d52001a8623d4ab538c546a4e05721e01d2
Kelvin Fichter <kelvin@optimism.io>
2022-05-01 21:00:13 -0400

    maint(ct): simplify test helpers
    Simplifies test helpers slightly by cutting out unnecessary code.

--- packages/contracts/contracts/test-helpers/FailingReceiver.sol
+++ packages/contracts/contracts/test-helpers/FailingReceiver.sol
@@ -1,13 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.9;
 
-/**
- * @title FailingReceiver
- */
 contract FailingReceiver {
-    /**
-     * @notice Receiver that always reverts upon receiving ether.
-     */
     receive() external payable {
         require(false, "FailingReceiver");
     }
