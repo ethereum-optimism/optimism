ethereum-optimism/optimistic-specs

opnode: refactor RPC server / config, prototype batch serving

opnode/l2/source.go
@@ -6,6 +6,8 @@ import (
 	"math/big"
 	"time"
 
+	"github.com/ethereum/go-ethereum"
+
 	"github.com/ethereum-optimism/optimistic-specs/opnode/eth"
 	"github.com/ethereum-optimism/optimistic-specs/opnode/rollup"
 	"github.com/ethereum-optimism/optimistic-specs/opnode/rollup/derive"
@@ -147,4 +149,62 @@ func (s *Source) L2BlockRefByHash(ctx context.Context, l2Hash common.Hash) (eth.
 		return eth.L2BlockRef{}, fmt.Errorf("failed to determine block-hash of height %v, could not get header: %w", l2Hash, err)
 	}
 	return derive.BlockReferences(block, s.genesis)
+}
+
+type ReadOnlySource struct {
+	rpc     *rpc.Client       // raw RPC client. Used for methods that do not already have bindings
+	client  *ethclient.Client // go-ethereum's wrapper around the rpc client for the eth namespace
+	genesis *rollup.Genesis
+	log     log.Logger
+}
+
+func NewReadOnlySource(ll2Node *rpc.Client, genesis *rollup.Genesis, log log.Logger) (*ReadOnlySource, error) {
+	return &ReadOnlySource{
+		rpc:     ll2Node,
+		client:  ethclient.NewClient(ll2Node),
+		genesis: genesis,
+		log:     log,
+	}, nil
+}
+
+// TODO: de-duplicate Source and ReadOnlySource.
+// We should really have a L1-downloader like binding that is more configurable and has caching.
+
+// L2BlockRefByNumber returns the canonical block and parent ids.
+func (s *ReadOnlySource) L2BlockRefByNumber(ctx context.Context, l2Num *big.Int) (eth.L2BlockRef, error) {
+	block, err := s.client.BlockByNumber(ctx, l2Num)
+	if err != nil {
+		// w%: wrap the error, we still need to detect if a canonical block is not found, a.k.a. end of chain.
+		return eth.L2BlockRef{}, fmt.Errorf("failed to determine block-hash of height %v, could not get header: %w", l2Num, err)
+	}
+	return derive.BlockReferences(block, s.genesis)
+}
+
+// L2BlockRefByHash returns the block & parent ids based on the supplied hash. The returned BlockRef may not be in the canonical chain
+func (s *ReadOnlySource) L2BlockRefByHash(ctx context.Context, l2Hash common.Hash) (eth.L2BlockRef, error) {
+	block, err := s.client.BlockByHash(ctx, l2Hash)
+	if err != nil {
+		// w%: wrap the error, we still need to detect if a canonical block is not found, a.k.a. end of chain.
+		return eth.L2BlockRef{}, fmt.Errorf("failed to determine block-hash of height %v, could not get header: %w", l2Hash, err)
+	}
+	return derive.BlockReferences(block, s.genesis)
+}
+
+func (s *ReadOnlySource) BlockByNumber(ctx context.Context, number *big.Int) (*types.Block, error) {
+	return s.client.BlockByNumber(ctx, number)
+}
+
+func (s *ReadOnlySource) GetBlockHeader(ctx context.Context, blockTag string) (*types.Header, error) {
+	var head *types.Header
+	err := s.rpc.CallContext(ctx, &head, "eth_getBlockByNumber", blockTag, false)
+	return head, err
+}
+
+func (s *ReadOnlySource) GetProof(ctx context.Context, address common.Address, blockTag string) (*AccountResult, error) {
+	var getProofResponse *AccountResult
+	err := s.rpc.CallContext(ctx, &getProofResponse, "eth_getProof", address, []common.Hash{}, blockTag)
+	if err == nil && getProofResponse == nil {
+		err = ethereum.NotFound
+	}
+	return getProofResponse, err
 }

ethereum-optimism/optimism

opnode/l2/source.go
@@ -6,6 +6,8 @@ import (
 	"math/big"
 	"time"
 
+	"github.com/ethereum/go-ethereum"
+
 	"github.com/ethereum-optimism/optimistic-specs/opnode/eth"
 	"github.com/ethereum-optimism/optimistic-specs/opnode/rollup"
 	"github.com/ethereum-optimism/optimistic-specs/opnode/rollup/derive"
@@ -23,10 +25,10 @@ type Source struct {
 	log     log.Logger
 }
 
-func NewSource(ll2Node *rpc.Client, genesis *rollup.Genesis, log log.Logger) (*Source, error) {
+func NewSource(l2Node *rpc.Client, genesis *rollup.Genesis, log log.Logger) (*Source, error) {
 	return &Source{
-		rpc:     ll2Node,
-		client:  ethclient.NewClient(ll2Node),
+		rpc:     l2Node,
+		client:  ethclient.NewClient(l2Node),
 		genesis: genesis,
 		log:     log,
 	}, nil
@@ -253,4 +255,62 @@ func PayloadToBlockRef(payload *ExecutionPayload, genesis *rollup.Genesis) (eth.
 		L1Origin:       l1Origin,
 		SequenceNumber: sequenceNumber,
 	}, nil
+}
+
+type ReadOnlySource struct {
+	rpc     *rpc.Client       // raw RPC client. Used for methods that do not already have bindings
+	client  *ethclient.Client // go-ethereum's wrapper around the rpc client for the eth namespace
+	genesis *rollup.Genesis
+	log     log.Logger
+}
+
+func NewReadOnlySource(l2Node *rpc.Client, genesis *rollup.Genesis, log log.Logger) (*ReadOnlySource, error) {
+	return &ReadOnlySource{
+		rpc:     l2Node,
+		client:  ethclient.NewClient(l2Node),
+		genesis: genesis,
+		log:     log,
+	}, nil
+}
+
+// TODO: de-duplicate Source and ReadOnlySource.
+// We should really have a L1-downloader like binding that is more configurable and has caching.
+
+// L2BlockRefByNumber returns the canonical block and parent ids.
+func (s *ReadOnlySource) L2BlockRefByNumber(ctx context.Context, l2Num *big.Int) (eth.L2BlockRef, error) {
+	block, err := s.client.BlockByNumber(ctx, l2Num)
+	if err != nil {
+		// w%: wrap the error, we still need to detect if a canonical block is not found, a.k.a. end of chain.
+		return eth.L2BlockRef{}, fmt.Errorf("failed to determine block-hash of height %v, could not get header: %w", l2Num, err)
+	}
+	return blockToBlockRef(block, s.genesis)
+}
+
+// L2BlockRefByHash returns the block & parent ids based on the supplied hash. The returned BlockRef may not be in the canonical chain
+func (s *ReadOnlySource) L2BlockRefByHash(ctx context.Context, l2Hash common.Hash) (eth.L2BlockRef, error) {
+	block, err := s.client.BlockByHash(ctx, l2Hash)
+	if err != nil {
+		// w%: wrap the error, we still need to detect if a canonical block is not found, a.k.a. end of chain.
+		return eth.L2BlockRef{}, fmt.Errorf("failed to determine block-hash of height %v, could not get header: %w", l2Hash, err)
+	}
+	return blockToBlockRef(block, s.genesis)
+}
+
+func (s *ReadOnlySource) BlockByNumber(ctx context.Context, number *big.Int) (*types.Block, error) {
+	return s.client.BlockByNumber(ctx, number)
+}
+
+func (s *ReadOnlySource) GetBlockHeader(ctx context.Context, blockTag string) (*types.Header, error) {
+	var head *types.Header
+	err := s.rpc.CallContext(ctx, &head, "eth_getBlockByNumber", blockTag, false)
+	return head, err
+}
+
+func (s *ReadOnlySource) GetProof(ctx context.Context, address common.Address, blockTag string) (*AccountResult, error) {
+	var getProofResponse *AccountResult
+	err := s.rpc.CallContext(ctx, &getProofResponse, "eth_getProof", address, []common.Hash{}, blockTag)
+	if err == nil && getProofResponse == nil {
+		err = ethereum.NotFound
+	}
+	return getProofResponse, err
 }