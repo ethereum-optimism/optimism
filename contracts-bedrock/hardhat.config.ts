9c4965e707d0a03206fb7e726713982edf8539ca
Mark Tyneway <mark.tyneway@gmail.com>
2022-05-23 23:26:51 -0700

    contracts: fix to work with hardhat

--- contracts-bedrock/hardhat.config.ts
+++ contracts-bedrock/hardhat.config.ts
@@ -1,3 +1,6 @@
+import path from 'path'
+import fs from 'fs'
+
 import { HardhatUserConfig, task, subtask } from 'hardhat/config'
 import { TASK_COMPILE_SOLIDITY_GET_SOURCE_PATHS } from 'hardhat/builtin-tasks/task-names'
 import '@nomiclabs/hardhat-waffle'
@@ -5,6 +8,7 @@ import '@typechain/hardhat'
 import 'hardhat-gas-reporter'
 import 'solidity-coverage'
 import 'hardhat-deploy'
+import '@foundry-rs/hardhat'
 
 import './tasks/deposits'
 
@@ -23,6 +27,97 @@ task('accounts', 'Prints the list of accounts', async (_, hre) => {
     console.log(account.address)
   }
 })
+
+const getAllFiles = function(directory: string, arrayOfFiles: Array<string> = []) {
+  const files = fs.readdirSync(directory)
+
+  for (const file of files) {
+    const next = path.join(directory, file)
+    if (fs.statSync(next).isDirectory()) {
+      arrayOfFiles = getAllFiles(next, arrayOfFiles)
+    } else {
+      arrayOfFiles.push(next)
+    }
+  }
+
+  return arrayOfFiles
+}
+
+task('compile').setAction(async (taskArgs, hre, runSuper) => {
+  await runSuper(taskArgs)
+
+  // recursively get all of the source code and
+  // get the relative paths to each file
+  const allFiles = getAllFiles(hre.config.paths.sources)
+    .map(f => path.relative(__dirname, f))
+
+  // get the configured artifacts output path
+  const artifactsPath = hre.config.paths.artifacts
+  // get the absolute path to each foundry artifact
+  const paths = await hre.artifacts.getArtifactPaths()
+  for (const p of paths) {
+    // skip tests
+    if (p.includes('.t.sol')) {
+      continue
+    }
+
+    // read the artifact path from the filesystem
+    // put this in a try catch as it will fail if multiple
+    // contracts are defined in the same file
+    const info = path.parse(p)
+    let artifact
+    try {
+      artifact = await hre.artifacts.readArtifact(info.name)
+    } catch (e) {
+      console.log(e)
+      continue
+    }
+
+    // find the path to the source code
+    let target
+    for (const file of allFiles) {
+      if (path.parse(file).base === path.parse(info.dir).base) {
+        target = file
+        break
+      }
+    }
+
+    // unable to find the source code
+    if (!target) {
+      continue
+    }
+
+    // write the artifact
+    const dir = path.join(artifactsPath, target)
+    fs.mkdirSync(dir, {recursive: true})
+    const out = path.join(dir, info.base)
+    fs.writeFileSync(out, JSON.stringify(artifact, null, 2))
+  }
+});
+
+/*
+preprocess: {
+    eachLine: (hre) => ({
+      transform: (line: string) => {
+        if (line.match(/^\s*import /i)) {
+          getRemappings().forEach(([find, replace]) => {
+            if (line.match('"' + find)) {
+              line = line.replace('"' + find, '"' + replace);
+            }
+          });
+        }
+        return line;
+      },
+    }),
+  },
+
+function getRemappings() {
+  return fs
+    .readFileSync("remappings.txt", "utf8")
+    .split("\n")
+    .map((line) => line.trim().split("="));
+}
+*/
 
 const config: HardhatUserConfig = {
   networks: {
