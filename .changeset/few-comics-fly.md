416d2e6084acb4e065697e471983461f81b7b453
smartcontracts <kelvin@optimism.io>
2022-06-01 17:57:35 -0400

    feat(ctp): introduce Drippie and rework deploy (#2569)
    Drippie is a helper contract for managing automatic contract
    interactions. Drippie is meant to be deployed as a middle layer between
    our contracts and other automation services like Gelato. This commit
    also revamps the deployment process to use hardhat-deploy deterministic
    deployments, ledger support, and better authentication.

---
+++ .changeset/few-comics-fly.md
@@ -0,0 +1,5 @@
+---
+'@eth-optimism/contracts-periphery': patch
+---
+
+Introduce the Drippie peripheral contract for managing ETH drips
---
+++ packages/contracts-periphery/deploy/drippie/dripchecks/CheckTrue.ts
@@ -0,0 +1,18 @@
+/* Imports: External */
+import { DeployFunction } from 'hardhat-deploy/dist/types'
+
+const deployFn: DeployFunction = async (hre) => {
+  const { deployer } = await hre.getNamedAccounts()
+
+  const { deploy } = await hre.deployments.deterministic('CheckTrue', {
+    salt: hre.ethers.utils.solidityKeccak256(['string'], ['CheckTrue']),
+    from: deployer,
+    log: true,
+  })
+
+  await deploy()
+}
+
+deployFn.tags = ['CheckTrue']
+
+export default deployFn
