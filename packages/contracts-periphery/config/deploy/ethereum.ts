b30ea8ea0565c41c2f67e6a830104865cea7a01f
Kelvin Fichter <kelvin@optimism.io>
2022-06-03 15:07:55 -0400

    feat: deploy RetroReceiver

---
+++ packages/contracts-periphery/config/deploy/ethereum.ts
@@ -0,0 +1,8 @@
+import { DeployConfig } from '../../src'
+
+const config: DeployConfig = {
+  retroReceiverOwner: '0xc37f6a6c4AB335E20d10F034B90386E2fb70bbF5',
+  drippieOwner: '0xc37f6a6c4AB335E20d10F034B90386E2fb70bbF5',
+}
+
+export default config
