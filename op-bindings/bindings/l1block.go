71829503fe3067c9dd5dc12ebc0b97f651db64be
Mark Tyneway <mark.tyneway@gmail.com>
2022-06-07 13:22:09 -0700

    op-bindings: regenerate

--- op-bindings/bindings/l1block.go
+++ op-bindings/bindings/l1block.go
@@ -30,8 +30,8 @@ var (
 
 // L1BlockMetaData contains all meta data concerning the L1Block contract.
 var L1BlockMetaData = &bind.MetaData{
-	ABI: "[{\"inputs\":[],\"name\":\"OnlyDepositor\",\"type\":\"error\"},{\"inputs\":[],\"name\":\"DEPOSITOR_ACCOUNT\",\"outputs\":[{\"internalType\":\"address\",\"name\":\"\",\"type\":\"address\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[],\"name\":\"basefee\",\"outputs\":[{\"internalType\":\"uint256\",\"name\":\"\",\"type\":\"uint256\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[],\"name\":\"hash\",\"outputs\":[{\"internalType\":\"bytes32\",\"name\":\"\",\"type\":\"bytes32\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[],\"name\":\"number\",\"outputs\":[{\"internalType\":\"uint64\",\"name\":\"\",\"type\":\"uint64\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[],\"name\":\"sequenceNumber\",\"outputs\":[{\"internalType\":\"uint64\",\"name\":\"\",\"type\":\"uint64\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"uint64\",\"name\":\"_number\",\"type\":\"uint64\"},{\"internalType\":\"uint64\",\"name\":\"_timestamp\",\"type\":\"uint64\"},{\"internalType\":\"uint256\",\"name\":\"_basefee\",\"type\":\"uint256\"},{\"internalType\":\"bytes32\",\"name\":\"_hash\",\"type\":\"bytes32\"},{\"internalType\":\"uint64\",\"name\":\"_sequenceNumber\",\"type\":\"uint64\"}],\"name\":\"setL1BlockValues\",\"outputs\":[],\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"inputs\":[],\"name\":\"timestamp\",\"outputs\":[{\"internalType\":\"uint64\",\"name\":\"\",\"type\":\"uint64\"}],\"stateMutability\":\"view\",\"type\":\"function\"}]",
-	Bin: "0x608060405234801561001057600080fd5b5061025a806100206000396000f3fe608060405234801561001057600080fd5b506004361061007d5760003560e01c806364ca23ef1161005b57806364ca23ef146100bc5780638381f58a146100fd578063b80777ea14610111578063e591b2821461013157600080fd5b8063042c2f571461008257806309bd5a60146100975780635cf24969146100b3575b600080fd5b6100956100903660046101f6565b610171565b005b6100a060025481565b6040519081526020015b60405180910390f35b6100a060015481565b6000546100e490700100000000000000000000000000000000900467ffffffffffffffff1681565b60405167ffffffffffffffff90911681526020016100aa565b6000546100e49067ffffffffffffffff1681565b6000546100e49068010000000000000000900467ffffffffffffffff1681565b61014c73deaddeaddeaddeaddeaddeaddeaddeaddead000181565b60405173ffffffffffffffffffffffffffffffffffffffff90911681526020016100aa565b3373deaddeaddeaddeaddeaddeaddeaddeaddead0001146101be576040517fce8c104800000000000000000000000000000000000000000000000000000000815260040160405180910390fd5b60801b60409390931b90931791909117600055600155600255565b803567ffffffffffffffff811681146101f157600080fd5b919050565b600080600080600060a0868803121561020e57600080fd5b610217866101d9565b9450610225602087016101d9565b93506040860135925060608601359150610241608087016101d9565b9050929550929590935056fea164736f6c634300080a000a",
+	ABI: "[{\"inputs\":[],\"name\":\"OnlyDepositor\",\"type\":\"error\"},{\"inputs\":[],\"name\":\"DEPOSITOR_ACCOUNT\",\"outputs\":[{\"internalType\":\"address\",\"name\":\"\",\"type\":\"address\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[],\"name\":\"averageBasefee\",\"outputs\":[{\"internalType\":\"uint256\",\"name\":\"\",\"type\":\"uint256\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[],\"name\":\"basefee\",\"outputs\":[{\"internalType\":\"uint256\",\"name\":\"\",\"type\":\"uint256\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[],\"name\":\"hash\",\"outputs\":[{\"internalType\":\"bytes32\",\"name\":\"\",\"type\":\"bytes32\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[],\"name\":\"number\",\"outputs\":[{\"internalType\":\"uint64\",\"name\":\"\",\"type\":\"uint64\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[],\"name\":\"sequenceNumber\",\"outputs\":[{\"internalType\":\"uint64\",\"name\":\"\",\"type\":\"uint64\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"uint64\",\"name\":\"_number\",\"type\":\"uint64\"},{\"internalType\":\"uint64\",\"name\":\"_timestamp\",\"type\":\"uint64\"},{\"internalType\":\"uint256\",\"name\":\"_basefee\",\"type\":\"uint256\"},{\"internalType\":\"bytes32\",\"name\":\"_hash\",\"type\":\"bytes32\"},{\"internalType\":\"uint64\",\"name\":\"_sequenceNumber\",\"type\":\"uint64\"}],\"name\":\"setL1BlockValues\",\"outputs\":[],\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"inputs\":[],\"name\":\"timestamp\",\"outputs\":[{\"internalType\":\"uint64\",\"name\":\"\",\"type\":\"uint64\"}],\"stateMutability\":\"view\",\"type\":\"function\"}]",
+	Bin: "0x608060405234801561001057600080fd5b50610433806100206000396000f3fe608060405234801561001057600080fd5b50600436106100885760003560e01c806364ca23ef1161005b57806364ca23ef146100d05780638381f58a14610111578063b80777ea14610125578063e591b2821461014557600080fd5b8063042c2f571461008d57806309bd5a60146100a25780635cf24969146100be5780635f528dc3146100c7575b600080fd5b6100a061009b366004610365565b610185565b005b6100ab60035481565b6040519081526020015b60405180910390f35b6100ab60015481565b6100ab60025481565b6000546100f890700100000000000000000000000000000000900467ffffffffffffffff1681565b60405167ffffffffffffffff90911681526020016100b5565b6000546100f89067ffffffffffffffff1681565b6000546100f89068010000000000000000900467ffffffffffffffff1681565b61016073deaddeaddeaddeaddeaddeaddeaddeaddead000181565b60405173ffffffffffffffffffffffffffffffffffffffff90911681526020016100b5565b3373deaddeaddeaddeaddeaddeaddeaddeaddead0001146101d2576040517fce8c104800000000000000000000000000000000000000000000000000000000815260040160405180910390fd5b608081901b604085901b8617176000819055600184905560038390556101f784610202565b600255505050505050565b600061021060025483610216565b92915050565b60008061023c84610237676124fee993bc0000678ac7230489e80000610286565b6102a2565b9050600061025e846102376729a2241af62c0000678ac7230489e80000610286565b9050670de0b6b3a764000061027383836102f3565b61027d91906103eb565b95945050505050565b600061029b83670de0b6b3a764000084610329565b9392505050565b6000826102b157506000610210565b828202828482816102c4576102c46103bc565b041461029b577fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff915050610210565b60008282018381101561029b577fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff915050610210565b82820281151584158583048514171661034157600080fd5b0492915050565b803567ffffffffffffffff8116811461036057600080fd5b919050565b600080600080600060a0868803121561037d57600080fd5b61038686610348565b945061039460208701610348565b935060408601359250606086013591506103b060808701610348565b90509295509295909350565b7f4e487b7100000000000000000000000000000000000000000000000000000000600052601260045260246000fd5b600082610421577f4e487b7100000000000000000000000000000000000000000000000000000000600052601260045260246000fd5b50049056fea164736f6c634300080a000a",
 }
 
 // L1BlockABI is the input ABI used to generate the binding from.
@@ -230,6 +230,37 @@ func (_L1Block *L1BlockSession) DEPOSITORACCOUNT() (common.Address, error) {
 // Solidity: function DEPOSITOR_ACCOUNT() view returns(address)
 func (_L1Block *L1BlockCallerSession) DEPOSITORACCOUNT() (common.Address, error) {
 	return _L1Block.Contract.DEPOSITORACCOUNT(&_L1Block.CallOpts)
+}
+
+// AverageBasefee is a free data retrieval call binding the contract method 0x5f528dc3.
+//
+// Solidity: function averageBasefee() view returns(uint256)
+func (_L1Block *L1BlockCaller) AverageBasefee(opts *bind.CallOpts) (*big.Int, error) {
+	var out []interface{}
+	err := _L1Block.contract.Call(opts, &out, "averageBasefee")
+
+	if err != nil {
+		return *new(*big.Int), err
+	}
+
+	out0 := *abi.ConvertType(out[0], new(*big.Int)).(**big.Int)
+
+	return out0, err
+
+}
+
+// AverageBasefee is a free data retrieval call binding the contract method 0x5f528dc3.
+//
+// Solidity: function averageBasefee() view returns(uint256)
+func (_L1Block *L1BlockSession) AverageBasefee() (*big.Int, error) {
+	return _L1Block.Contract.AverageBasefee(&_L1Block.CallOpts)
+}
+
+// AverageBasefee is a free data retrieval call binding the contract method 0x5f528dc3.
+//
+// Solidity: function averageBasefee() view returns(uint256)
+func (_L1Block *L1BlockCallerSession) AverageBasefee() (*big.Int, error) {
+	return _L1Block.Contract.AverageBasefee(&_L1Block.CallOpts)
 }
 
 // Basefee is a free data retrieval call binding the contract method 0x5cf24969.
