e59438793d652ea6cb2301ebb7482478c3549c33
protolambda <proto@protolambda.com>
2022-06-09 23:07:46 +0200

    channels for multiplexing submissions, inclusion for ordering of processing, decoded batches can be ordered as necessary after pulling from the stream

--- op-node/rollup/buidl/outwards.go
+++ op-node/rollup/buidl/outwards.go
@@ -3,29 +3,33 @@ package buidl
 import (
 	"bytes"
 	"context"
+	"crypto/rand"
 	"encoding/binary"
 	"fmt"
 	"io"
+	"sort"
 
 	"github.com/ethereum-optimism/optimism/op-node/eth"
+	"github.com/ethereum/go-ethereum/log"
 )
 
 type OutChannel struct {
-	ID ChannelID
+	id ChannelID
 
-	// exclusive
-	Start eth.BlockID
-	// inclusive
-	End eth.BlockID
+	// the L2 blocks that were encoded in this channel
+	blocks []eth.BlockID
 
 	// Frame ID of the next frame to emit. Increment after emitting
-	Frame uint64
+	frame uint64
 
 	// How much we've pulled from the reader so far
-	Offset uint64
+	offset uint64
+
+	// time of creation, to prune out old timed-out channels
+	created uint64
 
 	// Nil when closed
-	Reader io.Reader
+	reader io.Reader
 
 	// scratch for temporary buffering
 	scratch bytes.Buffer
@@ -37,14 +41,18 @@ func makeUVarint(x uint64) []byte {
 	return tmp[:n]
 }
 
+func (oc *OutChannel) Closed() bool {
+	return oc.reader == nil
+}
+
 func (oc *OutChannel) Output(maxSize uint64) ([]byte, error) {
-	if oc.Reader == nil {
+	if oc.reader == nil {
 		return nil, fmt.Errorf("channel is closed")
 	}
 
 	var out []byte
-	out = append(out, makeUVarint(uint64(oc.ID))...)
-	out = append(out, makeUVarint(oc.Frame)...)
+	out = append(out, oc.id[:]...)
+	out = append(out, makeUVarint(oc.frame)...)
 	// +1 for single byte of frame content, +1 for lastFrame bool
 	if uint64(len(out))+2 > maxSize {
 		return nil, fmt.Errorf("no more space: %d > %d", len(out), maxSize)
@@ -57,9 +65,9 @@ func (oc *OutChannel) Output(maxSize uint64) ([]byte, error) {
 	maxFrameLen -= uint64(len(makeUVarint(maxFrameLen)))
 
 	oc.scratch.Reset()
-	_, err := io.CopyN(&oc.scratch, oc.Reader, int64(maxFrameLen))
+	_, err := io.CopyN(&oc.scratch, oc.reader, int64(maxFrameLen))
 	frameLen := uint64(len(oc.scratch.Bytes()))
-	oc.Offset += frameLen
+	oc.offset += frameLen
 	lastFrame := err == io.EOF
 	if err != nil && !lastFrame {
 		return nil, fmt.Errorf("failed to read data for frame: %w", err)
@@ -68,65 +76,185 @@ func (oc *OutChannel) Output(maxSize uint64) ([]byte, error) {
 	out = append(out, oc.scratch.Bytes()...)
 	if lastFrame {
 		out = append(out, 1)
-
+		oc.reader = nil // close the channel
 	} else {
 		out = append(out, 0)
 	}
-	oc.Frame += 1
+	oc.frame += 1
 	return out, nil
 }
 
 type Outgoing struct {
-	openChannels map[ChannelID]*OutChannel
+	log log.Logger
+
+	// pruned when timed out. We keep track of fully read channels to avoid resubmitting data.
+	channels map[ChannelID]*OutChannel
+
+	l1Head eth.L1BlockRef
 }
 
-// TODO: based on previous data we may be able to reconstruct a half-finished channel, to continue it on a fresh (restarted or different instance) rollup node.
+// SetL1Head updates the L1 head, so the old channels can be pruned
+func (og *Outgoing) SetL1Head(head eth.L1BlockRef) {
+	og.l1Head = head
+}
 
-func (og *Outgoing) Output(ctx context.Context, lastID ChannelID, lastComplete eth.BlockID, id ChannelID, maxSize uint64) ([]byte, error) {
-	if og.openChannels == nil {
-		og.openChannels = make(map[ChannelID]*OutChannel)
+// TODO: based on previous data we may be able to reconstruct a partially-consumed channel, to continue it on a fresh (restarted or different instance) rollup node.
+
+type OutputData struct {
+	// Channels identifies all channels that were involved in this output, with their last frame ID.
+	// Empty if no new data was produced.
+	Channels map[ChannelID]uint64
+
+	// Data to post to L1, encodes channel version byte and one or more frames
+	Data []byte
+}
+
+// history is the collection of channels that have been submitted, and the frame ID of the last submission
+func (og *Outgoing) Output(ctx context.Context, history map[ChannelID]uint64, maxSize uint64, maxBlocksPerFrame uint64) (*OutputData, error) {
+	if og.channels == nil {
+		og.channels = make(map[ChannelID]*OutChannel)
 	}
 
-	// TODO: prune timed out and closed channels
+	// prune timed out channels, before adding new ones
+	for id, ch := range og.channels {
+		if ch.created+ChannelTimeout < og.l1Head.Time {
+			if ch.Closed() {
+				og.log.Debug("cleaning up closed timed-out channel", "channel", ch)
+			} else {
+				og.log.Warn("channel timed out without completing", "channel", ch, "frame", ch.frame, "created", ch.created, "offset", ch.offset)
+			}
+			delete(og.channels, id)
+		}
+	}
 
-	var out []byte
-	out = append(out, ChannelVersion0)
+	// We find the first 1000 unsafe blocks that we may want to put in the output
+	unsafeBlocks := make(map[eth.BlockID]struct{})
+	// TODO scan from safe-head to unsafe-head and fill unsafeBlocks
 
-	// Open new channels while we have space left to output to
-	for {
-		if ctx.Err() != nil {
+	out := &OutputData{Channels: make(map[ChannelID]uint64)}
+	out.Data = append(out.Data, ChannelVersion0)
+
+	// check full history, and add data for any channels we still consider to be open.
+	for chID, frameNr := range history {
+		if ctx.Err() != nil { // return what we have if we run out of time.
 			return out, nil
 		}
 
 		// check if we can fit in one more frame
-		if uint64(len(out))+minimumFrameSize > maxSize {
+		if uint64(len(out.Data))+minimumFrameSize > maxSize {
 			return out, nil
 		}
 
-		outCh, ok := og.openChannels[lastID]
+		outCh, ok := og.channels[chID]
 		if !ok {
-			// TODO find L2 end block for channel. Block until it's found, or ctx ends.
-			end := eth.BlockID{}
-			// TODO construct reader for encoding the data
-			var r io.Reader
-			outCh = &OutChannel{
-				ID:     id,
-				Start:  lastComplete,
-				End:    end,
-				Frame:  0,
-				Offset: 0,
-				Reader: r,
-			}
-			og.openChannels[id] = outCh
-		}
-
-		frame, err := outCh.Output(maxSize - uint64(len(out)))
-		if err != nil {
-			// remove the faulty channel (it may be closed, not canonical chain anymore, or corrupted somehow)
-			delete(og.openChannels, outCh.ID)
-			// TODO log error
+			// we may already have pruned the timed-out channel.
+			// If timed-out, fair game to resubmit contents if we still consider the contents unsafe.
 			continue
 		}
-		out = append(out, frame...)
+		if len(outCh.blocks) == 0 {
+			delete(og.channels, chID)
+			og.log.Warn("found open channel without any blocks, deleting it", "channel", chID)
+			continue
+		}
+
+		nextFrame := frameNr + 1
+		// The caller is behind the previous state of this channel, e.g. due to a reorg of L1.
+		// There may be signed txs floating around that add frames to this channel.
+		// Let's avoid this channel, and don't encode any of the blocks.
+		// When the channel times out we can reinsert the blocks.
+		if outCh.frame > nextFrame {
+			for _, b := range outCh.blocks {
+				delete(unsafeBlocks, b)
+			}
+			og.log.Warn("Cannot continue channel from older frame, thus not submitting blocks of this channel",
+				"channel", chID, "expected_next_frame", outCh.frame, "history_next_frame", nextFrame)
+			continue // TODO: if we want to reproduce new (potentially conflicting) frames we can, but we may not want to.
+		}
+		// The channel is not as far, we don't know of the frame that was previously submitted
+		if outCh.frame < nextFrame {
+			for _, b := range outCh.blocks {
+				delete(unsafeBlocks, b)
+			}
+			og.log.Warn("Cannot continue channel from future frame, thus not submitting blocks of this channel",
+				"channel", chID, "expected_next_frame", outCh.frame, "history_next_frame", nextFrame)
+			continue
+		}
+
+		// If the channel is done, we avoid resubmitting any of the blocks
+		if outCh.Closed() {
+			for _, b := range outCh.blocks {
+				delete(unsafeBlocks, b)
+			}
+			og.log.Debug("Already submitted full channel contents of channel, not submitting it again",
+				"channel", chID)
+			continue
+		}
+
+		frame, err := outCh.Output(maxSize - uint64(len(out.Data)))
+		if err != nil {
+			// remove the channel (it may be closed, not canonical chain anymore, or corrupted somehow)
+			delete(og.channels, outCh.id)
+			log.Error("failed to output frame for channel", "channel", outCh.id, "err", err)
+			continue
+		}
+		out.Data = append(out.Data, frame...)
+		out.Channels[outCh.id] = nextFrame
+	}
+
+	// There may be gaps in the remaining unsafe blocks to submit.
+	// But we want to submit the lowest-number blocks first. So collect and sort them.
+	unsafeBlocksSorted := make([]eth.BlockID, 0, len(unsafeBlocks))
+	for b := range unsafeBlocks {
+		unsafeBlocksSorted = append(unsafeBlocksSorted, b)
+	}
+	sort.Slice(unsafeBlocksSorted, func(i, j int) bool {
+		return unsafeBlocksSorted[i].Number < unsafeBlocksSorted[j].Number
+	})
+
+	// Open new channels while we have space left to output to.
+	for {
+		if ctx.Err() != nil { // return what we have if we run out of time.
+			return out, nil
+		}
+
+		// check if we can fit in one more frame
+		if uint64(len(out.Data))+minimumFrameSize > maxSize {
+			return out, nil
+		}
+
+		var id ChannelID
+		if _, err := rand.Read(id[:]); err != nil {
+			return nil, fmt.Errorf("failed to create new random ID: %v", err)
+		}
+
+		if _, ok := og.channels[id]; ok {
+			log.Warn("generated a channel ID that already exists", "channel", id)
+			continue
+		}
+
+		blocks := unsafeBlocksSorted
+		// don't put too many L2 blocks into the same frame.
+		if uint64(len(blocks)) > maxBlocksPerFrame {
+			blocks = blocks[:maxBlocksPerFrame]
+		}
+		// TODO construct reader for encoding the data
+		var r io.Reader
+		outCh := &OutChannel{
+			id:      id,
+			blocks:  blocks,
+			frame:   0,
+			offset:  0,
+			created: og.l1Head.Time,
+			reader:  r,
+		}
+		og.channels[id] = outCh
+
+		frame, err := outCh.Output(maxSize - uint64(len(out.Data)))
+		if err != nil {
+			log.Error("failed to output frame for new channel", "channel", id, "err", err)
+			continue
+		}
+		out.Data = append(out.Data, frame...)
+		out.Channels[id] = 0
 	}
 }
---
+++ op-node/rollup/buidl/pipeline.go
@@ -0,0 +1,58 @@
+package buidl
+
+import (
+	"github.com/ethereum-optimism/optimism/op-node/eth"
+	"io"
+)
+
+type Pipeline struct {
+	// Returns the next frame to process
+	// blocks until there is new data to consume
+	// returns nil when the source is closed
+	source   func() *TaggedData
+	l1Origin eth.L1BlockRef
+	channel  ChannelID
+	buf      []byte
+}
+
+// Read data from the pipeline.
+// An EOF is returned when the reader should reset before reading more data again.
+// The returned data may not be canonical anymore when read. The CurrentSource should be checked.
+// No errors are returned otherwise.
+// The reader automatically moves to the next data sources as the current one gets exhausted.
+// It's up to the caller to check CurrentSource() before reading more information.
+// The CurrentSource() does not change until the first Read() after the old source has been completely exhausted.
+func (p *Pipeline) Read(dest []byte) (n int, err error) {
+	// if we're out of data, then rotate to the next frame
+	if len(p.buf) == 0 {
+		next := p.source()
+		if next == nil {
+			return 0, io.EOF
+		}
+		p.l1Origin = next.L1Origin
+		p.buf = next.Data
+		if p.channel != next.ChannelID {
+			p.channel = next.ChannelID
+			return 0, io.EOF // reset the stream before we start reading from the next channel
+		}
+	}
+
+	// try to consume current item
+	n = copy(dest, p.buf)
+	p.buf = p.buf[n:]
+	return n, nil
+}
+
+// Reset forces the next read to continue with the next item
+func (p *Pipeline) Reset() {
+	p.buf = p.buf[:0]
+	// empty channel ID, always different from the next thing that is read, since 0 is not a valid ID
+	p.channel = ChannelID{}
+}
+
+// CurrentSource returns the L1 block that encodes the data that is currently being read.
+// Batches should be filtered based on this source.
+// Note that the source might not be canonical anymore by the time the data is processed.
+func (p *Pipeline) CurrentSource() eth.L1BlockRef {
+	return p.l1Origin
+}
