ethereum-optimism/optimistic-specs

contracts: Fix OZ-N-03 Complex ERC165 implementation
fixup! contracts: Fix OZ-N-03 Complex ERC165 implementation

packages/contracts/contracts/test/OptimismMintableERC20.t.sol
@@ -3,6 +3,8 @@ pragma solidity 0.8.10;
 
 import { Bridge_Initializer } from "./CommonTest.t.sol";
 import { LibRLP } from "./Lib_RLP.t.sol";
+import "../universal/SupportedInterfaces.sol";
+import { IERC165 } from "@openzeppelin/contracts/utils/introspection/IERC165.sol";
 
 contract OptimismMintableTokenFactory_Test is Bridge_Initializer {
     event Mint(address indexed _account, uint256 _amount);
@@ -63,5 +65,25 @@ contract OptimismMintableTokenFactory_Test is Bridge_Initializer {
         vm.expectRevert("Only L2 Bridge can mint and burn");
         vm.prank(address(alice));
         L2Token.burn(alice, 100);
+    }
+
+    function test_erc165_supportsInterface() external {
+        // The assertEq calls in this test are comparing the manual calculation of the iface,
+        // with what is returned by the solidity's type().interfaceId, just to be safe.
+        bytes4 iface1 = bytes4(keccak256("supportsInterface(bytes4)"));
+        assertEq(iface1, type(IERC165).interfaceId);
+        assert(L2Token.supportsInterface(iface1));
+        emit log_bytes32(bytes32(iface1));
+
+        bytes4 iface2 = L2Token.l1Token.selector ^ L2Token.mint.selector ^ L2Token.burn.selector;
+        assertEq(iface2, type(L1TokenId).interfaceId);
+        assert(L2Token.supportsInterface(iface2));
+        emit log_bytes32(bytes32(iface2));
+
+        bytes4 iface3 = L2Token.remoteToken.selector ^ L2Token.mint.selector ^ L2Token.burn.selector;
+        assertEq(iface3, type(RemoteTokenId).interfaceId);
+        assert(L2Token.supportsInterface(iface3));
+        emit log_bytes32(bytes32(iface3));
+
     }
 }

packages/contracts/contracts/universal/StandardBridge.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.9;
 
 /* Interface Imports */
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "./SupportedInterfaces.sol";
 
 /* Library Imports */
 import { ERC165Checker } from "@openzeppelin/contracts/utils/introspection/ERC165Checker.sol";
@@ -374,8 +375,7 @@ abstract contract StandardBridge {
      * @return True if the token is an OptimismMintableERC20.
      */
     function _isOptimismMintableERC20(address _token) internal view returns (bool) {
-        // 0x1d1d8b63 is mint ^ burn ^ l1Token
-        return ERC165Checker.supportsInterface(_token, 0x1d1d8b63);
+        return ERC165Checker.supportsInterface(_token, type(L1TokenId).interfaceId);
     }
 
     /**

packages/contracts/contracts/universal/SupportedInterfaces.sol
@@ -1,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+// Import this here to make it available just by importing this file
+import { IERC165 } from "@openzeppelin/contracts/utils/introspection/IERC165.sol";
+
+interface RemoteTokenId {
+    function mint(address _to, uint256 _amount) external virtual;
+
+    function burn(address _from, uint256 _amount) external virtual;
+
+    function remoteToken() external virtual;
+}
+
+interface L1TokenId {
+    function mint(address _to, uint256 _amount) external virtual;
+
+    function burn(address _from, uint256 _amount) external virtual;
+
+    function l1Token() external virtual;
+}

ethereum-optimism/optimism

packages/contracts-bedrock/contracts/test/OptimismMintableERC20.t.sol
@@ -3,6 +3,8 @@ pragma solidity 0.8.10;
 
 import { Bridge_Initializer } from "./CommonTest.t.sol";
 import { LibRLP } from "./Lib_RLP.t.sol";
+import "../universal/SupportedInterfaces.sol";
+import { IERC165 } from "@openzeppelin/contracts/utils/introspection/IERC165.sol";
 
 contract OptimismMintableERC20_Test is Bridge_Initializer {
     event Mint(address indexed _account, uint256 _amount);
@@ -63,5 +65,25 @@ contract OptimismMintableERC20_Test is Bridge_Initializer {
         vm.expectRevert("Only L2 Bridge can mint and burn");
         vm.prank(address(alice));
         L2Token.burn(alice, 100);
+    }
+
+    function test_erc165_supportsInterface() external {
+        // The assertEq calls in this test are comparing the manual calculation of the iface,
+        // with what is returned by the solidity's type().interfaceId, just to be safe.
+        bytes4 iface1 = bytes4(keccak256("supportsInterface(bytes4)"));
+        assertEq(iface1, type(IERC165).interfaceId);
+        assert(L2Token.supportsInterface(iface1));
+        emit log_bytes32(bytes32(iface1));
+
+        bytes4 iface2 = L2Token.l1Token.selector ^ L2Token.mint.selector ^ L2Token.burn.selector;
+        assertEq(iface2, type(IL1Token).interfaceId);
+        assert(L2Token.supportsInterface(iface2));
+        emit log_bytes32(bytes32(iface2));
+
+        bytes4 iface3 = L2Token.remoteToken.selector ^ L2Token.mint.selector ^ L2Token.burn.selector;
+        assertEq(iface3, type(IRemoteToken).interfaceId);
+        assert(L2Token.supportsInterface(iface3));
+        emit log_bytes32(bytes32(iface3));
+
     }
 }

packages/contracts-bedrock/contracts/universal/StandardBridge.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.9;
 
 /* Interface Imports */
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "./SupportedInterfaces.sol";
 
 /* Library Imports */
 import { ERC165Checker } from "@openzeppelin/contracts/utils/introspection/ERC165Checker.sol";
@@ -377,8 +378,7 @@ abstract contract StandardBridge {
      * @return True if the token is an OptimismMintableERC20.
      */
     function _isOptimismMintableERC20(address _token) internal view returns (bool) {
-        // 0x1d1d8b63 is mint ^ burn ^ l1Token
-        return ERC165Checker.supportsInterface(_token, 0x1d1d8b63);
+        return ERC165Checker.supportsInterface(_token, type(IL1Token).interfaceId);
     }
 
     /**

packages/contracts-bedrock/contracts/universal/SupportedInterfaces.sol
@@ -1,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+// Import this here to make it available just by importing this file
+import { IERC165 } from "@openzeppelin/contracts/utils/introspection/IERC165.sol";
+
+interface IRemoteToken {
+    function mint(address _to, uint256 _amount) external virtual;
+
+    function burn(address _from, uint256 _amount) external virtual;
+
+    function remoteToken() external virtual;
+}
+
+interface IL1Token {
+    function mint(address _to, uint256 _amount) external virtual;
+
+    function burn(address _from, uint256 _amount) external virtual;
+
+    function l1Token() external virtual;
+}