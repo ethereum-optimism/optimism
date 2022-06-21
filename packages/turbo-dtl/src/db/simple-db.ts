f9bd3b87344a4372963ff359e384f92aa783179e
Kelvin Fichter <kelvin@optimism.io>
2022-06-21 15:40:27 -0400

    wip

--- packages/turbo-dtl/src/db/simple-db.ts
+++ packages/turbo-dtl/src/db/simple-db.ts
@@ -1,9 +1,9 @@
 import { BigNumber, BigNumberish } from 'ethers'
 import { Level } from 'level'
 
-import { ErrEntryInconsistency } from './errors'
+import { ErrEntryInconsistency } from '../errors'
 
-type IndexLike = BigNumberish | 'latest'
+export type IndexLike = BigNumberish | 'latest'
 
 export const makeKey = (
   key: number,
@@ -37,58 +37,37 @@ export class SimpleDB {
     }
   }
 
-  public async range(
-    key: number,
-    start: BigNumberish,
-    end: BigNumberish
-  ): Promise<any[]> {
-    try {
-      return new Promise<any[]>((resolve) => {
-        const entries: any[] = []
-        this.db
-          .createValueStream({
-            gte: makeKey(key, start),
-            lt: makeKey(key, end),
-          })
-          .on('data', (transaction: string) => {
-            entries.push(JSON.parse(transaction))
-          })
-          .on('error', () => {
-            resolve(null)
-          })
-          .on('close', () => {
-            resolve(entries)
-          })
-          .on('end', () => {
-            resolve(entries)
-          })
-      })
-    } catch (err) {
-      return null
-    }
-  }
-
   public async put(key: number, index: IndexLike, value: any): Promise<void> {
-    if (index !== 'latest') {
-      index = BigNumber.from(index).toNumber()
-      const latest = await this.get(key, 'latest')
-      console.log(index, latest)
-      if (index > 0 && (latest === null || latest.index < index - 1)) {
-        throw ErrEntryInconsistency
-      }
-    }
-
-    await this.db.batch([
-      {
-        type: 'put',
-        key: makeKey(key, index),
-        value: JSON.stringify(value),
-      },
-      {
+    const batch = []
+    if (index === 'latest') {
+      batch.push({
         type: 'put',
         key: makeKey(key, 'latest'),
         value: JSON.stringify(value),
-      },
-    ])
+      })
+    } else {
+      index = BigNumber.from(index).toNumber()
+      const latest = await this.get(key, 'latest')
+
+      if (index > 0 && (latest === null || latest.index < index - 1)) {
+        throw ErrEntryInconsistency
+      }
+
+      batch.push({
+        type: 'put',
+        key: makeKey(key, index),
+        value: JSON.stringify(value),
+      })
+
+      if (latest === null || index > latest.index) {
+        batch.push({
+          type: 'put',
+          key: makeKey(key, 'latest'),
+          value: JSON.stringify(value),
+        })
+      }
+    }
+
+    await this.db.batch(batch)
   }
 }
