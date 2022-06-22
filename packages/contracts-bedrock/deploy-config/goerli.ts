8f4303c870c5663bb1f3939d3086f3c109f15487
Mark Tyneway <mark.tyneway@gmail.com>
2022-06-22 12:28:09 -0700

    contracts-bedrock: deploy config

--- packages/contracts-bedrock/deploy-config/goerli.ts
+++ packages/contracts-bedrock/deploy-config/goerli.ts
@@ -5,7 +5,7 @@ const config = {
   l2BlockTime: 2,
   genesisOutput: ethers.constants.HashZero,
   historicalBlocks: 0,
-  startingBlockTimestamp: 1652907966,
+  startingTimestamp: 1652907966,
   sequencerAddress: '0x7431310e026B69BFC676C0013E12A1A11411EEc9',
 }
 
