ebfc3ae7f9091dd44aac61e7b6cfe63a3b64707f
Maurelian <maurelian@protonmail.ch>
2022-06-22 16:27:53 -0400

    bedrock: Generate unique deployer key in tests

--- op-e2e/setup.go
+++ op-e2e/setup.go
@@ -402,7 +402,7 @@ func (cfg SystemConfig) start() (*System, error) {
 	// Deploy Deposit Contract
 	deployerPrivKey, err := sys.wallet.PrivateKey(accounts.Account{
 		URL: accounts.URL{
-			Path: sys.cfg.DeployerHDPath,
+			Path: cfg.DeployerHDPath,
 		},
 	})
 	if err != nil {
@@ -425,7 +425,7 @@ func (cfg SystemConfig) start() (*System, error) {
 		sys.cfg.L2OOCfg.L2StartingTimeStamp,
 		sys.cfg.L2OOCfg.L2BlockTime,
 		l2OutputSubmitterAddr,
-		l2OutputSubmitterAddr, // todo: what account should this be?
+		crypto.PubkeyToAddress(deployerPrivKey.PublicKey),
 	)
 	sys.cfg.DepositCFG.L2Oracle = sys.L2OOContractAddr
 	if err != nil {
