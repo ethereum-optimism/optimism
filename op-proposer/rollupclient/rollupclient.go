91ec89bb3e4abe960139682009813f003b6b266e
protolambda <proto@protolambda.com>
2022-06-30 00:40:23 +0200

    op-proposer: extend RPC client bindings with sync status and version endpoint

--- op-proposer/rollupclient/rollupclient.go
+++ op-proposer/rollupclient/rollupclient.go
@@ -2,6 +2,7 @@ package rollupclient
 
 import (
 	"context"
+	"github.com/ethereum-optimism/optimism/op-node/rollup/driver"
 	"math/big"
 
 	"github.com/ethereum-optimism/optimism/op-node/eth"
@@ -20,5 +21,17 @@ func NewRollupClient(rpc *rpc.Client) *RollupClient {
 func (r *RollupClient) OutputAtBlock(ctx context.Context, blockNum *big.Int) ([]eth.Bytes32, error) {
 	var output []eth.Bytes32
 	err := r.rpc.CallContext(ctx, &output, "optimism_outputAtBlock", hexutil.EncodeBig(blockNum))
+	return output, err
+}
+
+func (r *RollupClient) SyncStatus(ctx context.Context) (*driver.SyncStatus, error) {
+	var output *driver.SyncStatus
+	err := r.rpc.CallContext(ctx, &output, "optimism_syncStatus")
+	return output, err
+}
+
+func (r *RollupClient) Version(ctx context.Context) (string, error) {
+	var output string
+	err := r.rpc.CallContext(ctx, &output, "optimism_version")
 	return output, err
 }
