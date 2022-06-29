4d59bf048c5020aced1502b524dba26a16d60f8b
smartcontracts <kelvin@optimism.io>
2022-06-29 11:58:55 -0400

    feat(ctp): install kovan faucet drip (#2884)
    Installs a new Drippie drip to fund Tony's kovan faucet.

--- packages/contracts-periphery/config/drippie/optimism-kovan.ts
+++ packages/contracts-periphery/config/drippie/optimism-kovan.ts
@@ -3,34 +3,17 @@ import { ethers } from 'ethers'
 import { DrippieConfig, Time } from '../../src'
 
 const config: DrippieConfig = {
-  TeleportrWithdrawal: {
-    interval: 10 * Time.MINUTE,
-    dripcheck: 'CheckBalanceHigh',
-    checkparams: {
-      target: '0x4821975ca220601c153d02353300d6ad34adc362',
-      threshold: ethers.utils.parseEther('1'),
-    },
-    actions: [
-      {
-        target: '0x78A25524D90E3D0596558fb43789bD800a5c3007',
-        data: {
-          fn: 'withdrawFromTeleportr',
-          args: [],
-        },
-      },
-    ],
-  },
   GelatoBalance: {
     interval: 1 * Time.DAY,
     dripcheck: 'CheckGelatoLow',
     checkparams: {
-      treasury: '0x340759c8346A1E6Ed92035FB8B6ec57cE1D82c2c',
+      treasury: '0x527a819db1eb0e34426297b03bae11F2f8B3A19E',
       recipient: '0xc37f6a6c4AB335E20d10F034B90386E2fb70bbF5',
       threshold: ethers.utils.parseEther('0.1'),
     },
     actions: [
       {
-        target: '0x340759c8346A1E6Ed92035FB8B6ec57cE1D82c2c',
+        target: '0x527a819db1eb0e34426297b03bae11F2f8B3A19E',
         value: ethers.utils.parseEther('1'),
         data: {
           fn: 'depositFunds',
@@ -43,6 +26,20 @@ const config: DrippieConfig = {
             ethers.utils.parseEther('1'),
           ],
         },
+      },
+    ],
+  },
+  TonyOptimismKovanFaucet: {
+    interval: 1 * Time.WEEK,
+    dripcheck: 'CheckBalanceLow',
+    checkparams: {
+      target: '0xa8019d6F7bC3008a0a708A422f223Ccb21b61eAD',
+      threshold: ethers.utils.parseEther('20'),
+    },
+    actions: [
+      {
+        target: '0xa8019d6F7bC3008a0a708A422f223Ccb21b61eAD',
+        value: ethers.utils.parseEther('100'),
       },
     ],
   },
