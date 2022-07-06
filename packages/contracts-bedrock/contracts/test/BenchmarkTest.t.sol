f3d5a35a0f73d672900d6eb061c40d89f1c8e553
Maurelian <maurelian@protonmail.ch>
2022-07-05 22:45:35 -0400

    test(ctb): Benchmark deposits at INITIAL_BASE_FEE

--- packages/contracts-bedrock/contracts/test/BenchmarkTest.t.sol
+++ packages/contracts-bedrock/contracts/test/BenchmarkTest.t.sol
@@ -2,16 +2,73 @@
 pragma solidity 0.8.10;
 
 /* Testing utilities */
+import { Test } from "forge-std/Test.sol";
+import { Vm } from "forge-std/Vm.sol";
 import "./CommonTest.t.sol";
 import { CrossDomainMessenger } from "../universal/CrossDomainMessenger.sol";
+import { ResourceMetering } from "../L1/ResourceMetering.sol";
+
+uint128 constant INITIAL_BASE_FEE = 1_000_000_000;
+
+// Free function for setting the prevBaseFee param in the OptimismPortal.
+function setPrevBaseFee(
+    Vm _vm,
+    address _op,
+    uint128 _prevBaseFee
+) {
+    _vm.store(
+        address(_op),
+        bytes32(uint256(1)),
+        bytes32(
+            abi.encode(
+                ResourceMetering.ResourceParams({
+                    prevBaseFee: _prevBaseFee,
+                    prevBoughtGas: 0,
+                    prevBlockNum: uint64(block.number)
+                })
+            )
+        )
+    );
+}
 
 // Tests for obtaining pure gas cost estimates for commonly used functions.
 // The objective with these benchmarks is to strip down the actual test functions
 // so that they are nothing more than the call we want measure the gas cost of.
 // In order to achieve this we make no assertions, and handle everything else in the setUp()
 // function.
+contract GasBenchMark_OptimismPortal is Portal_Initializer {
+    function test_depositTransaction_benchmark() external {
+        op.depositTransaction{ value: NON_ZERO_VALUE }(
+            NON_ZERO_ADDRESS,
+            ZERO_VALUE,
+            NON_ZERO_GASLIMIT,
+            false,
+            NON_ZERO_DATA
+        );
+    }
+
+    function test_depositTransaction_benchmark_1() external {
+        setPrevBaseFee(vm, address(op), INITIAL_BASE_FEE);
+        op.depositTransaction{ value: NON_ZERO_VALUE }(
+            NON_ZERO_ADDRESS,
+            ZERO_VALUE,
+            NON_ZERO_GASLIMIT,
+            false,
+            NON_ZERO_DATA
+        );
+    }
+}
+
 contract GasBenchMark_L1CrossDomainMessenger is Messenger_Initializer {
-    function test_L1MessengerSendMessage_benchmark() external {
+    function test_L1MessengerSendMessage_benchmark_0() external {
+        // The amount of data typically sent during a bridge deposit.
+        bytes
+            memory data = hex"ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff";
+        L1Messenger.sendMessage(bob, data, uint32(100));
+    }
+
+    function test_L1MessengerSendMessage_benchmark_1() external {
+        setPrevBaseFee(vm, address(op), INITIAL_BASE_FEE);
         // The amount of data typically sent during a bridge deposit.
         bytes
             memory data = hex"ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff";
@@ -26,11 +83,21 @@ contract GasBenchMark_L1StandardBridge_Deposit is Bridge_Initializer {
         vm.startPrank(alice, alice);
     }
 
-    function test_depositETH_benchmark() external {
+    function test_depositETH_benchmark_0() external {
         L1Bridge.depositETH{ value: 500 }(50000, hex"");
     }
 
-    function test_depositERC20_benchmark() external {
+    function test_depositETH_benchmark_1() external {
+        setPrevBaseFee(vm, address(op), INITIAL_BASE_FEE);
+        L1Bridge.depositETH{ value: 500 }(50000, hex"");
+    }
+
+    function test_depositERC20_benchmark_0() external {
+        L1Bridge.depositETH{ value: 500 }(50000, hex"");
+    }
+
+    function test_depositERC20_benchmark_1() external {
+        setPrevBaseFee(vm, address(op), INITIAL_BASE_FEE);
         L1Bridge.depositETH{ value: 500 }(50000, hex"");
     }
 }
@@ -53,22 +120,6 @@ contract GasBenchMark_L1StandardBridge_Finalize is Bridge_Initializer {
         // the call coming from the messenger, which bypasses the portal
         // and oracle.
         L1Bridge.finalizeETHWithdrawal{ value: 100 }(alice, alice, 100, hex"");
-    }
-}
-
-contract GasBenchMark_OptimismPortal is Portal_Initializer {
-    function setUp() public override {
-        super.setUp();
-    }
-
-    function test_depositTransaction_benchmark() external {
-        op.depositTransaction{ value: NON_ZERO_VALUE }(
-            NON_ZERO_ADDRESS,
-            ZERO_VALUE,
-            NON_ZERO_GASLIMIT,
-            false,
-            NON_ZERO_DATA
-        );
     }
 }
 
