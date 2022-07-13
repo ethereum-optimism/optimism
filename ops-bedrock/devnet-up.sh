f7e13566915cc39cbc3c5a8cff654db5b00004e9
Joshua Gutow <jgutow@optimism.io>
2022-07-13 13:08:48 -0700

    devnet: Use later L2 timestamp for network

--- ops-bedrock/devnet-up.sh
+++ ops-bedrock/devnet-up.sh
@@ -60,7 +60,7 @@ mkdir -p ./.devnet
 if [ ! -f ./.devnet/rollup.json ]; then
     GENESIS_TIMESTAMP=$(date +%s | xargs printf "0x%x")
 else
-    GENESIS_TIMESTAMP=$(jq '.genesis.l2_time' < .devnet/rollup.json)
+    GENESIS_TIMESTAMP=$(jq '.timestamp' < .devnet/genesis-l1.json)
 fi
 
 # Regenerate the L1 genesis file if necessary. The existence of the genesis
