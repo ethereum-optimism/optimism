122a43fcf52178778102d6e2eb940197b8ee82f1
Mark Tyneway <mark.tyneway@gmail.com>
2022-07-13 13:34:39 -0700

    contracts-bedrock: differential fuzzing

--- packages/contracts-bedrock/contracts/test/Encoding.t.sol
+++ packages/contracts-bedrock/contracts/test/Encoding.t.sol
@@ -1,15 +1,67 @@
-//SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
 pragma solidity 0.8.10;
 
 import { CommonTest } from "./CommonTest.t.sol";
 import { Encoding } from "../libraries/Encoding.sol";
 
 contract Encoding_Test is CommonTest {
+    function setUp() external {
+        _setUp();
+    }
+
     function test_nonceVersioning(uint240 _nonce, uint16 _version) external {
         (uint240 nonce, uint16 version) = Encoding.decodeVersionedNonce(
             Encoding.encodeVersionedNonce(_nonce, _version)
         );
         assertEq(version, _version);
         assertEq(nonce, _nonce);
+    }
+
+    function test_decodeVersionedNonce_differential(uint240 _nonce, uint16 _version) external {
+        uint256 nonce = uint256(Encoding.encodeVersionedNonce(_nonce, _version));
+        (uint256 decodedNonce, uint256 decodedVersion) = ffi.decodeVersionedNonce(nonce);
+
+        assertEq(
+            _version,
+            uint16(decodedVersion)
+        );
+
+        assertEq(
+            _nonce,
+            uint240(decodedNonce)
+        );
+    }
+
+    function test_encodeCrossDomainMessage_differential(
+        uint240 _nonce,
+        uint8 _version,
+        address _sender,
+        address _target,
+        uint256 _value,
+        uint256 _gasLimit,
+        bytes memory _data
+    ) external {
+        uint8 version = _version % 2;
+        uint256 nonce = Encoding.encodeVersionedNonce(_nonce, version);
+
+        bytes memory encoding = Encoding.encodeCrossDomainMessage(
+            nonce,
+            _sender,
+            _target,
+            _value,
+            _gasLimit,
+            _data
+        );
+
+        bytes memory _encoding = ffi.encodeCrossDomainMessage(
+            nonce,
+            _sender,
+            _target,
+            _value,
+            _gasLimit,
+            _data
+        );
+
+        assertEq(encoding, _encoding);
     }
 }
