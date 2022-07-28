296c725ca479aae83da1757c7bc969a8e2f851e9
Mark Tyneway <mark.tyneway@gmail.com>
2022-07-28 14:38:41 -0700

    ci: add immutables check to ci

--- packages/contracts-bedrock/hardhat.config.ts
+++ packages/contracts-bedrock/hardhat.config.ts
@@ -15,6 +15,7 @@ import './tasks/deposits'
 import './tasks/rekey'
 import './tasks/rollup-config'
 import './tasks/check-op-node'
+import './tasks/check-l2-config'
 
 subtask(TASK_COMPILE_SOLIDITY_GET_SOURCE_PATHS).setAction(
   async (_, __, runSuper) => {
