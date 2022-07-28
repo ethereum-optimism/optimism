ab619e25e36e39c92783c78583221b4477ea4f9c
Maurelian <maurelian@protonmail.ch>
2022-07-28 12:50:34 -0400

    ctb: Naming and comments to clarify interfaces usage
    The OptimismMintableERC20 token used two interfaces in its
    supportsInterface() function. These were confusingly named
    after the functions that they contain (ie. IL1Token, IRemoteToken),
    rather than the contracts these interfaces represent.
    The name has been tweaked slighly, and comments have been added to
    clarify the intent.
    
    fix(ctb): Clearer names for the mintable token iface

--- packages/contracts-bedrock/contracts/universal/OptimismMintableERC20.sol
+++ packages/contracts-bedrock/contracts/universal/OptimismMintableERC20.sol
@@ -2,7 +2,7 @@
 pragma solidity 0.8.15;
 
 import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "./SupportedInterfaces.sol";
+import { IERC165, ILegacyMintableERC20, IOptimismMintableERC20 } from "./SupportedInterfaces.sol";
 
 /**
  * @title OptimismMintableERC20
@@ -12,7 +12,7 @@ import "./SupportedInterfaces.sol";
  *         Designed to be backwards compatible with the older StandardL2ERC20 token which was only
  *         meant for use on L2.
  */
-contract OptimismMintableERC20 is ERC20 {
+contract OptimismMintableERC20 is IOptimismMintableERC20, ERC20 {
     /**
      * @notice Address of the corresponding version of this token on the remote chain.
      */
@@ -94,8 +94,10 @@ contract OptimismMintableERC20 is ERC20 {
      */
     function supportsInterface(bytes4 _interfaceId) external pure returns (bool) {
         bytes4 iface1 = type(IERC165).interfaceId;
-        bytes4 iface2 = type(IL1Token).interfaceId;
-        bytes4 iface3 = type(IRemoteToken).interfaceId;
+        // Interface corresponding to the legacy L2StandardERC20.
+        bytes4 iface2 = type(ILegacyMintableERC20).interfaceId;
+        // Interface corresponding to the updated OptimismMintableERC20 (this contract).
+        bytes4 iface3 = type(IOptimismMintableERC20).interfaceId;
         return _interfaceId == iface1 || _interfaceId == iface2 || _interfaceId == iface3;
     }
 
