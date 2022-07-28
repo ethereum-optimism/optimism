0df744f6c642b97d7ce54f00eeaa5cf15b1d4aa9
Mark Tyneway <mark.tyneway@gmail.com>
2022-07-28 14:12:15 -0700

    core-utils,contracts-bedrock,ci: OpNodeProvider + check
    Implements a simple `OpNodeProvider` that can be used
    to query the op-node. It is used as part of a ci check
    against the devnet. It should add some more health related
    things, now it just prints of rpc responses. This will
    help to debug things.

--- packages/contracts-bedrock/hardhat.config.ts
+++ packages/contracts-bedrock/hardhat.config.ts
@@ -14,6 +14,7 @@ import './tasks/genesis-l2'
 import './tasks/deposits'
 import './tasks/rekey'
 import './tasks/rollup-config'
+import './tasks/check-op-node'
 
 subtask(TASK_COMPILE_SOLIDITY_GET_SOURCE_PATHS).setAction(
   async (_, __, runSuper) => {
