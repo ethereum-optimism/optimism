e195291302fd9d3a3579edc87fb19c85aba7c867
Mark Tyneway <mark.tyneway@gmail.com>
2022-08-03 11:11:44 -0700

    review

--- packages/contracts-bedrock/contracts/L1/OptimismSystemConfig.sol
+++ packages/contracts-bedrock/contracts/L1/OptimismSystemConfig.sol
@@ -3,23 +3,33 @@ pragma solidity 0.8.15;
 
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 import { Semver } from "../universal/Semver.sol";
+import { L2OutputOracle } from "./L2OutputOracle.sol";
 
 contract OptimismSystemConfig is Ownable, Semver {
 
+    L2OutputOracle immutable public l2OutputOracle;
     address public unsafeBlockSigner;
 
     event SetBatcher(address indexed batcher);
     event SetUnsafeBlockSigner(address indexed previous, address indexed next);
 
-    constructor(address _unsafeBlockSigner) Semver(0, 0, 1) {
+    constructor(
+        L2OutputOracle _l2OutputOracle,
+        address _unsafeBlockSigner
+    ) Semver(0, 0, 1) {
         unsafeBlockSigner = _unsafeBlockSigner;
+        l2OutputOracle = _l2OutputOracle;
     }
 
-    function setBatcher(address batcher) onlyOwner public {
+    function safeBlockSigner() external view returns (address) {
+        return l2OutputOracle.proposer();
+    }
+
+    function setBatcher(address batcher) onlyOwner external {
         emit SetBatcher(batcher);
     }
 
-    function setUnsafeBlockSigner(address _unsafeBlockSigner) public {
+    function setUnsafeBlockSigner(address _unsafeBlockSigner) onlyOwner external {
         address prev = unsafeBlockSigner;
         unsafeBlockSigner = _unsafeBlockSigner;
         emit SetUnsafeBlockSigner(prev, _unsafeBlockSigner);
