19da50dfc7a9cecf52f2cc9b86cde7065310bcd2
Javed Khan <javed@optimism.io>
2022-08-06 09:06:25 +0530

    pkg driver: use derive error types to optionally retry with backoff (#3130)
    * pkg derive: wrap errors by severity
    
    * pkg derive: wrap errors by severity
    
    * pkg derive: fix call with 2 return values
    
    * ci: fix golangci-lint warnings
    
    * ci: fix attributes test
    
    * pkg derive: update tests, restore err types
    
    * driver: update to use err severity from derive
    
    * op-node: fix retry callers
    
    * pkg derive: reimplement the custom error type
    
    * pkg derive: error - nits
    
    * pkg driver: update Step in event loop to backoff
    
    * pkg derive: match epoch hash with l1 block hash
    
    * pkg driver: handle backoff error
    
    * pkg driver: handle temporary errs with backoff
    
    also include additional if / else statements to log errors
    
    * op-node: req next step, with backoff, without blocking other events
    
    * pkg derive: update error types
    
    * pkg derive: typo
    
    Co-authored-by: protolambda <proto@protolambda.com>

--- op-node/rollup/derive/l1_retrieval.go
+++ op-node/rollup/derive/l1_retrieval.go
@@ -2,6 +2,7 @@ package derive
 
 import (
 	"context"
+	"fmt"
 	"io"
 
 	"github.com/ethereum-optimism/optimism/op-node/eth"
@@ -65,8 +66,10 @@ func (l1r *L1Retrieval) Step(ctx context.Context, outer Progress) error {
 	if l1r.datas == nil {
 		datas, err := l1r.dataSrc.OpenData(ctx, l1r.progress.Origin.ID())
 		if err != nil {
-			l1r.log.Error("can't fetch L1 data", "origin", l1r.progress.Origin, "err", err)
-			return nil
+			return NewTemporaryError(
+				err,
+				fmt.Sprintf("can't fetch L1 data: %v, %v", l1r.progress.Origin, err),
+			)
 		}
 		l1r.datas = datas
 		return nil
