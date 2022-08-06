4545496bfda6c9bd0f96d30ec7ea69e46916f6f4
Javed Khan <javed@optimism.io>
2022-08-06 09:06:25 +0530

    pkg driver: use derive error types to optionally retry with backoff (#3130)
    * pkg derive: wrap errors by severity
    
    * pkg derive: wrap errors by severity
    
    * pkg derive: fix call with 2 return values
    
    * ci: fix golangci-lint warnings
    
    * ci: fix attributes test
    
    * pkg derive: update tests, restore err types
    
    * driver: update to use err severity from derive
    
    * op-node: fix retry callers
    
    * pkg derive: reimplement the custom error type
    
    * pkg derive: error - nits
    
    * pkg driver: update Step in event loop to backoff
    
    * pkg derive: match epoch hash with l1 block hash
    
    * pkg driver: handle backoff error
    
    * pkg driver: handle temporary errs with backoff
    
    also include additional if / else statements to log errors
    
    * op-node: req next step, with backoff, without blocking other events
    
    * pkg derive: update error types
    
    * pkg derive: typo
    
    Co-authored-by: protolambda <proto@protolambda.com>

---
+++ op-node/rollup/derive/error.go
@@ -0,0 +1,94 @@
+package derive
+
+import (
+	"fmt"
+)
+
+// Level is the severity level of the error.
+type Level uint
+
+// There are three levels currently, out of which only 2 are being used
+// to classify error by severity. LevelTemporary
+const (
+	// LevelTemporary is a temporary error for example due to an RPC or
+	// connection issue, and can be safely ignored and retried by the caller
+	LevelTemporary Level = iota
+	// LevelReset is a pipeline reset error. It must be treated like a reorg.
+	LevelReset
+	// LevelCritical is a critical error.
+	LevelCritical
+)
+
+// Error is a wrapper for error, description and a severity level.
+type Error struct {
+	err   error
+	desc  string
+	level Level
+}
+
+// Error satisfies the error interface.
+func (e Error) Error() string {
+	if e.err != nil {
+		return fmt.Errorf("%w: %s", e.err, e.desc).Error()
+	}
+	return e.desc
+}
+
+// Unwrap satisfies the Is/As interface.
+func (e Error) Unwrap() error {
+	return e.err
+}
+
+// Is satisfies the error Unwrap interface.
+func (e Error) Is(target error) bool {
+	if target == nil {
+		return e == target
+	}
+	err, ok := target.(Error)
+	if !ok {
+		return false
+	}
+	return e.level == err.level
+}
+
+// NewError returns a custom Error.
+func NewError(err error, desc string, level Level) error {
+	return Error{
+		err:   err,
+		desc:  desc,
+		level: level,
+	}
+}
+
+// NewTemporaryError returns a temporary error.
+func NewTemporaryError(err error, desc string) error {
+	return NewError(
+		err,
+		desc,
+		LevelTemporary,
+	)
+}
+
+// NewResetError returns a pipeline reset error.
+func NewResetError(err error, desc string) error {
+	return NewError(
+		err,
+		desc,
+		LevelReset,
+	)
+}
+
+// NewCriticalError returns a critical error.
+func NewCriticalError(err error, desc string) error {
+	return NewError(
+		err,
+		desc,
+		LevelCritical,
+	)
+}
+
+// Sentinel errors, use these to get the severity of errors by calling
+// errors.Is(err, ErrTemporary) for example.
+var ErrTemporary = NewTemporaryError(nil, "temporary error")
+var ErrReset = NewResetError(nil, "pipeline reset error")
+var ErrCritical = NewCriticalError(nil, "critical error")
