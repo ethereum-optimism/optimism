bbad39a4958dadfffa91fe6a99a8cf58c90fd801
Sam Goldman <sam@quixotic.io>
2022-08-11 15:25:22 -0700

    nft: N-10 Undocumented implicit approval requirements

--- packages/contracts-periphery/contracts/L1/L1ERC721Bridge.sol
+++ packages/contracts-periphery/contracts/L1/L1ERC721Bridge.sol
@@ -90,7 +90,9 @@ contract L1ERC721Bridge is Semver, CrossDomainEnabled, Initializable {
     }
 
     /**
-     * @notice Initiates a bridge of an NFT to the caller's account on L2.
+     * @notice Initiates a bridge of an NFT to the caller's account on L2. Note that the current
+     *         owner of the token on this chain must approve this contract to operate the NFT before
+     *         it can be bridged.
      *
      * @param _localToken  Address of the ERC721 on this domain.
      * @param _remoteToken Address of the ERC721 on the remote domain.
@@ -123,7 +125,9 @@ contract L1ERC721Bridge is Semver, CrossDomainEnabled, Initializable {
     }
 
     /**
-     * @notice Initiates a bridge of an NFT to some recipient's account on L2.
+     * @notice Initiates a bridge of an NFT to some recipient's account on L2. Note that the current
+     *         owner of the token on this chain must approve this contract to operate the NFT before
+     *         it can be bridged.
      *
      * @param _localToken  Address of the ERC721 on this domain.
      * @param _remoteToken Address of the ERC721 on the remote domain.
