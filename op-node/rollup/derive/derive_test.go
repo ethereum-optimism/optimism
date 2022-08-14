a0ac1abc3d73eeba4581f263daa99117f4f00f8a
Nathaniel Jensen <nathaniel@sigmaprime.io>
2022-08-14 22:23:57 +1000

    Add testing code, written by Michal.

---
+++ op-node/rollup/derive/derive_test.go
@@ -0,0 +1,44 @@
+package derive
+
+import (
+	"math/big"
+	"math/rand"
+	"testing"
+
+	"github.com/ethereum-optimism/optimism/op-node/testutils"
+	"github.com/ethereum/go-ethereum/common"
+)
+
+func TestMarshalBinary(t *testing.T) {
+	var a big.Int
+	a.Exp(big.NewInt(2), big.NewInt(256), nil) // 2**256
+	fee := &a
+
+	var hash []byte
+
+	l1 := L1BlockInfo{
+		Number:         0,
+		Time:           0,
+		BaseFee:        fee,
+		BlockHash:      common.BytesToHash(hash),
+		SequenceNumber: 0,
+	}
+
+	l1.MarshalBinary()
+}
+
+func TestMarshalDepositLogEvent(t *testing.T) {
+	var a big.Int
+	a.Exp(big.NewInt(2), big.NewInt(256), nil) // 2**256
+	big_val := &a
+
+	rng := rand.New(rand.NewSource(1234))
+	source := UserDepositSource{
+		L1BlockHash: testutils.RandomHash(rng),
+		LogIndex:    uint64(rng.Intn(10000)),
+	}
+	depInput := testutils.GenerateDeposit(source.SourceHash(), rng)
+	depInput.Value = big_val
+	depInput.Mint = big_val
+	MarshalDepositLogEvent(MockDepositContractAddr, depInput)
+}
