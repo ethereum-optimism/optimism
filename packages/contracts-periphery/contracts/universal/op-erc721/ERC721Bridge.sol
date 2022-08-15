4e9fe32722356a823c614eb331907cbc0ad19902
Sam Goldman <sam@quixotic.io>
2022-08-15 12:08:48 -0700

    nft: L-13 Duplicated code

---
+++ packages/contracts-periphery/contracts/universal/op-erc721/ERC721Bridge.sol
@@ -0,0 +1,172 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.15;
+
+import {
+    CrossDomainEnabled
+} from "@eth-optimism/contracts/contracts/libraries/bridge/CrossDomainEnabled.sol";
+import { Initializable } from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import { Address } from "@openzeppelin/contracts/utils/Address.sol";
+
+/**
+ * @title ERC721Bridge
+ * @notice ERC721Bridge is a base contract for the L1 and L2 ERC721 bridges.
+ */
+abstract contract ERC721Bridge is Initializable, CrossDomainEnabled {
+    /**
+     * @notice Emitted when an ERC721 bridge to the other network is initiated.
+     *
+     * @param localToken  Address of the token on this domain.
+     * @param remoteToken Address of the token on the remote domain.
+     * @param from        Address that initiated bridging action.
+     * @param to          Address to receive the token.
+     * @param tokenId     ID of the specific token deposited.
+     * @param extraData   Extra data for use on the client-side.
+     */
+    event ERC721BridgeInitiated(
+        address indexed localToken,
+        address indexed remoteToken,
+        address indexed from,
+        address to,
+        uint256 tokenId,
+        bytes extraData
+    );
+
+    /**
+     * @notice Emitted when an ERC721 bridge from the other network is finalized.
+     *
+     * @param localToken  Address of the token on this domain.
+     * @param remoteToken Address of the token on the remote domain.
+     * @param from        Address that initiated bridging action.
+     * @param to          Address to receive the token.
+     * @param tokenId     ID of the specific token deposited.
+     * @param extraData   Extra data for use on the client-side.
+     */
+    event ERC721BridgeFinalized(
+        address indexed localToken,
+        address indexed remoteToken,
+        address indexed from,
+        address to,
+        uint256 tokenId,
+        bytes extraData
+    );
+
+    /**
+     * @notice Emitted when an ERC721 bridge from the other network fails.
+     *
+     * @param localToken  Address of the token on this domain.
+     * @param remoteToken Address of the token on the remote domain.
+     * @param from        Address that initiated bridging action.
+     * @param to          Address to receive the token.
+     * @param tokenId     ID of the specific token deposited.
+     * @param extraData   Extra data for use on the client-side.
+     */
+    event ERC721BridgeFailed(
+        address indexed localToken,
+        address indexed remoteToken,
+        address indexed from,
+        address to,
+        uint256 tokenId,
+        bytes extraData
+    );
+
+    /**
+     * @notice Address of the bridge on the other network.
+     */
+    address public otherBridge;
+
+    /**
+     * @param _messenger   Address of the CrossDomainMessenger on this network.
+     * @param _otherBridge Address of the ERC721 bridge on the other network.
+     */
+    function initialize(address _messenger, address _otherBridge) public initializer {
+        messenger = _messenger;
+        otherBridge = _otherBridge;
+    }
+
+    /**
+     * @notice Initiates a bridge of an NFT to the caller's account on the other domain.
+     *
+     * @param _localToken  Address of the ERC721 on this domain.
+     * @param _remoteToken Address of the ERC721 on the remote domain.
+     * @param _tokenId     Token ID to bridge.
+     * @param _minGasLimit Minimum gas limit for the bridge message on the other domain.
+     * @param _extraData   Optional data to forward to the other domain. Data supplied here will
+     *                     not be used to execute any code on the other domain and is only emitted
+     *                     as extra data for the convenience of off-chain tooling.
+     */
+    function bridgeERC721(
+        address _localToken,
+        address _remoteToken,
+        uint256 _tokenId,
+        uint32 _minGasLimit,
+        bytes calldata _extraData
+    ) external {
+        // Modifier requiring sender to be EOA. This check could be bypassed by a malicious
+        // contract via initcode, but it takes care of the user error we want to avoid.
+        require(!Address.isContract(msg.sender), "ERC721Bridge: account is not externally owned");
+
+        _initiateBridgeERC721(
+            _localToken,
+            _remoteToken,
+            msg.sender,
+            msg.sender,
+            _tokenId,
+            _minGasLimit,
+            _extraData
+        );
+    }
+
+    /**
+     * @notice Initiates a bridge of an NFT to some recipient's account on the other domain.
+     *
+     * @param _localToken  Address of the ERC721 on this domain.
+     * @param _remoteToken Address of the ERC721 on the remote domain.
+     * @param _to          Address to receive the token on the other domain.
+     * @param _tokenId     Token ID to bridge.
+     * @param _minGasLimit Minimum gas limit for the bridge message on the other domain.
+     * @param _extraData   Optional data to forward to the other domain. Data supplied here will
+     *                     not be used to execute any code on the other domain and is only emitted
+     *                     as extra data for the convenience of off-chain tooling.
+     */
+    function bridgeERC721To(
+        address _localToken,
+        address _remoteToken,
+        address _to,
+        uint256 _tokenId,
+        uint32 _minGasLimit,
+        bytes calldata _extraData
+    ) external {
+        _initiateBridgeERC721(
+            _localToken,
+            _remoteToken,
+            msg.sender,
+            _to,
+            _tokenId,
+            _minGasLimit,
+            _extraData
+        );
+    }
+
+    /**
+     * @notice Internal function for initiating a token bridge to the other domain.
+     *
+     * @param _localToken  Address of the ERC721 on this domain.
+     * @param _remoteToken Address of the ERC721 on the remote domain.
+     * @param _from        Address of the sender on this domain.
+     * @param _to          Address to receive the token on the other domain.
+     * @param _tokenId     Token ID to bridge.
+     * @param _minGasLimit Minimum gas limit for the bridge message on the other domain.
+     * @param _extraData   Optional data to forward to the other domain. Data supplied here will
+     *                     not be used to execute any code on the other domain and is only emitted
+     *                     as extra data for the convenience of off-chain tooling.
+     */
+    function _initiateBridgeERC721(
+        address _localToken,
+        address _remoteToken,
+        address _from,
+        address _to,
+        uint256 _tokenId,
+        uint32 _minGasLimit,
+        bytes calldata _extraData
+    ) internal virtual;
+}
