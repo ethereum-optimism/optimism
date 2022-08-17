b5d477581b8c04a3aaf3faf9bc8b6f232bfcda7c
Mark Tyneway <mark.tyneway@gmail.com>
2022-08-17 14:29:58 -0700

    predeploys: fix address names
    Updates the address names and also create a
    mapping of addresses so that they can be iterated
    over.

--- op-bindings/predeploys/addresses.go
+++ op-bindings/predeploys/addresses.go
@@ -4,30 +4,47 @@ import "github.com/ethereum/go-ethereum/common"
 
 const (
 	L2ToL1MessagePasser          = "0x4200000000000000000000000000000000000000"
-	OVM_DeployerWhitelist        = "0x4200000000000000000000000000000000000002"
-	OVM_ETH                      = "0xDeadDeAddeAddEAddeadDEaDDEAdDeaDDeAD0000"
+	DeployerWhitelist            = "0x4200000000000000000000000000000000000002"
+	LegacyERC20ETH               = "0xDeadDeAddeAddEAddeadDEaDDEAdDeaDDeAD0000"
 	WETH9                        = "0x4200000000000000000000000000000000000006"
 	L2CrossDomainMessenger       = "0x4200000000000000000000000000000000000007"
 	L2StandardBridge             = "0x4200000000000000000000000000000000000010"
 	SequencerFeeVault            = "0x4200000000000000000000000000000000000011"
 	OptimismMintableERC20Factory = "0x4200000000000000000000000000000000000012"
 	L1BlockNumber                = "0x4200000000000000000000000000000000000013"
-	OVM_GasPriceOracle           = "0x420000000000000000000000000000000000000F"
+	GasPriceOracle               = "0x420000000000000000000000000000000000000F"
 	L1Block                      = "0x4200000000000000000000000000000000000015"
 	GovernanceToken              = "0x4200000000000000000000000000000000000042"
 )
 
 var (
 	L2ToL1MessagePasserAddr          = common.HexToAddress(L2ToL1MessagePasser)
-	OVM_DeployerWhitelistAddr        = common.HexToAddress(OVM_DeployerWhitelist)
-	OVM_ETHAddr                      = common.HexToAddress(OVM_ETH)
+	DeployerWhitelistAddr            = common.HexToAddress(DeployerWhitelist)
+	LegacyERC20ETHAddr               = common.HexToAddress(LegacyERC20ETH)
 	WETH9Addr                        = common.HexToAddress(WETH9)
 	L2CrossDomainMessengerAddr       = common.HexToAddress(L2CrossDomainMessenger)
 	L2StandardBridgeAddr             = common.HexToAddress(L2StandardBridge)
 	SequencerFeeVaultAddr            = common.HexToAddress(SequencerFeeVault)
 	OptimismMintableERC20FactoryAddr = common.HexToAddress(OptimismMintableERC20Factory)
 	L1BlockNumberAddr                = common.HexToAddress(L1BlockNumber)
-	OVM_GasPriceOracleAddr           = common.HexToAddress(OVM_GasPriceOracle)
+	GasPriceOracleAddr               = common.HexToAddress(GasPriceOracle)
 	L1BlockAddr                      = common.HexToAddress(L1Block)
 	GovernanceTokenAddr              = common.HexToAddress(GovernanceToken)
+
+	predeploys = make(map[string]*common.Address)
 )
+
+func init() {
+	predeploys["L2ToL1MessagePasser"] = &L2ToL1MessagePasserAddr
+	predeploys["DeployerWhitelist"] = &DeployerWhitelistAddr
+	predeploys["LegacyERC20ETH"] = &LegacyERC20ETHAddr
+	predeploys["WETH9"] = &WETH9Addr
+	predeploys["L2CrossDomainMessenger"] = &L2CrossDomainMessengerAddr
+	predeploys["L2StandardBridge"] = &L2StandardBridgeAddr
+	predeploys["SequencerFeeVault"] = &SequencerFeeVaultAddr
+	predeploys["OptimismMintableERC20Factory"] = &OptimismMintableERC20FactoryAddr
+	predeploys["L1BlockNumber"] = &L1BlockNumberAddr
+	predeploys["GasPriceOracle"] = &GasPriceOracleAddr
+	predeploys["L1Block"] = &L1BlockAddr
+	predeploys["GovernanceToken"] = &GovernanceTokenAddr
+}
