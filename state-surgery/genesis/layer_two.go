2d65f0d94bfc1ef035bb2ae1f13948b7fe439cbb
Mark Tyneway <mark.tyneway@gmail.com>
2022-08-19 16:15:11 -0700

    state-surgery: genesis block generation (#3264)
    * state-surgery: layer 2 genesis block generation
    
    Generate the L2 or L1 genesis block given a hardhat and a deploy config.
    
    * state-surgery: some comments
    
    * state-surgery: delete dead comment

---
+++ state-surgery/genesis/layer_two.go
@@ -0,0 +1,46 @@
+package genesis
+
+import (
+	"github.com/ethereum-optimism/optimism/state-surgery/hardhat"
+	"github.com/ethereum-optimism/optimism/state-surgery/state"
+
+	"github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/core"
+)
+
+// BuildOptimismGenesis
+func BuildOptimismGenesis(hh *hardhat.Hardhat, config *DeployConfig, chain ethereum.ChainReader) (*core.Genesis, error) {
+	genesis, err := NewL2Genesis(config, chain)
+	if err != nil {
+		return nil, err
+	}
+
+	db := state.NewMemoryStateDB(genesis)
+
+	if config.FundDevAccounts {
+		FundDevAccounts(db)
+	}
+
+	// TODO(tynes): need a function for clearing old, unused storage slots.
+	// Each deployed contract on L2 needs to have its existing storage
+	// inspected and then cleared if they are no longer used.
+
+	if err := SetProxies(hh, db); err != nil {
+		return nil, err
+	}
+
+	storage, err := NewStorageConfig(hh, config, chain)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := SetImplementations(hh, db, storage); err != nil {
+		return nil, err
+	}
+
+	if err := MigrateDepositHashes(hh, db); err != nil {
+		return nil, err
+	}
+
+	return db.Genesis(), nil
+}
---
+++ state-surgery/genesis/layer_two_test.go
@@ -0,0 +1,76 @@
+package genesis_test
+
+import (
+	"encoding/json"
+	"flag"
+	"io/ioutil"
+	"math/big"
+	"testing"
+
+	"github.com/ethereum/go-ethereum/accounts/abi/bind/backends"
+	"github.com/ethereum/go-ethereum/core"
+	"github.com/ethereum/go-ethereum/crypto"
+
+	"github.com/ethereum-optimism/optimism/op-bindings/predeploys"
+	"github.com/ethereum-optimism/optimism/state-surgery/genesis"
+	"github.com/ethereum-optimism/optimism/state-surgery/hardhat"
+	"github.com/stretchr/testify/require"
+)
+
+var writeFile bool
+
+func init() {
+	flag.BoolVar(&writeFile, "write-file", false, "write the genesis file to disk")
+}
+
+var testKey, _ = crypto.HexToECDSA("b71c71a67e1177ad4e901695e1b4b9ee17ae16c6668d313eac2f96dbcda3f291")
+
+func TestBuildOptimismGenesis(t *testing.T) {
+	hh, err := hardhat.New(
+		"goerli",
+		[]string{
+			"../../packages/contracts-bedrock/artifacts",
+			"../../packages/contracts-governance/artifacts",
+		},
+		[]string{"../../packages/contracts-bedrock/deployments"},
+	)
+	require.Nil(t, err)
+
+	config, err := genesis.NewDeployConfig("../../packages/contracts-bedrock/deploy-config/devnetL1.json")
+	require.Nil(t, err)
+
+	backend := backends.NewSimulatedBackend(
+		core.GenesisAlloc{
+			crypto.PubkeyToAddress(testKey.PublicKey): {Balance: big.NewInt(10000000000000000)},
+		},
+		15000000,
+	)
+
+	gen, err := genesis.BuildOptimismGenesis(hh, config, backend)
+	require.Nil(t, err)
+	require.NotNil(t, gen)
+
+	proxyAdmin, err := hh.GetDeployment("ProxyAdmin")
+	require.Nil(t, err)
+
+	for name, address := range predeploys.Predeploys {
+		addr := *address
+
+		account, ok := gen.Alloc[addr]
+		require.Equal(t, ok, true)
+		require.Greater(t, len(account.Code), 0)
+
+		if name == "GovernanceToken" || name == "LegacyERC20ETH" {
+			continue
+		}
+
+		adminSlot, ok := account.Storage[genesis.AdminSlot]
+		require.Equal(t, ok, true)
+		require.Equal(t, adminSlot, proxyAdmin.Address.Hash())
+	}
+
+	if writeFile {
+		file, _ := json.MarshalIndent(gen, "", " ")
+		_ = ioutil.WriteFile("genesis.json", file, 0644)
+	}
+}
