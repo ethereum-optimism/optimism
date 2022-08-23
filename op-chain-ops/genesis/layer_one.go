5421c9be08aa4ca71fbf86c5216694236a1c776c
Mark Tyneway <mark.tyneway@gmail.com>
2022-08-23 13:36:05 -0700

    op-chain-ops: some cleanup and modularization

--- op-chain-ops/genesis/layer_one.go
+++ op-chain-ops/genesis/layer_one.go
@@ -1,13 +1,15 @@
 package genesis
 
 import (
-	"github.com/ethereum-optimism/optimism/op-chain-ops/hardhat"
 	"github.com/ethereum-optimism/optimism/op-chain-ops/state"
 	"github.com/ethereum/go-ethereum/core"
 )
 
-// TODO(tynes): need bindings for all of the L1 contracts
-func BuildL1DeveloperGenesis(hh *hardhat.Hardhat, config *DeployConfig) (*core.Genesis, error) {
+// TODO(tynes): need bindings for all of the L1 contracts to be able
+// to create a genesis file with the L1 contracts predeployed.
+// This would speed up testing as deployments take time when
+// running tests.
+func BuildL1DeveloperGenesis(config *DeployConfig) (*core.Genesis, error) {
 	genesis, err := NewL1Genesis(config)
 	if err != nil {
 		return nil, err
@@ -16,6 +18,7 @@ func BuildL1DeveloperGenesis(hh *hardhat.Hardhat, config *DeployConfig) (*core.G
 	db := state.NewMemoryStateDB(genesis)
 
 	FundDevAccounts(db)
+	SetPrecompileBalances(db)
 
 	return db.Genesis(), nil
 }
