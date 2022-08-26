2414056a23b7d2052cb0152b30ede04fa3942d76
Javed Khan <javed@optimism.io>
2022-08-26 13:36:43 -0700

    indexer: use bedrock events for finalization

--- indexer/services/l1/bridge/eth_bridge.go
+++ indexer/services/l1/bridge/eth_bridge.go
@@ -50,6 +50,36 @@ func (e *EthBridge) GetDepositsByBlockRange(start, end uint64) (DepositsMap, err
 	return depositsByBlockhash, nil
 }
 
+func (s *EthBridge) GetWithdrawalsByBlockRange(start, end uint64) (WithdrawalsMap, error) {
+	withdrawalsByBlockHash := make(WithdrawalsMap)
+
+	iter, err := FilterETHWithdrawalFinalizedWithRetry(s.ctx, s.filterer, &bind.FilterOpts{
+		Start: start,
+		End:   &end,
+	})
+
+	if err != nil {
+		logger.Error("Error fetching filter", "err", err)
+	}
+
+	for iter.Next() {
+		withdrawalsByBlockHash[iter.Event.Raw.BlockHash] = append(
+			withdrawalsByBlockHash[iter.Event.Raw.BlockHash], db.Withdrawal{
+				TxHash:      iter.Event.Raw.TxHash,
+				FromAddress: iter.Event.From,
+				ToAddress:   iter.Event.To,
+				Amount:      iter.Event.Amount,
+				Data:        iter.Event.ExtraData,
+				LogIndex:    iter.Event.Raw.Index,
+			})
+	}
+	if err := iter.Error(); err != nil {
+		return nil, err
+	}
+
+	return withdrawalsByBlockHash, nil
+}
+
 func (e *EthBridge) String() string {
 	return e.name
 }
