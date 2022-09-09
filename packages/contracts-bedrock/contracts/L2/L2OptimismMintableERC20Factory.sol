0b19657f95faee70f721c1b72a0213dffde7bcce
Mark Tyneway <mark.tyneway@gmail.com>
2022-09-09 05:55:42 -0700

    contracts-bedrock: fix natspec

--- packages/contracts-bedrock/contracts/L2/L2OptimismMintableERC20Factory.sol
+++ packages/contracts-bedrock/contracts/L2/L2OptimismMintableERC20Factory.sol
@@ -7,11 +7,11 @@ import { Predeploys } from "../libraries/Predeploys.sol";
 /**
  * @custom:proxied
  * @custom:predeployed 0x4200000000000000000000000000000000000012
- * @title OptimismMintableERC20Factory
- * @notice OptimismMintableERC20Factory is a factory contract that generates OptimismMintableERC20
- *         contracts on the network it's deployed to. Simplifies the deployment process for users
- *         who may be less familiar with deploying smart contracts. Designed to be backwards
- *         compatible with the older StandardL2ERC20Factory contract.
+ * @title L2OptimismMintableERC20Factory
+ * @notice L2OptimismMintableERC20Factory is a factory contract that generates OptimismMintableERC20
+ *         contracts on L2 that allows for deposits of L1 native tokens. Simplifies the deployment
+ *         process for users who may be less familiar with deploying smart contracts. Designed to
+           be backwards compatible with the legacy StandardL2ERC20Factory contract.
  */
 contract L2OptimismMintableERC20Factory is OptimismMintableERC20Factory {
     constructor() OptimismMintableERC20Factory(Predeploys.L2_STANDARD_BRIDGE) {}
