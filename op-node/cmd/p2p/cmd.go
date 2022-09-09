4e7f3d3c769e27e399c2f5b441639e9f8a0e4ef7
Matthew Slipper <me@matthewslipper.com>
2022-09-09 12:08:54 +0200

    Add forgotten err to Pub2PeerID

--- op-node/cmd/p2p/cmd.go
+++ op-node/cmd/p2p/cmd.go
@@ -34,7 +34,7 @@ func Priv2PeerID(r io.Reader) (string, error) {
 func Pub2PeerID(r io.Reader) (string, error) {
 	b, err := readHexData(r)
 	if err != nil {
-		return "", nil
+		return "", err
 	}
 
 	p, err := crypto.UnmarshalSecp256k1PublicKey(b)
