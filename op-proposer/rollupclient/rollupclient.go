9c53d880903c13690f6a1dc1a08fb218b39c2e18
protolambda <proto@protolambda.com>
2022-09-09 10:55:43 +0200

    op-node,op-proposer: move rollup bindings into op-node to avoid unnecessary op-proposer dependency in tooling

--- op-proposer/rollupclient/rollupclient.go
+++ op-proposer/rollupclient/rollupclient.go
@@ -1,44 +1,14 @@
 package rollupclient
 
 import (
-	"context"
-	"math/big"
-
-	"github.com/ethereum-optimism/optimism/op-node/eth"
-	"github.com/ethereum-optimism/optimism/op-node/rollup"
-	"github.com/ethereum-optimism/optimism/op-node/rollup/driver"
-	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum-optimism/optimism/op-node/sources"
 	"github.com/ethereum/go-ethereum/rpc"
 )
 
-type RollupClient struct {
-	rpc *rpc.Client
-}
+// Deprecated: use sources.RollupClient instead
+type RollupClient = sources.RollupClient
 
-func NewRollupClient(rpc *rpc.Client) *RollupClient {
-	return &RollupClient{rpc}
-}
-
-func (r *RollupClient) OutputAtBlock(ctx context.Context, blockNum *big.Int) ([]eth.Bytes32, error) {
-	var output []eth.Bytes32
-	err := r.rpc.CallContext(ctx, &output, "optimism_outputAtBlock", hexutil.EncodeBig(blockNum))
-	return output, err
-}
-
-func (r *RollupClient) SyncStatus(ctx context.Context) (*driver.SyncStatus, error) {
-	var output *driver.SyncStatus
-	err := r.rpc.CallContext(ctx, &output, "optimism_syncStatus")
-	return output, err
-}
-
-func (r *RollupClient) RollupConfig(ctx context.Context) (*rollup.Config, error) {
-	var output *rollup.Config
-	err := r.rpc.CallContext(ctx, &output, "optimism_rollupConfig")
-	return output, err
-}
-
-func (r *RollupClient) Version(ctx context.Context) (string, error) {
-	var output string
-	err := r.rpc.CallContext(ctx, &output, "optimism_version")
-	return output, err
+// Deprecated: use sources.NewRollupClient instead
+func NewRollupClient(rpc *rpc.Client) *sources.RollupClient {
+	return sources.NewRollupClient(rpc)
 }
