4602af73f115df49c7b40ae13610700116acbd7f
Will Cory <willcory@Wills-MacBook-Pro.local>
2022-09-13 09:30:01 -0700

    export list too

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -1,3 +1,6 @@
+import optimismTokenList from './generated/tokenList.json'
+
+export { optimismTokenList }
 export * from './interfaces'
 export * from './utils'
 export * from './cross-chain-messenger'
