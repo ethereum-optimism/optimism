515685f44c1e6bbee211f5c9c51130f29922178f
smartcontracts <kelvin@optimism.io>
2022-09-16 01:48:58 +0200

    fix(ctb): use seaport style for gov token (#3467)
    Updates GovernanceToken to use seaport style comments. Minor fix, but
    might as well be consistent across the codebase.
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- packages/contracts-bedrock/contracts/L2/GovernanceToken.sol
+++ packages/contracts-bedrock/contracts/L2/GovernanceToken.sol
@@ -18,21 +18,21 @@ contract GovernanceToken is ERC20Burnable, ERC20Votes, Ownable {
     constructor() ERC20("Optimism", "OP") ERC20Permit("Optimism") {}
 
     /**
-     * @notice Allows the owner to mint tokens
+     * @notice Allows the owner to mint tokens.
      *
-     * @param _account The account receiving minted tokens
-     * @param _amount The amount of tokens to mint
+     * @param _account The account receiving minted tokens.
+     * @param _amount  The amount of tokens to mint.
      */
     function mint(address _account, uint256 _amount) public onlyOwner {
         _mint(_account, _amount);
     }
 
     /**
-     * @notice Callback called after a token transfer
+     * @notice Callback called after a token transfer.
      *
-     * @param from The account sending tokens
-     * @param to The account receiving tokens
-     * @param amount The amount of tokens being transfered
+     * @param from   The account sending tokens.
+     * @param to     The account receiving tokens.
+     * @param amount The amount of tokens being transfered.
      */
     function _afterTokenTransfer(
         address from,
@@ -43,20 +43,20 @@ contract GovernanceToken is ERC20Burnable, ERC20Votes, Ownable {
     }
 
     /**
-     * @notice Internal mint function
+     * @notice Internal mint function.
      *
-     * @param to The account receiving minted tokens
-     * @param amount The amount of tokens to mint
+     * @param to     The account receiving minted tokens.
+     * @param amount The amount of tokens to mint.
      */
     function _mint(address to, uint256 amount) internal override(ERC20, ERC20Votes) {
         super._mint(to, amount);
     }
 
     /**
-     * @notice Internal burn function
+     * @notice Internal burn function.
      *
-     * @param account The account that tokens will be burned from
-     * @param amount The amount of tokens that will be burned
+     * @param account The account that tokens will be burned from.
+     * @param amount  The amount of tokens that will be burned.
      */
     function _burn(address account, uint256 amount) internal override(ERC20, ERC20Votes) {
         super._burn(account, amount);
