a61265ff6eed377e4bbcc0710ebe216420f989ee
Maurelian <maurelian@protonmail.ch>
2022-09-16 15:17:34 -0400

    Various improvements to documentation in the smart contracts and deposit spec (#3440)
    * fix(ctb): M-03 Eth can be send to undeliverable recipient
    
    * fix(ctb): M-07  CrossDomainMessenger allows sending messages to unrelayable addresses"
    
    * fix(ctb): L-01 Improve deposit docs
    
    * fix(ctb): L-09 Misleading inline documentation
    
    * fix(ctb):  Standard Bridge does not support tokens with transfer fees
    
    * fix(ctb): N-07 Unexplained literal values

--- packages/contracts-bedrock/contracts/universal/CrossDomainMessenger.sol
+++ packages/contracts-bedrock/contracts/universal/CrossDomainMessenger.sol
@@ -107,9 +107,8 @@ abstract contract CrossDomainMessenger is
 
     /**
      * @notice Mapping of message hashes to boolean receipt values. Note that a message will only
-     *         be present in this mapping if it failed to be relayed on this chain at least once.
-     *         If a message is successfully relayed on the first attempt, then it will only be
-     *         present within the successfulMessages mapping.
+     *         be present in this mapping if it has successfully been relayed on this chain, and
+     *         can therefore not be relayed again.
      */
     mapping(bytes32 => bool) public successfulMessages;
 
@@ -200,7 +199,10 @@ abstract contract CrossDomainMessenger is
     }
 
     /**
-     * @notice Sends a message to some target address on the other chain.
+     * @notice Sends a message to some target address on the other chain. Note that if the call
+     *         always reverts, then the message will be unrelayable, and any ETH sent will be
+     *         permanently locked. The same will occur if the target on the other chain is
+     *         considered unsafe (see the _isUnsafeTarget() function).
      *
      * @param _target      Target contract or wallet address.
      * @param _message     Message to trigger the target address with.
