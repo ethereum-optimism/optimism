e30ee444da0239fc7819733de6d9c7edb4dd8b14
Matthew Slipper <me@matthewslipper.com>
2022-09-23 15:28:24 -0600

    op-node: Fix OPB-10
    Fixes OPB-10: No Checks for Deposit Transaction Type When Calling `L1InfoDepositTxData()`. This PR adds a check in `blockToBatch` that checks the type of the first transaction in the block, and returns an error if it isn't a deposit transaction.

---
+++ op-node/rollup/derive/channel_out_test.go
@@ -0,0 +1,26 @@
+package derive
+
+import (
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/stretchr/testify/require"
+	"math/big"
+	"testing"
+)
+
+func TestChannelOutAddBlock(t *testing.T) {
+	cout, err := NewChannelOut()
+	require.NoError(t, err)
+
+	t.Run("returns err if first tx is not an l1info tx", func(t *testing.T) {
+		header := &types.Header{Number: big.NewInt(1), Difficulty: big.NewInt(100)}
+		block := types.NewBlockWithHeader(header).WithBody(
+			[]*types.Transaction{
+				types.NewTx(&types.DynamicFeeTx{}),
+			},
+			nil,
+		)
+		err := cout.AddBlock(block)
+		require.Error(t, err)
+		require.Equal(t, ErrNotDepositTx, err)
+	})
+}
