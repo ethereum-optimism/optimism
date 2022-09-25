7c74449c1ae05489c08d718b07179c81c15cfa1c
Mark Tyneway <mark.tyneway@gmail.com>
2022-09-25 10:18:24 -0700

    predeploys: add constants
    - `L2ERC721Bridge`
    - `OptimismMintableERC721Factory`
    
    The `OptimismMintableERC721Factory` will be deployed regularly
    before bedrock and then will be included as a predeploy
    as part of the bedrock genesis creation.

--- op-bindings/predeploys/addresses.go
+++ op-bindings/predeploys/addresses.go
@@ -3,35 +3,39 @@ package predeploys
 import "github.com/ethereum/go-ethereum/common"
 
 const (
-	L2ToL1MessagePasser          = "0x4200000000000000000000000000000000000016"
-	DeployerWhitelist            = "0x4200000000000000000000000000000000000002"
-	LegacyERC20ETH               = "0xDeadDeAddeAddEAddeadDEaDDEAdDeaDDeAD0000"
-	WETH9                        = "0x4200000000000000000000000000000000000006"
-	L2CrossDomainMessenger       = "0x4200000000000000000000000000000000000007"
-	L2StandardBridge             = "0x4200000000000000000000000000000000000010"
-	SequencerFeeVault            = "0x4200000000000000000000000000000000000011"
-	OptimismMintableERC20Factory = "0x4200000000000000000000000000000000000012"
-	L1BlockNumber                = "0x4200000000000000000000000000000000000013"
-	GasPriceOracle               = "0x420000000000000000000000000000000000000F"
-	L1Block                      = "0x4200000000000000000000000000000000000015"
-	GovernanceToken              = "0x4200000000000000000000000000000000000042"
-	LegacyMessagePasser          = "0x4200000000000000000000000000000000000000"
+	L2ToL1MessagePasser           = "0x4200000000000000000000000000000000000016"
+	DeployerWhitelist             = "0x4200000000000000000000000000000000000002"
+	LegacyERC20ETH                = "0xDeadDeAddeAddEAddeadDEaDDEAdDeaDDeAD0000"
+	WETH9                         = "0x4200000000000000000000000000000000000006"
+	L2CrossDomainMessenger        = "0x4200000000000000000000000000000000000007"
+	L2StandardBridge              = "0x4200000000000000000000000000000000000010"
+	SequencerFeeVault             = "0x4200000000000000000000000000000000000011"
+	OptimismMintableERC20Factory  = "0x4200000000000000000000000000000000000012"
+	L1BlockNumber                 = "0x4200000000000000000000000000000000000013"
+	GasPriceOracle                = "0x420000000000000000000000000000000000000F"
+	L1Block                       = "0x4200000000000000000000000000000000000015"
+	GovernanceToken               = "0x4200000000000000000000000000000000000042"
+	LegacyMessagePasser           = "0x4200000000000000000000000000000000000000"
+	L2ERC721Bridge                = "0x4200000000000000000000000000000000000014"
+	OptimismMintableERC721Factory = "0x4200000000000000000000000000000000000018"
 )
 
 var (
-	L2ToL1MessagePasserAddr          = common.HexToAddress(L2ToL1MessagePasser)
-	DeployerWhitelistAddr            = common.HexToAddress(DeployerWhitelist)
-	LegacyERC20ETHAddr               = common.HexToAddress(LegacyERC20ETH)
-	WETH9Addr                        = common.HexToAddress(WETH9)
-	L2CrossDomainMessengerAddr       = common.HexToAddress(L2CrossDomainMessenger)
-	L2StandardBridgeAddr             = common.HexToAddress(L2StandardBridge)
-	SequencerFeeVaultAddr            = common.HexToAddress(SequencerFeeVault)
-	OptimismMintableERC20FactoryAddr = common.HexToAddress(OptimismMintableERC20Factory)
-	L1BlockNumberAddr                = common.HexToAddress(L1BlockNumber)
-	GasPriceOracleAddr               = common.HexToAddress(GasPriceOracle)
-	L1BlockAddr                      = common.HexToAddress(L1Block)
-	GovernanceTokenAddr              = common.HexToAddress(GovernanceToken)
-	LegacyMessagePasserAddr          = common.HexToAddress(LegacyMessagePasser)
+	L2ToL1MessagePasserAddr           = common.HexToAddress(L2ToL1MessagePasser)
+	DeployerWhitelistAddr             = common.HexToAddress(DeployerWhitelist)
+	LegacyERC20ETHAddr                = common.HexToAddress(LegacyERC20ETH)
+	WETH9Addr                         = common.HexToAddress(WETH9)
+	L2CrossDomainMessengerAddr        = common.HexToAddress(L2CrossDomainMessenger)
+	L2StandardBridgeAddr              = common.HexToAddress(L2StandardBridge)
+	SequencerFeeVaultAddr             = common.HexToAddress(SequencerFeeVault)
+	OptimismMintableERC20FactoryAddr  = common.HexToAddress(OptimismMintableERC20Factory)
+	L1BlockNumberAddr                 = common.HexToAddress(L1BlockNumber)
+	GasPriceOracleAddr                = common.HexToAddress(GasPriceOracle)
+	L1BlockAddr                       = common.HexToAddress(L1Block)
+	GovernanceTokenAddr               = common.HexToAddress(GovernanceToken)
+	LegacyMessagePasserAddr           = common.HexToAddress(LegacyMessagePasser)
+	L2ERC721BridgeAddr                = common.HexToAddress(L2ERC721Bridge)
+	OptimismMintableERC721FactoryAddr = common.HexToAddress(OptimismMintableERC721Factory)
 
 	Predeploys = make(map[string]*common.Address)
 )
@@ -50,4 +54,6 @@ func init() {
 	Predeploys["L1Block"] = &L1BlockAddr
 	Predeploys["GovernanceToken"] = &GovernanceTokenAddr
 	Predeploys["LegacyMessagePasser"] = &LegacyMessagePasserAddr
+	Predeploys["L2ERC721Bridge"] = &L2ERC721BridgeAddr
+	Predeploys["OptimismMintableERC721Factory"] = &OptimismMintableERC721FactoryAddr
 }
--- packages/contracts-bedrock/contracts/libraries/Predeploys.sol
+++ packages/contracts-bedrock/contracts/libraries/Predeploys.sol
@@ -23,6 +23,11 @@ library Predeploys {
     address internal constant L2_STANDARD_BRIDGE = 0x4200000000000000000000000000000000000010;
 
     /**
+     * @notice Address of the L2ERC721Bridge predeploy.
+     */
+    address internal constant L2_ERC721_BRIDGE = 0x4200000000000000000000000000000000000014;
+
+    /**
      * @notice Address of the SequencerFeeWallet predeploy.
      */
     address internal constant SEQUENCER_FEE_WALLET = 0x4200000000000000000000000000000000000011;
@@ -32,6 +37,11 @@ library Predeploys {
      */
     address internal constant OPTIMISM_MINTABLE_ERC20_FACTORY =
         0x4200000000000000000000000000000000000012;
+
+    /**
+     * @notice Address of the OptimismMintableERC721Factory predeploy.
+     */
+    address internal constant OPTIMISM_MINTABLE_ERC721_FACTORY = 0x4200000000000000000000000000000000000018;
 
     /**
      * @notice Address of the L1Block predeploy.
