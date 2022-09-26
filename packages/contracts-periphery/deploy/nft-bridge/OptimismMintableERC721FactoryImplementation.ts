cfc02e68dc4f563fc39e7e6756c447a200bbb8f9
Mark Tyneway <mark.tyneway@gmail.com>
2022-09-26 11:08:26 -0700

    ctp: fixup deploy scripts for the nft bridge
    The most important one is the deploy script for the
    `Proxy` that will end up at the predeploy on L2. There
    are specific checks for the correct deployer account
    being used. Will likely require some changes to the
    hardhat config when doing the actual deployment.

--- packages/contracts-periphery/deploy/nft-bridge/OptimismMintableERC721FactoryImplementation.ts
+++ packages/contracts-periphery/deploy/nft-bridge/OptimismMintableERC721FactoryImplementation.ts
@@ -1,13 +1,17 @@
 /* Imports: External */
 import { DeployFunction } from 'hardhat-deploy/dist/types'
-import { ethers } from 'hardhat'
+import '@eth-optimism/hardhat-deploy-config'
+import '@nomiclabs/hardhat-ethers'
+import 'hardhat-deploy'
+import { predeploys } from '@eth-optimism/contracts-bedrock'
 
 const deployFn: DeployFunction = async (hre) => {
   const { deployer } = await hre.getNamedAccounts()
+  const remoteChainId = hre.deployConfig.remoteChainId
 
   await hre.deployments.deploy('OptimismMintableERC721Factory', {
     from: deployer,
-    args: [ethers.constants.AddressZero, 0],
+    args: [predeploys.L2ERC721Bridge, remoteChainId],
     log: true,
   })
 }
