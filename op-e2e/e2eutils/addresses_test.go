dac7fa9eb9a2b7a15e8ebe1cd72dfb538e513adb
protolambda <proto@protolambda.com>
2022-09-28 15:01:14 +0200

    e2eutils: collect addresses from test setup for easy fuzzing/test usage

---
+++ op-e2e/e2eutils/addresses_test.go
@@ -0,0 +1,21 @@
+package e2eutils
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestCollectAddresses(t *testing.T) {
+	tp := &TestParams{
+		MaxSequencerDrift:   40,
+		SequencerWindowSize: 120,
+		ChannelTimeout:      120,
+	}
+	dp := MakeDeployParams(t, tp)
+	alloc := &AllocParams{PrefundTestUsers: true}
+	sd := Setup(t, dp, alloc)
+	addrs := CollectAddresses(sd, dp)
+	require.NotEmpty(t, addrs)
+	require.Contains(t, addrs, dp.Addresses.Batcher)
+}
