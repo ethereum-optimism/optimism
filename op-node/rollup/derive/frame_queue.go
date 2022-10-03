dc8fd2ca01b89fdfa748b581d09999bf58f2e718
Joshua Gutow <jgutow@optimism.io>
2022-10-03 13:22:24 -0700

    WIP: op-node: Add FrameQueue
    This replaces the L1 Retrieval and L1 Traversal Stages.

--- op-node/rollup/derive/frame_queue.go
+++ op-node/rollup/derive/frame_queue.go
@@ -2,60 +2,114 @@ package derive
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"io"
 
 	"github.com/ethereum-optimism/optimism/op-node/eth"
+	"github.com/ethereum-optimism/optimism/op-node/rollup"
+	"github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/log"
 )
 
-var _ NextFrameProvider = &FrameQueue{}
-
-type NextDataProvider interface {
-	NextData(context.Context) ([]byte, error)
-	Origin() eth.L1BlockRef
+type L1TransactionFetcher interface {
+	InfoAndTxsByHash(ctx context.Context, hash common.Hash) (eth.BlockInfo, types.Transactions, error)
 }
 
+// FrameWithL1Inclusion tags a frame with the L1 Block that is was derived from
+type FrameWithL1Inclusion struct {
+	frame            Frame
+	l1InclusionBlock eth.L1BlockRef
+}
+
+// FrameQueue maintains a queue of frames from L1. It fetches transactions from L1
+// and does all filtering/parsing when `ProcessOrigin` is called.
 type FrameQueue struct {
-	log    log.Logger
-	frames []Frame
-	prev   NextDataProvider
+	// Internal buffer of frames.
+	frames []FrameWithL1Inclusion
+
+	// Supporting data / objects
+	fetcher            L1TransactionFetcher // Fetcher for transactions
+	l1Signer           types.Signer         // signer to recover the sender address
+	batchInboxAddress  common.Address       // the `to` address of batch submissions
+	batchSenderAddress common.Address       // the address of authorized batch submitters
+	log                log.Logger
 }
 
-func NewFrameQueue(log log.Logger, prev NextDataProvider) *FrameQueue {
+// NewFrameQueue creates a new frame queue.
+func NewFrameQueue(log log.Logger, cfg *rollup.Config, fetcher L1TransactionFetcher) *FrameQueue {
 	return &FrameQueue{
-		log:  log,
-		prev: prev,
+		fetcher:            fetcher,
+		l1Signer:           cfg.L1Signer(),
+		batchInboxAddress:  cfg.BatchInboxAddress,
+		batchSenderAddress: cfg.BatchSenderAddress,
+		log:                log,
 	}
 }
 
-func (fq *FrameQueue) Origin() eth.L1BlockRef {
-	return fq.prev.Origin()
-}
-
-func (fq *FrameQueue) NextFrame(ctx context.Context) (Frame, error) {
-	// Find more frames if we need to
-	if len(fq.frames) == 0 {
-		if data, err := fq.prev.NextData(ctx); err != nil {
-			return Frame{}, err
-		} else {
-			if new, err := ParseFrames(data); err == nil {
-				fq.frames = append(fq.frames, new...)
-			} else {
-				fq.log.Warn("Failed to parse frames", "origin", fq.prev.Origin(), "err", err)
-			}
-		}
+// NextFrame returns either the next frame or io.EOF if there are no more frames.
+// No other errors will be returned.
+func (f *FrameQueue) NextFrame() (FrameWithL1Inclusion, error) {
+	if len(f.frames) == 0 {
+		return FrameWithL1Inclusion{}, io.EOF
 	}
-	// If we did not add more frames but still have more data, retry this function.
-	if len(fq.frames) == 0 {
-		return Frame{}, NotEnoughData
-	}
-
-	ret := fq.frames[0]
-	fq.frames = fq.frames[1:]
+	ret := f.frames[0]
+	f.frames = f.frames[1:]
 	return ret, nil
 }
 
-func (fq *FrameQueue) Reset(ctx context.Context, base eth.L1BlockRef) error {
-	fq.frames = fq.frames[:0]
-	return io.EOF
+// ProcessOrigin fetches all transactions from a specific L2 block and then loads them into it's internal frame
+// buffer. This function should only be called once per origin. This function will only err on a fetcher error.
+func (f *FrameQueue) ProcessOrigin(ctx context.Context, origin eth.L1BlockRef) error {
+	if _, txs, err := f.fetcher.InfoAndTxsByHash(ctx, origin.Hash); errors.Is(err, ethereum.NotFound) {
+		return NewResetError(fmt.Errorf("failed to find transactions to process a L1 Block: %w", err))
+	} else if err != nil {
+		return NewTemporaryError(fmt.Errorf("failed to find transactions to process a L1 Block: %w", err))
+	} else {
+		f.parseFrames(f.filterTransactions(txs), origin)
+		return nil
+	}
+}
+
+// filterTransactions returns the valid batch submission transactions (transactions that
+// are sent to the batch inbox address from the batch sender address).
+func (f *FrameQueue) filterTransactions(txs types.Transactions) types.Transactions {
+	var out []*types.Transaction
+	for i, tx := range txs {
+		if to := tx.To(); to != nil && *to == f.batchInboxAddress {
+			seqDataSubmitter, err := f.l1Signer.Sender(tx) // optimization: only derive sender if To is correct
+			if err != nil {
+				f.log.Warn("tx in inbox with invalid signature", "index", i, "err", err)
+				continue // bad signature, ignore
+			}
+			// some random L1 user might have sent a transaction to our batch inbox, ignore them
+			if seqDataSubmitter != f.batchSenderAddress {
+				f.log.Warn("tx in inbox with unauthorized submitter", "index", i, "err", err)
+				continue // not an authorized batch submitter, ignore
+			}
+			out = append(out, tx)
+		}
+	}
+	return out
+}
+
+// parseFrames parses the set of frames in each supplied transaction. It then stores
+// each frame as a FrameWithL1Inclusion in queue.
+func (f *FrameQueue) parseFrames(txs types.Transactions, origin eth.L1BlockRef) {
+	for i, tx := range txs {
+		if frames, err := ParseFrames(tx.Data()); err != nil {
+			f.log.Warn("failed to parse frame on tx %v: %w", i, err)
+		} else {
+			for _, frame := range frames {
+				f.frames = append(f.frames, FrameWithL1Inclusion{frame: frame, l1InclusionBlock: origin})
+			}
+		}
+	}
+}
+
+// Clear removes stored frames from the frame queue.
+func (f *FrameQueue) Clear() {
+	f.frames = f.frames[:]
 }
--- op-node/rollup/derive/frame_queue_test.go
+++ op-node/rollup/derive/frame_queue_test.go
@@ -1,61 +1,73 @@
 package derive
 
 import (
+	"crypto/ecdsa"
 	"math/big"
 	"math/rand"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 
-	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/crypto"
-	"github.com/ethereum/go-ethereum/log"
-
-	"github.com/ethereum-optimism/optimism/op-node/eth"
 	"github.com/ethereum-optimism/optimism/op-node/rollup"
 	"github.com/ethereum-optimism/optimism/op-node/testlog"
 	"github.com/ethereum-optimism/optimism/op-node/testutils"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/log"
+	"github.com/ethereum/go-ethereum/params"
 )
 
-// type testTx struct {
-// 	to      *common.Address
-// 	dataLen int
-// 	author  *ecdsa.PrivateKey
-// 	good    bool
-// 	value   int
-// }
+type testTx struct {
+	to      *common.Address
+	dataLen int
+	author  *ecdsa.PrivateKey
+	good    bool
+	value   int
+}
 
-// func (tx *testTx) Create(t *testing.T, signer types.Signer, rng *rand.Rand) *types.Transaction {
-// 	out, err := types.SignNewTx(tx.author, signer, &types.DynamicFeeTx{
-// 		ChainID:   signer.ChainID(),
-// 		Nonce:     0,
-// 		GasTipCap: big.NewInt(2 * params.GWei),
-// 		GasFeeCap: big.NewInt(30 * params.GWei),
-// 		Gas:       100_000,
-// 		To:        tx.to,
-// 		Value:     big.NewInt(int64(tx.value)),
-// 		Data:      testutils.RandomData(rng, tx.dataLen),
-// 	})
-// 	require.NoError(t, err)
-// 	return out
-// }
+func (tx *testTx) Create(t *testing.T, signer types.Signer, rng *rand.Rand) *types.Transaction {
+	out, err := types.SignNewTx(tx.author, signer, &types.DynamicFeeTx{
+		ChainID:   signer.ChainID(),
+		Nonce:     0,
+		GasTipCap: big.NewInt(2 * params.GWei),
+		GasFeeCap: big.NewInt(30 * params.GWei),
+		Gas:       100_000,
+		To:        tx.to,
+		Value:     big.NewInt(int64(tx.value)),
+		Data:      testutils.RandomData(rng, tx.dataLen),
+	})
+	require.NoError(t, err)
+	return out
+}
 
-// type calldataTest struct {
-// 	name string
-// 	txs  []testTx
-// }
+type calldataTest struct {
+	name string
+	txs  []testTx
+}
 
-// TestDataFromEVMTransactions creates some transactions from a specified template and asserts
-// that DataFromEVMTransactions properly filters and returns the data from the authorized transactions
+// TODO: Add the following tests:
+// FrameQueueProcessOrigin
+// - ethereum.NotFound
+// - other miscellaneous error
+// - valid data -> assert that frames w/ L1 Origin make it through
+//
+// FrameQueueNextFrame
+// - Assert it is a queue
+// - Test with data placed in ProcessOrigin??
+// - Assert that L1 Origin is valid
+
+// TestFrameQueueFilter creates some transactions from a specified template and asserts
+// that FrameQueueFilter properly filters and returns the data from the authorized transactions
 // inside the transaction set.
-func TestDataFromEVMTransactions(t *testing.T) {
+func TestFrameQueueFilter(t *testing.T) {
 	inboxPriv := testutils.RandomKey()
 	batcherPriv := testutils.RandomKey()
 	cfg := &rollup.Config{
-		L1ChainID:         big.NewInt(100),
-		BatchInboxAddress: crypto.PubkeyToAddress(inboxPriv.PublicKey),
+		L1ChainID:          big.NewInt(100),
+		BatchInboxAddress:  crypto.PubkeyToAddress(inboxPriv.PublicKey),
+		BatchSenderAddress: crypto.PubkeyToAddress(batcherPriv.PublicKey),
 	}
-	batcherAddr := crypto.PubkeyToAddress(batcherPriv.PublicKey)
 
 	altInbox := testutils.RandomAddress(rand.New(rand.NewSource(1234)))
 	altAuthor := testutils.RandomKey()
@@ -76,7 +88,7 @@ func TestDataFromEVMTransactions(t *testing.T) {
 			txs:  []testTx{{to: &cfg.BatchInboxAddress, dataLen: 1234, author: inboxPriv, good: false}}},
 		{
 			name: "author is inbox",
-			txs:  []testTx{{to: &batcherAddr, dataLen: 1234, author: batcherPriv, good: false}}},
+			txs:  []testTx{{to: &cfg.BatchSenderAddress, dataLen: 1234, author: batcherPriv, good: false}}},
 		{
 			name: "unrelated",
 			txs:  []testTx{{to: &altInbox, dataLen: 1234, author: altAuthor, good: false}}},
@@ -101,24 +113,25 @@ func TestDataFromEVMTransactions(t *testing.T) {
 				{to: &altInbox, dataLen: 2020, value: 12, author: batcherPriv, good: false},
 			},
 		},
-		// TODO: test with different batcher key, i.e. when it's changed from initial config value by L1 contract
 	}
 
 	for i, tc := range testCases {
 		rng := rand.New(rand.NewSource(int64(i)))
 		signer := cfg.L1Signer()
 
-		var expectedData []eth.Data
+		fq := NewFrameQueue(testlog.Logger(t, log.LvlCrit), *cfg, nil)
+
+		var expected []*types.Transaction
 		var txs []*types.Transaction
 		for i, tx := range tc.txs {
 			txs = append(txs, tx.Create(t, signer, rng))
 			if tx.good {
-				expectedData = append(expectedData, txs[i].Data())
+				expected = append(expected, txs[i])
 			}
 		}
 
-		out := DataFromEVMTransactions(cfg, batcherAddr, txs, testlog.Logger(t, log.LvlCrit))
-		require.ElementsMatch(t, expectedData, out)
+		out := fq.filterTransactions(txs)
+		require.ElementsMatch(t, expected, out)
 	}
 
 }
