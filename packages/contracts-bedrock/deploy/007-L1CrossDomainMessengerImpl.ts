a16f44f9136683912702bb86ed0b6af2d7c8afe8
Kelvin Fichter <kelvinfichter@gmail.com>
2022-10-08 00:32:37 -0500

    feat(ctb): simplfy deploy scripts with dictator
    Simplifies the deployment scripts by breaking them out into individual
    files. Also introduces a new FreshSystemDictator contract which is
    responsible for executing upgrades for fresh deployments.

---
+++ packages/contracts-bedrock/deploy/007-L1CrossDomainMessengerImpl.ts
@@ -0,0 +1,31 @@
+import { DeployFunction } from 'hardhat-deploy/dist/types'
+
+import {
+  assertContractVariable,
+  deployAndVerifyAndThen,
+  getContractFromArtifact,
+} from '../src/deploy-utils'
+
+const deployFn: DeployFunction = async (hre) => {
+  const OptimismPortalProxy = await getContractFromArtifact(
+    hre,
+    'OptimismPortalProxy'
+  )
+  await deployAndVerifyAndThen({
+    hre,
+    name: 'L1CrossDomainMessengerImpl',
+    contract: 'L1CrossDomainMessenger',
+    args: [OptimismPortalProxy.address],
+    postDeployAction: async (contract) => {
+      await assertContractVariable(
+        contract,
+        'portal',
+        OptimismPortalProxy.address
+      )
+    },
+  })
+}
+
+deployFn.tags = ['L1CrossDomainMessengerImpl', 'fresh', 'migration']
+
+export default deployFn
