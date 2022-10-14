6f9f6b8903668fc55acc7a1b43cb1f47d9de3a22
Maurelian <maurelian@protonmail.ch>
2022-10-14 17:21:38 -0500

    ctb: Clean up L1Block.t.sol

--- packages/contracts-bedrock/contracts/test/L1Block.t.sol
+++ packages/contracts-bedrock/contracts/test/L1Block.t.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.15;
 import { CommonTest } from "./CommonTest.t.sol";
 import { L1Block } from "../L2/L1Block.sol";
 
-contract L1BlockTest is CommonTest {
+contract L1Block_TestInit is CommonTest {
     L1Block lb;
     address depositor;
     bytes32 immutable NON_ZERO_HASH = keccak256(abi.encode(1));
@@ -24,29 +24,13 @@ contract L1BlockTest is CommonTest {
             _l1FeeScalar: 3
         });
     }
+}
 
-    function test_updatesValues(
-        uint64 n,
-        uint64 t,
-        uint256 b,
-        bytes32 h,
-        uint64 s,
-        bytes32 bt,
-        uint256 fo,
-        uint256 fs
-    ) external {
-        vm.prank(depositor);
-        lb.setL1BlockValues(n, t, b, h, s, bt, fo, fs);
-        assertEq(lb.number(), n);
-        assertEq(lb.timestamp(), t);
-        assertEq(lb.basefee(), b);
-        assertEq(lb.hash(), h);
-        assertEq(lb.sequenceNumber(), s);
-        assertEq(lb.batcherHash(), bt);
-        assertEq(lb.l1FeeOverhead(), fo);
-        assertEq(lb.l1FeeScalar(), fs);
-    }
+contract L1Block_Getters_TestFail is L1Block_TestInit {
+    // none
+}
 
+contract L1Block_Getters_Test is L1Block_TestInit {
     function test_number() external {
         assertEq(lb.number(), uint64(1));
     }
@@ -66,8 +50,14 @@ contract L1BlockTest is CommonTest {
     function test_sequenceNumber() external {
         assertEq(lb.sequenceNumber(), uint64(4));
     }
+}
 
-    function test_updateValues() external {
+contract L1Block_Setters_TestFail is L1Block_TestInit {
+    // none
+}
+
+contract L1Block_Setters_Test is L1Block_TestInit {
+    function test_setL1BlockValues_succeeds() external {
         vm.prank(depositor);
         lb.setL1BlockValues({
             _number: type(uint64).max,
@@ -79,5 +69,27 @@ contract L1BlockTest is CommonTest {
             _l1FeeOverhead: type(uint256).max,
             _l1FeeScalar: type(uint256).max
         });
+    }
+
+    function testFuzz_setL1BlockValues_succeeds(
+        uint64 n,
+        uint64 t,
+        uint256 b,
+        bytes32 h,
+        uint64 s,
+        bytes32 bt,
+        uint256 fo,
+        uint256 fs
+    ) external {
+        vm.prank(depositor);
+        lb.setL1BlockValues(n, t, b, h, s, bt, fo, fs);
+        assertEq(lb.number(), n);
+        assertEq(lb.timestamp(), t);
+        assertEq(lb.basefee(), b);
+        assertEq(lb.hash(), h);
+        assertEq(lb.sequenceNumber(), s);
+        assertEq(lb.batcherHash(), bt);
+        assertEq(lb.l1FeeOverhead(), fo);
+        assertEq(lb.l1FeeScalar(), fs);
     }
 }
