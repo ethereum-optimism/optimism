f7dbad0287fd97488e62a3bee53fb3353a6c56b9
cyborgdennett <40475862+cyborgdennett@users.noreply.github.com>
2022-10-31 22:36:45 +0100

    Natspec legacy contracts update (#3799)
    * Add natspec params and returns where missing
    
    * Improved natspec
    
    * remove space
    
    Co-authored-by: Casper Belier <casper@blocklab.nl;casperbelier@hotmail.com>
    Co-authored-by: Mark Tyneway <mark.tyneway@gmail.com>

--- packages/contracts/contracts/L1/verification/IBondManager.sol
+++ packages/contracts/contracts/L1/verification/IBondManager.sol
@@ -9,5 +9,11 @@ interface IBondManager {
      * Public Functions *
      ********************/
 
+    /**
+     * Checks whether a given address is properly collateralized and can perform actions within
+     * the system.
+     * @param _who Address to check.
+     * @return true if the address is properly collateralized, false otherwise.
+     */
     function isCollateralized(address _who) external view returns (bool);
 }
