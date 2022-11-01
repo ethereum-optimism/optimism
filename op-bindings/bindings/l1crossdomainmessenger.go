e5e1f50e7861f8f865eaa1b3d3d77079784b5270
Matthew Slipper <me@matthewslipper.com>
2022-11-01 15:41:56 -0600

    bindingz

--- op-bindings/bindings/l1crossdomainmessenger.go
+++ op-bindings/bindings/l1crossdomainmessenger.go
@@ -26,7 +26,6 @@ var (
 	_ = common.Big1
 	_ = types.BloomLookup
 	_ = event.NewSubscription
-	_ = abi.ConvertType
 )
 
 // L1CrossDomainMessengerMetaData contains all meta data concerning the L1CrossDomainMessenger contract.
@@ -157,11 +156,11 @@ func NewL1CrossDomainMessengerFilterer(address common.Address, filterer bind.Con
 
 // bindL1CrossDomainMessenger binds a generic wrapper to an already deployed contract.
 func bindL1CrossDomainMessenger(address common.Address, caller bind.ContractCaller, transactor bind.ContractTransactor, filterer bind.ContractFilterer) (*bind.BoundContract, error) {
-	parsed, err := L1CrossDomainMessengerMetaData.GetAbi()
+	parsed, err := abi.JSON(strings.NewReader(L1CrossDomainMessengerABI))
 	if err != nil {
 		return nil, err
 	}
-	return bind.NewBoundContract(address, *parsed, caller, transactor, filterer), nil
+	return bind.NewBoundContract(address, parsed, caller, transactor, filterer), nil
 }
 
 // Call invokes the (constant) contract method with params as input values and
--- op-bindings/bindings/proxy.go
+++ op-bindings/bindings/proxy.go
@@ -26,7 +26,6 @@ var (
 	_ = common.Big1
 	_ = types.BloomLookup
 	_ = event.NewSubscription
-	_ = abi.ConvertType
 )
 
 // ProxyMetaData contains all meta data concerning the Proxy contract.
@@ -157,11 +156,11 @@ func NewProxyFilterer(address common.Address, filterer bind.ContractFilterer) (*
 
 // bindProxy binds a generic wrapper to an already deployed contract.
 func bindProxy(address common.Address, caller bind.ContractCaller, transactor bind.ContractTransactor, filterer bind.ContractFilterer) (*bind.BoundContract, error) {
-	parsed, err := ProxyMetaData.GetAbi()
+	parsed, err := abi.JSON(strings.NewReader(ProxyABI))
 	if err != nil {
 		return nil, err
 	}
-	return bind.NewBoundContract(address, *parsed, caller, transactor, filterer), nil
+	return bind.NewBoundContract(address, parsed, caller, transactor, filterer), nil
 }
 
 // Call invokes the (constant) contract method with params as input values and
