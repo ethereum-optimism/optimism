76c11b79e2cfa79b02c01944c82944df4d37ecce
ex-scxr <ex-scxr>
2022-11-01 18:23:57 -0700

    Bumped compiler version for contracts-bedrock

--- packages/contracts-bedrock/hardhat.config.ts
+++ packages/contracts-bedrock/hardhat.config.ts
@@ -357,7 +357,7 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
-        version: '0.8.10',
+        version: '0.8.15',
         settings: {
           optimizer: { enabled: true, runs: 10_000 },
         },
