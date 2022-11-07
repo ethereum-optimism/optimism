b94e9cb5097564ebf15de8e8c7003d337a19a57b
Matthew Slipper <me@matthewslipper.com>
2022-11-06 22:46:56 -0700

    ctb: Add system config deployment

--- packages/contracts-bedrock/contracts/deployment/MigrationSystemDictator.sol
+++ packages/contracts-bedrock/contracts/deployment/MigrationSystemDictator.sol
@@ -7,6 +7,7 @@ import { L1CrossDomainMessenger } from "../L1/L1CrossDomainMessenger.sol";
 import { L1ChugSplashProxy } from "../legacy/L1ChugSplashProxy.sol";
 import { ProxyAdmin } from "../universal/ProxyAdmin.sol";
 import { PortalSender } from "./PortalSender.sol";
+import { SystemConfig } from "../L1/SystemConfig.sol";
 import { BaseSystemDictator } from "./BaseSystemDictator.sol";
 
 /**
@@ -19,7 +20,7 @@ contract MigrationSystemDictator is BaseSystemDictator {
     /**
      * @param _config System configuration.
      */
-    constructor(SystemConfig memory _config) BaseSystemDictator(_config) {}
+    constructor(DeployConfig memory _config) BaseSystemDictator(_config) {}
 
     /**
      * @notice Configures the ProxyAdmin contract.
@@ -152,6 +153,22 @@ contract MigrationSystemDictator is BaseSystemDictator {
         config.globalConfig.proxyAdmin.upgrade(
             payable(config.proxyAddressConfig.l1ERC721BridgeProxy),
             address(config.implementationAddressConfig.l1ERC721BridgeImpl)
+        );
+
+        // Upgrade and initialize the SystemConfig.
+        config.globalConfig.proxyAdmin.upgradeAndCall(
+            payable(config.proxyAddressConfig.systemConfigProxy),
+            address(config.implementationAddressConfig.systemConfigImpl),
+            abi.encodeCall(
+                SystemConfig.initialize,
+                (
+                    config.systemConfigConfig.owner,
+                    config.systemConfigConfig.overhead,
+                    config.systemConfigConfig.scalar,
+                    config.systemConfigConfig.batcherHash,
+                    config.systemConfigConfig.gasLimit
+                )
+            )
         );
     }
 
