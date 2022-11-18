37ddee05419c3b5f31154d9af6816037d05f5bb8
hmrtn <hmrtn@pm.me>
2022-11-17 21:36:25 -0800

    refactor: use openzepplin upgradeable contracts move to hardhat from foundry

--- packages/contracts-periphery/contracts/universal/op-nft/Optimist.sol
+++ packages/contracts-periphery/contracts/universal/op-nft/Optimist.sol
@@ -1,46 +1,59 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.15;
 
-import "@rari-capital/solmate/src/tokens/ERC721.sol";
-import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721BurnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/StringsUpgradeable.sol";
 import "./SocialContract.sol";
 
-contract Optimist is ERC721 {
-    SocialContract public immutable sc;
-    address public immutable admin;
+contract Optimist is
+    Initializable,
+    ERC721Upgradeable,
+    ERC721BurnableUpgradeable,
+    OwnableUpgradeable
+{
+    SocialContract public sc;
+    address public admin;
 
-    constructor(
-        string memory _name,
-        string memory _symbol,
+    function initialize(
+        string calldata _name,
+        string calldata _symbol,
         address _admin,
         address _sc
-    ) ERC721(_name, _symbol) {
-        admin = _admin;
+    ) public initializer {
+        __ERC721_init(_name, _symbol);
+        __ERC721Burnable_init();
+        __Ownable_init();
         sc = SocialContract(_sc);
+        admin = _admin;
     }
 
-    function mint(address recipient) public returns (uint256) {
-        uint256 newItemId = uint256(uint160(recipient));
-        _safeMint(recipient, newItemId);
-        return newItemId;
-    }
-
-    function transferFrom(
-        address,
-        address,
-        uint256
-    ) public override {
-        revert("Optimist::transferFrom: SOUL_BOUND");
+    function mint(address recipient) public onlyOwner {
+        uint256 tokenId = uint256(uint160(recipient));
+        _safeMint(recipient, tokenId);
     }
 
     function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
         if (ownerOf(tokenId) == address(0)) {
-            revert("Optimist:::tokenURI: TOKEN_URI_DNE");
+            revert("Optimist::tokenURI: TOKEN_URI_DNE");
         }
-        return Strings.toString(uint256(uint160(ownerOf(tokenId))));
+        return StringsUpgradeable.toString(uint256(uint160(ownerOf(tokenId))));
     }
 
     function baseURI() public view returns (bytes memory) {
         return sc.attestations(admin, address(this), keccak256("opnft.optimistNftBaseURI"));
+    }
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256
+    ) internal pure override {
+        require(
+            from == address(0) || to == address(0),
+            "Optimist::_beforeTokenTransfer: SOUL_BOUND"
+        );
     }
 }
---
+++ packages/contracts-periphery/test/contracts/universal/op-nft/Optimist.spec.ts
@@ -0,0 +1,5 @@
+describe('Optimist', () => {
+  it('should deploy the optimist contract', async () => {
+    return
+  })
+})
