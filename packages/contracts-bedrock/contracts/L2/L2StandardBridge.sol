056a9dfec4fdc1511dca0b94f89f401686f211af
Kelvin Fichter <kelvin@optimism.io>
2022-12-05 12:47:57 -0500

    maint(ctb): minor comment cleanup
    Cleans up a few minor issues with comments.

--- packages/contracts-bedrock/contracts/L2/L2StandardBridge.sol
+++ packages/contracts-bedrock/contracts/L2/L2StandardBridge.sol
@@ -13,10 +13,9 @@ import { OptimismMintableERC20 } from "../universal/OptimismMintableERC20.sol";
  * @notice The L2StandardBridge is responsible for transfering ETH and ERC20 tokens between L1 and
  *         L2. In the case that an ERC20 token is native to L2, it will be escrowed within this
  *         contract. If the ERC20 token is native to L1, it will be burnt.
- *         Note that this contract is not intended to support all variations of ERC20 tokens.
- *         Examples of some token types that may not be properly supported by this contract include,
- *         but are not limited to: tokens with transfer fees, rebasing tokens, and
- *         tokens with blocklists.
+ *         NOTE: this contract is not intended to support all variations of ERC20 tokens. Examples
+ *         of some token types that may not be properly supported by this contract include, but are
+ *         not limited to: tokens with transfer fees, rebasing tokens, and tokens with blocklists.
  */
 contract L2StandardBridge is StandardBridge, Semver {
     /**
