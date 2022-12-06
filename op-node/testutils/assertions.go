5ba3f4bf0a4e565bea14f05b2982c3c593b00821
Håvard Anda Estensen <haavard.ae@gmail.com>
2022-12-06 08:28:29 +0100

    op-node: Mark functions test helpers

--- op-node/testutils/assertions.go
+++ op-node/testutils/assertions.go
@@ -14,6 +14,7 @@ func BigEqual(a, b *big.Int) bool {
 }
 
 func RequireBigEqual(t *testing.T, exp, actual *big.Int) {
+	t.Helper()
 	if !BigEqual(exp, actual) {
 		t.Fatalf("expected %s to be equal to %s", exp.String(), actual.String())
 	}
