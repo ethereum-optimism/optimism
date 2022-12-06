ae25ddf81447374d87bff1c3ccad4bc7e9c8c21e
Kelvin Fichter <kelvin@optimism.io>
2022-12-06 14:19:40 -0500

    maint(ctb): clean up deploy function
    Cleans up the custom deployment function that we wrap around
    hardhat-deploy and adds new function comments.

--- packages/contracts-bedrock/deploy/016-PortalSenderImpl.ts
+++ packages/contracts-bedrock/deploy/016-PortalSenderImpl.ts
@@ -2,7 +2,7 @@ import { DeployFunction } from 'hardhat-deploy/dist/types'
 
 import {
   assertContractVariable,
-  deployAndVerifyAndThen,
+  deploy,
   getContractFromArtifact,
 } from '../src/deploy-utils'
 
@@ -12,7 +12,7 @@ const deployFn: DeployFunction = async (hre) => {
     'OptimismPortalProxy'
   )
 
-  await deployAndVerifyAndThen({
+  await deploy({
     hre,
     name: 'PortalSender',
     args: [OptimismPortalProxy.address],
