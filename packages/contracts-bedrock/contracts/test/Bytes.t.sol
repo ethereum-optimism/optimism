237bad047a511d00b8fa6ac2cc0be62f54145e0e
clabby <ben@clab.by>
2022-12-09 17:08:23 -0500

    More static tests for `toNibbles`

--- packages/contracts-bedrock/contracts/test/Bytes.t.sol
+++ packages/contracts-bedrock/contracts/test/Bytes.t.sol
@@ -5,7 +5,7 @@ import { Test } from "forge-std/Test.sol";
 
 contract Bytes_Test is Test {
     /// @dev Test that the `toBytes` function works as expected.
-    function testFuzz_toNibbles_isEquivalent_success(bytes memory _bytes) public {
+    function testFuzz_toNibbles_isEquivalent_succeeds(bytes memory _bytes) public {
         // Get nibbles of `_bytes` via the new optimized function
         bytes memory _nibbles = Bytes.toNibbles(_bytes);
 
@@ -25,15 +25,38 @@ contract Bytes_Test is Test {
         assertEq(_nibbles, nibbles);
     }
 
+    /// @dev Test that the `toNibbles` function works as expected with a static input.
+    function test_toNibbles_isEquivalent_succeeds() public {
+        bytes memory _bytes = hex"1234567890";
+        bytes memory _nibbles = new bytes(_bytes.length * 2);
+        _nibbles[0] = 0x01;
+        _nibbles[1] = 0x02;
+        _nibbles[2] = 0x03;
+        _nibbles[3] = 0x04;
+        _nibbles[4] = 0x05;
+        _nibbles[5] = 0x06;
+        _nibbles[6] = 0x07;
+        _nibbles[7] = 0x08;
+        _nibbles[8] = 0x09;
+        _nibbles[9] = 0x00;
+
+        assertEq(Bytes.toNibbles(_bytes), _nibbles);
+    }
+
+    /// @dev Test that the `toNibbles` function returns a zero-length array when given an empty array.
+    function test_toNibbles_zeroLength_succeeds() public {
+        assertEq(Bytes.toNibbles(hex""), hex"");
+    }
+
     /// @dev Test that the `equals` function works as expected.
-    function testFuzz_equals_success(bytes memory _a) public {
+    function testFuzz_equals_succeeds(bytes memory _a) public {
         assertTrue(Bytes.equal(_a, _a));
         assertEq(_a, _a);
     }
 
     /// @dev Test that the `equals` function returns false when passed
     /// non-equal byte arrays.
-    function testFuzz_equals_failure(bytes memory _a, bytes memory _b) public {
+    function testFuzz_equals_diffInputs_fails(bytes memory _a, bytes memory _b) public {
         vm.assume(keccak256(_a) != keccak256(_b));
 
         assertFalse(Bytes.equal(_a, _b));
