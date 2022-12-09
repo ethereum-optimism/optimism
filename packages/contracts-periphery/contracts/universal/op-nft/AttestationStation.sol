b05a3b9bcecd12778f0bde92de52e0699a65cf0d
Will Cory <willcory@Wills-MacBook-Pro.local>
2022-12-09 13:34:59 -0800

    add more comments

--- packages/contracts-periphery/contracts/universal/op-nft/AttestationStation.sol
+++ packages/contracts-periphery/contracts/universal/op-nft/AttestationStation.sol
@@ -4,15 +4,41 @@ pragma solidity 0.8.15;
 import { Semver } from "@eth-optimism/contracts-bedrock/contracts/universal/Semver.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
+/**
+ * @title AttestationStation
+ * @dev   Contract for creating attestations.
+ * @notice This contract is not yet audited.
+ */
 contract AttestationStation is Semver {
+    /**
+     * @notice Struct representing data that is being attested
+     *
+     * @custom:field about                    Address being attested about (not creator/msg.sender)
+     * @custom:field key                      A bytes32 key for the attestation.
+     * @custom:field val                      The attestation as arbitrary bytes
+     */
     struct AttestationData {
         address about;
         bytes32 key;
         bytes val;
     }
 
+    /**
+     * @notice Maps addresses to attestations
+     * @dev addresses map to attestations map of
+     *      `about` addresses to key/values
+     *      key/values are a map of bytes32 to bytes
+     */
     mapping(address => mapping(address => mapping(bytes32 => bytes))) public attestations;
 
+    /**
+     * @notice Emitted when Attestation is created
+     *
+     * @param creator Address that attested.
+     * @param about  Address attestation is about.
+     * @param key Key of the attestation.
+     * @param val Value of the attestation.
+     */
     event AttestationCreated(
         address indexed creator,
         address indexed about,
