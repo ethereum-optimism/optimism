44e8c095f28bbb4963233544f71eab5bbc7632db
Will Cory <willcory@Wills-MacBook-Pro.local>
2022-12-09 13:49:47 -0800

    code golf to one function

--- packages/contracts-periphery/contracts/universal/op-nft/AttestationStation.sol
+++ packages/contracts-periphery/contracts/universal/op-nft/AttestationStation.sol
@@ -53,28 +53,19 @@ contract AttestationStation is Semver {
     /**
      * @notice  Attest to the given data.
      * @dev     Attests to the given data from the sender.
-     * @param   _about  The address of the attestation subject.
-     * @param   _key  The key of the attestation.
-     * @param   _val  The value of the attestation.
-     */
-    function _attest(
-        address _about,
-        bytes32 _key,
-        bytes memory _val
-    ) private {
-        attestations[msg.sender][_about][_key] = _val;
-        emit AttestationCreated(msg.sender, _about, _key, _val);
-    }
-
-    /**
-     * @notice  Attest to the given data.
-     * @dev     Attests to the given data from the sender.
      * @param   _attestations  The array of attestation data.
      */
     function attest(AttestationData[] memory _attestations) public {
         uint256 length = _attestations.length;
         for (uint256 i = 0; i < length; ) {
-            _attest(_attestations[i].about, _attestations[i].key, _attestations[i].val);
+            AttestationData memory attestation = _attestations[i];
+            attestations[msg.sender][attestation.about][attestation.key] = attestation.val;
+            emit AttestationCreated(
+                msg.sender,
+                attestation.about,
+                attestation.key,
+                attestation.val
+            );
             unchecked {
                 ++i;
             }
