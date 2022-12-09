a6b857265f5686636b376354f7a5e73c2d403d09
Will Cory <willcory@Wills-MacBook-Pro.local>
2022-12-09 13:51:49 -0800

    unused import

--- packages/contracts-periphery/contracts/universal/op-nft/AttestationStation.sol
+++ packages/contracts-periphery/contracts/universal/op-nft/AttestationStation.sol
@@ -2,7 +2,6 @@
 pragma solidity 0.8.15;
 
 import { Semver } from "@eth-optimism/contracts-bedrock/contracts/universal/Semver.sol";
-import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
 /**
  * @title AttestationStation
