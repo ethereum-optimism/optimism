fe80a94886f08c3cf799ec6000ea09953539e46c
Maurelian <maurelian@protonmail.ch>
2022-12-12 15:19:59 -0500

    ctb: Add echidna test ensuring depositTransaction completes

---
+++ packages/contracts-bedrock/contracts/echidna/FuzzOptimismPortal.sol
@@ -0,0 +1,33 @@
+pragma solidity 0.8.15;
+
+import { OptimismPortal } from "../L1/OptimismPortal.sol";
+import { L2OutputOracle } from "../L1/L2OutputOracle.sol";
+import { AddressAliasHelper } from "../vendor/AddressAliasHelper.sol";
+
+contract EchidnaFuzzOptimismPortal {
+    OptimismPortal internal portal;
+    bool internal failedToComplete;
+
+    constructor() {
+        portal = new OptimismPortal(L2OutputOracle(address(0)), 10);
+    }
+
+    // A test intended to identify any unexpected halting conditions
+    function testDepositTransactionCompletes(
+        address _to,
+        uint256 _mint,
+        uint256 _value,
+        uint64 _gasLimit,
+        bool _isCreation,
+        bytes memory _data
+    ) public payable {
+        failedToComplete = true;
+        require(!_isCreation || _to == address(0), "EchidnaFuzzOptimismPortal: invalid test case.");
+        portal.depositTransaction{ value: _mint }(_to, _value, _gasLimit, _isCreation, _data);
+        failedToComplete = false;
+    }
+
+    function echidna_deposit_completes() public view returns (bool) {
+        return !failedToComplete;
+    }
+}
