9b84a6387ca72386458ce448a9a3ecba0a028c3e
Kelvin Fichter <kelvin@optimism.io>
2022-12-14 09:24:34 -0500

    feat(ctb): break out token interfaces
    Breaks the OptimismMintableERC20 and OptimismMintableERC721 token
    interfaces into their own files instead of having one joint file for
    supported interfaces. This is in preparation for portings tests for the
    OptimismMintableERC721 and its factory.

--- packages/contracts-bedrock/contracts/universal/IOptimismMintableERC721.sol
+++ packages/contracts-bedrock/contracts/universal/IOptimismMintableERC721.sol
@@ -1,41 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-// Import this here to make it available just by importing this file
-import { IERC165 } from "@openzeppelin/contracts/utils/introspection/IERC165.sol";
 import {
     IERC721Enumerable
 } from "@openzeppelin/contracts/token/ERC721/extensions/IERC721Enumerable.sol";
-
-/**
- * @title IOptimismMintableERC20
- * @notice This interface is available on the OptimismMintableERC20 contract. We declare it as a
- *         separate interface so that it can be used in custom implementations of
- *         OptimismMintableERC20.
- */
-interface IOptimismMintableERC20 {
-    function remoteToken() external returns (address);
-
-    function bridge() external returns (address);
-
-    function mint(address _to, uint256 _amount) external;
-
-    function burn(address _from, uint256 _amount) external;
-}
-
-/**
- * @custom:legacy
- * @title ILegacyMintableERC20
- * @notice This interface was available on the legacy L2StandardERC20 contract. It remains available
- *         on the OptimismMintableERC20 contract for backwards compatibility.
- */
-interface ILegacyMintableERC20 {
-    function l1Token() external returns (address);
-
-    function mint(address _to, uint256 _amount) external;
-
-    function burn(address _from, uint256 _amount) external;
-}
 
 /**
  * @title IOptimismMintableERC721
