a62d4254f9f551b6497570d626cfd88f6a009a43
Mark Tyneway <mark.tyneway@gmail.com>
2022-12-13 16:07:58 -0800

    op-nft: update deploy scripts
    For the Optimist implementation and proxy

--- packages/contracts-periphery/deploy/op-nft/OptimistImpl.ts
+++ packages/contracts-periphery/deploy/op-nft/OptimistImpl.ts
@@ -1,25 +1,30 @@
 /* Imports: External */
 import { DeployFunction } from 'hardhat-deploy/dist/types'
+import { HardhatRuntimeEnvironment } from 'hardhat/types'
+import '@nomiclabs/hardhat-ethers'
+import '@eth-optimism/hardhat-deploy-config'
+import 'hardhat-deploy'
 
-const deployFn: DeployFunction = async (hre) => {
+const deployFn: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
   const { deployer } = await hre.getNamedAccounts()
+
+  console.log(`Deploying Optimist implementation with ${deployer}`)
 
   const Deployment__AttestationStation = await hre.deployments.get(
     'AttestationStationProxy'
   )
   const attestationStationAddress = Deployment__AttestationStation.address
 
+  console.log(`Using ${attestationStationAddress} as the AttestationStation`)
+  console.log(`Using ${hre.deployConfig.attestorAddress} as ATTESTOR`)
+
   const { deploy } = await hre.deployments.deterministic('Optimist', {
     salt: hre.ethers.utils.solidityKeccak256(['string'], ['Optimist']),
     from: deployer,
-    // make these more configurable
     args: [
-      // TODO what should final name be
-      'OptimistNFT',
-      // TODO what should final symbol be
-      'OPSBT',
-      // TODO what is the multisig address?
-      '0x8F0EBDaA1cF7106bE861753B0f9F5c0250fE0819',
+      hre.deployConfig.optimistName,
+      hre.deployConfig.optimistSymbol,
+      hre.deployConfig.attestorAddress,
       attestationStationAddress,
     ],
     log: true,
