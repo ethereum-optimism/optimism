76f1e0bceed751f6e61e46956e11ced356c04a60
Joshua Gutow <jgutow@oplabs.co>
2023-01-03 14:51:36 -0800

    op-node: Fix invalid epoch advance
    This includes an op-e2e action test which can be used as a regression test
    against the invalid epoch advancement. I also had to fix several other tests.
    
    This bug is caused by the batch queue advancing to the next epoch prior to
    having the full sequence window for that epoch. This occurs when auto generating
    batches. Once it has generated all of the batches for the current epoch, it
    creates the first batch of the next epoch before it should.
    
    Note: I had to remove the assumption that the epoch of the safe head is
    the same as the epoch inside the batch queue. This is not true when generating
    the last batch of an epoch. We can advance the batch queue, but we don't
    generate a safehead with the new origin.

--- op-node/rollup/derive/batches_test.go
+++ op-node/rollup/derive/batches_test.go
@@ -169,22 +169,6 @@ func TestValidBatch(t *testing.T) {
 			Expected: BatchUndecided,
 		},
 		{
-			Name:       "inconsistent L1 info",
-			L1Blocks:   []eth.L1BlockRef{l1B},
-			L2SafeHead: l2A0,
-			Batch: BatchWithL1InclusionBlock{
-				L1InclusionBlock: l1B,
-				Batch: &BatchData{BatchV1{
-					ParentHash:   l2A1.ParentHash,
-					EpochNum:     rollup.Epoch(l2A1.L1Origin.Number),
-					EpochHash:    l2A1.L1Origin.Hash,
-					Timestamp:    l2A1.Time,
-					Transactions: nil,
-				}},
-			},
-			Expected: BatchUndecided,
-		},
-		{
 			Name:       "future timestamp",
 			L1Blocks:   []eth.L1BlockRef{l1A, l1B, l1C},
 			L2SafeHead: l2A0,
