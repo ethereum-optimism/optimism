d050d866112826803c1cbe628b8fb621c89d3a3c
Mark Tyneway <mark.tyneway@gmail.com>
2023-01-09 15:48:56 -0800

    op-node: higher level api for hashing output root proof

--- op-node/rollup/output_root.go
+++ op-node/rollup/output_root.go
@@ -1,11 +1,13 @@
 package rollup
 
 import (
+	"github.com/ethereum-optimism/optimism/op-bindings/bindings"
 	"github.com/ethereum-optimism/optimism/op-node/eth"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/crypto"
 )
 
+// ComputeL2OutputRoot computes the L2 output root
 func ComputeL2OutputRoot(l2OutputRootVersion eth.Bytes32, blockHash common.Hash, blockRoot common.Hash, storageRoot common.Hash) eth.Bytes32 {
 	digest := crypto.Keccak256Hash(
 		l2OutputRootVersion[:],
@@ -14,4 +16,14 @@ func ComputeL2OutputRoot(l2OutputRootVersion eth.Bytes32, blockHash common.Hash,
 		blockHash.Bytes(),
 	)
 	return eth.Bytes32(digest)
+}
+
+// HashOutputRootProof computes the hash of the output root proof
+func HashOutputRootProof(proof *bindings.TypesOutputRootProof) eth.Bytes32 {
+	return ComputeL2OutputRoot(
+		proof.Version,
+		proof.StateRoot,
+		proof.MessagePasserStorageRoot,
+		proof.LatestBlockhash,
+	)
 }
