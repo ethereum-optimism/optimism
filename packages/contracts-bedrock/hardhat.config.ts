e224f3da0b6c318e3151fa098df04ac6a9d2e0ff
Matthew Slipper <me@matthewslipper.com>
2023-01-11 00:07:35 -0700

    commit artifacts

--- packages/contracts-bedrock/hardhat.config.ts
+++ packages/contracts-bedrock/hardhat.config.ts
@@ -75,6 +75,12 @@ const config: HardhatUserConfig = {
       accounts: [process.env.PRIVATE_KEY_DEPLOYER || ethers.constants.HashZero],
       live: true,
     },
+    'late-night-rehearsal': {
+      chainId: 5,
+      url: process.env.L1_RPC || '',
+      accounts: [process.env.PRIVATE_KEY_DEPLOYER || ethers.constants.HashZero],
+      live: true,
+    },
   },
   foundry: {
     buildInfo: true,
@@ -111,6 +117,10 @@ const config: HardhatUserConfig = {
         '../contracts-periphery/deployments/goerli',
       ],
       'final-migration-rehearsal': [
+        '../contracts/deployments/goerli',
+        '../contracts-periphery/deployments/goerli',
+      ],
+      'late-night-rehearsal': [
         '../contracts/deployments/goerli',
         '../contracts-periphery/deployments/goerli',
       ],
