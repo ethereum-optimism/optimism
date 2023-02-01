3e3c61ce6f37f4ac77bd30b4aa0fa2219ae3ce28
jagdeep sidhu <jsidhu@blockchainfoundry.co>
2023-02-01 13:01:41 -0800

    update payload too old to account for larger descrepency of l1 blocks
    also sequence window should be less than this check as we saw on a deployment that trailed 288 L1 blocks (13 hours) that it wasn't able to derive blocks <= 288 blocks from tip since the time was too old (> 600 seconds).. so we reduce sequence window to 12 (1800 seconds on average)

--- op-node/p2p/gossip.go
+++ op-node/p2p/gossip.go
@@ -307,8 +307,8 @@ func BuildBlocksValidator(log log.Logger, cfg *rollup.Config, runCfg GossipRunti
 		// rounding down to seconds is fine here.
 		now := uint64(time.Now().Unix())
 		// SYSCOIN
-		// [REJECT] if the `payload.timestamp` is older than 600 seconds in the past
-		if uint64(payload.Timestamp) < now-600 {
+		// [REJECT] if the `payload.timestamp` is older than 3600 seconds in the past
+		if uint64(payload.Timestamp) < now-3600 {
 			log.Warn("payload is too old", "timestamp", uint64(payload.Timestamp))
 			return pubsub.ValidationReject
 		}
