4bd310cf6792c2beb3c92eeb94120692ceb0a4d3
Joshua Gutow <jgutow@oplabs.co>
2023-02-02 12:03:14 -0800

    txmgr: Simplify resubmit logic and scaffolding
    This makes the transaction manager responsible for determining the
    gas price. It also provides the ability to easily add a check if
    the gas price bump is correct or not.
    
    This fixes the bug where the proposer fails when it redoes gas
    estimation. Gas re-estimation is no longer done. This is fine
    because we have a minimal set of different transactions that
    use a consistent amount of gas.

--- op-e2e/actions/l2_proposer.go
+++ op-e2e/actions/l2_proposer.go
@@ -50,6 +50,8 @@ func NewL2Proposer(t Testing, log log.Logger, cfg *ProposerCfg, l1 *ethclient.Cl
 			ReceiptQueryInterval:      time.Second,
 			NumConfirmations:          1,
 			SafeAbortNonceTooLowCount: 4,
+			From:                      from,
+			// Signer is loaded in `proposer.NewL2OutputSubmitter`
 		},
 		L1Client:          l1,
 		RollupClient:      rollupCl,
@@ -85,6 +87,9 @@ func (p *L2Proposer) ActMakeProposalTx(t Testing) {
 	tx, err := p.driver.CreateProposalTx(t.Ctx(), output)
 	require.NoError(t, err)
 
+	// TODO: This send transaction is blocking on the receipt of the tx
+	// The receipt of the transaction does not occur until mining occurs
+	// which is after this function.
 	err = p.driver.SendTransaction(t.Ctx(), tx)
 	require.NoError(t, err)
 
