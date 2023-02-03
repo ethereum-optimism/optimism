91442183e7675bf260b9fb1ce09da85eda4db356
dcbuilder.eth <dcbuilder@protonmail.com>
2023-02-03 15:31:03 +0000

    add(local owner)

--- packages/contracts-bedrock/contracts/L2/CrossDomainOwnable2.sol
+++ packages/contracts-bedrock/contracts/L2/CrossDomainOwnable2.sol
@@ -14,23 +14,82 @@ import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
  */
 abstract contract CrossDomainOwnable2 is Ownable {
     /**
+     * @notice If true, the contract uses the cross domain _checkOwner function override. If false
+     *         it uses the standard Ownable _checkOwner function.
+     */
+    bool internal crossDomainSwitch = false;
+
+    /**
+     * @notice The local owner of the contract.
+     */
+    address private _localOwner = _msgSender();
+
+    /**
+     * @notice Thrown when the local owner changes.
+     */
+    event LocalOwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    /**
+     * @notice Throws if called by any account other than the localOwner.
+     */
+    modifier onlyLocalOwner() {
+        _checkLocalOwner();
+        _;
+    }
+
+    /**
+     * @notice Throws if the sender is not the localOwner.
+     */
+    function _checkLocalOwner() internal view {
+        require(_msgSender() == _localOwner, "CrossDomainOwnable2: caller is not the localOwner");
+    }
+
+    /**
      * @notice Overrides the implementation of the `onlyOwner` modifier to check that the unaliased
      *         `xDomainMessageSender` is the owner of the contract. This value is set to the caller
      *         of the L1CrossDomainMessenger.
      */
     function _checkOwner() internal view override {
-        L2CrossDomainMessenger messenger = L2CrossDomainMessenger(
-            Predeploys.L2_CROSS_DOMAIN_MESSENGER
-        );
+        if (crossDomainSwitch) {
+            L2CrossDomainMessenger messenger = L2CrossDomainMessenger(
+                Predeploys.L2_CROSS_DOMAIN_MESSENGER
+            );
 
-        require(
-            msg.sender == address(messenger),
-            "CrossDomainOwnable2: caller is not the messenger"
-        );
+            require(
+                msg.sender == address(messenger),
+                "CrossDomainOwnable2: caller is not the messenger"
+            );
 
+            require(
+                owner() == messenger.xDomainMessageSender(),
+                "CrossDomainOwnable2: caller is not the owner"
+            );
+        } else {
+            _checkLocalOwner();
+        }
+    }
+
+    /**
+     * @notice Allows the localOwner to turn on the cross domain ownership check.
+     */
+    function flipTheSwitch() external onlyLocalOwner {
+        require(_msgSender() == localOwner, "CrossDomainOwnable2: caller is not the localOwner");
+        crossDomainSwitch = !crossDomainSwitch;
+    }
+
+    /**
+     * @notice Allows the localOwner to transfer ownership to a .
+     * @param newOwner The address of the new owner.
+     */
+    function transferLocalOwnership(address newOwner) external onlyLocalOwner {
         require(
-            owner() == messenger.xDomainMessageSender(),
-            "CrossDomainOwnable2: caller is not the owner"
+            crossDomainSwitch == false,
+            "CrossDomainOwnable2: cross domain ownership turned on"
         );
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
+        address oldOwner = localOwner;
+        localOwner = newOwner;
+
+        emit LocalOwnershipTransferred(oldOwner, newOwner);
     }
 }
