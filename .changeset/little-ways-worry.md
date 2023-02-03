e1e8265af7e5a3bb4798b27f8fa9f7590bd06e73
Will Cory <willcory@Wills-MacBook-Pro.local>
2023-02-03 10:48:59 -0800

    Build sdk with tsup
    changeset
    
    yarn.lock
    
    update config
    
    documentation
    
    typo bug
    
    fix build passing in flag to typecheck
    
    update every sub dep too
    
    noexternal
    
    Revert "update every sub dep too"
    
    This reverts commit 5fb31700db3f6e0724e7b7a277d835a82f04a1b9.
    
    type error
    
    make sdk still do .d.ts files and update tsconfig
    
    update core-utils
    
    update contracts
    
    Update contracts-bedrock
    
    don't bundle contracts
    
    tsup esm only
    
    remove the one off bundle false since using tsc instedad
    
    remove clean
    
    revert prettier changes in otherwise unchanged tsconfig.jsons
    
    remove unused scripts added to sdk
    
    another tsconfig that changed from prettier
    
    nuke old changeset
    
    changeset

---
+++ .changeset/little-ways-worry.md
@@ -0,0 +1,8 @@
+---
+'@eth-optimism/contracts-bedrock': patch
+'@eth-optimism/core-utils': patch
+'@eth-optimism/contracts': patch
+'@eth-optimism/sdk': patch
+---
+
+Add esm builds to all packages sdk depends on
---
+++ packages/sdk/tsup.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'tsup'
+
+/**
+ * @see https://tsup.egoist.dev/
+ */
+export default defineConfig({
+  name: '@eth-optimism/sdk',
+  entry: ['src/index.ts'],
+  outDir: 'dist',
+  target: 'es2017',
+  // will create a .js file for commonjs and a .cjs file for esm
+  format: ['esm'],
+  // don't generate .d.ts files
+  dts: false,
+  splitting: false,
+  sourcemap: true,
+})
