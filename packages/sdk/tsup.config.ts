bc3f77c4838c92795237d75047b8818d9db122f0
Will Cory <willcory@Wills-MacBook-Pro.local>
2023-02-03 11:50:47 -0800

    documentation

--- packages/sdk/tsup.config.ts
+++ packages/sdk/tsup.config.ts
@@ -11,6 +11,12 @@ export default defineConfig({
   // will create a .js file for commonjs and a .cjs file for esm
   format: ['esm', 'cjs'],
   // don't generate .d.ts files.  This is default but being explicit
+  // note this means we need to generate our own .d.ts files
+  // this means we need to run the typechecker as a linter
+  // which is a general best practice anyways.  It also means
+  // we must use our .ts files as "types" in package.json
+  // this is better dx since it means the types take you to
+  // the source code instead of just the .d.ts file in editors
   dts: false,
   splitting: false,
   sourcemap: true,
