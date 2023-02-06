da88d795d4fd3ca87c90ac236fd42b8b8c15d338
Will Cory <willcory@Wills-MacBook-Pro.local>
2023-02-06 11:58:46 -0800

    update core-utils

---
+++ packages/core-utils/tsup.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'tsup'
+
+/**
+ * @see https://tsup.egoist.dev/
+ */
+export default defineConfig({
+  name: '@eth-optimism/core-utils',
+  entry: ['src/index.ts'],
+  outDir: 'dist',
+  target: 'es2015',
+  // will create a .js file for commonjs and a .cjs file for esm
+  format: ['esm', 'cjs'],
+  // don't generate .d.ts files
+  dts: false,
+  splitting: false,
+  sourcemap: true,
+  // remove dist folder before building
+  clean: true,
+})
