80399959f93dbabe60f57746a5c2e6ce80924188
Mark Tyneway <mark.tyneway@gmail.com>
2023-02-22 14:29:54 -0800

    contracts-bedrock: regenerate snapshot

--- packages/contracts-bedrock/contracts/test/Semver.t.sol
+++ packages/contracts-bedrock/contracts/test/Semver.t.sol
@@ -20,7 +20,6 @@ contract Semver_Test is CommonTest {
      * @notice Deploy a Semver contract
      */
     function setUp() public virtual override {
-        super.setUp();
         semver = new Semver(7, 8, 0);
     }
 
