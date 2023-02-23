0c1c718197e0e0b5fd27d814505736b3c2557bd1
Ori Pomerantz <qbzzt1@gmail.com>
2023-02-23 09:27:39 -0600

    feat(docs/op-stack): Add og:image

--- docs/op-stack/src/.vuepress/theme/components/Page.vue
+++ docs/op-stack/src/.vuepress/theme/components/Page.vue
@@ -45,6 +45,10 @@
       gtag('js', new Date());
       gtag('config', 'G-9KLVB8X0ME');
     </script>
+
+    <!-- Social sharing image -->
+    <meta property="og:image" content="https://stack.optimism.io/assets/logos/logo.png" />
+
   </main>
 </template>
 
