396532da7c70f5b047fc5a264ea34f908d2f726f
Mark Tyneway <mark.tyneway@gmail.com>
2023-02-27 16:00:24 -0800

    contracts-bedrock: ResourceMetering more readable
    Makes the arithmetic in the `ResourceMetering` contract
    more readable by using explicit argument syntax when calling
    the `Arithmetic` library functions. This syntax is not used
    throughout the rest of the codebase, although it should be.
    It was determined that it would be too large of a change
    to use this syntax throughout the codebase now, but it is
    something that we want to do in the future. This adds the
    syntax to a few key places in `ResourceMetering` which makes
    it much easier to read and will therefore increase auditability
    and reviewability of this code.

--- packages/contracts-bedrock/contracts/L1/ResourceMetering.sol
+++ packages/contracts-bedrock/contracts/L1/ResourceMetering.sol
@@ -96,11 +96,11 @@ abstract contract ResourceMetering is Initializable {
 
             // Update base fee by adding the base fee delta and clamp the resulting value between
             // min and max.
-            int256 newBaseFee = Arithmetic.clamp(
-                int256(uint256(params.prevBaseFee)) + baseFeeDelta,
-                MINIMUM_BASE_FEE,
-                MAXIMUM_BASE_FEE
-            );
+            int256 newBaseFee = Arithmetic.clamp({
+                _value: int256(uint256(params.prevBaseFee)) + baseFeeDelta,
+                _min: MINIMUM_BASE_FEE,
+                _max: MAXIMUM_BASE_FEE
+            });
 
             // If we skipped more than one block, we also need to account for every empty block.
             // Empty block means there was no demand for deposits in that block, so we should
@@ -109,15 +109,15 @@ abstract contract ResourceMetering is Initializable {
                 // Update the base fee by repeatedly applying the exponent 1-(1/change_denominator)
                 // blockDiff - 1 times. Simulates multiple empty blocks. Clamp the resulting value
                 // between min and max.
-                newBaseFee = Arithmetic.clamp(
-                    Arithmetic.cdexp(
-                        newBaseFee,
-                        BASE_FEE_MAX_CHANGE_DENOMINATOR,
-                        int256(blockDiff - 1)
-                    ),
-                    MINIMUM_BASE_FEE,
-                    MAXIMUM_BASE_FEE
-                );
+                newBaseFee = Arithmetic.clamp({
+                    _value: Arithmetic.cdexp({
+                        _coefficient: newBaseFee,
+                        _denominator: BASE_FEE_MAX_CHANGE_DENOMINATOR,
+                        _exponent: int256(blockDiff - 1)
+                    }),
+                    _min: MINIMUM_BASE_FEE,
+                    _max: MAXIMUM_BASE_FEE
+                });
             }
 
             // Update new base fee, reset bought gas, and update block number.
