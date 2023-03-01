0d37cf149a033784e18d173195f60c189f4084e0
Will Cory <willcory@Wills-MacBook-Pro.local>
2023-03-01 07:38:49 -0800

    add eventFilters
    add another events test

--- packages/atst/src/index.ts
+++ packages/atst/src/index.ts
@@ -10,6 +10,7 @@ export {
   readAttestationString,
 } from './lib/readAttestation'
 export { readAttestations } from './lib/readAttestations'
+export { getEvents } from './lib/getEvents'
 export { prepareWriteAttestation } from './lib/prepareWriteAttestation'
 export { prepareWriteAttestations } from './lib/prepareWriteAttestations'
 export { writeAttestation } from './lib/writeAttestation'
@@ -23,6 +24,7 @@ export {
   parseString,
 } from './lib/parseAttestationBytes'
 // types
+export type { AttestationCreatedEvent } from './types/AttestationCreatedEvent'
 export type { AttestationReadParams } from './types/AttestationReadParams'
 export type { DataTypeOption } from './types/DataTypeOption'
 export type { WagmiBytes } from './types/WagmiBytes'
---
+++ packages/atst/src/lib/getEvents.ts
@@ -0,0 +1,41 @@
+import { ethers } from 'ethers'
+import { Address } from 'wagmi'
+
+import { ATTESTATION_STATION_ADDRESS } from '../constants/attestationStationAddress'
+import { abi } from '../lib/abi'
+import { AttestationCreatedEvent } from '../types/AttestationCreatedEvent'
+import { encodeRawKey } from './encodeRawKey'
+
+export const getEvents = async ({
+  creator = null,
+  about = null,
+  key = null,
+  value = null,
+  provider,
+  fromBlockOrBlockhash,
+  toBlock,
+}: {
+  creator?: Address | null
+  about?: Address | null
+  key?: string | null
+  value?: string | null
+  provider: ethers.providers.JsonRpcProvider
+  fromBlockOrBlockhash?: ethers.providers.BlockTag | undefined
+  toBlock?: ethers.providers.BlockTag | undefined
+}) => {
+  const contract = new ethers.Contract(
+    ATTESTATION_STATION_ADDRESS,
+    abi,
+    provider
+  )
+  return contract.queryFilter(
+    contract.filters.AttestationCreated(
+      creator,
+      about,
+      key && encodeRawKey(key),
+      value
+    ),
+    fromBlockOrBlockhash,
+    toBlock
+  ) as Promise<AttestationCreatedEvent[]>
+}
