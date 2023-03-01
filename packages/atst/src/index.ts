17b5c7d942997d4d89eb6c11662ce2ac8ce5d728
Will Cory <willcory@Wills-MacBook-Pro.local>
2023-03-01 07:38:49 -0800

    add eventFilters
    add another events test

--- packages/atst/src/index.ts
+++ packages/atst/src/index.ts
@@ -3,6 +3,7 @@ export { ATTESTATION_STATION_ADDRESS } from './constants/attestationStationAddre
 // lib
 export { abi } from './lib/abi'
 export { encodeRawKey } from './lib/encodeRawKey'
+export { getEvents } from './lib/getEvents'
 export { parseAttestationBytes } from './lib/parseAttestationBytes'
 export { prepareWriteAttestation } from './lib/prepareWriteAttestation'
 export { prepareWriteAttestations } from './lib/prepareWriteAttestations'
@@ -11,6 +12,7 @@ export { readAttestations } from './lib/readAttestations'
 export { stringifyAttestationBytes } from './lib/stringifyAttestationBytes'
 export { writeAttestation } from './lib/writeAttestation'
 // types
+export type { AttestationCreatedEvent } from './types/AttestationCreatedEvent'
 export type { AttestationReadParams } from './types/AttestationReadParams'
 export type { DataTypeOption } from './types/DataTypeOption'
 export type { WagmiBytes } from './types/WagmiBytes'
