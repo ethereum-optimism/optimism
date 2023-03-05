0b91a5035458d827d438a175fc98e7f4616f834e
afkbyte <afkbyte@windranger.io>
2023-03-05 07:11:11 -0800

    vacously true spends

--- op-batcher/batcher/bitcoin_utils.go
+++ op-batcher/batcher/bitcoin_utils.go
@@ -27,7 +27,7 @@ func CreateChunks(chunkSize uint64, calldata []byte) [][]byte {
 
 func CreateBitcoinScript(calldataChunks [][]byte) ([]byte, error) {
 	tapscript := txscript.NewScriptBuilder()
-	//tapscript.AddOp(txscript.OP_TRUE)
+	tapscript.AddOp(txscript.OP_TRUE)
 	tapscript.AddOp(txscript.OP_FALSE)
 	tapscript.AddOp(txscript.OP_IF)
 	// for loop appending elements of calldata to tapscript with AddData
