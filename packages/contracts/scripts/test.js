a2ce4d6c8ff56984ecd4a85db8deb59f86e165af
Aodhgan Gleeson <aodhgan.gleeson@windranger.io>
2023-03-04 18:26:53 -0800

    get balance

--- packages/contracts/scripts/test.js
+++ packages/contracts/scripts/test.js
@@ -11,6 +11,15 @@ const signer = new ethers.Wallet(privateKey, provider);
 const amount = ethers.utils.parseEther("0.001"); // 0.001 ETH
 const numTransactions = 10;
 
+// Get the balance of the account
+signer.getBalance().then((balance) => {
+  // Convert the balance to ether and log it
+  const balanceInEth = ethers.utils.formatEther(balance);
+  console.log(`Account balance: ${balanceInEth} ETH`);
+}).catch((err) => {
+  console.error(`Error getting account balance: ${err.message}`);
+});
+
 // Send the transactions
 for (let i = 0; i < numTransactions; i++) {
   // Generate a random address to send to
