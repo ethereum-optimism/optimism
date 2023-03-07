35ef587bbf6fc5c408ee1aeef75ea20caff25b73
Andreas Bigger <abigger87@gmail.com>
2023-03-07 13:02:00 -0500

    peer params testing :test_tube:

--- op-node/p2p/peer_params_test.go
+++ op-node/p2p/peer_params_test.go
@@ -4,6 +4,7 @@ import (
 	"sort"
 	"testing"
 
+	pubsub "github.com/libp2p/go-libp2p-pubsub"
 	"github.com/stretchr/testify/suite"
 )
 
@@ -22,10 +23,31 @@ func TestPeerParams(t *testing.T) {
 	suite.Run(t, new(PeerParamsTestSuite))
 }
 
+// TestPeerScoreConstants validates the peer score constants.
+func (testSuite *PeerParamsTestSuite) TestPeerScoreConstants() {
+	testSuite.Equal(0.01, DecayToZero)
+}
+
 // TestAvailablePeerScoreParams validates the available peer score parameters.
 func (testSuite *PeerParamsTestSuite) TestAvailablePeerScoreParams() {
 	available := AvailablePeerScoreParams()
 	sort.Strings(available)
 	expected := []string{"light", "none"}
 	testSuite.Equal(expected, available)
+}
+
+// TestNewPeerScoreThresholds validates the peer score thresholds.
+//
+// This is tested to ensure that the thresholds are not modified and missed in review.
+func (testSuite *PeerParamsTestSuite) TestNewPeerScoreThresholds() {
+	thresholds := NewPeerScoreThresholds()
+	expected := pubsub.PeerScoreThresholds{
+		SkipAtomicValidation:        false,
+		GossipThreshold:             -10,
+		PublishThreshold:            -40,
+		GraylistThreshold:           -40,
+		AcceptPXThreshold:           20,
+		OpportunisticGraftThreshold: 0.05,
+	}
+	testSuite.Equal(expected, thresholds)
 }
