f2649ff743570c515a99c39fd2e4708b2161b6be
Andreas Bigger <abigger87@gmail.com>
2023-03-17 13:49:57 -0400

    finish driver e2e test

--- op-batcher/batcher/driver_test.go
+++ op-batcher/batcher/driver_test.go
@@ -29,7 +29,7 @@ func constructDefaultBatchSubmitter(l log.Logger, mockTxMgr ExternalTxManager, l
 		return nil, err
 	}
 
-	pollInterval, err := time.ParseDuration("10s")
+	pollInterval, err := time.ParseDuration("5s")
 	if err != nil {
 		return nil, err
 	}
@@ -57,8 +57,10 @@ func constructDefaultBatchSubmitter(l log.Logger, mockTxMgr ExternalTxManager, l
 			ChannelTimeout:     40,
 			MaxChannelDuration: 1,
 			SubSafetyMargin:    4,
-			// Set the max frame size to 1 so that we can test sending transactions
-			MaxFrameSize:     1,
+			// Set the max frame size to 24 so that we can test sending transactions
+			// The fixed overhead for frame size is 23, so we must be larger or else
+			// the uint64 will underflow, causing the frame size to be essentially unbound
+			MaxFrameSize:     24,
 			TargetFrameSize:  1,
 			TargetNumFrames:  1,
 			ApproxComprRatio: 0.4,
@@ -96,7 +98,6 @@ func TestDriverLoadBlocksIntoState(t *testing.T) {
 	rollupNode := mocks.RollupNodeConfigProvider{}
 	txMgr := mocks.ExternalTxManager{}
 	log := testlog.Logger(t, log.LvlCrit)
-	fmt.Printf("Constructing default batch submitter...\n")
 	b, err := constructDefaultBatchSubmitter(log, &txMgr, &l1Client, &l2Client, &rollupNode)
 	require.NoError(t, err)
 
@@ -118,12 +119,15 @@ func TestDriverLoadBlocksIntoState(t *testing.T) {
 			Time:       0,
 		},
 		UnsafeL2: eth.L2BlockRef{
-			Number:     101,
+			Number:     102,
 			Hash:       common.Hash{},
 			ParentHash: common.Hash{},
 			Time:       0,
 		},
-	}, nil).Once()
+	}, nil).Once().Run(func(args mock.Arguments) {
+		// In the first sync status call, nothing should be set
+		require.Equal(t, uint64(0), b.lastStoredBlock.Number)
+	})
 
 	// Mock the first L2 BlockByNumber call
 	oneZeroOne := new(big.Int).SetUint64(uint64(101))
@@ -133,21 +137,43 @@ func TestDriverLoadBlocksIntoState(t *testing.T) {
 	l1Block := types.NewBlock(&types.Header{
 		BaseFee:    big.NewInt(10),
 		Difficulty: common.Big0,
-		Number:     big.NewInt(100),
+		Number:     big.NewInt(101),
 	}, nil, nil, nil, trie.NewStackTrie(nil))
 	l1InfoTx, err := derive.L1InfoDeposit(0, l1Block, eth.SystemConfig{}, false)
 	require.NoError(t, err)
-	l1Client.On("HeaderByNumber", mock.Anything, mock.Anything).Return(&header, nil).Once()
-
 	txs := []*types.Transaction{types.NewTx(l1InfoTx)}
-	for i := 0; i < 500_000; i++ {
+	for i := 0; i < 100_000; i++ {
 		txData := make([]byte, 32)
 		_, _ = rand.Read(txData)
 		tx := types.NewTransaction(0, common.Address{}, big.NewInt(0), 0, big.NewInt(0), txData)
 		txs = append(txs, tx)
 	}
 	block := types.NewBlock(&header, txs, nil, nil, trie.NewStackTrie(nil))
-	l2Client.On("BlockByNumber", mock.Anything, oneZeroOne).Return(block, nil).Once()
+	oneZeroTwo := new(big.Int).SetUint64(uint64(102))
+	header2 := types.Header{
+		ParentHash: block.Hash(),
+		Number:     oneZeroTwo,
+	}
+	block2 := types.NewBlock(&header2, txs, nil, nil, trie.NewStackTrie(nil))
+	l2Client.On("BlockByNumber", mock.Anything, oneZeroOne).Return(block, nil).WaitUntil(time.After(2 * time.Second)).Once().Run(func(args mock.Arguments) {
+		// The last stored block should be set by the [SyncStatus] call
+		require.Equal(t, uint64(100), b.lastStoredBlock.Number)
+	})
+	l2Client.On("BlockByNumber", mock.Anything, oneZeroTwo).Return(block2, nil).Once().Run(func(args mock.Arguments) {
+		// The last stored block should be set by the [SyncStatus] call
+		require.Equal(t, uint64(101), b.lastStoredBlock.Number)
+	})
+
+	// Mock the [HeaderByNumber] call.
+	// This is triggered by the internal l1tip call in the batch submitter block loop.
+	// When this runs, the batch submitter should have already loaded blocks into state.
+	l1Client.On("HeaderByNumber", mock.Anything, mock.Anything).Return(&header, nil).Once().Run(func(args mock.Arguments) {
+		require.Equal(t, uint64(102), b.lastStoredBlock.Number)
+	})
+
+	// In between the header by number call and sending the transaction to the tx manager,
+	// The batch submitter's block loop should have collected the tx data from state.
+	// This will set the state's (Channel Manager's) pending transactions, blocks,
 
 	// Mock internal calls for the batch transaction manager's sending transaction
 	l1Client.On("SuggestGasTipCap", mock.Anything).Return(big.NewInt(10), nil).Once()
@@ -155,18 +181,14 @@ func TestDriverLoadBlocksIntoState(t *testing.T) {
 		BaseFee: big.NewInt(10),
 	}, nil).Once()
 	l1Client.On("NonceAt", mock.Anything, mock.Anything, mock.Anything).Return(uint64(0), nil).Once().Run(func(args mock.Arguments) {
-		// At this point, the batch submitter should have a pending transaction
-		// in the state.
-		fmt.Printf("Batch submitter pending transactions: %v\n", b.state.pendingTransactions)
-		require.Equal(t, 1, len(b.state.pendingTransactions))
-
-		// The number of frames should also be > 1, ie the batch submitter is not finished.
-		require.Greater(t, b.state.pendingChannel.NumFrames(), 1)
+		// require.Greater(t, b.state.pendingChannel.NumFrames(), 1)
 		require.False(t, b.state.pendingChannelIsFullySubmitted())
+
+		// At this point, the channel manager should have a pending transaction
+		require.Equal(t, 1, len(b.state.pendingTransactions))
 	})
 
 	// Block on the send call to allow
-	// .WaitUntil(time.After(100*time.Second))
 	txMgr.On("Send", mock.Anything, mock.Anything).Return(&types.Receipt{
 		TxHash:      common.Hash{},
 		BlockNumber: big.NewInt(1),
@@ -183,13 +205,13 @@ func TestDriverLoadBlocksIntoState(t *testing.T) {
 			Time:       0,
 		},
 		SafeL2: eth.L2BlockRef{
-			Number:     101,
+			Number:     102,
 			Hash:       common.Hash{},
 			ParentHash: common.Hash{},
 			Time:       0,
 		},
 		UnsafeL2: eth.L2BlockRef{
-			Number:     102,
+			Number:     103,
 			Hash:       common.Hash{},
 			ParentHash: common.Hash{},
 			Time:       0,
@@ -197,23 +219,34 @@ func TestDriverLoadBlocksIntoState(t *testing.T) {
 	}, nil).Once()
 
 	// Make the second L2 BlockByNumber call return a reorg error
-	oneZeroTwo := new(big.Int).SetUint64(uint64(102))
+	oneZeroThree := new(big.Int).SetUint64(uint64(103))
 	header = types.Header{
-		Number: oneZeroTwo,
+		Number: oneZeroThree,
 	}
-	l2Client.On("BlockByNumber", mock.Anything, oneZeroTwo).Return(nil, ErrReorg).Once()
-	l1Client.On("HeaderByNumber", mock.Anything, mock.Anything).Return(&header, nil).Once()
-	rollupNode.On("SyncStatus", mock.Anything).WaitUntil(time.After(10*time.Second)).Return(nil, nil).Once().Run(func(args mock.Arguments) {
-		fmt.Println("Batch submitter called sync status...")
+	var testPendingChannel *channelBuilder
+	l2Client.On("BlockByNumber", mock.Anything, oneZeroThree).Return(nil, ErrReorg).Once().Run(func(args mock.Arguments) {
+		// The pending channel should still have frames left to submit for the current channel
+		require.False(t, b.state.pendingChannelIsFullySubmitted())
+		require.True(t, b.state.pendingChannel.HasFrame())
+		require.Greater(t, b.state.pendingChannel.NumFrames(), 0)
+		testPendingChannel = b.state.pendingChannel
 	})
 	l1Client.On("HeaderByNumber", mock.Anything, mock.Anything).Return(&header, nil).Once().Run(func(args mock.Arguments) {
-		fmt.Println("Batch submitter called header by number...")
+		b.cancel()
+		b.loadBlocksIntoState(context.Background())
 		b.wg.Done()
 	})
+	rollupNode.On("SyncStatus", mock.Anything).Return(nil, fmt.Errorf("no-op")).Once()
+	l1Client.On("HeaderByNumber", mock.Anything, mock.Anything).Return(&header, nil).Once()
 
-	// Start the batch submitter
+	// Start the batch submitter and wait for the loop to close
 	err = b.Start()
 	require.NoError(t, err)
 	b.wg.Wait()
+
+	// The batch submitter should have wiped state
+	require.Nil(t, b.state.pendingChannel)
 	require.Equal(t, 0, len(b.state.pendingTransactions))
+	// But there should be a pending channel with these frames:
+	require.Greater(t, testPendingChannel.NumFrames(), 0)
 }
