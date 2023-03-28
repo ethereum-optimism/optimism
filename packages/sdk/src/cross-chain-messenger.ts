6fcdeaa0388751d11ed286efbc439f24e5d26004
Mark Tyneway <mark.tyneway@gmail.com>
2023-03-28 14:34:37 -0700

    sdk: fix linting
    Fix linting issues in the SDK. Ran `yarn lint --fix` and
    committed the diff.

--- packages/sdk/src/cross-chain-messenger.ts
+++ packages/sdk/src/cross-chain-messenger.ts
@@ -1004,18 +1004,23 @@ export class CrossChainMessenger {
    */
   public async getChallengePeriodSeconds(): Promise<number> {
     if (!this.bedrock) {
-      return (await this.contracts.l1.StateCommitmentChain.FRAUD_PROOF_WINDOW()).toNumber()
+      return (
+        await this.contracts.l1.StateCommitmentChain.FRAUD_PROOF_WINDOW()
+      ).toNumber()
     }
 
     const oracleVersion = await this.contracts.l1.L2OutputOracle.version()
-    const challengePeriod = oracleVersion === '1.0.0'
-      // The ABI in the SDK does not contain FINALIZATION_PERIOD_SECONDS
-      // in OptimismPortal, so making an explicit call instead.
-      ? BigNumber.from(await this.contracts.l1.OptimismPortal.provider.call({
-        to: this.contracts.l1.OptimismPortal.address,
-        data: '0xf4daa291' // FINALIZATION_PERIOD_SECONDS
-      }))
-      : await this.contracts.l1.L2OutputOracle.FINALIZATION_PERIOD_SECONDS()
+    const challengePeriod =
+      oracleVersion === '1.0.0'
+        ? // The ABI in the SDK does not contain FINALIZATION_PERIOD_SECONDS
+          // in OptimismPortal, so making an explicit call instead.
+          BigNumber.from(
+            await this.contracts.l1.OptimismPortal.provider.call({
+              to: this.contracts.l1.OptimismPortal.address,
+              data: '0xf4daa291', // FINALIZATION_PERIOD_SECONDS
+            })
+          )
+        : await this.contracts.l1.L2OutputOracle.FINALIZATION_PERIOD_SECONDS()
     return challengePeriod.toNumber()
   }
 
