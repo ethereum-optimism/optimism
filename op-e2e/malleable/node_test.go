aa42070c6c94de23583e89fd15176f1b6f1537ab
Andreas Bigger <abigger87@gmail.com>
2023-03-29 20:23:57 -0400

    start peer score test

--- op-e2e/malleable/node_test.go
+++ op-e2e/malleable/node_test.go
@@ -7,11 +7,8 @@ import (
 	"testing"
 	"time"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/types"
 	ethCrypto "github.com/ethereum/go-ethereum/crypto"
 	log "github.com/ethereum/go-ethereum/log"
-	"github.com/holiman/uint256"
 	crypto "github.com/libp2p/go-libp2p/core/crypto"
 	network "github.com/libp2p/go-libp2p/core/network"
 	peer "github.com/libp2p/go-libp2p/core/peer"
@@ -57,11 +54,13 @@ func TestMalleable_PublishPayload(t *testing.T) {
 	cid := big.NewInt(420)
 	l := testlog.Logger(t, log.LvlInfo)
 
+	// Construct the first malleable node
 	p, _, err := crypto.GenerateSecp256k1Key(rand.Reader)
 	require.NoError(t, err, "failed to generate new p2p priv key")
 	m, err := NewMalleable(l, cid, nil, p, OnUnsafeL2Payload)
 	require.NoError(t, err, "failed to create new malleable node")
 
+	// Construct the second malleable node
 	receivedBlockNumber := eth.Uint64Quantity(0)
 	m2BlocksCallback := func(ctx context.Context, from peer.ID, payload *eth.ExecutionPayload) error {
 		receivedBlockNumber = payload.BlockNumber
@@ -73,50 +72,22 @@ func TestMalleable_PublishPayload(t *testing.T) {
 	m2, err := NewMalleable(l, cid, nil, p2, m2BlocksCallback)
 	require.NoError(t, err, "failed to create new malleable node")
 
+	// Connect the nodes
 	err = m.h.Connect(context.Background(), peer.AddrInfo{ID: m2.ID(), Addrs: m2.Addrs()})
 	require.NoError(t, err, "failed to connect the hosts")
 
+	// Construct a payload
 	testKey := "289c2857d4598e37fb9647507e47a309d6133539bf21a8b9cb6df88fd5232032"
 	priv, err := ethCrypto.HexToECDSA(testKey)
 	require.NoError(t, err, "failed to build private key from hex string")
 	signer := &p2p.PreparedSigner{Signer: p2p.NewLocalSigner(priv)}
-	randomTx := types.NewTx(&types.DynamicFeeTx{
-		ChainID:   big.NewInt(420),
-		GasTipCap: big.NewInt(1),
-		GasFeeCap: big.NewInt(1),
-		Data:      []byte("hello world"),
-	})
-	// executionPayloadFixedPart := 32 + 20 + 32 + 32 + 256 + 32 + 8 + 8 + 8 + 8 + 4 + 32 + 32 + 4
 	payload := &eth.ExecutionPayload{
-		ParentHash:    common.HexToHash("0x5c698f13940a2153440c6d19660878bc90219d9298fdcf37365aa8d88d40fc42"),
-		FeeRecipient:  common.HexToAddress("0x376c47978271565f56DEB45495afa69E59c16Ab2"),
-		StateRoot:     eth.Bytes32{},
-		ReceiptsRoot:  eth.Bytes32{},
-		LogsBloom:     eth.Bytes256{},
-		PrevRandao:    eth.Bytes32{},
-		BlockNumber:   eth.Uint64Quantity(1),
-		GasLimit:      eth.Uint64Quantity(1337),
-		GasUsed:       0,
-		Timestamp:     eth.Uint64Quantity(1),
-		ExtraData:     []byte("hello world"), // make([]byte, 100000), // math.MaxUint32-executionPayloadFixedPart),
-		BaseFeePerGas: *uint256.NewInt(7),
-		BlockHash:     common.HexToHash("0x5c698f13940a2153440c6d19660878bc90219d9298fdcf37365aa8d88d40fc42"),
-		Transactions: []eth.Data{
-			randomTx.Data(),
-		},
+		BlockNumber: eth.Uint64Quantity(1),
 	}
 
+	// Publish the payload and give it time to be received
+	require.Zero(t, receivedBlockNumber)
 	m.PublishL2Payload(context.Background(), payload, signer)
-
-	// Wait for the payload to be gossiped
 	time.Sleep(1 * time.Second)
-
-	// This should have been received
 	require.Equal(t, receivedBlockNumber, eth.Uint64Quantity(1))
-
-	// Check that the payload was received by the other node at the other end of the pubsub topic.
-	require.Equal(t, m.h.Network().Connectedness(m2.ID()), network.Connected)
-	require.Equal(t, m2.h.Network().Connectedness(m.ID()), network.Connected)
-	require.Equal(t, m.h.Peerstore().Peers().Len(), 2)
-	require.Equal(t, m2.h.Peerstore().Peers().Len(), 2)
 }
---
+++ op-e2e/peer_score_test.go
@@ -0,0 +1,121 @@
+package op_e2e
+
+import (
+	"context"
+	"crypto/rand"
+	"fmt"
+	"math/big"
+	"testing"
+	"time"
+
+	common "github.com/ethereum/go-ethereum/common"
+	types "github.com/ethereum/go-ethereum/core/types"
+	ethCrypto "github.com/ethereum/go-ethereum/crypto"
+	log "github.com/ethereum/go-ethereum/log"
+	crypto "github.com/libp2p/go-libp2p/core/crypto"
+	network "github.com/libp2p/go-libp2p/core/network"
+	peer "github.com/libp2p/go-libp2p/core/peer"
+	require "github.com/stretchr/testify/require"
+	"golang.org/x/exp/slices"
+
+	malleable "github.com/ethereum-optimism/optimism/op-e2e/malleable"
+	eth "github.com/ethereum-optimism/optimism/op-node/eth"
+	p2p "github.com/ethereum-optimism/optimism/op-node/p2p"
+	testlog "github.com/ethereum-optimism/optimism/op-node/testlog"
+)
+
+// TestMalleable_PeerScoreUpdated tests publishing an [eth.ExecutionPayload]
+// through the [Malleable] node to the [pubsub.Topic].
+func TestMalleable_PeerScoreUpdated(t *testing.T) {
+	// Setup the default system first
+	cfg := DefaultSystemConfig(t)
+	cfg.P2PTopology = map[string][]string{
+		"verifier": {"sequencer"},
+	}
+	sys, err := cfg.Start()
+	require.Nil(t, err, "Error starting up system")
+	defer sys.Close()
+
+	// Give time for the system to start up
+	fmt.Println()
+	fmt.Println()
+	fmt.Println("Waiting for system to start up...")
+	fmt.Println()
+	fmt.Println()
+	time.Sleep(10 * time.Second)
+	fmt.Println()
+	fmt.Println()
+	fmt.Printf("sys rollup nodes: %v\n", sys.RollupNodes)
+	fmt.Println()
+	fmt.Println()
+	fmt.Printf("sys verifier node: %v\n", sys.RollupNodes["verifier"])
+	fmt.Println()
+	fmt.Println()
+	fmt.Printf("sys verifier node p2p: %v\n", sys.RollupNodes["verifier"].P2P())
+	fmt.Println()
+	fmt.Println()
+	fmt.Printf("sys verifier node p2p host: %v\n", sys.RollupNodes["verifier"].P2P().Host())
+	fmt.Println()
+	fmt.Println()
+	honestNodeId := sys.RollupNodes["verifier"].P2P().Host().ID()
+	honestNodeAddrs := sys.RollupNodes["verifier"].P2P().Host().Addrs()
+	fmt.Printf("Honest Rollup Node ID: %v\n", honestNodeId)
+
+	// Grab the chain id from the system config
+	cid := cfg.DeployConfig.L2ChainID
+	l := testlog.Logger(t, log.LvlInfo)
+	onUnsafeL2Payload := func(ctx context.Context, from peer.ID, payload *eth.ExecutionPayload) error {
+		return nil
+	}
+
+	// Construct a new malleable node.
+	p, _, err := crypto.GenerateSecp256k1Key(rand.Reader)
+	require.NoError(t, err, "failed to generate new p2p priv key")
+	m, err := malleable.NewMalleable(l, big.NewInt(int64(cid)), nil, p, onUnsafeL2Payload)
+	require.NoError(t, err, "failed to create new malleable node")
+	malleableNodeId := m.ID()
+
+	// Connect the Malleable node to the honest rollup node
+	err = m.Connect(context.Background(), peer.AddrInfo{ID: honestNodeId, Addrs: honestNodeAddrs})
+	require.NoError(t, err, "failed to connect the hosts")
+	require.Equal(t, m.Network().Connectedness(m.ID()), network.Connected)
+
+	// Construct an execution payload
+	testKey := "289c2857d4598e37fb9647507e47a309d6133539bf21a8b9cb6df88fd5232032"
+	priv, err := ethCrypto.HexToECDSA(testKey)
+	require.NoError(t, err, "failed to build private key from hex string")
+	signer := &p2p.PreparedSigner{Signer: p2p.NewLocalSigner(priv)}
+	randomTx := types.NewTx(&types.DynamicFeeTx{
+		ChainID:   big.NewInt(int64(cid)),
+		GasTipCap: big.NewInt(1),
+		GasFeeCap: big.NewInt(1),
+		Data:      []byte("hello world"),
+	})
+	payload := &eth.ExecutionPayload{
+		ParentHash:   common.HexToHash("0x5c698f13940a2153440c6d19660878bc90219d9298fdcf37365aa8d88d40fc42"),
+		FeeRecipient: common.HexToAddress("0x376c47978271565f56DEB45495afa69E59c16Ab2"),
+		StateRoot:    eth.Bytes32{},
+		ReceiptsRoot: eth.Bytes32{},
+		LogsBloom:    eth.Bytes256{},
+		PrevRandao:   eth.Bytes32{},
+		BlockNumber:  eth.Uint64Quantity(1),
+		GasLimit:     eth.Uint64Quantity(1337),
+		GasUsed:      0,
+		Timestamp:    eth.Uint64Quantity(1),
+		ExtraData:    []byte("hello world"),
+		// BaseFeePerGas: *uint256.NewInt(7),
+		BlockHash: common.HexToHash("0x5c698f13940a2153440c6d19660878bc90219d9298fdcf37365aa8d88d40fc42"),
+		Transactions: []eth.Data{
+			randomTx.Data(),
+		},
+	}
+
+	for {
+		// Publish the payload
+		m.PublishL2Payload(context.Background(), payload, signer)
+		time.Sleep(1 * time.Second)
+		// Get the peer score of the honest node
+		connected := slices.Contains(sys.RollupNodes["verifier"].P2P().Host().Peerstore().Peers(), malleableNodeId)
+		fmt.Printf("Is malleable node still connected to honest node: %v\n", connected)
+	}
+}
