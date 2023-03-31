1eb6a8ed1b063f276caa0e39b5ed17d018ac3c08
Andreas Bigger <abigger87@gmail.com>
2023-03-30 21:19:52 -0400

    update bindings

--- op-bindings/bindings/optimismmintableerc721factory.go
+++ op-bindings/bindings/optimismmintableerc721factory.go
@@ -26,6 +26,7 @@ var (
 	_ = common.Big1
 	_ = types.BloomLookup
 	_ = event.NewSubscription
+	_ = abi.ConvertType
 )
 
 // OptimismMintableERC721FactoryMetaData contains all meta data concerning the OptimismMintableERC721Factory contract.
@@ -156,11 +157,11 @@ func NewOptimismMintableERC721FactoryFilterer(address common.Address, filterer b
 
 // bindOptimismMintableERC721Factory binds a generic wrapper to an already deployed contract.
 func bindOptimismMintableERC721Factory(address common.Address, caller bind.ContractCaller, transactor bind.ContractTransactor, filterer bind.ContractFilterer) (*bind.BoundContract, error) {
-	parsed, err := abi.JSON(strings.NewReader(OptimismMintableERC721FactoryABI))
+	parsed, err := OptimismMintableERC721FactoryMetaData.GetAbi()
 	if err != nil {
 		return nil, err
 	}
-	return bind.NewBoundContract(address, parsed, caller, transactor, filterer), nil
+	return bind.NewBoundContract(address, *parsed, caller, transactor, filterer), nil
 }
 
 // Call invokes the (constant) contract method with params as input values and
