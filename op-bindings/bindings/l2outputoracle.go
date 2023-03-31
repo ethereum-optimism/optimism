6f23d224c63b250ff6fee2e7c94efa65e6a033f7
Andreas Bigger <abigger87@gmail.com>
2023-03-31 16:47:08 -0400

    revert bindings changes

--- op-bindings/bindings/l2outputoracle.go
+++ op-bindings/bindings/l2outputoracle.go
@@ -26,7 +26,6 @@ var (
 	_ = common.Big1
 	_ = types.BloomLookup
 	_ = event.NewSubscription
-	_ = abi.ConvertType
 )
 
 // TypesOutputProposal is an auto generated low-level Go binding around an user-defined struct.
@@ -164,11 +163,11 @@ func NewL2OutputOracleFilterer(address common.Address, filterer bind.ContractFil
 
 // bindL2OutputOracle binds a generic wrapper to an already deployed contract.
 func bindL2OutputOracle(address common.Address, caller bind.ContractCaller, transactor bind.ContractTransactor, filterer bind.ContractFilterer) (*bind.BoundContract, error) {
-	parsed, err := L2OutputOracleMetaData.GetAbi()
+	parsed, err := abi.JSON(strings.NewReader(L2OutputOracleABI))
 	if err != nil {
 		return nil, err
 	}
-	return bind.NewBoundContract(address, *parsed, caller, transactor, filterer), nil
+	return bind.NewBoundContract(address, parsed, caller, transactor, filterer), nil
 }
 
 // Call invokes the (constant) contract method with params as input values and
