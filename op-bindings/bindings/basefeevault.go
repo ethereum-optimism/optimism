e5a517957a87d5c1820775952a7ca694016eb66b
Andreas Bigger <abigger87@gmail.com>
2023-03-30 21:19:52 -0400

    update bindings

--- op-bindings/bindings/basefeevault.go
+++ op-bindings/bindings/basefeevault.go
@@ -26,6 +26,7 @@ var (
 	_ = common.Big1
 	_ = types.BloomLookup
 	_ = event.NewSubscription
+	_ = abi.ConvertType
 )
 
 // BaseFeeVaultMetaData contains all meta data concerning the BaseFeeVault contract.
@@ -156,11 +157,11 @@ func NewBaseFeeVaultFilterer(address common.Address, filterer bind.ContractFilte
 
 // bindBaseFeeVault binds a generic wrapper to an already deployed contract.
 func bindBaseFeeVault(address common.Address, caller bind.ContractCaller, transactor bind.ContractTransactor, filterer bind.ContractFilterer) (*bind.BoundContract, error) {
-	parsed, err := abi.JSON(strings.NewReader(BaseFeeVaultABI))
+	parsed, err := BaseFeeVaultMetaData.GetAbi()
 	if err != nil {
 		return nil, err
 	}
-	return bind.NewBoundContract(address, parsed, caller, transactor, filterer), nil
+	return bind.NewBoundContract(address, *parsed, caller, transactor, filterer), nil
 }
 
 // Call invokes the (constant) contract method with params as input values and
--- op-bindings/bindings/sequencerfeevault.go
+++ op-bindings/bindings/sequencerfeevault.go
@@ -26,6 +26,7 @@ var (
 	_ = common.Big1
 	_ = types.BloomLookup
 	_ = event.NewSubscription
+	_ = abi.ConvertType
 )
 
 // SequencerFeeVaultMetaData contains all meta data concerning the SequencerFeeVault contract.
@@ -156,11 +157,11 @@ func NewSequencerFeeVaultFilterer(address common.Address, filterer bind.Contract
 
 // bindSequencerFeeVault binds a generic wrapper to an already deployed contract.
 func bindSequencerFeeVault(address common.Address, caller bind.ContractCaller, transactor bind.ContractTransactor, filterer bind.ContractFilterer) (*bind.BoundContract, error) {
-	parsed, err := abi.JSON(strings.NewReader(SequencerFeeVaultABI))
+	parsed, err := SequencerFeeVaultMetaData.GetAbi()
 	if err != nil {
 		return nil, err
 	}
-	return bind.NewBoundContract(address, parsed, caller, transactor, filterer), nil
+	return bind.NewBoundContract(address, *parsed, caller, transactor, filterer), nil
 }
 
 // Call invokes the (constant) contract method with params as input values and
