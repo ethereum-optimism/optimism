4131f966dbd40d5837c15ea62b321b411996e9d1
James Kim <jakim929@gmail.com>
2023-04-03 17:46:54 -0400

    remove unused import

--- packages/contracts-periphery/contracts/testing/helpers/ExternalContractCompiler.sol
+++ packages/contracts-periphery/contracts/testing/helpers/ExternalContractCompiler.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.0;
 
 import { ProxyAdmin } from "@eth-optimism/contracts-bedrock/contracts/universal/ProxyAdmin.sol";
 import { Proxy } from "@eth-optimism/contracts-bedrock/contracts/universal/Proxy.sol";
-import { Multicall3 } from "multicall/src/Multicall3.sol";
 
 /**
  * Just exists so we can compile external contracts.
