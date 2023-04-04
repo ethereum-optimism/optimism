5dfe3102080eda0f56af469e1e6e635d11b4bb95
Adrian Sutton <adrian@oplabs.co>
2023-04-04 12:40:09 +1000

    op-program: More tests

--- op-program/l2/engine_backend_test.go
+++ op-program/l2/engine_backend_test.go
@@ -5,6 +5,7 @@ import (
 	"math/big"
 	"testing"
 
+	"github.com/ethereum-optimism/optimism/op-chain-ops/genesis"
 	"github.com/ethereum-optimism/optimism/op-node/testlog"
 	"github.com/ethereum-optimism/optimism/op-program/l2/engineapi"
 	"github.com/ethereum-optimism/optimism/op-program/l2/engineapi/test"
@@ -13,9 +14,9 @@ import (
 	"github.com/ethereum/go-ethereum/core"
 	"github.com/ethereum/go-ethereum/core/rawdb"
 	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/ethdb"
 	"github.com/ethereum/go-ethereum/log"
 	"github.com/ethereum/go-ethereum/params"
-	"github.com/ethereum/go-ethereum/trie"
 	"github.com/stretchr/testify/require"
 )
 
@@ -31,7 +32,9 @@ func TestGetBlocks(t *testing.T) {
 	blocks, chain := setupOracleBackedChain(t, 5)
 
 	for i, block := range blocks {
-		assertBlockDataAvailable(t, chain, block, uint64(i))
+		blockNumber := uint64(i)
+		assertBlockDataAvailable(t, chain, block, blockNumber)
+		require.Equal(t, block.Hash(), chain.GetCanonicalHash(blockNumber), "get canonical hash for block %v", blockNumber)
 	}
 }
 
@@ -57,13 +60,61 @@ func TestCanonicalHashNotFoundPastChainHead(t *testing.T) {
 	require.Equal(t, common.Hash{}, chain.GetCanonicalHash(5))
 }
 
+func TestAppendToChain(t *testing.T) {
+	blocks, chain := setupOracleBackedChainWithLowerHead(t, 4, 3)
+	newBlock := blocks[4]
+	require.Nil(t, chain.GetBlockByHash(newBlock.Hash()), "block unknown before being added")
+
+	require.NoError(t, chain.InsertBlockWithoutSetHead(newBlock))
+	require.Equal(t, blocks[3].Header(), chain.CurrentBlock(), "should not update chain head yet")
+	require.Equal(t, common.Hash{}, chain.GetCanonicalHash(uint64(4)), "not yet a canonical hash")
+	assertBlockDataAvailable(t, chain, newBlock, 4)
+
+	canonical, err := chain.SetCanonical(newBlock)
+	require.NoError(t, err)
+	require.Equal(t, newBlock.Hash(), canonical)
+	require.Equal(t, newBlock.Hash(), chain.GetCanonicalHash(uint64(4)), "get canonical hash for new head")
+}
+
+func TestSetFinalized(t *testing.T) {
+	blocks, chain := setupOracleBackedChainWithLowerHead(t, 5, 0)
+	for _, block := range blocks[1:] {
+		require.NoError(t, chain.InsertBlockWithoutSetHead(block))
+	}
+	chain.SetFinalized(blocks[2].Header())
+	require.Equal(t, blocks[2].Header(), chain.CurrentFinalBlock())
+}
+
+func TestSetSafe(t *testing.T) {
+	blocks, chain := setupOracleBackedChainWithLowerHead(t, 5, 0)
+	for _, block := range blocks[1:] {
+		require.NoError(t, chain.InsertBlockWithoutSetHead(block))
+	}
+	chain.SetSafe(blocks[2].Header())
+	require.Equal(t, blocks[2].Header(), chain.CurrentSafeBlock())
+}
+
+func TestUpdateStateDatabaseWhenImportingBlock(t *testing.T) {
+	blocks, chain := setupOracleBackedChainWithLowerHead(t, 4, 3)
+	newBlock := blocks[4]
+
+	state, err := chain.StateAt(blocks[0].Root())
+	require.NoError(t, err)
+	balance := state.GetBalance(genesis.DevAccounts[0])
+	require.NotEqual(t, balance, big.NewInt(0), "should have balance at imported block")
+
+	state, err = chain.StateAt(newBlock.Root())
+	require.NoError(t, err)
+	balance = state.GetBalance(genesis.DevAccounts[0])
+	require.Equal(t, balance, big.NewInt(0), "should not have balance from not-yet-imported block")
+}
+
 func assertBlockDataAvailable(t *testing.T, chain *OracleBackedL2Chain, block *types.Block, blockNumber uint64) {
 	require.Equal(t, block, chain.GetBlockByHash(block.Hash()), "get block %v by hash", blockNumber)
 	require.Equal(t, block.Header(), chain.GetHeaderByHash(block.Hash()), "get header %v by hash", blockNumber)
 	require.Equal(t, block, chain.GetBlock(block.Hash(), blockNumber), "get block %v by hash and number", blockNumber)
 	require.Equal(t, block.Header(), chain.GetHeader(block.Hash(), blockNumber), "get header %v by hash and number", blockNumber)
 	require.True(t, chain.HasBlockAndState(block.Hash(), blockNumber), "has block and state for block %v", blockNumber)
-	require.Equal(t, block.Hash(), chain.GetCanonicalHash(blockNumber), "get canonical hash for block %v", blockNumber)
 }
 
 func setupOracleBackedChain(t *testing.T, blockCount int) ([]*types.Block, *OracleBackedL2Chain) {
@@ -72,75 +123,53 @@ func setupOracleBackedChain(t *testing.T, blockCount int) ([]*types.Block, *Orac
 
 func setupOracleBackedChainWithLowerHead(t *testing.T, blockCount int, headBlockNumber int) ([]*types.Block, *OracleBackedL2Chain) {
 	logger := testlog.Logger(t, log.LvlDebug)
-	chainCfg, blocks, oracle := setupOracle(blockCount)
+	chainCfg, blocks, oracle := setupOracle(t, blockCount, headBlockNumber)
 	head := blocks[headBlockNumber].Hash()
 	chain, err := NewOracleBackedL2Chain(context.Background(), logger, oracle, chainCfg, head)
 	require.NoError(t, err)
 	return blocks, chain
 }
 
-func setupOracle(blockCount int) (*params.ChainConfig, []*types.Block, *stubOracle) {
-	regolithTime := uint64(0)
-	chainCfg := &params.ChainConfig{
-		ChainID:                       big.NewInt(4),
-		HomesteadBlock:                big.NewInt(0),
-		DAOForkBlock:                  nil,
-		DAOForkSupport:                false,
-		EIP150Block:                   big.NewInt(0),
-		EIP150Hash:                    common.Hash{},
-		EIP155Block:                   big.NewInt(0),
-		EIP158Block:                   big.NewInt(0),
-		ByzantiumBlock:                big.NewInt(0),
-		ConstantinopleBlock:           big.NewInt(0),
-		PetersburgBlock:               big.NewInt(0),
-		IstanbulBlock:                 big.NewInt(0),
-		MuirGlacierBlock:              big.NewInt(0),
-		BerlinBlock:                   big.NewInt(0),
-		LondonBlock:                   big.NewInt(0),
-		ArrowGlacierBlock:             big.NewInt(0),
-		GrayGlacierBlock:              big.NewInt(0),
-		MergeNetsplitBlock:            big.NewInt(0),
-		TerminalTotalDifficulty:       common.Big0,
-		TerminalTotalDifficultyPassed: true,
-		Optimism: &params.OptimismConfig{
-			EIP1559Denominator: uint64(7),
-			EIP1559Elasticity:  uint64(2),
-		},
-		BedrockBlock: big.NewInt(0),
-		RegolithTime: &regolithTime,
+func setupOracle(t *testing.T, blockCount int, headBlockNumber int) (*params.ChainConfig, []*types.Block, *stubOracle) {
+	deployConfig := &genesis.DeployConfig{
+		L1ChainID:       900,
+		L2ChainID:       901,
+		L2BlockTime:     2,
+		FundDevAccounts: true,
 	}
-	// Set minimal amount of stuff to avoid nil references later
-	genesisBlock := types.NewBlock(&types.Header{
-		Difficulty: common.Big0,
-		Number:     common.Big0,
-		GasLimit:   30_000_000,
-		GasUsed:    0,
-		Time:       10000,
-		BaseFee:    big.NewInt(7),
-	}, nil, nil, nil, trie.NewStackTrie(nil))
+	l1Genesis, err := genesis.NewL1Genesis(deployConfig)
+	require.NoError(t, err)
+	l2Genesis, err := genesis.NewL2Genesis(deployConfig, l1Genesis.ToBlock())
+	chainCfg := l2Genesis.Config
 	consensus := beacon.New(nil)
 	db := rawdb.NewMemoryDatabase()
+
+	// Set minimal amount of stuff to avoid nil references later
+	genesisBlock := l2Genesis.MustCommit(db)
 	blocks, _ := core.GenerateChain(chainCfg, genesisBlock, consensus, db, blockCount, func(i int, gen *core.BlockGen) {})
 	blocks = append([]*types.Block{genesisBlock}, blocks...)
-	oracle := newStubOracle(blocks)
+	oracle := newStubOracle(blocks[:headBlockNumber+1], db)
 	return chainCfg, blocks, oracle
 }
 
 type stubOracle struct {
 	blocks map[common.Hash]*types.Block
+	db     ethdb.Database
 }
 
-func newStubOracle(chain []*types.Block) *stubOracle {
+func newStubOracle(chain []*types.Block, db ethdb.Database) *stubOracle {
 	blocks := make(map[common.Hash]*types.Block, len(chain))
 	for _, block := range chain {
 		blocks[block.Hash()] = block
 	}
-	return &stubOracle{blocks: blocks}
+	return &stubOracle{
+		blocks: blocks,
+		db:     db,
+	}
 }
 
 func (o stubOracle) NodeByHash(ctx context.Context, nodeHash common.Hash) ([]byte, error) {
-	//TODO implement me
-	panic("implement me")
+	return o.db.Get(nodeHash[:])
 }
 
 func (o stubOracle) BlockByHash(ctx context.Context, blockHash common.Hash) (*types.Block, error) {
@@ -148,8 +177,6 @@ func (o stubOracle) BlockByHash(ctx context.Context, blockHash common.Hash) (*ty
 }
 
 func TestEngineAPITests(t *testing.T) {
-	// TODO: Unskip these
-	//t.Skip("Not enough functionality supported to run these")
 	test.RunEngineAPITests(t, func() engineapi.EngineBackend {
 		_, chain := setupOracleBackedChain(t, 0)
 		return chain
