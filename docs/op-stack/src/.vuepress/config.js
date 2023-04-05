7c999ddda50c4f321da58bebca470b66d46e84b8
Ori Pomerantz <qbzzt1@gmail.com>
2023-04-05 13:15:04 -0500

    feat(docs/op-stack): Add forced withdrawals
    Merge after https://github.com/ethereum-optimism/optimism-tutorial/pull/178
    
    Closing: DEVRL-783

--- docs/op-stack/src/.vuepress/config.js
+++ docs/op-stack/src/.vuepress/config.js
@@ -186,6 +186,7 @@ module.exports = {
         children: [
           '/docs/security/faq.md',
           '/docs/security/policy.md',
+          '/docs/security/forced-withdrawal.md'
         ]
       },
     ],  // end of sidebar
