51fda2766b9bed8efc701a06f80887a044228fc6
Maurelian <maurelian@protonmail.ch>
2023-04-13 15:04:37 -0400

    feat(ctb): Immediately fail any message upon reentry

--- packages/contracts-bedrock/contracts/test/CrossDomainMessenger.t.sol
+++ packages/contracts-bedrock/contracts/test/CrossDomainMessenger.t.sol
@@ -2,6 +2,12 @@
 pragma solidity 0.8.15;
 
 import { Messenger_Initializer, Reverter, CallerCaller } from "./CommonTest.t.sol";
+import { L1CrossDomainMessenger } from "../L1/L1CrossDomainMessenger.sol";
+
+// Libraries
+import { Predeploys } from "../libraries/Predeploys.sol";
+import { Hashing } from "../libraries/Hashing.sol";
+import { Encoding } from "../libraries/Encoding.sol";
 
 // CrossDomainMessenger_Test is for testing functionality which is common to both the L1 and L2
 // CrossDomainMessenger contracts. For simplicity, we use the L1 Messenger as the test contract.
@@ -15,5 +21,98 @@ contract CrossDomainMessenger_BaseGas_Test is Messenger_Initializer {
     // Fuzz for other values which might cause a revert in baseGas.
     function testFuzz_baseGas_succeeds(uint32 _minGasLimit) external view {
         L1Messenger.baseGas(hex"ff", _minGasLimit);
+    }
+}
+
+// CrossDomainMessenger_RelayMessage_Test tests re-entrency of relayMessage.
+contract CrossDomainMessenger_RelayMessage_Test is Messenger_Initializer {
+    // Storage slot of the l2Sender
+    uint256 constant senderSlotIndex = 50;
+
+    address public fuzzedSender;
+    address public sender;
+
+    address public target;
+
+    // Internal helper function to relay a message and perform assertions.
+    function internalRelay(address innerSender) internal {
+        assertEq(sender, L1Messenger.xDomainMessageSender());
+
+        bytes memory callMessage = abi.encodeWithSelector(
+            CrossDomainMessenger_RelayMessage_Test.relayMessage.selector
+        );
+
+        bytes32 hash = Hashing.hashCrossDomainMessage(
+            Encoding.encodeVersionedNonce({ _nonce: 0, _version: 1 }),
+            innerSender,
+            target,
+            0,
+            0,
+            callMessage
+        );
+        vm.prank(address(op));
+        L1Messenger.relayMessage(
+            Encoding.encodeVersionedNonce({ _nonce: 0, _version: 1 }),
+            innerSender,
+            target,
+            0,
+            0,
+            callMessage
+        );
+
+        assertTrue(L1Messenger.failedMessages(hash));
+        assertFalse(L1Messenger.successfulMessages(hash));
+        assertEq(sender, L1Messenger.xDomainMessageSender());
+    }
+
+    // relayMessage is called by the L1 Cross Domain Messenger.
+    function relayMessage() external payable {
+        for (uint256 i = 0; i < 10; i++) {
+            address innerSender;
+            unchecked {
+                innerSender = address(uint160(uint256(uint160(fuzzedSender)) + i));
+            }
+            internalRelay(innerSender);
+        }
+    }
+
+    function testFuzz_relayMessageReenter_succeeds(address _sender) external {
+        vm.assume(_sender != Predeploys.L2_CROSS_DOMAIN_MESSENGER);
+        fuzzedSender = _sender;
+
+        target = address(this);
+        sender = Predeploys.L2_CROSS_DOMAIN_MESSENGER;
+        bytes memory callMessage = abi.encodeWithSelector(
+            CrossDomainMessenger_RelayMessage_Test.relayMessage.selector
+        );
+
+        vm.expectCall(target, callMessage);
+
+        bytes32 hash = Hashing.hashCrossDomainMessage(
+            Encoding.encodeVersionedNonce({ _nonce: 0, _version: 1 }),
+            sender,
+            target,
+            0,
+            0,
+            callMessage
+        );
+
+        // set the value of op.l2Sender() to be the L2 Cross Domain Messenger.
+        vm.store(address(op), bytes32(senderSlotIndex), bytes32(abi.encode(sender)));
+        vm.prank(address(op));
+        L1Messenger.relayMessage(
+            Encoding.encodeVersionedNonce({ _nonce: 0, _version: 1 }),
+            sender,
+            target,
+            0,
+            0,
+            callMessage
+        );
+
+        assert(L1Messenger.successfulMessages(hash));
+        assertEq(L1Messenger.failedMessages(hash), false);
+
+        vm.expectRevert("CrossDomainMessenger: xDomainMessageSender is not set");
+        L1Messenger.xDomainMessageSender();
     }
 }
--- packages/contracts-bedrock/contracts/universal/CrossDomainMessenger.sol
+++ packages/contracts-bedrock/contracts/universal/CrossDomainMessenger.sol
@@ -176,16 +176,11 @@ abstract contract CrossDomainMessenger is
     mapping(bytes32 => bool) public failedMessages;
 
     /**
-     * @notice A mapping of hashes to reentrancy locks.
-     */
-    mapping(bytes32 => bool) internal reentrancyLocks;
-
-    /**
      * @notice Reserve extra slots in the storage layout for future upgrades.
      *         A gap size of 41 was chosen here, so that the first slot used in a child contract
      *         would be a multiple of 50.
      */
-    uint256[41] private __gap;
+    uint256[42] private __gap;
 
     /**
      * @notice Emitted whenever a message is sent to the other chain.
@@ -323,13 +318,6 @@ abstract contract CrossDomainMessenger is
             _message
         );
 
-        // Check if the reentrancy lock for the `versionedHash` is already set.
-        if (reentrancyLocks[versionedHash]) {
-            revert("ReentrancyGuard: reentrant call");
-        }
-        // Trigger the reentrancy lock for `versionedHash`
-        reentrancyLocks[versionedHash] = true;
-
         if (_isOtherMessenger()) {
             // These properties should always hold when the message is first submitted (as
             // opposed to being replayed).
@@ -357,6 +345,12 @@ abstract contract CrossDomainMessenger is
             "CrossDomainMessenger: message has already been relayed"
         );
 
+        // Another message is being relayed, so we just store this for future replay.
+        if(xDomainMsgSender != Constants.DEFAULT_L2_SENDER) {
+            failedMessages[versionedHash] = true;
+            return;
+        }
+
         xDomainMsgSender = _sender;
         bool success = SafeCall.callWithMinGas(_target, _minGasLimit, _value, _message);
         xDomainMsgSender = Constants.DEFAULT_L2_SENDER;
@@ -377,9 +371,6 @@ abstract contract CrossDomainMessenger is
                 revert("CrossDomainMessenger: failed to relay message");
             }
         }
-
-        // Clear the reentrancy lock for `versionedHash`
-        reentrancyLocks[versionedHash] = false;
     }
 
     /**
