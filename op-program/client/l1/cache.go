319421c0ad811fdf13eec29aa96e08b2c6b41ddf
Adrian Sutton <adrian@oplabs.co>
2023-04-13 10:23:27 +1000

    op-program: Use an LRU for caches.

--- op-program/client/l1/cache.go
+++ op-program/client/l1/cache.go
@@ -4,53 +4,60 @@ import (
 	"github.com/ethereum-optimism/optimism/op-node/eth"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/hashicorp/golang-lru/v2/simplelru"
 )
+
+// Cache size is quite high as retrieving data from the pre-image oracle can be quite expensive
+const cacheSize = 2000
 
 // CachingOracle is an implementation of Oracle that delegates to another implementation, adding caching of all results
 type CachingOracle struct {
 	oracle Oracle
-	blocks map[common.Hash]eth.BlockInfo
-	txs    map[common.Hash]types.Transactions
-	rcpts  map[common.Hash]types.Receipts
+	blocks *simplelru.LRU[common.Hash, eth.BlockInfo]
+	txs    *simplelru.LRU[common.Hash, types.Transactions]
+	rcpts  *simplelru.LRU[common.Hash, types.Receipts]
 }
 
 func NewCachingOracle(oracle Oracle) *CachingOracle {
+	blockLRU, _ := simplelru.NewLRU[common.Hash, eth.BlockInfo](cacheSize, nil)
+	txsLRU, _ := simplelru.NewLRU[common.Hash, types.Transactions](cacheSize, nil)
+	rcptsLRU, _ := simplelru.NewLRU[common.Hash, types.Receipts](cacheSize, nil)
 	return &CachingOracle{
 		oracle: oracle,
-		blocks: make(map[common.Hash]eth.BlockInfo),
-		txs:    make(map[common.Hash]types.Transactions),
-		rcpts:  make(map[common.Hash]types.Receipts),
+		blocks: blockLRU,
+		txs:    txsLRU,
+		rcpts:  rcptsLRU,
 	}
 }
 
-func (o CachingOracle) HeaderByBlockHash(blockHash common.Hash) eth.BlockInfo {
-	block, ok := o.blocks[blockHash]
+func (o *CachingOracle) HeaderByBlockHash(blockHash common.Hash) eth.BlockInfo {
+	block, ok := o.blocks.Get(blockHash)
 	if ok {
 		return block
 	}
 	block = o.oracle.HeaderByBlockHash(blockHash)
-	o.blocks[blockHash] = block
+	o.blocks.Add(blockHash, block)
 	return block
 }
 
-func (o CachingOracle) TransactionsByBlockHash(blockHash common.Hash) (eth.BlockInfo, types.Transactions) {
-	txs, ok := o.txs[blockHash]
+func (o *CachingOracle) TransactionsByBlockHash(blockHash common.Hash) (eth.BlockInfo, types.Transactions) {
+	txs, ok := o.txs.Get(blockHash)
 	if ok {
 		return o.HeaderByBlockHash(blockHash), txs
 	}
 	block, txs := o.oracle.TransactionsByBlockHash(blockHash)
-	o.blocks[blockHash] = block
-	o.txs[blockHash] = txs
+	o.blocks.Add(blockHash, block)
+	o.txs.Add(blockHash, txs)
 	return block, txs
 }
 
-func (o CachingOracle) ReceiptsByBlockHash(blockHash common.Hash) (eth.BlockInfo, types.Receipts) {
-	rcpts, ok := o.rcpts[blockHash]
+func (o *CachingOracle) ReceiptsByBlockHash(blockHash common.Hash) (eth.BlockInfo, types.Receipts) {
+	rcpts, ok := o.rcpts.Get(blockHash)
 	if ok {
 		return o.HeaderByBlockHash(blockHash), rcpts
 	}
 	block, rcpts := o.oracle.ReceiptsByBlockHash(blockHash)
-	o.blocks[blockHash] = block
-	o.rcpts[blockHash] = rcpts
+	o.blocks.Add(blockHash, block)
+	o.rcpts.Add(blockHash, rcpts)
 	return block, rcpts
 }
