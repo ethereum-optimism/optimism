e1ce1a7aafbc6c92d18c246b90ca8cf21d4073c9
protolambda <proto@protolambda.com>
2023-04-17 15:46:33 +0200

    minigeth: remove more old minigeth files

--- sync_minigeth.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-FILES=$(grep -v "#" files_minigeth)
-MINIGETH=$PWD/minigeth
-git clone https://github.com/ethereum/go-ethereum.git
-cd go-ethereum
-git checkout 26675454bf93bf904be7a43cce6b3f550115ff90
-rsync -Rv $FILES $MINIGETH
