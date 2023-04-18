ba0754237f9b1dfd4ee173aeffbb32b04b267bf2
Michael de Hoog <michael.dehoog@coinbase.com>
2023-04-17 22:48:40 -0500

    Fix bug

--- op-batcher/batcher/channel_builder.go
+++ op-batcher/batcher/channel_builder.go
@@ -196,7 +196,7 @@ func (c *channelBuilder) AddBlock(block *types.Block) (derive.L1BlockInfo, error
 		return l1info, fmt.Errorf("converting block to batch: %w", err)
 	}
 
-	if _, err = c.co.AddBatch(batch); errors.Is(err, derive.ErrTooManyRLPBytes) {
+	if _, err = c.co.AddBatch(batch); errors.Is(err, derive.ErrTooManyRLPBytes) || errors.Is(err, derive.CompressorFullErr) {
 		c.setFullErr(err)
 		return l1info, c.FullErr()
 	} else if err != nil {
