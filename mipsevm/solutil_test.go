f629ad493335420394446e0a858c052c71b0d8ed
protolambda <proto@protolambda.com>
2023-04-24 11:39:35 +0200

    mipsevm: test sol trace util with MIPS.sol, remove Hello.sol

--- mipsevm/solutil_test.go
+++ mipsevm/solutil_test.go
@@ -8,9 +8,9 @@ import (
 )
 
 func TestSourcemap(t *testing.T) {
-	contract, err := LoadContract("Hello")
+	contract, err := LoadContract("MIPS")
 	require.NoError(t, err)
-	srcMap, err := contract.SourceMap([]string{"../contracts/src/Hello.sol"})
+	srcMap, err := contract.SourceMap([]string{"../contracts/src/MIPS.sol"})
 	require.NoError(t, err)
 	for i := 0; i < len(contract.DeployedBytecode.Object); i++ {
 		fmt.Println(srcMap.FormattedInfo(uint64(i)) + ": test")
