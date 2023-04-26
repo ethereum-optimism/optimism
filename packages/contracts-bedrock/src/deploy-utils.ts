97b9a0f99a174a8f714896c25ad0702a74736973
Maurelian <maurelian@protonmail.ch>
2023-04-25 20:55:31 -0400

    fix(ctb): Fix error on liveDeployer override

--- packages/contracts-bedrock/src/deploy-utils.ts
+++ packages/contracts-bedrock/src/deploy-utils.ts
@@ -366,12 +366,11 @@ export const liveDeployer = async (opts: {
   hre: HardhatRuntimeEnvironment
   disabled: string | undefined
 }): Promise<boolean> => {
-  let ret: boolean
   if (!!opts.disabled) {
-    ret = false
+    return false
   }
   const { deployer } = await opts.hre.getNamedAccounts()
-  ret =
+  const ret =
     deployer.toLowerCase() === opts.hre.deployConfig.controller.toLowerCase()
   console.log('Setting live deployer to', ret)
   return ret
