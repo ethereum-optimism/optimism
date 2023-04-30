935d06b00aa08715cacd60b2588ef733daf7b39c
Andreas Bigger <abigger87@gmail.com>
2023-04-29 18:27:34 -0600

    metrics updates

--- op-proposer/metrics/metrics.go
+++ op-proposer/metrics/metrics.go
@@ -2,6 +2,7 @@ package metrics
 
 import (
 	"context"
+	"math/big"
 
 	"github.com/ethereum-optimism/optimism/op-node/eth"
 
@@ -27,6 +28,8 @@ type Metricer interface {
 	txmetrics.TxMetricer
 
 	RecordL2BlocksProposed(l2ref eth.L2BlockRef)
+	RecordValidOutputAlreadyProposed(block *big.Int, output common.Hash)
+	RecordInvalidOutputAlreadyProposed(block *big.Int, output common.Hash)
 }
 
 type Metrics struct {
@@ -98,6 +101,8 @@ func (m *Metrics) RecordUp() {
 
 const (
 	BlockProposed = "proposed"
+	InvalidOutput = "invalid_output"
+	ValidOutput   = "valid_output"
 )
 
 // RecordL2BlocksProposed should be called when new L2 block is proposed
@@ -107,4 +112,22 @@ func (m *Metrics) RecordL2BlocksProposed(l2ref eth.L2BlockRef) {
 
 func (m *Metrics) Document() []opmetrics.DocumentedMetric {
 	return m.factory.Document()
+}
+
+// RecordValidOutputAlreadyProposed should be called when the proposer
+// sees an valid output root is already proposed for the given block.
+func (m *Metrics) RecordValidOutputAlreadyProposed(block *big.Int, output common.Hash) {
+	m.RecordL2Ref(ValidOutput, eth.L2BlockRef{
+		Number: block.Uint64(),
+		Hash:   output,
+	})
+}
+
+// RecordInvalidOutputAlreadyProposed should be called when the proposer
+// sees an invalid output root is already proposed for the given block.
+func (m *Metrics) RecordInvalidOutputAlreadyProposed(block *big.Int, output common.Hash) {
+	m.RecordL2Ref(InvalidOutput, eth.L2BlockRef{
+		Number: block.Uint64(),
+		Hash:   output,
+	})
 }
--- op-proposer/metrics/noop.go
+++ op-proposer/metrics/noop.go
@@ -1,9 +1,12 @@
 package metrics
 
 import (
+	"math/big"
+
 	"github.com/ethereum-optimism/optimism/op-node/eth"
 	opmetrics "github.com/ethereum-optimism/optimism/op-service/metrics"
 	txmetrics "github.com/ethereum-optimism/optimism/op-service/txmgr/metrics"
+	common "github.com/ethereum/go-ethereum/common"
 )
 
 type noopMetrics struct {
@@ -16,4 +19,6 @@ var NoopMetrics Metricer = new(noopMetrics)
 func (*noopMetrics) RecordInfo(version string) {}
 func (*noopMetrics) RecordUp()                 {}
 
-func (*noopMetrics) RecordL2BlocksProposed(l2ref eth.L2BlockRef) {}
+func (*noopMetrics) RecordL2BlocksProposed(l2ref eth.L2BlockRef)                           {}
+func (*noopMetrics) RecordValidOutputAlreadyProposed(block *big.Int, output common.Hash)   {}
+func (*noopMetrics) RecordInvalidOutputAlreadyProposed(block *big.Int, output common.Hash) {}
