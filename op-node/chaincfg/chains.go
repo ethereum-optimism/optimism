c3457b398cbd807284431eb776316feeb7e37aca
Andreas Bigger <abigger87@gmail.com>
2023-05-08 12:16:15 -0700

    Stub out mainnet chain config with moosey comments.

--- op-node/chaincfg/chains.go
+++ op-node/chaincfg/chains.go
@@ -10,6 +10,43 @@ import (
 	"github.com/ethereum-optimism/optimism/op-node/rollup"
 )
 
+var Mainnet = rollup.Config{
+	Genesis: rollup.Genesis{
+		L1: eth.BlockID{
+			// moose: Update this during migration
+			Hash: common.HexToHash("0x"),
+			// moose: Update this during migration
+			Number: 0,
+		},
+		L2: eth.BlockID{
+			// moose: Update this during migration
+			Hash: common.HexToHash("0x"),
+			// moose: Update this during migration
+			Number: 0,
+		},
+		// moose: Update this during migration
+		L2Time: 0,
+		SystemConfig: eth.SystemConfig{
+			BatcherAddr: common.HexToAddress("0x70997970C51812dc3A010C7d01b50e0d17dc79C8"),
+			Overhead:    eth.Bytes32(common.HexToHash("0x0000000000000000000000000000000000000000000000000000000000000834")),
+			Scalar:      eth.Bytes32(common.HexToHash("0x00000000000000000000000000000000000000000000000000000000000f4240")),
+			GasLimit:    25_000_000,
+		},
+	},
+	BlockTime:         2,
+	MaxSequencerDrift: 600,
+	SeqWindowSize:     3600,
+	ChannelTimeout:    300,
+	L1ChainID:         big.NewInt(1),
+	L2ChainID:         big.NewInt(10),
+	BatchInboxAddress: common.HexToAddress("0xff00000000000000000000000000000000000010"),
+	// moose: Update this during migration
+	DepositContractAddress: common.HexToAddress("0x"),
+	// moose: Update this during migration
+	L1SystemConfigAddress: common.HexToAddress("0x"),
+	RegolithTime:          u64Ptr(0),
+}
+
 var Goerli = rollup.Config{
 	Genesis: rollup.Genesis{
 		L1: eth.BlockID{
@@ -42,6 +79,8 @@ var Goerli = rollup.Config{
 
 var NetworksByName = map[string]rollup.Config{
 	"goerli": Goerli,
+	// moose: Update this during migration
+	// "mainnet": Mainnet,
 }
 
 var L2ChainIDToNetworkName = func() map[string]string {
