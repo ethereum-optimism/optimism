be71f0f145a050eb1e8dc5072ad05cc6c079aea0
tre <tremarkley@gmail.com>
2023-05-18 09:53:38 -0700

    test that balance updates properly

--- packages/contracts-periphery/tasks/FaucetTest.ts
+++ packages/contracts-periphery/tasks/FaucetTest.ts
@@ -6,28 +6,24 @@ import type { DeployConfig } from '../src'
 const main = async () => {
   const deployConfig = hre.deployConfig as DeployConfig
 
-  const [owner] = await hre.ethers.getSigners()
+  const [owner, nonAdmin] = await hre.ethers.getSigners()
 
   console.log('owner', owner.address)
   const optimistFamAddress = (
     await hre.deployments.get('OptimistAdminFaucetAuthModule')
   ).address
-  const OptimistFam = await hre.ethers.getContractAt(
-    'AdminFaucetAuthModule',
-    optimistFamAddress,
-    owner
-  )
   const faucetDeployment = await hre.deployments.get('Faucet')
   const Faucet = await hre.ethers.getContractAt(
     'Faucet',
     faucetDeployment.address,
-    owner
+    // owner
+    nonAdmin
   )
 
   // Send eth to faucet
   const transactionHash = await owner.sendTransaction({
     to: faucetDeployment.address,
-    value: hre.ethers.utils.parseEther('1.0'),
+    value: hre.ethers.utils.parseEther('10.0'),
   })
   await transactionHash.wait()
 
@@ -39,14 +35,16 @@ const main = async () => {
   )
   const proof = {
     recipient: owner.address,
+    // recipient: nonAdmin.address,
     nonce: encodedNonce,
     id: hre.ethers.utils.keccak256(owner.address),
+    // id: hre.ethers.utils.keccak256(nonAdmin.address),
   }
   const domain = {
     name: deployConfig.optimistFamName,
     version: deployConfig.optimistFamVersion,
     chainId: hre.network.config.chainId ?? 31337,
-    verifyingContract: OptimistFam.address,
+    verifyingContract: optimistFamAddress,
   }
   const types = {
     Proof: [
@@ -58,6 +56,7 @@ const main = async () => {
   const signature = await owner._signTypedData(domain, types, proof)
   const dripParams = {
     recipient: owner.address,
+    // recipient: nonAdmin.address,
     nonce: encodedNonce,
   }
   const authParams = {
@@ -65,8 +64,29 @@ const main = async () => {
     id: proof.id,
     proof: signature,
   }
+  // const ownerBalanceBefore = await owner.getBalance()
+  const ownerBalanceBefore = hre.ethers.utils.formatEther(
+    await hre.ethers.provider.getBalance(owner.address)
+  )
+  // const nonAdminBalanceBefore = await owner.getBalance()
+  const nonAdminBalanceBefore = hre.ethers.utils.formatEther(
+    await hre.ethers.provider.getBalance(nonAdmin.address)
+  )
+  console.log(`owner balance before ${ownerBalanceBefore}`)
+  console.log(`non admin balance before ${nonAdminBalanceBefore}`)
   const dripTx = await Faucet.drip(dripParams, authParams)
   await dripTx.wait()
+
+  // const ownerBalanceBefore = await owner.getBalance()
+  const ownerBalanceAfter = hre.ethers.utils.formatEther(
+    await hre.ethers.provider.getBalance(owner.address)
+  )
+  // const nonAdminBalanceBefore = await owner.getBalance()
+  const nonAdminBalanceAfter = hre.ethers.utils.formatEther(
+    await hre.ethers.provider.getBalance(nonAdmin.address)
+  )
+  console.log(`owner balance after: ${ownerBalanceAfter}`)
+  console.log(`non admin balance after: ${nonAdminBalanceAfter}`)
 }
 
 main()
