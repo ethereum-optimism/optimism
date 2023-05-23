5b97ddc5a8ca9e897a625839356d17195ee2b414
Javed Khan <tuxcanfly@gmail.com>
2023-05-23 15:55:05 -0700

    op-node: log namespaced data details (#94)
    * op-node: log namespaced data details
    
    * op-node: log hex

--- op-node/rollup/derive/calldata_source.go
+++ op-node/rollup/derive/calldata_source.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"context"
 	"encoding/binary"
+	"encoding/hex"
 	"errors"
 	"fmt"
 	"io"
@@ -144,11 +145,13 @@ func DataFromEVMTransactions(config *rollup.Config, daCfg *rollup.DAConfig, batc
 					log.Warn("unable to decode data pointer", "index", j, "err", err)
 					continue
 				}
+				log.Warn("requesting celestia namespaced data", "namespace", hex.EncodeToString(daCfg.NamespaceId[:]), "height", height)
 				data, err := daCfg.Client.NamespacedData(context.Background(), daCfg.NamespaceId, uint64(height))
 				if err != nil {
 					log.Warn("unable to retrieve data from da", "err", err)
 					return nil, err
 				}
+				log.Warn("retrieved data", "data", hex.EncodeToString(data[index]))
 				out = append(out, data[index])
 			} else {
 				out = append(out, tx.Data())
