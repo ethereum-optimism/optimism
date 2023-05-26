6859b81877c004a0e8afe82e662f2054bc498f62
Felipe Andrade <felipe@oplabs.co>
2023-05-25 21:44:48 -0700

    goimports

--- proxyd/server.go
+++ proxyd/server.go
@@ -7,7 +7,6 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"github.com/ethereum/go-ethereum/core"
 	"io"
 	"math"
 	"net/http"
@@ -16,6 +15,8 @@ import (
 	"strings"
 	"sync"
 	"time"
+
+	"github.com/ethereum/go-ethereum/core"
 
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/core/types"
