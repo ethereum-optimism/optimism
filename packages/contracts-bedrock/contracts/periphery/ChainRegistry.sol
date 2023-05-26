05bf3fee7d884b8a2c73c56ddd4b2b36faccef97
Sabnock01 <24715302+Sabnock01@users.noreply.github.com>
2023-05-25 19:02:45 -0500

    initial fixes

---
+++ packages/contracts-bedrock/contracts/periphery/ChainRegistry.sol
@@ -0,0 +1,82 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.15;
+
+/**
+ * @title  ChainRegistry
+ * @notice ChainRegistry acts as a way to register and query
+ *         contract addresses for an OP Stack chain
+ */
+contract ChainRegistry {
+
+    /**
+     * @notice Only the deployment admin can claim a deployment
+     */
+    error OnlyDeploymentAdmin();
+
+    /**
+     * @notice Emitted any time a deployment is claimed
+     *
+     * @param deployment The name of the deployment claimed
+     * @param admin The admin of the deployment claimed
+     */
+    event DeploymentClaimed(string deployment, address admin);
+
+    /**
+     * @notice Struct representing a deployment entry with
+     *         a contract's name and associated address
+     */
+    struct DeploymentEntry {
+        string entryName;
+        address entryAddress;
+    }
+
+    /**
+     * @notice Mapping of deployment names to deployment admins
+     */
+    mapping(string => address) public deployments;
+
+    /**
+     * @notice Mapping of deployments to their chain registry
+     */
+    mapping(string => mapping(string => address)) public registry;
+
+    /**
+     * @notice Claims a deployment
+     *
+     * @param _deployment The deployment to claim
+     */
+    function claimDeployment(string calldata _deployment, address _admin) public {
+        deployments[_deployment] = _admin;
+
+        emit DeploymentClaimed(_deployment, _admin);
+    }
+
+    /**
+     * @notice Registers entries in a deployment
+     *
+     * @param _deployment The deployment to register entries in
+     * @param _entries An array of entries to register
+     */
+    function register(string calldata _deployment, DeploymentEntry[] calldata _entries) public {
+        if (msg.sender != deployments[_deployment]) revert OnlyDeploymentAdmin();
+        for (uint i = 0; i < _entries.length; i++) {
+            registry[_deployment][_entries[i].entryName] = _entries[i].entryAddress;
+        }
+    }
+
+    /**
+     * @notice Queries the chain registry for a list of deployment addresses
+     *
+     * @param _deployment The deployment to query
+     * @param _names An array of names to query the addresses for
+     *
+     * @return An array of contract addresses for the names queried
+     */
+    function query(string calldata _deployment, string[] calldata _names) public view returns (address[] memory) {
+        address[] memory addresses = new address[](_names.length);
+        for (uint i = 0; i < _names.length; i++) {
+            addresses[i] = registry[_deployment][_names[i]];
+        }
+        return addresses;
+    }
+}
---
+++ packages/contracts-bedrock/contracts/test/ChainRegistry.t.sol
@@ -0,0 +1,90 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.15;
+
+import { Test } from "forge-std/Test.sol";
+import { ChainRegistry } from "../periphery/ChainRegistry.sol";
+
+/**
+ * @title  ChainRegistryTest
+ * @notice Test coverage of ChainRegistry
+ */
+contract ChainRegistryTest is Test {
+    /**
+     * @notice ChainRegistry
+     */
+    ChainRegistry public chainRegistry;
+
+    /**
+     * @notice Deploys the ChainRegistry
+     */
+    function setUp() public {
+        chainRegistry = new ChainRegistry();
+    }
+
+    /**
+     * @notice Claims a deployment
+     *
+     * @return Returns the name of the deployment
+     */
+    function _claim() public returns (string memory) {
+        string memory _deployment = "deployment";
+        chainRegistry.claimDeployment(_deployment, makeAddr("admin"));
+
+        return _deployment;
+    }
+
+    /**
+     * @notice Registers entries for a deployment
+     *
+     * @param _deployment The name of the deployment to register entries for
+     *
+     * @return Returns the entries registered
+     */
+    function _register(string memory _deployment) public returns (ChainRegistry.DeploymentEntry[] memory) {
+        ChainRegistry.DeploymentEntry[] memory _entries = new ChainRegistry.DeploymentEntry[](2);
+        _entries[0] = ChainRegistry.DeploymentEntry("entry1", makeAddr("entry1"));
+        _entries[1] = ChainRegistry.DeploymentEntry("entry2", makeAddr("entry2"));
+
+        vm.prank(makeAddr("admin"));
+        chainRegistry.register(_deployment, _entries);
+
+        return _entries;
+    }
+
+    /**
+     * @notice A user can set a deployment name and its admin
+     */
+    function test_claim() public {
+        _claim();
+    }
+
+    /**
+     * @notice The deployment admin can register contract addresses for the deployment
+     */
+    function test_register() public {
+        string memory _deployment = _claim();
+        _register(_deployment);
+    }
+
+    /**
+     * @notice Only the admin can register contract addresses for the deployment
+     */
+    function test_revertRegisterIfNotAdmin() public {
+        string memory _deployment = _claim();
+
+        vm.expectRevert(ChainRegistry.OnlyDeploymentAdmin.selector);
+        chainRegistry.register(_deployment, new ChainRegistry.DeploymentEntry[](0));
+    }
+
+    /**
+     * @notice A user can query contract addresses for a deployment
+     */
+    function test_query() public {
+        string memory _deployment = _claim();
+        ChainRegistry.DeploymentEntry[] memory _entries = _register(_deployment);
+
+        for (uint256 i = 0; i < _entries.length; i++) {
+            assertEq(chainRegistry.registry(_deployment, _entries[i].entryName), _entries[i].entryAddress);
+        }
+    }
+}
