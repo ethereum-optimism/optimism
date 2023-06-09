e11e0b72ed9f7070a7dfef8d6ce63af5c25e1e48
Will Cory <willcory@Wills-MacBook-Pro.local>
2023-06-09 14:46:50 -0700

    chore(atst): Nuke it

--- packages/atst/src/constants/attestationStationAddress.ts
+++
@@ -1,7 +0,0 @@
-/**
- * The attestation station contract address
- * The attestation station contract is deterministically deployed
- * to 0xEE36eaaD94d1Cc1d0eccaDb55C38bFfB6Be06C77
- */
-export const ATTESTATION_STATION_ADDRESS =
-  '0xEE36eaaD94d1Cc1d0eccaDb55C38bFfB6Be06C77'
