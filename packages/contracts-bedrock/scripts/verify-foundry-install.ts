4dd16bd68dc19459d7f53271673c2b71b59f4883
Will Cory <willcory10@gmail.com>
2023-06-11 16:35:30 -0700

    not 0.2.0

--- packages/contracts-bedrock/scripts/verify-foundry-install.ts
+++ packages/contracts-bedrock/scripts/verify-foundry-install.ts
@@ -16,7 +16,7 @@ const version = execSync('forge --version').toString()
 
 if (!version.includes('da239')) {
   console.warn(
-    `Detected forge version ${version}. This may work, but if you run into any issues, please upgrade to forge 0.2.0 or higher.
+    `Detected forge version ${version}. This may work, but if you run into any issues, please upgrade to forge da2392e58bb8a7fefeba46b40c4df1afad8ccd22.
 Consider installing via \`curl -L https://foundry.paradigm.xyz | bash\` and then running \`foundryup -C da2392e58bb8a7fefeba46b40c4df1afad8ccd22\` on a new terminal.
 For more context, check the installation instructions in the book: https://book.getfoundry.sh/getting-started/installation.html.`
   )
