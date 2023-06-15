656a9b7959f3c440fd31f33350f7ab1399f42c33
Andreas Bigger <abigger87@gmail.com>
2023-06-15 14:56:52 -0400

    Remove final owner from dgf impl initialization.

--- packages/contracts-bedrock/deploy/020-DisputeGameFactoryImpl.ts
+++ packages/contracts-bedrock/deploy/020-DisputeGameFactoryImpl.ts
@@ -1,5 +1,3 @@
-import assert from 'assert'
-
 import { DeployFunction } from 'hardhat-deploy/dist/types'
 
 import { deploy } from '../src/deploy-utils'
@@ -7,14 +5,11 @@ import { deploy } from '../src/deploy-utils'
 const deployFn: DeployFunction = async (hre) => {
   // We only want to deploy the dgf on devnet for now
   if (hre.deployConfig.l1ChainID === 900) {
-    const finalOwner = hre.deployConfig.finalSystemOwner
     const disputeGameFactory = await deploy({
       hre,
       name: 'DisputeGameFactory',
-      args: [finalOwner],
+      args: [],
     })
-    const fetchedOwner = await disputeGameFactory.owner()
-    assert(fetchedOwner === finalOwner)
     console.log('DisputeGameFactory deployed at ' + disputeGameFactory.address)
   }
 }
