67f05cad8afa71231e0c2b7226e7bf1fd5f32828
Andreas Bigger <abigger87@gmail.com>
2023-06-15 11:24:56 -0400

    DisputeGameFactory devnet deploy scripts

--- packages/contracts-bedrock/deploy/020-DisputeGameFactoryImpl.ts
+++ packages/contracts-bedrock/deploy/020-DisputeGameFactoryImpl.ts
@@ -1,5 +1,6 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 import { DeployFunction } from 'hardhat-deploy/dist/types'
 
 import { deploy } from '../src/deploy-utils'
@@ -48,3 +49,32 @@ deployFn.tags = ['DisputeGameFactoryImpl', 'setup', 'l1']
 
 export default deployFn
 >>>>>>> b998a6a67 (DisputeGameFactory devnet deploy scripts)
+||||||| parent of f8dc1be47 (DisputeGameFactory devnet deploy scripts)
+=======
+import assert from 'assert'
+
+import { DeployFunction } from 'hardhat-deploy/dist/types'
+
+import { deploy } from '../src/deploy-utils'
+
+const deployFn: DeployFunction = async (hre) => {
+  // We only want to deploy the dgf on devnet for now
+  if (hre.deployConfig.l1ChainID === 900) {
+    const disputeGameFactory = await deploy({
+      hre,
+      name: 'DisputeGameFactory',
+      args: [],
+    })
+    console.log('DisputeGameFactory deployed at ' + disputeGameFactory.address)
+    const finalOwner = hre.deployConfig.finalSystemOwner
+    await disputeGameFactory.initialize(finalOwner)
+    const fetchedOwner = await disputeGameFactory.owner()
+    assert(fetchedOwner === finalOwner)
+    console.log('DisputeGameFactory implementation initialized')
+  }
+}
+
+deployFn.tags = ['DisputeGameFactoryImpl', 'setup', 'l1']
+
+export default deployFn
+>>>>>>> f8dc1be47 (DisputeGameFactory devnet deploy scripts)
