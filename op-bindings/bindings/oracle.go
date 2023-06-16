a2102da93556947b0f7373670c7a1b6e1380f882
protolambda <proto@protolambda.com>
2023-06-16 23:00:29 +0200

    op-bindings: include Cannon contracts, update bindings-gen to include deployed-bytecode sourcemaps

---
+++ op-bindings/bindings/oracle.go
@@ -0,0 +1,320 @@
+// Code generated - DO NOT EDIT.
+// This file is a generated binding and any manual changes will be lost.
+
+package bindings
+
+import (
+	"errors"
+	"math/big"
+	"strings"
+
+	ethereum "github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/event"
+)
+
+// Reference imports to suppress errors if they are not otherwise used.
+var (
+	_ = errors.New
+	_ = big.NewInt
+	_ = strings.NewReader
+	_ = ethereum.NotFound
+	_ = bind.Bind
+	_ = common.Big1
+	_ = types.BloomLookup
+	_ = event.NewSubscription
+)
+
+// OracleMetaData contains all meta data concerning the Oracle contract.
+var OracleMetaData = &bind.MetaData{
+	ABI: "[{\"inputs\":[{\"internalType\":\"uint256\",\"name\":\"partOffset\",\"type\":\"uint256\"},{\"internalType\":\"bytes32\",\"name\":\"key\",\"type\":\"bytes32\"},{\"internalType\":\"bytes32\",\"name\":\"part\",\"type\":\"bytes32\"},{\"internalType\":\"uint256\",\"name\":\"size\",\"type\":\"uint256\"}],\"name\":\"cheat\",\"outputs\":[],\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"uint256\",\"name\":\"partOffset\",\"type\":\"uint256\"},{\"internalType\":\"bytes\",\"name\":\"preimage\",\"type\":\"bytes\"}],\"name\":\"loadKeccak256PreimagePart\",\"outputs\":[],\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"bytes32\",\"name\":\"\",\"type\":\"bytes32\"}],\"name\":\"preimageLengths\",\"outputs\":[{\"internalType\":\"uint256\",\"name\":\"\",\"type\":\"uint256\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"bytes32\",\"name\":\"key\",\"type\":\"bytes32\"},{\"internalType\":\"uint256\",\"name\":\"offset\",\"type\":\"uint256\"}],\"name\":\"readPreimage\",\"outputs\":[{\"internalType\":\"bytes32\",\"name\":\"dat\",\"type\":\"bytes32\"},{\"internalType\":\"uint256\",\"name\":\"datLen\",\"type\":\"uint256\"}],\"stateMutability\":\"view\",\"type\":\"function\"}]",
+	Bin: "0x608060405234801561001057600080fd5b50610477806100206000396000f3fe608060405234801561001057600080fd5b506004361061004c5760003560e01c8063e03110e114610051578063e15926111461007e578063fe4ac08e14610093578063fef2b4ed14610108575b600080fd5b61006461005f366004610326565b610136565b604080519283526020830191909152015b60405180910390f35b61009161008c366004610348565b610227565b005b6100916100a13660046103c4565b6000838152600260209081526040808320878452825280832080547fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff001660019081179091558684528252808320968352958152858220939093559283529082905291902055565b6101286101163660046103f6565b60006020819052908152604090205481565b604051908152602001610075565b6000828152600260209081526040808320848452909152812054819060ff166101bf576040517f08c379a000000000000000000000000000000000000000000000000000000000815260206004820152601360248201527f707265696d616765206d75737420657869737400000000000000000000000000604482015260640160405180910390fd5b50600083815260208181526040909120546101db81600861043e565b6101e685602061043e565b1061020457836101f782600861043e565b6102019190610457565b91505b506000938452600160209081526040808620948652939052919092205492909150565b60443560008060088301861061023c57600080fd5b60c083901b6080526088838682378087017ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff80151908490207effffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff167f02000000000000000000000000000000000000000000000000000000000000001760008181526002602090815260408083208b8452825280832080547fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff0016600190811790915584845282528083209a83529981528982209390935590815290819052959095209190915550505050565b6000806040838503121561033957600080fd5b50508035926020909101359150565b60008060006040848603121561035d57600080fd5b83359250602084013567ffffffffffffffff8082111561037c57600080fd5b818601915086601f83011261039057600080fd5b81358181111561039f57600080fd5b8760208285010111156103b157600080fd5b6020830194508093505050509250925092565b600080600080608085870312156103da57600080fd5b5050823594602084013594506040840135936060013592509050565b60006020828403121561040857600080fd5b5035919050565b7f4e487b7100000000000000000000000000000000000000000000000000000000600052601160045260246000fd5b808201808211156104515761045161040f565b92915050565b818103818111156104515761045161040f56fea164736f6c6343000813000a",
+}
+
+// OracleABI is the input ABI used to generate the binding from.
+// Deprecated: Use OracleMetaData.ABI instead.
+var OracleABI = OracleMetaData.ABI
+
+// OracleBin is the compiled bytecode used for deploying new contracts.
+// Deprecated: Use OracleMetaData.Bin instead.
+var OracleBin = OracleMetaData.Bin
+
+// DeployOracle deploys a new Ethereum contract, binding an instance of Oracle to it.
+func DeployOracle(auth *bind.TransactOpts, backend bind.ContractBackend) (common.Address, *types.Transaction, *Oracle, error) {
+	parsed, err := OracleMetaData.GetAbi()
+	if err != nil {
+		return common.Address{}, nil, nil, err
+	}
+	if parsed == nil {
+		return common.Address{}, nil, nil, errors.New("GetABI returned nil")
+	}
+
+	address, tx, contract, err := bind.DeployContract(auth, *parsed, common.FromHex(OracleBin), backend)
+	if err != nil {
+		return common.Address{}, nil, nil, err
+	}
+	return address, tx, &Oracle{OracleCaller: OracleCaller{contract: contract}, OracleTransactor: OracleTransactor{contract: contract}, OracleFilterer: OracleFilterer{contract: contract}}, nil
+}
+
+// Oracle is an auto generated Go binding around an Ethereum contract.
+type Oracle struct {
+	OracleCaller     // Read-only binding to the contract
+	OracleTransactor // Write-only binding to the contract
+	OracleFilterer   // Log filterer for contract events
+}
+
+// OracleCaller is an auto generated read-only Go binding around an Ethereum contract.
+type OracleCaller struct {
+	contract *bind.BoundContract // Generic contract wrapper for the low level calls
+}
+
+// OracleTransactor is an auto generated write-only Go binding around an Ethereum contract.
+type OracleTransactor struct {
+	contract *bind.BoundContract // Generic contract wrapper for the low level calls
+}
+
+// OracleFilterer is an auto generated log filtering Go binding around an Ethereum contract events.
+type OracleFilterer struct {
+	contract *bind.BoundContract // Generic contract wrapper for the low level calls
+}
+
+// OracleSession is an auto generated Go binding around an Ethereum contract,
+// with pre-set call and transact options.
+type OracleSession struct {
+	Contract     *Oracle           // Generic contract binding to set the session for
+	CallOpts     bind.CallOpts     // Call options to use throughout this session
+	TransactOpts bind.TransactOpts // Transaction auth options to use throughout this session
+}
+
+// OracleCallerSession is an auto generated read-only Go binding around an Ethereum contract,
+// with pre-set call options.
+type OracleCallerSession struct {
+	Contract *OracleCaller // Generic contract caller binding to set the session for
+	CallOpts bind.CallOpts // Call options to use throughout this session
+}
+
+// OracleTransactorSession is an auto generated write-only Go binding around an Ethereum contract,
+// with pre-set transact options.
+type OracleTransactorSession struct {
+	Contract     *OracleTransactor // Generic contract transactor binding to set the session for
+	TransactOpts bind.TransactOpts // Transaction auth options to use throughout this session
+}
+
+// OracleRaw is an auto generated low-level Go binding around an Ethereum contract.
+type OracleRaw struct {
+	Contract *Oracle // Generic contract binding to access the raw methods on
+}
+
+// OracleCallerRaw is an auto generated low-level read-only Go binding around an Ethereum contract.
+type OracleCallerRaw struct {
+	Contract *OracleCaller // Generic read-only contract binding to access the raw methods on
+}
+
+// OracleTransactorRaw is an auto generated low-level write-only Go binding around an Ethereum contract.
+type OracleTransactorRaw struct {
+	Contract *OracleTransactor // Generic write-only contract binding to access the raw methods on
+}
+
+// NewOracle creates a new instance of Oracle, bound to a specific deployed contract.
+func NewOracle(address common.Address, backend bind.ContractBackend) (*Oracle, error) {
+	contract, err := bindOracle(address, backend, backend, backend)
+	if err != nil {
+		return nil, err
+	}
+	return &Oracle{OracleCaller: OracleCaller{contract: contract}, OracleTransactor: OracleTransactor{contract: contract}, OracleFilterer: OracleFilterer{contract: contract}}, nil
+}
+
+// NewOracleCaller creates a new read-only instance of Oracle, bound to a specific deployed contract.
+func NewOracleCaller(address common.Address, caller bind.ContractCaller) (*OracleCaller, error) {
+	contract, err := bindOracle(address, caller, nil, nil)
+	if err != nil {
+		return nil, err
+	}
+	return &OracleCaller{contract: contract}, nil
+}
+
+// NewOracleTransactor creates a new write-only instance of Oracle, bound to a specific deployed contract.
+func NewOracleTransactor(address common.Address, transactor bind.ContractTransactor) (*OracleTransactor, error) {
+	contract, err := bindOracle(address, nil, transactor, nil)
+	if err != nil {
+		return nil, err
+	}
+	return &OracleTransactor{contract: contract}, nil
+}
+
+// NewOracleFilterer creates a new log filterer instance of Oracle, bound to a specific deployed contract.
+func NewOracleFilterer(address common.Address, filterer bind.ContractFilterer) (*OracleFilterer, error) {
+	contract, err := bindOracle(address, nil, nil, filterer)
+	if err != nil {
+		return nil, err
+	}
+	return &OracleFilterer{contract: contract}, nil
+}
+
+// bindOracle binds a generic wrapper to an already deployed contract.
+func bindOracle(address common.Address, caller bind.ContractCaller, transactor bind.ContractTransactor, filterer bind.ContractFilterer) (*bind.BoundContract, error) {
+	parsed, err := abi.JSON(strings.NewReader(OracleABI))
+	if err != nil {
+		return nil, err
+	}
+	return bind.NewBoundContract(address, parsed, caller, transactor, filterer), nil
+}
+
+// Call invokes the (constant) contract method with params as input values and
+// sets the output to result. The result type might be a single field for simple
+// returns, a slice of interfaces for anonymous returns and a struct for named
+// returns.
+func (_Oracle *OracleRaw) Call(opts *bind.CallOpts, result *[]interface{}, method string, params ...interface{}) error {
+	return _Oracle.Contract.OracleCaller.contract.Call(opts, result, method, params...)
+}
+
+// Transfer initiates a plain transaction to move funds to the contract, calling
+// its default method if one is available.
+func (_Oracle *OracleRaw) Transfer(opts *bind.TransactOpts) (*types.Transaction, error) {
+	return _Oracle.Contract.OracleTransactor.contract.Transfer(opts)
+}
+
+// Transact invokes the (paid) contract method with params as input values.
+func (_Oracle *OracleRaw) Transact(opts *bind.TransactOpts, method string, params ...interface{}) (*types.Transaction, error) {
+	return _Oracle.Contract.OracleTransactor.contract.Transact(opts, method, params...)
+}
+
+// Call invokes the (constant) contract method with params as input values and
+// sets the output to result. The result type might be a single field for simple
+// returns, a slice of interfaces for anonymous returns and a struct for named
+// returns.
+func (_Oracle *OracleCallerRaw) Call(opts *bind.CallOpts, result *[]interface{}, method string, params ...interface{}) error {
+	return _Oracle.Contract.contract.Call(opts, result, method, params...)
+}
+
+// Transfer initiates a plain transaction to move funds to the contract, calling
+// its default method if one is available.
+func (_Oracle *OracleTransactorRaw) Transfer(opts *bind.TransactOpts) (*types.Transaction, error) {
+	return _Oracle.Contract.contract.Transfer(opts)
+}
+
+// Transact invokes the (paid) contract method with params as input values.
+func (_Oracle *OracleTransactorRaw) Transact(opts *bind.TransactOpts, method string, params ...interface{}) (*types.Transaction, error) {
+	return _Oracle.Contract.contract.Transact(opts, method, params...)
+}
+
+// PreimageLengths is a free data retrieval call binding the contract method 0xfef2b4ed.
+//
+// Solidity: function preimageLengths(bytes32 ) view returns(uint256)
+func (_Oracle *OracleCaller) PreimageLengths(opts *bind.CallOpts, arg0 [32]byte) (*big.Int, error) {
+	var out []interface{}
+	err := _Oracle.contract.Call(opts, &out, "preimageLengths", arg0)
+
+	if err != nil {
+		return *new(*big.Int), err
+	}
+
+	out0 := *abi.ConvertType(out[0], new(*big.Int)).(**big.Int)
+
+	return out0, err
+
+}
+
+// PreimageLengths is a free data retrieval call binding the contract method 0xfef2b4ed.
+//
+// Solidity: function preimageLengths(bytes32 ) view returns(uint256)
+func (_Oracle *OracleSession) PreimageLengths(arg0 [32]byte) (*big.Int, error) {
+	return _Oracle.Contract.PreimageLengths(&_Oracle.CallOpts, arg0)
+}
+
+// PreimageLengths is a free data retrieval call binding the contract method 0xfef2b4ed.
+//
+// Solidity: function preimageLengths(bytes32 ) view returns(uint256)
+func (_Oracle *OracleCallerSession) PreimageLengths(arg0 [32]byte) (*big.Int, error) {
+	return _Oracle.Contract.PreimageLengths(&_Oracle.CallOpts, arg0)
+}
+
+// ReadPreimage is a free data retrieval call binding the contract method 0xe03110e1.
+//
+// Solidity: function readPreimage(bytes32 key, uint256 offset) view returns(bytes32 dat, uint256 datLen)
+func (_Oracle *OracleCaller) ReadPreimage(opts *bind.CallOpts, key [32]byte, offset *big.Int) (struct {
+	Dat    [32]byte
+	DatLen *big.Int
+}, error) {
+	var out []interface{}
+	err := _Oracle.contract.Call(opts, &out, "readPreimage", key, offset)
+
+	outstruct := new(struct {
+		Dat    [32]byte
+		DatLen *big.Int
+	})
+	if err != nil {
+		return *outstruct, err
+	}
+
+	outstruct.Dat = *abi.ConvertType(out[0], new([32]byte)).(*[32]byte)
+	outstruct.DatLen = *abi.ConvertType(out[1], new(*big.Int)).(**big.Int)
+
+	return *outstruct, err
+
+}
+
+// ReadPreimage is a free data retrieval call binding the contract method 0xe03110e1.
+//
+// Solidity: function readPreimage(bytes32 key, uint256 offset) view returns(bytes32 dat, uint256 datLen)
+func (_Oracle *OracleSession) ReadPreimage(key [32]byte, offset *big.Int) (struct {
+	Dat    [32]byte
+	DatLen *big.Int
+}, error) {
+	return _Oracle.Contract.ReadPreimage(&_Oracle.CallOpts, key, offset)
+}
+
+// ReadPreimage is a free data retrieval call binding the contract method 0xe03110e1.
+//
+// Solidity: function readPreimage(bytes32 key, uint256 offset) view returns(bytes32 dat, uint256 datLen)
+func (_Oracle *OracleCallerSession) ReadPreimage(key [32]byte, offset *big.Int) (struct {
+	Dat    [32]byte
+	DatLen *big.Int
+}, error) {
+	return _Oracle.Contract.ReadPreimage(&_Oracle.CallOpts, key, offset)
+}
+
+// Cheat is a paid mutator transaction binding the contract method 0xfe4ac08e.
+//
+// Solidity: function cheat(uint256 partOffset, bytes32 key, bytes32 part, uint256 size) returns()
+func (_Oracle *OracleTransactor) Cheat(opts *bind.TransactOpts, partOffset *big.Int, key [32]byte, part [32]byte, size *big.Int) (*types.Transaction, error) {
+	return _Oracle.contract.Transact(opts, "cheat", partOffset, key, part, size)
+}
+
+// Cheat is a paid mutator transaction binding the contract method 0xfe4ac08e.
+//
+// Solidity: function cheat(uint256 partOffset, bytes32 key, bytes32 part, uint256 size) returns()
+func (_Oracle *OracleSession) Cheat(partOffset *big.Int, key [32]byte, part [32]byte, size *big.Int) (*types.Transaction, error) {
+	return _Oracle.Contract.Cheat(&_Oracle.TransactOpts, partOffset, key, part, size)
+}
+
+// Cheat is a paid mutator transaction binding the contract method 0xfe4ac08e.
+//
+// Solidity: function cheat(uint256 partOffset, bytes32 key, bytes32 part, uint256 size) returns()
+func (_Oracle *OracleTransactorSession) Cheat(partOffset *big.Int, key [32]byte, part [32]byte, size *big.Int) (*types.Transaction, error) {
+	return _Oracle.Contract.Cheat(&_Oracle.TransactOpts, partOffset, key, part, size)
+}
+
+// LoadKeccak256PreimagePart is a paid mutator transaction binding the contract method 0xe1592611.
+//
+// Solidity: function loadKeccak256PreimagePart(uint256 partOffset, bytes preimage) returns()
+func (_Oracle *OracleTransactor) LoadKeccak256PreimagePart(opts *bind.TransactOpts, partOffset *big.Int, preimage []byte) (*types.Transaction, error) {
+	return _Oracle.contract.Transact(opts, "loadKeccak256PreimagePart", partOffset, preimage)
+}
+
+// LoadKeccak256PreimagePart is a paid mutator transaction binding the contract method 0xe1592611.
+//
+// Solidity: function loadKeccak256PreimagePart(uint256 partOffset, bytes preimage) returns()
+func (_Oracle *OracleSession) LoadKeccak256PreimagePart(partOffset *big.Int, preimage []byte) (*types.Transaction, error) {
+	return _Oracle.Contract.LoadKeccak256PreimagePart(&_Oracle.TransactOpts, partOffset, preimage)
+}
+
+// LoadKeccak256PreimagePart is a paid mutator transaction binding the contract method 0xe1592611.
+//
+// Solidity: function loadKeccak256PreimagePart(uint256 partOffset, bytes preimage) returns()
+func (_Oracle *OracleTransactorSession) LoadKeccak256PreimagePart(partOffset *big.Int, preimage []byte) (*types.Transaction, error) {
+	return _Oracle.Contract.LoadKeccak256PreimagePart(&_Oracle.TransactOpts, partOffset, preimage)
+}
