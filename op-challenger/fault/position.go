a0a96aa607b59addb82c80796098ce20e13d0178
Joshua Gutow <jgutow@oplabs.co>
2023-06-20 15:04:46 -0700

    game solver: Add generalized index & tests to position

--- op-challenger/fault/position.go
+++ op-challenger/fault/position.go
@@ -11,8 +11,7 @@ type Position struct {
 	IndexAtDepth int
 }
 
-// TraceIndex calculates the what the index of the claim value
-// would be inside the trace.
+// TraceIndex calculates the what the index of the claim value would be inside the trace.
 func (p *Position) TraceIndex(maxDepth int) int {
 	lo := 0
 	hi := 1 << maxDepth
@@ -30,6 +29,7 @@ func (p *Position) TraceIndex(maxDepth int) int {
 	return mid
 }
 
+// move goes to the left or right child in the generalized index tree.
 func (p *Position) move(right bool) {
 	p.Depth++
 	p.IndexAtDepth = (p.IndexAtDepth << 1) | boolToInt(right)
@@ -43,15 +43,18 @@ func boolToInt(b bool) int {
 	}
 }
 
+// parent moves up to the parent in the generalized index tree.
 func (p *Position) parent() {
 	p.Depth--
 	p.IndexAtDepth = p.IndexAtDepth >> 1
 }
 
+// Attack moves this position to a position to the left which disagrees with this position.
 func (p *Position) Attack() {
 	p.move(false)
 }
 
+// Defend moves this position to the right which agrees with this position. Note:
 func (p *Position) Defend() {
 	p.parent()
 	p.move(true)
@@ -59,5 +62,30 @@ func (p *Position) Defend() {
 }
 
 func (p *Position) Print(maxDepth int) {
-	fmt.Printf("Trace Position is %04b\tTrace Depth is: %d\tTrace Index is: %d\n", p.IndexAtDepth, p.Depth, p.TraceIndex(maxDepth))
+	fmt.Printf("GIN: %4b\tTrace Position is %4b\tTrace Depth is: %d\tTrace Index is: %d\n", p.ToGIN(), p.IndexAtDepth, p.Depth, p.TraceIndex(maxDepth))
+}
+
+func (p *Position) ToGIN() uint64 {
+	return uint64(1<<p.Depth | p.IndexAtDepth)
+}
+
+func FromGIN(x uint64) Position {
+	depth := MSBIndex(x)
+	indexAtDepth := ^(1 << depth) & x
+	return Position{
+		Depth:        depth,
+		IndexAtDepth: int(indexAtDepth),
+	}
+}
+
+// MSBIndex returns the index of the most significant bit
+func MSBIndex(x uint64) int {
+	if x == 0 {
+		return 0
+	}
+	out := 0
+	for ; x != 0; out++ {
+		x = x >> 1
+	}
+	return out - 1
 }
--- op-challenger/fault/position_test.go
+++ op-challenger/fault/position_test.go
@@ -1,1 +1,122 @@
 package fault
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestMSBIndex(t *testing.T) {
+	tests := []struct {
+		input    uint64
+		expected int
+	}{
+		{0, 0},
+		{1 << 0, 0},
+		{1 << 1, 1},
+		{1 << 2, 2},
+		{1 << 3, 3},
+		{16, 4},
+		{255, 7},
+		{1024, 10},
+		{18446744073709551615, 63},
+	}
+
+	for _, test := range tests {
+		result := MSBIndex(test.input)
+		if result != test.expected {
+			t.Errorf("MSBIndex(%d) expected %d, but got %d", test.input, test.expected, result)
+		}
+	}
+
+}
+
+func TestGINConvessions(t *testing.T) {
+	tests := []struct {
+		index    uint64
+		position Position
+	}{
+		{
+			index:    1,
+			position: Position{Depth: 0, IndexAtDepth: 0},
+		},
+		{
+			index:    0b10,
+			position: Position{Depth: 1, IndexAtDepth: 0},
+		},
+		{
+			index:    0b100,
+			position: Position{Depth: 2, IndexAtDepth: 0},
+		},
+		{
+			index:    0b110,
+			position: Position{Depth: 2, IndexAtDepth: 2},
+		},
+		{
+			index:    0b1010,
+			position: Position{Depth: 3, IndexAtDepth: 2},
+		},
+		{
+			index:    0b1100,
+			position: Position{Depth: 3, IndexAtDepth: 4},
+		},
+	}
+	for _, test := range tests {
+		from := FromGIN(test.index)
+		require.Equal(t, test.position, from)
+		to := test.position.ToGIN()
+		require.Equal(t, test.index, to)
+	}
+}
+
+func TestTraceIndex(t *testing.T) {
+	// Note: for whatever reason there appears to be an extra bit in the system & there are two valid options for every trace index.
+	// I think it is because we always go left at the start, though it is the lowest bit that we a re free to change without issue...
+	// Maybe it is fine to change that low bit because we always go left or do a loop that ends up going left so the lowest bit
+	// is always zero
+	tests := []struct {
+		pos Position
+		idx int
+	}{
+		{
+			pos: Position{Depth: 0, IndexAtDepth: 0},
+			idx: 8,
+		},
+		{
+			pos: Position{Depth: 1, IndexAtDepth: 0},
+			idx: 4,
+		},
+		{
+			pos: Position{Depth: 2, IndexAtDepth: 0}, // 0 or 1?
+			idx: 2,
+		},
+		{
+			pos: Position{Depth: 2, IndexAtDepth: 1}, // 0 or 1?
+			idx: 2,
+		},
+		{
+			pos: Position{Depth: 2, IndexAtDepth: 2}, // 2 or 3?
+			idx: 6,
+		},
+		{
+			pos: Position{Depth: 3, IndexAtDepth: 1}, // 0 or 1?
+			idx: 1,
+		},
+		{
+			pos: Position{Depth: 3, IndexAtDepth: 2}, // 2 or 3?
+			idx: 3,
+		},
+		{
+			pos: Position{Depth: 3, IndexAtDepth: 4}, // 4 or 5?
+			idx: 5,
+		},
+		{
+			pos: Position{Depth: 3, IndexAtDepth: 6}, // 6 or 7?
+			idx: 7,
+		},
+	}
+	for _, test := range tests {
+		result := test.pos.TraceIndex(3)
+		require.Equal(t, test.idx, result)
+	}
+}
