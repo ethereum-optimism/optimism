ffa2af22ec63ce4a125d19d0c6f91f8d867a6cef
Joshua Gutow <jgutow@oplabs.co>
2023-06-21 11:54:55 -0700

    Fix traceIndex function

--- op-challenger/fault/position.go
+++ op-challenger/fault/position.go
@@ -3,36 +3,42 @@ package fault
 import "fmt"
 
 // Position is a golang wrapper around the dispute game Position type.
-// Depth refers to how many bisection steps have occurred.
-// IndexAtDepth refers to the path that the bisection has taken
-// where 1 = goes right & 0 = goes left.
 type Position struct {
-	Depth        int
-	IndexAtDepth int
+	depth        int
+	indexAtDepth int
+}
+
+func NewPosition(depth, indexAtDepth int) Position {
+	return Position{depth, indexAtDepth}
+}
+
+func NewPositionFromGIndex(x uint64) Position {
+	depth := MSBIndex(x)
+	indexAtDepth := ^(1 << depth) & x
+	return NewPosition(depth, int(indexAtDepth))
+}
+
+func (p *Position) Depth() int {
+	return p.depth
+}
+
+func (p *Position) IndexAtDepth() int {
+	return p.indexAtDepth
 }
 
 // TraceIndex calculates the what the index of the claim value would be inside the trace.
+// It is equivalent to going right until the final depth has been reached.
 func (p *Position) TraceIndex(maxDepth int) int {
-	lo := 0
-	hi := (1 << maxDepth) - 1
-	mid := hi
-	path := p.IndexAtDepth
-	for i := p.Depth - 1; i >= 0; i-- {
-		mid = (lo + hi) / 2
-		mask := 1 << i
-		if path&mask == mask {
-			lo = mid
-		} else {
-			hi = mid
-		}
-	}
-	return mid
+	// When we go right, we do a shift left and set the bottom bit to be 1.
+	// To do this in a single step, do all the shifts at once & or in all 1s for the bottom bits.
+	rd := maxDepth - p.depth
+	return p.indexAtDepth<<rd | ((1 << rd) - 1)
 }
 
-// move goes to the left or right child in the generalized index tree.
+// move goes to the left or right child.
 func (p *Position) move(right bool) {
-	p.Depth++
-	p.IndexAtDepth = (p.IndexAtDepth << 1) | boolToInt(right)
+	p.depth++
+	p.indexAtDepth = (p.indexAtDepth << 1) | boolToInt(right)
 }
 
 func boolToInt(b bool) int {
@@ -43,10 +49,10 @@ func boolToInt(b bool) int {
 	}
 }
 
-// parent moves up to the parent in the generalized index tree.
+// parent moves up to the parent.
 func (p *Position) parent() {
-	p.Depth--
-	p.IndexAtDepth = p.IndexAtDepth >> 1
+	p.depth--
+	p.indexAtDepth = p.indexAtDepth >> 1
 }
 
 // Attack moves this position to a position to the left which disagrees with this position.
@@ -62,20 +68,11 @@ func (p *Position) Defend() {
 }
 
 func (p *Position) Print(maxDepth int) {
-	fmt.Printf("GIN: %4b\tTrace Position is %4b\tTrace Depth is: %d\tTrace Index is: %d\n", p.ToGIN(), p.IndexAtDepth, p.Depth, p.TraceIndex(maxDepth))
+	fmt.Printf("GIN: %4b\tTrace Position is %4b\tTrace Depth is: %d\tTrace Index is: %d\n", p.ToGIndex(), p.indexAtDepth, p.depth, p.TraceIndex(maxDepth))
 }
 
-func (p *Position) ToGIN() uint64 {
-	return uint64(1<<p.Depth | p.IndexAtDepth)
-}
-
-func FromGIN(x uint64) Position {
-	depth := MSBIndex(x)
-	indexAtDepth := ^(1 << depth) & x
-	return Position{
-		Depth:        depth,
-		IndexAtDepth: int(indexAtDepth),
-	}
+func (p *Position) ToGIndex() uint64 {
+	return uint64(1<<p.depth | p.indexAtDepth)
 }
 
 // MSBIndex returns the index of the most significant bit
--- op-challenger/fault/position_test.go
+++ op-challenger/fault/position_test.go
@@ -31,97 +31,67 @@ func TestMSBIndex(t *testing.T) {
 
 }
 
-func TestGINConvessions(t *testing.T) {
-	tests := []struct {
-		index    uint64
-		position Position
-	}{
-		{
-			index:    1,
-			position: Position{Depth: 0, IndexAtDepth: 0},
-		},
-		{
-			index:    0b10,
-			position: Position{Depth: 1, IndexAtDepth: 0},
-		},
-		{
-			index:    0b11,
-			position: Position{Depth: 1, IndexAtDepth: 1},
-		},
-		{
-			index:    0b100,
-			position: Position{Depth: 2, IndexAtDepth: 0},
-		},
-		{
-			index:    0b101,
-			position: Position{Depth: 2, IndexAtDepth: 1},
-		},
-		{
-			index:    0b110,
-			position: Position{Depth: 2, IndexAtDepth: 2},
-		},
-		{
-			index:    0b111,
-			position: Position{Depth: 2, IndexAtDepth: 3},
-		},
-		{
-			index:    0b1010,
-			position: Position{Depth: 3, IndexAtDepth: 2},
-		},
-		{
-			index:    0b1100,
-			position: Position{Depth: 3, IndexAtDepth: 4},
-		},
-	}
-	for _, test := range tests {
-		from := FromGIN(test.index)
-		require.Equal(t, test.position, from)
-		to := test.position.ToGIN()
-		require.Equal(t, test.index, to)
+type testNodeInfo struct {
+	GIndex       uint64
+	Depth        int
+	IndexAtDepth int
+	TraceIndex   int
+}
+
+var treeNodesMaxDepth4 = []testNodeInfo{
+	{GIndex: 1, Depth: 0, IndexAtDepth: 0, TraceIndex: 15},
+
+	{GIndex: 2, Depth: 1, IndexAtDepth: 0, TraceIndex: 7},
+	{GIndex: 3, Depth: 1, IndexAtDepth: 1, TraceIndex: 15},
+
+	{GIndex: 4, Depth: 2, IndexAtDepth: 0, TraceIndex: 3},
+	{GIndex: 5, Depth: 2, IndexAtDepth: 1, TraceIndex: 7},
+	{GIndex: 6, Depth: 2, IndexAtDepth: 2, TraceIndex: 11},
+	{GIndex: 7, Depth: 2, IndexAtDepth: 3, TraceIndex: 15},
+
+	{GIndex: 8, Depth: 3, IndexAtDepth: 0, TraceIndex: 1},
+	{GIndex: 9, Depth: 3, IndexAtDepth: 1, TraceIndex: 3},
+	{GIndex: 10, Depth: 3, IndexAtDepth: 2, TraceIndex: 5},
+	{GIndex: 11, Depth: 3, IndexAtDepth: 3, TraceIndex: 7},
+	{GIndex: 12, Depth: 3, IndexAtDepth: 4, TraceIndex: 9},
+	{GIndex: 13, Depth: 3, IndexAtDepth: 5, TraceIndex: 11},
+	{GIndex: 14, Depth: 3, IndexAtDepth: 6, TraceIndex: 13},
+	{GIndex: 15, Depth: 3, IndexAtDepth: 7, TraceIndex: 15},
+
+	{GIndex: 16, Depth: 4, IndexAtDepth: 0, TraceIndex: 0},
+	{GIndex: 17, Depth: 4, IndexAtDepth: 1, TraceIndex: 1},
+	{GIndex: 18, Depth: 4, IndexAtDepth: 2, TraceIndex: 2},
+	{GIndex: 19, Depth: 4, IndexAtDepth: 3, TraceIndex: 3},
+	{GIndex: 20, Depth: 4, IndexAtDepth: 4, TraceIndex: 4},
+	{GIndex: 21, Depth: 4, IndexAtDepth: 5, TraceIndex: 5},
+	{GIndex: 22, Depth: 4, IndexAtDepth: 6, TraceIndex: 6},
+	{GIndex: 23, Depth: 4, IndexAtDepth: 7, TraceIndex: 7},
+	{GIndex: 24, Depth: 4, IndexAtDepth: 8, TraceIndex: 8},
+	{GIndex: 25, Depth: 4, IndexAtDepth: 9, TraceIndex: 9},
+	{GIndex: 26, Depth: 4, IndexAtDepth: 10, TraceIndex: 10},
+	{GIndex: 27, Depth: 4, IndexAtDepth: 11, TraceIndex: 11},
+	{GIndex: 28, Depth: 4, IndexAtDepth: 12, TraceIndex: 12},
+	{GIndex: 29, Depth: 4, IndexAtDepth: 13, TraceIndex: 13},
+	{GIndex: 30, Depth: 4, IndexAtDepth: 14, TraceIndex: 14},
+	{GIndex: 31, Depth: 4, IndexAtDepth: 15, TraceIndex: 15},
+}
+
+// TestGINConversions does To & From the generalized index on the treeNodesMaxDepth4 data
+func TestGINConversions(t *testing.T) {
+	for _, test := range treeNodesMaxDepth4 {
+		from := NewPositionFromGIndex(test.GIndex)
+		pos := NewPosition(test.Depth, test.IndexAtDepth)
+		require.Equal(t, pos, from)
+		to := pos.ToGIndex()
+		require.Equal(t, test.GIndex, to)
 	}
 }
 
+// TestTraceIndex creates the position & then tests the trace index function on the treeNodesMaxDepth4 data
 func TestTraceIndex(t *testing.T) {
-	// Note: Only left children are are valid positions after a move.
-	tests := []struct {
-		pos Position
-		idx int
-	}{
-		{
-			pos: Position{Depth: 0, IndexAtDepth: 0},
-			idx: 7,
-		},
-		{
-			pos: Position{Depth: 1, IndexAtDepth: 0},
-			idx: 3,
-		},
-		{
-			pos: Position{Depth: 2, IndexAtDepth: 0},
-			idx: 1,
-		},
-		{
-			pos: Position{Depth: 2, IndexAtDepth: 2},
-			idx: 5,
-		},
-		{
-			pos: Position{Depth: 3, IndexAtDepth: 0},
-			idx: 0,
-		},
-		{
-			pos: Position{Depth: 3, IndexAtDepth: 2},
-			idx: 2,
-		},
-		{
-			pos: Position{Depth: 3, IndexAtDepth: 4},
-			idx: 4,
-		},
-		{
-			pos: Position{Depth: 3, IndexAtDepth: 6},
-			idx: 6,
-		},
-	}
-	for _, test := range tests {
-		result := test.pos.TraceIndex(3)
-		require.Equal(t, test.idx, result)
+	for _, test := range treeNodesMaxDepth4 {
+		pos := NewPosition(test.Depth, test.IndexAtDepth)
+		result := pos.TraceIndex(4)
+		require.Equal(t, test.TraceIndex, result)
 	}
 }
