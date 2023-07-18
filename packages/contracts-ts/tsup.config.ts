4703674b477cc5dd3626c51ea0ad508111339352
Will Cory <willcory@Wills-MacBook-Pro.local>
2023-07-17 19:54:00 -0700

    fix: Bad tsupconfig entrypoint

--- packages/contracts-ts/tsup.config.ts
+++ packages/contracts-ts/tsup.config.ts
@@ -3,7 +3,7 @@ import packageJson from './package.json'
 
 export default defineConfig({
   name: packageJson.name,
-  entry: ['src/index.ts', 'src/actions.ts', 'src/react.ts'],
+  entry: ['src/constants.ts', 'src/actions.ts', 'src/react.ts'],
   outDir: 'dist',
   format: ['esm', 'cjs'],
   splitting: false,
