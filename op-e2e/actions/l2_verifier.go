e472706694f9243e0c649653266199743234e9a4
Mark Tyneway <mark.tyneway@gmail.com>
2023-07-19 23:23:02 -0700

    op-e2e: early exit on error
    Co-authored-by: proto@protolambda.com

--- op-e2e/actions/l2_verifier.go
+++ op-e2e/actions/l2_verifier.go
@@ -5,6 +5,8 @@ import (
 	"errors"
 	"io"
 
+	"github.com/ethereum-optimism/optimism/op-node/rollup/sync"
+
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/log"
 	gnode "github.com/ethereum/go-ethereum/node"
@@ -215,6 +217,9 @@ func (s *L2Verifier) ActL2PipelineStep(t Testing) {
 		return
 	} else if err != nil && errors.Is(err, derive.ErrTemporary) {
 		s.log.Warn("Derivation process temporary error", "err", err)
+		if errors.Is(err, sync.WrongChainErr) { // action-tests don't back off on temporary errors. Avoid a bad genesis setup from looping.
+			t.Fatalf("genesis setup issue: %v", err)
+		}
 		return
 	} else if err != nil && errors.Is(err, derive.ErrCritical) {
 		t.Fatalf("derivation failed critically: %v", err)
