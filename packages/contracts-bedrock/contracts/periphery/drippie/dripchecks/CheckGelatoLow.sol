0ae1166b0db2d04dda52d13092b19ebbcf63f4c4
Andreas Bigger <abigger87@gmail.com>
2023-07-20 13:32:39 -0400

    Port drippie contracts to triple slash natspec styling

--- packages/contracts-bedrock/contracts/periphery/drippie/dripchecks/CheckGelatoLow.sol
+++ packages/contracts-bedrock/contracts/periphery/drippie/dripchecks/CheckGelatoLow.sol
@@ -7,10 +7,8 @@ interface IGelatoTreasury {
     function userTokenBalance(address _user, address _token) external view returns (uint256);
 }
 
-/**
- * @title CheckGelatoLow
- * @notice DripCheck for checking if an account's Gelato ETH balance is below some threshold.
- */
+/// @title CheckGelatoLow
+/// @notice DripCheck for checking if an account's Gelato ETH balance is below some threshold.
 contract CheckGelatoLow is IDripCheck {
     struct Params {
         address treasury;
@@ -18,25 +16,21 @@ contract CheckGelatoLow is IDripCheck {
         address recipient;
     }
 
-    /**
-     * @notice External event used to help client-side tooling encode parameters.
-     *
-     * @param params Parameters to encode.
-     */
+    /// @notice External event used to help client-side tooling encode parameters.
+    /// @param params Parameters to encode.
     event _EventToExposeStructInABI__Params(Params params);
 
-    /**
-     * @inheritdoc IDripCheck
-     */
-    function check(bytes memory _params) external view returns (bool) {
+    /// @inheritdoc IDripCheck
+    function check(bytes memory _params) external view returns (bool execute_) {
         Params memory params = abi.decode(_params, (Params));
 
         // Check GelatoTreasury ETH balance is below threshold.
-        return
+        execute_ =
             IGelatoTreasury(params.treasury).userTokenBalance(
                 params.recipient,
                 // Gelato represents ETH as 0xeeeee....eeeee
                 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE
-            ) < params.threshold;
+            ) <
+            params.threshold;
     }
 }
