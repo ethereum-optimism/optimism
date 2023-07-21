a22f52b0445a2e07153affa9b962a3b01bf0da7f
Andreas Bigger <abigger87@gmail.com>
2023-07-21 11:55:12 -0400

    Port EAS contracts to triple slash natspec styling

--- packages/contracts-bedrock/contracts/EAS/SchemaRegistry.sol
+++ packages/contracts-bedrock/contracts/EAS/SchemaRegistry.sol
@@ -1,5 +1,4 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity 0.8.19;
 
 import { Semver } from "../universal/Semver.sol";
@@ -22,14 +21,10 @@ contract SchemaRegistry is ISchemaRegistry, Semver {
     // Upgrade forward-compatibility storage gap
     uint256[MAX_GAP - 1] private __gap;
 
-    /**
-     * @dev Creates a new SchemaRegistry instance.
-     */
+    /// @dev Creates a new SchemaRegistry instance.
     constructor() Semver(1, 0, 0) {}
 
-    /**
-     * @inheritdoc ISchemaRegistry
-     */
+    /// @inheritdoc ISchemaRegistry
     function register(string calldata schema, ISchemaResolver resolver, bool revocable) external returns (bytes32) {
         SchemaRecord memory schemaRecord = SchemaRecord({
             uid: EMPTY_UID,
@@ -51,20 +46,14 @@ contract SchemaRegistry is ISchemaRegistry, Semver {
         return uid;
     }
 
-    /**
-     * @inheritdoc ISchemaRegistry
-     */
+    /// @inheritdoc ISchemaRegistry
     function getSchema(bytes32 uid) external view returns (SchemaRecord memory) {
         return _registry[uid];
     }
 
-    /**
-     * @dev Calculates a UID for a given schema.
-     *
-     * @param schemaRecord The input schema.
-     *
-     * @return schema UID.
-     */
+    /// @dev Calculates a UID for a given schema.
+    /// @param schemaRecord The input schema.
+    /// @return schema UID.
     function _getUID(SchemaRecord memory schemaRecord) private pure returns (bytes32) {
         return keccak256(abi.encodePacked(schemaRecord.schema, schemaRecord.resolver, schemaRecord.revocable));
     }
