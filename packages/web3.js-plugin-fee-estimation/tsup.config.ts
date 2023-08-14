9c21fdc1e36d3c50bde010dfc7eaa25ce7153f3d
Wyatt Barnes <me@wyatt.email>
2023-08-14 11:29:05 -1000

    Solve merge conflict

---
+++ packages/web3.js-plugin-fee-estimation/tsup.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'tsup'
+import packageJson from './package.json'
+
+// @see https://tsup.egoist.dev/
+export default defineConfig({
+  name: packageJson.name,
+  entry: ['src/plugin.ts'],
+  outDir: 'dist',
+  format: ['esm', 'cjs'],
+  splitting: false,
+  sourcemap: true,
+  clean: false,
+})
