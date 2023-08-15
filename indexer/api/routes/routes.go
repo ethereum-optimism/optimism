519373bf7010763f0c991a0ea9d017e19b6ba559
Will Cory <willcory@Wills-MacBook-Pro.local>
2023-08-15 11:43:08 -0700

    feat(indexer): Add generated api docs

--- indexer/api/routes/routes.go
+++ indexer/api/routes/routes.go
@@ -3,16 +3,19 @@ package routes
 import (
 	"github.com/ethereum-optimism/optimism/indexer/database"
 	"github.com/ethereum/go-ethereum/log"
+	"github.com/go-chi/chi/v5"
 )
 
 type Routes struct {
 	Logger              log.Logger
 	BridgeTransfersView database.BridgeTransfersView
+	Router              *chi.Mux
 }
 
-func NewRoutes(logger log.Logger, bv database.BridgeTransfersView) Routes {
+func NewRoutes(logger log.Logger, bv database.BridgeTransfersView, r *chi.Mux) Routes {
 	return Routes{
 		Logger:              logger,
 		BridgeTransfersView: bv,
+		Router:              r,
 	}
 }
