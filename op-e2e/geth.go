205ab46e24423ee1f90c472549baa5e396eddea9
Adrian Sutton <adrian@oplabs.co>
2023-08-22 08:59:00 +1000

    op-e2e: Use callTracer when tracing failed transactions

--- op-e2e/geth.go
+++ op-e2e/geth.go
@@ -24,6 +24,10 @@ import (
 	"github.com/ethereum/go-ethereum/log"
 	"github.com/ethereum/go-ethereum/miner"
 	"github.com/ethereum/go-ethereum/node"
+
+	// Force-load the tracer engines to trigger registration
+	_ "github.com/ethereum/go-ethereum/eth/tracers/js"
+	_ "github.com/ethereum/go-ethereum/eth/tracers/native"
 )
 
 var (
