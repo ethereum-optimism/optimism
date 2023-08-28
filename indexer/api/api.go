6edc47408290135a8f2aa72293b345bb478a0c5a
Will Cory <willcory@Wills-MacBook-Pro.local>
2023-08-28 14:51:20 -0700

    chore(indexer): Use middleware for healthz

--- indexer/api/api.go
+++ indexer/api/api.go
@@ -10,6 +10,7 @@ import (
 	"github.com/ethereum-optimism/optimism/op-service/httputil"
 	"github.com/ethereum/go-ethereum/log"
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 const ethereumAddressRegex = `^0x[a-fA-F0-9]{40}$`
@@ -23,7 +24,8 @@ func NewApi(logger log.Logger, bv database.BridgeTransfersView) *Api {
 	r := chi.NewRouter()
 	h := routes.NewRoutes(logger, bv, r)
 
-	r.Get("/healthz", h.HealthzHandler)
+	r.Use(middleware.Heartbeat("/healthz"))
+
 	r.Get(fmt.Sprintf("/api/v0/deposits/{address:%s}", ethereumAddressRegex), h.L1DepositsHandler)
 	r.Get(fmt.Sprintf("/api/v0/withdrawals/{address:%s}", ethereumAddressRegex), h.L2WithdrawalsHandler)
 	return &Api{log: logger, Router: r}
