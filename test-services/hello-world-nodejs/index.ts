296d8eca9e48754eb5c85da5caec9b5ee3784711
Wyatt Barnes <me@wyatt.email>
2023-08-31 09:59:43 -1000

    Update nodejs example to increment currentValue from Prometheus

--- test-services/hello-world-nodejs/index.ts
+++ test-services/hello-world-nodejs/index.ts
@@ -1,18 +1,46 @@
+import Axios from 'axios'
 import { Counter, register, Pushgateway } from 'prom-client';
 
-// Create a new counter
-const counter = new Counter({
-  name: 'nodejs_counter',
-  help: 'Description of my counter',
-});
+const metricName = 'nodejs_counter';
+const prometheusServerURL = 'http://prometheus:9090';
+const pushGatewayURL = 'http://pushgateway:9091';
 
-// Increment the counter
-counter.inc();
+async function getMetricValueFromPrometheus(metricName: string) {
+  try {
+      const response = await Axios.get(`${prometheusServerURL}/api/v1/query?query=${metricName}`);
+      const data = response.data;
 
-// Register the metric
-register.registerMetric(counter);
+      if (data && data.data && data.data.result && data.data.result.length > 0) {
+          return parseFloat(data.data.result[0].value[1]);
+      } else {
+          console.warn(`No data found for metric ${metricName} in Prometheus.`);
+          return 0;
+      }
+  } catch (error) {
+      console.error(`Error querying Prometheus for metric ${metricName}:`, error);
+      return 0;
+  }
+}
 
-// Push the metric to the Pushgateway
-const pushGateway = new Pushgateway('http://pushgateway:9091', undefined, register);
 
-pushGateway.pushAdd({ jobName: 'job_name', groupings: { instance: 'localhost' }});
+async function main() {
+    const currentValue = await getMetricValueFromPrometheus(metricName);
+
+    // Initialize the counter with the current value from Prometheus
+    const counter = new Counter({
+        name: metricName,
+        help: 'Description of the counter'
+    });
+
+    // Increment the counter
+    counter.inc(currentValue + 1);
+
+    register.registerMetric(counter);
+
+    const pushGateway = new Pushgateway(pushGatewayURL, undefined, register);
+
+    // Push the updated metric to the Push Gateway
+    pushGateway.pushAdd({ jobName: 'job_name', groupings: { instance: 'localhost' }});
+}
+
+main();
