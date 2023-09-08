cc163aad4a880b4fb7151610225f6dc6dd1b40af
Maurelian <maurelian@protonmail.ch>
2023-09-08 10:44:20 -0400

    feat(ctb): Simplify by reducing the amount of assembly used.

--- packages/contracts-bedrock/src/universal/DelayedVetoable.sol
+++ packages/contracts-bedrock/src/universal/DelayedVetoable.sol
@@ -32,26 +32,9 @@ contract DelayedVetoable {
     function _handleCall() internal {
         require(_target != address(0), "DelayedVetoable: target not initialized");
 
-        address target = _target;
-        // emit Forwarded(msg.data);
-        bytes32 forwardHash = keccak256("Forwarded(bytes)");
+        emit Forwarded(msg.data);
+        (bool success,) = _target.call(msg.data);
         assembly {
-            // Copy calldata into memory at 0x0....calldatasize.
-            calldatacopy(0x0, 0x0, calldatasize())
-
-            // TODO(maurelian): can we emit this in the assembly block to deduplicate
-            //   getting the calldata? I think I was doing it correctly, but the forge
-            //   test doesn't recognize this
-            log1(0x0, calldatasize(), forwardHash)
-
-            // Perform the call, make sure to pass all available gas.
-            let success := call(gas(), target, callvalue(), 0x0, calldatasize(), 0x0, 0x0)
-
-            // Copy returndata into memory at 0x0....returndatasize. Note that this *will*
-            // overwrite the calldata that we just copied into memory but that doesn't really
-            // matter because we'll be returning in a second anyway.
-            returndatacopy(0x0, 0x0, returndatasize())
-
             // Success == 0 means a revert. We'll revert too and pass the data up.
             if iszero(success) { revert(0x0, returndatasize()) }
 
