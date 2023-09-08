b1b8f2fa96662b454567181bbde26567cc7f4b3d
Maurelian <maurelian@protonmail.ch>
2023-09-08 14:03:16 -0400

    feat(ctb): Add the delay.

--- packages/contracts-bedrock/test/DelayedVetoable.t.sol
+++ packages/contracts-bedrock/test/DelayedVetoable.t.sol
@@ -5,38 +5,67 @@ import { CommonTest, Reverter } from "./CommonTest.t.sol";
 import { DelayedVetoable } from "../src/universal/DelayedVetoable.sol";
 
 contract DelayedVetoable_Init is CommonTest {
-    event Forwarded(bytes data);
+    event Initiated(bytes32 indexed callHash, bytes data);
+    event Forwarded(bytes32 indexed callHash, bytes data);
 
     address target = address(0xabba);
+    uint256 delay = 14 days;
     DelayedVetoable delayedVetoable;
     Reverter reverter;
 
     function setUp() public override {
         super.setUp();
         delayedVetoable = new DelayedVetoable({
-            target: address(target)
+            target: address(target),
+            delay: delay
         });
         reverter = new Reverter();
     }
 }
 
 contract DelayedVetoable_HandleCall_Test is DelayedVetoable_Init {
-    function testFuzz_handleCall_succeeds(bytes memory data) external {
-        vm.expectCall(target, data);
+    function testFuzz_handleCall_initiation_succeeds(bytes memory data) external {
         vm.expectEmit(true, false, false, true, address(delayedVetoable));
-        emit Forwarded(data);
+        emit Initiated(keccak256(data), data);
 
         (bool success,) = address(delayedVetoable).call(data);
+        assert(success);
+    }
+
+    function testFuzz_handleCall_forwarding_succeeds(bytes memory data) external {
+        // Initiate the call
+        (bool success,) = address(delayedVetoable).call(data);
+
+        vm.warp(block.timestamp + delay);
+        vm.expectEmit(true, false, false, true, address(delayedVetoable));
+        emit Forwarded(keccak256(data), data);
+
+        vm.expectCall({ callee: target, data: data });
+        (success,) = address(delayedVetoable).call(data);
         assert(success);
     }
 }
 
 contract DelayedVetoable_HandleCall_TestFail is DelayedVetoable_Init {
-    function test_handleCall_reverts() external {
-        vm.expectCall(target, NON_ZERO_DATA);
+    function test_handleCall_forwardingTooSoon_reverts(bytes memory data) external {
+        (bool success,) = address(delayedVetoable).call(data);
+
         vm.expectRevert();
-        // including data will call the fallback
-        (bool success,) = address(delayedVetoable).call(NON_ZERO_DATA);
+        (success,) = address(delayedVetoable).call(data);
+        assertFalse(success);
+    }
+
+    function test_handleCall_forwardingTargetReverts_reverts(bytes memory data) external {
+        vm.etch(target, address(reverter).code);
+
+        (bool success,) = address(delayedVetoable).call(data);
+
+        vm.warp(block.timestamp + delay);
+        vm.expectEmit(true, false, false, true, address(delayedVetoable));
+        emit Forwarded(keccak256(data), data);
+
+        vm.expectCall({ callee: target, data: data });
+        (success,) = address(delayedVetoable).call(data);
         assertFalse(success);
     }
 }
