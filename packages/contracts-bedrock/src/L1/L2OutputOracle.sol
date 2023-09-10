e7d6b16d18361387803f187f54ad5ee77a4c5211
Mark Tyneway <mark.tyneway@gmail.com>
2023-09-10 06:10:08 -0700

    contracts-bedrock: migrate to ISemver
    Migrate away from using `Semver` where the semantic version is immutable
    to using `ISemver` where the semantic version is a constant. Only update
    the L1 contracts, this change should be followed up with additional PRs
    that update the other contracts. This change simplifies the code and
    reduces the bytecode size of each of the contracts.

--- packages/contracts-bedrock/src/L1/L2OutputOracle.sol
+++ packages/contracts-bedrock/src/L1/L2OutputOracle.sol
@@ -2,7 +2,7 @@
 pragma solidity 0.8.15;
 
 import { Initializable } from "@openzeppelin/contracts/proxy/utils/Initializable.sol";
-import { Semver } from "../universal/Semver.sol";
+import { ISemver } from "../universal/ISemver.sol";
 import { Types } from "../libraries/Types.sol";
 
 /// @custom:proxied
@@ -10,7 +10,7 @@ import { Types } from "../libraries/Types.sol";
 /// @notice The L2OutputOracle contains an array of L2 state outputs, where each output is a
 ///         commitment to the state of the L2 chain. Other contracts like the OptimismPortal use
 ///         these outputs to verify information about the state of L2.
-contract L2OutputOracle is Initializable, Semver {
+contract L2OutputOracle is Initializable, ISemver {
     /// @notice The interval in L2 blocks at which checkpoints must be submitted.
     ///         Although this is immutable, it can safely be modified by upgrading the
     ///         implementation contract.
@@ -62,7 +62,10 @@ contract L2OutputOracle is Initializable, Semver {
     /// @param newNextOutputIndex  Next L2 output index after the deletion.
     event OutputsDeleted(uint256 indexed prevNextOutputIndex, uint256 indexed newNextOutputIndex);
 
-    /// @custom:semver 1.4.1
+    /// @notice Semantic version.
+    /// @custom:semver 1.4.2
+    string constant public version = "1.4.2";
+
     /// @notice Constructs the L2OutputOracle contract.
     /// @param _submissionInterval  Interval in blocks at which checkpoints must be submitted.
     /// @param _l2BlockTime         The time per L2 block, in seconds.
@@ -72,9 +75,7 @@ contract L2OutputOracle is Initializable, Semver {
         uint256 _submissionInterval,
         uint256 _l2BlockTime,
         uint256 _finalizationPeriodSeconds
-    )
-        Semver(1, 4, 1)
-    {
+    ) {
         require(_l2BlockTime > 0, "L2OutputOracle: L2 block time must be greater than 0");
         require(_submissionInterval > 0, "L2OutputOracle: submission interval must be greater than 0");
 
