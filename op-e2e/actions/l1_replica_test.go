533138f5ac480f56772d2c4f198c4f4840bda7ae
protolambda <proto@protolambda.com>
2023-09-20 15:43:28 +0200

    go: update to op-geth with upstream v1.13.1 changes

--- op-e2e/actions/l1_replica_test.go
+++ op-e2e/actions/l1_replica_test.go
@@ -10,6 +10,8 @@ import (
 	"github.com/ethereum/go-ethereum/core/rawdb"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/log"
+	"github.com/ethereum/go-ethereum/trie"
+	"github.com/ethereum/go-ethereum/trie/triedb/hashdb"
 	"github.com/stretchr/testify/require"
 
 	"github.com/ethereum-optimism/optimism/op-e2e/e2eutils"
@@ -58,7 +60,8 @@ func TestL1Replica_ActL1Sync(gt *testing.T) {
 	genesisBlock := sd.L1Cfg.ToBlock()
 	consensus := beacon.New(ethash.NewFaker())
 	db := rawdb.NewMemoryDatabase()
-	sd.L1Cfg.MustCommit(db)
+	tdb := trie.NewDatabase(db, &trie.Config{HashDB: hashdb.Defaults})
+	sd.L1Cfg.MustCommit(db, tdb)
 
 	chainA, _ := core.GenerateChain(sd.L1Cfg.Config, genesisBlock, consensus, db, 10, func(n int, g *core.BlockGen) {
 		g.SetCoinbase(common.Address{'A'})
