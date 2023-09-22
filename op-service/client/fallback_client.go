4dba38343201b5c420d94e82743cb6a8abb63a1e
welkin22 <xwmbxx@gmail.com>
2023-09-22 17:23:21 +0800

    add comment to op-batcher's FallbackClient

--- op-service/client/fallback_client.go
+++ op-service/client/fallback_client.go
@@ -2,6 +2,7 @@ package client
 
 import (
 	"context"
+	"errors"
 	opmetrics "github.com/ethereum-optimism/optimism/op-service/metrics"
 	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/common"
@@ -38,19 +39,25 @@ func NewFallbackClientMetrics(ns string, factory opmetrics.Factory) *FallbackCli
 // and automatically switch back after the first endpoint recovers.
 type FallbackClient struct {
 	// firstClient is created by the first of the urls, it should be used first in a healthy state
-	firstClient       EthClient
-	urlList           []string
-	clientInitFunc    func(url string) (EthClient, error)
-	lastMinuteFail    atomic.Int64
-	currentClient     atomic.Pointer[EthClient]
-	currentIndex      int
-	mx                sync.Mutex
-	log               log.Logger
+	firstClient    EthClient
+	urlList        []string
+	clientInitFunc func(url string) (EthClient, error)
+	// lastMinuteFail is used to record the number of errors in the last minute
+	lastMinuteFail atomic.Int64
+	// currentClient always points to the client currently being used
+	currentClient atomic.Pointer[EthClient]
+	// currentIndex is the index of the current client in the urlList
+	currentIndex int
+	mx           sync.Mutex
+	log          log.Logger
+	// isInFallbackState is used to record whether the current rpc is in a fallback state,
+	// Used to ensure that only one recoverIfFirstClientHealth process is started at the same time
 	isInFallbackState bool
 	// fallbackThreshold specifies how many errors have occurred in the past 1 minute to trigger the switching logic
 	fallbackThreshold int64
-	isClose           chan struct{}
-	metrics           FallbackClientMetricer
+	// isClose is used to close the goroutine that monitors the number of errors in the last minute
+	isClose chan struct{}
+	metrics FallbackClientMetricer
 }
 
 // NewFallbackClient returns a new FallbackClient.
@@ -65,6 +72,7 @@ func NewFallbackClient(rpc EthClient, urlList []string, log log.Logger, fallback
 		metrics:           m,
 	}
 	fallbackClient.currentClient.Store(&rpc)
+	// monitor the number of errors in the last minute
 	go func() {
 		ticker := time.NewTicker(1 * time.Minute)
 		for {
@@ -75,6 +83,7 @@ func NewFallbackClient(rpc EthClient, urlList []string, log log.Logger, fallback
 			case <-fallbackClient.isClose:
 				return
 			default:
+				// if the number of errors in the last minute exceeds the threshold, switch to the next client
 				if fallbackClient.lastMinuteFail.Load() >= fallbackClient.fallbackThreshold {
 					fallbackClient.switchCurrentClient()
 				}
@@ -201,50 +210,73 @@ func (l *FallbackClient) NonceAt(ctx context.Context, account common.Address, bl
 }
 
 func (l *FallbackClient) handleErr(err error) {
-	if err == rpc.ErrNoResult {
+	if errors.Is(err, rpc.ErrNoResult) {
 		return
 	}
-	if _, ok := err.(rpc.Error); ok {
+	var targetErr rpc.Error
+	if errors.As(err, &targetErr) {
 		return
 	}
 	l.lastMinuteFail.Add(1)
 }
 
+// switchCurrentClient switches the current client to the next client
 func (l *FallbackClient) switchCurrentClient() {
 	l.mx.Lock()
 	defer l.mx.Unlock()
+	// double check to avoid switching to the next client at the same time
 	if l.lastMinuteFail.Load() <= l.fallbackThreshold {
 		return
 	}
-	l.currentIndex++
-	if l.currentIndex >= len(l.urlList) {
-		l.log.Error("the fallback client has tried all urls")
-		return
+	// iterate through the urlList to find the next available rpc
+	for {
+		l.currentIndex++
+		if l.currentIndex >= len(l.urlList) {
+			l.log.Error("the fallback client has tried all urls, but all failed")
+			break
+		}
+		err := l.switchCurrentClientLogic()
+		if err != nil {
+			l.log.Warn("the fallback client failed to switch the current client", "err", err)
+		} else {
+			if l.metrics != nil {
+				l.metrics.RecordUrlSwitchEvt(strconv.Itoa(l.currentIndex))
+			}
+			break
+		}
 	}
-	l.metrics.RecordUrlSwitchEvt(strconv.Itoa(l.currentIndex))
+}
+
+func (l *FallbackClient) switchCurrentClientLogic() error {
 	url := l.urlList[l.currentIndex]
 	newClient, err := l.clientInitFunc(url)
 	if err != nil {
 		l.log.Error("the fallback client failed to switch the current client", "url", url, "err", err)
-		return
+		return err
 	}
 	lastClient := *l.currentClient.Load()
+	// switch to the new client
 	l.currentClient.Store(&newClient)
+	// we don't close first client, the first client need to be recovered when it is healthy
 	if lastClient != l.firstClient {
 		lastClient.Close()
 	}
+	// clear the number of errors in the last minute
 	l.lastMinuteFail.Store(0)
-	l.log.Info("switched current rpc to new url", "url", url)
+	l.log.Info("switched current client to new url", "url", url)
 	if !l.isInFallbackState {
 		l.isInFallbackState = true
-		l.recoverIfFirstRpcHealth()
+		l.recoverIfFirstClientHealth()
 	}
+	return nil
 }
 
-func (l *FallbackClient) recoverIfFirstRpcHealth() {
+// recoverIfFirstClientHealth recovers the first client if it is healthy
+func (l *FallbackClient) recoverIfFirstClientHealth() {
 	go func() {
 		count := 0
 		for {
+			// use eth_chainId to check whether the first client is healthy
 			_, err := l.firstClient.ChainID(context.Background())
 			if err != nil {
 				count = 0
@@ -252,12 +284,15 @@ func (l *FallbackClient) recoverIfFirstRpcHealth() {
 				continue
 			}
 			count++
+			// the client is considered healthy if it succeeds in 3 consecutive requests.
 			if count >= 3 {
 				break
 			}
 		}
+		// lock to avoid switching to the next client at the same time
 		l.mx.Lock()
 		defer l.mx.Unlock()
+		// double check
 		if !l.isInFallbackState {
 			return
 		}
@@ -265,6 +300,7 @@ func (l *FallbackClient) recoverIfFirstRpcHealth() {
 		l.currentClient.Store(&l.firstClient)
 		lastClient.Close()
 		l.lastMinuteFail.Store(0)
+		// go back to the first index
 		l.currentIndex = 0
 		l.isInFallbackState = false
 		l.log.Info("recover the current client to the first client", "url", l.urlList[0])
