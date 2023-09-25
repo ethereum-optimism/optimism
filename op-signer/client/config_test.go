2698fe83d1d139b10e1af7e91528f16d68e6ee4b
Sabnock01 <24715302+Sabnock01@users.noreply.github.com>
2023-09-25 15:25:22 -0500

    fix: signer config tests

--- op-signer/client/config_test.go
+++ op-signer/client/config_test.go
@@ -5,6 +5,8 @@ import (
 
 	"github.com/stretchr/testify/require"
 	"github.com/urfave/cli/v2"
+
+	optls "github.com/ethereum-optimism/optimism/op-service/tls"
 )
 
 func TestDefaultCLIOptionsMatchDefaultConfig(t *testing.T) {
@@ -22,26 +24,26 @@ func TestInvalidConfig(t *testing.T) {
 	tests := []struct {
 		name         string
 		expected     string
-		configChange func(config *CLIConfig)
+		configChange func(config *optls.SignerCLIConfig)
 	}{
 		{
 			name:     "MissingEndpoint",
 			expected: "signer endpoint and address must both be set or not set",
-			configChange: func(config *CLIConfig) {
+			configChange: func(config *optls.SignerCLIConfig) {
 				config.Address = "0x1234"
 			},
 		},
 		{
 			name:     "MissingAddress",
 			expected: "signer endpoint and address must both be set or not set",
-			configChange: func(config *CLIConfig) {
+			configChange: func(config *optls.SignerCLIConfig) {
 				config.Endpoint = "http://localhost"
 			},
 		},
 		{
 			name:     "InvalidTLSConfig",
 			expected: "all tls flags must be set if at least one is set",
-			configChange: func(config *CLIConfig) {
+			configChange: func(config *optls.SignerCLIConfig) {
 				config.TLSConfig.TLSKey = ""
 			},
 		},
@@ -56,11 +58,11 @@ func TestInvalidConfig(t *testing.T) {
 	}
 }
 
-func configForArgs(args ...string) CLIConfig {
+func configForArgs(args ...string) optls.SignerCLIConfig {
 	app := cli.NewApp()
 	app.Flags = CLIFlags("TEST_")
 	app.Name = "test"
-	var config CLIConfig
+	var config optls.SignerCLIConfig
 	app.Action = func(ctx *cli.Context) error {
 		config = ReadCLIConfig(ctx)
 		return nil
