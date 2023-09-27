be4365157400aac271e6958770af0ac30bbcd8d0
cby3149 <cby3149@gmail.com>
2023-09-27 13:26:50 -0700

    Remove L2RpcClient

--- op-e2e/op_geth.go
+++ op-e2e/op_geth.go
@@ -41,7 +41,6 @@ type OpGeth struct {
 	node          EthInstance
 	l2Engine      *sources.EngineClient
 	L2Client      *ethclient.Client
-	L2RpcClient   *rpc.Client
 	SystemConfig  eth.SystemConfig
 	L1ChainConfig *params.ChainConfig
 	L2ChainConfig *params.ChainConfig
@@ -82,7 +81,7 @@ func NewOpGeth(t *testing.T, ctx context.Context, cfg *SystemConfig) (*OpGeth, e
 		node = gethNode
 	} else {
 		externalNode := (&ExternalRunner{
-			Name:    "Sequencer",
+			Name:    "l2",
 			BinPath: cfg.ExternalL2Shim,
 			Genesis: l2Genesis,
 			JWTPath: cfg.JWTFilePath,
@@ -106,16 +105,12 @@ func NewOpGeth(t *testing.T, ctx context.Context, cfg *SystemConfig) (*OpGeth, e
 	l2Client, err := ethclient.Dial(node.HTTPEndpoint())
 	require.Nil(t, err)
 
-	l2RpcClient, err := rpc.Dial(node.HTTPEndpoint())
-	require.Nil(t, err)
-
 	genesisPayload, err := eth.BlockAsPayload(l2GenesisBlock)
 
 	require.Nil(t, err)
 	return &OpGeth{
 		node:          node,
 		L2Client:      l2Client,
-		L2RpcClient:   l2RpcClient,
 		l2Engine:      l2Engine,
 		SystemConfig:  rollupGenesis.SystemConfig,
 		L1ChainConfig: l1Genesis.Config,
@@ -129,7 +124,6 @@ func (d *OpGeth) Close() {
 	_ = d.node.Close()
 	d.l2Engine.Close()
 	d.L2Client.Close()
-	d.L2RpcClient.Close()
 }
 
 // AddL2Block Appends a new L2 block to the current chain including the specified transactions
