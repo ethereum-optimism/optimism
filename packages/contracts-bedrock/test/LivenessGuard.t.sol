0d9784bfd2bd69b5ca30c1f27ab9b0c4745d4240
Maurelian <maurelian@protonmail.ch>
2023-09-30 23:36:20 -0400

    feat(ctb): Simplify check tests with safe-tools

---
+++ packages/contracts-bedrock/test/LivenessGuard.t.sol
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.15;
+
+import { Test } from "forge-std/Test.sol";
+import { Safe } from "safe-contracts/Safe.sol";
+import { SafeProxyFactory } from "safe-contracts/proxies/SafeProxyFactory.sol";
+import { ModuleManager } from "safe-contracts/base/ModuleManager.sol";
+import { Enum } from "safe-contracts/common/Enum.sol";
+import "test/safe-tools/SafeTestTools.sol";
+
+import { LivenessGuard } from "src/Safe/LivenessGuard.sol";
+
+// Todo(Maurelian):
+// Other tests needed:
+//   - EIP1271 signatures
+//   - Signatures from contracts
+//   - Signatures from non-owners
+//   - Signers may call directly to prove liveness (must be an owner).
+//   - Unexpected length of signature data
+
+contract LivnessGuard_TestInit is Test, SafeTestTools {
+    using SafeTestLib for SafeInstance;
+
+    LivenessGuard livenessGuard;
+    SafeInstance safeInstance;
+
+    function setUp() public {
+        safeInstance = _setupSafe();
+        livenessGuard = new LivenessGuard(safeInstance.safe);
+        safeInstance.enableModule(address(livenessGuard));
+    }
+}
+
+contract LivnessGuard_TestCheckTx is LivnessGuard_TestInit {
+    using SafeTestLib for SafeInstance;
+
+    function test_checkTransaction_succeeds() external {
+        safeInstance.execTransaction({ to: address(1111), value: 0, data: hex"abba" });
+
+        // bug signers need to be sorted from low to high
+        for (uint256 i; i < safeInstance.owners.length; i++) {
+            assertEq(livenessGuard.lastSigned(safeInstance.owners[i]), block.timestamp);
+        }
+    }
+}
+
+contract LivenessGuard_ShowLiveness_Test is LivnessGuard_TestInit {
+    function test_showLiveness_succeeds() external {
+        // Cache the caller
+        address caller = safeInstance.owners[0];
+
+        // Construct a signers array with just the caller to identify the expected event.
+        address[] memory signers = new address[](1);
+        signers[0] = caller;
+        vm.expectEmit(address(livenessGuard));
+        emit SignersRecorded(0x0, signers);
+
+        vm.prank(caller);
+        livenessGuard.showLiveness();
+
+        assertEq(livenessGuard.lastSigned(caller), block.timestamp);
+    }
+}
