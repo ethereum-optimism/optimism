9ec88925b2cad4a33625f1e591d2ad81a5150100
protolambda <proto@protolambda.com>
2023-10-05 15:36:42 +0200

    op-service: command lifecycle testing with mock app and substitute process signals

--- op-service/cliapp/lifecycle.go
+++ op-service/cliapp/lifecycle.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"os"
 
 	"github.com/urfave/cli/v2"
 
@@ -34,23 +35,39 @@ type LifecycleAction func(ctx *cli.Context, close context.CancelCauseFunc) (Life
 // The app may continue to run post-processing until fully shutting down.
 // The user can force an early shut-down during post-processing by sending a second interruption signal.
 func LifecycleCmd(fn LifecycleAction) cli.ActionFunc {
+	return lifecycleCmd(fn, opio.BlockOnInterruptsContext)
+}
+
+type waitSignalFn func(ctx context.Context, signals ...os.Signal)
+
+var interruptErr = errors.New("interrupt signal")
+
+func lifecycleCmd(fn LifecycleAction, blockOnInterrupt waitSignalFn) cli.ActionFunc {
 	return func(ctx *cli.Context) error {
 		hostCtx := ctx.Context
 		appCtx, appCancel := context.WithCancelCause(hostCtx)
 		ctx.Context = appCtx
 
 		go func() {
-			opio.BlockOnInterruptsContext(appCtx)
-			appCancel(errors.New("interrupt signal"))
+			blockOnInterrupt(appCtx)
+			appCancel(interruptErr)
 		}()
 
 		appLifecycle, err := fn(ctx, appCancel)
 		if err != nil {
-			return fmt.Errorf("failed to setup: %w", err)
+			// join errors to include context cause (nil errors are dropped)
+			return errors.Join(
+				fmt.Errorf("failed to setup: %w", err),
+				context.Cause(appCtx),
+			)
 		}
 
 		if err := appLifecycle.Start(appCtx); err != nil {
-			return fmt.Errorf("failed to start: %w", err)
+			// join errors to include context cause (nil errors are dropped)
+			return errors.Join(
+				fmt.Errorf("failed to start: %w", err),
+				context.Cause(appCtx),
+			)
 		}
 
 		// wait for app to be closed (through interrupt, or app requests to be stopped by closing the context)
@@ -58,17 +75,23 @@ func LifecycleCmd(fn LifecycleAction) cli.ActionFunc {
 
 		// Graceful stop context.
 		// This allows the service to idle before shutdown, if halted. User may interrupt.
-		stopCtx, stopCancel := context.WithCancel(hostCtx)
+		stopCtx, stopCancel := context.WithCancelCause(hostCtx)
 		go func() {
-			opio.BlockOnInterruptsContext(stopCtx)
-			stopCancel()
+			blockOnInterrupt(stopCtx)
+			stopCancel(interruptErr)
 		}()
 
 		// Execute graceful stop.
 		stopErr := appLifecycle.Stop(stopCtx)
-		stopCancel()
+		stopCancel(nil)
+		// note: Stop implementation may choose to suppress a context error,
+		// if it handles it well (e.g. stop idling after a halt).
 		if stopErr != nil {
-			return fmt.Errorf("failed to stop app: %w", stopErr)
+			// join errors to include context cause (nil errors are dropped)
+			return errors.Join(
+				fmt.Errorf("failed to stop: %w", stopErr),
+				context.Cause(stopCtx),
+			)
 		}
 
 		return nil
