18756d2e0506293bfe0fa14882f4cf29c87dcc2e
alvrs <alvarius@lattice.xyz>
2023-10-11 14:44:39 +0200

    add tests for modifying contract parameters

--- packages/contracts-bedrock/test/DataAvailabilityChallenge.t.sol
+++ packages/contracts-bedrock/test/DataAvailabilityChallenge.t.sol
@@ -17,6 +17,17 @@ contract DataAvailabilityChallengeTest is Test {
         dac.initialize(DAC_OWNER, CHALLENGE_WINDOW, RESOLVE_WINDOW, BOND_SIZE);
     }
 
+    function testInitialize() public {
+        assertEq(dac.owner(), DAC_OWNER);
+        assertEq(dac.challengeWindow(), CHALLENGE_WINDOW);
+        assertEq(dac.resolveWindow(), RESOLVE_WINDOW);
+        assertEq(dac.bondSize(), BOND_SIZE);
+
+        vm.expectRevert("Initializable: contract is already initialized");
+        dac.initialize(DAC_OWNER, CHALLENGE_WINDOW, RESOLVE_WINDOW, BOND_SIZE);
+    }
+
+
     function testDeposit() public {
         assertEq(dac.balances(address(this)), 0);
         dac.deposit{value: 1000}();
@@ -25,6 +36,7 @@ contract DataAvailabilityChallengeTest is Test {
 
     function testWithdraw(address sender, uint256 amount) public {
         assumePayable(sender);
+        assumeNoPrecompiles(sender);
         vm.assume(sender.balance == 0);
         vm.deal(sender, amount);
 
@@ -317,5 +329,128 @@ contract DataAvailabilityChallengeTest is Test {
         // Expiring the challenge before the resolve window closes fails
         vm.expectRevert(abi.encodeWithSelector(DataAvailabilityChallenge.ResolveWindowNotClosed.selector));
         dac.expire(challengeBlock, challengeHash);
+    }
+
+    function testSetChallengeWindow(address challenger, uint256 challengedBlockNumber, bytes32 challengedHash) public {
+        // Assume the block number is not close to the max uint256 value
+        vm.assume(challengedBlockNumber < type(uint256).max - dac.challengeWindow() - dac.resolveWindow());
+
+        uint256 requiredBond = dac.bondSize();
+        uint256 blockDistance = CHALLENGE_WINDOW + 1;
+
+        // Move move to a block 100 blocks after the challenged block
+        vm.roll(challengedBlockNumber + blockDistance);
+
+        // Deposit the required bond
+        vm.deal(challenger, requiredBond);
+        vm.prank(challenger);
+        dac.deposit{value: requiredBond}();
+
+        // Challenge a (blockNumber,hash) tuple
+        // Expect challenge to fail because the challenge window is not open
+        vm.prank(challenger);
+        vm.expectRevert(abi.encodeWithSelector(DataAvailabilityChallenge.ChallengeWindowNotOpen.selector));
+        dac.challenge(challengedBlockNumber, challengedHash);
+
+        // Extend the challenge window
+        vm.prank(DAC_OWNER);
+        dac.setChallengeWindow(blockDistance);
+
+        // Expect the challenge to succeed
+        vm.prank(challenger);
+        dac.challenge(challengedBlockNumber, challengedHash);
+
+        // Challenge should have been created
+        (ChallengeStatus _status, address _challenger, uint256 _startBlock) =
+            dac.challenges(challengedBlockNumber, challengedHash);
+        assertEq(uint8(_status), uint8(ChallengeStatus.Active));
+        assertEq(_challenger, challenger);
+        assertEq(_startBlock, block.number);
+
+        // Challenge should have decreased the challenger's bond size
+        assertEq(dac.balances(challenger), 0);
+    }
+
+    function testSetChallengeWindowFailOnlyOwner(address notOwner, uint256 newChallengeWindow) public {
+        vm.assume(notOwner != DAC_OWNER);
+
+        // Expect setting the challenge window to fail because the sender is not the owner
+        vm.prank(notOwner);
+        vm.expectRevert("Ownable: caller is not the owner");
+        dac.setChallengeWindow(newChallengeWindow);
+    }
+
+    function testSetResolveWindow(bytes memory preImage, uint256 challengeBlock) public {
+        // Assume the block number is not close to the max uint256 value
+        vm.assume(challengeBlock < type(uint256).max - dac.challengeWindow() - dac.resolveWindow() - 1);
+        bytes32 challengeHash = keccak256(preImage);
+
+        uint256 challengeStartBlock = challengeBlock + 1;
+
+        // Move to block after challenged block
+        vm.roll(challengeStartBlock);
+
+        // Challenge the hash
+        dac.deposit{value: dac.bondSize()}();
+        dac.challenge(challengeBlock, challengeHash);
+
+        // Move to a block after the resolve window
+        uint256 blockDiff = dac.resolveWindow() + 1;
+        vm.roll(challengeStartBlock + blockDiff);
+
+        // Resolve the challenge
+        // Except the resolve to fail because the resolve window is not open
+        vm.expectRevert(abi.encodeWithSelector(DataAvailabilityChallenge.ResolveWindowNotOpen.selector));
+        dac.resolve(challengeBlock, preImage);
+
+        // Extend the resolve window
+        vm.prank(DAC_OWNER);
+        dac.setResolveWindow(blockDiff);
+
+        // Expect the resolve to succeed
+        dac.resolve(challengeBlock, preImage);
+
+        // Expect the challenge to be resolved
+        (ChallengeStatus _status, address _challenger, uint256 _startBlock) =
+            dac.challenges(challengeBlock, challengeHash);
+
+        assertEq(uint8(_status), uint8(ChallengeStatus.Resolved));
+        assertEq(_challenger, address(this));
+        assertEq(_startBlock, challengeStartBlock);
+    }
+
+    function testSetResolveWindowFailOnlyOwner(address notOwner, uint256 newResolveWindow) public {
+        vm.assume(notOwner != DAC_OWNER);
+
+        // Expect setting the resolve window to fail because the sender is not the owner
+        vm.prank(notOwner);
+        vm.expectRevert("Ownable: caller is not the owner");
+        dac.setResolveWindow(newResolveWindow);
+    }
+
+    function testSetBondSize() public {
+        uint256 requiredBond = dac.bondSize();
+        uint256 actualBond = requiredBond - 1;
+        dac.deposit{value: actualBond}();
+
+        // Expect the challenge to fail because the bond is too low
+        vm.expectRevert(abi.encodeWithSelector(DataAvailabilityChallenge.BondTooLow.selector, actualBond, requiredBond));
+        dac.challenge(0, "some hash");
+
+        // Reduce the required bond
+        vm.prank(DAC_OWNER);
+        dac.setBondSize(actualBond);
+
+        // Expect the challenge to succeed
+        dac.challenge(0, "some hash");
+    }
+
+    function testSetBondSizeFailOnlyOwner(address notOwner, uint256 newBondSize) public {
+        vm.assume(notOwner != DAC_OWNER);
+
+        // Expect setting the bond size to fail because the sender is not the owner
+        vm.prank(notOwner);
+        vm.expectRevert("Ownable: caller is not the owner");
+        dac.setBondSize(newBondSize);
     }
 }
