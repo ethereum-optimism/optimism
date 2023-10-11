e603267a3e5ee4926fecb3698272fb560ddc179f
alvrs <alvarius@lattice.xyz>
2023-10-11 13:17:36 +0200

    add DataAvailabilityChallenge contract and test

---
+++ packages/contracts-bedrock/src/L1/DataAvailabilityChallenge.sol
@@ -0,0 +1,212 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.15;
+
+/// @dev An enum representing the status of a DA challenge.
+enum ChallengeStatus {
+    Uninitialized,
+    Active,
+    Resolved,
+    Expired
+}
+
+/// @dev A struct representing a single DA challenge.
+/// @custom:field status The status of the challenge.
+/// @custom:field challenger The address that initiated the challenge.
+/// @custom:field startBlock The block number at which the challenge was initiated.
+struct Challenge {
+    ChallengeStatus status;
+    address challenger;
+    uint256 startBlock;
+}
+
+/// @title DataAvailabilityChallenge
+/// @notice This contract enables data availability of a data commitment at a given block number to be challenged.
+///         To challenge a commitment, the challenger must first post a bond (bondSize).
+///         Challenging a commitment is only possible within a certain block interval (challengeWindow) after the commitment was made.
+///         If the challenge is not resolved within a certain block interval (resolveWindow), the challenge can be expired.
+///         If a challenge is expired, the challenger's bond is unlocked and the challenged commitment is added to the chain of expired challenges.
+contract DataAvailabilityChallenge {
+    /// @notice Error for when the challenger's bond is too low.
+    error BondTooLow(uint256 balance, uint256 required);
+
+    /// @notice Error for when attempting to challenge a commitment that already has a challenge.
+    error ChallengeExists();
+
+    /// @notice Error for when attempting to resolve a challenge that is not active.
+    error ChallengeNotActive();
+
+    /// @notice Error for when attempting to challenge a commitment that is not in the challenge window.
+    error ChallengeWindowNotOpen();
+
+    /// @notice Error for when attempting to resolve a challenge that is not in the resolve window.
+    error ResolveWindowNotOpen();
+
+    /// @notice Error for when attempting to expire a challenge that is still in the resolve window.
+    error ResolveWindowNotClosed();
+
+    /// @notice An event that is emitted when the status of a challenge changes.
+    /// @param challengedHash The hash of the commitment that is being challenged.
+    /// @param challengedBlockNumber The block number at which the commitment was made.
+    /// @param status The new status of the challenge.
+    event ChallengeStatusChanged(
+        bytes32 indexed challengedHash, uint256 indexed challengedBlockNumber, ChallengeStatus status
+    );
+
+    /// @notice An event that is emitted when the head of the chain of expired challenges is updated.
+    /// @param expiredChallengesHead The new head of the chain of expired challenges.
+    event ExpiredChallengesHeadUpdated(bytes32 expiredChallengesHead);
+
+    /// @notice The block interval during which a commitment can be challenged.
+    uint256 public challengeWindow;
+
+    /// @notice The block interval during which a challenge can be resolved.
+    uint256 public resolveWindow;
+
+    /// @notice The amount required to post a challenge.
+    uint256 public bondSize;
+
+    /// @notice A mapping from addresses to their bond balance in the contract.
+    mapping(address => uint256) public balances;
+
+    /// @notice A mapping from challenged block numbers to challenged hashes to challenges.
+    mapping(uint256 => mapping(bytes32 => Challenge)) public challenges;
+
+    /// @notice The head of the chain of expired challenges.
+    bytes32 public expiredChallengesHead;
+
+    /// @notice The constructor for the DataAvailabilityChallenge contract.
+    /// @param _challengeWindow The block interval during which a commitment can be challenged.
+    /// @param _resolveWindow The block interval during which a challenge can be resolved.
+    /// @param _bondSize The amount required to post a challenge.
+    constructor(uint256 _challengeWindow, uint256 _resolveWindow, uint256 _bondSize) {
+        challengeWindow = _challengeWindow;
+        resolveWindow = _resolveWindow;
+        bondSize = _bondSize;
+    }
+
+    /// @notice Checks if the current block is within the challenge window for a given challenged block number.
+    /// @param challengedBlockNumber The block number at which the commitment was made.
+    /// @return True if the current block is within the challenge window, false otherwise.
+    function _isInChallengeWindow(uint256 challengedBlockNumber) internal view returns (bool) {
+        return (block.number > challengedBlockNumber && block.number <= challengedBlockNumber + challengeWindow);
+    }
+
+    /// @notice Checks if the current block is within the resolve window for a given challenge start block number.
+    /// @param challengeStartBlockNumber The block number at which the challenge was initiated.
+    /// @return True if the current block is within the resolve window, false otherwise.
+    function _isInResolveWindow(uint256 challengeStartBlockNumber) internal view returns (bool) {
+        return block.number <= challengeStartBlockNumber + resolveWindow;
+    }
+
+    /// @notice Post a bond as prerequisite for challenging a commitment.
+    function deposit() external payable {
+        balances[msg.sender] += msg.value;
+    }
+
+    /// @notice Withdraw a user's unlocked bond.
+    function withdraw() external {
+        // get caller's balance
+        uint256 balance = balances[msg.sender];
+
+        // set caller's balance to 0
+        balances[msg.sender] = 0;
+
+        // send caller's balance to caller
+        payable(msg.sender).transfer(balance);
+    }
+
+    /// @notice Challenge a data commitment at a given block number.
+    /// @dev The block number parameter is necessary for the contract to verify the challenge window,
+    ///      since the contract cannot access the block number of the commitment.
+    ///      The function reverts if the caller does not have a bond or if the challenge already exists.
+    /// @param challengedBlockNumber The block number at which the commitment was made.
+    /// @param challengedHash The data commitment that is being challenged.
+    function challenge(uint256 challengedBlockNumber, bytes32 challengedHash) external {
+        // require the caller to have a bond
+        if (balances[msg.sender] < bondSize) {
+            revert BondTooLow(balances[msg.sender], bondSize);
+        }
+
+        // reduce the caller's bond
+        balances[msg.sender] -= bondSize;
+
+        // require the challenge status to be uninitialized
+        Challenge storage existingChallenge = challenges[challengedBlockNumber][challengedHash];
+        if (existingChallenge.status != ChallengeStatus.Uninitialized) {
+            revert ChallengeExists();
+        }
+
+        // require the current block to be in the challenge window
+        if (!_isInChallengeWindow(challengedBlockNumber)) {
+            revert ChallengeWindowNotOpen();
+        }
+
+        // set the status of this challenge to active, store the current block number and address of the challenger
+        challenges[challengedBlockNumber][challengedHash] =
+            Challenge({status: ChallengeStatus.Active, challenger: msg.sender, startBlock: block.number});
+
+        // emit an event to notify that the challenge status is now active
+        emit ChallengeStatusChanged(challengedHash, challengedBlockNumber, ChallengeStatus.Active);
+    }
+
+    /// @notice Resolve a challenge by providing the pre-image data of the challenged commitment.
+    /// @dev The provided pre-image data is hashed (keccak256) to verify that it matches the challenged commitment.
+    ///      The function reverts if the challenge is not active or if the resolve window is not open.
+    /// @param challengedBlockNumber The block number at which the commitment was made.
+    /// @param preImage The pre-image data corresponding to the challenged commitment.
+    function resolve(uint256 challengedBlockNumber, bytes calldata preImage) external {
+        // hash the preImage
+        bytes32 challengedHash = keccak256(preImage);
+
+        // require the challenge to be active
+        Challenge storage activeChallenge = challenges[challengedBlockNumber][challengedHash];
+        if (activeChallenge.status != ChallengeStatus.Active) {
+            revert ChallengeNotActive();
+        }
+
+        // require the resolve window to be open
+        if (!_isInResolveWindow(activeChallenge.startBlock)) {
+            revert ResolveWindowNotOpen();
+        }
+
+        // set the challenge status to resolved
+        activeChallenge.status = ChallengeStatus.Resolved;
+
+        // emit an event to notify that the challenge status is now resolved
+        emit ChallengeStatusChanged(challengedHash, challengedBlockNumber, ChallengeStatus.Resolved);
+    }
+
+    /// @notice Expire a challenge that has not been resolved within the resolve window.
+    /// @dev The function reverts if the challenge is not active or if the resolve window is still open.
+    ///      If the expiration is successful, the challenger's bond is unlocked
+    ///      and the challenged commitment is added to the chain of expired challenges.
+    /// @param challengedBlockNumber The block number at which the commitment was made.
+    /// @param challengedHash The data commitment that is being challenged.
+    function expire(uint256 challengedBlockNumber, bytes32 challengedHash) external {
+        // require the challenge to be active
+        Challenge storage activeChallenge = challenges[challengedBlockNumber][challengedHash];
+        if (activeChallenge.status != ChallengeStatus.Active) {
+            revert ChallengeNotActive();
+        }
+
+        // require the resolve window to be closed
+        if (_isInResolveWindow(activeChallenge.startBlock)) {
+            revert ResolveWindowNotClosed();
+        }
+
+        // set the status to expired
+        activeChallenge.status = ChallengeStatus.Expired;
+
+        // restore the challenger's bond
+        balances[activeChallenge.challenger] += bondSize;
+
+        // update the head of the chain of expired challenges
+        expiredChallengesHead = keccak256(abi.encode(expiredChallengesHead, challengedHash));
+
+        // emit an event to notify that the challenge status is now expired
+        emit ChallengeStatusChanged(challengedHash, challengedBlockNumber, ChallengeStatus.Expired);
+
+        // emit an event to notify that the head of the chain of expired challenges has been updated
+        emit ExpiredChallengesHeadUpdated(expiredChallengesHead);
+    }
+}
---
+++ packages/contracts-bedrock/test/DataAvailabilityChallenge.t.sol
@@ -0,0 +1,315 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.15;
+
+import {Test} from "forge-std/Test.sol";
+import {DataAvailabilityChallenge, ChallengeStatus, Challenge} from "../src/L1/DataAvailabilityChallenge.sol";
+
+contract DataAvailabilityChallengeTest is Test {
+    DataAvailabilityChallenge public dac;
+
+    function setUp() public {
+        dac = new DataAvailabilityChallenge(1000, 1000, 1000);
+    }
+
+    function testDeposit() public {
+        assertEq(dac.balances(address(this)), 0);
+        dac.deposit{value: 1000}();
+        assertEq(dac.balances(address(this)), 1000);
+    }
+
+    function testWithdraw(address sender, uint256 amount) public {
+        assumePayable(sender);
+        vm.assume(sender.balance == 0);
+        vm.deal(sender, amount);
+
+        vm.prank(sender);
+        dac.deposit{value: amount}();
+
+        assertEq(dac.balances(sender), amount);
+        assertEq(sender.balance, 0);
+
+        vm.prank(sender);
+        dac.withdraw();
+
+        assertEq(dac.balances(sender), 0);
+        assertEq(sender.balance, amount);
+    }
+
+    function testChallengeSuccess(address challenger, uint256 challengedBlockNumber, bytes32 challengedHash) public {
+        // Assume the block number is not close to the max uint256 value
+        vm.assume(challengedBlockNumber < type(uint256).max - dac.challengeWindow() - dac.resolveWindow());
+        uint256 requiredBond = dac.bondSize();
+
+        // Move to a block after the challenged block
+        vm.roll(challengedBlockNumber + 1);
+
+        // Deposit the required bond
+        vm.deal(challenger, requiredBond);
+        vm.prank(challenger);
+        dac.deposit{value: requiredBond}();
+
+        // Challenge a (blockNumber,hash) tuple
+        vm.prank(challenger);
+        dac.challenge(challengedBlockNumber, challengedHash);
+
+        // Challenge should have been created
+        (ChallengeStatus _status, address _challenger, uint256 _startBlock) =
+            dac.challenges(challengedBlockNumber, challengedHash);
+        assertEq(uint8(_status), uint8(ChallengeStatus.Active));
+        assertEq(_challenger, challenger);
+        assertEq(_startBlock, block.number);
+
+        // Challenge should have decreased the challenger's bond size
+        assertEq(dac.balances(challenger), 0);
+    }
+
+    function testChallengeFailBondTooLow() public {
+        uint256 requiredBond = dac.bondSize();
+        uint256 actualBond = requiredBond - 1;
+        dac.deposit{value: actualBond}();
+
+        vm.expectRevert(abi.encodeWithSelector(DataAvailabilityChallenge.BondTooLow.selector, actualBond, requiredBond));
+        dac.challenge(0, "some hash");
+    }
+
+    function testChallengeFailChallengeExists() public {
+        // Move to a block after the hash to challenge
+        vm.roll(2);
+
+        // First challenge succeeds
+        dac.deposit{value: dac.bondSize()}();
+        dac.challenge(0, "some hash");
+
+        // Second challenge of the same hash/blockNumber fails
+        dac.deposit{value: dac.bondSize()}();
+        vm.expectRevert(abi.encodeWithSelector(DataAvailabilityChallenge.ChallengeExists.selector));
+        dac.challenge(0, "some hash");
+
+        // Challenge succeed if the challenged block number is different
+        dac.deposit{value: dac.bondSize()}();
+        dac.challenge(1, "some hash");
+
+        // Challenge succeed if the challenged hash is different
+        dac.deposit{value: dac.bondSize()}();
+        dac.challenge(0, "some other hash");
+    }
+
+    function testChallengeFailBeforeChallengeWindow() public {
+        uint256 challengeBlock = 1;
+        bytes32 challengeHash = "some hash";
+
+        // Move to challenged block
+        vm.roll(challengeBlock);
+
+        // Challenge fails because the current block number must be after the challenged block
+        dac.deposit{value: dac.bondSize()}();
+        vm.expectRevert(abi.encodeWithSelector(DataAvailabilityChallenge.ChallengeWindowNotOpen.selector));
+        dac.challenge(challengeBlock, challengeHash);
+    }
+
+    function testChallengeFailAfterChallengeWindow() public {
+        uint256 challengeBlock = 1;
+        bytes32 challengeHash = "some hash";
+
+        // Move to block after the challenge window
+        vm.roll(challengeBlock + dac.challengeWindow() + 1);
+
+        // Challenge fails because the block number is after the challenge window
+        dac.deposit{value: dac.bondSize()}();
+        vm.expectRevert(abi.encodeWithSelector(DataAvailabilityChallenge.ChallengeWindowNotOpen.selector));
+        dac.challenge(challengeBlock, challengeHash);
+    }
+
+    function testResolveSuccess(bytes memory preImage, uint256 challengeBlock) public {
+        // Assume the block number is not close to the max uint256 value
+        vm.assume(challengeBlock < type(uint256).max - dac.challengeWindow() - dac.resolveWindow());
+        bytes32 challengeHash = keccak256(preImage);
+
+        // Move to block after challenged block
+        vm.roll(challengeBlock + 1);
+
+        // Challenge the hash
+        dac.deposit{value: dac.bondSize()}();
+        dac.challenge(challengeBlock, challengeHash);
+
+        // Resolve the challenge
+        dac.resolve(challengeBlock, preImage);
+
+        // Expect the challenge to be resolved
+        (ChallengeStatus _status, address _challenger, uint256 _startBlock) =
+            dac.challenges(challengeBlock, challengeHash);
+
+        assertEq(uint8(_status), uint8(ChallengeStatus.Resolved));
+        assertEq(_challenger, address(this));
+        assertEq(_startBlock, block.number);
+    }
+
+    function testResolveFailNonExistentChallenge() public {
+        bytes memory preImage = "some preimage";
+        uint256 challengeBlock = 1;
+
+        // Move to block after challenged block
+        vm.roll(challengeBlock + 1);
+
+        // Resolving a non-existent challenge fails
+        vm.expectRevert(abi.encodeWithSelector(DataAvailabilityChallenge.ChallengeNotActive.selector));
+        dac.resolve(challengeBlock, preImage);
+    }
+
+    function testResolveFailResolved() public {
+        bytes memory preImage = "some preimage";
+        bytes32 challengeHash = keccak256(preImage);
+        uint256 challengeBlock = 1;
+
+        // Move to block after challenged block
+        vm.roll(challengeBlock + 1);
+
+        // Challenge the hash
+        dac.deposit{value: dac.bondSize()}();
+        dac.challenge(challengeBlock, challengeHash);
+
+        // Resolve the challenge
+        dac.resolve(challengeBlock, preImage);
+
+        // Resolving an already resolved challenge fails
+        vm.expectRevert(abi.encodeWithSelector(DataAvailabilityChallenge.ChallengeNotActive.selector));
+        dac.resolve(challengeBlock, preImage);
+    }
+
+    function testResolveFailExpired() public {
+        bytes memory preImage = "some preimage";
+        bytes32 challengeHash = keccak256(preImage);
+        uint256 challengeBlock = 1;
+
+        // Move to block after challenged block
+        vm.roll(challengeBlock + 1);
+
+        // Challenge the hash
+        dac.deposit{value: dac.bondSize()}();
+        dac.challenge(challengeBlock, challengeHash);
+
+        // Move to a block after the resolve window
+        vm.roll(block.number + dac.resolveWindow() + 1);
+
+        // Expire the challenge
+        dac.expire(challengeBlock, challengeHash);
+
+        // Resolving an expired challenge fails
+        vm.expectRevert(abi.encodeWithSelector(DataAvailabilityChallenge.ChallengeNotActive.selector));
+        dac.resolve(challengeBlock, preImage);
+    }
+
+    function testResolveFailAfterResolveWindow() public {
+        bytes memory preImage = "some preimage";
+        bytes32 challengeHash = keccak256(preImage);
+        uint256 challengeBlock = 1;
+
+        // Move to block after challenged block
+        vm.roll(challengeBlock + 1);
+
+        // Challenge the hash
+        dac.deposit{value: dac.bondSize()}();
+        dac.challenge(challengeBlock, challengeHash);
+
+        // Move to block after resolve window
+        vm.roll(block.number + dac.resolveWindow() + 1);
+
+        // Resolve the challenge
+        vm.expectRevert(abi.encodeWithSelector(DataAvailabilityChallenge.ResolveWindowNotOpen.selector));
+        dac.resolve(challengeBlock, preImage);
+    }
+
+    function testExpireSuccess(bytes memory preImage, uint256 challengeBlock) public {
+        // Assume the block number is not close to the max uint256 value
+        vm.assume(challengeBlock < type(uint256).max - dac.challengeWindow() - dac.resolveWindow());
+        bytes32 challengeHash = keccak256(preImage);
+
+        // Move to block after challenged block
+        vm.roll(challengeBlock + 1);
+
+        // Challenge the hash
+        dac.deposit{value: dac.bondSize()}();
+        dac.challenge(challengeBlock, challengeHash);
+
+        // Move to a block after the resolve window
+        vm.roll(block.number + dac.resolveWindow() + 1);
+
+        // Expire the challenge
+        dac.expire(challengeBlock, challengeHash);
+    }
+
+    function testExpireFailNonExistentChallenge() public {
+        bytes memory preImage = "some preimage";
+        bytes32 challengeHash = keccak256(preImage);
+        uint256 challengeBlock = 1;
+
+        // Move to block after challenged block
+        vm.roll(challengeBlock + 1);
+
+        // Expiring a non-existent challenge fails
+        vm.expectRevert(abi.encodeWithSelector(DataAvailabilityChallenge.ChallengeNotActive.selector));
+        dac.expire(challengeBlock, challengeHash);
+    }
+
+    function testExpireFailResolvedChallenge() public {
+        bytes memory preImage = "some preimage";
+        bytes32 challengeHash = keccak256(preImage);
+        uint256 challengeBlock = 1;
+
+        // Move to block after challenged block
+        vm.roll(challengeBlock + 1);
+
+        // Challenge the hash
+        dac.deposit{value: dac.bondSize()}();
+        dac.challenge(challengeBlock, challengeHash);
+
+        // Resolve the challenge
+        dac.resolve(challengeBlock, preImage);
+
+        // Expiring a resolved challenge fails
+        vm.expectRevert(abi.encodeWithSelector(DataAvailabilityChallenge.ChallengeNotActive.selector));
+        dac.expire(challengeBlock, challengeHash);
+    }
+
+    function testExpireFailExpiredChallenge() public {
+        bytes memory preImage = "some preimage";
+        bytes32 challengeHash = keccak256(preImage);
+        uint256 challengeBlock = 1;
+
+        // Move to block after challenged block
+        vm.roll(challengeBlock + 1);
+
+        // Challenge the hash
+        dac.deposit{value: dac.bondSize()}();
+        dac.challenge(challengeBlock, challengeHash);
+
+        // Move to a block after the challenge window
+        vm.roll(block.number + dac.resolveWindow() + 1);
+
+        // Expire the challenge
+        dac.expire(challengeBlock, challengeHash);
+
+        // Expiring an expired challenge fails
+        vm.expectRevert(abi.encodeWithSelector(DataAvailabilityChallenge.ChallengeNotActive.selector));
+        dac.expire(challengeBlock, challengeHash);
+    }
+
+    function testExpireFailResolveWindowNotClosed() public {
+        bytes memory preImage = "some preimage";
+        bytes32 challengeHash = keccak256(preImage);
+        uint256 challengeBlock = 1;
+
+        // Move to block after challenged block
+        vm.roll(challengeBlock + 1);
+
+        // Challenge the hash
+        dac.deposit{value: dac.bondSize()}();
+        dac.challenge(challengeBlock, challengeHash);
+
+        vm.roll(block.number + dac.resolveWindow() - 1);
+
+        // Expiring the challenge before the resolve window closes fails
+        vm.expectRevert(abi.encodeWithSelector(DataAvailabilityChallenge.ResolveWindowNotClosed.selector));
+        dac.expire(challengeBlock, challengeHash);
+    }
+}
