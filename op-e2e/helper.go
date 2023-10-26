fdc2193e25791eaf06284f26bb6634c09a4365fc
Adrian Sutton <adrian@oplabs.co>
2023-10-27 06:55:24 +1000

    op-e2e: Disable cannon tests when using HTTP instead of WS
    Fault dispute tests take longer to execute when polling with HTTP so disable the longer running cannon tests on HTTP.
    Cannon itself is unaffected by the connection type and the challenger is tested with HTTP via the alphabet game tests.

--- op-e2e/helper.go
+++ op-e2e/helper.go
@@ -7,9 +7,18 @@ import (
 
 var enableParallelTesting bool = os.Getenv("OP_E2E_DISABLE_PARALLEL") != "true"
 
-func InitParallel(t *testing.T) {
+func InitParallel(t *testing.T, opts ...func(t *testing.T)) {
 	t.Helper()
 	if enableParallelTesting {
 		t.Parallel()
+	}
+	for _, opt := range opts {
+		opt(t)
+	}
+}
+
+func SkipIfHTTP(t *testing.T) {
+	if UseHTTP() {
+		t.Skip("Skipping test because HTTP connection is in use")
 	}
 }
