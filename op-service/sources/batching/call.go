194c739e10274014084f533911f8f5c270632ec0
Adrian Sutton <adrian@oplabs.co>
2023-10-30 14:22:25 +1000

    batching: Add more unit tests.

--- op-service/sources/batching/call.go
+++ op-service/sources/batching/call.go
@@ -66,6 +66,26 @@ func (c *ContractCall) Unpack(hex hexutil.Bytes) (*CallResult, error) {
 	return &CallResult{out: out}, nil
 }
 
+func toCallArg(msg ethereum.CallMsg) interface{} {
+	arg := map[string]interface{}{
+		"from": msg.From,
+		"to":   msg.To,
+	}
+	if len(msg.Data) > 0 {
+		arg["data"] = hexutil.Bytes(msg.Data)
+	}
+	if msg.Value != nil {
+		arg["value"] = (*hexutil.Big)(msg.Value)
+	}
+	if msg.Gas != 0 {
+		arg["gas"] = hexutil.Uint64(msg.Gas)
+	}
+	if msg.GasPrice != nil {
+		arg["gasPrice"] = (*hexutil.Big)(msg.GasPrice)
+	}
+	return arg
+}
+
 type CallResult struct {
 	out []interface{}
 }
--- op-service/sources/batching/multicall.go
+++ op-service/sources/batching/multicall.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"io"
 
-	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/rpc"
 )
@@ -79,24 +78,4 @@ func (m *MultiCaller) CallLatest(ctx context.Context, calls ...*ContractCall) ([
 		callResults[i] = out
 	}
 	return callResults, nil
-}
-
-func toCallArg(msg ethereum.CallMsg) interface{} {
-	arg := map[string]interface{}{
-		"from": msg.From,
-		"to":   msg.To,
-	}
-	if len(msg.Data) > 0 {
-		arg["data"] = hexutil.Bytes(msg.Data)
-	}
-	if msg.Value != nil {
-		arg["value"] = (*hexutil.Big)(msg.Value)
-	}
-	if msg.Gas != 0 {
-		arg["gas"] = hexutil.Uint64(msg.Gas)
-	}
-	if msg.GasPrice != nil {
-		arg["gasPrice"] = (*hexutil.Big)(msg.GasPrice)
-	}
-	return arg
 }
