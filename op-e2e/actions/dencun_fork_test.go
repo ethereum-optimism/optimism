480686273e9a5cb816ef10f600af583466964598
EvanJRichard <evan@oplabs.co>
2023-11-01 16:27:23 -0400

    Start to tweak things in dencun_fork_test: mainly replacing "shanghai" with "Cancun"

--- op-e2e/actions/dencun_fork_test.go
+++ op-e2e/actions/dencun_fork_test.go
@@ -10,18 +10,18 @@ import (
 	"github.com/ethereum-optimism/optimism/op-node/testlog"
 )
 
-func TestShapellaL1Fork(gt *testing.T) {
+func TestDencunL1Fork(gt *testing.T) {
 	t := NewDefaultTesting(gt)
 	dp := e2eutils.MakeDeployParams(t, defaultRollupTestParams)
 
 	sd := e2eutils.Setup(t, dp, defaultAlloc)
 	activation := sd.L1Cfg.Timestamp + 24
-	sd.L1Cfg.Config.ShanghaiTime = &activation
+	sd.L1Cfg.Config.CancunTime = &activation
 	log := testlog.Logger(t, log.LvlDebug)
 
 	_, _, miner, sequencer, _, verifier, _, batcher := setupReorgTestActors(t, dp, sd, log)
 
-	require.False(t, sd.L1Cfg.Config.IsShanghai(miner.l1Chain.CurrentBlock().Time()), "not active yet")
+	require.False(t, sd.L1Cfg.Config.IsCancun(miner.l1Chain.CurrentBlock().Time()), "not active yet")
 
 	// start op-nodes
 	sequencer.ActL2PipelineFull(t)
@@ -32,11 +32,11 @@ func TestShapellaL1Fork(gt *testing.T) {
 	miner.ActEmptyBlock(t)
 	miner.ActEmptyBlock(t)
 
-	// verify Shanghai is active
+	// verify Cancun is active
 	l1Head := miner.l1Chain.CurrentBlock()
-	require.True(t, sd.L1Cfg.Config.IsShanghai(l1Head.Time()))
+	require.True(t, sd.L1Cfg.Config.IsCancun(l1Head.Time()))
 
-	// build L2 chain up to and including L2 blocks referencing shanghai L1 blocks
+	// build L2 chain up to and including L2 blocks referencing Cancun L1 blocks
 	sequencer.ActL1HeadSignal(t)
 	sequencer.ActBuildToL1Head(t)
 	miner.ActL1StartBlock(12)(t)
@@ -47,7 +47,7 @@ func TestShapellaL1Fork(gt *testing.T) {
 	// sync verifier
 	verifier.ActL1HeadSignal(t)
 	verifier.ActL2PipelineFull(t)
-	// verify verifier accepted shanghai L1 inputs
-	require.Equal(t, l1Head.Hash(), verifier.SyncStatus().SafeL2.L1Origin.Hash, "verifier synced L1 chain that includes shanghai headers")
+	// verify verifier accepted Cancun L1 inputs
+	require.Equal(t, l1Head.Hash(), verifier.SyncStatus().SafeL2.L1Origin.Hash, "verifier synced L1 chain that includes Cancun headers")
 	require.Equal(t, sequencer.SyncStatus().UnsafeL2, verifier.SyncStatus().UnsafeL2, "verifier and sequencer agree")
 }
