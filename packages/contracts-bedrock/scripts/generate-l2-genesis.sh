9be8666f487b6b3220d5574f3186cce0585e8a57
Mark Tyneway <mark.tyneway@gmail.com>
2023-11-13 17:16:39 +0300

    contracts-bedrock: cleanup scripts

--- packages/contracts-bedrock/scripts/generate-l2-genesis.sh
+++ packages/contracts-bedrock/scripts/generate-l2-genesis.sh
@@ -18,21 +18,17 @@ OUTFILE_L2="$TESTDATA_DIR/genesis.json"
 OUTFILE_ROLLUP="$TESTDATA_DIR/rollup.json"
 OUTFILE_ALLOC="$TESTDATA_DIR/alloc.json"
 
-mkdir -p "$TESTDATA_DIR"
-
 if [ ! -f "$DEPLOY_ARTIFACT" ]; then
   forge script $CONTRACTS_DIR/scripts/Deploy.s.sol:Deploy 2>&1 /dev/null
 fi
 
-# TODO:
-# if the testdata dir doesn't exist, run the command
-# add a clean command to the package.json
+if [ ! -d "$TESTDATA_DIR" ]; then
+  mkdir -p "$TESTDATA_DIR"
 
-go run $OP_NODE genesis l2 \
-  --deploy-config "$CONTRACTS_DIR/deploy-config/hardhat.json" \
-  --l1-deployments "$DEPLOY_ARTIFACT" \
-  --l1-starting-block "$L1_STARTING_BLOCK_PATH" \
-  --outfile.l2 "$OUTFILE_L2" \
-  --outfile.rollup "$OUTFILE_ROLLUP" >/dev/null 2>&1
-
-jq .alloc < "$OUTFILE_L2" > "$OUTFILE_ALLOC"
+  go run $OP_NODE genesis l2 \
+    --deploy-config "$CONTRACTS_DIR/deploy-config/hardhat.json" \
+    --l1-deployments "$DEPLOY_ARTIFACT" \
+    --l1-starting-block "$L1_STARTING_BLOCK_PATH" \
+    --outfile.l2 "$OUTFILE_L2" \
+    --outfile.rollup "$OUTFILE_ROLLUP" >/dev/null 2>&1
+fi
