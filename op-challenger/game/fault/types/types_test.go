22b0c43b50a9b970fc556fb95fabffabd6628cf2
Adrian Sutton <adrian@oplabs.co>
2023-11-21 08:48:31 +1000

    op-challenger: Fix tests.

--- op-challenger/game/fault/types/types_test.go
+++ op-challenger/game/fault/types/types_test.go
@@ -12,7 +12,7 @@ func TestNewPreimageOracleData(t *testing.T) {
 	t.Run("LocalData", func(t *testing.T) {
 		data := NewPreimageOracleData(common.Hash{0x01}, []byte{1, 2, 3}, []byte{4, 5, 6}, 7)
 		require.True(t, data.IsLocal)
-		require.Equal(t, uint64(1), data.LocalContext)
+		require.Equal(t, common.Hash{0x01}, data.LocalContext)
 		require.Equal(t, []byte{1, 2, 3}, data.OracleKey)
 		require.Equal(t, []byte{4, 5, 6}, data.OracleData)
 		require.Equal(t, uint32(7), data.OracleOffset)
@@ -21,7 +21,7 @@ func TestNewPreimageOracleData(t *testing.T) {
 	t.Run("GlobalData", func(t *testing.T) {
 		data := NewPreimageOracleData(common.Hash{0x01}, []byte{0, 2, 3}, []byte{4, 5, 6}, 7)
 		require.False(t, data.IsLocal)
-		require.Equal(t, uint64(1), data.LocalContext)
+		require.Equal(t, common.Hash{0x01}, data.LocalContext)
 		require.Equal(t, []byte{0, 2, 3}, data.OracleKey)
 		require.Equal(t, []byte{4, 5, 6}, data.OracleData)
 		require.Equal(t, uint32(7), data.OracleOffset)
