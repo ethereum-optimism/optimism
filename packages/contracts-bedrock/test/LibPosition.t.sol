83b192e88e124d76dd30579872417dd6c2e61172
Mark Tyneway <mark.tyneway@gmail.com>
2023-11-21 02:57:25 -0800

    ctb: silence compiler warning
    Make a function `pure` rather than `view` to silence a compiler warning.
    This likely appeared after updating the version of `forge-std`.
    The function that was updated wrapped the `bound` function so its likely
    that the `bound` function was updated to be `pure` when it used to be
    `view`.

--- packages/contracts-bedrock/test/LibPosition.t.sol
+++ packages/contracts-bedrock/test/LibPosition.t.sol
@@ -12,7 +12,7 @@ contract LibPosition_Test is Test {
     ///      will likely be much lower.
     uint8 internal constant MAX_DEPTH = 63;
 
-    function boundIndexAtDepth(uint8 _depth, uint64 _indexAtDepth) internal view returns (uint64) {
+    function boundIndexAtDepth(uint8 _depth, uint64 _indexAtDepth) internal pure returns (uint64) {
         // Index at depth bound: [0, 2 ** _depth-1]
         if (_depth > 0) {
             return uint64(bound(_indexAtDepth, 0, 2 ** (_depth - 1)));
