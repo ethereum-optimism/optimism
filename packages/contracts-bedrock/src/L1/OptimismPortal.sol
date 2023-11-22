7acc2d238db09f94c183d14bf34f6954c7ce5b57
Maurelian <maurelian@protonmail.ch>
2023-11-22 01:48:32 -0500

    gas snapshot and semver lock

--- packages/contracts-bedrock/src/L1/OptimismPortal.sol
+++ packages/contracts-bedrock/src/L1/OptimismPortal.sol
@@ -109,8 +109,8 @@ contract OptimismPortal is Initializable, ResourceMetering, ISemver {
     }
 
     /// @notice Semantic version.
-    /// @custom:semver 1.11.0
-    string public constant version = "1.11.0";
+    /// @custom:semver 2.0.0
+    string public constant version = "2.0.0";
 
     /// @notice Constructs the OptimismPortal contract.
     /// @param _l2Oracle Address of the L2OutputOracle contract.
