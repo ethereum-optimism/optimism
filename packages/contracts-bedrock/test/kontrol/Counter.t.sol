61625343a0480f6c8f2ceae8ddc2394fe378eee1
Juan C <juanconrod@protonmail.com>
2023-11-24 16:24:47 +0100

    Move `Counter.sol` to Kontrol folder

--- packages/contracts-bedrock/test/kontrol/Counter.t.sol
+++ packages/contracts-bedrock/test/kontrol/Counter.t.sol
@@ -1,8 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-/* import {Test} from "forge-std/Test.sol"; */
-import {Counter} from "src/L1/Counter.sol";
+import {Counter} from "./Counter.sol";
 
 contract CounterTest {
 
