c8fc7252f17a78d3104c2577ebac82839d6a8940
Hamdi Allam <hamdi.allam97@gmail.com>
2023-11-26 17:43:39 -0500

    ci + feedback

--- packages/contracts-bedrock/src/interop/L2CrossDomainMessenger.sol
+++ packages/contracts-bedrock/src/interop/L2CrossDomainMessenger.sol
@@ -1,58 +1,87 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.15;
 
-import { L2CrossDomainMessenger } from "src/L2/L2CrossDomainMessenger.sol";
-
+import { L2CrossDomainMessenger as LegacyL2CrossDomainMessenger } from "src/L2/L2CrossDomainMessenger.sol";
 import { Constants } from "src/libraries/Constants.sol";
 import { Encoding } from "src/libraries/Encoding.sol";
 import { Predeploys } from "src/libraries/Predeploys.sol";
 import { SafeCall } from "src/libraries/SafeCall.sol";
 
-contract InteropEnabledL2CrossDomainMessenger {
-    // new message versionn
-    uint16 public constant INTEROP_MESSAGE_VERSION = 2;
+/// @title L2CrossDomainMessenger
+/// @notice L2CrossDomainMessenger is an extended version of the existing predeploy that supports
+///         interopability between many chains (L2-L2). Since we are keeping the existing messaging
+///         contracts the same while iterating on the L2-L2 implementation, changes to the interface
+///         and messaging format that would reside in the CrossDomainMessenger are scoped internally
+///         in this contract. The L2-L1 flow remains unchanged.
+contract L2CrossDomainMessenger {
+    // CrossDomainMessenger: some copied internals & updated dispatch/message spec
 
-    bytes32 public constant ETH_MAINNET_ID = bytes32(hex"01");
-    bytes32 public CHAIN_ID;
+    /// @notice Latest message version identifier (interop-enabled)
+    uint16 public constant MESSAGE_VERSION = 2;
 
+    /// @notice Chain identifier of the network
+    bytes32 public immutable CHAIN_ID;
+
+    /// @param _chain_id Chain identifier of this network
     constructor(bytes32 _chain_id) {
         CHAIN_ID = _chain_id;
     }
 
     // TODO: Events
 
-    /**
-     * CrossDomainMessenger: some copied logic & updated/extended dispatch interface
-     */
-
+    /// @notice Nonce for the next message to be sent
     uint240 internal msgNonce;
+
+    /// @notice Address of the sender of the currently executing message
     address internal xDomainMsgSender;
+
+    /// @notice Mapping of succesfully delivered messages
     mapping(bytes32 => bool) public successfulMessages;
+
+    /// @notice Mapping of delivered messages in a failed state
     mapping(bytes32 => bool) public failedMessages;
 
-    function messageNonce() public view returns (uint256) {
-        return Encoding.encodeVersionedNonce(msgNonce, INTEROP_MESSAGE_VERSION);
-    }
-
-    function sendMessage(bytes32 _destination, address _target, bytes calldata _message, uint32 _minGasLimit) external payable {
+    /// @notice Sends a message to the specified destination chain. If the destination chain is ETH Mainnet (0x1), then
+    /// this
+    ///         message follows the legacy message flow and formates by forwarding to the existing
+    /// L2CrossDomainMessenger
+    ///         predeploy. When this implementation is ready as a replacement, L2ToL2 and L2ToL1 is natively handled by
+    /// the
+    ///         respective internal message passer contracts.
+    function sendMessage(
+        bytes32 _destination,
+        address _target,
+        bytes calldata _message,
+        uint32 _minGasLimit
+    )
+        external
+        payable
+    {
+        // L2->L1 Support: Required as we are leaving the CrossDomainMessenger untouched; All L2->L1 messages will still
+        // be using
+        // the old message format (Version 1), hence forwarding to the "legacy" contract.
+        bytes32 ETH_MAINNET_ID = bytes32(hex"01");
         if (_destination == ETH_MAINNET_ID) {
-            // Required as we are leaving the CrossDomainMessenger untouched. L2->L2 messages will still be using
-            // the old message format (Version 1).
-            L2CrossDomainMessenger(Predeploys.L2_CROSS_DOMAIN_MESSENGER).sendMessage(_target, _message, _minGasLimit);
+            LegacyL2CrossDomainMessenger(Predeploys.L2_CROSS_DOMAIN_MESSENGER).sendMessage(
+                _target, _message, _minGasLimit
+            );
             return;
         }
 
-        require(_destination != CHAIN_ID, "message cant be sent to self");
+        require(_destination != CHAIN_ID, "L2CrossDomainMessenger: message cant be sent to self");
 
+        uint256 nonce = Encoding.encodeVersionedNonce(msgNonce, MESSAGE_VERSION);
         bytes memory data = abi.encodeWithSelector(
-            this.relayMessage.selector, messageNonce(), CHAIN_ID, msg.sender, _target, msg.value, _minGasLimit, _message
+            this.relayMessage.selector, nonce, CHAIN_ID, msg.sender, _target, msg.value, _minGasLimit, _message
         );
 
         // (1) Send to the CrossL2Inbox.
-        // ***Note*** With an updated CrossDomainMessenger message format, this contract can replace the L2CDM predeploy
-        // and switch between L2ToL1MessagePasser/CrossL2Inbox based on `_destination`.
+        // With an updated CrossDomainMessenger message format, this contract will replace existing
+        // L2CrossDomainmMessenger
+        // predeploy and switch between L2ToL1MessagePasser/CrossL2Inbox based on the provided `_destination`.
         //
-        // i.e: CrossL2Inbox.sendMessage(source, destination, _minGasLimit, msg.value, data) // ensure _minGaslimit covers the base
+        // i.e: CrossL2Inbox.sendMessage(source, destination, _minGasLimit, msg.value, data) // ensure _minGaslimit
+        // covers the base
 
         // (2) Emit Events
 
@@ -61,6 +90,9 @@ contract InteropEnabledL2CrossDomainMessenger {
         }
     }
 
+    /// @notice Relays a message that was sent from a different chain. Since L1-L2 messages utilize the V1 message
+    /// format, this
+    ///         entrypoint only supports relaying L2-L2 messages.
     function relayMessage(
         uint256 _nonce,
         bytes32 _source,
@@ -69,12 +101,16 @@ contract InteropEnabledL2CrossDomainMessenger {
         uint256 _value,
         uint256 _minGasLimit,
         bytes calldata _message
-    ) external payable {
+    )
+        external
+        payable
+    {
         (, uint16 version) = Encoding.decodeVersionedNonce(_nonce);
-        require(version == INTEROP_MESSAGE_VERSION, "message is not of the right version");
+        require(version == MESSAGE_VERSION, "L2CrossDomainMessenger: incorrect message version");
 
-        bytes32 messageHash = hashCrossDomainMessageV2(_nonce, _source, CHAIN_ID, _sender, _target, _value, _minGasLimit, _message);
-        require(successfulMessages[messageHash] == false, "message already processed");
+        bytes32 messageHash =
+            hashCrossDomainMessageV2(_nonce, _source, CHAIN_ID, _sender, _target, _value, _minGasLimit, _message);
+        require(successfulMessages[messageHash] == false, "L2CrossDomainMessenger: message already processed");
 
         // (1) Allow for replay
         if (_sender == address(this)) {
@@ -82,10 +118,10 @@ contract InteropEnabledL2CrossDomainMessenger {
             assert(failedMessages[messageHash] == false);
         } else {
             require(msg.value == 0, "cannot replay with more funds");
-            require(failedMessages[messageHash], "message cannot be replayed");
+            require(failedMessages[messageHash], "L2CrossDomainMessengeR: message cannot be replayed");
         }
 
-        // .. check for min gas, unsafe target, etc.
+        // **CrossDomainMessenger Checks**. min gas, unsafe target, etc.
 
         // (2) Relay Message
         // -- Ignoring the reserved gas subtracted from gasleft() & xDomainMsgSender update
@@ -101,10 +137,9 @@ contract InteropEnabledL2CrossDomainMessenger {
         }
     }
 
-    
-    /**
-     * Encoding.sol: update for this new message version
-     */
+    // ------------------------------------------------------------------------------------------------------------
+
+    // Encoding.sol: v2 support
 
     function encodeCrossDomainMessageV2(
         uint256 _nonce,
@@ -115,7 +150,11 @@ contract InteropEnabledL2CrossDomainMessenger {
         uint256 _value,
         uint256 _gasLimit,
         bytes memory _data
-    ) internal pure returns (bytes memory) {
+    )
+        internal
+        pure
+        returns (bytes memory)
+    {
         return abi.encodeWithSignature(
             "relayMessage(uint256,bytes32,bytes32,address,address,uint256,uint256,bytes)",
             _nonce,
@@ -129,9 +168,8 @@ contract InteropEnabledL2CrossDomainMessenger {
         );
     }
 
-    /**
-     * Hashing.sol: update for this new message version
-     */
+    // Hashing.sol: v2 support
+
     function hashCrossDomainMessageV2(
         uint256 _nonce,
         bytes32 _source,
@@ -141,7 +179,13 @@ contract InteropEnabledL2CrossDomainMessenger {
         uint256 _value,
         uint256 _gasLimit,
         bytes memory _data
-    ) internal pure returns (bytes32) {
-        return keccak256(encodeCrossDomainMessageV2(_nonce, _source, _destination, _sender, _target, _value, _gasLimit, _data));
+    )
+        internal
+        pure
+        returns (bytes32)
+    {
+        return keccak256(
+            encodeCrossDomainMessageV2(_nonce, _source, _destination, _sender, _target, _value, _gasLimit, _data)
+        );
     }
 }
