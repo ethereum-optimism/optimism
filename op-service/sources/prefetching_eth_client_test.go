72f7f8f23de9aad8a156f3081e573f6aaebc429a
EvanJRichard <evan@oplabs.co>
2023-11-27 11:01:20 -0500

    redo test skeletons.

--- op-service/sources/prefetching_eth_client_test.go
+++ op-service/sources/prefetching_eth_client_test.go
@@ -1,1 +1,215 @@
 package sources
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	"github.com/ethereum-optimism/optimism/op-service/client"
+	"github.com/ethereum-optimism/optimism/op-service/eth"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
+)
+
+// MockEthClient is a mock of EthClient used for testing.
+type MockEthClient struct {
+	mock.Mock
+}
+
+func (m *MockEthClient) InfoByNumber(ctx context.Context, number uint64) (eth.BlockInfo, error) {
+	args := m.Called(ctx, number)
+	return args.Get(0).(eth.BlockInfo), args.Error(1)
+}
+
+// MockRPC is a mock of the RPC client, similar to mockRPC but tailored for PrefetchingEthClient.
+// Add any additional methods that might be required for testing.
+
+// ... [MockRPC implementation] ...
+
+var _ client.RPC = (*MockRPC)(nil)
+
+// TestPrefetchingEthClient_InfoByNumber tests the basic functionality of InfoByNumber.
+func TestPrefetchingEthClient_InfoByNumber(t *testing.T) {
+	ctx := context.Background()
+	mockRPC := new(MockRPC)
+	mockEthClient := new(MockEthClient)
+	prefetchedBlockNumber := uint64(1235)  // This is the next block after the requested one
+	prefetchedBlockInfo := eth.BlockInfo{} // Replace with a suitable mock response
+
+	// Mock the responses
+	mockEthClient.On("InfoByNumber", ctx, prefetchedBlockNumber).Return(prefetchedBlockInfo, nil)
+
+	// Initialize PrefetchingEthClient
+	client, err := NewPrefetchingEthClient(mockRPC, nil, nil, testEthClientConfig)
+	require.NoError(t, err)
+
+	// Test the call to InfoByNumber
+	blockNumber := uint64(1234)
+	blockInfo, err := client.InfoByNumber(ctx, blockNumber)
+	require.NoError(t, err)
+
+	// Assertions to verify the correct block info is returned and prefetching logic is invoked
+	// ...
+
+	// Verify that the internal EthClient was called for the prefetched block
+	mockEthClient.AssertCalled(t, "InfoByNumber", ctx, prefetchedBlockNumber)
+
+	// ... Additional assertions and verifications as required ...
+}
+
+// TestPrefetchingEthClient_ConsecutiveCalls tests consecutive calls to InfoByNumber.
+func TestPrefetchingEthClient_ConsecutiveCalls(t *testing.T) {
+	ctx := context.Background()
+	mockRPC := new(MockRPC)
+	mockEthClient := new(MockEthClient)
+
+	// Initialize PrefetchingEthClient
+	client, err := NewPrefetchingEthClient(mockRPC, nil, nil, testEthClientConfig)
+	require.NoError(t, err)
+
+	firstBlockNumber := uint64(1234)
+	secondBlockNumber := uint64(1235)
+
+	firstBlockInfo := eth.BlockInfo{}  // Mock response for the first block
+	secondBlockInfo := eth.BlockInfo{} // Mock response for the second block
+
+	// Mock the responses for the first and second blocks
+	mockEthClient.On("InfoByNumber", ctx, firstBlockNumber).Return(firstBlockInfo, nil).Once()
+	mockEthClient.On("InfoByNumber", ctx, secondBlockNumber).Return(secondBlockInfo, nil).Once()
+
+	// First call to InfoByNumber
+	info1, err1 := client.InfoByNumber(ctx, firstBlockNumber)
+	require.NoError(t, err1)
+	require.Equal(t, firstBlockInfo, info1)
+
+	// Second call to InfoByNumber, should use prefetched data
+	info2, err2 := client.InfoByNumber(ctx, secondBlockNumber)
+	require.NoError(t, err2)
+	require.Equal(t, secondBlockInfo, info2)
+
+	// Ensure the mock EthClient was called only for the first block
+	mockEthClient.AssertNumberOfCalls(t, "InfoByNumber", 1)
+	mockEthClient.AssertCalled(t, "InfoByNumber", ctx, firstBlockNumber)
+}
+
+// TestPrefetchingEthClient_CachingBehavior tests the caching behavior of prefetched blocks.
+func TestPrefetchingEthClient_CachingBehavior(t *testing.T) {
+	ctx := context.Background()
+	mockRPC := new(MockRPC)
+	mockEthClient := new(MockEthClient)
+
+	// Initialize PrefetchingEthClient
+	client, err := NewPrefetchingEthClient(mockRPC, nil, nil, testEthClientConfig)
+	require.NoError(t, err)
+
+	blockNumber := uint64(1234)
+	prefetchedBlockNumber := blockNumber + 1
+	blockInfo := eth.BlockInfo{} // Mock response for the block
+
+	// Mock the response for the block
+	mockEthClient.On("InfoByNumber", ctx, blockNumber).Return(blockInfo, nil).Once()
+
+	// Call to InfoByNumber
+	_, err = client.InfoByNumber(ctx, blockNumber)
+	require.NoError(t, err)
+
+	// Check if the next block is prefetched and cached
+	// This might involve inspecting the internal state of PrefetchingEthClient, depending on implementation
+	// ...
+
+	// Ensure the mock EthClient was called for the block but not for the prefetched one
+	mockEthClient.AssertCalled(t, "InfoByNumber", ctx, blockNumber)
+	mockEthClient.AssertNotCalled(t, "InfoByNumber", ctx, prefetchedBlockNumber)
+}
+
+// TestPrefetchingEthClient_PrefetchRange tests the range of blocks prefetched.
+func TestPrefetchingEthClient_PrefetchRange(t *testing.T) {
+	ctx := context.Background()
+	mockRPC := new(MockRPC)
+	mockEthClient := new(MockEthClient)
+
+	// Initialize PrefetchingEthClient with a specific prefetch range
+	prefetchRange := 5 // Example range for prefetching
+	client, err := NewPrefetchingEthClient(mockRPC, nil, nil, testEthClientConfig)
+	require.NoError(t, err)
+
+	blockNumber := uint64(1234)
+	blockInfo := eth.BlockInfo{} // Mock response for the block
+
+	// Mock the response for the block
+	mockEthClient.On("InfoByNumber", ctx, blockNumber).Return(blockInfo, nil)
+
+	// Call to InfoByNumber
+	_, err = client.InfoByNumber(ctx, blockNumber)
+	require.NoError(t, err)
+
+	// Ensure the mock EthClient was called for the specified range of blocks
+	for i := uint64(0); i < prefetchRange; i++ {
+		mockEthClient.AssertCalled(t, "InfoByNumber", ctx, blockNumber+i)
+	}
+}
+
+// TestPrefetchingEthClient_NewPrefetchingEthClient tests the constructor.
+func TestPrefetchingEthClient_NewPrefetchingEthClient(t *testing.T) {
+	mockRPC := new(MockRPC)
+
+	// Attempt to create a new PrefetchingEthClient with valid parameters
+	client, err := NewPrefetchingEthClient(mockRPC, nil, nil, testEthClientConfig)
+	require.NoError(t, err)
+	require.NotNil(t, client)
+
+	// Test cases for error scenarios can also be included here
+	// For example, passing invalid parameters and expecting an error
+}
+
+// TestPrefetchingEthClient_ErrorHandling tests how errors are handled.
+func TestPrefetchingEthClient_ErrorHandling(t *testing.T) {
+	ctx := context.Background()
+	mockRPC := new(MockRPC)
+	mockEthClient := new(MockEthClient)
+
+	// Initialize PrefetchingEthClient
+	client, err := NewPrefetchingEthClient(mockRPC, nil, nil, testEthClientConfig)
+	require.NoError(t, err)
+
+	blockNumber := uint64(1234)
+	expectedError := errors.New("mock error")
+
+	// Mock the EthClient to return an error
+	mockEthClient.On("InfoByNumber", ctx, blockNumber).Return(eth.BlockInfo{}, expectedError)
+
+	// Call to InfoByNumber should propagate the error
+	_, err = client.InfoByNumber(ctx, blockNumber)
+	require.Error(t, err)
+	require.Equal(t, expectedError, err)
+
+	// Additional tests can be added to simulate different error scenarios
+}
+
+// TestPrefetchingEthClient_StateInspection (Optional) - Test state inspection functionality.
+func TestPrefetchingEthClient_StateInspection(t *testing.T) {
+	ctx := context.Background()
+	mockRPC := new(MockRPC)
+	mockEthClient := new(MockEthClient)
+
+	// Initialize PrefetchingEthClient
+	client, err := NewPrefetchingEthClient(mockRPC, nil, nil, testEthClientConfig)
+	require.NoError(t, err)
+
+	blockNumber := uint64(1234)
+	blockInfo := eth.BlockInfo{} // Mock response for the block
+
+	// Mock the response for the block
+	mockEthClient.On("InfoByNumber", ctx, blockNumber).Return(blockInfo, nil)
+
+	// Call to InfoByNumber
+	_, err = client.InfoByNumber(ctx, blockNumber)
+	require.NoError(t, err)
+
+	// Inspect the state of prefetching
+	// This will depend on the method you add to PrefetchingEthClient for state inspection
+	// ...
+
+	// Assertions to verify the state of prefetching
+	// ...
+}
