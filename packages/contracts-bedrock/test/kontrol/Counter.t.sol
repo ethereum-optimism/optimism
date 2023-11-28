64a3a4a4aa7c4b57d532fe78fbbddc7b9c7c3295
Juan C <juanconrod@protonmail.com>
2023-11-28 14:04:13 +0100

    Format `Counter.t.sol` properly

--- packages/contracts-bedrock/test/kontrol/Counter.t.sol
+++ packages/contracts-bedrock/test/kontrol/Counter.t.sol
@@ -1,12 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Counter} from "./Counter.sol";
+import { Counter } from "./Counter.sol";
 
 contract CounterTest {
-
     Counter counter;
-
 
     function setUp() public {
         counter = new Counter();
