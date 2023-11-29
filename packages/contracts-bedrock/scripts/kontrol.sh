634c0515539161e47206c275824866d0382e771c
F-WRunTime <Freeman.Wenzl@runtimeverification.com>
2023-11-29 13:42:31 -0700

    Use Kontrol image for base image

--- packages/contracts-bedrock/scripts/kontrol.sh
+++ packages/contracts-bedrock/scripts/kontrol.sh
@@ -1,18 +1,4 @@
 #!/usr/bin/env bash
 set -exuo pipefail
 # Setup Kontrol 
-export KONTROL_VERSION=$(cat .kontrolrc)
-docker run --name optimism-ci \
-        --rm \
-        -v $(pwd)/kout/proofs:/home/user/workspace/packages/contracts-bedrock/kout/proofs/ \
-        --interactive \
-        --tty \
-        --detach \
-        --user root \
-        --workdir /home/user/workspace \
-        runtimeverificationinc/kontrol:ubuntu-jammy-${KONTROL_VERSION}
-# Copy in current test envionrment
-docker cp . optimism-ci:/home/user/workspace
-docker exec optimism-ci chown -R user:user /home/user
-# Run Kontrol Tests
-docker exec -u user optimism-ci bash -c 'cd packages/contracts-bedrock && ./test/kontrol/kontrol/run-kontrol.sh'
+cd packages/contracts-bedrock && ./test/kontrol/kontrol/run-kontrol.sh'
