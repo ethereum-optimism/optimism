aa0e4a25e0098e8a890e0a9f11ce8ceccf849088
F-WRunTime <Freeman.Wenzl@runtimeverification.com>
2023-11-29 13:49:06 -0700

    Bad end quote in script

--- packages/contracts-bedrock/scripts/kontrol.sh
+++ packages/contracts-bedrock/scripts/kontrol.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -exuo pipefail
 # Setup Kontrol 
-cd packages/contracts-bedrock && ./test/kontrol/kontrol/run-kontrol.sh'
+cd packages/contracts-bedrock && ./test/kontrol/kontrol/run-kontrol.sh
