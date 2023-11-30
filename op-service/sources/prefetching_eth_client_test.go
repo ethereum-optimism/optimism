d80570a3fa26b2ff537efa89174c2d663bd0dda0
EvanJRichard <evan@oplabs.co>
2023-11-30 15:20:36 -0500

    Move towards a single very simple test.

--- op-service/sources/prefetching_eth_client_test.go
+++ op-service/sources/prefetching_eth_client_test.go
@@ -5,9 +5,7 @@ import (
 	"strconv"
 	"testing"
 
-	"github.com/ethereum-optimism/optimism/op-service/eth"
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 )
 
@@ -25,114 +23,120 @@ var testCases = []struct {
 		prefetchingRanges: []uint64{0, 1, 5},
 		testFunc: func(t *testing.T, ctx context.Context, client *PrefetchingEthClient, mockRPC *mockRPC) error {
 			_, rhdr := randHeader()
-			expectedInfo, _ := rhdr.Info(true, false)
-			mockRPC.On("InfoByNumber", ctx, rhdr.Number).Return(expectedInfo, nil).Once()
-			info, err := client.InfoByNumber(ctx, uint64(rhdr.Number))
+
+			// Mock the call to CallContext for 'eth_getBlockByNumber', expecting it to be called at least once
+			mockRPC.On("CallContext", mock.Anything, mock.Anything, "eth_getBlockByNumber", mock.Anything).Return([]error{nil}).Maybe()
+
+			// Call the method which is expected to internally call 'eth_getBlockByNumber'
+			_, err := client.InfoByNumber(ctx, uint64(rhdr.Number))
 			require.NoError(t, err)
-			require.Equal(t, expectedInfo, info)
+
+			// Assert that 'eth_getBlockByNumber' was called at least once
+			mockRPC.AssertCalled(t, "CallContext", mock.Anything, mock.Anything, "eth_getBlockByNumber", mock.Anything)
+
 			return nil
 		},
 	},
-	{
-		name:              "InfoByHash",
-		prefetchingRanges: []uint64{0, 1, 5},
-		testFunc: func(t *testing.T, ctx context.Context, client *PrefetchingEthClient, mockRPC *mockRPC) error {
-			hash := randHash()
-			_, rhdr := randHeader()
-			rhdr.Hash = hash
-			expectedInfo, _ := rhdr.Info(true, false)
-			mockRPC.On("InfoByHash", ctx, hash).Return(expectedInfo, nil).Once()
-			info, err := client.InfoByHash(ctx, hash)
-			require.NoError(t, err)
-			require.Equal(t, expectedInfo, info)
-			return nil
-		},
-	},
-	{
-		name:              "InfoByLabel",
-		prefetchingRanges: []uint64{0, 1, 5},
-		testFunc: func(t *testing.T, ctx context.Context, client *PrefetchingEthClient, mockRPC *mockRPC) error {
-			label := eth.BlockLabel(eth.Unsafe)
-			_, rhdr := randHeader()
-			expectedInfo, _ := rhdr.Info(true, false)
-			mockRPC.On("InfoByLabel", ctx, label).Return(expectedInfo, nil).Once()
-			info, err := client.InfoByLabel(ctx, label)
-			require.NoError(t, err)
-			require.Equal(t, expectedInfo, info)
-			return nil
-		},
-	},
-	{
-		name:              "InfoAndTxsByHash",
-		prefetchingRanges: []uint64{0, 1, 5},
-		testFunc: func(t *testing.T, ctx context.Context, client *PrefetchingEthClient, mockRPC *mockRPC) error {
-			hash := common.Hash{}
-			block := &types.Block{}
-			blockInfo := eth.BlockToInfo(block)
-			txs := types.Transactions{}
-			mockRPC.On("InfoAndTxsByHash", ctx, hash).Return(blockInfo, txs, nil).Once()
-			_, _, err := client.InfoAndTxsByHash(ctx, hash)
-			return err
-		},
-	},
-	{
-		name:              "InfoAndTxsByNumber",
-		prefetchingRanges: []uint64{0, 1, 5},
-		testFunc: func(t *testing.T, ctx context.Context, client *PrefetchingEthClient, mockRPC *mockRPC) error {
-			number := uint64(1234)
-			block := &types.Block{}
-			blockInfo := eth.BlockToInfo(block)
-			txs := types.Transactions{}
-			mockRPC.On("InfoAndTxsByNumber", ctx, number).Return(blockInfo, txs, nil).Once()
-			_, _, err := client.InfoAndTxsByNumber(ctx, number)
-			return err
-		},
-	},
-	{
-		name:              "PayloadByHash",
-		prefetchingRanges: []uint64{0, 1, 5},
-		testFunc: func(t *testing.T, ctx context.Context, client *PrefetchingEthClient, mockRPC *mockRPC) error {
-			hash := common.Hash{}
-			payload := &eth.ExecutionPayload{}
-			mockRPC.On("PayloadByHash", ctx, hash).Return(payload, nil).Once()
-			_, err := client.PayloadByHash(ctx, hash)
-			return err
-		},
-	},
-	{
-		name:              "PayloadByNumber",
-		prefetchingRanges: []uint64{0, 1, 5},
-		testFunc: func(t *testing.T, ctx context.Context, client *PrefetchingEthClient, mockRPC *mockRPC) error {
-			number := uint64(1234)
-			payload := &eth.ExecutionPayload{}
-			mockRPC.On("PayloadByNumber", ctx, number).Return(payload, nil).Once()
-			_, err := client.PayloadByNumber(ctx, number)
-			return err
-		},
-	},
-	{
-		name:              "PayloadByLabel",
-		prefetchingRanges: []uint64{0, 1, 5},
-		testFunc: func(t *testing.T, ctx context.Context, client *PrefetchingEthClient, mockRPC *mockRPC) error {
-			label := eth.BlockLabel(eth.Unsafe)
-			payload := &eth.ExecutionPayload{}
-			mockRPC.On("PayloadByLabel", ctx, label).Return(payload, nil).Once()
-			_, err := client.PayloadByLabel(ctx, label)
-			return err
-		},
-	},
-	{
-		name:              "FetchReceipts",
-		prefetchingRanges: []uint64{0, 1, 5},
-		testFunc: func(t *testing.T, ctx context.Context, client *PrefetchingEthClient, mockRPC *mockRPC) error {
-			blockHash := common.Hash{}
-			blockInfo := eth.BlockToInfo(&types.Block{})
-			receipts := types.Receipts{}
-			mockRPC.On("FetchReceipts", ctx, blockHash).Return(blockInfo, receipts, nil).Once()
-			_, _, err := client.FetchReceipts(ctx, blockHash)
-			return err
-		},
-	},
+	// {
+	// 	name:              "InfoByHash",
+	// 	prefetchingRanges: []uint64{0, 1, 5},
+	// 	testFunc: func(t *testing.T, ctx context.Context, client *PrefetchingEthClient, mockRPC *mockRPC) error {
+	// 		hash := randHash()
+	// 		_, rhdr := randHeader()
+	// 		rhdr.Hash = hash
+	// 		expectedInfo, _ := rhdr.Info(true, false)
+	// 		mockRPC.On("InfoByHash", ctx, hash).Return(expectedInfo, nil).Once()
+	// 		info, err := client.InfoByHash(ctx, hash)
+	// 		require.NoError(t, err)
+	// 		require.Equal(t, expectedInfo, info)
+	// 		return nil
+	// 	},
+	// },
+	// {
+	// 	name:              "InfoByLabel",
+	// 	prefetchingRanges: []uint64{0, 1, 5},
+	// 	testFunc: func(t *testing.T, ctx context.Context, client *PrefetchingEthClient, mockRPC *mockRPC) error {
+	// 		label := eth.BlockLabel(eth.Unsafe)
+	// 		_, rhdr := randHeader()
+	// 		expectedInfo, _ := rhdr.Info(true, false)
+	// 		mockRPC.On("InfoByLabel", ctx, label).Return(expectedInfo, nil).Once()
+	// 		info, err := client.InfoByLabel(ctx, label)
+	// 		require.NoError(t, err)
+	// 		require.Equal(t, expectedInfo, info)
+	// 		return nil
+	// 	},
+	// },
+	// {
+	// 	name:              "InfoAndTxsByHash",
+	// 	prefetchingRanges: []uint64{0, 1, 5},
+	// 	testFunc: func(t *testing.T, ctx context.Context, client *PrefetchingEthClient, mockRPC *mockRPC) error {
+	// 		hash := common.Hash{}
+	// 		block := &types.Block{}
+	// 		blockInfo := eth.BlockToInfo(block)
+	// 		txs := types.Transactions{}
+	// 		mockRPC.On("InfoAndTxsByHash", ctx, hash).Return(blockInfo, txs, nil).Once()
+	// 		_, _, err := client.InfoAndTxsByHash(ctx, hash)
+	// 		return err
+	// 	},
+	// },
+	// {
+	// 	name:              "InfoAndTxsByNumber",
+	// 	prefetchingRanges: []uint64{0, 1, 5},
+	// 	testFunc: func(t *testing.T, ctx context.Context, client *PrefetchingEthClient, mockRPC *mockRPC) error {
+	// 		number := uint64(1234)
+	// 		block := &types.Block{}
+	// 		blockInfo := eth.BlockToInfo(block)
+	// 		txs := types.Transactions{}
+	// 		mockRPC.On("InfoAndTxsByNumber", ctx, number).Return(blockInfo, txs, nil).Once()
+	// 		_, _, err := client.InfoAndTxsByNumber(ctx, number)
+	// 		return err
+	// 	},
+	// },
+	// {
+	// 	name:              "PayloadByHash",
+	// 	prefetchingRanges: []uint64{0, 1, 5},
+	// 	testFunc: func(t *testing.T, ctx context.Context, client *PrefetchingEthClient, mockRPC *mockRPC) error {
+	// 		hash := common.Hash{}
+	// 		payload := &eth.ExecutionPayload{}
+	// 		mockRPC.On("PayloadByHash", ctx, hash).Return(payload, nil).Once()
+	// 		_, err := client.PayloadByHash(ctx, hash)
+	// 		return err
+	// 	},
+	// },
+	// {
+	// 	name:              "PayloadByNumber",
+	// 	prefetchingRanges: []uint64{0, 1, 5},
+	// 	testFunc: func(t *testing.T, ctx context.Context, client *PrefetchingEthClient, mockRPC *mockRPC) error {
+	// 		number := uint64(1234)
+	// 		payload := &eth.ExecutionPayload{}
+	// 		mockRPC.On("PayloadByNumber", ctx, number).Return(payload, nil).Once()
+	// 		_, err := client.PayloadByNumber(ctx, number)
+	// 		return err
+	// 	},
+	// },
+	// {
+	// 	name:              "PayloadByLabel",
+	// 	prefetchingRanges: []uint64{0, 1, 5},
+	// 	testFunc: func(t *testing.T, ctx context.Context, client *PrefetchingEthClient, mockRPC *mockRPC) error {
+	// 		label := eth.BlockLabel(eth.Unsafe)
+	// 		payload := &eth.ExecutionPayload{}
+	// 		mockRPC.On("PayloadByLabel", ctx, label).Return(payload, nil).Once()
+	// 		_, err := client.PayloadByLabel(ctx, label)
+	// 		return err
+	// 	},
+	// },
+	// {
+	// 	name:              "FetchReceipts",
+	// 	prefetchingRanges: []uint64{0, 1, 5},
+	// 	testFunc: func(t *testing.T, ctx context.Context, client *PrefetchingEthClient, mockRPC *mockRPC) error {
+	// 		blockHash := common.Hash{}
+	// 		blockInfo := eth.BlockToInfo(&types.Block{})
+	// 		receipts := types.Receipts{}
+	// 		mockRPC.On("FetchReceipts", ctx, blockHash).Return(blockInfo, receipts, nil).Once()
+	// 		_, _, err := client.FetchReceipts(ctx, blockHash)
+	// 		return err
+	// 	},
+	// },
 }
 
 // runTest runs a given test function with a specific prefetching range.
