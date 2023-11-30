d3e31027e09172e9c4e926eed7c128ae758619ca
Inphi <mlaw2501@gmail.com>
2023-11-30 13:13:32 -0500

    Update packages/contracts-bedrock/scripts/generate-snapshots.ts
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- packages/contracts-bedrock/scripts/generate-snapshots.ts
+++ packages/contracts-bedrock/scripts/generate-snapshots.ts
@@ -67,7 +67,11 @@ const sortKeys = (obj: any) => {
 // ContractName.0.9.8.json -> ContractName.sol
 // ContractName.json -> ContractName.sol
 const parseArtifactName = (artifactVersionFile: string): string => {
-  return artifactVersionFile.match(/(.*?)\.([0-9]+\.[0-9]+\.[0-9]+)?/)[1]
+  const match = artifactVersionFile.match(/(.*?)\.([0-9]+\.[0-9]+\.[0-9]+)?/);
+  if (!match) {
+    throw new Error(`Invalid artifact file name: ${artifactVersionFile}`);
+  }
+  return match[1];
 }
 
 const main = async () => {
