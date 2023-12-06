a1b63900dc33b99c672f544b9863a310149c4af3
Hamdi Allam <hamdi.allam97@gmail.com>
2023-12-06 13:56:41 -0500

    e2e test for storage updates

--- interop/e2e_tests/postie_test.go
+++ interop/e2e_tests/postie_test.go
@@ -1,11 +1,48 @@
 package e2e_tests
 
-import "testing"
+import (
+	"context"
+	"math/big"
+	"testing"
+	"time"
 
-func TestPostie(t *testing.T) {
-	_ = createE2ETestSuite(t)
+	"github.com/ethereum-optimism/optimism/op-bindings/bindings"
+	"github.com/ethereum-optimism/optimism/op-bindings/predeploys"
+	"github.com/ethereum-optimism/optimism/op-e2e/e2eutils/wait"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/params"
+	"github.com/stretchr/testify/require"
+)
 
-	for {
-		// just looping to make sure postie logs look good for now
-	}
+func TestPostieStorageRootUpdates(t *testing.T) {
+	testSuite := createE2ETestSuite(t)
+
+	// wait for the first storage root of chain B to change
+	var oldStorageRoot common.Hash
+	require.NoError(t, wait.For(context.Background(), time.Second/2, func() (bool, error) {
+		oldStorageRoot = testSuite.PostieA.OutboxStorageRoot(testSuite.ChainIdB)
+		return oldStorageRoot != common.Hash{}, nil
+	}))
+
+	// initiate an message on chain B
+	// NOTE: the destination chain does not matter for now as postie will update for any change
+	outbox, err := bindings.NewCrossL2Outbox(predeploys.CrossL2OutboxAddr, testSuite.OpSysB.Clients["sequencer"])
+	require.NoError(t, err)
+
+	sender, senderAddr := testSuite.OpCfg.Secrets.Bob, testSuite.OpCfg.Secrets.Addresses().Bob
+	senderOpts, err := bind.NewKeyedTransactorWithChainID(sender, big.NewInt(int64(testSuite.ChainIdB)))
+	require.NoError(t, err)
+	senderOpts.Value = big.NewInt(params.Ether / 2)
+
+	tx, err := outbox.InitiateMessage(senderOpts, common.BigToHash(big.NewInt(int64(testSuite.ChainIdA))), senderAddr, big.NewInt(25_000), []byte{})
+	require.NoError(t, err)
+
+	_, err = wait.ForReceiptOK(context.Background(), testSuite.OpSysB.Clients["sequencer"], tx.Hash())
+	require.NoError(t, err)
+
+	// wait for a changed root
+	require.NoError(t, wait.For(context.Background(), time.Second/2, func() (bool, error) {
+		return testSuite.PostieA.OutboxStorageRoot(testSuite.ChainIdB) != oldStorageRoot, nil
+	}))
 }
--- interop/e2e_tests/setup.go
+++ interop/e2e_tests/setup.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/log"
 	"github.com/stretchr/testify/require"
 
@@ -21,11 +22,13 @@ type E2ETestSuite struct {
 
 	OpCfg *op_e2e.SystemConfig
 
-	OpSysA  *op_e2e.System
-	PostieA *interop.Postie
+	ChainIdA uint64
+	OpSysA   *op_e2e.System
+	PostieA  *interop.Postie
 
-	OpSysB  *op_e2e.System
-	PostieB *interop.Postie
+	ChainIdB uint64
+	OpSysB   *op_e2e.System
+	PostieB  *interop.Postie
 }
 
 func createE2ETestSuite(t *testing.T) E2ETestSuite {
@@ -47,6 +50,9 @@ func createE2ETestSuite(t *testing.T) E2ETestSuite {
 	// NOTE: These two L2s will not settle on the same L1 local network which
 	// is fine for now because this prototype does not care about L1 state and
 	// L1 liquidity is still seperate per L2 chain.
+	interopAtGenesis := hexutil.Uint64(0)
+	opCfg.DeployConfig.L2GenesisInteropTimeOffset = &interopAtGenesis
+	opCfg.DeployConfig.SuperchainPostie = &opCfg.Secrets.Addresses().Alice
 
 	opCfg.DeployConfig.L2ChainID = 901
 	opSysA, err := opCfg.Start(t)
@@ -57,16 +63,16 @@ func createE2ETestSuite(t *testing.T) E2ETestSuite {
 	require.NoError(t, err)
 
 	postieCfgA := interop.PostieConfig{
-		Postie:           opSysA.Cfg.Secrets.SysCfgOwner, // setup the right private key
+		Postie:           opCfg.Secrets.Alice,
 		DestinationChain: opSysA.Clients["sequencer"],
-		ConnectedChains:  []node.EthClient{node.FromRPCClient(opSysB.RawClients["sequencer"], node.NewMetrics(m, "a"))},
-		UpdateInterval:   10 * time.Second,
+		ConnectedChains:  []node.EthClient{node.FromRPCClient(opSysB.RawClients["sequencer"], node.NewMetrics(m, "b"))},
+		UpdateInterval:   6 * time.Duration(opCfg.DeployConfig.L2BlockTime) * time.Second, // every ~6 blocks
 	}
 	postieCfgB := interop.PostieConfig{
-		Postie:           opSysB.Cfg.Secrets.SysCfgOwner, // setup the right private key
+		Postie:           opCfg.Secrets.Alice,
 		DestinationChain: opSysB.Clients["sequencer"],
-		ConnectedChains:  []node.EthClient{node.FromRPCClient(opSysA.RawClients["sequencer"], node.NewMetrics(m, "b"))},
-		UpdateInterval:   10 * time.Second,
+		ConnectedChains:  []node.EthClient{node.FromRPCClient(opSysA.RawClients["sequencer"], node.NewMetrics(m, "a"))},
+		UpdateInterval:   6 * time.Duration(opCfg.DeployConfig.L2BlockTime) * time.Second, // every ~6 blocks
 	}
 
 	log := testlog.Logger(t, log.LvlInfo).New("role", "postie")
@@ -87,10 +93,12 @@ func createE2ETestSuite(t *testing.T) E2ETestSuite {
 		t:     t,
 		OpCfg: &opCfg,
 
-		OpSysA:  opSysA,
-		PostieA: postieA,
+		ChainIdA: 901,
+		OpSysA:   opSysA,
+		PostieA:  postieA,
 
-		OpSysB:  opSysB,
-		PostieB: postieB,
+		ChainIdB: 902,
+		OpSysB:   opSysB,
+		PostieB:  postieB,
 	}
 }
