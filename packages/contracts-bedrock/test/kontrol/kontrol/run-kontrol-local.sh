93b24c7ab6dfc4fa429ae58680e33d0b99977c95
Juan C <juanconrod@protonmail.com>
2023-12-09 19:30:44 +0100

    Add `run-kontrol-local.sh`

---
+++ packages/contracts-bedrock/test/kontrol/kontrol/run-kontrol-local.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+export FOUNDRY_PROFILE=stategen
+
+# Create a log file to store standard out and standard error
+LOG_FILE="run-kontrol-$(date +'%Y-%m-%d-%H-%M-%S').log"
+exec > >(tee -i $LOG_FILE)
+exec 2>&1
+
+kontrol_build() {
+    kontrol build                     \
+            --verbose                 \
+            --require ${lemmas}       \
+            --module-import ${module} \
+            ${regen}                  \
+            ${rekompile}
+}
+  # --bmc-depth ${bmc_depth}           \
+kontrol_prove() {
+    kontrol prove                              \
+            --verbose                          \
+            --max-depth ${max_depth}           \
+            --max-iterations ${max_iterations} \
+            --smt-timeout ${smt_timeout}       \
+            --workers ${workers}               \
+            ${reinit}                          \
+            ${bug_report}                      \
+            ${break_on_calls}                  \
+            ${auto_abstract}                   \
+            ${tests}                           \
+            ${use_booster} # \
+             # --kore-rpc-command="kore-rpc-booster -l Rewrite"
+}
+
+###
+# kontrol build options
+###
+# NOTE: This script should be executed from the `contracts-bedrock` directory
+lemmas=test/kontrol/kontrol/pausability-lemmas.k
+base_module=PAUSABILITY-LEMMAS
+module=StateDiffTest:${base_module}
+
+rekompile=--rekompile
+regen=--regen
+rekompile=
+regen=
+
+###
+# kontrol prove options
+###
+max_depth=1000000
+
+max_iterations=1000000
+
+smt_timeout=100000
+
+# bmc_depth=10
+
+workers=1
+
+reinit=--reinit
+reinit=
+
+break_on_calls=--no-break-on-calls
+# break_on_calls=
+
+auto_abstract=--auto-abstract-gas
+# auto_abstract=
+
+bug_report=--bug-report
+bug_report=
+
+use_booster=--use-booster
+# use_booster=
+
+# List of tests to symbolically execute
+tests=""
+#tests+="--match-test CounterTest.test_SetNumber "
+#tests+="--match-test StateDiffTest.setUp "
+tests+="--match-test StateDiffCheatcode.recreateDeployment "
+# tests+="--match-test StateDiffTest.testVerifyStateChange "
+
+kontrol_build
+kontrol_prove
