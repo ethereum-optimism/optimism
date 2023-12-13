f82378b2ed601772128a4eaa1df2c83cf432bd58
Juan C <juanconrod@protonmail.com>
2023-12-13 14:29:20 +0100

    Rename `StateDiff` to `KontrolDeployment`

--- packages/contracts-bedrock/test/kontrol/KontrolDeployment.sol
+++ packages/contracts-bedrock/test/kontrol/KontrolDeployment.sol
@@ -1,29 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import { VmSafe } from "forge-std/Vm.sol";
-import { Script } from "forge-std/Script.sol";
-
-import { console2 as console } from "forge-std/console2.sol";
-import { stdJson } from "forge-std/StdJson.sol";
-
-import {Counter} from "./Counter.sol";
-import { LibStateDiff } from "scripts/libraries/LibStateDiff.sol";
-import { OptimismPortal } from "src/L1/OptimismPortal.sol";
 import { SuperchainConfig } from "src/L1/SuperchainConfig.sol";
 import { Deploy } from "scripts/Deploy.s.sol";
 
-contract MakeStateDiff is Deploy {
+contract KontrolDeployment is Deploy {
 
-    function testStateDiff() public stateDiff /* broadcast */ {
-        /* Counter counter = new Counter(); */
-        /* console.log("COUNTER", address(counter)); */
-        /* Counter counter2 = new Counter(); */
-        /* console.log("COUNTER2", address(counter2)); */
-        /* counter.setNumber(3); */
-        /* counter.setNumber(42); */
-        /* counter2.setNumber(777); */
-        /* OptimismPortal portal = new OptimismPortal */
+    function runKontrolDeployment() public stateDiff {
         deploySafe();
         setupSuperchain();
 
