999b2710cfb4f3f89b524bcd8b676a4f8d31278e
EvanJRichard <evan@oplabs.co>
2023-12-14 15:39:59 -0500

    op-service: Active L2 Provider - add test case for a sequencer returning an error.

--- op-service/dial/active_l2_provider_test.go
+++ op-service/dial/active_l2_provider_test.go
@@ -14,7 +14,7 @@ import (
 
 // TestActiveSequencerFailoverBehavior_RollupProvider tests that the ActiveL2RollupProvider
 // will failover to the next provider if the current one is not active.
-func TestActiveSequencerFailoverBehavior_RollupProviders(t *testing.T) {
+func TestActiveSequencerFailoverBehavior_RollupProviders_Inactive(t *testing.T) {
 	// Create two mock rollup clients, one of which will declare itself inactive after first check.
 	primarySequencer := new(testutils.MockRollupClient)
 	primarySequencer.ExpectSequencerActive(true, nil)
@@ -53,7 +53,7 @@ func TestActiveSequencerFailoverBehavior_RollupProviders(t *testing.T) {
 
 // TestActiveSequencerFailoverBehavior_L2Providers tests that the ActiveL2EndpointProvider
 // will failover to the next provider if the current one is not active.
-func TestActiveSequencerFailoverBehavior_L2Providers(t *testing.T) {
+func TestActiveSequencerFailoverBehavior_L2Providers_Inactive(t *testing.T) {
 	// as TestActiveSequencerFailoverBehavior_RollupProviders,
 	// but ensure the added `EthClient()` method also triggers the failover.
 	primarySequencer := new(testutils.MockRollupClient)
@@ -101,4 +101,43 @@ func TestActiveSequencerFailoverBehavior_L2Providers(t *testing.T) {
 	secondClientUsed, err := endpointProvider.EthClient(context.Background())
 	require.NoError(t, err)
 	require.Same(t, secondaryEthClient, secondClientUsed)
+}
+
+// TestActiveSequencerFailoverBehavior_RollupProvider tests that the ActiveL2RollupProvider
+// will failover to the next provider if the current one is not active.
+func TestActiveSequencerFailoverBehavior_RollupProviders_Errored(t *testing.T) {
+	// Create two mock rollup clients, one of which will error out after first check
+	primarySequencer := new(testutils.MockRollupClient)
+	primarySequencer.ExpectSequencerActive(true, nil)
+	primarySequencer.ExpectSequencerActive(true, fmt.Errorf("a test error"))
+	primarySequencer.ExpectClose()
+	secondarySequencer := new(testutils.MockRollupClient)
+	secondarySequencer.ExpectSequencerActive(true, nil)
+
+	mockRollupDialer := func(ctx context.Context, timeout time.Duration, log log.Logger, url string) (RollupClientInterface, error) {
+		if url == "primaryRollup" {
+			return primarySequencer, nil
+		} else if url == "secondaryRollup" {
+			return secondarySequencer, nil
+		} else {
+			return nil, fmt.Errorf("unknown test url: %s", url)
+		}
+	}
+
+	endpointProvider, err := newActiveL2RollupProvider(
+		context.Background(),
+		[]string{"primaryRollup", "secondaryRollup"},
+		1*time.Microsecond,
+		1*time.Minute,
+		testlog.Logger(t, log.LvlDebug),
+		mockRollupDialer,
+	)
+	require.NoError(t, err)
+	// Check that the first client is used, then the second once the first declares itself inactive.
+	firstSequencerUsed, err := endpointProvider.RollupClient(context.Background())
+	require.NoError(t, err)
+	require.Same(t, primarySequencer, firstSequencerUsed)
+	secondSequencerUsed, err := endpointProvider.RollupClient(context.Background())
+	require.NoError(t, err)
+	require.Same(t, secondarySequencer, secondSequencerUsed)
 }
