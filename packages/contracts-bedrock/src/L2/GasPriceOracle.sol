65e008da5d3de863fb98887125123ca3b597c1f9
Michael de Hoog <michael.dehoog@coinbase.com>
2023-12-14 15:17:45 -1000

    Add FastLZ for better L1Cost estimation

--- packages/contracts-bedrock/src/L2/GasPriceOracle.sol
+++ packages/contracts-bedrock/src/L2/GasPriceOracle.sol
@@ -158,4 +158,99 @@ contract GasPriceOracle is ISemver {
         }
         return total + (68 * 16);
     }
+
+    /// @notice LZ77 implementation based on FastLZ.
+    ///         Equivalent to level 1 compression and decompression at the following commit:
+    ///         https://github.com/ariya/FastLZ/commit/344eb4025f9ae866ebf7a2ec48850f7113a97a42
+    ///         Decompression is backwards compatible.
+    /// @dev Returns the compressed `data`.
+    /// @custom:attribution Solady <https://github.com/Vectorized/Solady>
+    function flzCompress(bytes memory data) internal pure returns (bytes memory result) {
+        /// @solidity memory-safe-assembly
+        assembly {
+            function ms8(d_, v_) -> _d {
+                mstore8(d_, v_)
+                _d := add(d_, 1)
+            }
+            function u24(p_) -> _u {
+                let w := mload(p_)
+                _u := or(shl(16, byte(2, w)), or(shl(8, byte(1, w)), byte(0, w)))
+            }
+            function cmp(p_, q_, e_) -> _l {
+                for { e_ := sub(e_, q_) } lt(_l, e_) { _l := add(_l, 1) } {
+                    e_ := mul(iszero(byte(0, xor(mload(add(p_, _l)), mload(add(q_, _l))))), e_)
+                }
+            }
+            function literals(runs_, src_, dest_) -> _o {
+                for { _o := dest_ } iszero(lt(runs_, 0x20)) { runs_ := sub(runs_, 0x20) } {
+                    mstore(ms8(_o, 31), mload(src_))
+                    _o := add(_o, 0x21)
+                    src_ := add(src_, 0x20)
+                }
+                if iszero(runs_) { leave }
+                mstore(ms8(_o, sub(runs_, 1)), mload(src_))
+                _o := add(1, add(_o, runs_))
+            }
+            function match(l_, d_, o_) -> _o {
+                for { d_ := sub(d_, 1) } iszero(lt(l_, 263)) { l_ := sub(l_, 262) } {
+                    o_ := ms8(ms8(ms8(o_, add(224, shr(8, d_))), 253), and(0xff, d_))
+                }
+                if iszero(lt(l_, 7)) {
+                    _o := ms8(ms8(ms8(o_, add(224, shr(8, d_))), sub(l_, 7)), and(0xff, d_))
+                    leave
+                }
+                _o := ms8(ms8(o_, add(shl(5, l_), shr(8, d_))), and(0xff, d_))
+            }
+            function setHash(i_, v_) {
+                let p := add(mload(0x40), shl(2, i_))
+                mstore(p, xor(mload(p), shl(224, xor(shr(224, mload(p)), v_))))
+            }
+            function getHash(i_) -> _h {
+                _h := shr(224, mload(add(mload(0x40), shl(2, i_))))
+            }
+            function hash(v_) -> _r {
+                _r := and(shr(19, mul(2654435769, v_)), 0x1fff)
+            }
+            function setNextHash(ip_, ipStart_) -> _ip {
+                setHash(hash(u24(ip_)), sub(ip_, ipStart_))
+                _ip := add(ip_, 1)
+            }
+            codecopy(mload(0x40), codesize(), 0x8000) // Zeroize the hashmap.
+            let op := add(mload(0x40), 0x8000)
+            let a := add(data, 0x20)
+            let ipStart := a
+            let ipLimit := sub(add(ipStart, mload(data)), 13)
+            for { let ip := add(2, a) } lt(ip, ipLimit) {} {
+                let r := 0
+                let d := 0
+                for {} 1 {} {
+                    let s := u24(ip)
+                    let h := hash(s)
+                    r := add(ipStart, getHash(h))
+                    setHash(h, sub(ip, ipStart))
+                    d := sub(ip, r)
+                    if iszero(lt(ip, ipLimit)) { break }
+                    ip := add(ip, 1)
+                    if iszero(gt(d, 0x1fff)) { if eq(s, u24(r)) { break } }
+                }
+                if iszero(lt(ip, ipLimit)) { break }
+                ip := sub(ip, 1)
+                if gt(ip, a) { op := literals(sub(ip, a), a, op) }
+                let l := cmp(add(r, 3), add(ip, 3), add(ipLimit, 9))
+                op := match(l, d, op)
+                ip := setNextHash(setNextHash(add(ip, l), ipStart), ipStart)
+                a := ip
+            }
+            op := literals(sub(add(ipStart, mload(data)), a), a, op)
+            result := mload(0x40)
+            let t := add(result, 0x8000)
+            let n := sub(op, t)
+            mstore(result, n) // Store the length.
+            // Copy the result to compact the memory, overwriting the hashmap.
+            let o := add(result, 0x20)
+            for { let i } lt(i, n) { i := add(i, 0x20) } { mstore(add(o, i), mload(add(t, i))) }
+            mstore(add(o, n), 0) // Zeroize the slot after the string.
+            mstore(0x40, add(add(o, n), 0x20)) // Allocate the memory.
+        }
+    }
 }
