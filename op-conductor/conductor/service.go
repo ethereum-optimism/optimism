188ad48a3ee632516de142efda089dd34fc2b56a
Francis Li <francis.li@coinbase.com>
2023-12-18 15:35:58 -0800

    Finish sequencer control

--- op-conductor/conductor/service.go
+++ op-conductor/conductor/service.go
@@ -4,13 +4,66 @@ import (
 	"context"
 
 	"github.com/ethereum/go-ethereum/log"
+	"github.com/hashicorp/go-multierror"
+	"github.com/pkg/errors"
 
+	"github.com/ethereum-optimism/optimism/op-conductor/client"
 	"github.com/ethereum-optimism/optimism/op-service/cliapp"
+	opclient "github.com/ethereum-optimism/optimism/op-service/client"
+	"github.com/ethereum-optimism/optimism/op-service/sources"
 )
 
 // New creates a new OpConductor instance.
 func New(ctx context.Context, cfg *Config, log log.Logger, version string) (*OpConductor, error) {
-	panic("unimplemented")
+	if err := cfg.Check(); err != nil {
+		return nil, errors.Wrap(err, "invalid config")
+	}
+
+	oc := &OpConductor{
+		log:     log,
+		version: version,
+		cfg:     cfg,
+	}
+
+	err := oc.init(ctx)
+	if err != nil {
+		log.Error("failed to initialize OpConductor", "err", err)
+		// ensure we always close the resources if we fail to initialize the conductor.
+		if closeErr := oc.Stop(ctx); closeErr != nil {
+			return nil, multierror.Append(err, closeErr)
+		}
+	}
+
+	return oc, nil
+}
+
+func (c *OpConductor) init(ctx context.Context) error {
+	c.log.Info("initializing OpConductor", "version", c.version)
+	if err := c.initSequencerControl(ctx); err != nil {
+		return errors.Wrap(err, "failed to initialize sequencer control")
+	}
+	return nil
+}
+
+func (c *OpConductor) initSequencerControl(ctx context.Context) error {
+	ec, err := opclient.NewRPC(ctx, c.log, c.cfg.ExecutionRPC)
+	if err != nil {
+		return errors.Wrap(err, "failed to create geth rpc client")
+	}
+	gethCfg := sources.L2ClientDefaultConfig(&c.cfg.RollupCfg, true)
+	// TODO: Add metrics tracer here. tracked by https://github.com/ethereum-optimism/protocol-quest/issues/45
+	geth, err := sources.NewEthClient(ec, c.log, nil, &gethCfg.EthClientConfig)
+	if err != nil {
+		return errors.Wrap(err, "failed to create geth client")
+	}
+
+	nc, err := opclient.NewRPC(ctx, c.log, c.cfg.NodeRPC)
+	if err != nil {
+		return errors.Wrap(err, "failed to create node rpc client")
+	}
+	node := sources.NewRollupClient(nc)
+	c.ctrl = client.NewSequencerControl(geth, node)
+	return nil
 }
 
 // OpConductor represents a full conductor instance and its resources, it does:
@@ -23,7 +76,12 @@ func New(ctx context.Context, cfg *Config, log log.Logger, version string) (*OpC
 //  2. paused: control loop (sequencer start/stop) is paused, but it still participates in leader election.
 //     it is paused for disaster recovery situation
 //  3. stopped: it is stopped, which means it is not participating in leader election and control loop. OpConductor cannot be started again from stopped mode.
-type OpConductor struct{}
+type OpConductor struct {
+	log     log.Logger
+	version string
+	cfg     *Config
+	ctrl    client.SequencerControl
+}
 
 var _ cliapp.Lifecycle = (*OpConductor)(nil)
 
