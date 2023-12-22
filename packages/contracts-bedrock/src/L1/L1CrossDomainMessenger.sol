55bdf7e059d267233f9c618b33d3bd39f27ac995
Diego <105765223+0xfuturistic@users.noreply.github.com>
2023-12-22 17:37:38 +0100

    contracts-bedrock: improve parameter labeling in L1CrossDomainMessenger

--- packages/contracts-bedrock/src/L1/L1CrossDomainMessenger.sol
+++ packages/contracts-bedrock/src/L1/L1CrossDomainMessenger.sol
@@ -13,41 +13,41 @@ import { SuperchainConfig } from "src/L1/SuperchainConfig.sol";
 ///         for sending and receiving data on the L1 side. Users are encouraged to use this
 ///         interface instead of interacting with lower-level contracts directly.
 contract L1CrossDomainMessenger is CrossDomainMessenger, ISemver {
-    /// @notice Contract of the SuperchainConfig contract.
+    /// @notice Contract of the SuperchainConfig.
     SuperchainConfig public superchainConfig;
-
-    /// @notice Contract of the OptimismPortal.
-    /// @custom:network-specific
-    OptimismPortal public portal;
 
     /// @notice Semantic version.
     /// @custom:semver 2.2.0
     string public constant version = "2.2.0";
+
+    /// @notice Contract of the OptimismPortal.
+    /// @custom:network-specific
+    OptimismPortal public portal;
 
     /// @notice Constructs the L1CrossDomainMessenger contract.
     constructor() CrossDomainMessenger() {
         initialize({
             _superchainConfig: SuperchainConfig(address(0)),
             _portal: OptimismPortal(payable(address(0))),
-            _otherMessenger: Predeploys.L2_CROSS_DOMAIN_MESSENGER
+            _l2CrossDomainMessenger: Predeploys.L2_CROSS_DOMAIN_MESSENGER
         });
     }
 
     /// @notice Initializes the contract.
     /// @param _superchainConfig Contract of the SuperchainConfig contract on this network.
     /// @param _portal Contract of the OptimismPortal contract on this network.
-    /// @param _otherMessenger Address of the L2CrossDomainMessenger contract on the other network.
+    /// @param _l2CrossDomainMessenger Address of the L2CrossDomainMessenger contract on the other network.
     function initialize(
         SuperchainConfig _superchainConfig,
         OptimismPortal _portal,
-        address _otherMessenger
+        address _l2CrossDomainMessenger
     )
         public
         initializer
     {
         superchainConfig = _superchainConfig;
         portal = _portal;
-        __CrossDomainMessenger_init({ _otherMessenger: _otherMessenger });
+        __CrossDomainMessenger_init({ _otherMessenger: _l2CrossDomainMessenger });
     }
 
     /// @notice Getter function for the address of the OptimismPortal on this chain.
