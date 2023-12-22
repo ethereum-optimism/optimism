c1b4a25985a0f297eedd57af9477e4dace4d4818
refcell <abigger87@gmail.com>
2023-12-22 10:35:08 -0800

    small fixes and port other alphabet e2e test

--- op-e2e/faultproofs/output_alphabet_test.go
+++ op-e2e/faultproofs/output_alphabet_test.go
@@ -77,11 +77,12 @@ func TestOutputAlphabetGame_ExhaustiveDisputeGame(t *testing.T) {
 		game := disputeGameFactory.StartOutputAlphabetGame(ctx, "sequencer", 3, rootClaimedAlphabet)
 		require.NotNil(t, game)
 		game.LogGameData(ctx)
+		gameDepth := uint64(game.MaxDepth(ctx))
 		gameDuration := game.GameDuration(ctx)
 
 		// Start honest challenger
 		game.StartChallenger(ctx, "sequencer", "Challenger",
-			challenger.WithAlphabet(disputegame.CorrectAlphabet),
+			challenger.WithOutputAlphabet(disputegame.CorrectAlphabet, sys.RollupEndpoint("sequencer")),
 			challenger.WithPrivKey(sys.Cfg.Secrets.Alice),
 			// Ensures the challenger responds to all claims before test timeout
 			challenger.WithPollInterval(time.Millisecond*400),
@@ -91,7 +92,7 @@ func TestOutputAlphabetGame_ExhaustiveDisputeGame(t *testing.T) {
 		dishonestHelper := game.CreateDishonestHelper(
 			ctx,
 			disputegame.CorrectAlphabet,
-			4,
+			gameDepth,
 			"sequencer",
 			!isRootCorrect,
 		)
@@ -123,4 +124,107 @@ func TestOutputAlphabetGame_ExhaustiveDisputeGame(t *testing.T) {
 		op_e2e.InitParallel(t, op_e2e.UseExecutor(1))
 		testCase(t, false)
 	})
+}
+
+func TestChallengerCompleteOutputDisputeGame(t *testing.T) {
+	op_e2e.InitParallel(t, op_e2e.UseExecutor(1))
+
+	tests := []struct {
+		name              string
+		rootClaimAlphabet string
+		otherAlphabet     string
+		expectedResult    disputegame.Status
+		expectStep        bool
+	}{
+		{
+			name:              "ChallengerWins_DefenseStep",
+			rootClaimAlphabet: "abcdexyz",
+			otherAlphabet:     disputegame.CorrectAlphabet,
+			expectedResult:    disputegame.StatusChallengerWins,
+			expectStep:        true,
+		},
+		{
+			name:              "DefenderWins_DefenseStep",
+			rootClaimAlphabet: disputegame.CorrectAlphabet,
+			otherAlphabet:     "abcdexyz",
+			expectedResult:    disputegame.StatusDefenderWins,
+			expectStep:        false,
+		},
+		{
+			name:              "ChallengerWins_AttackStep",
+			rootClaimAlphabet: "abcdefghzyx",
+			otherAlphabet:     disputegame.CorrectAlphabet,
+			expectedResult:    disputegame.StatusChallengerWins,
+			expectStep:        true,
+		},
+		{
+			name:              "DefenderWins_AttackStep",
+			rootClaimAlphabet: disputegame.CorrectAlphabet,
+			otherAlphabet:     "abcdexyz",
+			expectedResult:    disputegame.StatusDefenderWins,
+			expectStep:        false,
+		},
+		{
+			name:              "DefenderIncorrectAtTraceZero",
+			rootClaimAlphabet: "zyxwvut",
+			otherAlphabet:     disputegame.CorrectAlphabet,
+			expectedResult:    disputegame.StatusChallengerWins,
+			expectStep:        true,
+		},
+		{
+			name:              "ChallengerIncorrectAtTraceZero",
+			rootClaimAlphabet: disputegame.CorrectAlphabet,
+			otherAlphabet:     "zyxwvut",
+			expectedResult:    disputegame.StatusDefenderWins,
+			expectStep:        false,
+		},
+		{
+			name:              "DefenderIncorrectAtLastTraceIndex",
+			rootClaimAlphabet: "abcdefghijklmnoz",
+			otherAlphabet:     disputegame.CorrectAlphabet,
+			expectedResult:    disputegame.StatusChallengerWins,
+			expectStep:        true,
+		},
+		{
+			name:              "ChallengerIncorrectAtLastTraceIndex",
+			rootClaimAlphabet: disputegame.CorrectAlphabet,
+			otherAlphabet:     "abcdefghijklmnoz",
+			expectedResult:    disputegame.StatusDefenderWins,
+			expectStep:        false,
+		},
+	}
+	for _, test := range tests {
+		test := test
+		t.Run(test.name, func(t *testing.T) {
+			op_e2e.InitParallel(t, op_e2e.UseExecutor(1))
+
+			ctx := context.Background()
+			sys, l1Client := startFaultDisputeSystem(t)
+			t.Cleanup(sys.Close)
+
+			disputeGameFactory := disputegame.NewFactoryHelper(t, ctx, sys)
+			game := disputeGameFactory.StartOutputAlphabetGame(ctx, "sequencer", 1, test.rootClaimAlphabet)
+			require.NotNil(t, game)
+			gameDuration := game.GameDuration(ctx)
+
+			game.StartChallenger(ctx, "sequencer", "Defender",
+				challenger.WithPrivKey(sys.Cfg.Secrets.Mallory),
+			)
+
+			game.StartChallenger(ctx, "sequencer", "Challenger",
+				challenger.WithOutputAlphabet(test.otherAlphabet, sys.RollupEndpoint("sequencer")),
+				challenger.WithPrivKey(sys.Cfg.Secrets.Alice),
+			)
+
+			// Wait for a claim at the maximum depth that has been countered to indicate we're ready to resolve the game
+			game.WaitForClaimAtMaxDepth(ctx, test.expectStep)
+
+			sys.TimeTravelClock.AdvanceTime(gameDuration)
+			require.NoError(t, wait.ForNextBlock(ctx, l1Client))
+
+			game.WaitForInactivity(ctx, 10, true)
+			game.LogGameData(ctx)
+			require.EqualValues(t, test.expectedResult, game.Status(ctx))
+		})
+	}
 }
