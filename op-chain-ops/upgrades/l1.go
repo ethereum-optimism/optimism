ffa1f1388ab45e14cda32e0b72c8625b996bf1c5
Diego <105765223+0xfuturistic@users.noreply.github.com>
2023-12-24 21:00:19 +0100

    op-chain-ops: fix typo in variable name

--- op-chain-ops/upgrades/l1.go
+++ op-chain-ops/upgrades/l1.go
@@ -442,7 +442,7 @@ func OptimismMintableERC20Factory(batch *safe.Batch, implementations superchain.
 		}
 	}
 
-	OptimismMintableERC20FactoryeABI, err := bindings.OptimismMintableERC20FactoryMetaData.GetAbi()
+	OptimismMintableERC20FactoryABI, err := bindings.OptimismMintableERC20FactoryMetaData.GetAbi()
 	if err != nil {
 		return err
 	}
@@ -461,7 +461,7 @@ func OptimismMintableERC20Factory(batch *safe.Batch, implementations superchain.
 		}
 	}
 
-	calldata, err := OptimismMintableERC20FactoryeABI.Pack("initialize", bridge)
+	calldata, err := OptimismMintableERC20FactoryABI.Pack("initialize", bridge)
 	if err != nil {
 		return err
 	}
