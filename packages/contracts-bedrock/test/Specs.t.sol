b9c751090b8651143e61489a214c00821c9476bd
Diego <105765223+0xfuturistic@users.noreply.github.com>
2023-12-26 13:06:06 +0100

    contracts-bedrock: update Specs.t.sol with new methods on SystemConfig

--- packages/contracts-bedrock/test/Specs.t.sol
+++ packages/contracts-bedrock/test/Specs.t.sol
@@ -281,6 +281,21 @@ contract Specification_Test is CommonTest {
         _addSpec({ _name: "SystemConfig", _sel: _getSel("transferOwnership(address)"), _auth: Role.SYSTEMCONFIGOWNER });
         _addSpec({ _name: "SystemConfig", _sel: SystemConfig.unsafeBlockSigner.selector });
         _addSpec({ _name: "SystemConfig", _sel: _getSel("version()") });
+        _addSpec({ _name: "SystemConfig", _sel: _getSel("l1CrossDomainMessenger()") });
+        _addSpec({ _name: "SystemConfig", _sel: _getSel("l1ERC721Bridge()") });
+        _addSpec({ _name: "SystemConfig", _sel: _getSel("l1StandardBridge()") });
+        _addSpec({ _name: "SystemConfig", _sel: _getSel("l2OutputOracle()") });
+        _addSpec({ _name: "SystemConfig", _sel: _getSel("optimismPortal()") });
+        _addSpec({ _name: "SystemConfig", _sel: _getSel("optimismMintableERC20Factory()") });
+        _addSpec({ _name: "SystemConfig", _sel: _getSel("batchInbox()") });
+        _addSpec({ _name: "SystemConfig", _sel: _getSel("startBlock()") });
+        _addSpec({ _name: "SystemConfig", _sel: _getSel("L1_CROSS_DOMAIN_MESSENGER_SLOT()") });
+        _addSpec({ _name: "SystemConfig", _sel: _getSel("L1_ERC_721_BRIDGE_SLOT()") });
+        _addSpec({ _name: "SystemConfig", _sel: _getSel("L1_STANDARD_BRIDGE_SLOT()") });
+        _addSpec({ _name: "SystemConfig", _sel: _getSel("L2_OUTPUT_ORACLE_SLOT()") });
+        _addSpec({ _name: "SystemConfig", _sel: _getSel("OPTIMISM_PORTAL_SLOT()") });
+        _addSpec({ _name: "SystemConfig", _sel: _getSel("OPTIMISM_MINTABLE_ERC20_FACTORY_SLOT()") });
+        _addSpec({ _name: "SystemConfig", _sel: _getSel("BATCH_INBOX_SLOT()") });
 
         // ProxyAdmin
         _addSpec({ _name: "ProxyAdmin", _sel: _getSel("addressManager()") });
