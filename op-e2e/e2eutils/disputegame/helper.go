e020d5a54945b6bfdd45bb2773e65e85f214eba3
clabby <ben@clab.by>
2023-12-31 14:12:38 -0500

    e2e helper refactor

--- op-e2e/e2eutils/disputegame/helper.go
+++ op-e2e/e2eutils/disputegame/helper.go
@@ -32,10 +32,9 @@ import (
 )
 
 const (
-	alphabetGameType       uint8 = 255
-	outputCannonGameType   uint8 = 1
-	outputAlphabetGameType uint8 = 254
-	alphabetGameDepth            = 4
+	cannonGameType    uint8 = 0
+	alphabetGameType  uint8 = 255
+	alphabetGameDepth       = 4
 )
 
 var lastAlphabetTraceIndex = big.NewInt(1<<alphabetGameDepth - 1)
@@ -110,44 +109,6 @@ func NewFactoryHelper(t *testing.T, ctx context.Context, system DisputeSystem) *
 	}
 }
 
-func (h *FactoryHelper) StartAlphabetGame(ctx context.Context, claimedAlphabet string) *AlphabetGameHelper {
-	extraData, _ := h.createDisputeGameExtraData(ctx)
-
-	ctx, cancel := context.WithTimeout(ctx, 2*time.Minute)
-	defer cancel()
-
-	trace := alphabet.NewTraceProvider(claimedAlphabet, alphabetGameDepth)
-	pos := faultTypes.NewPosition(alphabetGameDepth, lastAlphabetTraceIndex)
-	rootClaim, err := trace.Get(ctx, pos)
-	h.require.NoError(err, "get root claim")
-	tx, err := transactions.PadGasEstimate(h.opts, 2, func(opts *bind.TransactOpts) (*types.Transaction, error) {
-		return h.factory.Create(opts, alphabetGameType, rootClaim, extraData)
-	})
-	h.require.NoError(err, "create fault dispute game")
-	h.opts.GasLimit = 0
-	rcpt, err := wait.ForReceiptOK(ctx, h.client, tx.Hash())
-	h.require.NoError(err, "wait for create fault dispute game receipt to be OK")
-	h.require.Len(rcpt.Logs, 1, "should have emitted a single DisputeGameCreated event")
-	createdEvent, err := h.factory.ParseDisputeGameCreated(*rcpt.Logs[0])
-	h.require.NoError(err)
-	game, err := bindings.NewFaultDisputeGame(createdEvent.DisputeProxy, h.client)
-	h.require.NoError(err)
-
-	return &AlphabetGameHelper{
-		FaultGameHelper: FaultGameHelper{
-			t:           h.t,
-			require:     h.require,
-			system:      h.system,
-			client:      h.client,
-			opts:        h.opts,
-			game:        game,
-			factoryAddr: h.factoryAddr,
-			addr:        createdEvent.DisputeProxy,
-		},
-		claimedAlphabet: claimedAlphabet,
-	}
-}
-
 func (h *FactoryHelper) StartOutputCannonGameWithCorrectRoot(ctx context.Context, l2Node string, l2BlockNumber uint64) *OutputCannonGameHelper {
 	h.waitForBlockToBeSafe(l2Node, l2BlockNumber)
 	output, err := h.system.RollupClient(l2Node).OutputAtBlock(ctx, l2BlockNumber)
@@ -165,7 +126,7 @@ func (h *FactoryHelper) StartOutputCannonGame(ctx context.Context, l2Node string
 	defer cancel()
 
 	tx, err := transactions.PadGasEstimate(h.opts, 2, func(opts *bind.TransactOpts) (*types.Transaction, error) {
-		return h.factory.Create(opts, outputCannonGameType, rootClaim, extraData)
+		return h.factory.Create(opts, cannonGameType, rootClaim, extraData)
 	})
 	h.require.NoError(err, "create fault dispute game")
 	rcpt, err := wait.ForReceiptOK(ctx, h.client, tx.Hash())
@@ -214,7 +175,7 @@ func (h *FactoryHelper) StartOutputAlphabetGame(ctx context.Context, l2Node stri
 	rootClaim, err := trace.Get(ctx, pos)
 	h.require.NoError(err, "get root claim")
 	tx, err := transactions.PadGasEstimate(h.opts, 2, func(opts *bind.TransactOpts) (*types.Transaction, error) {
-		return h.factory.Create(opts, outputAlphabetGameType, rootClaim, extraData)
+		return h.factory.Create(opts, alphabetGameType, rootClaim, extraData)
 	})
 	h.require.NoError(err, "create output bisection game")
 	rcpt, err := wait.ForReceiptOK(ctx, h.client, tx.Hash())
@@ -264,13 +225,6 @@ func (h *FactoryHelper) waitForBlockToBeSafe(l2Node string, l2BlockNumber uint64
 	h.require.NoErrorf(err, "Block number %v did not become safe", l2BlockNumber)
 }
 
-func (h *FactoryHelper) createDisputeGameExtraData(ctx context.Context) (extraData []byte, l2BlockNumber uint64) {
-	l2BlockNumber = h.waitForProposals(ctx)
-	extraData = make([]byte, 32)
-	binary.BigEndian.PutUint64(extraData[24:], l2BlockNumber)
-	return
-}
-
 func (h *FactoryHelper) StartChallenger(ctx context.Context, name string, options ...challenger.Option) *challenger.Helper {
 	opts := []challenger.Option{
 		challenger.WithFactoryAddress(h.factoryAddr),
@@ -281,14 +235,4 @@ func (h *FactoryHelper) StartChallenger(ctx context.Context, name string, option
 		_ = c.Close()
 	})
 	return c
-}
-
-// waitForProposals waits until there are at least two proposals in the output oracle
-// This is the minimum required for creating a game.
-// Returns the l2 block number of the latest available proposal
-func (h *FactoryHelper) waitForProposals(ctx context.Context) uint64 {
-	ctx, cancel := context.WithTimeout(ctx, 2*time.Minute)
-	defer cancel()
-	latestOutputIdx := h.l2ooHelper.WaitForProposals(ctx, 2)
-	return h.l2ooHelper.GetL2Output(ctx, latestOutputIdx).L2BlockNumber.Uint64()
 }
