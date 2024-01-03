51c24c48985b66242868e8c0197d360b3b688419
Michael Lynch <git@mtlynch.io>
2024-01-02 19:20:25 -0500

    Get rid of extra function docstring

--- op-challenger/game/fault/types/position.go
+++ op-challenger/game/fault/types/position.go
@@ -25,12 +25,7 @@ func NewPosition(depth int, indexAtDepth *big.Int) Position {
 	}
 }
 
-// NewPositionFromGIndex creates a new Position given a generalized index,
-// numbered like the following:
-//
 // NewPositionFromGIndex creates a new Position given a generalized index.
-//
-// See ../../../../specs/fault-dispute-game.md#game-tree
 func NewPositionFromGIndex(x *big.Int) Position {
 	depth := bigMSB(x)
 	withoutMSB := new(big.Int).Not(new(big.Int).Lsh(big.NewInt(1), uint(depth)))
