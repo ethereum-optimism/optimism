d52ca23c4fc72a8f257f657e616fee90f67d7e6e
Mark Tyneway <mark.tyneway@gmail.com>
2024-01-09 13:29:22 -0500

    contracts-bedrock: cleanup libraries
    Updates the pragma in libraries to be more permissive, making it less
    complex to import the libraries into different codebases. The actual
    implementations enforce a particular version, so it is safe to allow
    the libs to have permissive versioning contrainsts. Also update the
    code to use the absolute path for imports relative from the project
    root. Using this import pattern is the standard for the codebase.

--- packages/contracts-bedrock/src/libraries/DisputeErrors.sol
+++ packages/contracts-bedrock/src/libraries/DisputeErrors.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.15;
 
-import "./DisputeTypes.sol";
+import "src/libraries/DisputeTypes.sol";
 
 ////////////////////////////////////////////////////////////////
 //                `DisputeGameFactory` Errors                 //
