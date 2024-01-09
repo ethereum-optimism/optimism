e3ac90b73f404abb10084a96f69ac2c54680d9e0
Francis Li <francis.li@coinbase.com>
2024-01-08 17:00:49 -0800

    Finish test setup

--- op-e2e/sequencer_failover_test.go
+++ op-e2e/sequencer_failover_test.go
@@ -2,7 +2,9 @@ package op_e2e
 
 import "testing"
 
-func TestSequencerFailver(t *testing.T) {
+// [Category: Initial Setup]
+// In this test, we test that we can successfully setup a working cluster.
+func TestSequencerFailver_SetupCluster(t *testing.T) {
 	sys, _ := setupSequencerFailoverTest(t)
 	defer sys.Close()
 }
