2fc5c84d21218647e5c9933539444299a7152268
Mark Tyneway <mark.tyneway@gmail.com>
2024-01-10 03:38:54 +0300

    contracts-bedrock: cleanup libraries (#8907)
    * contracts-bedrock: cleanup libraries
    
    Updates the pragma in libraries to be more permissive, making it less
    complex to import the libraries into different codebases. The actual
    implementations enforce a particular version, so it is safe to allow
    the libs to have permissive versioning contrainsts. Also update the
    code to use the absolute path for imports relative from the project
    root. Using this import pattern is the standard for the codebase.
    
    * contracts-bedrock: remove warning

--- packages/contracts-bedrock/src/libraries/Encoding.sol
+++ packages/contracts-bedrock/src/libraries/Encoding.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import { Types } from "./Types.sol";
-import { Hashing } from "./Hashing.sol";
-import { RLPWriter } from "./rlp/RLPWriter.sol";
+import { Types } from "src/libraries/Types.sol";
+import { Hashing } from "src/libraries/Hashing.sol";
+import { RLPWriter } from "src/libraries/rlp/RLPWriter.sol";
 
 /// @title Encoding
 /// @notice Encoding handles Optimism's various different encoding schemes.
