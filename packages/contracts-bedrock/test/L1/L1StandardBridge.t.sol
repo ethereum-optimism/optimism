f7e8523131a27d2bc7bc60b63080ee3055ed85c1
Diego <105765223+0xfuturistic@users.noreply.github.com>
2024-01-10 12:55:41 -0500

    contracts-bedrock: restore empty test contract for L1StandardBridge

--- packages/contracts-bedrock/test/L1/L1StandardBridge.t.sol
+++ packages/contracts-bedrock/test/L1/L1StandardBridge.t.sol
@@ -153,6 +153,8 @@ contract L1StandardBridge_Pause_TestFail is Bridge_Initializer {
     }
 }
 
+contract L1StandardBridge_Initialize_TestFail is Bridge_Initializer { }
+
 contract L1StandardBridge_Receive_Test is Bridge_Initializer {
     /// @dev Tests receive bridges ETH successfully.
     function test_receive_succeeds() external {
