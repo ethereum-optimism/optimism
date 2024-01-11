55682c0fb4e0f6f318c330c59ce07ba1e13b3481
Juan C <juanconrod@protonmail.com>
2024-01-11 14:49:56 +0100

    make-summary-deployment.sh: add `forge fmt` as last summary gen step

--- packages/contracts-bedrock/test/kontrol/scripts/make-summary-deployment.sh
+++ packages/contracts-bedrock/test/kontrol/scripts/make-summary-deployment.sh
@@ -48,4 +48,6 @@ CONTRACT_NAMES=${CONTRACT_NAMES}Reversed
 SUMMARY_DIR=test/kontrol/proofs/utils
 SUMMARY_NAME=DeploymentSummary
 kontrol summary ${SUMMARY_NAME} snapshots/state-diff/${STATEDIFF} --contract-names ${CONTRACT_NAMES} --output-dir ${SUMMARY_DIR}
+forge fmt ${SUMMARY_DIR}/${SUMMARY_NAME}.sol
+forge fmt ${SUMMARY_DIR}/${SUMMARY_NAME}Code.sol
 echo "Added state updates to ${SUMMARY_DIR}/${SUMMARY_NAME}.sol"
