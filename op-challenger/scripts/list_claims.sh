cfffa38365b2934fcdc3e20c53145a6b819cb93d
Adrian Sutton <adrian@oplabs.co>
2024-01-11 12:26:05 +1000

    op-challenger: Update manual testing scripts (#8948)
    Removes the `make alphabet` target and associated scripts as its no longer possible to specify an alternate canonical alphabet so both challengers are being honest.

--- op-challenger/scripts/list_claims.sh
+++ op-challenger/scripts/list_claims.sh
@@ -10,12 +10,12 @@ echo "Claim count: ${COUNT}"
 ((COUNT=COUNT-1))
 for i in $(seq 0 "${COUNT}")
 do
-  CLAIM=$(cast call --rpc-url "${RPC}" "${GAME_ADDR}" 'claimData(uint256) returns(uint32 parentIndex, bool countered, bytes32 claim, uint128 position, uint128 clock)' "${i}")
+  CLAIM=$(cast call --rpc-url "${RPC}" "${GAME_ADDR}" 'claimData(uint256) returns(uint32 parentIndex, address counteredBy, address claimant, uint128 bond, bytes32 claim, uint128 position, uint128 clock)' "${i}")
   SAVEIFS=$IFS   # Save current IFS (Internal Field Separator)
   IFS=$'\n'      # Change IFS to newline char
   # shellcheck disable=SC2206
   CLAIM=($CLAIM) # split the string into an array by the same name
   IFS=$SAVEIFS # Restore original IFS
 
-  echo "${i}  Parent: ${CLAIM[0]} Countered: ${CLAIM[1]} Claim: ${CLAIM[2]} Position: ${CLAIM[3]} Clock ${CLAIM[4]}"
+  echo "${i}  Parent: ${CLAIM[0]} Countered By: ${CLAIM[1]} Claimant: ${CLAIM[2]} Bond: ${CLAIM[3]} Claim: ${CLAIM[4]} Position: ${CLAIM[5]} Clock ${CLAIM[6]}"
 done
