543c55cb7d1571d526a7e3b69d31d566c1fb7c26
Francis Li <francis.li@coinbase.com>
2024-01-14 10:04:36 -0800

    9th try

--- op-conductor/health/monitor.go
+++ op-conductor/health/monitor.go
@@ -119,7 +119,7 @@ func (hm *SequencerHealthMonitor) healthCheck() bool {
 	now := uint64(time.Now().Unix())
 	if now-status.UnsafeL2.Time > hm.unsafeInterval {
 		hm.log.Error(
-			"unsafe head is not syncing at tip",
+			"unsafe head is not progressing as expected",
 			"now", now,
 			"unsafe_head_num", status.UnsafeL2.Number,
 			"unsafe_head_time", status.UnsafeL2.Time,
