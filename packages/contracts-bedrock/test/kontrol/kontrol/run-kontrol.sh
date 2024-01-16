23dd85a41d7c2e029da87861716572916d325d40
refcell.eth <abigger87@gmail.com>
2024-01-16 00:09:49 -0800

    feat(ops): Consolidate Version Control (#8920)
    * feat(ops): consolidate version control
    
    * fix: remove added slitherrc copy
    
    * fix: Makefile geth install target
    
    * fix: remove other redundant cat commands
    
    * fix: rabbit's suggestion

--- packages/contracts-bedrock/test/kontrol/kontrol/run-kontrol.sh
+++ packages/contracts-bedrock/test/kontrol/kontrol/run-kontrol.sh
@@ -22,7 +22,7 @@ blank_line
 
 export FOUNDRY_PROFILE=kontrol
 export CONTAINER_NAME=kontrol-tests
-KONTROLRC=$(cat "${WORKSPACE_DIR}/../../.kontrolrc")
+KONTROLRC=$(jq -r .kontrol < ${WORKSPACE_DIR}/../../versions.json)
 export KONTROL_RELEASE=${KONTROLRC}
 
 
