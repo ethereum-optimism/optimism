289e84adfeaa15ab04a4a83f9bd77f66b29d35fe
refcell <abigger87@gmail.com>
2024-01-16 09:34:38 -0800

    feat(op-challenger): large preimage uploader

--- op-challenger/game/fault/preimages/types.go
+++ op-challenger/game/fault/preimages/types.go
@@ -4,10 +4,16 @@ import (
 	"context"
 
 	"github.com/ethereum-optimism/optimism/op-challenger/game/fault/types"
+	"github.com/ethereum-optimism/optimism/op-service/txmgr"
 )
 
 // PreimageUploader is responsible for posting preimages.
 type PreimageUploader interface {
 	// UploadPreimage uploads the provided preimage.
 	UploadPreimage(ctx context.Context, claimIdx uint64, data *types.PreimageOracleData) error
+}
+
+// PreimageOracleContract is the interface for interacting with the PreimageOracle contract.
+type PreimageOracleContract interface {
+	UpdateOracleTx(ctx context.Context, claimIdx uint64, data *types.PreimageOracleData) (txmgr.TxCandidate, error)
 }
