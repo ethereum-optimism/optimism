7787fc1910ce0e76e6dbd45191dcdfd007a97a01
Diego <105765223+0xfuturistic@users.noreply.github.com>
2024-01-16 10:08:53 -0500

    op-node: capitalize writeJSONFile function
    this makes it accessible when op-node/cmd/genesis is imported as a package

--- op-node/cmd/genesis/cmd.go
+++ op-node/cmd/genesis/cmd.go
@@ -126,7 +126,7 @@ var Subcommands = cli.Commands{
 				return err
 			}
 
-			return writeJSONFile(ctx.String("outfile.l1"), l1Genesis)
+			return WriteJSONFile(ctx.String("outfile.l1"), l1Genesis)
 		},
 	},
 	{
@@ -249,17 +249,17 @@ var Subcommands = cli.Commands{
 				return fmt.Errorf("generated rollup config does not pass validation: %w", err)
 			}
 
-			if err := writeJSONFile(ctx.String("outfile.l2"), l2Genesis); err != nil {
+			if err := WriteJSONFile(ctx.String("outfile.l2"), l2Genesis); err != nil {
 				return err
 			}
-			return writeJSONFile(ctx.String("outfile.rollup"), rollupConfig)
+			return WriteJSONFile(ctx.String("outfile.rollup"), rollupConfig)
 		},
 	},
 }
 
-// writeJSONFile will write a JSON file to disk at the given path
+// WriteJSONFile will write a JSON file to disk at the given path
 // containing the JSON serialized input value.
-func writeJSONFile(outfile string, input any) error {
+func WriteJSONFile(outfile string, input any) error {
 	f, err := os.OpenFile(outfile, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0o644)
 	if err != nil {
 		return err
