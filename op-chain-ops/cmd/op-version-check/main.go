b6ceb40c0a22fd4d7022f460bf8959d1ec677679
Diego <105765223+0xfuturistic@users.noreply.github.com>
2024-01-17 13:20:30 -0500

    op-chain-ops: pass RPC URLs as flags in op-version-check

--- op-chain-ops/cmd/op-version-check/main.go
+++ op-chain-ops/cmd/op-version-check/main.go
@@ -2,14 +2,9 @@ package main
 
 import (
 	"encoding/json"
-	"errors"
 	"fmt"
 	"os"
-	"slices"
 
-	"golang.org/x/exp/maps"
-
-	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/ethereum/go-ethereum/log"
 	"github.com/mattn/go-isatty"
 	"github.com/urfave/cli/v2"
@@ -38,20 +33,15 @@ func main() {
 		Name:  "op-version-check",
 		Usage: "Determine which contract versions are deployed for chains in a superchain",
 		Flags: []cli.Flag{
-			&cli.StringFlag{
-				Name:    "l1-rpc-url",
-				Value:   "http://127.0.0.1:8545",
-				Usage:   "L1 RPC URL, the chain ID will be used to determine the superchain",
+			&cli.StringSliceFlag{
+				Name:    "l1-rpc-urls",
+				Usage:   "L1 RPC URLs, the chain ID will be used to determine the superchain",
 				EnvVars: []string{"L1_RPC_URL"},
 			},
-			&cli.Uint64SliceFlag{
-				Name:  "chain-ids",
-				Usage: "L2 Chain IDs corresponding to chains to check versions for. Corresponds to all chains if empty",
-			},
-			&cli.StringFlag{
-				Name:    "superchain-target",
-				Usage:   "The name of the superchain",
-				EnvVars: []string{"SUPERCHAIN_TARGET"},
+			&cli.StringSliceFlag{
+				Name:    "l2-rpc-urls",
+				Usage:   "L2 RPC URLs, corresponding to chains to check versions for. Corresponds to all chains if empty",
+				EnvVars: []string{"L1_RPC_URL"},
 			},
 			&cli.PathFlag{
 				Name:    "outfile",
@@ -69,104 +59,82 @@ func main() {
 
 // entrypoint contains the main logic of the script
 func entrypoint(ctx *cli.Context) error {
-	client, err := ethclient.Dial(ctx.String("l1-rpc-url"))
-	if err != nil {
-		return err
-	}
-
-	// Fetch the L1 chain ID to determine the superchain name
-	l1ChainID, err := client.ChainID(ctx.Context)
-	if err != nil {
-		return err
-	}
-
-	superchainName := ctx.String("superchain-target")
-	if superchainName == "" {
-		superchainName, err = upgrades.ToSuperchainName(l1ChainID.Uint64())
-		if err != nil {
-			return err
-		}
-	}
-
-	chainIDs := ctx.Uint64Slice("chain-ids")
-
-	// If no chain IDs are specified, check all chains
-	if len(chainIDs) == 0 {
-		chainIDs = maps.Keys(superchain.OPChains)
-	}
-	slices.Sort(chainIDs)
-
-	targets := make([]*superchain.ChainConfig, 0)
-	for _, chainConfig := range superchain.OPChains {
-		if chainConfig.Superchain == superchainName && slices.Contains(chainIDs, chainConfig.ChainID) {
-			targets = append(targets, chainConfig)
-		} else {
-			log.Info("Skipping chain", "superchain", chainConfig.Superchain, "chainID", chainConfig.ChainID)
-		}
-	}
-
-	slices.SortFunc(targets, func(i, j *superchain.ChainConfig) int {
-		return int(i.ChainID) - int(j.ChainID)
-	})
+	l1RPCURLs := ctx.StringSlice("l1-rpc-urls")
+	l2RPCURLs := ctx.StringSlice("l2-rpc-urls")
 
 	output := []ChainVersionCheck{}
 
-	for _, chainConfig := range targets {
-		clients, err := clients.NewClients(ctx.String("l1-rpc-url"), chainConfig.PublicRPC)
-		if err != nil {
-			return fmt.Errorf("cannot create RPC clients: %w", err)
+	if len(l2RPCURLs) == 0 {
+		for _, chainConfig := range superchain.OPChains {
+			l2RPCURLs = append(l2RPCURLs, chainConfig.PublicRPC)
 		}
-		// The L1Client is required
-		if clients.L1Client == nil {
-			return errors.New("cannot create L1 client")
-		}
-
-		l1ChainID, err := clients.L1Client.ChainID(ctx.Context)
-		if err != nil {
-			return fmt.Errorf("cannot fetch L1 chain ID: %w", err)
-		}
-
-		// The L2Client is not required, but double check the chain id matches if possible
-		if clients.L2Client != nil {
-			l2ChainID, err := clients.L2Client.ChainID(ctx.Context)
-			if err != nil {
-				return fmt.Errorf("cannot fetch L2 chain ID: %w", err)
-			}
-			if chainConfig.ChainID != l2ChainID.Uint64() {
-				return fmt.Errorf("mismatched chain IDs: %d != %d", chainConfig.ChainID, l2ChainID)
-			}
-		}
-
-		log.Info(chainConfig.Name, "l1-chain-id", l1ChainID, "l2-chain-id", chainConfig.ChainID)
-
-		log.Info("Detecting on chain contracts")
-		// Tracking the individual addresses can be deprecated once the system is upgraded
-		// to the new contracts where the system config has a reference to each address.
-		addresses, ok := superchain.Addresses[chainConfig.ChainID]
-		if !ok {
-			return fmt.Errorf("no addresses for chain ID %d", chainConfig.ChainID)
-		}
-
-		versions, err := upgrades.GetContractVersions(ctx.Context, addresses, chainConfig, clients.L1Client)
-		if err != nil {
-			return fmt.Errorf("error getting contract versions: %w", err)
-		}
-
-		contracts := make(map[string]Contract)
-
-		contracts["AddressManager"] = Contract{Version: "null", Address: addresses.AddressManager}
-		contracts["L1CrossDomainMessenger"] = Contract{Version: versions.L1CrossDomainMessenger, Address: addresses.L1CrossDomainMessengerProxy}
-		contracts["L1ERC721Bridge"] = Contract{Version: versions.L1ERC721Bridge, Address: addresses.L1ERC721BridgeProxy}
-		contracts["L1StandardBridge"] = Contract{Version: versions.L1ERC721Bridge, Address: addresses.L1StandardBridgeProxy}
-		contracts["L2OutputOracle"] = Contract{Version: versions.L2OutputOracle, Address: addresses.L2OutputOracleProxy}
-		contracts["OptimismMintableERC20Factory"] = Contract{Version: versions.OptimismMintableERC20Factory, Address: addresses.OptimismMintableERC20FactoryProxy}
-		contracts["OptimismPortal"] = Contract{Version: versions.OptimismPortal, Address: addresses.OptimismPortalProxy}
-		contracts["SystemConfig"] = Contract{Version: versions.SystemConfig, Address: chainConfig.SystemConfigAddr}
-		contracts["ProxyAdmin"] = Contract{Version: "null", Address: addresses.ProxyAdmin}
-
-		output = append(output, ChainVersionCheck{Name: chainConfig.Name, ChainID: chainConfig.ChainID, Contracts: contracts})
 	}
 
+	for _, l1RPCURL := range l1RPCURLs {
+		for _, l2RPCURL := range l2RPCURLs {
+			clients, err := clients.NewClients(l1RPCURL, l2RPCURL)
+			if err != nil {
+				log.Warn("cannot create RPC clients", "err", err)
+				continue
+			}
+
+			// The L1Client is required
+			if clients.L1Client == nil {
+				log.Warn("cannot create L1 client")
+				continue
+			}
+
+			l1ChainID, err := clients.L1Client.ChainID(ctx.Context)
+			if err != nil {
+				log.Warn("cannot fetch L1 chain ID", "err", err)
+				continue
+			}
+
+			// The L2Client is not required, but double check the chain id matches if possible
+			if clients.L2Client == nil {
+				log.Warn("cannot create L2 client")
+				continue
+			}
+
+			l2ChainID, err := clients.L2Client.ChainID(ctx.Context)
+			if err != nil {
+				log.Warn("cannot fetch L2 chain ID", "err", err)
+				continue
+			}
+
+			chainConfig := superchain.OPChains[l2ChainID.Uint64()]
+
+			log.Info(chainConfig.Name, "l1-chain-id", l1ChainID, "l2-chain-id", chainConfig.ChainID)
+
+			log.Info("Detecting on chain contracts")
+			// Tracking the individual addresses can be deprecated once the system is upgraded
+			// to the new contracts where the system config has a reference to each address.
+			addresses, ok := superchain.Addresses[chainConfig.ChainID]
+			if !ok {
+				return fmt.Errorf("no addresses for chain ID %d", chainConfig.ChainID)
+			}
+			versions, err := upgrades.GetContractVersions(ctx.Context, addresses, chainConfig, clients.L1Client)
+			if err != nil {
+				//return fmt.Errorf("error getting contract versions: %w", err)
+				log.Warn("error getting contract versions", "err", err)
+				continue
+			}
+
+			contracts := make(map[string]Contract)
+
+			contracts["AddressManager"] = Contract{Version: "null", Address: addresses.AddressManager}
+			contracts["L1CrossDomainMessenger"] = Contract{Version: versions.L1CrossDomainMessenger, Address: addresses.L1CrossDomainMessengerProxy}
+			contracts["L1ERC721Bridge"] = Contract{Version: versions.L1ERC721Bridge, Address: addresses.L1ERC721BridgeProxy}
+			contracts["L1StandardBridge"] = Contract{Version: versions.L1ERC721Bridge, Address: addresses.L1StandardBridgeProxy}
+			contracts["L2OutputOracle"] = Contract{Version: versions.L2OutputOracle, Address: addresses.L2OutputOracleProxy}
+			contracts["OptimismMintableERC20Factory"] = Contract{Version: versions.OptimismMintableERC20Factory, Address: addresses.OptimismMintableERC20FactoryProxy}
+			contracts["OptimismPortal"] = Contract{Version: versions.OptimismPortal, Address: addresses.OptimismPortalProxy}
+			contracts["SystemConfig"] = Contract{Version: versions.SystemConfig, Address: chainConfig.SystemConfigAddr}
+			contracts["ProxyAdmin"] = Contract{Version: "null", Address: addresses.ProxyAdmin}
+
+			output = append(output, ChainVersionCheck{Name: chainConfig.Name, ChainID: chainConfig.ChainID, Contracts: contracts})
+		}
+	}
 	// Write contract versions to disk or stdout
 	if outfile := ctx.Path("outfile"); outfile != "" {
 		if err := jsonutil.WriteJSON(outfile, output); err != nil {
