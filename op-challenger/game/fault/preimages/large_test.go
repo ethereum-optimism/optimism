036850b8afe3c36c97b245beaf254b529eabed2a
refcell.eth <abigger87@gmail.com>
2024-01-18 08:17:37 -0800

    feat(op-challenger): Preimage Oracle Contract Calls (#9032)

--- op-challenger/game/fault/preimages/large_test.go
+++ op-challenger/game/fault/preimages/large_test.go
@@ -2,10 +2,15 @@ package preimages
 
 import (
 	"context"
+	"math/big"
 	"testing"
 
+	"github.com/ethereum-optimism/optimism/op-challenger/game/fault/contracts"
 	"github.com/ethereum-optimism/optimism/op-challenger/game/fault/types"
+	"github.com/ethereum-optimism/optimism/op-challenger/game/keccak/matrix"
 	"github.com/ethereum-optimism/optimism/op-service/testlog"
+	"github.com/ethereum-optimism/optimism/op-service/txmgr"
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/log"
 	"github.com/stretchr/testify/require"
 )
@@ -14,7 +19,7 @@ func TestLargePreimageUploader_UploadPreimage(t *testing.T) {
 	t.Run("Success", func(t *testing.T) {
 		oracle, _, _ := newTestLargePreimageUploader(t)
 		err := oracle.UploadPreimage(context.Background(), 0, &types.PreimageOracleData{})
-		// todo(proofs#467): fix this to not error. See LargePreimageUploader.UploadPreimage.
+		// TODO(proofs#467): fix this to not error. See LargePreimageUploader.UploadPreimage.
 		require.ErrorIs(t, err, errNotSupported)
 	})
 }
@@ -24,4 +29,16 @@ func newTestLargePreimageUploader(t *testing.T) (*LargePreimageUploader, *mockTx
 	txMgr := &mockTxMgr{}
 	contract := &mockPreimageOracleContract{}
 	return NewLargePreimageUploader(logger, txMgr, contract), txMgr, contract
+}
+
+type mockPreimageOracleContract struct{}
+
+func (s *mockPreimageOracleContract) InitLargePreimage(_ *big.Int, _ uint32, _ uint32) (txmgr.TxCandidate, error) {
+	return txmgr.TxCandidate{}, nil
+}
+func (s *mockPreimageOracleContract) AddLeaves(_ *big.Int, _ []contracts.Leaf, _ bool) ([]txmgr.TxCandidate, error) {
+	return []txmgr.TxCandidate{}, nil
+}
+func (s *mockPreimageOracleContract) Squeeze(_ common.Address, _ *big.Int, _ *matrix.StateMatrix, _ contracts.Leaf, _ contracts.MerkleProof, _ contracts.Leaf, _ contracts.MerkleProof) (txmgr.TxCandidate, error) {
+	return txmgr.TxCandidate{}, nil
 }
