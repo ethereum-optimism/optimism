20201b5cfa766b7ef6f1033a9f703cc21a58f7e4
Juan C <juanconrod@protonmail.com>
2024-01-24 23:49:40 +0100

    KontrolInterfaces: add interfaces for bridges

--- packages/contracts-bedrock/test/kontrol/proofs/interfaces/KontrolInterfaces.sol
+++ packages/contracts-bedrock/test/kontrol/proofs/interfaces/KontrolInterfaces.sol
@@ -31,9 +31,49 @@ interface ISuperchainConfig {
     function unpause() external;
 }
 
-interface IL1CrossDomainMessenger {
+interface ICrossDomainMessenger {
+    function xDomainMessageSender() external view returns (address);
+}
+
+interface IL1StandardBridge {
     function paused() external view returns (bool);
 
+    function MESSENGER() external view returns (ICrossDomainMessenger);
+
+    function OTHER_BRIDGE() external view returns (IL1StandardBridge);
+
+    function finalizeBridgeERC20(
+        address _localToken,
+        address _remoteToken,
+        address _from,
+        address _to,
+        uint256 _amount,
+        bytes calldata _extraData
+    )
+        external;
+
+    function finalizeBridgeETH(address _from, address _to, uint256 _amount, bytes calldata _extraData) external;
+}
+
+interface IL1ERC721Bridge {
+    function paused() external view returns (bool);
+
+    function MESSENGER() external view returns (ICrossDomainMessenger);
+
+    function OTHER_BRIDGE() external view returns (IL1StandardBridge);
+
+    function finalizeBridgeERC721(
+        address _localToken,
+        address _remoteToken,
+        address _from,
+        address _to,
+        uint256 _amount,
+        bytes calldata _extraData
+    )
+        external;
+}
+
+interface IL1CrossDomainMessenger {
     function relayMessage(
         uint256 _nonce,
         address _sender,
