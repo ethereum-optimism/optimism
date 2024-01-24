40def25447796f36fdda708317d7393babfc4752
Juan C <juanconrod@protonmail.com>
2024-01-24 23:50:04 +0100

    KontrolDeployment: add deployment for L1 bridges

--- packages/contracts-bedrock/test/kontrol/deployment/KontrolDeployment.sol
+++ packages/contracts-bedrock/test/kontrol/deployment/KontrolDeployment.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.0;
 
 import { Deploy } from "scripts/Deploy.s.sol";
+import { StandardBridge } from "src/universal/StandardBridge.sol";
 
 contract KontrolDeployment is Deploy {
     function runKontrolDeployment() public stateDiff {
@@ -12,7 +13,9 @@ contract KontrolDeployment is Deploy {
         deployERC1967Proxy("OptimismPortalProxy");
         deployERC1967Proxy("L2OutputOracleProxy");
         deployERC1967Proxy("SystemConfigProxy");
+        deployL1StandardBridgeProxy();
         deployL1CrossDomainMessengerProxy();
+        deployERC1967Proxy("L1ERC721BridgeProxy");
         transferAddressManagerOwnership(); // to the ProxyAdmin
 
         // deployImplementations();
@@ -20,9 +23,13 @@ contract KontrolDeployment is Deploy {
         deployL1CrossDomainMessenger();
         deployL2OutputOracle();
         deploySystemConfig();
+        deployL1StandardBridge();
+        deployL1ERC721Bridge();
 
         // initializeImplementations();
         initializeSystemConfig();
+        initializeL1StandardBridge();
+        initializeL1ERC721Bridge();
         initializeL1CrossDomainMessenger();
         initializeOptimismPortal();
     }
