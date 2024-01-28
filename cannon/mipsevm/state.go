ccc76bc37508cd6f9fd633c0f18081c73bb2dce1
zhiqiangxu <652732310@qq.com>
2024-01-29 05:41:12 +0800

    fix error msg (#9223)

--- cannon/mipsevm/state.go
+++ cannon/mipsevm/state.go
@@ -81,7 +81,7 @@ const (
 
 func (sw StateWitness) StateHash() (common.Hash, error) {
 	if len(sw) != 226 {
-		return common.Hash{}, fmt.Errorf("Invalid witness length. Got %d, expected at least 88", len(sw))
+		return common.Hash{}, fmt.Errorf("Invalid witness length. Got %d, expected 226", len(sw))
 	}
 
 	hash := crypto.Keccak256Hash(sw)
