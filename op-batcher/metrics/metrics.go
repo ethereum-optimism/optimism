bea0e273d96ed13e25184bedd2ae2e17213c92bb
Henri Devieux <henri.devieux@gmail.com>
2024-01-29 16:03:15 -0500

    Emit gauge for blob size

--- op-batcher/metrics/metrics.go
+++ op-batcher/metrics/metrics.go
@@ -46,6 +46,8 @@ type Metricer interface {
 	RecordBatchTxSuccess()
 	RecordBatchTxFailed()
 
+	RecordBlobUsedBytes(num int)
+
 	Document() []opmetrics.DocumentedMetric
 }
 
@@ -79,6 +81,8 @@ type Metrics struct {
 	channelOutputBytesTotal prometheus.Counter
 
 	batcherTxEvs opmetrics.EventVec
+
+	blobUsedBytes prometheus.Gauge
 }
 
 var _ Metricer = (*Metrics)(nil)
@@ -180,6 +184,11 @@ func NewMetrics(procName string) *Metrics {
 			Namespace: ns,
 			Name:      "output_bytes_total",
 			Help:      "Total number of compressed output bytes from a channel.",
+		}),
+		blobUsedBytes: factory.NewGauge(prometheus.GaugeOpts{
+			Namespace: ns,
+			Name:      "blob_used_bytes",
+			Help:      "Blob size in bytes being submitted.",
 		}),
 
 		batcherTxEvs: opmetrics.NewEventVec(factory, ns, "", "batcher_tx", "BatcherTx", []string{"stage"}),
@@ -302,6 +311,10 @@ func (m *Metrics) RecordBatchTxSuccess() {
 
 func (m *Metrics) RecordBatchTxFailed() {
 	m.batcherTxEvs.Record(TxStageFailed)
+}
+
+func (m *Metrics) RecordBlobUsedBytes(num int) {
+	m.blobUsedBytes.Set(float64(num))
 }
 
 // estimateBatchSize estimates the size of the batch
