dbd5e37187dbc64046482f506a338f6bc40c1b59
Francis Li <francis.li@coinbase.com>
2024-01-31 12:50:32 -0800

    Fix shell to pass CI check (#9278)

--- op-conductor/run_test_100times.sh
+++ op-conductor/run_test_100times.sh
@@ -1,10 +1,11 @@
+#!/bin/bash
+
 set -e
 
 for i in {1..100}; do
   echo "======================="
   echo "Running iteration $i"
-  gotestsum -- -run 'TestControlLoop' ./... --count=1 --timeout=5s -race
-  if [ $? -ne 0 ]; then
+  if ! gotestsum -- -run 'TestControlLoop' ./... --count=1 --timeout=5s -race; then
     echo "Test failed"
     exit 1
   fi
