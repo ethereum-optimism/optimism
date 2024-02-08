c61e08093b8c2efc71fdddfa0b37d1a03fc63092
Wyatt Barnes <me@wyatt.email>
2024-02-07 14:42:20 -1000

    Expected Foundry checks and install script updates

--- packages/contracts-bedrock/scripts/getting-started/versions.sh
+++ packages/contracts-bedrock/scripts/getting-started/versions.sh
@@ -14,23 +14,37 @@ version() {
   fi
 }
 
+versionFoundry() {
+  local string="$1"
+  local version_regex='forge ([0-9]+\.[0-9]+\.[0-9]+)'
+  local commit_hash_regex='\(([a-fA-F0-9]+)'
+  local full_regex="${version_regex} ${commit_hash_regex}"
+
+  if [[ $string =~ $full_regex ]]; then
+    echo "${BASH_REMATCH[1]} (${BASH_REMATCH[2]})"
+  else
+    echo "No version, commit hash, and timestamp found."
+  fi
+}
+
+
 # Grab versions
 ver_git=$(version "$(git --version)")
 ver_go=$(version "$(go version)")
 ver_node=$(version "$(node --version)")
 ver_pnpm=$(version "$(pnpm --version)")
-ver_foundry=$(version "$(forge --version)")
+ver_foundry=$(versionFoundry "$(forge --version)")
 ver_make=$(version "$(make --version)")
 ver_jq=$(version "$(jq --version)")
 ver_direnv=$(version "$(direnv --version)")
 
 # Print versions
-echo "Dependency | Minimum | Actual"
-echo "git          2         $ver_git"
-echo "go           1.21      $ver_go"
-echo "node         20        $ver_node"
-echo "pnpm         8         $ver_pnpm"
-echo "foundry      0.2.0     $ver_foundry"
-echo "make         3         $ver_make"
-echo "jq           1.6       $ver_jq"
-echo "direnv       2         $ver_direnv"
+echo "Dependency | Minimum         | Actual"
+echo "git          2                $ver_git"
+echo "go           1.21             $ver_go"
+echo "node         20               $ver_node"
+echo "pnpm         8                $ver_pnpm"
+echo "foundry      0.2.0 (a5efe4f)  $ver_foundry"
+echo "make         3                $ver_make"
+echo "jq           1.6              $ver_jq"
+echo "direnv       2                $ver_direnv"
