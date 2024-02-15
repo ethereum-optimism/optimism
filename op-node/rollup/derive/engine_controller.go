baf3f624c88fccdd98abec29a39aaf6ac91f4ab3
Axel Kingsley <axel.kingsley@gmail.com>
2024-02-15 00:56:41 -0600

    add additional check (#9560)

--- op-node/rollup/derive/engine_controller.go
+++ op-node/rollup/derive/engine_controller.go
@@ -172,7 +172,7 @@ func (e *EngineController) StartPayload(ctx context.Context, parent eth.L2BlockR
 }
 
 func (e *EngineController) ConfirmPayload(ctx context.Context, agossip async.AsyncGossiper, sequencerConductor conductor.SequencerConductor) (out *eth.ExecutionPayloadEnvelope, errTyp BlockInsertionErrType, err error) {
-	if e.buildingID == (eth.PayloadID{}) {
+	if e.buildingID == (eth.PayloadID{}) && agossip.Get() == nil {
 		return nil, BlockInsertPrestateErr, fmt.Errorf("cannot complete payload building: not currently building a payload")
 	}
 	if e.buildingOnto.Hash != e.unsafeHead.Hash { // E.g. when safe-attributes consolidation fails, it will drop the existing work.
