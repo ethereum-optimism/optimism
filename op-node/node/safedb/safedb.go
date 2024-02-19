2a4363e604666c59eef867e26f99d2d38b6234fd
Adrian Sutton <adrian@oplabs.co>
2024-02-20 09:57:38 +1000

    op-node: Improve thread safety of safedb

--- op-node/node/safedb/safedb.go
+++ op-node/node/safedb/safedb.go
@@ -6,7 +6,7 @@ import (
 	"errors"
 	"fmt"
 	"math"
-	"sync/atomic"
+	"sync"
 
 	"github.com/cockroachdb/pebble"
 	"github.com/ethereum-optimism/optimism/op-service/eth"
@@ -20,12 +20,15 @@ var (
 )
 
 type SafeDB struct {
+	// m ensures all read iterators are closed before closing the database by preventing concurrent read and write
+	// operations (with close considered a write operation).
+	m   *sync.RWMutex
 	log log.Logger
 	db  *pebble.DB
 
 	writeOpts *pebble.WriteOptions
 
-	closed atomic.Bool
+	closed bool
 }
 
 func KeyL1BlockNum(num uint64) []byte {
@@ -65,23 +68,32 @@ func NewSafeDB(logger log.Logger, path string) (*SafeDB, error) {
 }
 
 func (d *SafeDB) SafeHeadUpdated(safeHead eth.L2BlockRef, l1Head eth.BlockID) error {
-	d.log.Debug("Update safe head", "l2", safeHead.ID(), "l1", l1Head)
+	d.m.Lock()
+	defer d.m.Unlock()
+	d.log.Info("Update safe head", "l2", safeHead.ID(), "l1", l1Head)
+	batch := d.db.NewBatch()
+	defer batch.Close()
 	// Delete any entries after this L1 block. Normally the l1Head continuously increases and this does nothing
 	// However when the pipeline resets the L1 head may drop back and we need to remove later entries and allow them
 	// to be repopulated as derivation progresses again. The resulting data may be different if L1 reorged.
-	if err := d.db.DeleteRange(KeyL1BlockNum(l1Head.Number+1), KeyL1BlockNum(math.MaxUint64), d.writeOpts); err != nil {
+	if err := batch.DeleteRange(KeyL1BlockNum(l1Head.Number+1), KeyL1BlockNum(math.MaxUint64), d.writeOpts); err != nil {
 		return fmt.Errorf("failed to truncate safe head entries: %w", err)
 	}
-	if err := d.db.Set(KeyL1BlockNum(l1Head.Number), ValueL1BlockNum(l1Head.Hash, safeHead.Hash), d.writeOpts); err != nil {
+	if err := batch.Set(KeyL1BlockNum(l1Head.Number), ValueL1BlockNum(l1Head.Hash, safeHead.Hash), d.writeOpts); err != nil {
 		// TODO(client-pod#593): Add tests to ensure we don't lose data here
 		// We do in fact lose this update here. Even if we didn't the correct behaviour is to retry the exact same write
 		// so maybe we should just keep retrying here instead of returning an error?
 		return fmt.Errorf("failed to record safe head update: %w", err)
 	}
+	if err := batch.Commit(d.writeOpts); err != nil {
+		return fmt.Errorf("failed to commit safe head update: %w", err)
+	}
 	return nil
 }
 
 func (d *SafeDB) SafeHeadAtL1(ctx context.Context, l1BlockNum uint64) (l1Hash common.Hash, l2Hash common.Hash, err error) {
+	d.m.RLock()
+	defer d.m.RUnlock()
 	iter, err := d.db.NewIterWithContext(ctx, &pebble.IterOptions{
 		LowerBound: KeyL1BlockNum(0),
 		UpperBound: KeyL1BlockNum(math.MaxUint64),
@@ -104,9 +116,12 @@ func (d *SafeDB) SafeHeadAtL1(ctx context.Context, l1BlockNum uint64) (l1Hash co
 }
 
 func (d *SafeDB) Close() error {
-	if !d.closed.CompareAndSwap(false, true) {
+	d.m.Lock()
+	defer d.m.Unlock()
+	if d.closed {
 		// Already closed
 		return nil
 	}
+	d.closed = true
 	return d.db.Close()
 }
