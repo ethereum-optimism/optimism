7359a1bf4e5baed4a7505333ed1c09f699294b0a
Diego <105765223+0xfuturistic@users.noreply.github.com>
2024-02-21 20:10:14 +0100

    contracts-bedrock: refactor L2ToL2CrossDomainMessenger

--- packages/contracts-bedrock/src/L2/L2ToL2CrossDomainMessenger.sol
+++ packages/contracts-bedrock/src/L2/L2ToL2CrossDomainMessenger.sol
@@ -1,31 +1,63 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import { Initializable } from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import { SafeCall } from "src/libraries/SafeCall.sol";
 import { CrossL2Inbox } from "src/L2/CrossL2Inbox.sol";
 import { Encoding } from "src/libraries/Encoding.sol";
 
 /// @custom:upgradeable
 /// @title L2ToL2CrossDomainMessenger
-abstract contract L2ToL2CrossDomainMessenger is Initializable, CrossL2Inbox {
-    uint16 public constant MESSAGE_VERSION = 1;
-    uint240 public nonce;
-
-    mapping(bytes32 => bool) public sentMessages;
-
-    // bytes32(uint256(keccak256("l2tol2crossdomainmessenger.sender")) - 1)
-    bytes32 internal constant CROSS_DOMAIN_MESSAGE_SENDER_SLOT =
+contract L2ToL2CrossDomainMessenger {
+    /// @notice Slot for the sender of the current cross domain message.
+    /// @dev Equal to bytes32(uint256(keccak256("l2tol2crossdomainmessenger.sender")) - 1)
+    bytes32 public constant CROSS_DOMAIN_MESSAGE_SENDER_SLOT =
         0xb83444d07072b122e2e72a669ce32857d892345c19856f4e7142d06a167ab3f3;
 
-    CrossL2Inbox public constant CROSS_L2_INBOX = CrossL2Inbox(0x4200000000000000000000000000000000000022);
+    /// @notice Current message version identifier.
+    uint16 public constant MESSAGE_VERSION = uint16(0);
 
-    event SentMessage(bytes message) anonymous;
+    /// @notice Initial balance for the contract.
+    uint248 public constant INITIAL_BALANCE = type(uint248).max;
 
-    function messageNonce() public view returns (uint256) {
-        return Encoding.encodeVersionedNonce(nonce, MESSAGE_VERSION);
+    /// @notice Contract of the L2 Cross Domain Messenger on this chain.
+    CrossL2Inbox public immutable CROSS_L2_INBOX;
+
+    /// @notice Mapping of message hashes to boolean receipt values. Note that a message will only
+    ///         be present in this mapping if it has successfully been relayed on this chain, and
+    ///         can therefore not be relayed again.
+    mapping(bytes32 => bool) public successfulMessages;
+
+    /// @notice Nonce for the next message to be sent, without the message version applied. Use the
+    ///         messageNonce getter which will insert the message version into the nonce to give you
+    ///         the actual nonce to be used for the message.
+    uint240 internal msgNonce;
+
+    /// @notice Emitted whenever a message is sent to the other chain.
+    /// @param destination Chain ID of the destination chain.
+    /// @param target      Target contract or wallet address.
+    /// @param message     Message to trigger the target address with.
+    /// @param data        Data to be sent with the message.
+    event SentMessage(uint256 destination, address target, bytes message, bytes data) anonymous;
+
+    constructor(address _crossDomainMessenger) {
+        CROSS_L2_INBOX = CrossL2Inbox(_crossDomainMessenger);
     }
 
+    /// @notice Retrieves the next message nonce. Message version will be added to the upper two
+    ///         bytes of the message nonce. Message version allows us to treat messages as having
+    ///         different structures.
+    /// @return Nonce of the next message to be sent, with added message version.
+    function messageNonce() public view returns (uint256) {
+        return Encoding.encodeVersionedNonce(msgNonce, MESSAGE_VERSION);
+    }
+
+    /// @notice Sends a message to some target address on a destination chain. Note that if the call
+    ///         always reverts, then the message will be unrelayable, and any ETH sent will be
+    ///         permanently locked. The same will occur if the target on the other chain is
+    ///         considered unsafe (see the _isUnsafeTarget() function).
+    /// @param _destination Chain ID of the destination chain.
+    /// @param _target      Target contract or wallet address.
+    /// @param _message     Message to trigger the target address with.
     function sendMessage(uint256 _destination, address _target, bytes calldata _message) external payable {
         require(_destination != block.chainid);
 
@@ -33,10 +65,19 @@ abstract contract L2ToL2CrossDomainMessenger is Initializable, CrossL2Inbox {
             L2ToL2CrossDomainMessenger.relayMessage,
             (_destination, messageNonce(), msg.sender, _target, msg.value, _message)
         );
-        emit SentMessage(data);
-        nonce++;
+        emit SentMessage(_destination, _target, _message, data);
+        msgNonce++;
     }
 
+    /// @notice Relays a message that was sent by the other CrossDomainMessenger contract. Can only
+    ///         be executed via cross-chain call from the other messenger OR if the message was
+    ///         already received once and is currently being replayed.
+    /// @param _destination Chain ID of the destination chain.
+    /// @param _nonce       Nonce of the message being relayed.
+    /// @param _sender      Address of the user who sent the message.
+    /// @param _target      Address that the message is targeted at.
+    /// @param _value       ETH value to send with the message.
+    /// @param _message     Message to send to the target.
     function relayMessage(
         uint256 _destination,
         uint256 _nonce,
@@ -48,12 +89,12 @@ abstract contract L2ToL2CrossDomainMessenger is Initializable, CrossL2Inbox {
         external
     {
         require(msg.sender == address(CROSS_L2_INBOX));
-        require(_destination == block.chainid);
         require(CROSS_L2_INBOX.origin() == address(this));
+        require(_destination == block.chainid);
         require(_target != address(this));
 
         bytes32 messageHash = keccak256(abi.encode(_destination, _nonce, _sender, _target, _value, _message));
-        require(sentMessages[messageHash] == false);
+        require(successfulMessages[messageHash] == false);
 
         assembly {
             tstore(CROSS_DOMAIN_MESSAGE_SENDER_SLOT, _sender)
@@ -63,6 +104,6 @@ abstract contract L2ToL2CrossDomainMessenger is Initializable, CrossL2Inbox {
 
         require(success);
 
-        sentMessages[messageHash] = true;
+        successfulMessages[messageHash] = true;
     }
 }
