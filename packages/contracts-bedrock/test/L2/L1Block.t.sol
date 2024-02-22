ff3c0c862434d71dae28c131fe51779ee4d6a618
Diego <105765223+0xfuturistic@users.noreply.github.com>
2024-02-22 15:10:14 +0100

    contracts-bedrock: add interop tests to L1Block

--- packages/contracts-bedrock/test/L2/L1Block.t.sol
+++ packages/contracts-bedrock/test/L2/L1Block.t.sol
@@ -36,6 +36,8 @@ contract L1BlockBedrock_Test is L1BlockTest {
     )
         external
     {
+        vm.assume(isz == cis.length);
+
         vm.prank(depositor);
         l1Block.setL1BlockValues(n, t, b, h, s, bt, fo, fs, isz, cis);
         assertEq(l1Block.number(), n);
@@ -154,5 +156,148 @@ contract L1BlockEcotone_Test is L1BlockTest {
         // make sure return value is the expected function selector for "NotDepositor()"
         bytes memory expReturn = hex"3cc50b45";
         assertEq(data, expReturn);
+    }
+}
+
+contract L1BlockInterop_Test is L1BlockTest {
+    /// @dev Tests that setL1BlockValuesInterop updates the values appropriately.
+    function testFuzz_setL1BlockValuesInterop_succeeds(
+        uint32 baseFeeScalar,
+        uint32 blobBaseFeeScalar,
+        uint64 sequenceNumber,
+        uint64 timestamp,
+        uint64 number,
+        uint256 baseFee,
+        uint256 blobBaseFee,
+        bytes32 hash,
+        bytes32 batcherHash,
+        uint8 interopSetSize,
+        uint256[] calldata chainIds
+    )
+        external
+    {
+        vm.assume(interopSetSize == chainIds.length);
+
+        bytes memory functionCallDataPacked = Encoding.encodeSetL1BlockValuesEcotone(
+            baseFeeScalar, blobBaseFeeScalar, sequenceNumber, timestamp, number, baseFee, blobBaseFee, hash, batcherHash
+        );
+
+        vm.prank(depositor);
+        (bool success,) = address(l1Block).call(functionCallDataPacked);
+        assertTrue(success, "Function call failed");
+
+        functionCallDataPacked = Encoding.encodeSetL1BlockValuesInterop(
+            baseFeeScalar,
+            blobBaseFeeScalar,
+            sequenceNumber,
+            timestamp,
+            number,
+            baseFee,
+            blobBaseFee,
+            hash,
+            batcherHash,
+            interopSetSize,
+            chainIds
+        );
+
+        vm.prank(depositor);
+        (success,) = address(l1Block).call(functionCallDataPacked);
+        assertTrue(success, "Function call failed");
+
+        assertEq(l1Block.baseFeeScalar(), baseFeeScalar);
+        assertEq(l1Block.blobBaseFeeScalar(), blobBaseFeeScalar);
+        assertEq(l1Block.sequenceNumber(), sequenceNumber);
+        assertEq(l1Block.timestamp(), timestamp);
+        assertEq(l1Block.number(), number);
+        assertEq(l1Block.basefee(), baseFee);
+        assertEq(l1Block.blobBaseFee(), blobBaseFee);
+        assertEq(l1Block.hash(), hash);
+        assertEq(l1Block.batcherHash(), batcherHash);
+        assertEq(l1Block.interopSetSize(), interopSetSize);
+        for (uint256 i = 0; i < chainIds.length; i++) {
+            assertEq(l1Block.chainIds(i), chainIds[i]);
+        }
+
+        // ensure we didn't accidentally pollute the 128 bits of the sequencenum+scalars slot that
+        // should be empty
+        bytes32 scalarsSlot = vm.load(address(l1Block), bytes32(uint256(3)));
+        bytes32 mask128 = hex"FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF00000000000000000000000000000000";
+
+        assertEq(0, scalarsSlot & mask128);
+
+        // ensure we didn't accidentally pollute the 128 bits of the number & timestamp slot that
+        // should be empty
+        bytes32 numberTimestampSlot = vm.load(address(l1Block), bytes32(uint256(0)));
+        assertEq(0, numberTimestampSlot & mask128);
+    }
+
+    /// @dev Tests that `setL1BlockValuesInterop` succeeds if sender address is the depositor
+    function test_setL1BlockValuesInterop_isDepositor_succeeds() external {
+        bytes memory functionCallDataPacked = Encoding.encodeSetL1BlockValuesInterop(
+            type(uint32).max,
+            type(uint32).max,
+            type(uint64).max,
+            type(uint64).max,
+            type(uint64).max,
+            type(uint256).max,
+            type(uint256).max,
+            bytes32(type(uint256).max),
+            bytes32(type(uint256).max),
+            0,
+            new uint256[](0)
+        );
+
+        vm.prank(depositor);
+        (bool success,) = address(l1Block).call(functionCallDataPacked);
+        assertTrue(success, "function call failed");
+    }
+
+    /// @dev Tests that `setL1BlockValuesInterop` fails if sender address is not the depositor
+    function test_setL1BlockValuesInterop_notDepositor_fails() external {
+        bytes memory functionCallDataPacked = Encoding.encodeSetL1BlockValuesInterop(
+            type(uint32).max,
+            type(uint32).max,
+            type(uint64).max,
+            type(uint64).max,
+            type(uint64).max,
+            type(uint256).max,
+            type(uint256).max,
+            bytes32(type(uint256).max),
+            bytes32(type(uint256).max),
+            0,
+            new uint256[](0)
+        );
+
+        (bool success, bytes memory data) = address(l1Block).call(functionCallDataPacked);
+        assertTrue(!success, "function call should have failed");
+        // make sure return value is the expected function selector for "NotDepositor()"
+        bytes memory expReturn = hex"3cc50b45";
+        assertEq(data, expReturn);
+    }
+
+    function testFuzz_isInDependencySet_succeeds(uint256[] calldata chainIds) external {
+        vm.prank(depositor);
+        l1Block.setL1BlockValues(0, 0, 0, bytes32(0), 0, bytes32(0), 0, 0, uint8(chainIds.length), chainIds);
+        for (uint256 i = 0; i < chainIds.length; i++) {
+            assertTrue(l1Block.isInDependencySet(chainIds[i]));
+        }
+    }
+
+    function testFuzz_isInDependencySet_isChainId_succeeds() external {
+        assertTrue(l1Block.isInDependencySet(block.chainid));
+    }
+
+    function test_isInDependencySet_fails() external {
+        uint256[] memory chainIds = new uint256[](2);
+        chainIds[0] = 1;
+        chainIds[1] = 2;
+        vm.prank(depositor);
+        l1Block.setL1BlockValues(0, 0, 0, bytes32(0), 0, bytes32(0), 0, 0, uint8(chainIds.length), chainIds);
+        assertFalse(l1Block.isInDependencySet(3));
+    }
+
+    function test_isInDependencySet_isDependencySetEmpty_fails() external {
+        assertTrue(l1Block.interopSetSize() == 0);
+        assertFalse(l1Block.isInDependencySet(1));
     }
 }
