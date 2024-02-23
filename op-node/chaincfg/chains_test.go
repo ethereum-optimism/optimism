84f8cdb33a5ebca981f0cd5030650ba8199d20bc
cby3149 <cby3149@gmail.com>
2024-02-22 17:06:03 -0800

    Fix op-node test

--- op-node/chaincfg/chains_test.go
+++ op-node/chaincfg/chains_test.go
@@ -21,9 +21,8 @@ import (
 // the superchain-registry is no longer deemed experimental.
 func TestGetRollupConfig(t *testing.T) {
 	configsByName := map[string]rollup.Config{
-		"goerli": goerliCfg,
-		// TODO(jky) unskip this test once superchain registry updated
-		// "mainnet": mainnetCfg,
+		"goerli":  goerliCfg,
+		"mainnet": mainnetCfg,
 		"sepolia": sepoliaCfg,
 	}
 	_ = mainnetCfg
@@ -132,9 +131,9 @@ var sepoliaCfg = rollup.Config{
 	DepositContractAddress:  common.HexToAddress("0x16fc5058f25648194471939df75cf27a2fdc48bc"),
 	L1SystemConfigAddress:   common.HexToAddress("0x034edd2a225f7f429a63e0f1d2084b9e0a93b538"),
 	RegolithTime:            u64Ptr(0),
-	CanyonTime:              u64Ptr(1699981200),
-	DeltaTime:               u64Ptr(1703203200),
-	EcotoneTime:             u64Ptr(1708534800),
+	CanyonTime:              u64Ptr(1705600788),
+	DeltaTime:               u64Ptr(1709078400),
+	EcotoneTime:             u64Ptr(1709078400),
 	ProtocolVersionsAddress: common.HexToAddress("0x79ADD5713B383DAa0a138d3C4780C7A1804a8090"),
 }
 
