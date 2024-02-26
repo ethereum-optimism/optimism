e93bda08cadb127b22ea08efd7a4d2162e538fa1
Adrian Sutton <adrian@oplabs.co>
2024-02-27 02:22:00 +1000

    op-e2e: Update the exhaustive alphabet e2e test to handle duplicates (#9655)
    The new challenger algorithm may post some claims that the exhaustive test was posting.

--- op-e2e/faultproofs/output_alphabet_test.go
+++ op-e2e/faultproofs/output_alphabet_test.go
@@ -103,8 +103,6 @@ func TestOutputAlphabetGame_ValidOutputRoot(t *testing.T) {
 }
 
 func TestChallengerCompleteExhaustiveDisputeGame(t *testing.T) {
-	// TODO(client-pod#103): Update ExhaustDishonestClaims to not fail if claim it tried to post exists
-	t.Skip("Challenger performs many more moves now creating conflicts")
 	op_e2e.InitParallel(t)
 
 	testCase := func(t *testing.T, isRootCorrect bool) {
@@ -205,17 +203,17 @@ func TestOutputAlphabetGame_FreeloaderEarnsNothing(t *testing.T) {
 	// dishonest
 	dishonest := correctTrace.AttackClaim(ctx, claim)
 
-	freeloaders = append(freeloaders, correctTrace.AttackClaimWithTransactOpts(ctx, dishonest, freeloaderOpts))
-	freeloaders = append(freeloaders, dishonest.AttackWithTransactOpts(ctx, common.Hash{0x02}, freeloaderOpts))
-	freeloaders = append(freeloaders, dishonest.DefendWithTransactOpts(ctx, common.Hash{0x03}, freeloaderOpts))
+	freeloaders = append(freeloaders, correctTrace.AttackClaim(ctx, dishonest, disputegame.WithTransactOpts(freeloaderOpts)))
+	freeloaders = append(freeloaders, dishonest.Attack(ctx, common.Hash{0x02}, disputegame.WithTransactOpts(freeloaderOpts)))
+	freeloaders = append(freeloaders, dishonest.Defend(ctx, common.Hash{0x03}, disputegame.WithTransactOpts(freeloaderOpts)))
 
 	// Ensure freeloaders respond before the honest challenger
 	game.StartChallenger(ctx, "sequencer", "Challenger", challenger.WithPrivKey(sys.Cfg.Secrets.Alice))
 	dishonest.WaitForCounterClaim(ctx, freeloaders...)
 
 	// Freeloaders after the honest challenger
-	freeloaders = append(freeloaders, dishonest.AttackWithTransactOpts(ctx, common.Hash{0x04}, freeloaderOpts))
-	freeloaders = append(freeloaders, dishonest.DefendWithTransactOpts(ctx, common.Hash{0x05}, freeloaderOpts))
+	freeloaders = append(freeloaders, dishonest.Attack(ctx, common.Hash{0x04}, disputegame.WithTransactOpts(freeloaderOpts)))
+	freeloaders = append(freeloaders, dishonest.Defend(ctx, common.Hash{0x05}, disputegame.WithTransactOpts(freeloaderOpts)))
 
 	for _, freeloader := range freeloaders {
 		if freeloader.IsMaxDepth(ctx) {
