0b7d0e51e982c4c539dd49cdf23a5a7c33e5cc03
rac-sri <rac.sri25@gmail.com>
2024-02-29 02:20:26 +0530

    chore: seperate the common error codes

--- op-plasma/da_interface.go
+++ op-plasma/da_interface.go
@@ -1,8 +1,20 @@
 package plasma
 
-import "context"
+import (
+	"context"
+	"errors"
+)
 
 type DataClient interface {
 	GetInput(ctx context.Context, key []byte) ([]byte, error)
 	SetInput(ctx context.Context, img []byte) ([]byte, error)
 }
+
+// ErrNotFound is returned when the server could not find the input.
+var ErrNotFound = errors.New("not found")
+
+// ErrCommitmentMismatch is returned when the server returns the wrong input for the given commitment.
+var ErrCommitmentMismatch = errors.New("commitment mismatch")
+
+// ErrInvalidInput is returned when the input is not valid for posting to the DA storage.
+var ErrInvalidInput = errors.New("invalid input")
