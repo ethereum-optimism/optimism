3b5c2fc666c5c10ccd2425b1496f2405ccc59b6c
felipe <130432649+felipe-op@users.noreply.github.com>
2024-02-29 16:23:34 -0800

    feat(op-node): clean peer state when disconnectPeer is called and log intercept blocks (#9706)
    * feat(op-node): clean peer state when disconnectPeer is called and log intercept blocks
    
    * nit

--- op-node/p2p/gating/scoring.go
+++ op-node/p2p/gating/scoring.go
@@ -1,6 +1,7 @@
 package gating
 
 import (
+	"github.com/ethereum/go-ethereum/log"
 	"github.com/libp2p/go-libp2p/core/network"
 	"github.com/libp2p/go-libp2p/core/peer"
 	"github.com/multiformats/go-multiaddr"
@@ -22,22 +23,43 @@ func AddScoring(gater BlockingConnectionGater, scores Scores, minScore float64) 
 	return &ScoringConnectionGater{BlockingConnectionGater: gater, scores: scores, minScore: minScore}
 }
 
-func (g *ScoringConnectionGater) checkScore(p peer.ID) (allow bool) {
+func (g *ScoringConnectionGater) checkScore(p peer.ID) (allow bool, score float64) {
 	score, err := g.scores.GetPeerScore(p)
 	if err != nil {
-		return false
+		return false, score
 	}
-	return score >= g.minScore
+	return score >= g.minScore, score
 }
 
 func (g *ScoringConnectionGater) InterceptPeerDial(p peer.ID) (allow bool) {
-	return g.BlockingConnectionGater.InterceptPeerDial(p) && g.checkScore(p)
+	if !g.BlockingConnectionGater.InterceptPeerDial(p) {
+		return false
+	}
+	check, score := g.checkScore(p)
+	if !check {
+		log.Warn("peer has failed checkScore", "peer_id", p, "score", score, "min_score", g.minScore)
+	}
+	return check
 }
 
 func (g *ScoringConnectionGater) InterceptAddrDial(id peer.ID, ma multiaddr.Multiaddr) (allow bool) {
-	return g.BlockingConnectionGater.InterceptAddrDial(id, ma) && g.checkScore(id)
+	if !g.BlockingConnectionGater.InterceptAddrDial(id, ma) {
+		return false
+	}
+	check, score := g.checkScore(id)
+	if !check {
+		log.Warn("peer has failed checkScore", "peer_id", id, "score", score, "min_score", g.minScore)
+	}
+	return check
 }
 
 func (g *ScoringConnectionGater) InterceptSecured(dir network.Direction, id peer.ID, mas network.ConnMultiaddrs) (allow bool) {
-	return g.BlockingConnectionGater.InterceptSecured(dir, id, mas) && g.checkScore(id)
+	if !g.BlockingConnectionGater.InterceptSecured(dir, id, mas) {
+		return false
+	}
+	check, score := g.checkScore(id)
+	if !check {
+		log.Warn("peer has failed checkScore", "peer_id", id, "score", score, "min_score", g.minScore)
+	}
+	return check
 }
