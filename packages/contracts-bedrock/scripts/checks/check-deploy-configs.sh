5b23c3d1d90d9112bb59492df45735c2e93b4484
smartcontracts <kelvin@optimism.io>
2024-03-05 10:37:13 -0500

    maint(ct): minor contracts script cleanup (#9724)
    Just some minor cleanup and reorganization in the script folder.
    Wanted to tidy up all the different scripts we have.

--- packages/contracts-bedrock/scripts/checks/check-deploy-configs.sh
+++ packages/contracts-bedrock/scripts/checks/check-deploy-configs.sh
@@ -7,7 +7,7 @@
 code=0
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-CONTRACTS_BASE=$(dirname "$SCRIPT_DIR")
+CONTRACTS_BASE=$(dirname "$(dirname "$SCRIPT_DIR")")
 MONOREPO_BASE=$(dirname "$(dirname "$CONTRACTS_BASE")")
 
 for config in "$CONTRACTS_BASE"/deploy-config/*.json; do
