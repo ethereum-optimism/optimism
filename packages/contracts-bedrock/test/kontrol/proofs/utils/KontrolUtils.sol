98ae98b95a9d00ef595d1e080e832d4fba1c457d
Juan C <juanconrod@protonmail.com>
2024-03-06 13:56:11 +0100

    KontrolUtils: add documentation comment

--- packages/contracts-bedrock/test/kontrol/proofs/utils/KontrolUtils.sol
+++ packages/contracts-bedrock/test/kontrol/proofs/utils/KontrolUtils.sol
@@ -1,4 +1,15 @@
 // SPDX-License-Identifier: MIT
+
+// This file exists to provide the `internal constant vm` on top of `KontrolCheats`.
+// The reason for explicitly defining `vm` here instead of inheriting Forge's `Test`
+// contract is the K summary of the `copy_memory_to_memory` function.
+// This summary dependent on the bytecode of the test contract, which means that if `Test`
+// was inherited, updating the version of `Test` could potentially imply having to adjust
+// said summary for the latest version, introducing a flakiness source.
+// Note that starting with version 0.8.24, the opcode `MCOPY` is introduced, removing the
+// need for the `copy_memory_to_memory` function and its summary, and thus this workaroud.
+// For more information refer to the `copy_memory_to_memory` summary section of `pausability-lemmas.md`.
+
 pragma solidity 0.8.15;
 
 import { Vm } from "forge-std/Vm.sol";
