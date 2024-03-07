2d66e2cd2cb54f53d01b0857ca56c2040252ca2a
Diego <105765223+0xfuturistic@users.noreply.github.com>
2024-03-07 13:40:28 -0800

    contracts-bedrock: drop unnecessary test for L2ToL2CrossDomainMessenger

--- packages/contracts-bedrock/test/L2/L2ToL2CrossDomainMessenger.t.sol
+++ packages/contracts-bedrock/test/L2/L2ToL2CrossDomainMessenger.t.sol
@@ -23,8 +23,6 @@ contract L2ToL2CrossDomainMessengerTest is Bridge_Initializer {
     /// @dev Tests that the implementation is initialized correctly.
     function test_constructor_succeeds() external {
         assertEq(l2ToL2CrossDomainMessenger.MESSAGE_VERSION(), uint16(0));
-        assertEq(l2ToL2CrossDomainMessenger.INITIAL_BALANCE(), type(uint248).max);
-        assertEq(l2ToL2CrossDomainMessenger.crossL2Inbox(), address(crossL2Inbox));
         assertEq(l2ToL2CrossDomainMessenger.messageNonce(), uint256(0));
     }
 
@@ -90,65 +88,5 @@ contract L2ToL2CrossDomainMessengerTest is Bridge_Initializer {
                 )
             )
         );
-    }
-
-    /// @dev Tests that `relayMessage` is able to send a successful call
-    ///      to the target contract after the first message fails and ETH
-    ///      gets stuck, but the second message succeeds.
-    function test_relayMessage_retry_succeeds() external {
-        address sender = address(crossL2Inbox);
-        uint256 value = 100;
-
-        bytes32 hash = keccak256(
-            abi.encode(block.chainid, block.chainid, Encoding.encodeVersionedNonce(0, 1), sender, target, hex"1111")
-        );
-
-        ICrossL2Inbox.Identifier memory id = ICrossL2Inbox.Identifier({
-            origin: address(l2ToL2CrossDomainMessenger),
-            blocknumber: 0,
-            logIndex: 0,
-            timestamp: block.timestamp,
-            chainId: block.chainid
-        });
-
-        vm.prank(tx.origin);
-        crossL2Inbox.executeMessage(id, target, hex"1111");
-
-        vm.etch(target, address(new Reverter()).code);
-        vm.deal(address(crossL2Inbox), value);
-        vm.prank(address(crossL2Inbox));
-        l2ToL2CrossDomainMessenger.relayMessage{ value: value }(
-            block.chainid,
-            block.chainid,
-            Encoding.encodeVersionedNonce(0, 1), // nonce
-            sender,
-            target,
-            hex"1111"
-        );
-
-        assertEq(address(l2ToL2CrossDomainMessenger).balance, value);
-        assertEq(address(target).balance, 0);
-        assertEq(l2ToL2CrossDomainMessenger.successfulMessages(hash), false);
-        //assertEq(l2ToL2CrossDomainMessenger.failedMessages(hash), true);
-
-        vm.expectEmit(true, true, true, true);
-
-        emit RelayedMessage(hash);
-
-        vm.etch(target, address(0).code);
-        vm.prank(address(sender));
-        l2ToL2CrossDomainMessenger.relayMessage(
-            block.chainid,
-            block.chainid,
-            Encoding.encodeVersionedNonce(0, 1), // nonce
-            sender,
-            target,
-            hex"1111"
-        );
-
-        assertEq(address(l2ToL2CrossDomainMessenger).balance, 0);
-        assertEq(address(target).balance, value);
-        assertEq(l2ToL2CrossDomainMessenger.successfulMessages(hash), true);
-        //assertEq(l2ToL2CrossDomainMessenger.failedMessages(hash), true);
     }
 }
