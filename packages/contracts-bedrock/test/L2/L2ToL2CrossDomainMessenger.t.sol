966dd2bb159d442e3cc644f7821369349451fff3
Diego <105765223+0xfuturistic@users.noreply.github.com>
2024-03-08 18:11:07 -0800

    contracts-bedrock: drop redundant tests for L2ToL2CrossDomainMessenger

--- packages/contracts-bedrock/test/L2/L2ToL2CrossDomainMessenger.t.sol
+++ packages/contracts-bedrock/test/L2/L2ToL2CrossDomainMessenger.t.sol
@@ -20,19 +20,6 @@ contract L2ToL2CrossDomainMessengerTest is Bridge_Initializer {
     /// @dev Target address for testing
     address target = address(0xabcd);
 
-    /// @dev Tests that the implementation is initialized correctly.
-    function test_constructor_succeeds() external {
-        assertEq(l2ToL2CrossDomainMessenger.MESSAGE_VERSION(), uint16(0));
-        assertEq(l2ToL2CrossDomainMessenger.messageNonce(), uint256(0));
-    }
-
-    /// @dev Tests that `messageNonce` can be decoded correctly.
-    function test_messageVersion_succeeds() external {
-        assertEq(address(l2ToL2CrossDomainMessenger), 0x4200000000000000000000000000000000000023);
-        (, uint16 version) = Encoding.decodeVersionedNonce(l2ToL2CrossDomainMessenger.messageNonce());
-        assertEq(version, l2ToL2CrossDomainMessenger.MESSAGE_VERSION());
-    }
-
     /// @dev Tests that `sendMessage` executes successfully.
     function testFuzz_sendMessage_succeeds(uint256 _destination, address _target) external {
         vm.assume(_destination != block.chainid);
@@ -40,18 +27,6 @@ contract L2ToL2CrossDomainMessengerTest is Bridge_Initializer {
         bytes memory xDomainCallData = hex"aa";
 
         l2ToL2CrossDomainMessenger.sendMessage(_destination, _target, xDomainCallData);
-    }
-
-    /// @dev Tests that `sendMessage` can be called twice and that
-    ///      the nonce increments correctly.
-    function test_sendMessage_twice_succeeds() external {
-        uint256 destination = 123 == block.chainid ? 456 : 123;
-        bytes memory xDomainCallData = hex"aa";
-        uint256 nonce = l2ToL2CrossDomainMessenger.messageNonce();
-        l2ToL2CrossDomainMessenger.sendMessage(destination, target, xDomainCallData);
-        l2ToL2CrossDomainMessenger.sendMessage(destination, target, xDomainCallData);
-        // the nonce increments for each message sent
-        assertEq(nonce + 2, l2ToL2CrossDomainMessenger.messageNonce());
     }
 
     /// @dev Tests that `relayMessage` executes successfully.
@@ -77,16 +52,6 @@ contract L2ToL2CrossDomainMessengerTest is Bridge_Initializer {
             sender,
             target,
             hex"1111"
-        );
-
-        assert(
-            l2ToL2CrossDomainMessenger.successfulMessages(
-                keccak256(
-                    abi.encode(
-                        block.chainid, block.chainid, Encoding.encodeVersionedNonce(0, 1), sender, target, hex"1111"
-                    )
-                )
-            )
         );
 
         assertEq(l2ToL2CrossDomainMessenger.crossDomainMessageSender(), sender);
