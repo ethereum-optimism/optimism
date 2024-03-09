16702e5011ae86c706c7fcbd2b490b1b6aa49d70
Diego <105765223+0xfuturistic@users.noreply.github.com>
2024-03-08 17:00:11 -0800

    contracts-bedrock: refactor comments for CrossL2Inbox.sol

--- packages/contracts-bedrock/src/L2/CrossL2Inbox.sol
+++ packages/contracts-bedrock/src/L2/CrossL2Inbox.sol
@@ -4,7 +4,15 @@ pragma solidity ^0.8.24;
 import { Predeploys } from "src/libraries/Predeploys.sol";
 import { ISemver } from "src/universal/ISemver.sol";
 
-interface IL1Block {
+/// @custom:proxied
+/// @custom:predeploy 0x4200000000000000000000000000000000000022
+/// @title IL1Block_OnlyIsInDependencySet
+/// @notice Interface for L1Block with only `isInDependencySet(uint256)` method.
+interface IL1Block_OnlyIsInDependencySet {
+    /// @notice Returns true iff the chain associated with input chain ID is in the interop dependency set.
+    ///         Every chain is in the interop dependency set of itself.
+    /// @param _chainId The input chain ID.
+    /// @return True if the input chain ID corresponds to a chain in the interop dependency set. False otherwise.
     function isInDependencySet(uint256 _chainId) external view returns (bool);
 }
 
@@ -14,6 +22,7 @@ interface IL1Block {
 /// @notice The CrossL2Inbox is responsible for executing a cross chain message on the destination
 ///         chain. It is permissionless to execute a cross chain message on behalf of any user.
 contract CrossL2Inbox is ISemver {
+    /// @notice The struct for a pointer to a message payload in a remote (or local) chain.
     struct Identifier {
         address origin;
         uint256 blocknumber;
@@ -22,67 +31,85 @@ contract CrossL2Inbox is ISemver {
         uint256 chainId;
     }
 
-    // bytes32(uint256(keccak256("crossl2inbox.identifier.origin")) - 1)
+    /// @notice Transient storage slot that the origin for an Identifier is stored at.
+    ///         Equal to bytes32(uint256(keccak256("crossl2inbox.identifier.origin")) - 1)
     bytes32 public constant ORIGIN_SLOT = 0xd2b7c5071ec59eb3ff0017d703a8ea513a7d0da4779b0dbefe845808c300c815;
 
-    // bytes32(uint256(keccak256("crossl2inbox.identifier.blocknumber")) - 1)
+    /// @notice Transient storage slot that the blocknumber for an Identifier is stored at.
+    ///         Equal to bytes32(uint256(keccak256("crossl2inbox.identifier.blocknumber")) - 1)
     bytes32 public constant BLOCKNUMBER_SLOT = 0x5a1da0738b7fdc60047c07bb519beb02aa32a8619de57e6258da1f1c2e020ccc;
 
-    // bytes32(uint256(keccak256("crossl2inbox.identifier.logindex")) - 1)
+    /// @notice Transient storage slot that the logIndex for an Identifier is stored at.
+    ///         Equal to bytes32(uint256(keccak256("crossl2inbox.identifier.logindex")) - 1)
     bytes32 public constant LOG_INDEX_SLOT = 0xab8acc221aecea88a685fabca5b88bf3823b05f335b7b9f721ca7fe3ffb2c30d;
 
-    // bytes32(uint256(keccak256("crossl2inbox.identifier.timestamp")) - 1)
+    /// @notice Transient storage slot that the timestamp for an Identifier is stored at.
+    ///         Equal to bytes32(uint256(keccak256("crossl2inbox.identifier.timestamp")) - 1)
     bytes32 public constant TIMESTAMP_SLOT = 0x2e148a404a50bb94820b576997fd6450117132387be615e460fa8c5e11777e02;
 
-    // bytes32(uint256(keccak256("crossl2inbox.identifier.chainid")) - 1)
+    /// @notice Transient storage slot that the chainId for an Identifier is stored at.
+    ///         Equal to bytes32(uint256(keccak256("crossl2inbox.identifier.chainid")) - 1)
     bytes32 public constant CHAINID_SLOT = 0x6e0446e8b5098b8c8193f964f1b567ec3a2bdaeba33d36acb85c1f1d3f92d313;
 
     /// @custom:semver 1.0.0
     string public constant version = "1.0.0";
 
+    /// @notice Returns the origin address of the Identifier.
+    /// @return _origin The origin address of the Identifier.
     function origin() public view returns (address _origin) {
         assembly {
             _origin := tload(ORIGIN_SLOT)
         }
     }
 
+    /// @notice Returns the block number of the Identifier.
+    /// @return _blocknumber The block number of the Identifier.
     function blocknumber() public view returns (uint256 _blocknumber) {
         assembly {
             _blocknumber := tload(BLOCKNUMBER_SLOT)
         }
     }
 
+    /// @notice Returns the log index of the Identifier.
+    /// @return _logIndex The log index of the Identifier.
     function logIndex() public view returns (uint256 _logIndex) {
         assembly {
             _logIndex := tload(LOG_INDEX_SLOT)
         }
     }
 
+    /// @notice Returns the timestamp of the Identifier.
+    /// @return _timestamp The timestamp of the Identifier.
     function timestamp() public view returns (uint256 _timestamp) {
         assembly {
             _timestamp := tload(TIMESTAMP_SLOT)
         }
     }
 
+    /// @notice Returns the chain ID of the Identifier.
+    /// @return _chainId The chain ID of the Identifier.
     function chainId() public view returns (uint256 _chainId) {
         assembly {
             _chainId := tload(CHAINID_SLOT)
         }
     }
 
-    /// @notice Executes a cross chain message on the destination chain
-    /// @param _id A Identifier pointing to the initiating message.
+    /// @notice Executes a cross chain message on the destination chain.
+    /// @param _id An Identifier pointing to the initiating message.
     /// @param _target Account that is called with _msg.
     /// @param _msg The message payload, matching the initiating message.
     function executeMessage(Identifier calldata _id, address _target, bytes memory _msg) public payable {
-        require(_id.timestamp <= block.timestamp, "CrossL2Inbox: invalid id timestamp"); // timestamp invariant
+        require(msg.sender == tx.origin, "CrossL2Inbox: not EOA sender");
+        require(_id.timestamp <= block.timestamp, "CrossL2Inbox: invalid id timestamp");
         require(
-            IL1Block(Predeploys.L1_BLOCK_ATTRIBUTES).isInDependencySet(_id.chainId), "CrossL2Inbox: invalid id chainId"
-        ); // invariant
-        require(msg.sender == tx.origin, "CrossL2Inbox: Not EOA sender"); // only EOA invariant
+            IL1Block_OnlyIsInDependencySet(Predeploys.L1_BLOCK_ATTRIBUTES).isInDependencySet(_id.chainId),
+            "CrossL2Inbox: id chain not in dependency set"
+        );
 
-        _tStoreCalldataID();
+        // Store the Identifier in transient storage.
+        _storeCalldataID();
 
+        // Call the target account with the message payload.
         bool success;
         assembly {
             success :=
@@ -97,10 +124,12 @@ contract CrossL2Inbox is ISemver {
                 )
         }
 
-        require(success, "CrossL2Inbox: call failed");
+        // Revert if the target call failed.
+        require(success, "CrossL2Inbox: target call failed");
     }
 
-    function _tStoreCalldataID() internal {
+    /// @notice Stores the Identifier in transient storage.
+    function _storeCalldataID() internal {
         assembly {
             tstore(ORIGIN_SLOT, calldataload(4))
             tstore(BLOCKNUMBER_SLOT, calldataload(36))
