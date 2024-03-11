015447e6046a4ca7eb30faf62ba6920a021b3829
Roberto Bayardo <roberto.bayardo@coinbase.com>
2024-03-11 12:25:42 -0700

    have batcher submit appropriate cancellation transactions when mempool is blocked

--- op-service/txmgr/txmgr.go
+++ op-service/txmgr/txmgr.go
@@ -317,7 +317,6 @@ func (m *SimpleTxManager) craftTx(ctx context.Context, candidate TxCandidate) (*
 		}
 	}
 	return m.signWithNextNonce(ctx, txMessage) // signer sets the nonce field of the tx
-
 }
 
 // MakeSidecar builds & returns the BlobTxSidecar and corresponding blob hashes from the raw blob
@@ -898,4 +897,72 @@ func finishBlobTx(message *types.BlobTx, chainID, tip, fee, blobFee, value *big.
 		return fmt.Errorf("Value overflow")
 	}
 	return nil
+}
+
+// JamTxPool sends a transaction intended to get stuck in the txpool, and should be used ONLY for testing.
+func (m *SimpleTxManager) JamTxPool(ctx context.Context, candidate TxCandidate) (*types.Receipt, error) {
+	tx, err := m.makeStuckTx(ctx, candidate)
+	if err != nil {
+		return nil, err
+	}
+	sendState := NewSendState(m.cfg.SafeAbortNonceTooLowCount, m.cfg.TxNotInMempoolTimeout)
+	err = m.backend.SendTransaction(ctx, tx)
+	if err != nil {
+		return nil, err
+	}
+	return m.waitMined(ctx, tx, sendState)
+}
+
+func (m *SimpleTxManager) makeStuckTx(ctx context.Context, candidate TxCandidate) (*types.Transaction, error) {
+	gasTipCap, _, blobBaseFee, err := m.suggestGasPriceCaps(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	// overried with minimal fees to make sure tx gets stuck in the pool
+	gasFeeCap := big.NewInt(2)
+	gasTipCap.SetUint64(1)
+
+	var sidecar *types.BlobTxSidecar
+	var blobHashes []common.Hash
+	if len(candidate.Blobs) > 0 {
+		if sidecar, blobHashes, err = MakeSidecar(candidate.Blobs); err != nil {
+			return nil, err
+		}
+	}
+
+	nonce, err := m.backend.NonceAt(ctx, m.cfg.From, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var txMessage types.TxData
+	if sidecar != nil {
+		blobFeeCap := calcBlobFeeCap(blobBaseFee)
+		message := &types.BlobTx{
+			To:         *candidate.To,
+			Data:       candidate.TxData,
+			Gas:        candidate.GasLimit,
+			BlobHashes: blobHashes,
+			Sidecar:    sidecar,
+			Nonce:      nonce,
+		}
+		if err := finishBlobTx(message, m.chainID, gasTipCap, gasFeeCap, blobFeeCap, candidate.Value); err != nil {
+			return nil, err
+		}
+		txMessage = message
+	} else {
+		txMessage = &types.DynamicFeeTx{
+			ChainID:   m.chainID,
+			To:        candidate.To,
+			GasTipCap: gasTipCap,
+			GasFeeCap: gasFeeCap,
+			Value:     candidate.Value,
+			Data:      candidate.TxData,
+			Gas:       candidate.GasLimit,
+			Nonce:     nonce,
+		}
+	}
+
+	return m.cfg.Signer(ctx, m.cfg.From, types.NewTx(txMessage))
 }
