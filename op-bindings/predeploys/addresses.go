ae7a0a42ff39679cfebfe68e786fe224314c5d29
Diego <105765223+0xfuturistic@users.noreply.github.com>
2024-03-12 12:22:21 -0700

    op-bindings: add L2ToL2CrossDomainMessenger to addresses.go

--- op-bindings/predeploys/addresses.go
+++ op-bindings/predeploys/addresses.go
@@ -36,6 +36,7 @@ const (
 	Permit2                       = "0x000000000022D473030F116dDEE9F6B43aC78BA3"
 	SenderCreator                 = "0x7fc98430eaedbb6070b35b39d798725049088348"
 	EntryPoint                    = "0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789"
+	L2ToL2CrossDomainMessenger    = "0x4200000000000000000000000000000000000023"
 )
 
 var (
@@ -69,6 +70,7 @@ var (
 	Permit2Addr                       = common.HexToAddress(Permit2)
 	SenderCreatorAddr                 = common.HexToAddress(SenderCreator)
 	EntryPointAddr                    = common.HexToAddress(EntryPoint)
+	L2ToL2CrossDomainMessengerAddr    = common.HexToAddress(L2ToL2CrossDomainMessenger)
 
 	Predeploys          = make(map[string]*Predeploy)
 	PredeploysByAddress = make(map[common.Address]*Predeploy)
@@ -143,6 +145,9 @@ func init() {
 	Predeploys["EntryPoint"] = &Predeploy{
 		Address:       EntryPointAddr,
 		ProxyDisabled: true,
+	}
+	Predeploys["L2ToL2CrossDomainMessenger"] = &Predeploy{
+		Address: L2ToL2CrossDomainMessengerAddr,
 	}
 
 	for _, predeploy := range Predeploys {
