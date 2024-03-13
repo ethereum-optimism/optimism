eaf7927e28acedc7393b73ca5229df5712d1f921
Diego <105765223+0xfuturistic@users.noreply.github.com>
2024-03-13 15:19:18 -0700

    contracts-bedrock: refactor setL1BlockValuesInterop in L1Block

--- packages/contracts-bedrock/src/L2/L1Block.sol
+++ packages/contracts-bedrock/src/L2/L1Block.sol
@@ -125,11 +125,14 @@ contract L1Block is ISemver {
     }
 
     /// @notice Updates the L1 block values for an Interop upgraded chain.
-    /// @dev The interop upgrade block itself MUST include a call to setL1BlockValuesEcotone.
     /// Params are packed and passed in as raw msg.data instead of ABI to reduce calldata size.
-    /// Params are expected to extend setL1BlockValuesEcotone calldata in the following order:
-    ///  1. _interopSetSize     Size of the interop dependency set.
-    ///  2. _chainIds           Array of chain IDs in the interop dependency set.
+    /// Params are expected to be in the following order:
+    ///   1. _baseFeeScalar      L1 base fee scalar
+    ///   2. _blobBaseFeeScalar  L1 blob base fee scalar
+    ///   3. _sequenceNumber     Number of L2 blocks since epoch start.
+    ///   4. _timestamp          L1 timestamp.
+    ///   5. _number             L1 blocknumber.
+    ///   6. _basefee            L1 base fee.
     ///   7. _blobBaseFee        L1 blob base fee.
     ///   8. _hash               L1 blockhash.
     ///   9. _batcherHash        Versioned hash to authenticate batcher by.
@@ -142,26 +145,33 @@ contract L1Block is ISemver {
                 mstore(0x00, 0x3cc50b45) // 0x3cc50b45 is the 4-byte selector of "NotDepositor()"
                 revert(0x1C, 0x04) // returns the stored 4-byte selector from above
             }
+            let data := calldataload(4)
+            // sequencenum (uint64), blobBaseFeeScalar (uint32), baseFeeScalar (uint32)
+            sstore(sequenceNumber.slot, shr(128, calldataload(4)))
+            // number (uint64) and timestamp (uint64)
             sstore(number.slot, shr(128, calldataload(20)))
+            sstore(basefee.slot, calldataload(36)) // uint256
+            sstore(blobBaseFee.slot, calldataload(68)) // uint256
+            sstore(hash.slot, calldataload(100)) // bytes32
+            sstore(batcherHash.slot, calldataload(132)) // bytes32
 
-            // Load interopSetSize from calldata (at offset 164 after calldata for setL1BlockValuesEcotone ends)
-            let interopSetSize_ := shr(248, calldataload(164))
+            // Load dependencySetSize from calldata (at offset 164 after calldata for setL1BlockValuesEcotone ends)
+            let dependencySetSize_ := shr(248, calldataload(164))
 
-            // Revert if interopSetSize_ doesn't match the length of chainIds in calldata
             // Revert if dependencySetSize_ doesn't match the length of dependencySet in calldata
+            if xor(add(165, mul(dependencySetSize_, 0x20)), calldatasize()) {
+                mstore(0x00, 0x613457f2) // 0x613457f2 is the 4-byte selector of "NotdependencySetSize()"
                 revert(0x1C, 0x04) // returns the stored 4-byte selector from above
             }
 
-            // Write interopSetSize_ to storage
-            sstore(interopSetSize.slot, interopSetSize_)
+            // Use memory to hash and get the start index of dependencySet
+            mstore(0x00, dependencySet.slot)
+            let dependencySetStartIndex := keccak256(0x00, 0x20)
 
-            // Use memory to hash and get the start index of chainIds
-            let chainIdsStartIndex := keccak256(0x00, 0x20)
-
-            // Iterate over calldata chainIds and write to store chainIds
-            for { let i := 0 } lt(i, interopSetSize_) { i := add(i, 1) } {
-                // Increase length of chainIds array
+            // Iterate over calldata dependencySet and write to store dependencySet
+            for { let i := 0 } lt(i, dependencySetSize_) { i := add(i, 1) } {
                 // Increase length of dependencySet array
+                sstore(dependencySet.slot, add(sload(dependencySet.slot), 1))
 
                 // Load value from calldata and write to storage (dependencySet) at index
                 let val := calldataload(add(165, mul(i, 0x20)))
