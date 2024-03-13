11e9b245147d258515bf0a4cdd3cf7eac5f1b113
netrats <163349918+netrats@users.noreply.github.com>
2024-03-13 21:20:44 +0100

    Update log_setup.py

--- bedrock-devnet/devnet/log_setup.py
+++ bedrock-devnet/devnet/log_setup.py
@@ -1,30 +1,32 @@
 import os
-
 from logging.config import dictConfig
 
-log_level = os.getenv('LOG_LEVEL')
+# Retrieve the log level from an environment variable, default to 'INFO' if not set
+log_level = os.getenv('LOG_LEVEL', 'INFO')
 
+# Define the logging configuration as a dictionary
 log_config = {
-    'version': 1,
+    'version': 1, # Schema version
     'loggers': {
-        '': {
-            'handlers': ['console'],
-            'level': log_level if log_level is not None else 'INFO'
+        '': { # Root logger
+            'handlers': ['console'], # Handlers to use
+            'level': log_level, # Log level, default to 'INFO' if not set
         },
     },
     'handlers': {
-        'console': {
-            'formatter': 'stderr',
-            'class': 'logging.StreamHandler',
-            'stream': 'ext://sys.stdout'
+        'console': { # Console handler
+            'formatter': 'stderr', # Formatter to use
+            'class': 'logging.StreamHandler', # Handler class
+            'stream': 'ext://sys.stdout', # Output stream
         }
     },
     'formatters': {
-        'stderr': {
-            'format': '[%(levelname)s|%(asctime)s] %(message)s',
-            'datefmt': '%m-%d-%Y %I:%M:%S'
+        'stderr': { # Formatter for console output
+            'format': '[%(levelname)s|%(asctime)s] %(message)s', # Log message format
+            'datefmt': '%m-%d-%Y %I:%M:%S', # Date format
         }
     },
 }
 
+# Apply the logging configuration
 dictConfig(log_config)
