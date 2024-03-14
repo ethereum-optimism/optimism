43eabfbae35e82c31551c63e068a0900197f8c6b
Diego <105765223+0xfuturistic@users.noreply.github.com>
2024-03-14 11:31:59 -0700

    op-node: fix fuzz_parsers_test

--- op-node/rollup/derive/fuzz_parsers_test.go
+++ op-node/rollup/derive/fuzz_parsers_test.go
@@ -167,7 +167,6 @@ func FuzzL1InfoBedrockAgainstContract(f *testing.F) {
 			eth.AddressAsLeftPaddedHash(common.BytesToAddress(batcherHash)),
 			common.BytesToHash(l1FeeOverhead).Big(),
 			common.BytesToHash(l1FeeScalar).Big(),
-			[]*big.Int{},
 		)
 		if err != nil {
 			t.Fatalf("Failed to create the transaction: %v", err)
