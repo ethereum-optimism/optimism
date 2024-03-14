ca54911ba4745c4c5b9954136cb670a7a9746d26
Diego <105765223+0xfuturistic@users.noreply.github.com>
2024-03-13 20:26:55 -0700

    contracts-bedrock: bump version of L1Block to 1.3.0

--- packages/contracts-bedrock/src/L2/L1Block.sol
+++ packages/contracts-bedrock/src/L2/L1Block.sol
@@ -52,8 +52,8 @@ contract L1Block is ISemver {
     /// @notice The chain IDs of the interop dependency set.
     uint256[] public dependencySet;
 
-    /// @custom:semver 1.2.1
-    string public constant version = "1.2.1";
+    /// @custom:semver 1.3.0
+    string public constant version = "1.3.0";
 
     /// @custom:legacy
     /// @notice Updates the L1 block values.
