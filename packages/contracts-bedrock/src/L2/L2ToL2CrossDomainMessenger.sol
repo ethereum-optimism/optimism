46befb8bebbe752208b618b2e2d72024a9b8c0ae
Diego <105765223+0xfuturistic@users.noreply.github.com>
2024-03-15 20:11:23 -0500

    contracts-bedrock: update pragma to 0.8.25 for L2ToL2CrossDomainMessenger

--- packages/contracts-bedrock/src/L2/L2ToL2CrossDomainMessenger.sol
+++ packages/contracts-bedrock/src/L2/L2ToL2CrossDomainMessenger.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.24;
+pragma solidity 0.8.25;
 
 import { CrossL2Inbox } from "src/L2/CrossL2Inbox.sol";
 import { Encoding } from "src/libraries/Encoding.sol";
