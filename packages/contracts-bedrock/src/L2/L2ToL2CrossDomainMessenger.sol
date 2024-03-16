74c99f8143af14834a04713eda75d8bcaac2676c
Diego <105765223+0xfuturistic@users.noreply.github.com>
2024-03-15 21:33:22 -0500

    contracts-bedrock: add underscore to var in L2ToL2CrossDomainMessenger

--- packages/contracts-bedrock/src/L2/L2ToL2CrossDomainMessenger.sol
+++ packages/contracts-bedrock/src/L2/L2ToL2CrossDomainMessenger.sol
@@ -168,9 +168,9 @@ contract L2ToL2CrossDomainMessenger is IL2ToL2CrossDomainMessenger, ISemver {
     }
 
     /// @notice Calls the target account with the message payload and all available gas.
-    function _callWithAllGas(address _target, bytes memory _msg) internal returns (bool success) {
+    function _callWithAllGas(address _target, bytes memory _msg) internal returns (bool _success) {
         assembly {
-            success :=
+            _success :=
                 call(
                     gas(), // gas
                     _target, // recipient
