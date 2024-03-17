ced4d74c1bdddedeee90a6d49772178f6de20aa2
Mark Tyneway <mark.tyneway@gmail.com>
2024-03-17 16:48:37 -0600

    op-wheel: fix build

--- op-wheel/cheat/cheat.go
+++ op-wheel/cheat/cheat.go
@@ -21,7 +21,6 @@ import (
 	"github.com/ethereum/go-ethereum/core/rawdb"
 	"github.com/ethereum/go-ethereum/core/state"
 	"github.com/ethereum/go-ethereum/core/vm"
-	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethdb"
 	"github.com/ethereum/go-ethereum/params"
 	"github.com/ethereum/go-ethereum/rlp"
