8af855aeb7fcc73a4cd2a289f2116f942ffcbfce
Will Cory <willcory10@gmail.com>
2024-04-01 08:58:12 -0700

    fix: make sdk-next tests run in ci (#10003)
    Co-authored-by: Will Cory <willcory@Wills-MacBook-Pro.local>

--- packages/sdk/test-next/testUtils/ethersProviders.ts
+++ packages/sdk/test-next/testUtils/ethersProviders.ts
@@ -1,28 +1,58 @@
 import ethers from 'ethers'
 import { z } from 'zod'
 
+/**
+ * @deprecated
+ */
 const E2E_RPC_URL_L1 = z
   .string()
   .url()
+  .default('http://localhost:8545')
   .describe('L1 ethereum rpc Url')
   .parse(import.meta.env.VITE_E2E_RPC_URL_L1)
+/**
+ * @deprecated
+ */
 const E2E_RPC_URL_L2 = z
   .string()
   .url()
-  .describe('L1 ethereum rpc Url')
+  .default('http://localhost:9545')
+  .describe('L2 ethereum rpc Url')
   .parse(import.meta.env.VITE_E2E_RPC_URL_L2)
 
 const jsonRpcHeaders = { 'User-Agent': 'eth-optimism/@gateway/backend' }
 /**
- * Initialize the signer, prover, and cross chain messenger
+ * @deprecated
  */
-const l1Provider = new ethers.providers.JsonRpcProvider({
+export const l1Provider = new ethers.providers.JsonRpcProvider({
   url: E2E_RPC_URL_L1,
   headers: jsonRpcHeaders,
 })
-const l2Provider = new ethers.providers.JsonRpcProvider({
+/**
+ * @deprecated
+ */
+export const l2Provider = new ethers.providers.JsonRpcProvider({
   url: E2E_RPC_URL_L2,
   headers: jsonRpcHeaders,
 })
 
-export { l1Provider, l2Provider }
+export const E2E_RPC_URL_SEPOLIA = z
+  .string()
+  .url()
+  .default('http://localhost:8545')
+  .describe('SEPOLIA ethereum rpc Url')
+  .parse(import.meta.env.VITE_E2E_RPC_URL_SEPOLIA)
+export const E2E_RPC_URL_OP_SEPOLIA = z
+  .string()
+  .url()
+  .default('http://localhost:9545')
+  .describe('OP_SEPOLIA ethereum rpc Url')
+  .parse(import.meta.env.VITE_E2E_RPC_URL_OP_SEPOLIA)
+export const sepoliaProvider = new ethers.providers.JsonRpcProvider({
+  url: E2E_RPC_URL_SEPOLIA,
+  headers: jsonRpcHeaders,
+})
+export const opSepoliaProvider = new ethers.providers.JsonRpcProvider({
+  url: E2E_RPC_URL_OP_SEPOLIA,
+  headers: jsonRpcHeaders,
+})
