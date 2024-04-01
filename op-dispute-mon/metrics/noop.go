28d5526c0dc7200915500a6400cc08b107c1de98
refcell <abigger87@gmail.com>
2024-04-01 15:58:12 -0400

    feat(op-dispute-mon): WithdrawalRequests Metrics (#9942)
    * feat(op-challenger): Delayed Weth Withdrawal Request Caller
    
    * fix(op-challenger): withdrawal request field ordering
    
    * fix(op-challenger): encapsulate delayed weth behind the fault dispute game contract binding
    
    * feat(op-dispute-mon): get withdrawals game caller method
    
    * fix(op-dispute-mon): revert service name changes
    
    * fix(op-dispute-mon): revert extractor test name
    
    * fix(op-dispute-mon): revert extractor changes
    
    * fix(op-dispute-mon): revert newline add
    
    * fix(op-dispute-mon): revert caller field reordering
    
    * feat(op-dispute-mon): weth caller creation and pass it through sub components
    
    * chore(op-dispute-mon): weth caller creation unit test
    
    * fix(op-dispute-mon): extractor test for weth caller creation error
    
    * feat(op-dispute-mon): wire up the withdrawals extractor
    
    * fix(op-dispute-mon): rebases
    
    * fix(op-dispute-mon): revert caller test change
    
    * fix(op-dispute-mon): recipient enricher
    
    * feat(op-dispute-mon): withdrawal request metrics
    
    * Update op-dispute-mon/mon/withdrawals.go
    
    Co-authored-by: Adrian Sutton <adrian@oplabs.co>
    
    * fix(op-dispute-mon): withdrawal request amounts
    
    ---------
    
    Co-authored-by: Adrian Sutton <adrian@oplabs.co>

--- op-dispute-mon/metrics/noop.go
+++ op-dispute-mon/metrics/noop.go
@@ -19,7 +19,8 @@ func (*NoopMetricsImpl) RecordUp()                 {}
 func (*NoopMetricsImpl) CacheAdd(_ string, _ int, _ bool) {}
 func (*NoopMetricsImpl) CacheGet(_ string, _ bool)        {}
 
-func (*NoopMetricsImpl) RecordClaimResolutionDelayMax(delay float64) {}
+func (*NoopMetricsImpl) RecordWithdrawalRequests(_ common.Address, _ bool, _ int) {}
+func (*NoopMetricsImpl) RecordClaimResolutionDelayMax(delay float64)              {}
 
 func (*NoopMetricsImpl) RecordOutputFetchTime(timestamp float64) {}
 
