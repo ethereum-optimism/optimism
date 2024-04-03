1b0e4f7f1d7192ce7b62cdad88927217e84d27e2
Mark Tyneway <mark.tyneway@gmail.com>
2024-04-03 14:31:06 -0600

    op-chain-ops: revert diff

--- op-chain-ops/genesis/config.go
+++ op-chain-ops/genesis/config.go
@@ -28,13 +28,6 @@ import (
 var (
 	ErrInvalidDeployConfig     = errors.New("invalid deploy config")
 	ErrInvalidImmutablesConfig = errors.New("invalid immutables config")
-	// L1BlockGasPayingTokenSlot represents the storage slot in the L1Block predeploy that containst the address of the gas paying token.
-	// It is computed with the following digest: bytes32(uint256(keccak256("l1block.gaspayingtoken")) - 1)
-	L1BlockGasPayingTokenSlot = common.HexToHash("0x139ee11fa3c568c59ec2e47800b66a422e22a029a261f59aab388d8041d78abd")
-	// EtherGasPayingAddress represents the magic account that represents ether to be the gas paying asset.
-	EtherGasPayingAddress = common.HexToAddress("0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE")
-	// EtherDecimals represents the number of decimals for ether.
-	EtherDecimals = big.NewInt(18)
 )
 
 // DeployConfig represents the deployment configuration for an OP Stack chain.
@@ -929,30 +922,15 @@ func NewL2StorageConfig(config *DeployConfig, block *types.Block) (state.Storage
 		"bridge":        predeploys.L2StandardBridgeAddr,
 	}
 
-	// Compute the initial storage slot storage slot for the gas token in genesis storage.
-	// This is to ensure that the ether config is set in storage in genesis since
-	// the magic address is used to represent ether. If an actual gas token is used
-	// then the L1 SystemConfig will push a transaction that updates the L2 state with
-	// the correct amount of decimals.
-	var value common.Hash
-	if config.UseCustomGasToken {
-		value = eth.AddressAsLeftPaddedHash(config.CustomGasTokenAddress)
-	} else {
-		decimals := new(big.Int).Set(EtherDecimals)
-		decimals = decimals.Lsh(decimals, 160)
-		value = common.BigToHash(decimals.Or(decimals, EtherGasPayingAddress.Big()))
-	}
-
 	storage["L1Block"] = state.StorageValues{
-		"number":                        block.Number(),
-		"timestamp":                     block.Time(),
-		"basefee":                       block.BaseFee(),
-		"hash":                          block.Hash(),
-		"sequenceNumber":                0,
-		"batcherHash":                   eth.AddressAsLeftPaddedHash(config.BatchSenderAddress),
-		"l1FeeOverhead":                 config.GasPriceOracleOverhead,
-		"l1FeeScalar":                   config.GasPriceOracleScalar,
-		L1BlockGasPayingTokenSlot.Hex(): value.Hex(),
+		"number":         block.Number(),
+		"timestamp":      block.Time(),
+		"basefee":        block.BaseFee(),
+		"hash":           block.Hash(),
+		"sequenceNumber": 0,
+		"batcherHash":    eth.AddressAsLeftPaddedHash(config.BatchSenderAddress),
+		"l1FeeOverhead":  config.GasPriceOracleOverhead,
+		"l1FeeScalar":    config.GasPriceOracleScalar,
 	}
 	storage["LegacyERC20ETH"] = state.StorageValues{
 		"_name":   "Ether",
