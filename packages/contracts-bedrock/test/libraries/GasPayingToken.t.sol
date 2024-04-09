44cb747b908f4035cb105944fc4ec9bd32427651
Diego <105765223+0xfuturistic@users.noreply.github.com>
2024-04-09 15:07:40 -0300

    contracts-bedrock: fix tests for GasPayingToken

--- packages/contracts-bedrock/test/libraries/GasPayingToken.t.sol
+++ packages/contracts-bedrock/test/libraries/GasPayingToken.t.sol
@@ -3,53 +3,49 @@ pragma solidity 0.8.15;
 
 // Target contract
 import { GasPayingToken } from "src/libraries/GasPayingToken.sol";
+import { Constants } from "src/libraries/Constants.sol";
 import { Test } from "forge-std/Test.sol";
 
 /// @title GasPayingToken_Roundtrip_Test
 /// @notice Tests the roundtrip of setting and getting the gas paying token.
 contract GasPayingToken_Roundtrip_Test is Test {
     /// @notice Test that the gas paying token correctly sets values in storage
-    function testFuzz_set_succeeds(
-        address _token,
-        uint8 _decimals,
-        string memory _name,
-        string memory _symbol
-    )
-        external
-    {
+    function testFuzz_set_succeeds(address _token, uint8 _decimals, bytes32 _name, bytes32 _symbol) external {
         GasPayingToken.set(_token, _decimals, _name, _symbol);
 
         // Check the token address and decimals
         assertEq(
             bytes32(uint256(_decimals) << 160 | uint256(uint160(_token))),
-            uint256(vm.load(address(this), GasPayingToken.GAS_PAYING_TOKEN_SLOT()))
+            vm.load(address(this), GasPayingToken.GAS_PAYING_TOKEN_SLOT)
         );
 
         // Check the token name
-        assertEq(
-            bytes32(abi.encodePacked(_name)),
-            uint256(vm.load(address(this), GasPayingToken.GAS_PAYING_TOKEN_NAME_SLOT()))
-        );
+        assertEq(_name, vm.load(address(this), GasPayingToken.GAS_PAYING_TOKEN_NAME_SLOT));
 
         // Check the token symbol
-        assertEq(
-            bytes32(abi.encodePacked(_symbol)),
-            uint256(vm.load(address(this), GasPayingToken.GAS_PAYING_TOKEN_SYMBOL_SLOT()))
-        );
+        assertEq(_symbol, vm.load(address(this), GasPayingToken.GAS_PAYING_TOKEN_SYMBOL_SLOT));
     }
 
-    /// @dev Test that the gas paying token correctly gets values from storage
-    function testFuzz_setGet_succeeds(
-        address _token,
-        uint8 _decimals,
-        string memory _name,
-        string memory _symbol
-    )
-        external
-    {
+    /// @dev Test that the gas paying token returns values associated with Ether when unset
+    function testFuzz_get_empty_succeeds() external {
+        (address token, uint8 decimals) = GasPayingToken.getToken();
+        assertEq(Constants.ETHER, token);
+        assertEq(18, decimals);
+
+        assertEq("Ether", GasPayingToken.getName());
+
+        assertEq("ETH", GasPayingToken.getSymbol());
+    }
+
+    /// @dev Test that the gas paying token correctly gets values from storage when set
+    function testFuzz_get_nonEmpty_succeeds(address _token, uint8 _decimals, bytes32 _name, bytes32 _symbol) external {
+        vm.assume(_token != address(0));
+
         GasPayingToken.set(_token, _decimals, _name, _symbol);
 
-        assertEq((_token, _decimals), GasPayingToken.getToken());
+        (address token, uint8 decimals) = GasPayingToken.getToken();
+        assertEq(_token, token);
+        assertEq(_decimals, decimals);
 
         assertEq(_name, GasPayingToken.getName());
 
