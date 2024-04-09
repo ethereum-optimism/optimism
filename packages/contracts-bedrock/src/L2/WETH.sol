24181c436f941ebd8163e6ca4e723684adabe1ec
Diego <105765223+0xfuturistic@users.noreply.github.com>
2024-04-09 15:14:18 -0300

    contracts-bedrock: trim right padded zeroes in WETH

--- packages/contracts-bedrock/src/L2/WETH.sol
+++ packages/contracts-bedrock/src/L2/WETH.sol
@@ -10,13 +10,36 @@ contract WETH is WETH98 {
     /// @notice Returns the name of the token from the L1Block contract
     function name() external view override returns (string memory) {
         return string.concat(
-            "Wrapped ", string(abi.encodePacked(L1Block(Predeploys.L1_BLOCK_ATTRIBUTES).gasPayingTokenName()))
+            "Wrapped ",
+            _trimRightPaddedZeroes(
+                string(abi.encodePacked(L1Block(Predeploys.L1_BLOCK_ATTRIBUTES).gasPayingTokenName()))
+            )
         );
     }
 
     /// @notice Returns the symbol of the token from the L1Block contract
     function symbol() external view override returns (string memory) {
-        return
-            string.concat("W", string(abi.encodePacked(L1Block(Predeploys.L1_BLOCK_ATTRIBUTES).gasPayingTokenSymbol())));
+        return string.concat(
+            "W",
+            _trimRightPaddedZeroes(
+                string(abi.encodePacked(L1Block(Predeploys.L1_BLOCK_ATTRIBUTES).gasPayingTokenSymbol()))
+            )
+        );
+    }
+
+    /// @notice Helper function to trim zero bytes from the end of a string.
+    function _trimRightPaddedZeroes(string memory str) internal pure returns (string memory) {
+        bytes memory strBytes = bytes(str);
+        uint256 newLength = strBytes.length;
+        for (; newLength > 0; newLength--) {
+            if (strBytes[newLength - 1] != 0) {
+                break;
+            }
+        }
+        bytes memory trimmedBytes = new bytes(newLength);
+        for (uint256 i = 0; i < newLength; i++) {
+            trimmedBytes[i] = strBytes[i];
+        }
+        return string(trimmedBytes);
     }
 }
