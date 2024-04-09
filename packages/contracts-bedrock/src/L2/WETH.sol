d6aea65a2df580b390811c1678e7b6d4477d27d3
Diego <105765223+0xfuturistic@users.noreply.github.com>
2024-04-09 16:28:17 -0300

    contracts-bedrock: create trimTrailingZeroes in Bytes.sol

--- packages/contracts-bedrock/src/L2/WETH.sol
+++ packages/contracts-bedrock/src/L2/WETH.sol
@@ -9,50 +9,11 @@ import { L1Block } from "src/L2/L1Block.sol";
 contract WETH is WETH98 {
     /// @notice Returns the name of the token from the L1Block contract
     function name() external view override returns (string memory) {
-        return string.concat(
-            "Wrapped ",
-            _trimRightPaddedZeroes(
-                string(abi.encodePacked(L1Block(Predeploys.L1_BLOCK_ATTRIBUTES).gasPayingTokenName()))
-            )
-        );
+        return string.concat("Wrapped ", L1Block(Predeploys.L1_BLOCK_ATTRIBUTES).gasPayingTokenName());
     }
 
     /// @notice Returns the symbol of the token from the L1Block contract
     function symbol() external view override returns (string memory) {
-        return string.concat(
-            "W",
-            _trimRightPaddedZeroes(
-                string(abi.encodePacked(L1Block(Predeploys.L1_BLOCK_ATTRIBUTES).gasPayingTokenSymbol()))
-            )
-        );
-    }
-
-    /// @notice Helper function to trim zero bytes from the end of a string.
-    function _trimRightPaddedZeroes(string memory str) internal pure returns (string memory) {
-        bytes memory strBytes = bytes(str);
-        uint256 length = strBytes.length;
-
-        // Find the index where the non-zero byte is last seen.
-        uint256 lastIndex = 0; // We'll use this to mark the end of non-zero bytes.
-        for (uint256 i = 0; i < length; i++) {
-            if (strBytes[i] != 0) {
-                lastIndex = i;
-            }
-        }
-
-        // If no non-zero byte is found, return an empty string.
-        // This also covers the case where the string is fully padded with zeroes.
-        if (lastIndex == 0 && strBytes[0] == 0) {
-            return "";
-        }
-
-        // Create a new bytes array of the appropriate length.
-        // +1 because lastIndex represents the index of the last non-zero byte, and array indices are 0-based.
-        bytes memory trimmedBytes = new bytes(lastIndex + 1);
-        for (uint256 i = 0; i <= lastIndex; i++) {
-            trimmedBytes[i] = strBytes[i];
-        }
-
-        return string(trimmedBytes);
+        return string.concat("W", L1Block(Predeploys.L1_BLOCK_ATTRIBUTES).gasPayingTokenSymbol());
     }
 }
