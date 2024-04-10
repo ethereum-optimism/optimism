71b312b9bb1781e421e4cd00eaea02575c8aa3a4
tchardin <tdotchardin@gmail.com>
2024-04-10 14:49:36 -0600

    fix: websocket check origin

--- proxyd/server.go
+++ proxyd/server.go
@@ -192,6 +192,8 @@ func NewServer(
 		maxBatchSize:         maxBatchSize,
 		upgrader: &websocket.Upgrader{
 			HandshakeTimeout: defaultWSHandshakeTimeout,
+			// see https://pkg.go.dev/github.com/gorilla/websocket#hdr-Origin_Considerations
+			CheckOrigin: func(r *http.Request) bool { return true },
 		},
 		mainLim:                mainLim,
 		overrideLims:           overrideLims,
