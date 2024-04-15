a487b2b2676b302491c11a6b0802ff792041fca0
Matt Solomon <matt@mattsolomon.dev>
2024-04-15 14:03:24 -0700

    remove unused import

--- op-chain-ops/upgrades/l1.go
+++ op-chain-ops/upgrades/l1.go
@@ -8,7 +8,6 @@ import (
 	"strconv"
 
 	"github.com/ethereum/go-ethereum"
-	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
 
