dde03ae72a9adda6afb512cb21731485d7bddfbb
geoknee <georgeknee@googlemail.com>
2024-04-22 10:34:59 +0100

    add failing test for bridging a custom gas token to L2

---
+++ op-e2e/custom_gas_token_bridge_test.go
@@ -0,0 +1,95 @@
+package op_e2e
+
+import (
+	"context"
+	"math"
+	"math/big"
+	"testing"
+
+	"github.com/ethereum-optimism/optimism/op-bindings/bindings"
+	"github.com/ethereum-optimism/optimism/op-e2e/e2eutils/receipts"
+	"github.com/ethereum-optimism/optimism/op-e2e/e2eutils/transactions"
+	"github.com/ethereum-optimism/optimism/op-e2e/e2eutils/wait"
+	"github.com/ethereum-optimism/optimism/op-node/rollup/derive"
+	"github.com/ethereum-optimism/optimism/op-service/testlog"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/log"
+	"github.com/ethereum/go-ethereum/params"
+	"github.com/stretchr/testify/require"
+)
+
+func TestCustomGasTokenLockAndMint(t *testing.T) {
+	InitParallel(t)
+
+	cfg := DefaultSystemConfig(t)
+
+	sys, err := cfg.Start(t)
+	require.Nil(t, err, "Error starting up system")
+	defer sys.Close()
+
+	log := testlog.Logger(t, log.LevelInfo)
+	log.Info("genesis", "l2", sys.RollupConfig.Genesis.L2, "l1", sys.RollupConfig.Genesis.L1, "l2_time", sys.RollupConfig.Genesis.L2Time)
+
+	l1Client := sys.Clients["l1"]
+	l2Client := sys.Clients["sequencer"]
+
+	opts, err := bind.NewKeyedTransactorWithChainID(sys.Cfg.Secrets.Alice, cfg.L1ChainIDBig())
+	require.Nil(t, err)
+
+	// Deploy WETH
+	_, tx, WETH, err := bindings.DeployWETH(opts, l1Client)
+	require.NoError(t, err)
+	_, err = wait.ForReceiptOK(context.Background(), l1Client, tx.Hash())
+	require.NoError(t, err, "Waiting for deposit tx on L1")
+
+	// Get some WETH
+	opts.Value = big.NewInt(params.Ether)
+	tx, err = WETH.Deposit(opts)
+	require.NoError(t, err)
+	_, err = wait.ForReceiptOK(context.Background(), l1Client, tx.Hash())
+	require.NoError(t, err)
+	opts.Value = nil
+	wethBalance, err := WETH.BalanceOf(&bind.CallOpts{}, opts.From)
+	require.NoError(t, err)
+	require.Equal(t, big.NewInt(params.Ether), wethBalance)
+
+	// Approve WETH with the bridge
+	tx, err = WETH.Approve(opts, cfg.L1Deployments.OptimismPortal, new(big.Int).SetUint64(math.MaxUint64))
+	require.NoError(t, err)
+	_, err = wait.ForReceiptOK(context.Background(), l1Client, tx.Hash())
+	require.NoError(t, err)
+
+	// Bridge the WETH]
+	portal, err := bindings.NewOptimismPortal(cfg.L1Deployments.OptimismPortalProxy, l1Client)
+	require.NoError(t, err)
+
+	to := opts.From
+	mint := big.NewInt(100)
+	value := big.NewInt(0)
+	isCreation := false
+	data := []byte{}
+
+	tx, err = transactions.PadGasEstimate(opts, 1.1, func(opts *bind.TransactOpts) (*types.Transaction, error) {
+		return portal.DepositERC20Transaction(opts, to, mint, value, 100000, isCreation, data)
+	})
+	require.NoError(t, err)
+	depositReceipt, err := wait.ForReceiptOK(context.Background(), l1Client, tx.Hash())
+	require.NoError(t, err)
+
+	t.Log("Deposit custom gas token through OptimismPortal", "gas used", depositReceipt.GasUsed)
+
+	// compute the deposit transaction hash + poll for it
+	depositEvent, err := receipts.FindLog(depositReceipt.Logs, portal.ParseTransactionDeposited)
+	require.NoError(t, err, "Should emit deposit event")
+
+	depositTx, err := derive.UnmarshalDepositLogEvent(&depositEvent.Raw)
+	require.NoError(t, err)
+	_, err = wait.ForReceiptOK(context.Background(), l2Client, types.NewTx(depositTx).Hash())
+	require.NoError(t, err)
+
+	// Should have balance on L2
+	l2Balance, err := l2Client.BalanceAt(context.Background(), opts.From, nil)
+	require.NoError(t, err)
+	require.Equal(t, l2Balance, big.NewInt(100))
+}
