12cdf8548ffb68ff0d4c32cb9535a0bedc57de44
Diego <105765223+0xfuturistic@users.noreply.github.com>
2024-04-22 21:40:17 -0300

    contracts-bedrock: add documentation for Transient library

--- packages/contracts-bedrock/src/libraries/Transient.sol
+++ packages/contracts-bedrock/src/libraries/Transient.sol
@@ -1,17 +1,27 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.24;
+pragma solidity ^0.8.24;
 
+/// @title Transient
+/// @notice Transient handles transient storage for reentrancy.
+///         Forked from https://github.com/jtriley-eth/tcontext
 library Transient {
+    /// @notice Slot for transient context.
     uint256 internal constant CONTEXT_SLOT = 0;
 
-    function getTransientContext() internal view returns (uint256 ctx) {
+    /// @notice Get the transient context.
+    /// @return _ctx Transient context.
+    function getTransientContext() internal view returns (uint256 _ctx) {
         assembly {
             mstore(0x00, tload(CONTEXT_SLOT))
-            ctx := keccak256(0x00, 0x20)
+            _ctx := keccak256(0x00, 0x20)
         }
     }
 
-    function setTransientValue(uint256 value, address target, bytes memory payload) public {
+    /// @notice Set a transient value.
+    /// @param _value   Value to set.
+    /// @param _target  Target contract to call.
+    /// @param _payload Payload to call target with.
+    function setTransientValue(uint256 _value, address _target, bytes memory _payload) public {
         assembly {
             tstore(CONTEXT_SLOT, add(tload(CONTEXT_SLOT), 1))
         }
@@ -19,21 +29,23 @@ library Transient {
         uint256 ctx = getTransientContext();
 
         assembly {
-            tstore(ctx, value)
+            tstore(ctx, _value)
         }
 
-        if (target == address(0)) return;
+        if (_target == address(0)) return;
 
-        (bool success,) = target.call(payload);
+        (bool success,) = _target.call(_payload);
 
         require(success, "setTransientValue::call");
     }
 
-    function getTransientValue() public view returns (uint256 value) {
+    /// @notice Get value in transient context.
+    /// @return _value Transient value.
+    function getTransientValue() public view returns (uint256 _value) {
         uint256 ctx = getTransientContext();
 
         assembly {
-            value := tload(ctx)
+            _value := tload(ctx)
         }
     }
 }
--- packages/contracts-bedrock/test/libraries/Transient.t.sol
+++ packages/contracts-bedrock/test/libraries/Transient.t.sol
@@ -1,61 +1,91 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.24;
+pragma solidity ^0.8.24;
 
+// Testing utilities
 import { Test } from "forge-std/Test.sol";
 
 // Target contract
 import { Transient } from "src/libraries/Transient.sol";
 
+/// @title Base
+/// @notice This contract uses the Transient library to set and get transient values.
 contract Base {
-    function setTransientValue(uint256 value, address target, bytes memory payload) public {
-        Transient.setTransientValue(value, target, payload);
+    /// @notice Set a transient value.
+    /// @param _value   Value to set.
+    /// @param _target  Target contract to call.
+    /// @param _payload Payload to call target with.
+    function setTransientValue(uint256 _value, address _target, bytes memory _payload) public {
+        Transient.setTransientValue(_value, _target, _payload);
     }
 
-    function getTransientValue() public view returns (uint256 value) {
+    /// @notice Get value in transient context.
+    /// @return _value Transient value.
+    function getTransientValue() public view returns (uint256 _value) {
         return Transient.getTransientValue();
     }
 }
 
+/// @title NonReentrant
+/// @notice This contract uses the Base contract to set a transient value.
 contract NonReentrant {
+    /// @notice Transient variable.
     uint256 public tVariable;
 
+    /// @notice Set the transient variable.
     function setTVariable() public {
         tVariable = Base(msg.sender).getTransientValue();
     }
 }
 
+/// @title Reentrant
+/// @notice This contract uses the Base contract to set a transient value and call a function that reads it.
 contract Reentrant {
+    /// @notice Transient variable.
     uint256 public tVariable;
 
+    /// @notice Set the transient variable and call a function that reads it.
     function reentrant() public {
-        Base(msg.sender).setTransientValue(696969, address(this), abi.encodeWithSelector(this.getTVariable.selector));
+        Base(msg.sender).setTransientValue(1, address(this), abi.encodeWithSelector(this.getTVariable.selector));
     }
 
+    /// @notice Get the transient variable.
     function getTVariable() public {
         tVariable = Base(msg.sender).getTransientValue();
     }
 }
 
+/// @title TransientTest
+/// @notice Tests the Transient library.
 contract TransientTest is Test {
+    /// @notice Base contract.
     Base base;
+
+    /// @notice NonReentrant contract.
     NonReentrant nonReentrant;
+
+    /// @notice Reentrant contract.
     Reentrant reentrant;
 
+    /// @notice Set up the test environment.
     function setUp() public {
         base = new Base();
         nonReentrant = new NonReentrant();
         reentrant = new Reentrant();
     }
 
-    function testTransientVariableNonReentrant(uint256 _value) public {
+    /// @notice Test setting a transient variable in a non-reentrant function.
+    /// @param _value Value to set.
+    function testFuzz_transient_nonReentrant_succeeds(uint256 _value) public {
         base.setTransientValue(_value, address(nonReentrant), abi.encodeCall(NonReentrant.setTVariable, ()));
 
         assertEq(_value, nonReentrant.tVariable());
     }
 
-    function testTransientVariableReentrant() public {
-        base.setTransientValue(69_420, address(reentrant), abi.encodeWithSelector(Reentrant.reentrant.selector));
+    /// @notice Test setting a transient variable in a reentrant function fails.
+    /// @param _value Value to fail to set.
+    function test_transient_reentrant_fails(uint256 _value) public {
+        base.setTransientValue(_value, address(reentrant), abi.encodeWithSelector(Reentrant.reentrant.selector));
 
-        assertEq(696969, reentrant.tVariable());
+        assertNotEq(_value, reentrant.tVariable());
     }
 }
