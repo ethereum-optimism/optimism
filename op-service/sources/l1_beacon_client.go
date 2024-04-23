69533547803be1c44da204faebf47859578d9e4b
zhiqiangxu <652732310@qq.com>
2024-04-23 20:40:05 +0800

    make l1.beacon-archiver work

--- op-service/sources/l1_beacon_client.go
+++ op-service/sources/l1_beacon_client.go
@@ -209,7 +209,7 @@ func (cl *L1BeaconClient) fetchSidecars(ctx context.Context, slot uint64, hashes
 	for i := 0; i < cl.pool.Len(); i++ {
 		f := cl.pool.Get()
 		resp, err := f.BeaconBlobSideCars(ctx, cl.cfg.FetchAllSidecars, slot, hashes)
-		if err != nil {
+		if err != nil || len(resp.Data) != len(hashes) {
 			cl.pool.MoveToNext()
 			errs = append(errs, err)
 		} else {
