c3b5969d3423c727a37d8190c492d1c498758cbe
Diego <105765223+0xfuturistic@users.noreply.github.com>
2024-04-23 18:58:41 -0300

    contracts-bedrock: refactor tests for TransientContext

--- packages/contracts-bedrock/test/libraries/TransientContext.t.sol
+++ packages/contracts-bedrock/test/libraries/TransientContext.t.sol
@@ -8,20 +8,36 @@ import { Test } from "forge-std/Test.sol";
 import { TransientContext } from "src/libraries/TransientContext.sol";
 
 contract TransientContextTest is Test {
-    function test_increment_zero_succeeds(bytes32 _slot) public view {
-        assertEq(TransientContext.get(_slot), 0);
+    bytes32 internal constant CALL_DEPTH_SLOT = 0x7a74fd168763fd280eaec3bcd2fd62d0e795027adc8183a693c497a7c2b10b5c;
+
+    function testFuzz_callDepth_succeeds(uint256 _value) public {
+        assembly {
+            tstore(CALL_DEPTH_SLOT, _value)
+        }
+        assertEq(TransientContext.callDepth(), _value);
     }
 
-    function test_increment_one_succeeds(bytes32 _slot) public view {
+    function test_increment_zero_succeeds() public view {
+        assertEq(TransientContext.get(CALL_DEPTH_SLOT), 0);
+    }
+
+    function test_increment_one_succeeds() public {
         TransientContext.increment();
-        assertEq(TransientContext.get(_slot), 1);
+        assertEq(TransientContext.get(CALL_DEPTH_SLOT), 1);
     }
 
-    function test_increment_many_succeeds(uint256 _times) public view {
+    function test_increment_many_succeeds(uint256 _times) public {
         for (uint256 i = 0; i < _times; i++) {
             TransientContext.increment();
-            assertEq(TransientContext.get(_slot), i + 1);
+            assertEq(TransientContext.get(CALL_DEPTH_SLOT), i + 1);
         }
+    }
+
+    function test_decrement_zero_succeeds() public {
+        assembly {
+            tstore(CALL_DEPTH_SLOT, 1)
+        }
+        assertEq(TransientContext.get(CALL_DEPTH_SLOT), 1);
     }
 }
 
