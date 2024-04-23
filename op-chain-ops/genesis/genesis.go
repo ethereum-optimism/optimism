45511494c3b2a0576dc207a9e201d13bc8a354b0
Danyal Prout <me@dany.al>
2024-04-22 19:27:42 -0500

    Enable Fjord in e2e tests / test for RIP-7212

--- op-chain-ops/genesis/genesis.go
+++ op-chain-ops/genesis/genesis.go
@@ -66,6 +66,7 @@ func NewL2Genesis(config *DeployConfig, block *types.Block) (*core.Genesis, erro
 		ShanghaiTime:                  config.CanyonTime(block.Time()),
 		CancunTime:                    config.EcotoneTime(block.Time()),
 		EcotoneTime:                   config.EcotoneTime(block.Time()),
+		FjordTime:                     config.FjordTime(block.Time()),
 		InteropTime:                   config.InteropTime(block.Time()),
 		Optimism: &params.OptimismConfig{
 			EIP1559Denominator:       eip1559Denom,
