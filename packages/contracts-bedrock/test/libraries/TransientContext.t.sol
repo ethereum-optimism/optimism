c30bf765b4744682aa96b98729800d4608a4000a
Diego <105765223+0xfuturistic@users.noreply.github.com>
2024-04-23 19:56:11 -0300

    contracts-bedrock: refactor tests for TransientContext

--- packages/contracts-bedrock/test/libraries/TransientContext.t.sol
+++ packages/contracts-bedrock/test/libraries/TransientContext.t.sol
@@ -10,34 +10,63 @@ import { TransientContext } from "src/libraries/TransientContext.sol";
 contract TransientContextTest is Test {
     bytes32 internal constant CALL_DEPTH_SLOT = 0x7a74fd168763fd280eaec3bcd2fd62d0e795027adc8183a693c497a7c2b10b5c;
 
-    function testFuzz_callDepth_succeeds(uint256 _value) public {
+    function testFuzz_callDepth_succeeds(uint256 _callDepth) public {
         assembly {
-            tstore(CALL_DEPTH_SLOT, _value)
+            tstore(CALL_DEPTH_SLOT, _callDepth)
         }
-        assertEq(TransientContext.callDepth(), _value);
+
+        assertEq(TransientContext.callDepth(), _callDepth);
     }
 
-    function test_increment_zero_succeeds() public view {
-        assertEq(TransientContext.get(CALL_DEPTH_SLOT), 0);
-    }
+    function testFuzz_increment_succeeds(uint256 _startingCallDepth) public {
+        vm.assume(_startingCallDepth < type(uint256).max);
+        assembly {
+            tstore(CALL_DEPTH_SLOT, _startingCallDepth)
+        }
+        assertEq(TransientContext.callDepth(), _startingCallDepth);
 
-    function test_increment_one_succeeds() public {
         TransientContext.increment();
-        assertEq(TransientContext.get(CALL_DEPTH_SLOT), 1);
+        assertEq(TransientContext.callDepth(), _startingCallDepth + 1);
     }
 
-    function test_increment_many_succeeds(uint256 _times) public {
-        for (uint256 i = 0; i < _times; i++) {
-            TransientContext.increment();
-            assertEq(TransientContext.get(CALL_DEPTH_SLOT), i + 1);
-        }
+    function testFuzz_increment_twice_succeeds(uint256 _startingCallDepth) public {
+        vm.assume(_startingCallDepth < type(uint256).max - 1);
+        testFuzz_increment_succeeds(_startingCallDepth);
+        assertEq(TransientContext.callDepth(), _startingCallDepth + 1);
+
+        TransientContext.increment();
+        assertEq(TransientContext.callDepth(), _startingCallDepth + 2);
     }
 
-    function test_decrement_zero_succeeds() public {
+    function testFuzz_decrement_succeeds(uint256 _startingCallDepth) public {
+        vm.assume(_startingCallDepth > 0);
         assembly {
-            tstore(CALL_DEPTH_SLOT, 1)
+            tstore(CALL_DEPTH_SLOT, _startingCallDepth)
         }
-        assertEq(TransientContext.get(CALL_DEPTH_SLOT), 1);
+        assertEq(TransientContext.callDepth(), _startingCallDepth);
+
+        TransientContext.decrement();
+        assertEq(TransientContext.callDepth(), _startingCallDepth - 1);
+    }
+
+    function testFuzz_decrement_twice_succeeds(uint256 _startingCallDepth) public {
+        vm.assume(_startingCallDepth > 1);
+        assembly {
+            tstore(CALL_DEPTH_SLOT, _startingCallDepth)
+        }
+        assertEq(TransientContext.callDepth(), _startingCallDepth);
+
+        TransientContext.decrement();
+        assertEq(TransientContext.callDepth(), _startingCallDepth - 1);
+
+        testFuzz_decrement_succeeds(_startingCallDepth - 1);
+    }
+
+    function test_decrement_fromZero_reverts() public {
+        assertEq(TransientContext.callDepth(), 0);
+
+        vm.expectRevert();
+        TransientContext.decrement();
     }
 }
 
