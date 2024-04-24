cf699b8a51c27c719ba0631cfd72dbd1130b3155
Diego <105765223+0xfuturistic@users.noreply.github.com>
2024-04-24 18:38:50 -0300

    contracts-bedrock: pin sol version of CrossL2Inbox to 0.8.25

--- packages/contracts-bedrock/src/L2/CrossL2Inbox.sol
+++ packages/contracts-bedrock/src/L2/CrossL2Inbox.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.24;
+pragma solidity 0.8.25;
 
 import { Predeploys } from "src/libraries/Predeploys.sol";
 import { ISemver } from "src/universal/ISemver.sol";
