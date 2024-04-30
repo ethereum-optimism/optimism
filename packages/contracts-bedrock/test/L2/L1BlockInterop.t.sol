87267f32775f26191508ed79763f5700252b97c6
Diego <105765223+0xfuturistic@users.noreply.github.com>
2024-04-30 16:31:11 -0300

    contracts-bedrock: refactor tests for L1BlockInterop

--- packages/contracts-bedrock/test/L2/L1BlockInterop.t.sol
+++ packages/contracts-bedrock/test/L2/L1BlockInterop.t.sol
@@ -32,7 +32,6 @@ contract L1BlockInterop_Test is Test {
         vm.assume(dependencySet.length <= type(uint8).max);
         vm.assume(uint160(uint256(batcherHash)) == uint256(batcherHash));
 
-
         bytes memory functionCallDataPacked = Encoding.encodeSetL1BlockValuesInterop({
             _baseFeeScalar: baseFeeScalar,
             _blobBaseFeeScalar: blobBaseFeeScalar,
@@ -46,11 +45,9 @@ contract L1BlockInterop_Test is Test {
             _dependencySet: dependencySet
         });
 
-
         vm.prank(depositor);
         (bool success,) = address(l1Block).call(functionCallDataPacked);
         assertTrue(success, "Function call failed");
-
 
         assertEq(l1Block.baseFeeScalar(), baseFeeScalar);
         assertEq(l1Block.blobBaseFeeScalar(), blobBaseFeeScalar);
@@ -67,22 +64,18 @@ contract L1BlockInterop_Test is Test {
             assertTrue(l1Block.isInDependencySet(dependencySet[i]));
         }
 
-
         // ensure we didn't accidentally pollute the 128 bits of the sequencenum+scalars slot that
         // should be empty
         bytes32 scalarsSlot = vm.load(address(l1Block), bytes32(uint256(3)));
         bytes32 mask128 = hex"FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF00000000000000000000000000000000";
 
-
         assertEq(0, scalarsSlot & mask128);
-
 
         // ensure we didn't accidentally pollute the 128 bits of the number & timestamp slot that
         // should be empty
         bytes32 numberTimestampSlot = vm.load(address(l1Block), bytes32(uint256(0)));
         assertEq(0, numberTimestampSlot & mask128);
     }
-
 
     /// @dev Tests that `setL1BlockValuesInterop` succeeds if sender address is the depositor
     function test_setL1BlockValuesInterop_isDepositor_succeeds() external {
@@ -99,15 +92,13 @@ contract L1BlockInterop_Test is Test {
             _dependencySet: new uint256[](0)
         });
 
-
         vm.prank(depositor);
         (bool success,) = address(l1Block).call(functionCallDataPacked);
         assertTrue(success, "function call failed");
     }
 
-
-    /// @dev Tests that `setL1BlockValuesInterop` fails if sender address is not the depositor
-    function test_setL1BlockValuesInterop_isDepositor_fails() external {
+    /// @dev Tests that `setL1BlockValuesInterop` reverts if sender address is not the depositor
+    function test_setL1BlockValuesInterop_isDepositor_reverts() external {
         bytes memory functionCallDataPacked = Encoding.encodeSetL1BlockValuesInterop({
             _baseFeeScalar: type(uint32).max,
             _blobBaseFeeScalar: type(uint32).max,
@@ -121,13 +112,11 @@ contract L1BlockInterop_Test is Test {
             _dependencySet: new uint256[](0)
         });
 
-
         (bool success, bytes memory data) = address(l1Block).call(functionCallDataPacked);
         assertTrue(!success, "function call should have failed");
         // make sure return value is the expected function selector for "NotDepositor()"
         assertEq(bytes4(data), NotDepositor.selector);
     }
-
 
     /// @dev Tests that `setL1BlockValuesInterop` succeeds if _dependencySetSize is the same as
     ///      the length of _dependencySet. (happy path)
@@ -135,7 +124,6 @@ contract L1BlockInterop_Test is Test {
         external
     {
         vm.assume(dependencySet.length <= type(uint8).max);
-
 
         bytes memory functionCallDataPacked = Encoding.encodeSetL1BlockValuesInterop({
             _baseFeeScalar: type(uint32).max,
@@ -150,16 +138,14 @@ contract L1BlockInterop_Test is Test {
             _dependencySet: dependencySet
         });
 
-
         vm.prank(depositor);
         (bool success,) = address(l1Block).call(functionCallDataPacked);
         assertTrue(success, "function call failed");
     }
 
-
-    /// @dev Tests that `setL1BlockValuesInterop` fails if _dependencySetSize is not the same as
+    /// @dev Tests that `setL1BlockValuesInterop` reverts if _dependencySetSize is not the same as
     ///      the length of _dependencySet. (bad path)
-    function testFuzz_setL1BlockValuesInterop_dependencySetSizeMatch_fails(
+    function testFuzz_setL1BlockValuesInterop_dependencySetSizeMatch_reverts(
         uint8 notDependencySetSize,
         uint256[] calldata dependencySet
     )
@@ -168,11 +154,12 @@ contract L1BlockInterop_Test is Test {
         vm.assume(dependencySet.length <= type(uint8).max);
         vm.assume(notDependencySetSize != dependencySet.length);
 
-
         bytes memory functionCallDataPacked = abi.encodePacked(
             bytes4(keccak256("setL1BlockValuesInterop()")),
             type(uint32).max,
             type(uint32).max,
+            type(uint64).max,
+            type(uint64).max,
             type(uint64).max,
             type(uint256).max,
             type(uint256).max,
@@ -182,14 +169,12 @@ contract L1BlockInterop_Test is Test {
             dependencySet
         );
 
-
         vm.prank(depositor);
         (bool success, bytes memory data) = address(l1Block).call(functionCallDataPacked);
         assertTrue(!success, "function call should have failed");
         // make sure return value is the expected function selector for "DependencySetSizeMismatch()"
         assertEq(bytes4(data), DependencySetSizeMismatch.selector);
     }
-
 
     /// @dev Tests that an arbitrary dependency set can be set and that ìsInDependencySet returns
     ///      the expected results.
@@ -210,7 +195,6 @@ contract L1BlockInterop_Test is Test {
         vm.assume(dependencySet.length <= type(uint8).max);
         vm.assume(uint160(uint256(batcherHash)) == uint256(batcherHash));
 
-
         bytes memory functionCallDataPacked = Encoding.encodeSetL1BlockValuesInterop({
             _baseFeeScalar: baseFeeScalar,
             _blobBaseFeeScalar: blobBaseFeeScalar,
@@ -224,35 +208,28 @@ contract L1BlockInterop_Test is Test {
             _dependencySet: dependencySet
         });
 
-
         vm.prank(depositor);
         (bool success,) = address(l1Block).call(functionCallDataPacked);
         assertTrue(success, "Function call failed");
 
-
         assertEq(l1Block.dependencySetSize(), dependencySet.length);
-
 
         for (uint256 i = 0; i < dependencySet.length; i++) {
             assertTrue(l1Block.isInDependencySet(dependencySet[i]));
         }
     }
 
-
     /// @dev Tests that `isInDependencySet` returns true when the current chain ID is passed as the input
     function test_isInDependencySet_isChainId_succeeds() external view {
         assertTrue(l1Block.isInDependencySet(block.chainid));
     }
 
-
-    /// @dev Tests that `isInDependencySet` fails when the input chain ID is not in the dependency set
-    function testFuzz_isInDependencySet_fails(uint256 chainId) external {
+    /// @dev Tests that `isInDependencySet` reverts when the input chain ID is not in the dependency set
+    function testFuzz_isInDependencySet_reverts(uint256 chainId) external {
         vm.assume(chainId != 1);
-
 
         uint256[] memory dependencySet = new uint256[](1);
         dependencySet[0] = 1;
-
 
         bytes memory functionCallDataPacked = Encoding.encodeSetL1BlockValuesInterop({
             _baseFeeScalar: 0,
@@ -267,18 +244,15 @@ contract L1BlockInterop_Test is Test {
             _dependencySet: dependencySet
         });
 
-
         vm.prank(depositor);
         (bool success,) = address(l1Block).call(functionCallDataPacked);
         assertTrue(success, "Function call failed");
 
-
         assertFalse(l1Block.isInDependencySet(chainId));
     }
 
-
     /// @dev Tests that `isInDependencySet` returns false when the dependency set is empty
-    function testFuzz_isInDependencySet_dependencySetEmpty_fails(uint256 chainId) external view {
+    function testFuzz_isInDependencySet_dependencySetEmpty_succeeds(uint256 chainId) external view {
         assertTrue(l1Block.dependencySetSize() == 0);
         assertFalse(l1Block.isInDependencySet(chainId));
     }
