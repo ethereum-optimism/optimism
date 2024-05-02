8f55a2be689357849ae16937ef188d0ce92ddcc9
Mark Tyneway <mark.tyneway@gmail.com>
2024-05-01 22:24:34 -0600

    snapshots: regenerate

--- packages/contracts-bedrock/test/kontrol/proofs/utils/DeploymentSummary.sol
+++ packages/contracts-bedrock/test/kontrol/proofs/utils/DeploymentSummary.sol
@@ -17,7 +17,7 @@ contract DeploymentSummary is DeploymentSummaryCode {
     address internal constant l1CrossDomainMessengerProxyAddress = 0x20A42a5a785622c6Ba2576B2D6e924aA82BFA11D;
     address internal constant l1ERC721BridgeAddress = 0x44637A4292E0CD2B17A55d5F6B2F05AFcAcD0586;
     address internal constant l1ERC721BridgeProxyAddress = 0xDeF3bca8c80064589E6787477FFa7Dd616B5574F;
-    address internal constant l1StandardBridgeAddress = 0x1870eE1cA086E5EC25C9F8786FE05a3deF3877e6;
+    address internal constant l1StandardBridgeAddress = 0x04c50B398Cd4182428E79f7186b7C919cF17e86F;
     address internal constant l1StandardBridgeProxyAddress = 0x0c8b5822b6e02CDa722174F19A1439A7495a3fA6;
     address internal constant l2OutputOracleAddress = 0x19652082F846171168Daf378C4fD3ee85a0D4A60;
     address internal constant l2OutputOracleProxyAddress = 0x8B71b41D4dBEb2b6821d44692d3fACAAf77480Bb;
@@ -344,7 +344,7 @@ contract DeploymentSummary is DeploymentSummaryCode {
         value = hex"0000000000000000000000000000000000000000000000000000000000000005";
         vm.store(systemOwnerSafeAddress, slot, value);
         slot = hex"360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc";
-        value = hex"0000000000000000000000001870ee1ca086e5ec25c9f8786fe05a3def3877e6";
+        value = hex"00000000000000000000000004c50b398cd4182428e79f7186b7c919cf17e86f";
         vm.store(l1StandardBridgeProxyAddress, slot, value);
         slot = hex"0000000000000000000000000000000000000000000000000000000000000000";
         value = hex"0000000000000000000000000000000000000000000000000000000000000001";
