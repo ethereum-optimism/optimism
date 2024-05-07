d0fcfec58710ed7054aa3ae802f58f6c8fdfcc57
clabby <ben@clab.by>
2024-05-07 16:40:04 -0400

    Update `check-snapshots`

--- packages/contracts-bedrock/scripts/checks/check-snapshots.sh
+++ packages/contracts-bedrock/scripts/checks/check-snapshots.sh
@@ -4,6 +4,8 @@ set -euo pipefail
 
 # Generate the snapshots
 pnpm snapshots
+pnpm kontrol-summary
+pnpm kontrol-summary-fp
 
 # Check if the generated snapshots are different from the committed snapshots
 if git diff --exit-code snapshots > /dev/null; then
