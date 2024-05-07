84d516a8833cdee2147f126e6fb8ebbcb10ad20e
Mariano Cortesi <mariano@clabs.co>
2024-05-07 16:56:31 -0300

    Remove frozen blocks from nonAncient DB

--- op-chain-ops/cmd/celo-dbmigrate/main.go
+++ op-chain-ops/cmd/celo-dbmigrate/main.go
@@ -143,7 +143,7 @@ func migrateAncientsDb(oldDBPath, newDBPath string, batchSize uint64) (uint64, e
 	return numAncientsNew, nil
 }
 
-func migrateNonAncientsDb(oldDbPath, newDbPath string, fromBlock, batchSize uint64) (uint64, error) {
+func migrateNonAncientsDb(oldDbPath, newDbPath string, lastAncientBlock, batchSize uint64) (uint64, error) {
 	// First copy files from old database to new database
 	log.Info("Copy files from old database", "process", "db migration")
 	cmd := exec.Command("rsync", "-v", "-a", "--exclude=ancient", oldDbPath+"/", newDbPath)
@@ -165,7 +165,7 @@ func migrateNonAncientsDb(oldDbPath, newDbPath string, fromBlock, batchSize uint
 	lastMigratedBlock := readLastMigratedBlock(newDB)
 
 	// if migration was interrupted, start from the last migrated block
-	fromBlock = max(fromBlock, lastMigratedBlock+1)
+	fromBlock := max(lastAncientBlock, lastMigratedBlock) + 1
 
 	log.Info("Migration started", "process", "db migration", "startBlock", fromBlock, "endBlock", lastBlock, "count", lastBlock-fromBlock)
 
@@ -189,25 +189,33 @@ func migrateNonAncientsDb(oldDbPath, newDbPath string, fromBlock, batchSize uint
 			}
 
 			// write header and body
-			rawdb.WriteBodyRLP(newDB, numberHash.Hash, numberHash.Number, newBody)
-
-			if err := newDB.Put(celo1.HeaderKey(numberHash.Number, numberHash.Hash), newHeader); err != nil {
-				return 0, fmt.Errorf("can't write header to new database: %v", err)
+			batch := newDB.NewBatch()
+			rawdb.WriteBodyRLP(batch, numberHash.Hash, numberHash.Number, newBody)
+			_ = batch.Put(celo1.HeaderKey(numberHash.Number, numberHash.Hash), newHeader)
+			_ = writeLastMigratedBlock(batch, numberHash.Number)
+			if err := batch.Write(); err != nil {
+				return 0, fmt.Errorf("failed to write header and body: block %d - %x: %w", numberHash.Number, numberHash.Hash, err)
 			}
-
-			if err = writeLastMigratedBlock(newDB, numberHash.Number); err != nil {
-				return 0, fmt.Errorf("failed to write last migration number: %v", err)
-			}
-
 		}
+	}
+
+	toBeRemoved := rawdb.ReadAllHashesInRange(newDB, 1, lastAncientBlock)
+	log.Info("Removing frozen blocks", "process", "db migration", "count", len(toBeRemoved))
+	batch := newDB.NewBatch()
+	for _, numberHash := range toBeRemoved {
+		rawdb.DeleteBlockWithoutNumber(batch, numberHash.Hash, numberHash.Number)
+		rawdb.DeleteCanonicalHash(batch, numberHash.Number)
+	}
+	if err := batch.Write(); err != nil {
+		return 0, fmt.Errorf("failed to delete frozen blocks: %w", err)
 	}
 
 	// if migration finished, remove the last migration number
 	if err := deleteLastMigratedBlock(newDB); err != nil {
 		return 0, fmt.Errorf("failed to delete last migration number: %v", err)
 	}
+	log.Info("Migration ended", "process", "db migration", "migratedBlocks", lastBlock-fromBlock+1, "removedBlocks", len(toBeRemoved))
 
-	log.Info("Migration ended", "process", "db migration", "migratedBlocks", lastBlock-fromBlock+1)
 	return lastBlock - fromBlock + 1, nil
 }
 
