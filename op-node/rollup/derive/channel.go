d50841257e382375e4d907beee7b021c0d05c77f
Cody Wang <cody.wang@coinbase.com>
2024-05-11 16:41:27 -0700

    fix error msg

--- op-node/rollup/derive/channel.go
+++ op-node/rollup/derive/channel.go
@@ -178,7 +178,7 @@ func BatchReader(r io.Reader, maxRLPBytesPerChannel uint64, isFjord bool) (func(
 	} else if compressionType[0] == ChannelVersionBrotli {
 		// If before Fjord, we cannot accept brotli compressed batch
 		if !isFjord {
-			return nil, fmt.Errorf("cannot accept brotli compressed batch after Fjord")
+			return nil, fmt.Errorf("cannot accept brotli compressed batch before Fjord")
 		}
 		// discard the first byte
 		_, err := bufReader.Discard(1)
