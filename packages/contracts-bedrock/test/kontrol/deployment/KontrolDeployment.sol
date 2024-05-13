bd7a75582db4d7f8170868920575fcd0421b5dec
clabby <ben@clab.by>
2024-05-13 13:08:56 -0400

    feat(ctb): Support `OptimismPortal2` in kontrol tests (#10429)
    * feat(ctb): Support `OptimismPortal2` in kontrol tests
    
    Adds a second `DeploymentSummary` to `kontrol` for fault proofs, and
    ports the existing `OptimismPortal` proofs with the `OptimismPortal2`.
    
    * summary tests
    
    * Update packages/contracts-bedrock/test/kontrol/README.md
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    
    * tests
    
    * Update `check-snapshots`
    
    * Update packages/contracts-bedrock/test/kontrol/README.md
    
    Co-authored-by: Matt Solomon <matt@mattsolomon.dev>
    
    * Separate snapshot gen
    
    ---------
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    Co-authored-by: Matt Solomon <matt@mattsolomon.dev>

--- packages/contracts-bedrock/test/kontrol/deployment/KontrolDeployment.sol
+++ packages/contracts-bedrock/test/kontrol/deployment/KontrolDeployment.sol
@@ -32,4 +32,46 @@ contract KontrolDeployment is Deploy {
         initializeL1CrossDomainMessenger();
         initializeOptimismPortal();
     }
+
+    function runKontrolDeploymentFaultProofs() public stateDiff {
+        deploySafe("SystemOwnerSafe");
+        setupSuperchain();
+
+        // deployProxies();
+        deployERC1967Proxy("OptimismPortalProxy");
+        deployERC1967Proxy("DisputeGameFactoryProxy");
+        deployERC1967Proxy("AnchorStateRegistryProxy");
+        deployERC1967Proxy("DelayedWETHProxy");
+        deployERC1967Proxy("SystemConfigProxy");
+        deployL1StandardBridgeProxy();
+        deployL1CrossDomainMessengerProxy();
+        deployERC1967Proxy("L1ERC721BridgeProxy");
+        transferAddressManagerOwnership(); // to the ProxyAdmin
+
+        // deployImplementations();
+        deployOptimismPortal2();
+        deployL1CrossDomainMessenger();
+        deploySystemConfig();
+        deployL1StandardBridge();
+        deployL1ERC721Bridge();
+        deployDisputeGameFactory();
+        deployDelayedWETH();
+        deployPreimageOracle();
+        deployMips();
+        deployAnchorStateRegistry();
+
+        // initializeImplementations();
+        initializeSystemConfig();
+        initializeL1StandardBridge();
+        initializeL1ERC721Bridge();
+        initializeL1CrossDomainMessenger();
+        initializeDisputeGameFactory();
+        initializeDelayedWETH();
+        initializeAnchorStateRegistry();
+        initializeOptimismPortal2();
+
+        // Set dispute game implementations in DGF
+        setCannonFaultGameImplementation({ _allowUpgrade: false });
+        setPermissionedCannonFaultGameImplementation({ _allowUpgrade: false });
+    }
 }
