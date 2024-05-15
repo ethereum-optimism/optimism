e30ccc041a8ed8854be724f54e3eb707dfa15789
Matt Solomon <matt@mattsolomon.dev>
2024-05-15 13:55:41 -0700

    fix(ctb): kontrol snapshots (#10557)
    * fix(ctb): snapshots
    
    * ci: fix snapshot check command to also consider kontrol
    
    * ctb: Disable the LivenessModule after ownership is transferred to
    fallback owner
    
    ctb: Use deactivated flag
    
    ctb: Update snapshots
    
    * ctb: Update LM deactivation
    
    Co-authored-by: Matt Solomon <matt@mattsolomon.dev>
    
    ---------
    
    Co-authored-by: Maurelian <maurelian@protonmail.ch>
    Co-authored-by: Maurelian <john@oplabs.co>

--- packages/contracts-bedrock/scripts/checks/check-snapshots.sh
+++ packages/contracts-bedrock/scripts/checks/check-snapshots.sh
@@ -5,9 +5,9 @@ set -euo pipefail
 # Generate the snapshots
 pnpm snapshots
 
-# Check if the generated snapshots are different from the committed snapshots
-if git diff --exit-code snapshots > /dev/null; then
-  [ -z "$(git ls-files --others --exclude-standard snapshots)" ] || exit 1
+# Check if the generated `snapshots` or `test/kontrol` files are different from the committed versions
+if git diff --exit-code snapshots test/kontrol > /dev/null; then
+  [ -z "$(git ls-files --others --exclude-standard snapshots test/kontrol)" ] || exit 1
 else
   exit 1
 fi
