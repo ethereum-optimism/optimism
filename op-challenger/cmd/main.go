27d2f16312e8416c0d947c2820c831285d69ccd1
Adrian Sutton <adrian@oplabs.co>
2024-05-16 09:17:17 +1000

    op-challenger: Fix highest acted L1 block metric for completed games (#10549)
    * op-challenger: Fix highest acted L1 block metric for completed games
    
    Adds an e2e test to prove it really does work.
    
    * op-challenger: Add more context to comment.

--- op-challenger/cmd/main.go
+++ op-challenger/cmd/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"os"
 
+	"github.com/ethereum-optimism/optimism/op-challenger/metrics"
 	"github.com/urfave/cli/v2"
 
 	"github.com/ethereum/go-ethereum/log"
@@ -29,7 +30,9 @@ var VersionWithMeta = opservice.FormatVersion(version.Version, GitCommit, GitDat
 func main() {
 	args := os.Args
 	ctx := opio.WithInterruptBlocker(context.Background())
-	if err := run(ctx, args, challenger.Main); err != nil {
+	if err := run(ctx, args, func(ctx context.Context, l log.Logger, config *config.Config) (cliapp.Lifecycle, error) {
+		return challenger.Main(ctx, l, config, metrics.NewMetrics())
+	}); err != nil {
 		log.Crit("Application failed", "err", err)
 	}
 }
