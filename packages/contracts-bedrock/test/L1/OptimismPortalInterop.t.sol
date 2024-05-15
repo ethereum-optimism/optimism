92cbd901fa2998e384e81dc625c9835d9a13e889
Diego <105765223+0xfuturistic@users.noreply.github.com>
2024-05-15 19:18:25 +0200

    contracts-bedrock: create tests for OptimismPortalInterop

---
+++ packages/contracts-bedrock/test/L1/OptimismPortalInterop.t.sol
@@ -0,0 +1,94 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.15;
+
+// Testing utilities
+import { CommonTest } from "test/setup/CommonTest.sol";
+
+// Libraries
+import { Constants } from "src/libraries/Constants.sol";
+import { Predeploys } from "src/libraries/Predeploys.sol";
+
+// Target contract dependencies
+import "src/libraries/PortalErrors.sol";
+import { OptimismPortalInterop } from "src/L1/OptimismPortalInterop.sol";
+import { L1BlockInterop, ConfigType } from "src/L2/L1BlockInterop.sol";
+
+contract OptimismPortalInterop_Test is CommonTest {
+    OptimismPortalInterop optimismPortalInterop;
+
+    /// @notice Marked virtual to be overridden in
+    ///         test/kontrol/deployment/DeploymentSummary.t.sol
+    function setUp() public virtual override {
+        super.setUp();
+        optimismPortalInterop = new OptimismPortalInterop();
+    }
+
+    /// @dev Tests that the config for the gas paying token can be set.
+    function testFuzz_setConfig_gasPayingToken_succeeds(bytes calldata _value) public {
+        vm.expectEmit(address(optimismPortalInterop));
+        emitTransactionDeposited({
+            _from: Constants.DEPOSITOR_ACCOUNT,
+            _to: Predeploys.L1_BLOCK_ATTRIBUTES,
+            _value: 0,
+            _mint: 0,
+            _gasLimit: 200_000,
+            _isCreation: false,
+            _data: abi.encodeCall(L1BlockInterop.setConfig, (ConfigType.GAS_PAYING_TOKEN, _value))
+        });
+
+        vm.prank(address(optimismPortalInterop.systemConfig()));
+        optimismPortalInterop.setConfig(ConfigType.GAS_PAYING_TOKEN, _value);
+    }
+
+    /// @dev Tests that setting the gas paying token config as not the system config reverts.
+    function testFuzz_setConfig_gasPayingToken_notSystemConfig_reverts(bytes calldata _value) public {
+        vm.expectRevert(Unauthorized.selector);
+        optimismPortalInterop.setConfig(ConfigType.GAS_PAYING_TOKEN, _value);
+    }
+
+    /// @dev Tests that the config for adding a dependency can be set.
+    function testFuzz_setConfig_addDependency_succeeds(bytes calldata _value) public {
+        vm.expectEmit(address(optimismPortalInterop));
+        emitTransactionDeposited({
+            _from: Constants.DEPOSITOR_ACCOUNT,
+            _to: Predeploys.L1_BLOCK_ATTRIBUTES,
+            _value: 0,
+            _mint: 0,
+            _gasLimit: 200_000,
+            _isCreation: false,
+            _data: abi.encodeCall(L1BlockInterop.setConfig, (ConfigType.ADD_DEPENDENCY, _value))
+        });
+
+        vm.prank(address(optimismPortalInterop.systemConfig()));
+        optimismPortalInterop.setConfig(ConfigType.ADD_DEPENDENCY, _value);
+    }
+
+    /// @dev Tests that setting the add dependency config as not the system config reverts.
+    function testFuzz_setConfig_addDependency_notSystemConfig_reverts(bytes calldata _value) public {
+        vm.expectRevert(Unauthorized.selector);
+        optimismPortalInterop.setConfig(ConfigType.ADD_DEPENDENCY, _value);
+    }
+
+    /// @dev Tests that the config for removing a dependency can be set.
+    function testFuzz_setConfig_removeDependency_succeeds(bytes calldata _value) public {
+        vm.expectEmit(address(optimismPortalInterop));
+        emitTransactionDeposited({
+            _from: Constants.DEPOSITOR_ACCOUNT,
+            _to: Predeploys.L1_BLOCK_ATTRIBUTES,
+            _value: 0,
+            _mint: 0,
+            _gasLimit: 200_000,
+            _isCreation: false,
+            _data: abi.encodeCall(L1BlockInterop.setConfig, (ConfigType.REMOVE_DEPENDENCY, _value))
+        });
+
+        vm.prank(address(optimismPortalInterop.systemConfig()));
+        optimismPortalInterop.setConfig(ConfigType.REMOVE_DEPENDENCY, _value);
+    }
+
+    /// @dev Tests that setting the remove dependency config as not the system config reverts.
+    function testFuzz_setConfig_removeDependency_notSystemConfig_reverts(bytes calldata _value) public {
+        vm.expectRevert(Unauthorized.selector);
+        optimismPortalInterop.setConfig(ConfigType.REMOVE_DEPENDENCY, _value);
+    }
+}
