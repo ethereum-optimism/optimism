41f8a3bfa296401a81e4ed4f42b2e1e5eaffef3c
Diego <105765223+0xfuturistic@users.noreply.github.com>
2024-05-16 20:41:49 +0200

    contracts-bedrock: update tests for L1BlockInterop

--- packages/contracts-bedrock/test/L2/L1BlockInterop.t.sol
+++ packages/contracts-bedrock/test/L2/L1BlockInterop.t.sol
@@ -1,234 +1,208 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.15;
 
-import { Test } from "forge-std/Test.sol";
-import { Encoding } from "src/libraries/Encoding.sol";
-import { L1BlockInterop, DependencySetSizeMismatch, NotDepositor } from "src/L2/L1BlockInterop.sol";
+import { CommonTest } from "test/setup/CommonTest.sol";
 
-contract L1BlockInteropTest is Test {
-    L1BlockInterop l1Block;
-    address depositor;
+// Target contract dependencies
+import { L1BlockInterop, ConfigType } from "src/L2/L1BlockInterop.sol";
 
-    function setUp() public {
-        l1Block = new L1BlockInterop();
-        depositor = l1Block.DEPOSITOR_ACCOUNT();
+contract L1BlockInteropTest is CommonTest {
+    /// @notice Thrown when a non-depositor account attempts to set L1 block values.
+    error NotDepositor();
+
+    /// @notice Error when a chain ID is not in the interop dependency set.
+    error NotDependency();
+
+    /// @notice Error when the interop dependency set size is too large.
+    error DependencySetSizeTooLarge();
+
+    /// @notice Error when the chain's chain ID is attempted to be removed from the interop dependency set.
+    error CantRemovedChainId();
+
+    /// @notice Event emitted when the gas paying token is set.
+    event GasPayingTokenSet(address indexed token, uint8 indexed decimals, bytes32 name, bytes32 symbol);
+
+    /// @notice Event emitted when a new dependency is added to the interop dependency set.
+    event DependencyAdded(uint256 indexed chainId);
+
+    /// @notice Event emitted when a dependency is removed from the interop dependency set.
+    event DependencyRemoved(uint256 indexed chainId);
+
+    /// @notice Marked virtual to be overridden in
+    ///         test/kontrol/deployment/DeploymentSummary.t.sol
+    function setUp() public virtual override {
+        super.setUp();
+        vm.etch(address(l1Block), address(new L1BlockInterop()).code);
     }
 
-    /// @dev Tests that setL1BlockValuesInterop updates the values appropriately.
-    function testFuzz_setL1BlockValuesInterop_succeeds(
-        uint32 _baseFeeScalar,
-        uint32 _blobBaseFeeScalar,
-        uint64 _sequenceNumber,
-        uint64 _timestamp,
-        uint64 _number,
-        uint256 _baseFee,
-        uint256 _blobBaseFee,
-        bytes32 _hash,
-        bytes32 _batcherHash,
-        uint256[] calldata _dependencySet
-    )
-        external
-    {
-        vm.assume(_dependencySet.length <= type(uint8).max);
-        vm.assume(uint160(uint256(_batcherHash)) == uint256(_batcherHash));
+    /// @dev Tests that an arbitrary chain ID can be added to the dependency set.
+    function testFuzz_isInDependencySet_succeeds(uint256 _chainId) public {
+        vm.assume(_chainId != 0xfbb67fda52d4bfb8bf);
 
-        bytes memory functionCallDataPacked = Encoding.encodeSetL1BlockValuesInterop({
-            _baseFeeScalar: _baseFeeScalar,
-            _blobBaseFeeScalar: _blobBaseFeeScalar,
-            _sequenceNumber: _sequenceNumber,
-            _timestamp: _timestamp,
-            _number: _number,
-            _baseFee: _baseFee,
-            _blobBaseFee: _blobBaseFee,
-            _hash: _hash,
-            _batcherHash: _batcherHash,
-            _dependencySet: _dependencySet
-        });
+        vm.prank(_l1BlockInterop().DEPOSITOR_ACCOUNT());
+        _l1BlockInterop().setConfig(ConfigType.ADD_DEPENDENCY, abi.encode(_chainId));
 
-        vm.prank(depositor);
-        (bool success,) = address(l1Block).call(functionCallDataPacked);
-        assertTrue(success, "Function call failed");
-
-        assertEq(l1Block.baseFeeScalar(), _baseFeeScalar);
-        assertEq(l1Block.blobBaseFeeScalar(), _blobBaseFeeScalar);
-        assertEq(l1Block.sequenceNumber(), _sequenceNumber);
-        assertEq(l1Block.timestamp(), _timestamp);
-        assertEq(l1Block.number(), _number);
-        assertEq(l1Block.basefee(), _baseFee);
-        assertEq(l1Block.blobBaseFee(), _blobBaseFee);
-        assertEq(l1Block.hash(), _hash);
-        assertEq(l1Block.batcherHash(), _batcherHash);
-        assertEq(l1Block.dependencySetSize(), _dependencySet.length);
-        for (uint256 i = 0; i < _dependencySet.length; i++) {
-            assertEq(l1Block.dependencySet(i), _dependencySet[i]);
-            assertTrue(l1Block.isInDependencySet(_dependencySet[i]));
-        }
-
-        // ensure we didn't accidentally pollute the 128 bits of the sequencenum+scalars slot that
-        // should be empty
-        bytes32 scalarsSlot = vm.load(address(l1Block), bytes32(uint256(3)));
-        bytes32 mask128 = hex"FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF00000000000000000000000000000000";
-
-        assertEq(0, scalarsSlot & mask128);
-
-        // ensure we didn't accidentally pollute the 128 bits of the number & timestamp slot that
-        // should be empty
-        bytes32 numberTimestampSlot = vm.load(address(l1Block), bytes32(uint256(0)));
-        assertEq(0, numberTimestampSlot & mask128);
+        assertTrue(_l1BlockInterop().isInDependencySet(_chainId));
     }
 
-    /// @dev Tests that `setL1BlockValuesInterop` succeeds if sender address is the depositor
-    function test_setL1BlockValuesInterop_isDepositor_succeeds() external {
-        bytes memory functionCallDataPacked = Encoding.encodeSetL1BlockValuesInterop({
-            _baseFeeScalar: type(uint32).max,
-            _blobBaseFeeScalar: type(uint32).max,
-            _sequenceNumber: type(uint64).max,
-            _timestamp: type(uint64).max,
-            _number: type(uint64).max,
-            _baseFee: type(uint256).max,
-            _blobBaseFee: type(uint256).max,
-            _hash: bytes32(type(uint256).max),
-            _batcherHash: bytes32(0),
-            _dependencySet: new uint256[](0)
-        });
-
-        vm.prank(depositor);
-        (bool success,) = address(l1Block).call(functionCallDataPacked);
-        assertTrue(success, "function call failed");
-    }
-
-    /// @dev Tests that `setL1BlockValuesInterop` reverts if sender address is not the depositor
-    function test_setL1BlockValuesInterop_isDepositor_reverts() external {
-        bytes memory functionCallDataPacked = Encoding.encodeSetL1BlockValuesInterop({
-            _baseFeeScalar: type(uint32).max,
-            _blobBaseFeeScalar: type(uint32).max,
-            _sequenceNumber: type(uint64).max,
-            _timestamp: type(uint64).max,
-            _number: type(uint64).max,
-            _baseFee: type(uint256).max,
-            _blobBaseFee: type(uint256).max,
-            _hash: bytes32(type(uint256).max),
-            _batcherHash: bytes32(0),
-            _dependencySet: new uint256[](0)
-        });
-
-        (bool success, bytes memory data) = address(l1Block).call(functionCallDataPacked);
-        assertTrue(!success, "function call should have failed");
-        // make sure return value is the expected function selector for "NotDepositor()"
-        assertEq(bytes4(data), NotDepositor.selector);
-    }
-
-    /// @dev Tests that `setL1BlockValuesInterop` reverts if _dependencySetSize is not the same as
-    ///      the length of _dependencySet. (bad path)
-    function testFuzz_setL1BlockValuesInterop_dependencySetSizeMatch_reverts(
-        uint8 _notDependencySetSize,
-        uint256[] calldata _dependencySet
-    )
-        external
-    {
-        vm.assume(_dependencySet.length <= type(uint8).max);
-        vm.assume(_notDependencySetSize != _dependencySet.length);
-
-        bytes memory functionCallDataPacked = abi.encodePacked(
-            bytes4(keccak256("setL1BlockValuesInterop()")),
-            type(uint32).max,
-            type(uint32).max,
-            type(uint64).max,
-            type(uint64).max,
-            type(uint64).max,
-            type(uint256).max,
-            type(uint256).max,
-            bytes32(type(uint256).max),
-            bytes32(type(uint256).max),
-            _notDependencySetSize,
-            _dependencySet
-        );
-
-        vm.prank(depositor);
-        (bool success, bytes memory data) = address(l1Block).call(functionCallDataPacked);
-        assertTrue(!success, "function call should have failed");
-        // make sure return value is the expected function selector for "DependencySetSizeMismatch()"
-        assertEq(bytes4(data), DependencySetSizeMismatch.selector);
-    }
-
-    /// @dev Tests that an arbitrary dependency set can be set and that ìsInDependencySet returns
-    ///      the expected results.
-    function testFuzz_isInDependencySet_succeeds(
-        uint32 _baseFeeScalar,
-        uint32 _blobBaseFeeScalar,
-        uint64 _sequenceNumber,
-        uint64 _timestamp,
-        uint64 _number,
-        uint256 _baseFee,
-        uint256 _blobBaseFee,
-        bytes32 _hash,
-        bytes32 _batcherHash,
-        uint256[] calldata _dependencySet
-    )
-        external
-    {
-        vm.assume(_dependencySet.length <= type(uint8).max);
-        vm.assume(uint160(uint256(_batcherHash)) == uint256(_batcherHash));
-
-        bytes memory functionCallDataPacked = Encoding.encodeSetL1BlockValuesInterop({
-            _baseFeeScalar: _baseFeeScalar,
-            _blobBaseFeeScalar: _blobBaseFeeScalar,
-            _sequenceNumber: _sequenceNumber,
-            _timestamp: _timestamp,
-            _number: _number,
-            _baseFee: _baseFee,
-            _blobBaseFee: _blobBaseFee,
-            _hash: _hash,
-            _batcherHash: _batcherHash,
-            _dependencySet: _dependencySet
-        });
-
-        vm.prank(depositor);
-        (bool success,) = address(l1Block).call(functionCallDataPacked);
-        assertTrue(success, "Function call failed");
-
-        assertEq(l1Block.dependencySetSize(), _dependencySet.length);
-
-        for (uint256 i = 0; i < _dependencySet.length; i++) {
-            assertTrue(l1Block.isInDependencySet(_dependencySet[i]));
-        }
-    }
-
-    /// @dev Tests that `isInDependencySet` returns true when the current chain ID is passed as the input
-    function test_isInDependencySet_isChainId_succeeds() external view {
-        assertTrue(l1Block.isInDependencySet(block.chainid));
+    /// @dev Tests that `isInDependencySet` returns true when the chain's chain ID is passed as the input.
+    function test_isInDependencySet_chainChainId_succeeds() public view {
+        assertTrue(_l1BlockInterop().isInDependencySet(block.chainid));
     }
 
     /// @dev Tests that `isInDependencySet` reverts when the input chain ID is not in the dependency set
-    function testFuzz_isInDependencySet_reverts(uint256 _chainId) external {
-        vm.assume(_chainId != 1);
+    ///      and is not the chain's chain ID.
+    function testFuzz_isInDependencySet_notDependency_reverts(uint256 _chainId) public view {
+        vm.assume(_chainId != block.chainid);
 
-        uint256[] memory dependencySet = new uint256[](1);
-        dependencySet[0] = 1;
-
-        bytes memory functionCallDataPacked = Encoding.encodeSetL1BlockValuesInterop({
-            _baseFeeScalar: 0,
-            _blobBaseFeeScalar: 0,
-            _sequenceNumber: 0,
-            _timestamp: 0,
-            _number: 0,
-            _baseFee: 0,
-            _blobBaseFee: 0,
-            _hash: bytes32(0),
-            _batcherHash: bytes32(0),
-            _dependencySet: dependencySet
-        });
-
-        vm.prank(depositor);
-        (bool success,) = address(l1Block).call(functionCallDataPacked);
-        assertTrue(success, "Function call failed");
-
-        assertFalse(l1Block.isInDependencySet(_chainId));
+        // Check that the chain ID is not in the dependency set
+        assertFalse(_l1BlockInterop().isInDependencySet(_chainId));
     }
 
-    /// @dev Tests that `isInDependencySet` returns false when the dependency set is empty
-    function testFuzz_isInDependencySet_dependencySetEmpty_succeeds(uint256 _chainId) external view {
-        assertTrue(l1Block.dependencySetSize() == 0);
-        assertFalse(l1Block.isInDependencySet(_chainId));
+    /// @dev Tests that `isInDependencySet` returns false when the dependency set is empty.
+    function testFuzz_isInDependencySet_dependencySetEmpty_succeeds(uint256 _chainId) public view {
+        vm.assume(_chainId != block.chainid);
+
+        assertTrue(_l1BlockInterop().dependencySetSize() == 0);
+
+        assertFalse(_l1BlockInterop().isInDependencySet(_chainId));
+    }
+
+    /// @dev Tests that the dependency set size is correct when adding an arbitrary number of chain IDs.
+    function testFuzz_dependencySetSize_succeeds(uint256[] calldata _dependencySet) public {
+        vm.startPrank(_l1BlockInterop().DEPOSITOR_ACCOUNT());
+
+        for (uint256 i = 0; i < _dependencySet.length; i++) {
+            // 0xfbb67fda52d4bfb8bf is Solady's EnumerableSetLib _ZERO_SENTINEL
+            vm.assume(_dependencySet[i] != 0xfbb67fda52d4bfb8bf);
+            _l1BlockInterop().setConfig(ConfigType.ADD_DEPENDENCY, abi.encode(_dependencySet[i]));
+        }
+
+        // Count the number of unique items in _dependencySet to compare with the dependency set size,
+        // since the dependency set is a set and should not contain duplicates
+        uint256 uniqueCount = 0;
+        bool found;
+        for (uint256 i = 0; i < _dependencySet.length; i++) {
+            found = false;
+            for (uint256 j = 0; j < i; j++) {
+                if (_dependencySet[i] == _dependencySet[j]) {
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                uniqueCount++;
+            }
+        }
+
+        assertEq(_l1BlockInterop().dependencySetSize(), uniqueCount);
+    }
+
+    /// @dev Tests that the dependency set size is correct when the dependency set is empty.
+    function test_dependencySetSize_dependencySetEmpty_succeeds() public view {
+        assertTrue(_l1BlockInterop().dependencySetSize() == 0);
+    }
+
+    /// @dev Tests that the config for the gas paying token can be set.
+    function testFuzz_setConfig_gasPayingToken_succeeds(
+        address _token,
+        uint8 _decimals,
+        bytes32 _name,
+        bytes32 _symbol
+    )
+        public
+    {
+        vm.expectEmit(address(l1Block));
+        emit GasPayingTokenSet({ token: _token, decimals: _decimals, name: _name, symbol: _symbol });
+
+        vm.prank(_l1BlockInterop().DEPOSITOR_ACCOUNT());
+        _l1BlockInterop().setConfig(ConfigType.GAS_PAYING_TOKEN, abi.encode(_token, _decimals, _name, _symbol));
+    }
+
+    /// @dev Tests that setting the gas paying token config as not the depositor reverts.
+    function testFuzz_setConfig_gasPayingToken_notDepositor_reverts(
+        address _token,
+        uint8 _decimals,
+        bytes32 _name,
+        bytes32 _symbol
+    )
+        public
+    {
+        vm.expectRevert(NotDepositor.selector);
+        _l1BlockInterop().setConfig(ConfigType.GAS_PAYING_TOKEN, abi.encode(_token, _decimals, _name, _symbol));
+    }
+
+    /// @dev Tests that the config for adding a dependency can be set.
+    function testFuzz_setConfig_addDependency_succeeds(uint256 _chainId) public {
+        vm.assume(_chainId != 0xfbb67fda52d4bfb8bf); // 0xfbb67fda52d4bfb8bf is Solady's EnumerableSetLib _ZERO_SENTINEL
+
+        vm.expectEmit(address(l1Block));
+        emit DependencyAdded(_chainId);
+
+        vm.prank(_l1BlockInterop().DEPOSITOR_ACCOUNT());
+        _l1BlockInterop().setConfig(ConfigType.ADD_DEPENDENCY, abi.encode(_chainId));
+    }
+
+    /// @dev Tests that setting the add dependency config as not the depositor reverts.
+    function testFuzz_setConfig_addDependency_notDepositor_reverts(uint256 _chainId) public {
+        vm.expectRevert(NotDepositor.selector);
+        _l1BlockInterop().setConfig(ConfigType.ADD_DEPENDENCY, abi.encode(_chainId));
+    }
+
+    /// @dev Tests that setting the add dependency config when the dependency set size is too large reverts.
+    function test_setConfig_addDependency_DependencySetSizeTooLarge_reverts() public {
+        vm.startPrank(_l1BlockInterop().DEPOSITOR_ACCOUNT());
+
+        for (uint256 i = 0; i < type(uint8).max; i++) {
+            _l1BlockInterop().setConfig(ConfigType.ADD_DEPENDENCY, abi.encode(i));
+        }
+
+        assertEq(_l1BlockInterop().dependencySetSize(), type(uint8).max);
+
+        vm.expectRevert(DependencySetSizeTooLarge.selector);
+        _l1BlockInterop().setConfig(ConfigType.ADD_DEPENDENCY, abi.encode(1));
+    }
+
+    /// @dev Tests that the config for removing a dependency can be set.
+    function testFuzz_setConfig_removeDependency_succeeds(uint256 _chainId) public {
+        vm.assume(_chainId != 0xfbb67fda52d4bfb8bf); // 0xfbb67fda52d4bfb8bf is Solady's EnumerableSetLib _ZERO_SENTINEL
+
+        vm.startPrank(_l1BlockInterop().DEPOSITOR_ACCOUNT());
+
+        // Add the chain ID to the dependency set before removing it
+        _l1BlockInterop().setConfig(ConfigType.ADD_DEPENDENCY, abi.encode(_chainId));
+
+        vm.expectEmit(address(l1Block));
+        emit DependencyRemoved(_chainId);
+
+        _l1BlockInterop().setConfig(ConfigType.REMOVE_DEPENDENCY, abi.encode(_chainId));
+    }
+
+    /// @dev Tests that setting the remove dependency config as not the depositor reverts.
+    function testFuzz_setConfig_removeDependency_notDepositor_reverts(uint256 _chainId) public {
+        vm.expectRevert(NotDepositor.selector);
+        _l1BlockInterop().setConfig(ConfigType.REMOVE_DEPENDENCY, abi.encode(_chainId));
+    }
+
+    /// @dev Tests that setting the remove dependency config for the chain's chain ID reverts.
+    function test_setConfig_removeDependency_chainChainId_reverts() public {
+        vm.startPrank(_l1BlockInterop().DEPOSITOR_ACCOUNT());
+        vm.expectRevert(CantRemovedChainId.selector);
+        _l1BlockInterop().setConfig(ConfigType.REMOVE_DEPENDENCY, abi.encode(block.chainid));
+    }
+
+    /// @dev Tests that setting the remove dependency config for a chain ID that is not in the dependency set reverts.
+    function testFuzz_setConfig_removeDependency_notDependency_reverts(uint256 _chainId) public {
+        vm.assume(_chainId != 0xfbb67fda52d4bfb8bf); // 0xfbb67fda52d4bfb8bf is Solady's EnumerableSetLib _ZERO_SENTINEL
+        vm.assume(_chainId != block.chainid);
+
+        vm.startPrank(_l1BlockInterop().DEPOSITOR_ACCOUNT());
+        vm.expectRevert(NotDependency.selector);
+        _l1BlockInterop().setConfig(ConfigType.REMOVE_DEPENDENCY, abi.encode(_chainId));
+    }
+
+    /// @dev Returns the L1BlockInterop instance.
+    function _l1BlockInterop() internal view returns (L1BlockInterop) {
+        return L1BlockInterop(address(l1Block));
     }
 }
