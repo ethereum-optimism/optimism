95bc82f9612445e0c444e90407f88ae65dac098a
zhiqiangxu <652732310@qq.com>
2024-05-17 22:40:36 +0800

    add test for BeaconBlobSideCars

--- op-service/sources/l1_beacon_client_test.go
+++ op-service/sources/l1_beacon_client_test.go
@@ -1,10 +1,18 @@
 package sources
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"errors"
+	"io"
+	"net/http"
+	"net/url"
+	"path"
+	"strconv"
 	"testing"
 
+	client_mocks "github.com/ethereum-optimism/optimism/op-service/client/mocks"
 	"github.com/ethereum-optimism/optimism/op-service/eth"
 	"github.com/ethereum-optimism/optimism/op-service/sources/mocks"
 	"github.com/ethereum/go-ethereum/crypto/kzg4844"
@@ -172,6 +180,36 @@ func TestBeaconClientFallback(t *testing.T) {
 	require.Equal(t, sidecars, resp)
 	require.NoError(t, err)
 
+}
+
+func TestBeaconHTTPClient(t *testing.T) {
+	c := client_mocks.NewHTTP(t)
+	b := NewBeaconHTTPClient(c)
+
+	ctx := context.Background()
+
+	indices := []uint64{3, 9, 11}
+	index0, _ := makeTestBlobSidecar(indices[0])
+	index1, _ := makeTestBlobSidecar(indices[1])
+	index2, _ := makeTestBlobSidecar(indices[2])
+
+	hashes := []eth.IndexedBlobHash{index0, index1, index2}
+
+	// mocks returning a 200 with empty list
+	respBytes, _ := json.Marshal(eth.APIGetBlobSidecarsResponse{})
+	slot := uint64(2)
+	path := path.Join(sidecarsMethodPrefix, strconv.FormatUint(slot, 10))
+	reqQuery := url.Values{}
+	for i := range hashes {
+		reqQuery.Add("indices", strconv.FormatUint(hashes[i].Index, 10))
+	}
+	headers := http.Header{}
+	headers.Add("Accept", "application/json")
+	c.EXPECT().Get(ctx, path, reqQuery, headers).Return(&http.Response{StatusCode: http.StatusOK, Body: io.NopCloser(bytes.NewReader(respBytes))}, nil)
+
+	// BeaconBlobSideCars should return error when client.HTTP returns a 200 with empty list
+	_, err := b.BeaconBlobSideCars(ctx, false, slot, hashes)
+	require.Error(t, err)
 }
 
 func TestClientPoolSingle(t *testing.T) {
