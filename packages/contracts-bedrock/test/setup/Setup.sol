06ce977d46d20d2d4e029804fa0586d977f4e9d0
Diego <105765223+0xfuturistic@users.noreply.github.com>
2024-05-17 18:09:26 +0200

    contracts-bedrock: add SystemConfigInterop to tests Setup

--- packages/contracts-bedrock/test/setup/Setup.sol
+++ packages/contracts-bedrock/test/setup/Setup.sol
@@ -30,6 +30,7 @@ import { L2Genesis, L1Dependencies, OutputMode } from "scripts/L2Genesis.s.sol";
 import { L2OutputOracle } from "src/L1/L2OutputOracle.sol";
 import { ProtocolVersions } from "src/L1/ProtocolVersions.sol";
 import { SystemConfig } from "src/L1/SystemConfig.sol";
+import { SystemConfigInterop } from "src/L1/SystemConfigInterop.sol";
 import { L1StandardBridge } from "src/L1/L1StandardBridge.sol";
 import { AddressManager } from "src/legacy/AddressManager.sol";
 import { L1ERC721Bridge } from "src/L1/L1ERC721Bridge.sol";
@@ -67,6 +68,7 @@ contract Setup {
     DelayedWETH delayedWeth;
     L2OutputOracle l2OutputOracle;
     SystemConfig systemConfig;
+    SystemConfigInterop systemConfigInterop;
     L1StandardBridge l1StandardBridge;
     L1CrossDomainMessenger l1CrossDomainMessenger;
     AddressManager addressManager;
@@ -131,6 +133,7 @@ contract Setup {
         delayedWeth = DelayedWETH(deploy.mustGetAddress("DelayedWETHProxy"));
         l2OutputOracle = L2OutputOracle(deploy.mustGetAddress("L2OutputOracleProxy"));
         systemConfig = SystemConfig(deploy.mustGetAddress("SystemConfigProxy"));
+        systemConfigInterop = SystemConfigInterop(deploy.mustGetAddress("SystemConfigProxy"));
         l1StandardBridge = L1StandardBridge(deploy.mustGetAddress("L1StandardBridgeProxy"));
         l1CrossDomainMessenger = L1CrossDomainMessenger(deploy.mustGetAddress("L1CrossDomainMessengerProxy"));
         addressManager = AddressManager(deploy.mustGetAddress("AddressManager"));
