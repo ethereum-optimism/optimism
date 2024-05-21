9e38b3f26c1cc1999f5d6bfd0e5564a5654553d6
Diego <105765223+0xfuturistic@users.noreply.github.com>
2024-05-21 13:59:17 +0200

    contracts-bedrock: use StaticConfig lib in tests for L1BlockInterop

--- packages/contracts-bedrock/test/L2/L1BlockInterop.t.sol
+++ packages/contracts-bedrock/test/L2/L1BlockInterop.t.sol
@@ -1,7 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.15;
 
+// Testing utilities
 import { CommonTest } from "test/setup/CommonTest.sol";
+
+// Libraries
+import { StaticConfig } from "src/libraries/StaticConfig.sol";
 
 // Target contract dependencies
 import { L1BlockInterop, ConfigType } from "src/L2/L1BlockInterop.sol";
@@ -29,7 +33,7 @@ contract L1BlockInteropTest is CommonTest {
     function testFuzz_isInDependencySet_succeeds(uint256 _chainId) public prankDepositor {
         vm.assume(_chainId != block.chainid);
 
-        _l1BlockInterop().setConfig(ConfigType.ADD_DEPENDENCY, abi.encode(_chainId));
+        _l1BlockInterop().setConfig(ConfigType.ADD_DEPENDENCY, StaticConfig.encodeAddDependency(_chainId));
 
         assertTrue(_l1BlockInterop().isInDependencySet(_chainId));
     }
@@ -65,7 +69,7 @@ contract L1BlockInteropTest is CommonTest {
 
         for (uint256 i = 0; i < _dependencySetSize; i++) {
             if (i == block.chainid) continue;
-            _l1BlockInterop().setConfig(ConfigType.ADD_DEPENDENCY, abi.encode(i));
+            _l1BlockInterop().setConfig(ConfigType.ADD_DEPENDENCY, StaticConfig.encodeAddDependency(i));
             uniqueCount++;
         }
 
@@ -90,7 +94,9 @@ contract L1BlockInteropTest is CommonTest {
         vm.expectEmit(address(l1Block));
         emit GasPayingTokenSet({ token: _token, decimals: _decimals, name: _name, symbol: _symbol });
 
-        _l1BlockInterop().setConfig(ConfigType.GAS_PAYING_TOKEN, abi.encode(_token, _decimals, _name, _symbol));
+        _l1BlockInterop().setConfig(
+            ConfigType.GAS_PAYING_TOKEN, StaticConfig.encodeSetGasPayingToken(_token, _decimals, _name, _symbol)
+        );
     }
 
     /// @dev Tests that setting the gas paying token config as not the depositor reverts.
@@ -103,7 +109,9 @@ contract L1BlockInteropTest is CommonTest {
         public
     {
         vm.expectRevert(NotDepositor.selector);
-        _l1BlockInterop().setConfig(ConfigType.GAS_PAYING_TOKEN, abi.encode(_token, _decimals, _name, _symbol));
+        _l1BlockInterop().setConfig(
+            ConfigType.GAS_PAYING_TOKEN, StaticConfig.encodeSetGasPayingToken(_token, _decimals, _name, _symbol)
+        );
     }
 
     /// @dev Tests that the config for adding a dependency can be set.
@@ -113,41 +121,41 @@ contract L1BlockInteropTest is CommonTest {
         vm.expectEmit(address(l1Block));
         emit DependencyAdded(_chainId);
 
-        _l1BlockInterop().setConfig(ConfigType.ADD_DEPENDENCY, abi.encode(_chainId));
+        _l1BlockInterop().setConfig(ConfigType.ADD_DEPENDENCY, StaticConfig.encodeAddDependency(_chainId));
     }
 
     /// @dev Tests that adding a dependency reverts if it's the chain's chain id
     function test_setConfig_addDependency_chainChainId_reverts() public prankDepositor {
         vm.expectRevert(AlreadyDependency.selector);
-        _l1BlockInterop().setConfig(ConfigType.ADD_DEPENDENCY, abi.encode(block.chainid));
+        _l1BlockInterop().setConfig(ConfigType.ADD_DEPENDENCY, StaticConfig.encodeAddDependency(block.chainid));
     }
 
     /// @dev Tests that adding a dependency already in the set reverts
     function test_setConfig_addDependency_alreadyDependency_reverts(uint256 _chainId) public prankDepositor {
         vm.assume(_chainId != block.chainid);
 
-        _l1BlockInterop().setConfig(ConfigType.ADD_DEPENDENCY, abi.encode(_chainId));
+        _l1BlockInterop().setConfig(ConfigType.ADD_DEPENDENCY, StaticConfig.encodeAddDependency(_chainId));
 
         vm.expectRevert(AlreadyDependency.selector);
-        _l1BlockInterop().setConfig(ConfigType.ADD_DEPENDENCY, abi.encode(_chainId));
+        _l1BlockInterop().setConfig(ConfigType.ADD_DEPENDENCY, StaticConfig.encodeAddDependency(_chainId));
     }
 
     /// @dev Tests that setting the add dependency config as not the depositor reverts.
     function testFuzz_setConfig_addDependency_notDepositor_reverts(uint256 _chainId) public {
         vm.expectRevert(NotDepositor.selector);
-        _l1BlockInterop().setConfig(ConfigType.ADD_DEPENDENCY, abi.encode(_chainId));
+        _l1BlockInterop().setConfig(ConfigType.ADD_DEPENDENCY, StaticConfig.encodeAddDependency(_chainId));
     }
 
     /// @dev Tests that setting the add dependency config when the dependency set size is too large reverts.
     function test_setConfig_addDependency_dependencySetSizeTooLarge_reverts() public prankDepositor {
         for (uint256 i = 0; i < type(uint8).max; i++) {
-            _l1BlockInterop().setConfig(ConfigType.ADD_DEPENDENCY, abi.encode(i));
+            _l1BlockInterop().setConfig(ConfigType.ADD_DEPENDENCY, StaticConfig.encodeAddDependency(i));
         }
 
         assertEq(_l1BlockInterop().dependencySetSize(), type(uint8).max);
 
         vm.expectRevert(DependencySetSizeTooLarge.selector);
-        _l1BlockInterop().setConfig(ConfigType.ADD_DEPENDENCY, abi.encode(1));
+        _l1BlockInterop().setConfig(ConfigType.ADD_DEPENDENCY, StaticConfig.encodeAddDependency(1));
     }
 
     /// @dev Tests that the config for removing a dependency can be set.
@@ -155,24 +163,24 @@ contract L1BlockInteropTest is CommonTest {
         vm.assume(_chainId != block.chainid);
 
         // Add the chain ID to the dependency set before removing it
-        _l1BlockInterop().setConfig(ConfigType.ADD_DEPENDENCY, abi.encode(_chainId));
+        _l1BlockInterop().setConfig(ConfigType.ADD_DEPENDENCY, StaticConfig.encodeAddDependency(_chainId));
 
         vm.expectEmit(address(l1Block));
         emit DependencyRemoved(_chainId);
 
-        _l1BlockInterop().setConfig(ConfigType.REMOVE_DEPENDENCY, abi.encode(_chainId));
+        _l1BlockInterop().setConfig(ConfigType.REMOVE_DEPENDENCY, StaticConfig.encodeRemoveDependency(_chainId));
     }
 
     /// @dev Tests that setting the remove dependency config as not the depositor reverts.
     function testFuzz_setConfig_removeDependency_notDepositor_reverts(uint256 _chainId) public {
         vm.expectRevert(NotDepositor.selector);
-        _l1BlockInterop().setConfig(ConfigType.REMOVE_DEPENDENCY, abi.encode(_chainId));
+        _l1BlockInterop().setConfig(ConfigType.REMOVE_DEPENDENCY, StaticConfig.encodeRemoveDependency(_chainId));
     }
 
     /// @dev Tests that setting the remove dependency config for the chain's chain ID reverts.
     function test_setConfig_removeDependency_chainChainId_reverts() public prankDepositor {
         vm.expectRevert(CantRemovedDependency.selector);
-        _l1BlockInterop().setConfig(ConfigType.REMOVE_DEPENDENCY, abi.encode(block.chainid));
+        _l1BlockInterop().setConfig(ConfigType.REMOVE_DEPENDENCY, StaticConfig.encodeRemoveDependency(block.chainid));
     }
 
     /// @dev Tests that setting the remove dependency config for a chain ID that is not in the dependency set reverts.
@@ -180,7 +188,7 @@ contract L1BlockInteropTest is CommonTest {
         vm.assume(_chainId != block.chainid);
 
         vm.expectRevert(NotDependency.selector);
-        _l1BlockInterop().setConfig(ConfigType.REMOVE_DEPENDENCY, abi.encode(_chainId));
+        _l1BlockInterop().setConfig(ConfigType.REMOVE_DEPENDENCY, StaticConfig.encodeRemoveDependency(_chainId));
     }
 
     /// @dev Returns the L1BlockInterop instance.
