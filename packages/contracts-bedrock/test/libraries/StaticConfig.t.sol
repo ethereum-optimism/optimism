250fe21a85d5ee08e2c9aa82d68bf581c975df91
Diego <105765223+0xfuturistic@users.noreply.github.com>
2024-05-23 11:58:53 +0200

    contracts-bedrock: use Test instead of CommonTest in tests for StaticConfig

--- packages/contracts-bedrock/test/libraries/StaticConfig.t.sol
+++ packages/contracts-bedrock/test/libraries/StaticConfig.t.sol
@@ -2,12 +2,20 @@
 pragma solidity 0.8.15;
 
 // Testing utilities
-import { CommonTest } from "test/setup/CommonTest.sol";
+import { Test } from "forge-std/Test.sol";
+import { FFIInterface } from "test/setup/FFIInterface.sol";
 
 // Target contract
 import { StaticConfig } from "src/libraries/StaticConfig.sol";
 
-contract StaticConfig_Test is CommonTest {
+contract StaticConfig_Test is Test {
+    FFIInterface constant ffi = FFIInterface(address(uint160(uint256(keccak256(abi.encode("optimism.ffi"))))));
+
+    function setUp() public {
+        vm.etch(address(ffi), vm.getDeployedCode("FFIInterface.sol:FFIInterface"));
+        vm.label(address(ffi), "FFIInterface");
+    }
+
     /// @dev Tests set gas paying token encoding.
     function testDiff_encodeSetGasPayingToken_succeeds(
         address _token,
