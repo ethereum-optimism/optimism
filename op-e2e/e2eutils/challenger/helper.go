a318967df7a66154f1d8ca6f1fffd5c80bcb153c
refcell <abigger87@gmail.com>
2024-05-31 09:03:17 -0500

    stash: url loaded prestates

--- op-e2e/e2eutils/challenger/helper.go
+++ op-e2e/e2eutils/challenger/helper.go
@@ -88,28 +88,9 @@ func WithPollInterval(pollInterval time.Duration) Option {
 	}
 }
 
-// FindMonorepoRoot finds the relative path to the monorepo root
-// Different tests might be nested in subdirectories of the op-e2e dir.
-func FindMonorepoRoot(t *testing.T) string {
-	path := "./"
-	// Only search up 5 directories
-	// Avoids infinite recursion if the root isn't found for some reason
-	for i := 0; i < 5; i++ {
-		_, err := os.Stat(path + "op-e2e")
-		if errors.Is(err, os.ErrNotExist) {
-			path = path + "../"
-			continue
-		}
-		require.NoErrorf(t, err, "Failed to stat %v even though it existed", path)
-		return path
-	}
-	t.Fatalf("Could not find monorepo root, trying up to %v", path)
-	return ""
-}
-
 func applyCannonConfig(c *config.Config, t *testing.T, rollupCfg *rollup.Config, l2Genesis *core.Genesis) {
 	require := require.New(t)
-	root := FindMonorepoRoot(t)
+	root := e2eutils.FindMonorepoRoot(t)
 	c.CannonBin = root + "cannon/bin/cannon"
 	c.CannonServer = root + "op-program/bin/op-program"
 	c.CannonAbsolutePreState = root + "op-program/bin/prestate.json"
