3820162efdb48f3f44575b98652ac886e3b9142a
Maurelian <maurelian@protonmail.ch>
2024-06-04 15:17:50 -0400

    Add allow-empty overload field on Process.run

--- packages/contracts-bedrock/scripts/libraries/Process.sol
+++ packages/contracts-bedrock/scripts/libraries/Process.sol
@@ -10,14 +10,27 @@ library Process {
     /// @notice Foundry cheatcode VM.
     Vm private constant vm = Vm(address(uint160(uint256(keccak256("hevm cheat code")))));
 
-    function run(string[] memory cmd) internal returns (bytes memory stdout_) {
-        Vm.FfiResult memory result = vm.tryFfi(cmd);
+    /// @notice Run a command in a subprocess. Fails if no output is returned.
+    /// @param _command Command to run.
+    function run(string[] memory _command) internal returns (bytes memory stdout_) {
+        stdout_ = run({ _command: _command, _allowEmpty: false });
+    }
+
+    /// @notice Run a command in a subprocess.
+    /// @param _command Command to run.
+    /// @param _allowEmpty Allow empty output.
+    function run(string[] memory _command, bool _allowEmpty) internal returns (bytes memory stdout_) {
+        Vm.FfiResult memory result = vm.tryFfi(_command);
+        string memory command;
+        for (uint256 i = 0; i < _command.length; i++) {
+            command = string.concat(command, _command[i], " ");
+        }
         if (result.exitCode != 0) {
-            string memory command;
-            for (uint256 i = 0; i < cmd.length; i++) {
-                command = string.concat(command, cmd[i], " ");
-            }
             revert FfiFailed(string.concat("Command: ", command, "\nError: ", string(result.stderr)));
+        }
+        // If the output is empty, result.stdout is "[]".
+        if (!_allowEmpty && keccak256(result.stdout) == keccak256(bytes("[]"))) {
+            revert FfiFailed(string.concat("No output from Command: ", command));
         }
         stdout_ = result.stdout;
     }
