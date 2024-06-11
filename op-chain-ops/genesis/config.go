1cb2864f2e85fc614659e7e2100f64fde525895e
Mark Tyneway <mark.tyneway@gmail.com>
2024-06-11 15:07:02 -0400

    config: fix

--- op-chain-ops/genesis/config.go
+++ op-chain-ops/genesis/config.go
@@ -690,9 +690,9 @@ func NewDeployConfigWithNetwork(network, path string) (*DeployConfig, error) {
 }
 
 // L1Deployments represents a set of L1 contracts that are deployed.
+// This should be consolidated with https://github.com/ethereum-optimism/superchain-registry/blob/f9702a89214244c8dde39e45f5c2955f26d857d0/superchain/superchain.go#L227
 type L1Deployments struct {
 	AddressManager                    common.Address `json:"AddressManager"`
-	BlockOracle                       common.Address `json:"BlockOracle"`
 	DisputeGameFactory                common.Address `json:"DisputeGameFactory"`
 	DisputeGameFactoryProxy           common.Address `json:"DisputeGameFactoryProxy"`
 	L1CrossDomainMessenger            common.Address `json:"L1CrossDomainMessenger"`
@@ -740,8 +740,7 @@ func (d *L1Deployments) Check(deployConfig *DeployConfig) error {
 		name := val.Type().Field(i).Name
 		if !deployConfig.UseFaultProofs &&
 			(name == "DisputeGameFactory" ||
-				name == "DisputeGameFactoryProxy" ||
-				name == "BlockOracle") {
+				name == "DisputeGameFactoryProxy") {
 			continue
 		}
 		if !deployConfig.UsePlasma &&
