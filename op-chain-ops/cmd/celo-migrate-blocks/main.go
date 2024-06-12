9a88fd08f92e439319feb856e2898a44d3cee91e
alecps <alec@cLabs.co>
2024-06-12 16:50:00 -0400

    rename scripts to celo-migrate-state and celo-migrate-blocks

--- op-chain-ops/cmd/celo-migrate-blocks/main.go
+++ op-chain-ops/cmd/celo-migrate-blocks/main.go
@@ -16,10 +16,9 @@ import (
 )
 
 // How to run:
-// 		go run ./op-chain-ops/cmd/celo-dbmigrate -oldDB /path/to/oldDB -newDB /path/to/newDB [-batchSize 1000] [-verbosity 3] [-memoryLimit 7500] [-clear-all] [-clear-nonAncients]
+// 		go run ./op-chain-ops/cmd/celo-migrate-blocks -oldDB /path/to/oldDB -newDB /path/to/newDB [-batchSize 1000] [-memoryLimit 7500] [-clear-all] [-clear-nonAncients]
 //
 // This script will migrate block data from the old database to the new database
-// You can set the log level using the -verbosity flag
 // The number of ancient records to migrate in one batch can be set using the -batchSize flag
 // The default batch size is 1000
 // You can set a memory limit in MB using the -memoryLimit flag. Defaults to 7500 MB. Make sure to set a limit that is less than your machine's available memory.
