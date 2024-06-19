e4df4d3a4b5e20d86d082ae315d4e37b49f743d4
protolambda <proto@protolambda.com>
2024-06-19 15:46:25 -0600

    op-program: encapsulate ValidateClaim

--- op-program/client/driver/driver_test.go
+++ op-program/client/driver/driver_test.go
@@ -69,63 +69,6 @@ func TestNoError(t *testing.T) {
 	require.NoError(t, err)
 }
 
-func TestValidateClaim(t *testing.T) {
-	t.Run("Valid", func(t *testing.T) {
-		driver := createDriver(t, io.EOF)
-		expected := eth.Bytes32{0x11}
-		driver.l2OutputRoot = func(_ uint64) (eth.Bytes32, error) {
-			return expected, nil
-		}
-		err := driver.ValidateClaim(uint64(0), expected)
-		require.NoError(t, err)
-	})
-
-	t.Run("Valid-PriorToSafeHead", func(t *testing.T) {
-		driver := createDriverWithNextBlock(t, io.EOF, 10)
-		expected := eth.Bytes32{0x11}
-		requestedOutputRoot := uint64(0)
-		driver.l2OutputRoot = func(blockNum uint64) (eth.Bytes32, error) {
-			requestedOutputRoot = blockNum
-			return expected, nil
-		}
-		err := driver.ValidateClaim(uint64(20), expected)
-		require.NoError(t, err)
-		require.Equal(t, uint64(10), requestedOutputRoot)
-	})
-
-	t.Run("Invalid", func(t *testing.T) {
-		driver := createDriver(t, io.EOF)
-		driver.l2OutputRoot = func(_ uint64) (eth.Bytes32, error) {
-			return eth.Bytes32{0x22}, nil
-		}
-		err := driver.ValidateClaim(uint64(0), eth.Bytes32{0x11})
-		require.ErrorIs(t, err, ErrClaimNotValid)
-	})
-
-	t.Run("Invalid-PriorToSafeHead", func(t *testing.T) {
-		driver := createDriverWithNextBlock(t, io.EOF, 10)
-		expected := eth.Bytes32{0x11}
-		requestedOutputRoot := uint64(0)
-		driver.l2OutputRoot = func(blockNum uint64) (eth.Bytes32, error) {
-			requestedOutputRoot = blockNum
-			return expected, nil
-		}
-		err := driver.ValidateClaim(uint64(20), eth.Bytes32{0x55})
-		require.ErrorIs(t, err, ErrClaimNotValid)
-		require.Equal(t, uint64(10), requestedOutputRoot)
-	})
-
-	t.Run("Error", func(t *testing.T) {
-		driver := createDriver(t, io.EOF)
-		expectedErr := errors.New("boom")
-		driver.l2OutputRoot = func(_ uint64) (eth.Bytes32, error) {
-			return eth.Bytes32{}, expectedErr
-		}
-		err := driver.ValidateClaim(uint64(0), eth.Bytes32{0x11})
-		require.ErrorIs(t, err, expectedErr)
-	})
-}
-
 func createDriver(t *testing.T, derivationResult error) *Driver {
 	return createDriverWithNextBlock(t, derivationResult, 0)
 }
