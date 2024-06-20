17a54ef205fa11a14370734b8ab63ab0d52459eb
protolambda <proto@protolambda.com>
2024-06-20 15:40:09 -0600

    op-program: refactor to use events derivers

--- op-program/client/driver/driver.go
+++ op-program/client/driver/driver.go
@@ -3,161 +3,88 @@ package driver
 import (
 	"context"
 	"errors"
-	"fmt"
-	"io"
 
 	"github.com/ethereum/go-ethereum/log"
 
 	"github.com/ethereum-optimism/optimism/op-node/metrics"
 	"github.com/ethereum-optimism/optimism/op-node/rollup"
-	"github.com/ethereum-optimism/optimism/op-node/rollup/attributes"
 	"github.com/ethereum-optimism/optimism/op-node/rollup/derive"
-	"github.com/ethereum-optimism/optimism/op-node/rollup/driver"
 	"github.com/ethereum-optimism/optimism/op-node/rollup/engine"
 	"github.com/ethereum-optimism/optimism/op-node/rollup/sync"
 	plasma "github.com/ethereum-optimism/optimism/op-plasma"
-	"github.com/ethereum-optimism/optimism/op-service/eth"
 )
 
-type Derivation interface {
-	Step(ctx context.Context) error
-}
-
-type Pipeline interface {
-	Step(ctx context.Context, pendingSafeHead eth.L2BlockRef) (outAttrib *derive.AttributesWithParent, outErr error)
-	ConfirmEngineReset()
-}
-
-type Engine interface {
-	SafeL2Head() eth.L2BlockRef
-	PendingSafeL2Head() eth.L2BlockRef
-	TryUpdateEngine(ctx context.Context) error
-	engine.ResetEngineControl
-}
-
-type L2Source interface {
-	engine.Engine
-	L2OutputRoot(uint64) (eth.Bytes32, error)
-}
-
-type Deriver interface {
-	SafeL2Head() eth.L2BlockRef
-	SyncStep(ctx context.Context) error
-}
-
-type MinimalSyncDeriver struct {
-	logger            log.Logger
-	pipeline          Pipeline
-	attributesHandler driver.AttributesHandler
-	l1Source          derive.L1Fetcher
-	l2Source          L2Source
-	engine            Engine
-	syncCfg           *sync.Config
-	initialResetDone  bool
-	cfg               *rollup.Config
-}
-
-func (d *MinimalSyncDeriver) SafeL2Head() eth.L2BlockRef {
-	return d.engine.SafeL2Head()
-}
-
-func (d *MinimalSyncDeriver) SyncStep(ctx context.Context) error {
-	if !d.initialResetDone {
-		if err := d.engine.TryUpdateEngine(ctx); !errors.Is(err, engine.ErrNoFCUNeeded) {
-			return err
-		}
-		// The below two calls emulate ResetEngine, without event-processing.
-		// This will be omitted after op-program adopts events, and the deriver code is used instead.
-		result, err := sync.FindL2Heads(ctx, d.cfg, d.l1Source, d.l2Source, d.logger, d.syncCfg)
-		if err != nil {
-			// not really a temporary error in this context, but preserves old ResetEngine behavior.
-			return derive.NewTemporaryError(fmt.Errorf("failed to determine starting point: %w", err))
-		}
-		engine.ForceEngineReset(d.engine, engine.ForceEngineResetEvent{
-			Unsafe:    result.Unsafe,
-			Safe:      result.Safe,
-			Finalized: result.Finalized,
-		})
-		d.pipeline.ConfirmEngineReset()
-		d.initialResetDone = true
-	}
-
-	if err := d.engine.TryUpdateEngine(ctx); !errors.Is(err, engine.ErrNoFCUNeeded) {
-		return err
-	}
-	if err := d.attributesHandler.Proceed(ctx); err != io.EOF {
-		// EOF error means we can't process the next attributes. Then we should derive the next attributes.
-		return err
-	}
-
-	attrib, err := d.pipeline.Step(ctx, d.engine.PendingSafeL2Head())
-	if err != nil {
-		return err
-	}
-	d.attributesHandler.SetAttributes(attrib)
-	return nil
+type EndCondition interface {
+	Closing() bool
+	Result() error
 }
 
 type Driver struct {
 	logger log.Logger
 
-	deriver Deriver
+	events []rollup.Event
 
-	l2OutputRoot   func(uint64) (eth.Bytes32, error)
-	targetBlockNum uint64
+	end     EndCondition
+	deriver rollup.Deriver
 }
 
-func NewDriver(logger log.Logger, cfg *rollup.Config, l1Source derive.L1Fetcher, l1BlobsSource derive.L1BlobsFetcher, l2Source L2Source, targetBlockNum uint64) *Driver {
-	engine := engine.NewEngineController(l2Source, logger, metrics.NoopMetrics, cfg, sync.CLSync, rollup.NoopEmitter{})
-	attributesHandler := attributes.NewAttributesHandler(logger, cfg, engine, l2Source)
-	syncCfg := &sync.Config{SyncMode: sync.CLSync}
-	pipeline := derive.NewDerivationPipeline(logger, cfg, l1Source, l1BlobsSource, plasma.Disabled, l2Source, metrics.NoopMetrics)
-	return &Driver{
+func NewDriver(logger log.Logger, cfg *rollup.Config, l1Source derive.L1Fetcher,
+	l1BlobsSource derive.L1BlobsFetcher, l2Source engine.Engine, targetBlockNum uint64) *Driver {
+
+	d := &Driver{
 		logger: logger,
-		deriver: &MinimalSyncDeriver{
-			logger:            logger,
-			pipeline:          pipeline,
-			attributesHandler: attributesHandler,
-			l1Source:          l1Source,
-			l2Source:          l2Source,
-			engine:            engine,
-			syncCfg:           syncCfg,
-			cfg:               cfg,
-		},
-		l2OutputRoot:   l2Source.L2OutputRoot,
+	}
+
+	pipeline := derive.NewDerivationPipeline(logger, cfg, l1Source, l1BlobsSource, plasma.Disabled, l2Source, metrics.NoopMetrics)
+	pipelineDeriver := derive.NewPipelineDeriver(context.Background(), pipeline, d)
+
+	ec := engine.NewEngineController(l2Source, logger, metrics.NoopMetrics, cfg, sync.CLSync, d)
+	engineDeriv := engine.NewEngDeriver(logger, context.Background(), cfg, ec, d)
+	syncCfg := &sync.Config{SyncMode: sync.CLSync}
+	engResetDeriv := engine.NewEngineResetDeriver(context.Background(), logger, cfg, l1Source, l2Source, syncCfg, d)
+
+	prog := &ProgramDeriver{
+		logger:         logger,
+		Emitter:        d,
+		closing:        false,
+		result:         nil,
 		targetBlockNum: targetBlockNum,
 	}
+
+	d.deriver = &rollup.SynchronousDerivers{
+		prog,
+		engineDeriv,
+		pipelineDeriver,
+		engResetDeriv,
+	}
+	d.end = prog
+
+	return d
 }
 
-// Step runs the next step of the derivation pipeline.
-// Returns nil if there are further steps to be performed
-// Returns io.EOF if the derivation completed successfully
-// Returns a non-EOF error if the derivation failed
-func (d *Driver) Step(ctx context.Context) error {
-	if err := d.deriver.SyncStep(ctx); errors.Is(err, io.EOF) {
-		d.logger.Info("Derivation complete: reached L1 head", "head", d.deriver.SafeL2Head())
-		return io.EOF
-	} else if errors.Is(err, derive.NotEnoughData) {
-		// NotEnoughData is not handled differently than a nil error.
-		// This used to be returned by the EngineQueue when a block was derived, but also other stages.
-		// Instead, every driver-loop iteration we check if the target block number has been reached.
-		d.logger.Debug("Data is lacking")
-	} else if errors.Is(err, derive.ErrTemporary) {
-		// While most temporary errors are due to requests for external data failing which can't happen,
-		// they may also be returned due to other events like channels timing out so need to be handled
-		d.logger.Warn("Temporary error in derivation", "err", err)
-		return nil
-	} else if err != nil {
-		return fmt.Errorf("pipeline err: %w", err)
+func (d *Driver) Emit(ev rollup.Event) {
+	if d.end.Closing() {
+		return
 	}
-	head := d.deriver.SafeL2Head()
-	if head.Number >= d.targetBlockNum {
-		d.logger.Info("Derivation complete: reached L2 block", "head", head)
-		return io.EOF
-	}
-	return nil
+	d.events = append(d.events, ev)
 }
 
-func (d *Driver) SafeHead() eth.L2BlockRef {
-	return d.deriver.SafeL2Head()
+var ExhaustErr = errors.New("exhausted events before completing program")
+
+func (d *Driver) RunComplete() error {
+	// Initial reset
+	d.Emit(engine.ResetEngineRequestEvent{})
+
+	for !d.end.Closing() {
+		if len(d.events) == 0 {
+			return ExhaustErr
+		}
+		if len(d.events) > 10000 { // sanity check, in case of bugs. Better than going OOM.
+			return errors.New("way too many events queued up, something is wrong")
+		}
+		ev := d.events[0]
+		d.events = d.events[1:]
+		d.deriver.OnEvent(ev)
+	}
+	return d.end.Result()
 }
