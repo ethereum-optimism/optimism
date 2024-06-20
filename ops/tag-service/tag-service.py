8b305af815353606c2d571a2ac6a76121e5e791e
Sebastian Stammler <seb@oplabs.co>
2024-06-20 02:18:06 +0200

    tag-service: add da-server (#10949)

--- ops/tag-service/tag-service.py
+++ ops/tag-service/tag-service.py
@@ -12,6 +12,7 @@ MIN_VERSIONS = {
     'ci-builder': '0.6.0',
     'ci-builder-rust': '0.1.0',
     'chain-mon': '0.2.2',
+    'da-server': '0.0.4',
     'op-node': '0.10.14',
     'op-batcher': '0.10.14',
     'op-challenger': '0.0.4',
