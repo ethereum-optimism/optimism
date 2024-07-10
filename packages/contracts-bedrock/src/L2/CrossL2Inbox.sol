ceb1b5995db4bb2cd4b1eeca387c9e4ed9003096
Carter Carlson <pegahcarter@gmail.com>
2024-07-10 09:29:45 -0700

    docs(typo): `CrossL2Inbox.sol`
    `if{"f" => ""}`

--- packages/contracts-bedrock/src/L2/CrossL2Inbox.sol
+++ packages/contracts-bedrock/src/L2/CrossL2Inbox.sol
@@ -10,7 +10,7 @@ import { SafeCall } from "src/libraries/SafeCall.sol";
 /// @title IDependencySet
 /// @notice Interface for L1Block with only `isInDependencySet(uint256)` method.
 interface IDependencySet {
-    /// @notice Returns true iff the chain associated with input chain ID is in the interop dependency set.
+    /// @notice Returns true if the chain associated with input chain ID is in the interop dependency set.
     ///         Every chain is in the interop dependency set of itself.
     /// @param _chainId Input chain ID.
     /// @return True if the input chain ID corresponds to a chain in the interop dependency set, and false otherwise.
