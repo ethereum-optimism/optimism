732f140ce27598e3590c0bded0d8d4904d42eeeb
Brian Bland <brian.t.bland@gmail.com>
2024-07-11 17:17:37 -0700

    Add buffered caching of L1 block refs to the confDepth-aware fetcher

--- op-node/rollup/confdepth/conf_depth.go
+++ op-node/rollup/confdepth/conf_depth.go
@@ -13,30 +13,66 @@ import (
 // and hides the part of the L1 chain with insufficient confirmations.
 //
 // At 0 depth the l1 head is completely ignored.
+//
+// confDepth also caches the L1 head block references to avoid unnecessary fetches,
+// by storing a sequence of up to 1000 blocks with a valid chain of parent hashes.
 type confDepth struct {
 	// everything fetched by hash is trusted already, so we implement those by embedding the fetcher
 	derive.L1Fetcher
-	l1Head func() eth.L1BlockRef
-	depth  uint64
+	l1Head           func() eth.L1BlockRef
+	depth            uint64
+	l1Cache          map[uint64]eth.L1BlockRef
+	oldestCacheEntry uint64
+	newestCacheEntry uint64
 }
 
 func NewConfDepth(depth uint64, l1Head func() eth.L1BlockRef, fetcher derive.L1Fetcher) *confDepth {
-	return &confDepth{L1Fetcher: fetcher, l1Head: l1Head, depth: depth}
+	return &confDepth{L1Fetcher: fetcher, l1Head: l1Head, depth: depth, l1Cache: make(map[uint64]eth.L1BlockRef)}
 }
 
 // L1BlockRefByNumber is used for L1 traversal and for finding a safe common point between the L2 engine and L1 chain.
 // Any block numbers that are within confirmation depth of the L1 head are mocked to be "not found",
 // effectively hiding the uncertain part of the L1 chain.
 func (c *confDepth) L1BlockRefByNumber(ctx context.Context, num uint64) (eth.L1BlockRef, error) {
-	// TODO: performance optimization: buffer the l1Unsafe, invalidate any reorged previous buffer content,
-	// and instantly return the origin by number from the buffer if we can.
-
 	// Don't apply the conf depth if l1Head is empty (as it is during the startup case before the l1State is initialized).
 	l1Head := c.l1Head()
 	if l1Head == (eth.L1BlockRef{}) {
 		return c.L1Fetcher.L1BlockRefByNumber(ctx, num)
 	}
+
+	// First, check if the L1 head is in the cache.
+	// If the hash doesn't match the one in the cache, we have a reorg and need to remove all entries after the new head.
+	if ref, ok := c.l1Cache[l1Head.Number]; ok {
+		if ref.Hash != l1Head.Hash {
+			// Reorg detected, invalidate all entries after the new head.
+			for idx := l1Head.Number + 1; idx <= c.newestCacheEntry; idx++ {
+				delete(c.l1Cache, idx)
+			}
+			c.l1Cache[l1Head.Number] = l1Head
+			c.newestCacheEntry = l1Head.Number
+		}
+	} else if ref, ok := c.l1Cache[l1Head.Number-1]; ok && ref.Hash == l1Head.ParentHash {
+		// Parent hash matches, so we can safely add the new head to the cache.
+		c.l1Cache[l1Head.Number] = l1Head
+		c.newestCacheEntry = l1Head.Number
+		if len(c.l1Cache) > 1000 {
+			delete(c.l1Cache, c.oldestCacheEntry)
+			c.oldestCacheEntry++
+		}
+	} else {
+		// Parent not found or doesn't match, so invalidate the entire cache.
+		c.l1Cache = make(map[uint64]eth.L1BlockRef)
+		c.l1Cache[l1Head.Number] = l1Head
+		c.oldestCacheEntry = l1Head.Number
+		c.newestCacheEntry = l1Head.Number
+	}
+
 	if num == 0 || c.depth == 0 || num+c.depth <= l1Head.Number {
+		// Attempt to retrieve from the cache first, falling back to a live fetch
+		if ref, ok := c.l1Cache[num]; ok {
+			return ref, nil
+		}
+
 		return c.L1Fetcher.L1BlockRefByNumber(ctx, num)
 	}
 	return eth.L1BlockRef{}, ethereum.NotFound
--- op-node/rollup/confdepth/conf_depth_test.go
+++ op-node/rollup/confdepth/conf_depth_test.go
@@ -16,29 +16,36 @@ import (
 var exHash = common.Hash{0xff}
 
 type confTest struct {
-	name  string
-	head  uint64
-	hash  common.Hash // hash of head block
-	req   uint64
-	depth uint64
-	pass  bool
+	name      string
+	head      uint64
+	req       uint64
+	depth     uint64
+	pass      bool
+	fromCache bool
+}
+
+func mockL1BlockRef(num uint64) eth.L1BlockRef {
+	return eth.L1BlockRef{Number: num, Hash: common.Hash{byte(num)}, ParentHash: common.Hash{byte(num - 1)}}
 }
 
 func (ct *confTest) Run(t *testing.T) {
 	l1Fetcher := &testutils.MockL1Source{}
-	l1Head := eth.L1BlockRef{Number: ct.head, Hash: ct.hash}
+	var l1Head eth.L1BlockRef
+	if ct.head != 0 {
+		l1Head = mockL1BlockRef(ct.head)
+	}
 	l1HeadGetter := func() eth.L1BlockRef { return l1Head }
 
 	cd := NewConfDepth(ct.depth, l1HeadGetter, l1Fetcher)
-	if ct.pass {
+	if ct.pass && !ct.fromCache {
 		// no calls to the l1Fetcher are made if the confirmation depth of the request is not met
-		l1Fetcher.ExpectL1BlockRefByNumber(ct.req, eth.L1BlockRef{Number: ct.req}, nil)
+		l1Fetcher.ExpectL1BlockRefByNumber(ct.req, mockL1BlockRef(ct.req), nil)
 	}
 	out, err := cd.L1BlockRefByNumber(context.Background(), ct.req)
 	l1Fetcher.AssertExpectations(t)
 	if ct.pass {
 		require.NoError(t, err)
-		require.Equal(t, out, eth.L1BlockRef{Number: ct.req})
+		require.Equal(t, out, mockL1BlockRef(ct.req))
 	} else {
 		require.Equal(t, ethereum.NotFound, err)
 	}
@@ -48,21 +55,99 @@ func TestConfDepth(t *testing.T) {
 	// note: we're not testing overflows.
 	// If a request is large enough to overflow the conf depth check, it's not returning anything anyway.
 	testCases := []confTest{
-		{name: "zero conf future", head: 4, hash: exHash, req: 5, depth: 0, pass: true},
-		{name: "zero conf present", head: 4, hash: exHash, req: 4, depth: 0, pass: true},
-		{name: "zero conf past", head: 4, hash: exHash, req: 4, depth: 0, pass: true},
-		{name: "one conf future", head: 4, hash: exHash, req: 5, depth: 1, pass: false},
-		{name: "one conf present", head: 4, hash: exHash, req: 4, depth: 1, pass: false},
-		{name: "one conf past", head: 4, hash: exHash, req: 3, depth: 1, pass: true},
-		{name: "two conf future", head: 4, hash: exHash, req: 5, depth: 2, pass: false},
-		{name: "two conf present", head: 4, hash: exHash, req: 4, depth: 2, pass: false},
-		{name: "two conf not like 1", head: 4, hash: exHash, req: 3, depth: 2, pass: false},
-		{name: "two conf pass", head: 4, hash: exHash, req: 2, depth: 2, pass: true},
-		{name: "easy pass", head: 100, hash: exHash, req: 20, depth: 5, pass: true},
-		{name: "genesis case", head: 0, hash: exHash, req: 0, depth: 4, pass: true},
+		{name: "zero conf future", head: 4, req: 5, depth: 0, pass: true},
+		{name: "zero conf present", head: 4, req: 4, depth: 0, pass: true, fromCache: true},
+		{name: "zero conf past", head: 4, req: 3, depth: 0, pass: true},
+		{name: "one conf future", head: 4, req: 5, depth: 1, pass: false},
+		{name: "one conf present", head: 4, req: 4, depth: 1, pass: false},
+		{name: "one conf past", head: 4, req: 3, depth: 1, pass: true},
+		{name: "two conf future", head: 4, req: 5, depth: 2, pass: false},
+		{name: "two conf present", head: 4, req: 4, depth: 2, pass: false},
+		{name: "two conf not like 1", head: 4, req: 3, depth: 2, pass: false},
+		{name: "two conf pass", head: 4, req: 2, depth: 2, pass: true},
+		{name: "easy pass", head: 100, req: 20, depth: 5, pass: true},
+		{name: "genesis case", head: 0, req: 0, depth: 4, pass: true},
 		{name: "no L1 state", req: 10, depth: 4, pass: true},
 	}
 	for _, tc := range testCases {
 		t.Run(tc.name, tc.Run)
+	}
+}
+
+func TestConfDepthCachingReorgs(t *testing.T) {
+	l1Fetcher := &testutils.MockL1Source{}
+
+	l1Head := mockL1BlockRef(100)
+	l1HeadGetter := func() eth.L1BlockRef { return l1Head }
+
+	cd := NewConfDepth(0, l1HeadGetter, l1Fetcher)
+
+	l1Fetcher.ExpectL1BlockRefByNumber(99, mockL1BlockRef(99), nil)
+	out, err := cd.L1BlockRefByNumber(context.Background(), 99)
+	require.NoError(t, err)
+	require.Equal(t, out, mockL1BlockRef(99))
+	l1Fetcher.AssertExpectations(t)
+
+	// from cache
+	out, err = cd.L1BlockRefByNumber(context.Background(), 100)
+	require.NoError(t, err)
+	require.Equal(t, out, mockL1BlockRef(100))
+
+	l1Head = mockL1BlockRef(101)
+
+	// from cache
+	out, err = cd.L1BlockRefByNumber(context.Background(), 101)
+	require.NoError(t, err)
+	require.Equal(t, out, mockL1BlockRef(101))
+
+	// trigger a reorg of block 100, invalidating the following cache elements
+	l1Head = mockL1BlockRef(100)
+	l1Head.Hash = common.Hash{0xde, 0xad, 0xbe, 0xef}
+
+	l1Fetcher.ExpectL1BlockRefByNumber(101, mockL1BlockRef(101), nil)
+	out, err = cd.L1BlockRefByNumber(context.Background(), 101)
+	require.NoError(t, err)
+	require.Equal(t, out, mockL1BlockRef(101))
+	l1Fetcher.AssertExpectations(t)
+
+	// head jumps ahead, invalidating the entire cache
+	l1Head = mockL1BlockRef(200)
+
+	l1Fetcher.ExpectL1BlockRefByNumber(100, mockL1BlockRef(100), nil)
+	out, err = cd.L1BlockRefByNumber(context.Background(), 100)
+	require.NoError(t, err)
+	require.Equal(t, out, mockL1BlockRef(100))
+	l1Fetcher.AssertExpectations(t)
+}
+
+func TestConfDepthCachingEviction(t *testing.T) {
+	l1Fetcher := &testutils.MockL1Source{}
+
+	l1Head := mockL1BlockRef(100)
+	l1HeadGetter := func() eth.L1BlockRef { return l1Head }
+
+	cd := NewConfDepth(0, l1HeadGetter, l1Fetcher)
+
+	// insert 1001 elements into the cache, resulting in the oldest entry being evicted
+	for idx := 1000; idx <= 2000; idx++ {
+		l1Head = mockL1BlockRef(uint64(idx))
+
+		// request current head from cache
+		out, err := cd.L1BlockRefByNumber(context.Background(), uint64(idx))
+		require.NoError(t, err)
+		require.Equal(t, out, mockL1BlockRef(uint64(idx)))
+	}
+
+	l1Fetcher.ExpectL1BlockRefByNumber(1000, mockL1BlockRef(1000), nil)
+	out, err := cd.L1BlockRefByNumber(context.Background(), 1000)
+	require.NoError(t, err)
+	require.Equal(t, out, mockL1BlockRef(1000))
+	l1Fetcher.AssertExpectations(t)
+
+	for idx := 1001; idx <= 2000; idx++ {
+		// these elements are still in the cache
+		out, err = cd.L1BlockRefByNumber(context.Background(), uint64(idx))
+		require.NoError(t, err)
+		require.Equal(t, out, mockL1BlockRef(uint64(idx)))
 	}
 }
