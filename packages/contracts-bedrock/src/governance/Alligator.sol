4ef7f59cf4616da350f5a7bacb999a9d1b2799a7
Diego <105765223+0xfuturistic@users.noreply.github.com>
2024-07-16 21:27:04 +0200

    contracts-bedrock: add missing functions to Alligator

--- packages/contracts-bedrock/src/governance/Alligator.sol
+++ packages/contracts-bedrock/src/governance/Alligator.sol
@@ -9,4 +9,20 @@ contract Alligator {
     /// @param to     The account receiving tokens.
     /// @param amount The amount of tokens being transfered.
     function afterTokenTransfer(address from, address to, uint256 amount) internal { }
+
+    function checkpoints(address _account, uint32 _pos) public view returns (Checkpoint memory) { }
+
+    /// @notice Returns the number of checkpoints for a given account.
+    /// @param _account Account to get the number of checkpoints for.
+    /// @return Number of checkpoints for the given account.
+    function numCheckpoints(address _account) public view returns (uint32) { }
+
+    /// @notice Returns the delegatee of an account.
+    /// @param _account Account to get the delegatee of.
+    /// @return Delegatee of the given account.
+    function delegates(address _account) public view returns (address) { }
+
+    /// @notice Delegates votes from the sender to `delegatee`.
+    /// @param _delegatee Account to delegate votes to.
+    function delegate(address _delegatee) public { }
 }
