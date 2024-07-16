c1e2a79dc4f7a09a900473e712f4cd26959037aa
Jinsuk Park <totorovirus@gmail.com>
2024-07-16 08:58:24 +0100

    wsutils

--- op-node/node/node.go
+++ op-node/node/node.go
@@ -16,6 +16,7 @@ import (
 	"github.com/ethereum-optimism/optimism/op-node/rollup/derive"
 	plasma "github.com/ethereum-optimism/optimism/op-plasma"
 	"github.com/ethereum-optimism/optimism/op-service/httputil"
+	"github.com/ethereum-optimism/optimism/op-service/wsutil"
 	"github.com/r3labs/sse"
 
 	"github.com/hashicorp/go-multierror"
@@ -90,6 +91,7 @@ type OpNode struct {
 
 	httpEventStream       *sse.Server
 	httpEventStreamServer *httputil.HTTPServer
+	webSocketServer       *wsutil.WSServer
 }
 
 // The OpNode handles incoming gossip
@@ -439,11 +441,16 @@ func (n *OpNode) initRPCServer(cfg *Config) error {
 }
 
 func (n *OpNode) initHTTPEventStreamServer(cfg *Config) error {
+
+	wss := wsutil.NewWebSocketServer()
+	wss.Start()
+
 	server := sse.New()
 	server.CreateStream("payload_attributes")
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/events", server.HTTPHandler)
+	mux.HandleFunc("/ws", wss.HTTPHandler)
 	addr := net.JoinHostPort(cfg.RPC.ListenAddr, strconv.Itoa(cfg.RPC.ListenPort+1))
 
 	var err error
@@ -453,6 +460,7 @@ func (n *OpNode) initHTTPEventStreamServer(cfg *Config) error {
 	}
 	n.log.Info("Started HTTP event stream server", "addr", addr)
 	n.httpEventStream = server
+	n.webSocketServer = wss
 
 	return nil
 }
@@ -766,6 +774,12 @@ func (n *OpNode) Stop(ctx context.Context) error {
 	if n.httpEventStreamServer != nil {
 		if err := n.httpEventStreamServer.Stop(ctx); err != nil {
 			result = multierror.Append(result, fmt.Errorf("failed to close http event stream server: %w", err))
+		}
+	}
+
+	if n.webSocketServer != nil {
+		if err := n.webSocketServer.Stop(ctx); err != nil {
+			result = multierror.Append(result, fmt.Errorf("failed to close ws event stream server: %w", err))
 		}
 	}
 
