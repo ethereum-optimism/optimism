e32a9fbdbed7321419633f6597fbf3d8601f574a
mbaxter <meredith@oplabs.co>
2024-07-18 18:23:27 -0400

    mt-cannon: Implement clone syscall

--- cannon/mipsevm/multithreaded/mips.go
+++ cannon/mipsevm/multithreaded/mips.go
@@ -4,12 +4,16 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/hexutil"
 
+	"github.com/ethereum-optimism/optimism/cannon/mipsevm"
 	"github.com/ethereum-optimism/optimism/cannon/mipsevm/exec"
 )
 
-func (m *InstrumentedState) handleSyscall() error {
-	syscallNum, a0, a1, a2 := exec.GetSyscallArgs(m.state.GetRegisters())
+const EMPTY_SIGNAL = ^uint32(0)
 
+func (m *InstrumentedState) handleSyscall() error {
+	thread := m.state.getCurrentThread()
+
+	syscallNum, a0, a1, a2 := exec.GetSyscallArgs(m.state.GetRegisters())
 	v0 := uint32(0)
 	v1 := uint32(0)
 
@@ -21,8 +25,36 @@ func (m *InstrumentedState) handleSyscall() error {
 		m.state.Heap = newHeap
 	case exec.SysBrk:
 		v0 = exec.BrkStart
-	case exec.SysClone: // clone (not supported)
-		v0 = 1
+	case exec.SysClone: // clone
+		v0 = m.state.NextThreadId
+		v1 = 0
+		newThread := &ThreadState{
+			ThreadId:         m.state.NextThreadId,
+			ExitCode:         0,
+			Exited:           false,
+			FutexAddr:        EMPTY_SIGNAL,
+			FutexVal:         0,
+			FutexTimeoutStep: 0,
+			Cpu: mipsevm.CpuScalars{
+				PC:     thread.Cpu.NextPC,
+				NextPC: thread.Cpu.NextPC + 4,
+				HI:     thread.Cpu.HI,
+				LO:     thread.Cpu.LO,
+			},
+		}
+		for i := 0; i < 32; i++ {
+			newThread.Registers[i] = thread.Registers[i]
+		}
+		newThread.Registers[29] = a1
+		// the child will perceive a 0 value as returned value instead, and no error
+		newThread.Registers[2] = 0
+		newThread.Registers[7] = 0
+		m.state.NextThreadId++
+
+		// Preempt this thread for the new one. But not before updating PCs
+		thread.Cpu.PC = thread.Cpu.NextPC
+		thread.Cpu.NextPC = thread.Cpu.NextPC + 4
+		m.state.PushThread(newThread)
 	case exec.SysExitGroup:
 		m.state.Exited = true
 		m.state.ExitCode = uint8(a0)
