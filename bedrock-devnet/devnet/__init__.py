e4664372694e60654ae24e22da98225fb6a88586
Ethen Pociask <ethenpo@gmail.com>
2024-07-23 03:53:21 -0400

    chore: Update devnet with janky grafana and prometheus svc deps

--- bedrock-devnet/devnet/__init__.py
+++ bedrock-devnet/devnet/__init__.py
@@ -171,6 +171,16 @@ def devnet_l2_allocs(paths):
 
 # Bring up the devnet where the contracts are deployed to L1
 def devnet_deploy(paths):
+
+    log.info('Starting peripheral telemetry services.')
+    run_command(['docker', 'compose', 'up', '-d', 'prometheus'], cwd=paths.ops_bedrock_dir, env={
+        'PWD': paths.ops_bedrock_dir
+    })
+
+    run_command(['docker', 'compose', 'up', '-d', 'grafana'], cwd=paths.ops_bedrock_dir, env={
+        'PWD': paths.ops_bedrock_dir
+    })
+
     if os.path.exists(paths.genesis_l1_path):
         log.info('L1 genesis already generated.')
     else:
