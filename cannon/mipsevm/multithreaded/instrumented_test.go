3ca7faee79f1794e2780be54fb3935a2955de890
mbaxter <meredith@oplabs.co>
2024-07-24 10:45:38 -0400

    mt-cannon: Add multithreaded program test

--- cannon/mipsevm/multithreaded/instrumented_test.go
+++ cannon/mipsevm/multithreaded/instrumented_test.go
@@ -1,6 +1,7 @@
 package multithreaded
 
 import (
+	"bytes"
 	"io"
 	"os"
 	"testing"
@@ -27,6 +28,28 @@ func TestInstrumentedState_Hello(t *testing.T) {
 
 func TestInstrumentedState_Claim(t *testing.T) {
 	testutil.RunVMTest_Claim(t, CreateInitialState, vmFactory, false)
+}
+
+func TestInstrumentedState_MultithreadedProgram(t *testing.T) {
+	state := testutil.LoadELFProgram(t, "../../example/bin/multithreaded.elf", CreateInitialState, false)
+	oracle := testutil.StaticOracle(t, []byte{})
+
+	var stdOutBuf, stdErrBuf bytes.Buffer
+	us := NewInstrumentedState(state, oracle, io.MultiWriter(&stdOutBuf, os.Stdout), io.MultiWriter(&stdErrBuf, os.Stderr), testutil.CreateLogger())
+	for i := 0; i < 1_000_000; i++ {
+		if us.GetState().GetExited() {
+			break
+		}
+		_, err := us.Step(false)
+		require.NoError(t, err)
+	}
+	t.Logf("Completed in %d steps", state.Step)
+
+	require.True(t, state.Exited, "must complete program")
+	require.Equal(t, uint8(0), state.ExitCode, "exit with 0")
+	require.Contains(t, "waitgroup result: 42", stdErrBuf.String())
+	require.Contains(t, "channels result: 1234", stdErrBuf.String())
+	require.Equal(t, "", stdErrBuf.String(), "should not print any errors")
 }
 
 func TestInstrumentedState_Alloc(t *testing.T) {
