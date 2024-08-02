7cbb3b1f90b299e12b591b42df87b93cd0d66945
Mark Tyneway <mark.tyneway@gmail.com>
2024-08-02 16:10:17 -0600

    contracts-bedrock: fix build

--- packages/contracts-bedrock/scripts/checks/names/check-test-names.go
+++ packages/contracts-bedrock/scripts/checks/names/check-test-names.go
@@ -3,7 +3,6 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"os"
 	"os/exec"
 	"path/filepath"
--- packages/contracts-bedrock/scripts/checks/spacers/check-spacers.go
+++ packages/contracts-bedrock/scripts/checks/spacers/check-spacers.go
@@ -3,7 +3,6 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"os"
 	"path/filepath"
 	"regexp"
