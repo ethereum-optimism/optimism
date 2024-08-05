c93a1288209368f1a48c32db9c60ab1fc87c9b86
mbaxter <meredith@oplabs.co>
2024-08-05 16:05:11 -0400

    cannon: Implement multithreaded clone fuzz test

--- cannon/mipsevm/tests/fuzz_evm_multithreaded_test.go
+++ cannon/mipsevm/tests/fuzz_evm_multithreaded_test.go
@@ -8,20 +8,24 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"github.com/ethereum-optimism/optimism/cannon/mipsevm/exec"
+	"github.com/ethereum-optimism/optimism/cannon/mipsevm/multithreaded"
 	"github.com/ethereum-optimism/optimism/cannon/mipsevm/testutil"
 )
 
-// TODO
 func FuzzStateSyscallCloneMT(f *testing.F) {
 	v := GetMultiThreadedTestCase(f)
-	// t.Skip is causing linting check to fail, disable for now
-	//nolint:staticcheck
-	f.Fuzz(func(t *testing.T, seed int64) {
-		// TODO(cp-903) Customize test for multi-threaded vm
-		t.Skip("TODO - customize this test for MTCannon")
+	f.Fuzz(func(t *testing.T, nextThreadId, stackPtr uint32, seed int64) {
 		goVm := v.VMFactory(nil, os.Stdout, os.Stderr, testutil.CreateLogger(), testutil.WithRandomization(seed))
 		state := goVm.GetState()
+		mtState, ok := state.(*multithreaded.State)
+		if !ok {
+			require.Fail(t, "Failed to cast FPVMState to multithreaded State type")
+		}
+
+		mtState.NextThreadId = nextThreadId
 		state.GetRegistersRef()[2] = exec.SysClone
+		state.GetRegistersRef()[4] = exec.ValidCloneFlags
+		state.GetRegistersRef()[5] = stackPtr
 		state.GetMemory().SetMemory(state.GetPC(), syscallInsn)
 		step := state.GetStep()
 
@@ -29,14 +33,19 @@ func FuzzStateSyscallCloneMT(f *testing.F) {
 		expected.Step += 1
 		expected.PC = state.GetCpu().NextPC
 		expected.NextPC = state.GetCpu().NextPC + 4
-		expected.Registers[2] = 0x1
+		expected.Registers[2] = 0
 		expected.Registers[7] = 0
+		expected.Registers[29] = stackPtr
 
 		stepWitness, err := goVm.Step(true)
 		require.NoError(t, err)
 		require.False(t, stepWitness.HasPreimage())
 
 		expected.Validate(t, state)
+		// Check mt-specific fields
+		newThread := mtState.GetCurrentThread()
+		require.Equal(t, nextThreadId, newThread.ThreadId)
+		require.Equal(t, nextThreadId+1, mtState.NextThreadId)
 
 		evm := testutil.NewMIPSEVM(v.Contracts)
 		evmPost := evm.Step(t, stepWitness, step, v.StateHashFn)
