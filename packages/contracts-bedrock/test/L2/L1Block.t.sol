9fd174efdb21e16e4aa2ba8d71f9962f5576a976
0xDiscotech <131301107+0xDiscotech@users.noreply.github.com>
2024-08-07 13:03:08 -0300

    feat: create some set l1 block values ithmus test
    * refactor: not cross l2 inbox error

--- packages/contracts-bedrock/test/L2/L1Block.t.sol
+++ packages/contracts-bedrock/test/L2/L1Block.t.sol
@@ -10,7 +10,8 @@ import { Constants } from "src/libraries/Constants.sol";
 
 // Target contract
 import { L1Block } from "src/L2/L1Block.sol";
-import "src/libraries/L1BlockErrors.sol";
+import { NotDepositor, NotCrossL2Inbox } from "src/libraries/L1BlockErrors.sol";
+import { Predeploys } from "src/libraries/Predeploys.sol";
 
 contract L1BlockTest is CommonTest {
     address depositor;
@@ -21,6 +22,38 @@ contract L1BlockTest is CommonTest {
     function setUp() public virtual override {
         super.setUp();
         depositor = l1Block.DEPOSITOR_ACCOUNT();
+    }
+}
+
+contract L1BlockIsDeposit_Test is L1BlockTest {
+    /// @dev Tests that `isDeposit` reverts if the caller is not the cross L2 inbox.
+    function test_isDeposit_notCrossL2Inbox_reverts(address _caller) external {
+        vm.assume(_caller != Predeploys.CROSS_L2_INBOX);
+        vm.expectRevert(NotCrossL2Inbox.selector);
+        l1Block.isDeposit();
+    }
+
+    /// @dev Tests that `isDeposit` always returns the correct value.
+    function test_isDeposit_succeeds(bool _isDeposit) external {
+        vm.prank(Predeploys.CROSS_L2_INBOX);
+        assertEq(l1Block.isDeposit(), false);
+
+        /// @dev Assuming that `setL1BlockValues` will set the proper value. That function is tested as well
+        vm.prank(depositor);
+        l1Block.setL1BlockValues({
+            _number: type(uint64).max,
+            _timestamp: type(uint64).max,
+            _basefee: type(uint256).max,
+            _hash: keccak256(abi.encode(1)),
+            _sequenceNumber: type(uint64).max,
+            _batcherHash: bytes32(type(uint256).max),
+            _l1FeeOverhead: type(uint256).max,
+            _l1FeeScalar: type(uint256).max,
+            _isDeposit: _isDeposit
+        });
+
+        vm.prank(Predeploys.CROSS_L2_INBOX);
+        assertEq(l1Block.isDeposit(), _isDeposit);
     }
 }
 
@@ -82,6 +115,33 @@ contract L1BlockBedrock_Test is L1BlockTest {
             _l1FeeScalar: type(uint256).max,
             _isDeposit: true
         });
+    }
+}
+
+contract L1BlockSetL1BlockValuesIsthmus_Test is L1BlockTest {
+    /// @dev Tests that `setL1BlockValuesIsthmus` reverts if sender address is not the depositor
+    function test_setL1BlockValuesIsthmus_notDepositor_reverts(address _caller) external {
+        vm.assume(_caller != depositor);
+        vm.prank(_caller);
+        vm.expectRevert(NotDepositor.selector);
+        l1Block.setL1BlockValuesIsthmus();
+    }
+
+    /// @dev Tests that `setL1BlockValuesIsthmus` succeeds if sender address is the depositor
+    function test_setL1BlockValuesIsthmus_succeeds() external {
+        // Ensure the `isDepositTransaction` flag is false before calling `setL1BlockValuesIsthmus`
+        vm.prank(Predeploys.CROSS_L2_INBOX);
+        assertEq(l1Block.isDeposit(), false);
+
+        vm.prank(depositor);
+        l1Block.setL1BlockValuesIsthmus();
+
+        // Assert that the `isDepositTransaction` flag was properly set to true
+        vm.prank(Predeploys.CROSS_L2_INBOX);
+        assertEq(l1Block.isDeposit(), true);
+
+        // Assert `setL1BlockValuesEcotone` was properly called, forwarding the calldata to it
+        // TODO(disco)
     }
 }
 
@@ -169,6 +229,45 @@ contract L1BlockEcotone_Test is L1BlockTest {
         // make sure return value is the expected function selector for "NotDepositor()"
         bytes memory expReturn = hex"3cc50b45";
         assertEq(data, expReturn);
+    }
+}
+
+contract L1BlockDepositsComplete_Test is L1BlockTest {
+    // @dev Tests that `depositsComplete` reverts if the caller is not the depositor.
+    function test_deposits_is_depositor_reverts(address _caller) external {
+        vm.assume(_caller != depositor);
+        vm.expectRevert(NotDepositor.selector);
+        l1Block.depositsComplete();
+    }
+
+    // @dev Tests that `depositsComplete` succeeds if the caller is the depositor.
+    function test_depositsComplete_succeeds() external {
+        // Set the `isDeposit` flag to true
+        vm.prank(depositor);
+        l1Block.setL1BlockValues({
+            _number: type(uint64).max,
+            _timestamp: type(uint64).max,
+            _basefee: type(uint256).max,
+            _hash: keccak256(abi.encode(1)),
+            _sequenceNumber: type(uint64).max,
+            _batcherHash: bytes32(type(uint256).max),
+            _l1FeeOverhead: type(uint256).max,
+            _l1FeeScalar: type(uint256).max,
+            _isDeposit: true
+        });
+
+        // Assert that the `isDeposit` flag was properly set to true
+        vm.prank(Predeploys.CROSS_L2_INBOX);
+        assertTrue(l1Block.isDeposit());
+
+        // Call `depositsComplete`
+        vm.prank(depositor);
+        l1Block.depositsComplete();
+
+        // Assert that the `isDeposit` flag was properly set to false
+        /// @dev Assuming that `isDeposit()` wil return the proper value. That function is tested as well
+        vm.prank(Predeploys.CROSS_L2_INBOX);
+        assertEq(l1Block.isDeposit(), false);
     }
 }
 
