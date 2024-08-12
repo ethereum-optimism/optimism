2ba4f883ff69c3d545e29041d0f38b7baf4278bc
Jinsuk Park <totorovirus@gmail.com>
2024-08-12 22:15:17 +0900

    typo fix

--- op-node/service.go
+++ op-node/service.go
@@ -197,7 +197,7 @@ func NewDriverConfig(ctx *cli.Context) *driver.Config {
 		SequencerEnabled:           ctx.Bool(flags.SequencerEnabledFlag.Name),
 		SequencerStopped:           ctx.Bool(flags.SequencerStoppedFlag.Name),
 		SequencerMaxSafeLag:        ctx.Uint64(flags.SequencerMaxSafeLagFlag.Name),
-		SEquencerPublishAttributes: ctx.Bool(flags.SequencerPublishAttributesFlag.Name),
+		SequencerPublishAttributes: ctx.Bool(flags.SequencerPublishAttributesFlag.Name),
 	}
 }
 
