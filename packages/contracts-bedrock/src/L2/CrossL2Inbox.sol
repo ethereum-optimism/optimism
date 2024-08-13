8644bf2188ca1ee68358a53db9e41915845bd2e6
Michael Amadi <amadimichaeld@gmail.com>
2024-08-13 14:00:30 +0100

    bump CrossL2Inbox semver, run just semver-lock & just snapshots

--- packages/contracts-bedrock/src/L2/CrossL2Inbox.sol
+++ packages/contracts-bedrock/src/L2/CrossL2Inbox.sol
@@ -70,8 +70,8 @@ contract CrossL2Inbox is ICrossL2Inbox, ISemver, TransientReentrancyAware {
     address internal constant DEPOSITOR_ACCOUNT = 0xDeaDDEaDDeAdDeAdDEAdDEaddeAddEAdDEAd0001;
 
     /// @notice Semantic version.
-    /// @custom:semver 1.0.0-beta.4
-    string public constant version = "1.0.0-beta.4";
+    /// @custom:semver 1.0.0-beta.5
+    string public constant version = "1.0.0-beta.5";
 
     /// @notice Emitted when a cross chain message is being executed.
     /// @param msgHash Hash of message payload being executed.
