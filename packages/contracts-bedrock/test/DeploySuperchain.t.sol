f953574a6510558b1b923005196a436655702aeb
Matt Solomon <matt@mattsolomon.dev>
2024-08-13 19:57:57 -0700

    test: additional assertions

--- packages/contracts-bedrock/test/DeploySuperchain.t.sol
+++ packages/contracts-bedrock/test/DeploySuperchain.t.sol
@@ -3,6 +3,7 @@ pragma solidity 0.8.15;
 
 import { Test } from "forge-std/Test.sol";
 
+import { Proxy } from "src/universal/Proxy.sol";
 import { ProtocolVersion } from "src/L1/ProtocolVersions.sol";
 import { DeploySuperchain } from "scripts/DeploySuperchain.s.sol";
 
@@ -37,13 +38,24 @@ contract DeploySuperchain_Test is Test {
 
         DeploySuperchain.Output memory output = deploySuperchain.runWithoutIO(_input);
 
-        // We assert on the inputs only, as the outputs are asserts on via require statements directly in the script.
+        // Assert the inputs were properly set.
         assertEq(address(output.superchainProxyAdmin.owner()), _input.roles.proxyAdminOwner, "100");
         assertEq(address(output.protocolVersionsProxy.owner()), _input.roles.protocolVersionsOwner, "200");
         assertEq(address(output.superchainConfigProxy.guardian()), _input.roles.guardian, "300");
         assertEq(output.superchainConfigProxy.paused(), _input.paused, "400");
         assertEq(unwrap(output.protocolVersionsProxy.required()), unwrap(_input.requiredProtocolVersion), "500");
         assertEq(unwrap(output.protocolVersionsProxy.recommended()), unwrap(_input.recommendedProtocolVersion), "600");
+
+        // Architecture assertions.
+        // We prank as the zero address due to the Proxy's `proxyCallIfNotAdmin` modifier.
+        Proxy superchainConfigProxy = Proxy(payable(address(output.superchainConfigProxy)));
+        Proxy protocolVersionsProxy = Proxy(payable(address(output.protocolVersionsProxy)));
+        vm.startPrank(address(0));
+
+        assertEq(superchainConfigProxy.implementation(), address(output.superchainConfigImpl), "700");
+        assertEq(protocolVersionsProxy.implementation(), address(output.protocolVersionsImpl), "800");
+        assertEq(superchainConfigProxy.admin(), protocolVersionsProxy.admin(), "900");
+        assertEq(superchainConfigProxy.admin(), address(output.superchainProxyAdmin), "1000");
     }
 
     function test_runWithoutIOAndZeroAddressRoles_reverts() public {
