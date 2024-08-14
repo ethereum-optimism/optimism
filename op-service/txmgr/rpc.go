38167ed07397db4f0b32ab7975a858eaaf6febfc
Samuel Stokes <sam.adam.stokes@gmail.com>
2024-08-14 13:06:20 -0400

    txmgr: cleanup ctx input arg in rpc methods

--- op-service/txmgr/rpc.go
+++ op-service/txmgr/rpc.go
@@ -13,42 +13,42 @@ type SimpleTxmgrAPI struct {
 	l   log.Logger
 }
 
-func (a *SimpleTxmgrAPI) GetMinBaseFee(ctx context.Context) *big.Int {
+func (a *SimpleTxmgrAPI) GetMinBaseFee(_ context.Context) *big.Int {
 	return a.mgr.GetMinBaseFee()
 }
 
-func (a *SimpleTxmgrAPI) SetMinBaseFee(ctx context.Context, val *big.Int) {
+func (a *SimpleTxmgrAPI) SetMinBaseFee(_ context.Context, val *big.Int) {
 	a.mgr.SetMinBaseFee(val)
 }
 
-func (a *SimpleTxmgrAPI) GetPriorityFee(ctx context.Context) *big.Int {
+func (a *SimpleTxmgrAPI) GetPriorityFee(_ context.Context) *big.Int {
 	return a.mgr.GetPriorityFee()
 }
 
-func (a *SimpleTxmgrAPI) SetPriorityFee(ctx context.Context, val *big.Int) {
+func (a *SimpleTxmgrAPI) SetPriorityFee(_ context.Context, val *big.Int) {
 	a.mgr.SetPriorityFee(val)
 }
 
-func (a *SimpleTxmgrAPI) GetMinBlobFee(ctx context.Context) *big.Int {
+func (a *SimpleTxmgrAPI) GetMinBlobFee(_ context.Context) *big.Int {
 	return a.mgr.GetMinBlobFee()
 }
 
-func (a *SimpleTxmgrAPI) SetMinBlobFee(ctx context.Context, val *big.Int) {
+func (a *SimpleTxmgrAPI) SetMinBlobFee(_ context.Context, val *big.Int) {
 	a.mgr.SetMinBlobFee(val)
 }
 
-func (a *SimpleTxmgrAPI) GetFeeThreshold(ctx context.Context) *big.Int {
+func (a *SimpleTxmgrAPI) GetFeeThreshold(_ context.Context) *big.Int {
 	return a.mgr.GetFeeThreshold()
 }
 
-func (a *SimpleTxmgrAPI) SetFeeThreshold(ctx context.Context, val *big.Int) {
+func (a *SimpleTxmgrAPI) SetFeeThreshold(_ context.Context, val *big.Int) {
 	a.mgr.SetFeeThreshold(val)
 }
 
-func (a *SimpleTxmgrAPI) GetBumpFeeRetryTime(ctx context.Context) time.Duration {
+func (a *SimpleTxmgrAPI) GetBumpFeeRetryTime(_ context.Context) time.Duration {
 	return a.mgr.GetBumpFeeRetryTime()
 }
 
-func (a *SimpleTxmgrAPI) SetBumpFeeRetryTime(ctx context.Context, val time.Duration) {
+func (a *SimpleTxmgrAPI) SetBumpFeeRetryTime(_ context.Context, val time.Duration) {
 	a.mgr.SetBumpFeeRetryTime(val)
 }
