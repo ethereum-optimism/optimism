82ca09f198ec2309d4cfba2a02bdd2a7338e4803
siddharth0a <maeng779@gmail.com>
2024-08-15 23:03:19 +0900

    add construction function for WrappedCloser

--- op-service/ioutil/gzip.go
+++ op-service/ioutil/gzip.go
@@ -16,20 +16,36 @@ type WrappedReadCloser struct {
 	closer io.Closer
 }
 
-// Close closes both the gzip.Reader and the underlying reader.
-func (g *WrappedReadCloser) Close() error {
-	return errors.Join(g.ReadCloser.Close(), g.closer.Close())
-}
-
 // WrappedWriteCloser is a struct that closes both the gzip.Writer and the underlying io.Closer.
 type WrappedWriteCloser struct {
 	io.WriteCloser
 	closer io.Closer
 }
 
+// Close closes both the gzip.Reader and the underlying reader.
+func (g *WrappedReadCloser) Close() error {
+	return errors.Join(g.ReadCloser.Close(), g.closer.Close())
+}
+
 // Close closes both the gzip.Writer and the underlying writer.
 func (g *WrappedWriteCloser) Close() error {
 	return errors.Join(g.WriteCloser.Close(), g.closer.Close())
+}
+
+// NewWrappedReadCloser is a constructor function that initializes a WrappedReadCloser structure.
+func NewWrappedReadCloser(r io.ReadCloser, c io.Closer) *WrappedReadCloser {
+	return &WrappedReadCloser{
+		ReadCloser: r,
+		closer:     c,
+	}
+}
+
+// NewWrappedWriteCloser is a constructor function that initializes a WrappedWriteCloser structure.
+func NewWrappedWriteCloser(r io.WriteCloser, c io.Closer) *WrappedWriteCloser {
+	return &WrappedWriteCloser{
+		WriteCloser: r,
+		closer:      c,
+	}
 }
 
 // OpenDecompressed opens a reader for the specified file and automatically gzip decompresses the content
