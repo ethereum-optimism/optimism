ab9115f171f7518624c0e14a9a9155da0bbda0ff
protolambda <proto@protolambda.com>
2024-08-14 19:32:21 -0600

    fix lint

--- packages/contracts-bedrock/scripts/ScriptExample.s.sol
+++ packages/contracts-bedrock/scripts/ScriptExample.s.sol
@@ -7,7 +7,6 @@ import { console2 as console } from "forge-std/console2.sol";
 /// @title ScriptExample
 /// @notice ScriptExample is an example script. The Go forge script code tests that it can run this.
 contract ScriptExample is CommonBase {
-
     /// @notice example function, runs through basic cheat-codes and console logs.
     function run() public view {
         console.log("contract addr", address(this));
