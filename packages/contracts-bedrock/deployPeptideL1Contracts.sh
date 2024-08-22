351cbd5020e24944d07401c90b6fd276ff438b21
RnkSngh <raunaksingh2.718@gmail.com>
2024-08-22 01:47:48 -0400

    add parsing to deploypeptidescripts

--- packages/contracts-bedrock/deployPeptideL1Contracts.sh
+++ packages/contracts-bedrock/deployPeptideL1Contracts.sh
@@ -1,14 +1,33 @@
 
-export DEPLOY_CONFIG_PATH=./deploy-config/polymer-mainnet.json
+export OLD_DEPLOY_CONFIG_PATH=./deploy-config/polymer-mainnet.json
+export DEPLOY_CONFIG_PATH=./deploy-config/polymer-mainnet-1.json
 export IMPL_SALT="polymer-deploy-1"
 
 cat $DEPLOY_CONFIG_PATH
 
-echo $(pwd)
+export blockNumber=$(cast block --rpc-url $LOCAL_RPC| grep "number" | grep -Eo '[0-9]+' | sed 's/\.$//')
+
+echo $blockNumber
 # Deploy l2output oracle address
+jq --arg new_value $blockNumber '.systemConfigStartBlock= $new_value' $OLD_DEPLOY_CONFIG_PATH > $DEPLOY_CONFIG_PATH
+
+# forge script \
+#     scripts/deploy/Deploy.s.sol:Deploy \
+#     --sig deployPolymerL1Contracts \
+#     --broadcast \
+#     --private-key $DUMMY_PRIVATE_KEY \
+#     --rpc-url $LOCAL_RPC
+
 forge script \
     scripts/deploy/Deploy.s.sol:Deploy \
-    --sig deployPolymerL1Contracts \
     --broadcast \
     --private-key $DUMMY_PRIVATE_KEY \
     --rpc-url $LOCAL_RPC
+
+
+export FORK=latest
+export CONTRACT_ADDRESSES_PATH=./deployments/1-deploy.json
+export STATE_DUMP_PATH=peptide-allocs.json
+
+forge script scripts/L2Genesis.s.sol:L2Genesis \
+    --sig 'runWithStateDump()'
