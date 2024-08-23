c2c8f401e3aed52f6d7e5f27486cf07aa33c8bfd
inphi <mlaw2501@gmail.com>
2024-08-22 23:55:53 -0400

    cannon: Enable program VM debugging for tests
    - Also add a symbol lookup routine to the FPVM interface for
      convenience.

--- cannon/mipsevm/testutil/elf.go
+++ cannon/mipsevm/testutil/elf.go
@@ -10,8 +10,22 @@ import (
 )
 
 func LoadELFProgram[T mipsevm.FPVMState](t require.TestingT, name string, initState program.CreateInitialFPVMState[T], doPatchGo bool) T {
+	state, _ := loadELFProgram(t, name, initState, doPatchGo, false)
+	return state
+}
+
+func LoadELFProgramWithMetadata[T mipsevm.FPVMState](t require.TestingT, name string, initState program.CreateInitialFPVMState[T], doPatchGo bool) (T, *program.Metadata) {
+	return loadELFProgram(t, name, initState, doPatchGo, true)
+}
+
+func loadELFProgram[T mipsevm.FPVMState](t require.TestingT, name string, initState program.CreateInitialFPVMState[T], doPatchGo bool, loadMetadata bool) (T, *program.Metadata) {
 	elfProgram, err := elf.Open(name)
 	require.NoError(t, err, "open ELF file")
+	var meta *program.Metadata
+	if loadMetadata {
+		meta, err = program.MakeMetadata(elfProgram)
+		require.NoError(t, err, "load metadata")
+	}
 
 	state, err := program.LoadELF(elfProgram, initState)
 	require.NoError(t, err, "load ELF into state")
@@ -22,5 +36,5 @@ func LoadELFProgram[T mipsevm.FPVMState](t require.TestingT, name string, initSt
 	}
 
 	require.NoError(t, program.PatchStack(state), "add initial stack")
-	return state
+	return state, meta
 }
