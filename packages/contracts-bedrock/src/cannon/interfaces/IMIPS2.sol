7fadaa5bb4f6bb7adf6235d6370aff1406d72c46
Kelvin Fichter <kelvinfichter@gmail.com>
2024-08-27 19:41:10 -0400

    maint: add interfaces for cannon contracts

---
+++ packages/contracts-bedrock/src/cannon/interfaces/IMIPS2.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.15;
+
+import { ISemver } from "src/universal/ISemver.sol";
+
+/// @title IMIPS2
+/// @notice Interface for the MIPS2 contract.
+interface IMIPS2 is ISemver {
+    function step(bytes memory _stateData, bytes memory _proof, bytes32 _localContext) external returns (bytes32);
+}
