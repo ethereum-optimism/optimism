b8b4a4302dbf9d927f8fac3792f1106ca69164c7
agusduha <agusnduha@gmail.com>
2024-08-29 18:15:05 -0300

    test: remove unused import

--- packages/contracts-bedrock/test/vendor/InitializableOZv5.t.sol
+++ packages/contracts-bedrock/test/vendor/InitializableOZv5.t.sol
@@ -3,7 +3,6 @@ pragma solidity 0.8.25;
 
 import { Test } from "forge-std/Test.sol";
 import { OptimismSuperchainERC20 } from "src/L2/OptimismSuperchainERC20.sol";
-import { SuperchainERC20 } from "src/L2/SuperchainERC20.sol";
 import { Initializable } from "@openzeppelin/contracts-v5/proxy/utils/Initializable.sol";
 
 /// @title InitializerOZv5_Test
