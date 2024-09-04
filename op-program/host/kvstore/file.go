e80d23b64ceb207e8f70c2a497f9d6d49155e7ac
Adrian Sutton <adrian@oplabs.co>
2024-09-04 12:47:55 +1000

    op-program: Use PebbleDB for DiskKV (Reapply with fixes) (#11709)
    * feat(op-program): Use `PebbleDB` for `DiskKV` (#11705)
    
    * feat(op-program): Use `PebbleDB` for `DiskKV`
    
    * close db
    
    * fix `testFaultProofProgramScenario` tests
    
    * switch to snappy compression
    
    https://github.com/cockroachdb/pebble/issues/3434
    
    * fix tempdir
    
    * update compat release
    
    * defer k/v until preimage server and hinter have both exited
    
    * Only open preimage source when actually needing preimages.
    
    Avoids concurrent access to the pebbledb.
    
    * op-program: Handle interrupts in host
    
    Ensures the preimage kv store is closed cleanly even when the process is sent an interrupt signal.
    Enables non-blocking IO so that read calls return io.EOF when a file handle is closed rather than blocking forever.
    
    * op-program: Restore file based kv store. Add pebble as an additional option.
    
    * op-program: Add option to specify KV store format to use
    
    * op-program: Switch pebble to perform sync writes.
    
    * op-program: Rename disk kv to file kv
    
    * op-program: Rename DBFormat to DataFormat for consistency.
    
    ---------
    
    Co-authored-by: clabby <ben@clab.by>

--- op-program/host/kvstore/file.go
+++ op-program/host/kvstore/file.go
@@ -13,28 +13,28 @@ import (
 )
 
 // read/write mode for user/group/other, not executable.
-const diskPermission = 0666
+const filePermission = 0666
 
-// DiskKV is a disk-backed key-value store, every key-value pair is a hex-encoded .txt file, with the value as content.
-// DiskKV is safe for concurrent use with a single DiskKV instance.
-// DiskKV is safe for concurrent use between different DiskKV instances of the same disk directory as long as the
+// FileKV is a disk-backed key-value store, every key-value pair is a hex-encoded .txt file, with the value as content.
+// FileKV is safe for concurrent use with a single FileKV instance.
+// FileKV is safe for concurrent use between different FileKV instances of the same disk directory as long as the
 // file system supports atomic renames.
-type DiskKV struct {
+type FileKV struct {
 	sync.RWMutex
 	path string
 }
 
-// NewDiskKV creates a DiskKV that puts/gets pre-images as files in the given directory path.
+// NewFileKV creates a FileKV that puts/gets pre-images as files in the given directory path.
 // The path must exist, or subsequent Put/Get calls will error when it does not.
-func NewDiskKV(path string) *DiskKV {
-	return &DiskKV{path: path}
+func NewFileKV(path string) *FileKV {
+	return &FileKV{path: path}
 }
 
-func (d *DiskKV) pathKey(k common.Hash) string {
+func (d *FileKV) pathKey(k common.Hash) string {
 	return path.Join(d.path, k.String()+".txt")
 }
 
-func (d *DiskKV) Put(k common.Hash, v []byte) error {
+func (d *FileKV) Put(k common.Hash, v []byte) error {
 	d.Lock()
 	defer d.Unlock()
 	f, err := openTempFile(d.path, k.String()+".txt.*")
@@ -72,10 +72,10 @@ func openTempFile(dir string, nameTemplate string) (*os.File, error) {
 	return f, nil
 }
 
-func (d *DiskKV) Get(k common.Hash) ([]byte, error) {
+func (d *FileKV) Get(k common.Hash) ([]byte, error) {
 	d.RLock()
 	defer d.RUnlock()
-	f, err := os.OpenFile(d.pathKey(k), os.O_RDONLY, diskPermission)
+	f, err := os.OpenFile(d.pathKey(k), os.O_RDONLY, filePermission)
 	if err != nil {
 		if errors.Is(err, os.ErrNotExist) {
 			return nil, ErrNotFound
@@ -90,4 +90,8 @@ func (d *DiskKV) Get(k common.Hash) ([]byte, error) {
 	return hex.DecodeString(string(dat))
 }
 
-var _ KV = (*DiskKV)(nil)
+func (d *FileKV) Close() error {
+	return nil
+}
+
+var _ KV = (*FileKV)(nil)
