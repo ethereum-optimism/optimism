f954cbb8dcb050c5fcf3421c092ea218561524b2
smartcontracts <kelvin@optimism.io>
2024-09-06 19:32:38 -0400

    ci: speed up CI by up to 50% (#11794)
    Introduces a number of improvements to CI that significantly
    reduce overal CI time by up to 50% of the previous time.

--- packages/contracts-bedrock/scripts/checks/check-snapshots.sh
+++ packages/contracts-bedrock/scripts/checks/check-snapshots.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# Generate the snapshots
-just snapshots
+# Check for the --no-build flag
+# Generate snapshots
+if [ "$1" == "--no-build" ]; then
+    just snapshots-no-build
+else
+    just snapshots
+fi
 
 # Check if the generated `snapshots` files are different from the committed versions
 if git diff --exit-code snapshots > /dev/null; then
