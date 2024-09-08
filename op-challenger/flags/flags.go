40750a58e7a4a6f06370d18dfe6c6eab309012d9
Adrian Sutton <adrian@oplabs.co>
2024-09-09 07:54:31 +1000

    op-challenger: Support binary and JSON snapshots (#11754)
    * op-challenger: Support loading json or binary cannon states.
    
    * op-challenger: Use binary cannon snapshots
    
    * op-challenger: Support downloading prestates in multiple formats.
    
    * op-challenger: Verify newly downloaded snapshots.
    
    * op-e2e: Update test to handle binary snapshots correctly.
    
    * op-e2e: Remove unused parameter
    
    * op-challenger: Add more varied data to the test cannon state.
    
    * op-challenger: Add more varied data to the test cannon state.

--- op-challenger/flags/flags.go
+++ op-challenger/flags/flags.go
@@ -578,6 +578,7 @@ func NewConfigFromCLI(ctx *cli.Context, logger log.Logger) (*config.Config, erro
 			SnapshotFreq:     ctx.Uint(CannonSnapshotFreqFlag.Name),
 			InfoFreq:         ctx.Uint(CannonInfoFreqFlag.Name),
 			DebugInfo:        true,
+			BinarySnapshots:  true,
 		},
 		CannonAbsolutePreState:        ctx.String(CannonPreStateFlag.Name),
 		CannonAbsolutePreStateBaseURL: cannonPrestatesURL,
