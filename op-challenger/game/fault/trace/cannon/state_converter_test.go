1dd1668fbe3b0f37432b762bd2259103cb105d51
Adrian Sutton <adrian@oplabs.co>
2024-09-10 09:24:38 +1000

    op-challenger: Update canon state loading test to cover multiple state versions.

--- op-challenger/game/fault/trace/cannon/state_converter_test.go
+++ op-challenger/game/fault/trace/cannon/state_converter_test.go
@@ -1,13 +1,12 @@
 package cannon
 
 import (
-	"compress/gzip"
 	_ "embed"
-	"encoding/json"
-	"os"
 	"path/filepath"
 	"testing"
 
+	"github.com/ethereum-optimism/optimism/cannon/mipsevm"
+	"github.com/ethereum-optimism/optimism/cannon/mipsevm/multithreaded"
 	"github.com/ethereum-optimism/optimism/cannon/mipsevm/versions"
 	"github.com/ethereum-optimism/optimism/cannon/serialize"
 	"github.com/stretchr/testify/require"
@@ -15,59 +14,78 @@ import (
 	"github.com/ethereum-optimism/optimism/cannon/mipsevm/singlethreaded"
 )
 
-//go:embed test_data/state.json
-var testState []byte
-
 func TestLoadState(t *testing.T) {
-	t.Run("Uncompressed", func(t *testing.T) {
-		dir := t.TempDir()
-		path := filepath.Join(dir, "state.json")
-		require.NoError(t, os.WriteFile(path, testState, 0644))
+	tests := []struct {
+		name         string
+		creator      func() mipsevm.FPVMState
+		supportsJSON bool
+	}{
+		{
+			name:         "singlethreaded",
+			creator:      func() mipsevm.FPVMState { return singlethreaded.CreateInitialState(234, 82) },
+			supportsJSON: true,
+		},
+		{
+			name:         "multithreaded",
+			creator:      func() mipsevm.FPVMState { return multithreaded.CreateInitialState(982, 492) },
+			supportsJSON: false,
+		},
+	}
+	for _, test := range tests {
+		test := test
+		loadExpectedState := func(t *testing.T) *versions.VersionedState {
+			state, err := versions.NewFromState(test.creator())
+			require.NoError(t, err)
+			return state
+		}
+		t.Run(test.name, func(t *testing.T) {
+			t.Run("Uncompressed", func(t *testing.T) {
+				if !test.supportsJSON {
+					t.Skip("JSON not supported by state version")
+				}
+				expected := loadExpectedState(t)
+				path := writeState(t, "state.json", expected)
 
-		state, err := parseState(path)
-		require.NoError(t, err)
+				state, err := parseState(path)
+				require.NoError(t, err)
 
-		expected := loadExpectedState(t)
-		require.Equal(t, expected, state)
-	})
+				require.Equal(t, expected, state)
+			})
 
-	t.Run("Gzipped", func(t *testing.T) {
-		dir := t.TempDir()
-		path := filepath.Join(dir, "state.json.gz")
-		f, err := os.OpenFile(path, os.O_WRONLY|os.O_TRUNC|os.O_CREATE, 0644)
-		require.NoError(t, err)
-		defer f.Close()
-		writer := gzip.NewWriter(f)
-		_, err = writer.Write(testState)
-		require.NoError(t, err)
-		require.NoError(t, writer.Close())
+			t.Run("Gzipped", func(t *testing.T) {
+				if !test.supportsJSON {
+					t.Skip("JSON not supported by state version")
+				}
+				expected := loadExpectedState(t)
+				path := writeState(t, "state.json.gz", expected)
 
-		state, err := parseState(path)
-		require.NoError(t, err)
+				state, err := parseState(path)
+				require.NoError(t, err)
 
-		expected := loadExpectedState(t)
-		require.Equal(t, expected, state)
-	})
+				require.Equal(t, expected, state)
+			})
 
-	t.Run("Binary", func(t *testing.T) {
-		expected := loadExpectedState(t)
+			t.Run("Binary", func(t *testing.T) {
+				expected := loadExpectedState(t)
 
-		path := writeState(t, "state.bin", expected)
+				path := writeState(t, "state.bin", expected)
 
-		state, err := parseState(path)
-		require.NoError(t, err)
-		require.Equal(t, expected, state)
-	})
+				state, err := parseState(path)
+				require.NoError(t, err)
+				require.Equal(t, expected, state)
+			})
 
-	t.Run("BinaryGzip", func(t *testing.T) {
-		expected := loadExpectedState(t)
+			t.Run("BinaryGzip", func(t *testing.T) {
+				expected := loadExpectedState(t)
 
-		path := writeState(t, "state.bin.gz", expected)
+				path := writeState(t, "state.bin.gz", expected)
 
-		state, err := parseState(path)
-		require.NoError(t, err)
-		require.Equal(t, expected, state)
-	})
+				state, err := parseState(path)
+				require.NoError(t, err)
+				require.Equal(t, expected, state)
+			})
+		})
+	}
 }
 
 func writeState(t *testing.T, filename string, state *versions.VersionedState) string {
@@ -75,12 +93,4 @@ func writeState(t *testing.T, filename string, state *versions.VersionedState) s
 	path := filepath.Join(dir, filename)
 	require.NoError(t, serialize.Write(path, state, 0644))
 	return path
-}
-
-func loadExpectedState(t *testing.T) *versions.VersionedState {
-	var expected singlethreaded.State
-	require.NoError(t, json.Unmarshal(testState, &expected))
-	state, err := versions.NewFromState(&expected)
-	require.NoError(t, err)
-	return state
 }
