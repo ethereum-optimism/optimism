ab05aa2a892cf51c242fa5fd9b685bce3c3cd879
Matt Joiner <anacrolix@gmail.com>
2024-09-09 16:16:48 +1000

    Disable stdout debugging

--- op-node/rollup/driver/state.go
+++ op-node/rollup/driver/state.go
@@ -296,11 +296,11 @@ func (s *Driver) OnEvent(ev event.Event) bool {
 		}()
 		return true
 	case engine.ForkchoiceUpdateEvent:
-		fmt.Println("fork choice update event")
+		//fmt.Println("fork choice update event")
 		s.forkChoiceUpdated.Broadcast()
 		return true
 	case engine.ForkchoiceRequestEvent:
-		fmt.Println("fork choice request event")
+		//fmt.Println("fork choice request event")
 		s.forkChoiceUpdated.Broadcast()
 		return true
 	default:
