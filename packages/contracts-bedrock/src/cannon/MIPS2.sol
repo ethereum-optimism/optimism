d84e43bec1d7929cda086a57b419f1a15ddd206f
mbaxter <meredith@oplabs.co>
2024-09-16 11:24:50 -0400

    cannon: Bump MIP2.sol version

--- packages/contracts-bedrock/src/cannon/MIPS2.sol
+++ packages/contracts-bedrock/src/cannon/MIPS2.sol
@@ -57,8 +57,8 @@ contract MIPS2 is ISemver {
     }
 
     /// @notice The semantic version of the MIPS2 contract.
-    /// @custom:semver 1.0.0-beta.8
-    string public constant version = "1.0.0-beta.8";
+    /// @custom:semver 1.0.0-beta.9
+    string public constant version = "1.0.0-beta.9";
 
     /// @notice The preimage oracle contract.
     IPreimageOracle internal immutable ORACLE;
