4b1b9a8e144e918576de0d5f8e46899b76a7c183
axelKingsley <axel.kingsley@gmail.com>
2024-09-18 10:24:53 -0500

    update generate.sh

--- op-e2e/interop/contracts/generate.sh
+++ op-e2e/interop/contracts/generate.sh
@@ -4,9 +4,9 @@ set -euo
 
 forge build
 
-pushd test-artifacts/emit.sol
+cd contracts/emit.sol
 cat EmitEvent.json | jq -r '.bytecode.object' > EmitEvent.bin
 cat EmitEvent.json | jq '.abi' > EmitEvent.abi
-popd
+cd ../..
 
 abigen --abi ./test-artifacts/emit.sol/EmitEvent.abi --bin ./test-artifacts/emit.sol/EmitEvent.bin --pkg emit --out ./emit.go
