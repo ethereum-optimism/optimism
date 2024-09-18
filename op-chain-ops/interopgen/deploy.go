213f7627da4ee053f15d746119af2c8304ec3134
Matthew Slipper <me@matthewslipper.com>
2024-09-18 15:02:58 -0600

    Add inspection commands, fix deployer bugs (#11964)
    Adds `op-deployer inspection *` commands to expose the generated genesis and rollup configs via the CLI or Golang libraries. Also fixes a bug in deployment - I accidentally deleted the code that set the deployer nonce, so live chain deployments were reverting.

--- op-chain-ops/interopgen/deploy.go
+++ op-chain-ops/interopgen/deploy.go
@@ -289,7 +289,7 @@ func completeL2(l2Host *script.Host, cfg *L2Config, l1Block *types.Block, deploy
 		},
 	}
 	// l1Block is used to determine genesis time.
-	l2Genesis, err := genesis.NewL2Genesis(deployCfg, l1Block)
+	l2Genesis, err := genesis.NewL2Genesis(deployCfg, l1Block.Header())
 	if err != nil {
 		return nil, fmt.Errorf("failed to build L2 genesis config: %w", err)
 	}
@@ -314,7 +314,7 @@ func completeL2(l2Host *script.Host, cfg *L2Config, l1Block *types.Block, deploy
 	l2Genesis.Alloc = allocs.Accounts
 	l2GenesisBlock := l2Genesis.ToBlock()
 
-	rollupCfg, err := deployCfg.RollupConfig(l1Block, l2GenesisBlock.Hash(), l2GenesisBlock.NumberU64())
+	rollupCfg, err := deployCfg.RollupConfig(l1Block.Header(), l2GenesisBlock.Hash(), l2GenesisBlock.NumberU64())
 	if err != nil {
 		return nil, fmt.Errorf("failed to build L2 rollup config: %w", err)
 	}
