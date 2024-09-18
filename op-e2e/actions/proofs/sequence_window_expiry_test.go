6ca2be19610844dea7cd7a1dcf04ec74704c250b
clabby <ben@clab.by>
2024-09-18 18:21:38 -0400

    feat(op-e2e): Open channel closed after sequence window expiry test
    Adds a test to the proof actions that proves a block at the safe head
    after the batcher has opened a channel, allowed the sequence window to
    expire, and then attempted to close their open channel after the fact.

--- op-e2e/actions/proofs/sequence_window_expiry_test.go
+++ op-e2e/actions/proofs/sequence_window_expiry_test.go
@@ -48,6 +48,79 @@ func runSequenceWindowExpireTest(gt *testing.T, testCfg *helpers.TestCfg[any]) {
 	env.RunFaultProofProgram(t, l2SafeHead.Number.Uint64()/2, testCfg.CheckResult, testCfg.InputParams...)
 }
 
+// Runs a that proves a block in a chain where the batcher opens a channel, the sequence window expires, and then the
+// batcher attempts to close the channel afterwards.
+func runSequenceWindowExpire_ChannelCloseAfterWindowExpiry_Test(gt *testing.T, testCfg *helpers.TestCfg[any]) {
+	t := actionsHelpers.NewDefaultTesting(gt)
+	tp := helpers.NewTestParams()
+	env := helpers.NewL2FaultProofEnv(t, testCfg, tp, helpers.NewBatcherCfg())
+
+	// Mine 2 empty blocks on L2.
+	for i := 0; i < 2; i++ {
+		env.Sequencer.ActL2StartBlock(t)
+		env.Sequencer.ActL2EndBlock(t)
+	}
+
+	// Open the channel on L1.
+	env.Batcher.ActL2BatchBuffer(t)
+	env.Batcher.ActL2BatchSubmit(t)
+	env.Miner.ActL1StartBlock(12)(t)
+	env.Miner.ActL1IncludeTxByHash(env.Batcher.LastSubmitted.Hash())(t)
+	env.Miner.ActL1EndBlock(t)
+
+	// Finalize the block with the first channel frame on L1.
+	env.Miner.ActL1SafeNext(t)
+	env.Miner.ActL1FinalizeNext(t)
+
+	// Instruct the sequencer to derive the L2 chain from the data on L1 that the batcher just posted.
+	env.Sequencer.ActL1HeadSignal(t)
+	env.Sequencer.ActL2PipelineFull(t)
+
+	// Ensure the safe head is still 0.
+	l2SafeHead := env.Engine.L2Chain().CurrentSafeBlock()
+	require.EqualValues(t, 0, l2SafeHead.Number.Uint64())
+
+	// Expire the sequence window by building `SequenceWindow + 1` empty blocks on L1.
+	for i := 0; i < int(tp.SequencerWindowSize)+1; i++ {
+		env.Alice.L1.ActResetTxOpts(t)
+		env.Alice.ActDeposit(t)
+
+		env.Miner.ActL1StartBlock(12)(t)
+		env.Miner.ActL1IncludeTx(env.Alice.Address())(t)
+		env.Miner.ActL1EndBlock(t)
+
+		env.Miner.ActL1SafeNext(t)
+		env.Miner.ActL1FinalizeNext(t)
+	}
+
+	// Instruct the batcher the channel on L1, after the sequence window + channel timeout has elapsed.
+	env.Batcher.ActL2BatchBuffer(t)
+	env.Batcher.ActL2ChannelClose(t)
+	env.Batcher.ActL2BatchSubmit(t)
+	env.Miner.ActL1StartBlock(12)(t)
+	env.Miner.ActL1IncludeTxByHash(env.Batcher.LastSubmitted.Hash())(t)
+	env.Miner.ActL1EndBlock(t)
+
+	// Finalize the block with the second channel frame on L1.
+	env.Miner.ActL1SafeNext(t)
+	env.Miner.ActL1FinalizeNext(t)
+
+	// Ensure the safe head is still 0.
+	l2SafeHead = env.Engine.L2Chain().CurrentSafeBlock()
+	require.EqualValues(t, 0, l2SafeHead.Number.Uint64())
+
+	// Ask the sequencer to derive the deposit-only L2 chain.
+	env.Sequencer.ActL1HeadSignal(t)
+	env.Sequencer.ActL2PipelineFull(t)
+
+	// Ensure the safe head advanced forcefully.
+	l2SafeHead = env.Engine.L2Chain().CurrentSafeBlock()
+	require.Greater(t, l2SafeHead.Number.Uint64(), uint64(0))
+
+	// Run the FPP on one of the auto-derived blocks.
+	env.RunFaultProofProgram(t, l2SafeHead.Number.Uint64()/2, testCfg.CheckResult, testCfg.InputParams...)
+}
+
 func Test_ProgramAction_SequenceWindowExpired(gt *testing.T) {
 	matrix := helpers.NewMatrix[any]()
 	defer matrix.Run(gt)
@@ -64,6 +137,21 @@ func Test_ProgramAction_SequenceWindowExpired(gt *testing.T) {
 		nil,
 		helpers.LatestForkOnly,
 		runSequenceWindowExpireTest,
+		helpers.ExpectError(claim.ErrClaimNotValid),
+		helpers.WithL2Claim(common.HexToHash("0xdeadbeef")),
+	)
+	matrix.AddTestCase(
+		"ChannelCloseAfterWindowExpiry-HonestClaim",
+		nil,
+		helpers.LatestForkOnly,
+		runSequenceWindowExpire_ChannelCloseAfterWindowExpiry_Test,
+		helpers.ExpectNoError(),
+	)
+	matrix.AddTestCase(
+		"ChannelCloseAfterWindowExpiry-JunkClaim",
+		nil,
+		helpers.LatestForkOnly,
+		runSequenceWindowExpire_ChannelCloseAfterWindowExpiry_Test,
 		helpers.ExpectError(claim.ErrClaimNotValid),
 		helpers.WithL2Claim(common.HexToHash("0xdeadbeef")),
 	)
