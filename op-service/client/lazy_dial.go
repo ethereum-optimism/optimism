cb665a87821807721158ebf408de281f68b8ff22
Samuel Stokes <sam.adam.stokes@gmail.com>
2024-09-23 23:14:14 -0400

    op-service: fix lazy_dial

--- op-service/client/lazy_dial.go
+++ op-service/client/lazy_dial.go
@@ -49,7 +49,7 @@ func (l *LazyRPC) dial(ctx context.Context) error {
 	if err != nil {
 		return fmt.Errorf("failed to dial: %w", err)
 	}
-	l.inner = &BaseRPCClient{c: underlying}
+	l.inner = NewBaseRPCClient(underlying)
 	return nil
 }
 
@@ -66,6 +66,7 @@ func (l *LazyRPC) CallContext(ctx context.Context, result any, method string, ar
 	if err := l.dial(ctx); err != nil {
 		return err
 	}
+	fmt.Println("checkpoin 1")
 	return l.inner.CallContext(ctx, result, method, args...)
 }
 
