f99c98064cec1f8402946621b6dc0a48a12ff90a
tre <tremarkley@gmail.com>
2024-09-24 12:41:52 -0700

    bump version

--- packages/contracts-bedrock/src/L2/L2ToL2CrossDomainMessenger.sol
+++ packages/contracts-bedrock/src/L2/L2ToL2CrossDomainMessenger.sol
@@ -58,7 +58,7 @@ contract L2ToL2CrossDomainMessenger is IL2ToL2CrossDomainMessenger, ISemver, Tra
 
     /// @notice Semantic version.
     /// @custom:semver 1.0.0-beta.4
-    string public constant version = "1.0.0-beta.4";
+    string public constant version = "1.0.0-beta.5";
 
     /// @notice Mapping of message hashes to boolean receipt values. Note that a message will only be present in this
     ///         mapping if it has successfully been relayed on this chain, and can therefore not be relayed again.
