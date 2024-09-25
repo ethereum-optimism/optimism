56502ddc8773dc13b057445111e9aeaa5b8d14f0
Inphi <mlaw2501@gmail.com>
2024-09-25 00:34:05 -0400

    cannon: Multi VM executor (#12072)
    * cannon: Multi VM executor
    
    * fix run subcmd arg fwding
    
    * fix mt prestate
    
    * add list subcmd; multicannon in op-stack-go
    
    * remove cannon-latest
    
    * safer strconv
    
    * lint
    
    * include .gitkeep in embed
    
    * fix .git copy
    
    * add detect.go tests
    
    * add nosemgrep
    
    * review comments
    
    * list filtering
    
    * add note to MIPS.sol in version stf ref
    
    * use fork-exec
    
    * minimal flag parsing
    
    * load old cannon binaries from docker images
    
    * note
    
    * --help flag defaults
    
    * remove redundant copy from cannon-builder-0

---
+++ cannon/multicannon/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"os"
+
+	"github.com/ethereum-optimism/optimism/op-service/ctxinterrupt"
+	"github.com/urfave/cli/v2"
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "multicannon"
+	app.Usage = "MIPS Fault Proof tool"
+	app.Description = "MIPS Fault Proof tool"
+	app.Commands = []*cli.Command{
+		LoadELFCommand,
+		WitnessCommand,
+		RunCommand,
+		ListCommand,
+	}
+	ctx := ctxinterrupt.WithCancelOnInterrupt(context.Background())
+	err := app.RunContext(ctx, os.Args)
+	if err != nil {
+		if errors.Is(err, ctx.Err()) {
+			_, _ = fmt.Fprintf(os.Stderr, "command interrupted")
+			os.Exit(130)
+		} else {
+			_, _ = fmt.Fprintf(os.Stderr, "error: %v", err)
+			os.Exit(1)
+		}
+	}
+}
