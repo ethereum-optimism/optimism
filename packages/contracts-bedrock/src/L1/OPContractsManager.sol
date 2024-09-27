20e8ac5a3a608485e5b18022b868400011cfc2d1
Matthew Slipper <me@matthewslipper.com>
2024-09-27 11:18:06 -0600

    bump semver

--- packages/contracts-bedrock/src/L1/OPContractsManager.sol
+++ packages/contracts-bedrock/src/L1/OPContractsManager.sol
@@ -129,7 +129,7 @@ contract OPContractsManager is ISemver, Initializable {
     // -------- Constants and Variables --------
 
     /// @custom:semver 1.0.0-beta.14
-    string public constant version = "1.0.0-beta.14";
+    string public constant version = "1.0.0-beta.15";
 
     /// @notice Represents the interface version so consumers know how to decode the DeployOutput struct
     /// that's emitted in the `Deployed` event. Whenever that struct changes, a new version should be used.
