faec33112966d85dba30e20fec8e3a0e789ed806
Mark Tyneway <mark.tyneway@gmail.com>
2024-09-28 20:28:44 -0700

    portal interop: cleanup

--- packages/contracts-bedrock/src/L1/OptimismPortalInterop.sol
+++ packages/contracts-bedrock/src/L1/OptimismPortalInterop.sol
@@ -3,7 +3,6 @@ pragma solidity 0.8.15;
 
 // Contracts
 import { OptimismPortal2 } from "src/L1/OptimismPortal2.sol";
-import { L1BlockInterop, ConfigType } from "src/L2/L1BlockInterop.sol";
 
 // Libraries
 import { Predeploys } from "src/libraries/Predeploys.sol";
@@ -26,30 +25,5 @@ contract OptimismPortalInterop is OptimismPortal2 {
     /// @custom:semver +interop-beta.1
     function version() public pure override returns (string memory) {
         return string.concat(super.version(), "+interop-beta.1");
-    }
-
-    /// @notice Sets static configuration options for the L2 system.
-    /// @param _type  Type of configuration to set.
-    /// @param _value Encoded value of the configuration.
-    function setConfig(ConfigType _type, bytes memory _value) external override {
-        if (msg.sender != address(systemConfig)) revert Unauthorized();
-
-        // Set L2 deposit gas as used without paying burning gas. Ensures that deposits cannot use too much L2 gas.
-        // This value must be large enough to cover the cost of calling `L1Block.setConfig`.
-        useGas(SYSTEM_DEPOSIT_GAS_LIMIT);
-
-        // Emit the special deposit transaction directly that sets the config in the L1Block predeploy contract.
-        emit TransactionDeposited(
-            Constants.DEPOSITOR_ACCOUNT,
-            Predeploys.L1_BLOCK_ATTRIBUTES,
-            DEPOSIT_VERSION,
-            abi.encodePacked(
-                uint256(0), // mint
-                uint256(0), // value
-                uint64(SYSTEM_DEPOSIT_GAS_LIMIT), // gasLimit
-                false, // isCreation,
-                abi.encodeCall(L1BlockInterop.setConfig, (_type, _value))
-            )
-        );
     }
 }
