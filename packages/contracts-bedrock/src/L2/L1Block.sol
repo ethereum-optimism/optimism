5bd72f690bf09a6ae3bbe1802c4be60cf99628b3
Roberto Bayardo <roberto.bayardo@coinbase.com>
2024-09-30 14:50:45 -0700

    Holocene extensions to L1Block.sol (#12096)

--- packages/contracts-bedrock/src/L2/L1Block.sol
+++ packages/contracts-bedrock/src/L2/L1Block.sol
@@ -57,9 +57,15 @@ contract L1Block is ISemver, IGasToken {
     /// @notice The latest L1 blob base fee.
     uint256 public blobBaseFee;
 
-    /// @custom:semver 1.5.1-beta.2
+    /// @notice The eip-1550 base fee change denominator value.
+    uint64 public eip1559Denominator;
+
+    /// @notice The eip-1550 base fee change elasticity value.
+    uint64 public eip1559Elasticity;
+
+    /// @custom:semver 1.5.1-beta.3
     function version() public pure virtual returns (string memory) {
-        return "1.5.1-beta.2";
+        return "1.5.1-beta.3";
     }
 
     /// @notice Returns the gas paying token, its decimals, name and symbol.
@@ -165,6 +171,59 @@ contract L1Block is ISemver, IGasToken {
             sstore(blobBaseFee.slot, calldataload(68)) // uint256
             sstore(hash.slot, calldataload(100)) // bytes32
             sstore(batcherHash.slot, calldataload(132)) // bytes32
+        }
+    }
+
+    /// @notice Updates the L1 block values for a Holocene upgraded chain.
+    /// Params are packed and passed in as raw msg.data instead of ABI to reduce calldata size.
+    /// Params are expected to be in the following order:
+    ///   1. _baseFeeScalar      L1 base fee scalar
+    ///   2. _blobBaseFeeScalar  L1 blob base fee scalar
+    ///   3. _sequenceNumber     Number of L2 blocks since epoch start.
+    ///   4. _timestamp          L1 timestamp.
+    ///   5. _number             L1 blocknumber.
+    ///   6. _basefee            L1 base fee.
+    ///   7. _blobBaseFee        L1 blob base fee.
+    ///   8. _hash               L1 blockhash.
+    ///   9. _batcherHash        Versioned hash to authenticate batcher by.
+    ///   10. _eip1559Elasticity EIP-1559 elasticity multiplier value.
+    ///   11. _eip1559Denominator EIP-1559 base fee change denominator value.
+    function setL1BlockValuesHolocene() public {
+        _setL1BlockValuesHolocene();
+    }
+
+    /// @notice Updates the L1 block values for a Holocene upgraded chain.
+    /// Params are packed and passed in as raw msg.data instead of ABI to reduce calldata size.
+    /// Params are expected to be in the following order:
+    ///   1. _baseFeeScalar      L1 base fee scalar
+    ///   2. _blobBaseFeeScalar  L1 blob base fee scalar
+    ///   3. _sequenceNumber     Number of L2 blocks since epoch start.
+    ///   4. _timestamp          L1 timestamp.
+    ///   5. _number             L1 blocknumber.
+    ///   6. _basefee            L1 base fee.
+    ///   7. _blobBaseFee        L1 blob base fee.
+    ///   8. _hash               L1 blockhash.
+    ///   9. _batcherHash        Versioned hash to authenticate batcher by.
+    ///   10. _eip1559Elasticity EIP-1559 elasticity multiplier value.
+    ///   11. _eip1559Denominator EIP-1559 base fee change denominator value.
+    function _setL1BlockValuesHolocene() internal {
+        address depositor = DEPOSITOR_ACCOUNT();
+        assembly {
+            // Revert if the caller is not the depositor account.
+            if xor(caller(), depositor) {
+                mstore(0x00, 0x3cc50b45) // 0x3cc50b45 is the 4-byte selector of "NotDepositor()"
+                revert(0x1C, 0x04) // returns the stored 4-byte selector from above
+            }
+            // sequencenum (uint64), blobBaseFeeScalar (uint32), baseFeeScalar (uint32)
+            sstore(sequenceNumber.slot, shr(128, calldataload(4)))
+            // number (uint64) and timestamp (uint64)
+            sstore(number.slot, shr(128, calldataload(20)))
+            sstore(basefee.slot, calldataload(36)) // uint256
+            sstore(blobBaseFee.slot, calldataload(68)) // uint256
+            sstore(hash.slot, calldataload(100)) // bytes32
+            sstore(batcherHash.slot, calldataload(132)) // bytes32
+            // eip1559Denominator (uint64) and eip1559Elasticity (uint64)
+            sstore(eip1559Denominator.slot, shr(128, calldataload(164))) // uint64
         }
     }
 
