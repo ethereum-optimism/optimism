6f812788540c73d286dbe1473522842e9a58cfa2
Michael Amadi <amadimichaeld@gmail.com>
2024-10-01 02:40:40 +0100

    fix change

--- packages/contracts-bedrock/test/opcm/DeployImplementations.t.sol
+++ packages/contracts-bedrock/test/opcm/DeployImplementations.t.sol
@@ -20,6 +20,7 @@ import { IL1StandardBridge } from "src/L1/interfaces/IL1StandardBridge.sol";
 import { IOptimismMintableERC20Factory } from "src/universal/interfaces/IOptimismMintableERC20Factory.sol";
 import { IProxyAdmin } from "src/universal/interfaces/IProxyAdmin.sol";
 import { IProxy } from "src/universal/interfaces/IProxy.sol";
+import { console2 } from "forge-std/console2.sol";
 
 import {
     DeployImplementationsInput,
@@ -96,8 +97,12 @@ contract DeployImplementationsOutput_Test is Test {
     }
 
     function test_set_succeeds() public {
-        IProxy proxy =
-            IProxy(DeployUtils.create1({ _name: "Proxy", _args: abi.encodeCall(IProxy.__constructor__, (address(0))) }));
+        IProxy proxy = IProxy(
+            DeployUtils.create1({
+                _name: "Proxy",
+                _args: DeployUtils.encodeConstructor(abi.encodeCall(IProxy.__constructor__, (address(0))))
+            })
+        );
         address opcmImpl = address(makeAddr("opcmImpl"));
         vm.prank(address(0));
         proxy.upgradeTo(opcmImpl);
