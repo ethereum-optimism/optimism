c971b2430f17efc88db1d5d966f9ccc4762b9777
Mark Tyneway <mark.tyneway@gmail.com>
2024-10-03 15:45:49 -0700

    contracts-bedrock: fixes `OptimismMintableERC721Factory` test
    This contract is only used as a predeploy so it should be tested based
    on the L2 genesis generation setup. Ensures that the address is stored
    and labeled as part of in memory test setup, as well as updates the
    tests to work based on the predeploy.
    
    This is a small refactor to make future Standard L2 genesis work more
    simple.
    
    Also includes a small ABI fix to the `OptimismMintableERC721Factory`
    since this contract has not been updated to decouple immutable naming
    conventions from its ABI.

--- packages/contracts-bedrock/src/universal/interfaces/IOptimismMintableERC721Factory.sol
+++ packages/contracts-bedrock/src/universal/interfaces/IOptimismMintableERC721Factory.sol
@@ -6,15 +6,11 @@ interface IOptimismMintableERC721Factory {
 
     function BRIDGE() external view returns (address);
     function REMOTE_CHAIN_ID() external view returns (uint256);
-    function createOptimismMintableERC721(
-        address _remoteToken,
-        string memory _name,
-        string memory _symbol
-    )
+    function bridge() external view returns (address);
+    function createOptimismMintableERC721(address _remoteToken, string memory _name, string memory _symbol)
         external
         returns (address);
     function isOptimismMintableERC721(address) external view returns (bool);
+    function remoteChainID() external view returns (uint256);
     function version() external view returns (string memory);
-
-    function __constructor__(address _bridge, uint256 _remoteChainId) external;
 }
