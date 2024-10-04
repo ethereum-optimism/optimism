ec12fd16b6c2efca0f5694a92d2a105c19b8c378
clabby <ben@clab.by>
2024-10-04 15:48:46 -0400

    update spec test

--- packages/contracts-bedrock/test/universal/Specs.t.sol
+++ packages/contracts-bedrock/test/universal/Specs.t.sol
@@ -486,6 +486,8 @@ contract Specification_Test is CommonTest {
         _addSpec({ _name: "SystemConfigInterop", _sel: _getSel("VERSION()") });
         _addSpec({ _name: "SystemConfigInterop", _sel: _getSel("batcherHash()") });
         _addSpec({ _name: "SystemConfigInterop", _sel: _getSel("gasLimit()") });
+        _addSpec({ _name: "SystemConfigInterop", _sel: _getSel("eip1559Denominator()") });
+        _addSpec({ _name: "SystemConfigInterop", _sel: _getSel("eip1559Elasticity()") });
         _addSpec({ _name: "SystemConfigInterop", _sel: ISystemConfig.initialize.selector });
         _addSpec({ _name: "SystemConfigInterop", _sel: ISystemConfig.minimumGasLimit.selector });
         _addSpec({ _name: "SystemConfigInterop", _sel: _getSel("overhead()") });
@@ -501,6 +503,11 @@ contract Specification_Test is CommonTest {
         _addSpec({
             _name: "SystemConfigInterop",
             _sel: ISystemConfig.setGasConfig.selector,
+            _auth: Role.SYSTEMCONFIGOWNER
+        });
+        _addSpec({
+            _name: "SystemConfigInterop",
+            _sel: ISystemConfig.setEIP1559Params.selector,
             _auth: Role.SYSTEMCONFIGOWNER
         });
         _addSpec({
