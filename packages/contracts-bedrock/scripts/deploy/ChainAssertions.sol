17233586a5303786328ccf184bc22dac64e4cb6f
Mark Tyneway <mark.tyneway@gmail.com>
2024-10-04 09:25:36 -0700

    lint: fix

--- packages/contracts-bedrock/scripts/deploy/ChainAssertions.sol
+++ packages/contracts-bedrock/scripts/deploy/ChainAssertions.sol
@@ -564,10 +564,7 @@ library ChainAssertions {
     function assertSlotValueIsSet(address _contractAddress, uint256 _slot, uint256 _offset) internal view {
         bytes32 slotVal = vm.load(_contractAddress, bytes32(_slot));
         uint8 val = uint8((uint256(slotVal) >> (_offset * 8)) & 0xFF);
-        require(
-            val == uint8(1) || val == uint8(0xff),
-            "Storage value is not 1 or 0xff at the given slot and offset"
-        );
+        require(val == uint8(1) || val == uint8(0xff), "Storage value is not 1 or 0xff at the given slot and offset");
     }
 
     /// @notice Asserts that the SuperchainConfig is setup correctly
