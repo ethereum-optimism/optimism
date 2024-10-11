0466b62d21403f851760bf5f50e6db3d35db7668
Maurelian <john@oplabs.co>
2024-10-11 14:40:37 -0400

    deployOpChain using OPCM (#12291)
    * feat: move log to top of save function
    
    Makes debugging slightly easier.
    
    * feat: Deploy system with OPCM
    
    * feat: Deploy missing Permissionless DWETH
    
    * feat: Fully OPCM based deployment
    
    At this point tests that depend on the L2OutputOracle will still fail,
    but those with Fault Proofs _should_ pass.
    
    * feat: Refactor _setFaultGameImplementation
    
    It no longer handles the PERMISSIONED game type deploy by OPCM.
    
    * test: Make caller explicit in unauthorized test case
    
    * test: Fix have guardian setRespectedGameType to Cannon
    
    THis is necessary because the tests assume the respecte game is cannon,
    but the OPCM assumes it is the permissioned game
    
    * test: Bridge_Initializer defaulst to FP on
    
    * test: setRespectedGameType to Cannon in Deploy
    
    * test: Bridge_Initializer defaulst to FP on
    
    * feat: resetInitializedProxy func to allow reinitializing contracts
    
    * feat: Delete initializeOpChain, we don't need it anymore
    
    * feat: Set batch inbox address in config using hashed method
    
    * feat: Make caller explicit in testFuzz_removeDependency_notDependencyManager_reverts
    
    * feat: lint
    
    * Revert "test: Bridge_Initializer defaulst to FP on"
    
    This reverts commit d435653b5405f35970e9663cfddd5a7214fcdb57.
    
    * feat: Consolidate useFaultProofs modifications into _run
    
    * Revert "test: Bridge_Initializer defaulst to FP on"
    
    This reverts commit af8d99b94393c3adef32b6ae5d5384e6766569e5.
    
    * feat: Some annotations in Deploy.s.ol
    
    * feat: Skip checking L2OO in Initializable.t.sol
    
    * feat: Delete unused initialize functions
    
    * fix: Remove unused imports
    
    * fix: unused import
    
    * feat: Use respectedGameType from deploy-config
    
    * use startPrank
    
    * feat: Prevent deploying legacy portal for interop
    
    * fix: incorrect var name
    
    * feat: detect when OPCM is deploying the Permissionless game
    
    * fix: error format

--- packages/contracts-bedrock/scripts/libraries/Constants.sol
+++ packages/contracts-bedrock/scripts/libraries/Constants.sol
@@ -10,12 +10,16 @@ import { GameTypes, OutputRoot, Hash } from "src/dispute/lib/Types.sol";
 ///         should be defined in that contract instead.
 library Constants {
     /// @notice Returns the default starting anchor roots value to be used in a new dispute game.
+    function DEFAULT_OUTPUT_ROOT() internal pure returns (OutputRoot memory) {
+        return OutputRoot({ root: Hash.wrap(bytes32(hex"dead")), l2BlockNumber: 0 });
+    }
+
     function DEFAULT_STARTING_ANCHOR_ROOTS() internal pure returns (IAnchorStateRegistry.StartingAnchorRoot[] memory) {
         IAnchorStateRegistry.StartingAnchorRoot[] memory defaultStartingAnchorRoots =
             new IAnchorStateRegistry.StartingAnchorRoot[](1);
         defaultStartingAnchorRoots[0] = IAnchorStateRegistry.StartingAnchorRoot({
             gameType: GameTypes.PERMISSIONED_CANNON,
-            outputRoot: OutputRoot({ root: Hash.wrap(bytes32(hex"dead")), l2BlockNumber: 0 })
+            outputRoot: DEFAULT_OUTPUT_ROOT()
         });
         return defaultStartingAnchorRoots;
     }
