014a4ce35f12207be7cfb2131d81191ae4eab6b4
angel-ding-cb <angel.ding@coinbase.com>
2024-10-14 13:55:56 -0500

    potential fix 2

--- op-node/service.go
+++ op-node/service.go
@@ -236,6 +236,7 @@ Conflicting configuration is deprecated, and will stop the op-node from starting
 		if err != nil {
 			return nil, err
 		}
+		rollupConfig.BlockBuildingThreshold = time.Duration(blockBuidlingThreshold) * time.Second
 		return rollupConfig, nil
 	}
 
@@ -251,8 +252,6 @@ Conflicting configuration is deprecated, and will stop the op-node from starting
 	if err := dec.Decode(&rollupConfig); err != nil {
 		return nil, fmt.Errorf("failed to decode rollup config: %w", err)
 	}
-	log.Info("***222 the block building threshold is set to", "threshold", blockBuidlingThreshold)
-	log.Info("***333 the block building threshold is set to", "threshold", time.Duration(blockBuidlingThreshold)*time.Second)
 	rollupConfig.BlockBuildingThreshold = time.Duration(blockBuidlingThreshold) * time.Second
 	return &rollupConfig, nil
 }
