0432cd753b7e4a37a97465af7fa2a7cb7655d695
Qi Zhou <qizhou@ethstorage.io>
2024-10-17 05:36:22 +0000

    feat: op-program --network flag accepts number as chainid

--- op-program/verify/devnet/cmd/devnet.go
+++ op-program/verify/devnet/cmd/devnet.go
@@ -44,7 +44,7 @@ func main() {
 	}
 
 	// Apply the custom configs by running op-program in the same process
-	runner, err := verify.NewRunner(l1RpcUrl, l1RpcKind, l1BeaconUrl, l2RpcUrl, dataDir, "", 901, true)
+	runner, err := verify.NewRunner(l1RpcUrl, l1RpcKind, l1BeaconUrl, l2RpcUrl, dataDir, "901", 901, false)
 	if err != nil {
 		_, _ = fmt.Fprintf(os.Stderr, "Failed to create runner: %v\n", err.Error())
 		os.Exit(1)
