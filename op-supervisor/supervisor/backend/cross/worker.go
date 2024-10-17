95701576d94c5d4a232e1a25c883f9215d3997aa
axelKingsley <axel.kingsley@gmail.com>
2024-10-17 16:28:53 -0500

    Configurable WorkFn for Workers

--- op-supervisor/supervisor/backend/cross/worker.go
+++ op-supervisor/supervisor/backend/cross/worker.go
@@ -25,7 +25,13 @@ type Worker struct {
 
 	chain types.ChainID
 
-	deps CrossSafeDeps
+	// safe and unsafe dependencies
+	// only one of these is expected to be used by the worker
+	// and would be used in conjunction with a matching workFn
+	safeDeps   CrossSafeDeps
+	unsafeDeps CrossUnsafeDeps
+	// workFn is the function to call to process the scope
+	workFn workFn
 
 	// channel with capacity of 1, full if there is work to do
 	poke chan struct{}
@@ -39,20 +45,90 @@ type Worker struct {
 	wg     sync.WaitGroup
 }
 
+// NewCrossSafeVerifier creates a new worker to process cross-safe updates
+// it sets the safeDeps, and the workFn to a crossSafeWorkFn
 func NewCrossSafeVerifier(log log.Logger, chain types.ChainID, deps CrossSafeDeps) *Worker {
 	ctx, cancel := context.WithCancel(context.Background())
 	out := &Worker{
-		log:    log,
-		chain:  chain,
-		deps:   deps,
-		poke:   make(chan struct{}, 1),
-		out:    make(chan struct{}, 1),
-		ctx:    ctx,
-		cancel: cancel,
+		log:      log,
+		chain:    chain,
+		safeDeps: deps,
+		poke:     make(chan struct{}, 1),
+		out:      make(chan struct{}, 1),
+		ctx:      ctx,
+		cancel:   cancel,
 	}
+	out.workFn = crossSafeWorkFn(out)
 	out.wg.Add(1)
 	go out.worker()
 	return out
+}
+
+// NewCrossUnsafeVerifier creates a new worker to process cross-unsafe updates
+// it sets the unsafeDeps, and the workFn to a crossUnsafeWorkFn
+func NewCrossUnsafeVerifier(log log.Logger, chain types.ChainID, deps CrossUnsafeDeps) *Worker {
+	ctx, cancel := context.WithCancel(context.Background())
+	out := &Worker{
+		log:        log,
+		chain:      chain,
+		unsafeDeps: deps,
+		poke:       make(chan struct{}, 1),
+		out:        make(chan struct{}, 1),
+		ctx:        ctx,
+		cancel:     cancel,
+	}
+	out.workFn = crossUnsafeWorkFn(out)
+	out.wg.Add(1)
+	go out.worker()
+	return out
+}
+
+// workFn is a function used by the worker
+// it is opaque to the worker, and is set by the constructor
+type workFn func() error
+
+// crossUnsafeWorkFn creates a workFn to process cross-unsafe updates for a worker
+func crossUnsafeWorkFn(s *Worker) workFn {
+	return func() error {
+		ctx, cancel := context.WithTimeout(s.ctx, maxCrossSafeUpdateDuration)
+		// TODO: rather than "scope.Process", we need an actual function to process the scope
+		// as far as I can tell, no such thing exists as "scope" in this context
+		err := s.scope.Process(ctx)
+		cancel()
+		if err != nil {
+			if errors.Is(err, ctx.Err()) {
+				s.log.Debug("Processed some, but not all data", "err", err)
+				return err
+			} else {
+				s.log.Error("Failed to process new block", "err", err)
+				return err
+			}
+		}
+		s.log.Debug("Continuing cross-safe-processing")
+		return nil
+	}
+}
+
+// crossSafeWorkFn creates a workFn to process cross-safe updates for a worker
+func crossSafeWorkFn(s *Worker) workFn {
+	return func() error {
+		ctx, cancel := context.WithTimeout(s.ctx, maxCrossSafeUpdateDuration)
+		// TODO: rather than "scope.Process", we need an actual function to process the scope
+		// as far as I can tell, no such thing exists as "scope" in this context
+		err := s.scope.Process(ctx)
+		cancel()
+		if err != nil {
+			if errors.Is(err, ctx.Err()) {
+				s.log.Debug("Processed some, but not all data", "err", err)
+				return err
+			} else {
+				s.log.Error("Failed to process new block", "err", err)
+				return err
+			}
+		}
+		s.log.Debug("Continuing cross-safe-processing")
+		return nil
+	}
 }
 
 func (s *Worker) worker() {
@@ -64,18 +140,9 @@ func (s *Worker) worker() {
 			return
 		}
 
-		ctx, cancel := context.WithTimeout(s.ctx, maxCrossSafeUpdateDuration)
-		err := s.scope.Process(ctx)
-		cancel()
-		if err != nil {
-			if errors.Is(err, ctx.Err()) {
-				s.log.Debug("Processed some, but not all data", "err", err)
-			} else {
-				s.log.Error("Failed to process new block", "err", err)
-			}
-			// idle until next update trigger (or resource-context may make the worker stop)
-		} else {
-			s.log.Debug("Continuing cross-safe-processing")
+		// do the work
+		err := s.workFn()
+		if err == nil {
 			continue
 		}
 
