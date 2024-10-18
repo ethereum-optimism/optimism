a10023ed6d3e5444af0c8d886ec8ed840b0d96fd
Blaine Malone <blainemalone01@gmail.com>
2024-10-18 14:31:14 -0400

    op-deployer: update default gas limit values.

--- op-deployer/pkg/deployer/pipeline/opchain.go
+++ op-deployer/pkg/deployer/pipeline/opchain.go
@@ -42,7 +42,7 @@ func DeployOPChain(ctx context.Context, env *Env, bundle ArtifactsBundle, intent
 		L2ChainId:               chainID.Big(),
 		OpcmProxy:               st.ImplementationsDeployment.OpcmProxyAddress,
 		SaltMixer:               st.Create2Salt.String(), // passing through salt generated at state initialization
-		GasLimit:                30_000_000,
+		GasLimit:                60_000_000,
 		DisputeGameType:         1, // PERMISSIONED_CANNON Game Type
 		DisputeAbsolutePrestate: common.HexToHash("0x038512e02c4c3f7bdaec27d00edf55b7155e0905301e1a88083e4e0a6764d54c"),
 		DisputeMaxGameDepth:     73,
