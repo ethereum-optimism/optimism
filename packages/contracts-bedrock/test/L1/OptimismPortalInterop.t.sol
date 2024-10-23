22584e66261e1243c54c1d765d81d434a30f11bd
Maurelian <maurelian@protonmail.ch>
2024-10-23 15:18:39 -0400

    test: OptimismPortalInterop - test all auth failures in one fuzz test
    test: OptimismPortalInterop - test all setConfig succeeds in one fuzz test

--- packages/contracts-bedrock/test/L1/OptimismPortalInterop.t.sol
+++ packages/contracts-bedrock/test/L1/OptimismPortalInterop.t.sol
@@ -29,8 +29,11 @@ contract OptimismPortalInterop_Test is CommonTest {
         super.setUp();
     }
 
-    /// @dev Tests that the config for the gas paying token can be set.
-    function testFuzz_setConfig_gasPayingToken_succeeds(bytes calldata _value) public {
+    /// @dev Tests that any config type can be set by the system config.
+    function testFuzz_setConfig_succeeds(uint8 _configType, bytes calldata _value) public {
+        // Ensure that _configType is within the range of the ConfigType enum
+        _configType = uint8(bound(uint256(_configType), 0, uint256(type(Types.ConfigType).max)));
+
         vm.expectEmit(address(optimismPortal));
         emitTransactionDeposited({
             _from: Constants.DEPOSITOR_ACCOUNT,
@@ -39,63 +42,28 @@ contract OptimismPortalInterop_Test is CommonTest {
             _mint: 0,
             _gasLimit: 200_000,
             _isCreation: false,
-            _data: abi.encodeCall(L1BlockInterop.setConfig, (Types.ConfigType.SET_GAS_PAYING_TOKEN, _value))
+            _data: abi.encodeCall(L1BlockInterop.setConfig, (Types.ConfigType(_configType), _value))
         });
 
         vm.prank(address(_optimismPortalInterop().systemConfig()));
-        _optimismPortalInterop().setConfig(Types.ConfigType.SET_GAS_PAYING_TOKEN, _value);
+        _optimismPortalInterop().setConfig(Types.ConfigType(_configType), _value);
     }
 
-    /// @dev Tests that setting the gas paying token config as not the system config reverts.
-    function testFuzz_setConfig_gasPayingToken_notSystemConfig_reverts(bytes calldata _value) public {
+    /// @dev Tests that setting any config type as not the system config reverts.
+    function testFuzz_setConfig_notSystemConfig_reverts(
+        address _caller,
+        uint8 _configType,
+        bytes calldata _value
+    )
+        external
+    {
+        // Ensure that _configType is within the range of the ConfigType enum
+        _configType = uint8(bound(uint256(_configType), 0, uint256(type(Types.ConfigType).max)));
+
+        vm.assume(_caller != address(_optimismPortalInterop().systemConfig()));
+        vm.prank(_caller);
         vm.expectRevert(Unauthorized.selector);
-        _optimismPortalInterop().setConfig(Types.ConfigType.SET_GAS_PAYING_TOKEN, _value);
-    }
-
-    /// @dev Tests that the config for adding a dependency can be set.
-    function testFuzz_setConfig_addDependency_succeeds(bytes calldata _value) public {
-        vm.expectEmit(address(optimismPortal));
-        emitTransactionDeposited({
-            _from: Constants.DEPOSITOR_ACCOUNT,
-            _to: Predeploys.L1_BLOCK_ATTRIBUTES,
-            _value: 0,
-            _mint: 0,
-            _gasLimit: 200_000,
-            _isCreation: false,
-            _data: abi.encodeCall(L1BlockInterop.setConfig, (Types.ConfigType.ADD_DEPENDENCY, _value))
-        });
-
-        vm.prank(address(_optimismPortalInterop().systemConfig()));
-        _optimismPortalInterop().setConfig(Types.ConfigType.ADD_DEPENDENCY, _value);
-    }
-
-    /// @dev Tests that setting the add dependency config as not the system config reverts.
-    function testFuzz_setConfig_addDependency_notSystemConfig_reverts(bytes calldata _value) public {
-        vm.expectRevert(Unauthorized.selector);
-        _optimismPortalInterop().setConfig(Types.ConfigType.ADD_DEPENDENCY, _value);
-    }
-
-    /// @dev Tests that the config for removing a dependency can be set.
-    function testFuzz_setConfig_removeDependency_succeeds(bytes calldata _value) public {
-        vm.expectEmit(address(optimismPortal));
-        emitTransactionDeposited({
-            _from: Constants.DEPOSITOR_ACCOUNT,
-            _to: Predeploys.L1_BLOCK_ATTRIBUTES,
-            _value: 0,
-            _mint: 0,
-            _gasLimit: 200_000,
-            _isCreation: false,
-            _data: abi.encodeCall(L1BlockInterop.setConfig, (Types.ConfigType.REMOVE_DEPENDENCY, _value))
-        });
-
-        vm.prank(address(_optimismPortalInterop().systemConfig()));
-        _optimismPortalInterop().setConfig(Types.ConfigType.REMOVE_DEPENDENCY, _value);
-    }
-
-    /// @dev Tests that setting the remove dependency config as not the system config reverts.
-    function testFuzz_setConfig_removeDependency_notSystemConfig_reverts(bytes calldata _value) public {
-        vm.expectRevert(Unauthorized.selector);
-        _optimismPortalInterop().setConfig(Types.ConfigType.REMOVE_DEPENDENCY, _value);
+        _optimismPortalInterop().setConfig(Types.ConfigType(_configType), _value);
     }
 
     /// @dev Returns the OptimismPortalInterop instance.
