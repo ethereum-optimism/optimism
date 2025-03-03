e72a898ab5c0f5dcb0ec6d3fc79aa263cd510593
Julian Meyer <julian.meyer@coinbase.com>
2025-03-03 12:18:59 -0800

    Remove untested EIP from comment

--- op-e2e/actions/proofs/isthmus_requests_test.go
+++ op-e2e/actions/proofs/isthmus_requests_test.go
@@ -17,7 +17,7 @@ import (
 )
 
 func TestIsthmusExcludedPredeploys(gt *testing.T) {
-	// Ensures that if EIP-6110, EIP-7251, or EIP-7002 predeploys are deployed manually after the fork,
+	// Ensures that if EIP-7251, or EIP-7002 predeploys are deployed manually after the fork,
 	// Isthmus block processing still works correctly. Also ensures that if requests are sent to these
 	// contracts, they are not processed and do not show up in the block body or requests hash.
 
