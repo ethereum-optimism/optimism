8b90ce44e17f5c97d109849910b5ff0188706037
Matthias Seitz <matthias.seitz@outlook.de>
2025-03-04 20:08:11 +0100

    fix: apply eip2935 for op (paradigmxyz/reth#14832)

--- evm/src/execute.rs
+++ evm/src/execute.rs
@@ -162,6 +162,7 @@ where
             self.chain_spec.is_spurious_dragon_active_at_block(self.evm.block().number);
         self.evm.db_mut().set_state_clear_flag(state_clear_flag);
 
+        self.system_caller.apply_blockhashes_contract_call(self.ctx.parent_hash, &mut self.evm)?;
         self.system_caller
             .apply_beacon_root_contract_call(self.ctx.parent_beacon_block_root, &mut self.evm)?;
 
