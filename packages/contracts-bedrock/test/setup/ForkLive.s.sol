0d2f36e4606b1ca863508e990addca9adb61dc5c
JosepBove <josep@oplabs.co>
2025-03-06 10:23:50 +0100

    superchain-ops like fix

--- packages/contracts-bedrock/test/setup/ForkLive.s.sol
+++ packages/contracts-bedrock/test/setup/ForkLive.s.sol
@@ -108,7 +108,7 @@ contract ForkLive is Deployer {
         // Superchain shared contracts
         saveProxyAndImpl("SuperchainConfig", superchainToml, ".superchain_config_addr");
         saveProxyAndImpl("ProtocolVersions", superchainToml, ".protocol_versions_addr");
-        artifacts.save("OPContractsManager", vm.parseTomlAddress(standardVersionsToml, "$.op_contracts_manager.address"));
+        artifacts.save("OPContractsManager", vm.parseTomlAddress(standardVersionsToml, "$.RELEASE.op_contracts_manager.address"));
 
         // Core contracts
         artifacts.save("ProxyAdmin", vm.parseTomlAddress(opToml, ".addresses.ProxyAdmin"));
