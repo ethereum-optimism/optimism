3580eb789a625d1031a0e2fc6d03ba701b666931
Park Changwan <changwan@oplabs.co>
2025-03-12 18:52:53 +0900

    Add low level client access

--- devnet-sdk/system/interfaces.go
+++ devnet-sdk/system/interfaces.go
@@ -13,6 +13,7 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	coreTypes "github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/ethereum/go-ethereum/params"
 )
 
