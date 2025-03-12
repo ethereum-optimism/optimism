4b58b64e75f7dc4fd60c25bf53efa9018034c4f4
JosepBove <josep@oplabs.co>
2025-03-12 18:05:42 +0100

    remove correct import

--- packages/contracts-bedrock/test/L1/OPContractsManager.t.sol
+++ packages/contracts-bedrock/test/L1/OPContractsManager.t.sol
@@ -2,8 +2,7 @@
 pragma solidity 0.8.15;
 
 // Testing
-import { stdStorage, StdStorage } from "forge-std/Test.sol";
-import { console } from "forge-std/console.sol";
+import { Test, stdStorage, StdStorage } from "forge-std/Test.sol";
 import { VmSafe } from "forge-std/Vm.sol";
 import { CommonTest } from "test/setup/CommonTest.sol";
 import { DeployOPChain_TestBase } from "test/opcm/DeployOPChain.t.sol";
