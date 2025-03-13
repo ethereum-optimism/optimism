7c03293a2a495df364c417fc8f6d35b06a25fb36
Eric Woolsey <ewoolsey@ualberta.ca>
2025-03-12 19:54:15 -0700

    fix for log target string (flashbots/rollup-boost)

--- src/tracing.rs
+++ src/tracing.rs
@@ -1,3 +1,5 @@
+use std::str::FromStr;
+
 use eyre::Context as _;
 use metrics::histogram;
 use opentelemetry::trace::TracerProvider as _;
@@ -9,7 +11,7 @@ use tracing::level_filters::LevelFilter;
 use tracing_opentelemetry::OpenTelemetryLayer;
 use tracing_subscriber::filter::Targets;
 use tracing_subscriber::layer::SubscriberExt;
-use tracing_subscriber::Layer;
+use tracing_subscriber::{EnvFilter, Layer};
 
 use crate::{Args, LogFormat};
 
@@ -48,11 +50,18 @@ impl SpanProcessor for MetricsSpanProcessor {
 }
 
 pub(crate) fn init_tracing(args: &Args) -> eyre::Result<()> {
+    // Be cautious with snake_case and kebab-case here
+    let filter_name = "rollup_boost".to_string();
+
+    let global_filter = Targets::new()
+        .with_default(LevelFilter::INFO)
+        .with_target(&filter_name, LevelFilter::TRACE);
+
+    let registry = tracing_subscriber::registry().with(global_filter);
+
     let log_filter = Targets::new()
         .with_default(LevelFilter::INFO)
-        .with_target("rollup-boost", args.log_level);
-
-    let registry = tracing_subscriber::registry();
+        .with_target(&filter_name, args.log_level);
 
     // Weird control flow here is required because of type system
     if args.tracing {
@@ -84,9 +93,13 @@ pub(crate) fn init_tracing(args: &Args) -> eyre::Result<()> {
 
         let trace_filter = Targets::new()
             .with_default(LevelFilter::OFF)
-            .with_target(env!("CARGO_PKG_NAME"), LevelFilter::TRACE);
+            .with_target(&filter_name, LevelFilter::TRACE);
 
-        let registry = registry.with(OpenTelemetryLayer::new(tracer).with_filter(trace_filter));
+        let registry = registry.with(
+            OpenTelemetryLayer::new(tracer)
+                .with_level(true)
+                .with_filter(trace_filter),
+        );
 
         match args.log_format {
             LogFormat::Json => {
