09e9ab2a18785ecb56ef95e607fe2ceb9913d2ac
Park Changwan <changwan@oplabs.co>
2025-03-20 00:24:03 +0900

    add init/exec trigger

--- op-service/txintent/interop/interop.go
+++ op-service/txintent/interop/interop.go
@@ -3,16 +3,23 @@ package txintent
 import (
 	"context"
 	"fmt"
+	"math/big"
 
+	"github.com/ethereum-optimism/optimism/devnet-sdk/contracts/constants"
 	"github.com/ethereum-optimism/optimism/op-service/eth"
 	"github.com/ethereum-optimism/optimism/op-service/plan"
 	"github.com/ethereum-optimism/optimism/op-service/txintent"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/lmittmann/w3"
 
 	suptypes "github.com/ethereum-optimism/optimism/op-supervisor/supervisor/types"
 )
+
+var _ txintent.Call = (*InitTrigger)(nil)
+var _ txintent.Call = (*ExecTrigger)(nil)
+var _ txintent.Result = (*InteropOutput)(nil)
 
 type InitTrigger struct {
 	Emitter    common.Address // address of the EventLogger contract
@@ -26,6 +33,12 @@ func (v *InitTrigger) To() (*common.Address, error) {
 
 func (v *InitTrigger) Data() ([]byte, error) {
 	// TODO format call
+	// This OpaqueData was for using the EventLogger contract to test
+	// This is a temp bypass for setting calldata for basic testing
+	return v.OpaqueData, nil
+}
+
+func (v *InitTrigger) AccessList() (types.AccessList, error) {
 	return nil, nil
 }
 
@@ -39,21 +52,47 @@ func (v *ExecTrigger) To() (*common.Address, error) {
 }
 
 func (v *ExecTrigger) Data() ([]byte, error) {
-	// TODO format call to CrossL2Inbox
-	return nil, nil
+	// TODO: Need to do better construct call input than this
+	validateMessage := w3.MustNewFunc("validateMessage((address Origin, uint256 BlockNumber, uint256 LogIndex, uint256 Timestamp, uint256 ChainId), bytes32)", "")
+	type Identifier struct {
+		Origin      common.Address
+		BlockNumber *big.Int
+		LogIndex    *big.Int
+		Timestamp   *big.Int
+		ChainId     *big.Int
+	}
+	identifier := &Identifier{
+		v.Msg.Identifier.Origin,
+		big.NewInt(int64(v.Msg.Identifier.BlockNumber)),
+		big.NewInt(int64(v.Msg.Identifier.LogIndex)),
+		big.NewInt(int64(v.Msg.Identifier.Timestamp)),
+		v.Msg.Identifier.ChainID.ToBig(),
+	}
+	validateMessageCalldata, err := validateMessage.EncodeArgs(
+		identifier,
+		v.Msg.PayloadHash,
+	)
+	if err != nil {
+		return nil, err
+	}
+	return validateMessageCalldata, nil
 }
 
-type MultiTrigger struct {
-	Calls []txintent.Call
-}
-
-func (v *MultiTrigger) Data() ([]byte, error) {
-	// TODO format multi-call
-	return nil, nil
+func (v *ExecTrigger) AccessList() (types.AccessList, error) {
+	access := v.Msg.Access()
+	accessList := types.AccessList{{
+		Address:     constants.CrossL2Inbox,
+		StorageKeys: suptypes.EncodeAccessList([]suptypes.Access{access}),
+	}}
+	return accessList, nil
 }
 
 type InteropOutput struct {
 	Entries []suptypes.Message
+}
+
+func (i *InteropOutput) Init() txintent.Result {
+	return &InteropOutput{}
 }
 
 func (i *InteropOutput) FromReceipt(ctx context.Context, rec *types.Receipt, includedIn eth.BlockRef, chainID eth.ChainID) error {
@@ -75,7 +114,7 @@ func (i *InteropOutput) FromReceipt(ctx context.Context, rec *types.Receipt, inc
 	return nil
 }
 
-func executeIndexed(events *plan.Lazy[*InteropOutput], index int) func(ctx context.Context) (*ExecTrigger, error) {
+func ExecuteIndexed(events *plan.Lazy[*InteropOutput], index int) func(ctx context.Context) (*ExecTrigger, error) {
 	return func(ctx context.Context) (*ExecTrigger, error) {
 		if x := len(events.Value().Entries); x <= index {
 			return nil, fmt.Errorf("invalid index: %d, only have %d events", index, x)
