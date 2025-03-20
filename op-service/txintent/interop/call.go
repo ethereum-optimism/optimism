0b2bc57577bb3706d2ea9d98f8c6fc7360c63981
Park Changwan <changwan@oplabs.co>
2025-03-20 23:05:45 +0900

    fully implement triggers

--- op-service/txintent/interop/call.go
+++ op-service/txintent/interop/call.go
@@ -4,6 +4,7 @@ import (
 	"math/big"
 
 	"github.com/ethereum-optimism/optimism/devnet-sdk/contracts/constants"
+	"github.com/ethereum-optimism/optimism/op-service/eth"
 	"github.com/ethereum-optimism/optimism/op-service/txintent"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
@@ -14,11 +15,14 @@ import (
 
 var _ txintent.Call = (*InitTrigger)(nil)
 var _ txintent.Call = (*ExecTrigger)(nil)
+
+var _ txintent.Call = (*SendTrigger)(nil)
 var _ txintent.Call = (*RelayTrigger)(nil)
 
+// Trigger for using the EventLogger to trigger emitLog
 type InitTrigger struct {
-	Emitter    common.Address // address of the EventLogger contract
-	Topics     []common.Hash
+	Emitter    common.Address // address of the EventLogger
+	Topics     [][32]byte
 	OpaqueData []byte
 }
 
@@ -27,18 +31,41 @@ func (v *InitTrigger) To() (*common.Address, error) {
 }
 
 func (v *InitTrigger) Data() ([]byte, error) {
-	// TODO format call
-	// This OpaqueData was for using the EventLogger contract to test
-	// This is a temp bypass for setting calldata for basic testing
-	return v.OpaqueData, nil
+	// TODO: Need to do better construct call input than this
+	emitLog := w3.MustNewFunc("emitLog(bytes32[] topics, bytes data)", "")
+	return emitLog.EncodeArgs(v.Topics, v.OpaqueData)
 }
 
 func (v *InitTrigger) AccessList() (types.AccessList, error) {
 	return nil, nil
 }
 
+// Trigger for using the L2ToL2CrossDomainMessenger to trigger sendMessage
+type SendTrigger struct {
+	Emitter         common.Address // address of the L2ToL2CrossDomainMessenger
+	DestChainID     eth.ChainID
+	Target          common.Address
+	RelayedCalldata []byte
+}
+
+func (v *SendTrigger) To() (*common.Address, error) {
+	return &v.Emitter, nil
+}
+
+func (v *SendTrigger) Data() ([]byte, error) {
+	// TODO: Need to do better construct call input than this
+	sendMessage := w3.MustNewFunc("sendMessage(uint256, address, bytes calldata)", "bytes32")
+	return sendMessage.EncodeArgs(v.DestChainID.ToBig(), v.Target, v.RelayedCalldata)
+}
+
+func (v *SendTrigger) AccessList() (types.AccessList, error) {
+	return nil, nil
+}
+
+// Trigger for using the CrossL2Inbox to trigger validateMesssage
+// This Trigger may be embedded to other triggers for preparing access lists
 type ExecTrigger struct {
-	Executor common.Address // address of the EventLogger contract
+	Executor common.Address // address of the EventLogger or CrossL2Inbox
 	Msg      suptypes.Message
 }
 
@@ -82,6 +109,7 @@ func (v *ExecTrigger) AccessList() (types.AccessList, error) {
 	return accessList, nil
 }
 
+// Trigger for using the L2ToL2CrossDomainMessenger to trigger relayMessage
 type RelayTrigger struct {
 	ExecTrigger
 	Payload []byte
