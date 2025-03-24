d9157745560cd1915ca891f5ab53452c1dc50df2
0xOsiris <djosiris@proton.me>
2025-03-24 13:04:17 -0700

    feat: make kurtosis integration ci better (flashbots/rollup-boost)

--- scripts/ci/kurtosis.sh
+++ scripts/ci/kurtosis.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -exu
+
 install() {
     # https://docs.kurtosis.com/install/
     echo "Installing Kurtosis..."
@@ -21,27 +23,35 @@ install_contender() {
     cargo install --git https://github.com/flashbots/contender --bin contender --force
 }
 
+build_and_tag() {
+    docker buildx build -t rollup-boost:develop . 
+}
+
 deploy() {
     # Run the kurtosis optimism package
     echo "Running Kurtosis..."
     kurtosis analytics disable
-    kurtosis run github.com/ethpandaops/optimism-package --args-file ./scripts/ci/kurtosis-params.yaml
+    kurtosis run github.com/ethpandaops/optimism-package --args-file ./scripts/ci/kurtosis-params.yaml --enclave op-rollup-boost
 }
 
 run() {
+    # Note we use `rollup-boost` in combination with `op-geth-builder` as the JSON RPC servers to assert transaction relaying functionality
+    # as well as inclusion of transactions that have only been sent to the builder (verifying the builder's payloads are being included in the canonical chain)
+
     # Execute 100 transfer transactions sent directly to the builder
     # Only if the builder is running and connected to the network with rollup-boost
     # the transactions will be included in the canonical blocks and finalized.
 
     # Figure out first the builder's JSON-RPC URL
-    L2_PORT=$(docker inspect --format='{{(index .NetworkSettings.Ports "8545/tcp" 0).HostPort}}' $(docker ps --filter "name=op-el-builder-" -q))
-    
+    ROLLUP_BOOST_SOCKET=$(kurtosis port print op-rollup-boost op-rollup-boost-1-op-kurtosis rpc)
+    OP_GETH_BUILDER_SOCKET=$(kurtosis port print op-rollup-boost op-el-builder-1-op-geth-op-node-op-kurtosis rpc)
+
     # Private key with prefunded balance
     PREFUNDED_PRIV_KEY=0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d
     PREFUNDED_ADDRESS=$(cast wallet address --private-key $PREFUNDED_PRIV_KEY)
 
     # Safe check to ensure there is balance in the prefunded address
-    BALANCE=$(cast balance --rpc-url http://localhost:$L2_PORT $PREFUNDED_ADDRESS)
+    BALANCE=$(cast balance --rpc-url $ROLLUP_BOOST_SOCKET $PREFUNDED_ADDRESS)
 
     # We have to check for "0" string because it is returned in wei and the value is too big to compare as a number
     if [ "$BALANCE" = "0" ]; then
@@ -52,14 +62,14 @@ run() {
     fi
     
     # Download the scenario for contender
-    wget https://raw.githubusercontent.com/flashbots/contender/refs/heads/main/scenarios/stress.toml -O /tmp/scenario.toml
+    wget https://raw.githubusercontent.com/flashbots/contender/refs/heads/main/scenarios/stress.toml -O scenario.toml
 
     # Deploy the contract with contender, this should be enough to check that the
     # builder is working as expected
-    # I have not managed to send a working transaction with cast yet. That should replace
-    # this setup eventually since we are only testing if a single transaction is included
-    # in the canonical chain.
-    contender setup -p $PREFUNDED_PRIV_KEY /tmp/scenario.toml http://localhost:$L2_PORT
+    contender setup -p $PREFUNDED_PRIV_KEY scenario.toml $ROLLUP_BOOST_SOCKET
+
+    # Run the scenario on the builder
+    contender run -p $PREFUNDED_PRIV_KEY fill-block $OP_GETH_BUILDER_SOCKET
 }
 
 clean() {
@@ -76,6 +86,9 @@ case "$1" in
     "install-contender")
         install_contender
         ;;
+    "build-and-tag")
+        build_and_tag
+        ;;
     "deploy")
         deploy
         ;;
@@ -90,6 +103,7 @@ case "$1" in
         echo "Commands:"
         echo "  install - Install Kurtosis CLI"
         echo "  install-contender - Install Contender"
+        echo "  build-and-tag - Build and tag the Development Rollup Boost image"
         echo "  deploy  - Deploy the Optimism package"
         echo "  run     - Run the Optimism package"
         echo "  clean   - Clean up the Kurtosis environment"
