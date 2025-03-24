28bb96558ab3aa6733c3b4c5f460dddd7c7e9d07
Park Changwan <changwan@oplabs.co>
2025-03-24 19:15:10 +0900

    Tests for multicall + multicalloutput

--- op-service/txintent/txintent_test.go
+++ op-service/txintent/txintent_test.go
@@ -1,8 +1,8 @@
 package txintent
 
 import (
+	"bytes"
 	"context"
-	"math/big"
 	"math/rand"
 	"testing"
 
@@ -39,7 +39,9 @@ func TestTxIntent(t *testing.T) {
 	rng := rand.New(rand.NewSource(1234))
 	ctx := context.Background()
 
-	signer := types.LatestSignerForChainID(big.NewInt(1))
+	chainID := eth.ChainIDFromUInt64(1)
+
+	signer := types.LatestSignerForChainID(chainID.ToBig())
 	randomTx := testutils.RandomAccessListTx(rng, signer)
 	tx := NewIntent[*call, *result]()
 
@@ -79,4 +81,72 @@ func TestTxIntent(t *testing.T) {
 	// Check that FromReceipt correctly processed desired result
 	require.Equal(t, randomBlockRef.Hash, result.blockHash)
 	require.Equal(t, randomBlockRef.Number, result.blockNumber)
+}
+
+func TestTxIntentMultiCall(t *testing.T) {
+	rng := rand.New(rand.NewSource(12345))
+	ctx := context.Background()
+
+	chainID := eth.ChainIDFromUInt64(1)
+
+	signer := types.LatestSignerForChainID(chainID.ToBig())
+	tx := NewIntent[*MultiTrigger, *MulticallOutput]()
+
+	callCnt := 3
+	multiTrigger := &MultiTrigger{Emitter: testutils.RandomAddress(rng)}
+	randomTxs := []*types.Transaction{}
+	for range callCnt {
+		randomTx := testutils.RandomAccessListTx(rng, signer)
+		randomTxs = append(randomTxs, randomTx)
+		// make sure To is not nil
+		randomTo := testutils.RandomAddress(rng)
+		multiTrigger.Calls = append(multiTrigger.Calls, &call{
+			to:         &randomTo,
+			data:       randomTx.Data(),
+			accessList: randomTx.AccessList(),
+		})
+	}
+	tx.Content.Fn(func(ctx context.Context) (*MultiTrigger, error) {
+		return multiTrigger, nil
+	})
+	_, err := tx.Content.Eval(ctx)
+	require.NoError(t, err)
+
+	// Evaulate to check that the Content info propagated to PlannedTx
+	to, err := tx.PlannedTx.To.Eval(ctx)
+	require.NoError(t, err)
+	require.Equal(t, multiTrigger.Emitter, *to)
+	data, err := tx.PlannedTx.Data.Eval(ctx)
+	require.NoError(t, err)
+	accessList, err := tx.PlannedTx.AccessList.Eval(ctx)
+	require.NoError(t, err)
+
+	var stackedAccessList types.AccessList
+	for _, call := range multiTrigger.Calls {
+		// Check batched tx contains each calldata
+		subData, err := call.Data()
+		require.NoError(t, err)
+		require.True(t, bytes.Contains(data, subData))
+		// Check batched tx contains each accesslist
+		subAccessList, err := call.AccessList()
+		require.NoError(t, err)
+		stackedAccessList = append(stackedAccessList, subAccessList...)
+	}
+	require.Equal(t, stackedAccessList, accessList)
+
+	randomReceipt := testutils.RandomReceipt(rng, signer, randomTxs[0], 0, 0)
+	randomBlockRef := testutils.RandomBlockRef(rng)
+	randomReceipt.BlockHash = randomBlockRef.Hash
+
+	tx.PlannedTx.Included.Set(randomReceipt)
+	tx.PlannedTx.IncludedBlock.Set(randomBlockRef)
+	tx.PlannedTx.ChainID.Set(chainID)
+
+	result, err := tx.Result.Eval(ctx)
+	require.NoError(t, err)
+
+	// Check that FromReceipt correctly processed desired result
+	require.Equal(t, randomBlockRef.Hash, result.receipt.BlockHash)
+	require.Equal(t, randomBlockRef.Number, result.includedIn.Number)
+	require.Equal(t, chainID, result.chainID)
 }
