0f2f853033f541040c8ea14d9adbfb831da570b7
Samuel Stokes <sam.adam.stokes@gmail.com>
2025-03-25 13:13:15 -0400

    fix error message expectation

--- op-deployer/pkg/deployer/standard/standard.go
+++ op-deployer/pkg/deployer/standard/standard.go
@@ -137,11 +137,11 @@ func SuperchainFor(chainID uint64) (superchain.Superchain, error) {
 func ManagerImplementationAddrFor(chainID uint64, tag string) (common.Address, error) {
 	versionsData, err := L1VersionsFor(chainID)
 	if err != nil {
-		return common.Address{}, fmt.Errorf("unsupported chain ID: %d", chainID)
+		return common.Address{}, fmt.Errorf("unsupported chainID: %d", chainID)
 	}
 	versionData, ok := versionsData[validation.Semver(tag)]
 	if !ok {
-		return common.Address{}, fmt.Errorf("unsupported tag for chain ID %d: %s", chainID, tag)
+		return common.Address{}, fmt.Errorf("unsupported tag for chainID %d: %s", chainID, tag)
 	}
 	if versionData.OPContractsManager.Address != nil {
 		// op-contracts/v1.8.0 and earlier use proxied opcm
