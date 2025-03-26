0a1f7244ea16a711fb81e8f94a88775eaa4231a4
kustrun <9192608+kustrun@users.noreply.github.com>
2025-03-26 20:21:24 +0100

    feat(full-provider): Add Debug trait to FullProvider. (paradigmxyz/reth#15307)

--- evm/src/lib.rs
+++ evm/src/lib.rs
@@ -109,7 +109,8 @@ where
         Block = alloy_consensus::Block<R::Transaction>,
     >,
     OpTransaction<TxEnv>: FromRecoveredTx<N::SignedTx>,
-    R: OpReceiptBuilder<Receipt: DepositReceipt, Transaction: SignedTransaction>,
+    // Can remove Debug on R once it's added as OpReceiptBuilder constraint
+    R: OpReceiptBuilder<Receipt: DepositReceipt, Transaction: SignedTransaction> + Debug,
     Self: Send + Sync + Unpin + Clone + 'static,
 {
     type Primitives = N;
