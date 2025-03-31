30c323b43f10b41730df957be63d47801ff05177
Federico Gimenez <fgimenez@users.noreply.github.com>
2025-03-31 21:00:07 +0200

    chore: remove unused deps (paradigmxyz/reth#15416)

--- chainspec/src/lib.rs
+++ chainspec/src/lib.rs
@@ -6,6 +6,7 @@
     issue_tracker_base_url = "https://github.com/paradigmxyz/reth/issues/"
 )]
 #![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg))]
+#![cfg_attr(not(test), warn(unused_crate_dependencies))]
 #![cfg_attr(not(feature = "std"), no_std)]
 
 extern crate alloc;
--- evm/src/lib.rs
+++ evm/src/lib.rs
@@ -7,6 +7,7 @@
 )]
 #![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg))]
 #![cfg_attr(not(feature = "std"), no_std)]
+#![cfg_attr(not(test), warn(unused_crate_dependencies))]
 
 extern crate alloc;
 
