88aabdce36814d241eea1787903dd95ef347cc1e
Jan Nanista <jan.jakub.nanista@gmail.com>
2025-03-30 22:16:11 -0700

    wip: Tests

---
+++ op-chain-ops/script/deploy_test.go
@@ -0,0 +1,142 @@
+package script
+
+import (
+	"fmt"
+	"math/big"
+	"testing"
+
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/stretchr/testify/require"
+)
+
+func TestNewDeployScriptWithoutOutput(t *testing.T) {
+	type ExampleInput struct {
+		FieldA common.Address
+		FieldB common.Address
+	}
+
+	t.Run("should fail if the script does not have a specified method", func(t *testing.T) {
+		script := &mockForgeScript{
+			abi: abi.ABI{
+				Methods: map[string]abi.Method{},
+			},
+		}
+
+		_, err := NewDeployScriptWithoutOutput[ExampleInput](script, "run")
+		require.EqualError(t, err, "script MockScript does not have a method called run")
+	})
+
+	t.Run("should fail if the specified method does not have exactly one input", func(t *testing.T) {
+		script := &mockForgeScript{
+			abi: abi.ABI{
+				Methods: map[string]abi.Method{
+					"run": abi.NewMethod("Run", "run", abi.Function, "", false, false, []abi.Argument{}, []abi.Argument{}),
+				},
+			},
+		}
+
+		_, err := NewDeployScriptWithoutOutput[ExampleInput](script, "run")
+		require.EqualError(t, err, "script MockScript does not have a method run that accepts an argument of type script.ExampleInput: ABI arguments don't match Go types: ABI has 0 arguments, Go has 1")
+	})
+
+	t.Run("should fail if the specified method does not have exactly one input whose type matches the input type", func(t *testing.T) {
+		script := &mockForgeScript{
+			abi: abi.ABI{
+				Methods: map[string]abi.Method{
+					"run": abi.NewMethod("Run", "run", abi.Function, "", false, false, []abi.Argument{
+						{
+							Name: "_input",
+							Type: die(abi.NewType("uint256", "", []abi.ArgumentMarshaling{})),
+						},
+					}, []abi.Argument{}),
+				},
+			},
+		}
+
+		_, err := NewDeployScriptWithoutOutput[ExampleInput](script, "run")
+		require.EqualError(t, err, "script MockScript does not have a method run that accepts an argument of type script.ExampleInput: ABI argument _input at index 0 doesn't match Go type: ABI type uint256 (represented by *big.Int) is not assignable to Go type script.ExampleInput")
+	})
+
+	t.Run("should not fail if the specified method has exactly one input whose type matches the input type", func(t *testing.T) {
+		script := &mockForgeScript{
+			abi: abi.ABI{
+				Methods: map[string]abi.Method{
+					"run": abi.NewMethod("Run", "run", abi.Function, "", false, false, []abi.Argument{
+						{
+							Name: "_input",
+							Type: die(abi.NewType("tuple", "", []abi.ArgumentMarshaling{{Name: "fieldA", Type: "address"}, {Name: "fieldB", Type: "address"}})),
+						},
+					}, []abi.Argument{}),
+				},
+			},
+		}
+
+		deployScript, err := NewDeployScriptWithoutOutput[ExampleInput](script, "run")
+		require.NotNil(t, deployScript)
+		require.NoError(t, err)
+	})
+}
+
+func TestDeployScriptWithoutOutputImpl(t *testing.T) {
+	makeDeployScript := func(t *testing.T) (*mockForgeScript, DeployScriptWithoutOutput[*big.Int]) {
+		script := &mockForgeScript{
+			abi: abi.ABI{
+				Methods: map[string]abi.Method{
+					"run": abi.NewMethod("Run", "run", abi.Function, "", false, false, []abi.Argument{
+						{
+							Name: "_input",
+							Type: die(abi.NewType("uint256", "", []abi.ArgumentMarshaling{})),
+						},
+					}, []abi.Argument{}),
+				},
+			},
+		}
+
+		deployScript, err := NewDeployScriptWithoutOutput[*big.Int](script, "run")
+		require.NoError(t, err)
+
+		return script, deployScript
+	}
+
+	t.Run("should return an error if script.Call returns an error", func(t *testing.T) {
+		script, deployScript := makeDeployScript(t)
+		script.callError = fmt.Errorf("oh no")
+
+		err := deployScript.Run(big.NewInt(1))
+		require.EqualError(t, err, "failed to run run method of script MockScript using:\n\n1\n\n: oh no")
+	})
+
+	t.Run("should not return an error if script.Call does not return an error", func(t *testing.T) {
+		script, deployScript := makeDeployScript(t)
+		script.callResult = []byte{1}
+
+		err := deployScript.Run(big.NewInt(1))
+		require.NoError(t, err)
+	})
+}
+
+type mockForgeScript struct {
+	abi        abi.ABI
+	callResult []byte
+	callError  error
+}
+
+// ABI implements ForgeScript.
+func (m *mockForgeScript) ABI() abi.ABI {
+	return m.abi
+}
+
+// Call implements ForgeScript.
+func (m *mockForgeScript) Call(input []byte) (result []byte, err error) {
+	return m.callResult, m.callError
+}
+
+// Name implements ForgeScript.
+func (m *mockForgeScript) Name() string {
+	return "MockScript"
+}
+
+var (
+	_ ForgeScript = (*mockForgeScript)(nil)
+)
