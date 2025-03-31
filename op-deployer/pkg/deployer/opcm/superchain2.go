f2ea3ebf672f0e910b7b900d43bd3b32d62d4551
Jan Nanista <jan.jakub.nanista@gmail.com>
2025-03-31 12:29:09 -0700

    chore: DeploySuperchain2

---
+++ op-deployer/pkg/deployer/opcm/superchain2.go
@@ -0,0 +1,44 @@
+package opcm
+
+import (
+	"math/big"
+
+	"github.com/ethereum-optimism/optimism/op-chain-ops/script"
+	"github.com/ethereum/go-ethereum/common"
+)
+
+type DeploySuperchain2Input struct {
+	Guardian                   common.Address `toml:"guardian"`
+	ProtocolVersionsOwner      common.Address `toml:"protocolVersionsOwner"`
+	SuperchainProxyAdminOwner  common.Address `toml:"superchainProxyAdminOwner"`
+	Paused                     bool           `toml:"paused"`
+	RecommendedProtocolVersion *big.Int       `toml:"recommendedProtocolVersion"`
+	RequiredProtocolVersion    *big.Int       `toml:"requiredProtocolVersion"`
+}
+
+type DeploySuperchain2Output struct {
+	ProtocolVersionsImpl  common.Address `json:"protocolVersionsImplAddress"`
+	ProtocolVersionsProxy common.Address `json:"protocolVersionsProxyAddress"`
+	SuperchainConfigImpl  common.Address `json:"superchainConfigImplAddress"`
+	SuperchainConfigProxy common.Address `json:"superchainConfigProxyAddress"`
+	SuperchainProxyAdmin  common.Address `json:"proxyAdminAddress"`
+}
+
+type DeploySuperchainScript script.DeployScriptWithOutput[DeploySuperchain2Input, DeploySuperchain2Output]
+
+// NewDeploySuperchainScript loads and validates the DeploySuperchain2 script contract
+func NewDeploySuperchainScript(host *script.Host) (DeploySuperchainScript, error) {
+	return script.NewDeployScriptWithOutputFromFile[DeploySuperchain2Input, DeploySuperchain2Output](host, "DeploySuperchain2.s.sol", "DeploySuperchain2")
+}
+
+// DeploySuperchain2 exists for backwards compatibility and migration period
+//
+// Eventually the script loading and validation should occur before any scripts are run to avoid errors being thrown in the middle of the deployment
+func DeploySuperchain2(host *script.Host, input DeploySuperchain2Input) (output DeploySuperchain2Output, err error) {
+	script, err := NewDeploySuperchainScript(host)
+	if err != nil {
+		return output, err
+	}
+
+	return script.Run(input)
+}
---
+++ op-deployer/pkg/deployer/opcm/superchain2_test.go
@@ -0,0 +1,31 @@
+package opcm_test
+
+import (
+	"math/big"
+	"testing"
+
+	"github.com/ethereum-optimism/optimism/op-deployer/pkg/deployer/opcm"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/stretchr/testify/require"
+)
+
+func TestDeploySuperchain2(t *testing.T) {
+	t.Run("should not fail with current version of DeploySuperchain2 contract", func(t *testing.T) {
+		// First we grab a test host
+		host := createTestHost(t)
+
+		// Then we deploy
+		output, err := opcm.DeploySuperchain2(host, opcm.DeploySuperchain2Input{
+			Guardian:                   common.BigToAddress(big.NewInt(1)),
+			ProtocolVersionsOwner:      common.BigToAddress(big.NewInt(2)),
+			SuperchainProxyAdminOwner:  common.BigToAddress(big.NewInt(3)),
+			Paused:                     true,
+			RecommendedProtocolVersion: big.NewInt(2),
+			RequiredProtocolVersion:    big.NewInt(1),
+		})
+
+		// And do some simple asserts
+		require.NoError(t, err)
+		require.NotNil(t, output)
+	})
+}
