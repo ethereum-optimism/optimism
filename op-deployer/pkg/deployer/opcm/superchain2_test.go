e3bea2650a1b781085d0d6e84c4363474a1a0f0f
Jan Nanista <jan.jakub.nanista@gmail.com>
2025-03-31 19:07:27 -0700

    chore: Some more assertions

--- op-deployer/pkg/deployer/opcm/superchain2_test.go
+++ op-deployer/pkg/deployer/opcm/superchain2_test.go
@@ -28,5 +28,26 @@ func TestDeploySuperchain2(t *testing.T) {
 		// And do some simple asserts
 		require.NoError(t, err)
 		require.NotNil(t, output)
+
+		// Now we run the old deployer
+		deprecatedOutput, err := opcm.DeploySuperchain(host, opcm.DeploySuperchainInput{
+			Guardian:                   common.BigToAddress(big.NewInt(1)),
+			ProtocolVersionsOwner:      common.BigToAddress(big.NewInt(2)),
+			SuperchainProxyAdminOwner:  common.BigToAddress(big.NewInt(3)),
+			Paused:                     true,
+			RecommendedProtocolVersion: params.ProtocolVersion{1},
+			RequiredProtocolVersion:    params.ProtocolVersion{2},
+		})
+
+		// Make sure it succeeded
+		require.NoError(t, err)
+		require.NotNil(t, deprecatedOutput)
+
+		// And make sure that the basics match
+		require.Equal(t, deprecatedOutput.ProtocolVersionsImpl, output.ProtocolVersionsImpl)
+		require.Equal(t, deprecatedOutput.SuperchainConfigImpl, output.SuperchainConfigImpl)
+		require.Equal(t, host.GetCode(deprecatedOutput.SuperchainConfigProxy), host.GetCode(output.SuperchainConfigProxy))
+		require.Equal(t, host.GetCode(deprecatedOutput.ProtocolVersionsProxy), host.GetCode(output.ProtocolVersionsProxy))
+		require.Equal(t, host.GetCode(deprecatedOutput.SuperchainProxyAdmin), host.GetCode(output.SuperchainProxyAdmin))
 	})
 }
