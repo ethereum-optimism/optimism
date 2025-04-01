bd50e745cee26903f983c13c5493d4068f126279
Federico Gimenez <fgimenez@users.noreply.github.com>
2025-04-01 11:20:13 +0200

    chore: remove unused deps (paradigmxyz/reth#15429)

--- node/src/lib.rs
+++ node/src/lib.rs
@@ -9,6 +9,7 @@
     issue_tracker_base_url = "https://github.com/paradigmxyz/reth/issues/"
 )]
 #![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg))]
+#![cfg_attr(not(test), warn(unused_crate_dependencies))]
 
 /// CLI argument parsing for the optimism node.
 pub mod args;
