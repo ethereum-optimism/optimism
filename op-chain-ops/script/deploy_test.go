0a0aa8d10413359a788aafe64dd3a37db78151b1
Jan Nanista <jan.jakub.nanista@gmail.com>
2025-04-02 10:01:39 -0700

    chore: Typo

--- op-chain-ops/script/deploy_test.go
+++ op-chain-ops/script/deploy_test.go
@@ -398,7 +398,7 @@ func TestNewForgeScriptFromFile(t *testing.T) {
 		host := NewHost(logger, af, nil, DefaultContext)
 
 		// We'll use an example script that returns the input data, just mapped to a different struct
-		deploySuperchain, err := NewDeployScriptWithOutputFromFile[DeployScriptExampleInput, DeployScriptExampleOutput](host, "DeployScriptExample.s.sol", "DeployScriptExample")
+		deployExampleScript, err := NewDeployScriptWithOutputFromFile[DeployScriptExampleInput, DeployScriptExampleOutput](host, "DeployScriptExample.s.sol", "DeployScriptExample")
 		require.NoError(t, err)
 
 		// Put some input & expected output together
@@ -412,7 +412,7 @@ func TestNewForgeScriptFromFile(t *testing.T) {
 		}
 
 		// And make sure that we get what we would expect
-		output, err := deploySuperchain.Run(input)
+		output, err := deployExampleScript.Run(input)
 		require.NoError(t, err)
 		require.Equal(t, expectedOutput, output)
 
@@ -421,7 +421,7 @@ func TestNewForgeScriptFromFile(t *testing.T) {
 			InputFieldA: common.BigToAddress(big.NewInt(0)),
 			InputFieldB: common.BigToAddress(big.NewInt(0)),
 		}
-		_, err = deploySuperchain.Run(zeroInput)
+		_, err = deployExampleScript.Run(zeroInput)
 		require.ErrorContains(t, err, "failed to call script DeployScriptExample using data 0xfc61915400000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000: failed to call backend: execution reverted at")
 	})
 }
