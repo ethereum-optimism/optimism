271eec7eee522f572832fd8910bd5561042ee9a6
Matthew Slipper <me@matthewslipper.com>
2025-04-02 14:52:19 -0600

    fix test

--- op-deployer/pkg/deployer/pipeline/start_block.go
+++ op-deployer/pkg/deployer/pipeline/start_block.go
@@ -9,17 +9,7 @@ import (
 	"github.com/ethereum-optimism/optimism/op-deployer/pkg/deployer/state"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/hexutil"
-	"github.com/ethereum/go-ethereum/rpc"
 )
-
-func blockRefFromRpc(ctx context.Context, l1Client *rpc.Client, arg string) (*state.L1BlockRefJSON, error) {
-	var l1BRJ state.L1BlockRefJSON
-	if err := l1Client.CallContext(ctx, &l1BRJ, "eth_getBlockByHash", arg, false); err != nil {
-		return nil, fmt.Errorf("failed to get L1 block header for block: %w", err)
-	}
-
-	return &l1BRJ, nil
-}
 
 func SetStartBlockLiveStrategy(ctx context.Context, intent *state.Intent, env *Env, st *state.State, chainID common.Hash) error {
 	lgr := env.Logger.New("stage", "set-start-block", "strategy", "live")
@@ -35,17 +25,22 @@ func SetStartBlockLiveStrategy(ctx context.Context, intent *state.Intent, env *E
 		return fmt.Errorf("failed to get chain state: %w", err)
 	}
 
-	var headerArg string
-	if thisIntent.L1StartBlockHash != nil {
-		headerArg = thisIntent.L1StartBlockHash.Hex()
-	} else {
-		headerArg = "latest"
-	}
-	headerBlockRef, err := blockRefFromRpc(ctx, env.L1Client.Client(), headerArg)
-	if err != nil {
-		return fmt.Errorf("failed to get L1 block header: %w", err)
-	}
+	l1Client := env.L1Client.Client()
 
+	var headerBlockRef *state.L1BlockRefJSON
+	if thisIntent.L1StartBlockHash != nil {
+		var l1BRJ state.L1BlockRefJSON
+		if err := l1Client.CallContext(ctx, &l1BRJ, "eth_getBlockByHash", thisIntent.L1StartBlockHash.Hex(), false); err != nil {
+			return fmt.Errorf("failed to get L1 block header for block: %w", err)
+		}
+		headerBlockRef = &l1BRJ
+	} else {
+		var l1BRJ state.L1BlockRefJSON
+		if err := l1Client.CallContext(ctx, &l1BRJ, "eth_getBlockByNumber", "latest", false); err != nil {
+			return fmt.Errorf("failed to get L1 block header for block: %w", err)
+		}
+		headerBlockRef = &l1BRJ
+	}
 	thisChainState.StartBlock = headerBlockRef
 
 	return nil
