96c738667aec5da25dbf866c251fbea004539c42
George Knee <georgeknee@googlemail.com>
2025-04-04 11:29:46 +0100

    run FPP from genesis up to safe head in runBadTxInBatch_ResubmitBadFirstFrame_Test

--- op-e2e/actions/proofs/bad_tx_in_batch_test.go
+++ op-e2e/actions/proofs/bad_tx_in_batch_test.go
@@ -178,7 +178,7 @@ func runBadTxInBatch_ResubmitBadFirstFrame_Test(gt *testing.T, testCfg *helpers.
 	l2SafeHead = env.Engine.L2Chain().CurrentSafeBlock()
 	require.Equal(t, uint64(2), l2SafeHead.Number.Uint64())
 
-	env.RunFaultProofProgramFromGenesis(t, l2SafeHead.Number.Uint64()-1, testCfg.CheckResult, testCfg.InputParams...)
+	env.RunFaultProofProgramFromGenesis(t, l2SafeHead.Number.Uint64(), testCfg.CheckResult, testCfg.InputParams...)
 }
 
 func Test_ProgramAction_BadTxInBatch(gt *testing.T) {
