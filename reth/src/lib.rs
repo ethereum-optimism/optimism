2543c14333560b4edd39ca01041074f9ae8a1c9c
Ishika Choudhury <117741714+Rimeeeeee@users.noreply.github.com>
2025-04-05 21:12:10 +0530

    chore:added transaction-pool and reth-node-core for reth-ethereum and reth-op (paradigmxyz/reth#15559)
    Co-authored-by: Matthias Seitz <matthias.seitz@outlook.de>

--- reth/src/lib.rs
+++ reth/src/lib.rs
@@ -24,6 +24,10 @@ pub mod primitives {
 #[cfg(feature = "cli")]
 pub use reth_optimism_cli as cli;
 
+/// Re-exported pool types
+#[cfg(feature = "pool")]
+pub use reth_transaction_pool as pool;
+
 /// Re-exported consensus types
 #[cfg(feature = "consensus")]
 pub mod consensus {
@@ -88,6 +92,8 @@ pub mod storage {
 pub mod node {
     #[doc(inline)]
     pub use reth_node_api as api;
+    #[doc(inline)]
+    pub use reth_node_core as core;
     #[cfg(feature = "node")]
     pub use reth_optimism_node::*;
 }
