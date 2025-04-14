a7afede3c28640906cd83d46c38e0150570dada4
protolambda <proto@protolambda.com>
2025-04-14 22:18:51 +0200

    devnet-sdk/devstack/dsl: stronger warning on Escape func usage

--- devnet-sdk/devstack/dsl/doc.go
+++ devnet-sdk/devstack/dsl/doc.go
@@ -5,6 +5,7 @@ Each component in the devstack has a DSL wrapper.
 The wrapper itself does not have any state, and may be recreated or shallow-copied.
 
 Each DSL wrapper provides an Escape method, in case the DSL is not sufficient for a given use-case.
-The Escape method usage should be kept to a minimum, for readability and maintainability.
+The Escape method is a temporary compromise to allow more incremental development of and
+migration to the DSL. It should be avoided whenever possible and will be removed in the future.
 */
 package dsl
