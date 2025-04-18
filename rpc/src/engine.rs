a7924c00605d8612b45cde33e8ecfb3e3b9193db
Ishika Choudhury <117741714+Rimeeeeee@users.noreply.github.com>
2025-04-18 18:12:59 +0530

    chore: removed deprecated endpoint (paradigmxyz/reth#15804)

--- rpc/src/engine.rs
+++ rpc/src/engine.rs
@@ -28,7 +28,6 @@ pub const OP_ENGINE_CAPABILITIES: &[&str] = &[
     "engine_forkchoiceUpdatedV1",
     "engine_forkchoiceUpdatedV2",
     "engine_forkchoiceUpdatedV3",
-    "engine_exchangeTransitionConfigurationV1",
     "engine_getClientVersionV1",
     "engine_getPayloadV2",
     "engine_getPayloadV3",
