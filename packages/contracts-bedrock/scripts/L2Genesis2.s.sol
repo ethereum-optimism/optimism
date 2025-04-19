8d2dc6b031dc4301457f63e95a770cc4ca68f264
Matthew Slipper <me@matthewslipper.com>
2025-04-18 21:49:41 -0600

    remove methods

--- packages/contracts-bedrock/scripts/L2Genesis2.s.sol
+++ packages/contracts-bedrock/scripts/L2Genesis2.s.sol
@@ -231,7 +231,7 @@ contract L2Genesis is Script {
         if (_input.useInterop) {
             setCrossL2Inbox(); // 22
             setL2ToL2CrossDomainMessenger(); // 23
-            setSuperchainWETH(); // 24
+            setSuperchainETHBridge(); // 24
             setETHLiquidity(); // 25
             setSuperchainTokenBridge(); // 28
         }
@@ -514,27 +514,6 @@ contract L2Genesis is Script {
     function setETHLiquidity() internal {
         _setImplementationCode(Predeploys.ETH_LIQUIDITY);
         vm.deal(Predeploys.ETH_LIQUIDITY, type(uint248).max);
-    }
-
-    /// @notice This predeploy is following the safety invariant #1.
-    ///         This contract has no initializer.
-    function setSuperchainWETH() internal {
-        _setImplementationCode(Predeploys.SUPERCHAIN_WETH);
-    }
-
-    /// @notice This predeploy is following the safety invariant #1.
-    ///         This contract has no initializer.
-    function setOptimismSuperchainERC20Factory() internal {
-        _setImplementationCode(Predeploys.OPTIMISM_SUPERCHAIN_ERC20_FACTORY);
-    }
-
-    /// @notice This predeploy is following the safety invariant #1.
-    ///         This contract has no initializer.
-    function setOptimismSuperchainERC20Beacon() internal {
-        address superchainERC20Impl = Predeploys.OPTIMISM_SUPERCHAIN_ERC20;
-        vm.etch(superchainERC20Impl, vm.getDeployedCode("OptimismSuperchainERC20.sol:OptimismSuperchainERC20"));
-
-        _setImplementationCode(Predeploys.OPTIMISM_SUPERCHAIN_ERC20_BEACON);
     }
 
     /// @notice This predeploy is following the safety invariant #1.
