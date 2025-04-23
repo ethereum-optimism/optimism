7323d649c9dc03e1d38736d2f7a7e5ee72d8dd48
Jan Nanista <jan.jakub.nanista@gmail.com>
2025-04-23 08:49:57 -0700

    chore: Collect all deployment scripts under a struct

---
+++ op-deployer/pkg/deployer/opcm/scripts_test.go
@@ -0,0 +1,32 @@
+package opcm_test
+
+import (
+	"testing"
+
+	"github.com/ethereum-optimism/optimism/op-deployer/pkg/deployer/opcm"
+	"github.com/stretchr/testify/require"
+)
+
+func TestNewScripts(t *testing.T) {
+	t.Run("should not fail with current versions of script contracts", func(t *testing.T) {
+		// First we grab a test host
+		host := createTestHost(t)
+
+		// Then we load the scripts
+		//
+		// This would raise an error if the Go types didn't match the ABI
+		scripts, err := opcm.NewScripts(host)
+		require.NoError(t, err)
+
+		// And we just make sure we have all the scripts loaded
+		require.NotNil(t, scripts.DeployImplementations)
+		require.NotNil(t, scripts.DeploySuperchain)
+		require.NotNil(t, scripts.DeployAlphabetVM)
+		require.NotNil(t, scripts.DeployAltDA)
+		require.NotNil(t, scripts.DeployAsterisc)
+		require.NotNil(t, scripts.DeployDisputeGame)
+		require.NotNil(t, scripts.DeployMIPS)
+		require.NotNil(t, scripts.DeployPreimageOracle)
+		require.NotNil(t, scripts.DeployProxy)
+	})
+}
