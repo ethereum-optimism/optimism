126b314d187f93a09fc690048e71c6b533e71dd9
Adrian Sutton <adrian@oplabs.co>
2025-04-24 14:29:22 +1000

    op-program: Update compat tests.
    Introduce tests for granite, holocene and isthmus.
    Drop the mainnet-genesis test which covered just after the bedrock migration.

--- op-program/scripts/run-compat.sh
+++ op-program/scripts/run-compat.sh
@@ -5,7 +5,7 @@ SCRIPTS_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 COMPAT_DIR="${SCRIPTS_DIR}/../temp/compat"
 
 TESTNAME="${1?Must specify compat file to run}"
-BASEURL="${2:-https://github.com/ethereum-optimism/chain-test-data/releases/download/2024-09-01}"
+BASEURL="${2:-https://github.com/ethereum-optimism/chain-test-data/releases/download/2025-04-24}"
 
 URL="${BASEURL}/${TESTNAME}.tar.bz"
 
