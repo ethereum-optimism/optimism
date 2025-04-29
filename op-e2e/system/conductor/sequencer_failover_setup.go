a4385984071544c6f168c7e778d901336db943cc
George Knee <georgeknee@googlemail.com>
2025-04-29 22:52:18 +0100

    fix failover test setup

--- op-e2e/system/conductor/sequencer_failover_setup.go
+++ op-e2e/system/conductor/sequencer_failover_setup.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"strings"
 	"testing"
 	"time"
 
@@ -276,20 +275,20 @@ func setupBatcher(t *testing.T, sys *e2esys.System, conductors map[string]*condu
 	// enable active sequencer follow mode.
 	// in sequencer HA, all batcher / proposer requests will be proxied by conductor so that we can make sure
 	// that requests are always handled by leader.
-	l2EthRpc := strings.Join([]string{
+	l2EthRpc := []string{
 		conductors[Sequencer1Name].RPCEndpoint(),
 		conductors[Sequencer2Name].RPCEndpoint(),
 		conductors[Sequencer3Name].RPCEndpoint(),
-	}, ",")
-	rollupRpc := strings.Join([]string{
+	}
+	rollupRpc := []string{
 		conductors[Sequencer1Name].RPCEndpoint(),
 		conductors[Sequencer2Name].RPCEndpoint(),
 		conductors[Sequencer3Name].RPCEndpoint(),
-	}, ",")
+	}
 	batcherCLIConfig := &bss.CLIConfig{
 		L1EthRpc:               sys.EthInstances["l1"].UserRPC().RPC(),
-		L2EthRpc:               []string{l2EthRpc},
-		RollupRpc:              []string{rollupRpc},
+		L2EthRpc:               l2EthRpc,
+		RollupRpc:              rollupRpc,
 		MaxPendingTransactions: 0,
 		MaxChannelDuration:     1,
 		MaxL1TxSize:            120_000,
