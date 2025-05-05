24c0769daaedd15af0f77d5c4a1b3507dbeea3bf
Matthew Slipper <me@matthewslipper.com>
2025-05-04 19:49:04 -0600

    fix setup

--- packages/contracts-bedrock/test/setup/Setup.sol
+++ packages/contracts-bedrock/test/setup/Setup.sol
@@ -301,7 +301,7 @@ contract Setup {
                 l1CrossDomainMessengerProxy: payable(address(l1CrossDomainMessenger)),
                 l1StandardBridgeProxy: payable(address(l1StandardBridge)),
                 l1ERC721BridgeProxy: payable(address(l1ERC721Bridge)),
-                l2ProxyAdminOwner: deploy.cfg().proxyAdminOwner(),
+                opChainProxyAdminOwner: deploy.cfg().proxyAdminOwner(),
                 sequencerFeeVaultRecipient: deploy.cfg().sequencerFeeVaultRecipient(),
                 sequencerFeeVaultMinimumWithdrawalAmount: deploy.cfg().sequencerFeeVaultMinimumWithdrawalAmount(),
                 sequencerFeeVaultWithdrawalNetwork: deploy.cfg().sequencerFeeVaultWithdrawalNetwork(),
