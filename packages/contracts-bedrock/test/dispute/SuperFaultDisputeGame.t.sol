6b5943bb985c27ee5848d6476cc1974b67457277
Michael Amadi <amadimichaeld@gmail.com>
2025-05-06 15:07:32 +0100

    fix game types var used for tests

--- packages/contracts-bedrock/test/dispute/SuperFaultDisputeGame.t.sol
+++ packages/contracts-bedrock/test/dispute/SuperFaultDisputeGame.t.sol
@@ -32,7 +32,7 @@ import { IDelayedWETH } from "interfaces/dispute/IDelayedWETH.sol";
 
 contract SuperFaultDisputeGame_Init is DisputeGameFactory_Init {
     /// @dev The type of the game being tested.
-    GameType internal constant GAME_TYPE = GameType.wrap(4);
+    GameType internal constant GAME_TYPE = GameTypes.SUPER_FAULT_CANNON;
 
     /// @dev The implementation of the game.
     ISuperFaultDisputeGame internal gameImpl;
