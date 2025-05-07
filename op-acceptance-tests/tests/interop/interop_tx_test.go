007e5d420b62ad7eac88e64b8a756a2f87e148f8
Park Changwan <pcw109550@gmail.com>
2025-05-08 00:38:50 +0900

    op-acceptance-tests: interop: explicitly wait until l2 genesis pass

--- op-acceptance-tests/tests/interop/interop_tx_test.go
+++ op-acceptance-tests/tests/interop/interop_tx_test.go
@@ -4,6 +4,7 @@ import (
 	"encoding/hex"
 	"math/big"
 	"testing"
+	"time"
 
 	"github.com/ethereum-optimism/optimism/devnet-sdk/contracts/bindings"
 	"github.com/ethereum-optimism/optimism/devnet-sdk/contracts/constants"
@@ -30,6 +31,21 @@ func messagePassingScenario(sourceChainIdx, destChainIdx uint64, sourceWalletGet
 		chainB := sys.L2s()[destChainIdx]
 
 		logger.Info("chain info", "sourceChain", chainA.ID(), "destChain", chainB.ID())
+
+		logger.Info("wait until both chains pass genesis")
+		require.Eventually(t, func() bool {
+			elclientA, err := chainA.Nodes()[0].GethClient()
+			require.NoError(t, err)
+			elclientB, err := chainB.Nodes()[0].GethClient()
+			require.NoError(t, err)
+			chainAUnsafeHeadNum, err := elclientA.BlockNumber(ctx)
+			require.NoError(t, err)
+			chainBUnsafeHeadNum, err := elclientB.BlockNumber(ctx)
+			require.NoError(t, err)
+			check := chainAUnsafeHeadNum > 0 && chainBUnsafeHeadNum > 0
+			logger.Info("wait until both chains pass genesis", "check", check, "chainA", chainAUnsafeHeadNum, "chainB", chainBUnsafeHeadNum)
+			return check
+		}, 3*time.Minute, 2*time.Second)
 
 		// userA is funded at chainA and want to initialize message at chain A
 		userA := sourceWalletGetter(ctx)
