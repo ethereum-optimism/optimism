bca5aa09796c3efde3f3b74bf9b9b39ed03bb194
inphi <mlaw2501@gmail.com>
2025-05-07 15:21:17 -0400

    fix semgrep solidity lint

--- op-e2e/e2eutils/contracts/src/Invoker.sol
+++ op-e2e/e2eutils/contracts/src/Invoker.sol
@@ -3,12 +3,14 @@ pragma solidity ^0.8.15;
 
 contract Invoker {
     event PrecompileInvoked(address indexed precompile, bytes result);
+    error PrecompileCallFailed();
 
     function invokePrecompile(address _precompile, bytes memory _input) external {
         // Call the precompile contract with the provided input
         (bool success, bytes memory result) = _precompile.call(_input);
-        require(success, "precompile call failed");
-
+        if (!success) {
+            revert PrecompileCallFailed();
+        }
         emit PrecompileInvoked(_precompile, result);
     }
 }
