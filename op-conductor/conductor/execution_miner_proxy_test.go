8d585d2ef9eec7a5003d029b92fcdc23949cd7d6
George Knee <georgeknee@googlemail.com>
2025-05-09 10:47:08 +0100

    use op-service MockRPC to simplify test

--- op-conductor/conductor/execution_miner_proxy_test.go
+++ op-conductor/conductor/execution_miner_proxy_test.go
@@ -2,11 +2,8 @@ package conductor
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 	"log/slog"
-	"net/http"
-	"net/http/httptest"
 	"testing"
 
 	clientmocks "github.com/ethereum-optimism/optimism/op-conductor/client/mocks"
@@ -14,61 +11,10 @@ import (
 	healthmocks "github.com/ethereum-optimism/optimism/op-conductor/health/mocks"
 	"github.com/ethereum-optimism/optimism/op-conductor/metrics"
 	"github.com/ethereum-optimism/optimism/op-service/testlog"
+	"github.com/ethereum-optimism/optimism/op-service/testutils/mockrpc"
 	"github.com/ethereum/go-ethereum/rpc"
 	"github.com/stretchr/testify/require"
 )
-
-// createHTTPHandler creates a mock HTTP handler for testing. If alwaysFails is true,
-// the handler will return a JSON-RPC MethodNotFound error response for any vaild request.
-func createHTTPHandler(t *testing.T, alwaysFails bool) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == "POST" {
-			var req struct {
-				JSONRPC string        `json:"jsonrpc"`
-				Method  string        `json:"method"`
-				Params  []interface{} `json:"params"`
-				ID      interface{}   `json:"id"`
-			}
-			if err := json.NewDecoder(r.Body).Decode(&req); err == nil {
-
-				if alwaysFails {
-					w.Header().Set("Content-Type", "application/json")
-					errorResp := struct {
-						JSONRPC string      `json:"jsonrpc"`
-						ID      interface{} `json:"id"`
-						Error   struct {
-							Code    int    `json:"code"`
-							Message string `json:"message"`
-						} `json:"error"`
-					}{
-						JSONRPC: "2.0",
-						ID:      req.ID,
-						Error: struct {
-							Code    int    `json:"code"`
-							Message string `json:"message"`
-						}{
-							Code:    -32601, // Method not found error code
-							Message: "Method not found",
-						},
-					}
-					if err := json.NewEncoder(w).Encode(errorResp); err != nil {
-						t.Logf("Error writing response: %v", err)
-					}
-					return
-				}
-				if req.Method == "miner_setMaxDASize" && len(req.Params) == 2 {
-					w.Header().Set("Content-Type", "application/json")
-					_, err := w.Write([]byte(`{"jsonrpc":"2.0","id":1,"result":true}`))
-					if err != nil {
-						t.Logf("Error writing response: %v", err)
-					}
-					return
-				}
-			}
-		}
-		http.Error(w, "Unexpected request", http.StatusBadRequest)
-	}
-}
 
 // TestSetMaxDASize tests the SetMaxDASize method of the ExecutionMinerProxyBackend
 // It ensures that the proxy is transparently proxying the call to the execution engine
@@ -83,12 +29,17 @@ func TestSetMaxDASize(t *testing.T) {
 
 func testSetMaxDASize(t *testing.T, compliantSequencer bool) {
 	ctx := context.Background()
-	sequencer := httptest.NewServer(createHTTPHandler(t, !compliantSequencer))
-	defer sequencer.Close()
+	var expectationsFile string
+	if compliantSequencer {
+		expectationsFile = "testdata/compliant-sequencer.json"
+	} else {
+		expectationsFile = "testdata/non-compliant-sequencer.json"
+	}
+	sequencer := mockrpc.NewMockRPC(t, testlog.Logger(t, slog.LevelDebug), mockrpc.WithExpectationsFile(t, expectationsFile))
 
 	config := mockConfig(t)
-	config.ExecutionRPC = sequencer.URL
-	config.NodeRPC = sequencer.URL // this won't be used but needs to be set to get the conductor to init properly
+	config.ExecutionRPC = sequencer.Endpoint()
+	config.NodeRPC = sequencer.Endpoint() // this won't be used but needs to be set to get the conductor to init properly
 	config.RPCEnableProxy = true
 	config.RPC.ListenAddr = "localhost"
 	config.RPC.ListenPort = 0 // Let the system pick a random port, which we will inspect later
