581e9d77fb20a1fcb19afbddf42a272a094d0f36
Park Changwan <pcw109550@gmail.com>
2025-05-12 19:16:47 +0900

    Refactor using dsl

--- op-acceptance-tests/tests/interop/sync/interop_sync_test.go
+++ op-acceptance-tests/tests/interop/sync/interop_sync_test.go
@@ -2,11 +2,10 @@ package sync
 
 import (
 	"testing"
-	"time"
 
 	"github.com/ethereum-optimism/optimism/op-devstack/devtest"
+	"github.com/ethereum-optimism/optimism/op-devstack/dsl"
 	"github.com/ethereum-optimism/optimism/op-service/eth"
-	"github.com/stretchr/testify/require"
 )
 
 // TestL2CLResync checks that unsafe head advances after restarting L2CL.
@@ -16,47 +15,21 @@ func TestL2CLResync(gt *testing.T) {
 	sys := SimpleInterop(t)
 	logger := sys.Log.With("Test", "TestL2CLResync")
 
-	waitTime := time.Second * 3
-	logger.Info("wait until passing genesis")
-	var prevBlockA, prevBlockB eth.BlockRef
-	require.Eventually(t, func() bool {
-		blockA := sys.L2ELA.BlockRefByLabel(eth.Unsafe)
-		blockB := sys.L2ELB.BlockRefByLabel(eth.Unsafe)
-		logger.Info("chain A", "tip", blockA)
-		logger.Info("chain B", "tip", blockB)
-
-		prevBlockA, prevBlockB = blockA, blockB
-		return blockA.Number > 0 && blockB.Number > 0
-	}, 16*time.Second, waitTime)
-
-	time.Sleep(waitTime)
-
 	logger.Info("check unsafe chains are advancing")
-	require.Never(t, func() bool {
-		blockA := sys.L2ELA.BlockRefByLabel(eth.Unsafe)
-		blockB := sys.L2ELB.BlockRefByLabel(eth.Unsafe)
-		logger.Info("chain A", "tip", blockA)
-		logger.Info("chain B", "tip", blockB)
-
-		advanced := prevBlockA.Number < blockA.Number && prevBlockB.Number < blockB.Number
-		prevBlockA, prevBlockB = blockA, blockB
-		return !advanced
-	}, 10*time.Second, waitTime)
+	dsl.CheckAll(t,
+		sys.L2ELA.Advance(eth.Unsafe, 5),
+		sys.L2ELB.Advance(eth.Unsafe, 5),
+	)
 
 	logger.Info("stop L2CL nodes")
 	sys.L2CLA.Stop()
 	sys.L2CLB.Stop()
 
 	logger.Info("make sure L2ELs does not advance")
-	require.Eventually(t, func() bool {
-		blockA := sys.L2ELA.BlockRefByLabel(eth.Unsafe)
-		blockB := sys.L2ELB.BlockRefByLabel(eth.Unsafe)
-		logger.Info("chain A", "tip", blockA)
-		logger.Info("chain B", "tip", blockB)
-		isStatic := prevBlockA.Hash == blockA.Hash && prevBlockB.Hash == blockB.Hash
-		prevBlockA, prevBlockB = blockA, blockB
-		return isStatic
-	}, 10*time.Second, waitTime)
+	dsl.CheckAll(t,
+		sys.L2ELA.DoesNotAdvance(eth.Unsafe),
+		sys.L2ELB.DoesNotAdvance(eth.Unsafe),
+	)
 
 	logger.Info("restart L2CL nodes")
 	sys.L2CLA.Start()
@@ -65,27 +38,17 @@ func TestL2CLResync(gt *testing.T) {
 	// L2CL may advance a few blocks without supervisor connection, but eventually it will stop without the connection
 	// we must check that unsafe head is advancing due to reconnection
 	logger.Info("boot up L2CL nodes")
-	require.Eventually(t, func() bool {
-		blockA := sys.L2ELA.BlockRefByLabel(eth.Unsafe)
-		blockB := sys.L2ELB.BlockRefByLabel(eth.Unsafe)
-		logger.Info("chain A", "tip", blockA)
-		logger.Info("chain B", "tip", blockB)
-		advanced := prevBlockA.Number < blockA.Number && prevBlockB.Number < blockB.Number
-		prevBlockA, prevBlockB = blockA, blockB
-		return advanced
-	}, 30*time.Second, waitTime)
+	dsl.CheckAll(t,
+		sys.L2ELA.Advance(eth.Unsafe, 10),
+		sys.L2ELB.Advance(eth.Unsafe, 10),
+	)
 
 	// supervisor will attempt to reconnect with L2CLs at this point because L2CL ws endpoint is recovered
 	logger.Info("check unsafe chains are advancing again")
-	require.Never(t, func() bool {
-		blockA := sys.L2ELA.BlockRefByLabel(eth.Unsafe)
-		blockB := sys.L2ELB.BlockRefByLabel(eth.Unsafe)
-		logger.Info("chain A", "tip", blockA)
-		logger.Info("chain B", "tip", blockB)
-		advanced := prevBlockA.Number < blockA.Number && prevBlockB.Number < blockB.Number
-		prevBlockA, prevBlockB = blockA, blockB
-		return !advanced
-	}, 15*time.Second, waitTime*2)
+	dsl.CheckAll(t,
+		sys.L2ELA.Advance(eth.Unsafe, 10),
+		sys.L2ELB.Advance(eth.Unsafe, 10),
+	)
 
 	// supervisor successfully connected with managed L2CLs
 }
