9faee2ed87782eedac5adbbb1780df6bdd53c1d8
Park Changwan <pcw109550@gmail.com>
2025-05-13 22:30:49 +0900

    dsl can choose second l2cl and l2el

---
+++ op-devstack/stack/match/second.go
@@ -0,0 +1,6 @@
+package match
+
+import "github.com/ethereum-optimism/optimism/op-devstack/stack"
+
+var SecondL2EL = Second[stack.L2ELNodeID, stack.L2ELNode]()
+var SecondL2CL = Second[stack.L2CLNodeID, stack.L2CLNode]()
