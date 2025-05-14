c63713014bd22cd917557cf00515d7cc96d0c375
Kelvin Fichter <kelvinfichter@gmail.com>
2025-05-14 13:58:04 -0400

    fix: add warning comment to ProxyAdminOwnedBase

--- packages/contracts-bedrock/src/L1/ProxyAdminOwnedBase.sol
+++ packages/contracts-bedrock/src/L1/ProxyAdminOwnedBase.sol
@@ -13,6 +13,9 @@ import { IAddressManager } from "interfaces/legacy/IAddressManager.sol";
 ///         compatible Proxy contracts so that their ProxyAdmin and ProxyAdmin owner addresses can
 ///         be retrieved onchain. Existing Proxy contracts don't have these getters, so we need a
 ///         base contract instead.
+/// @dev WARNING: This contract is ONLY designed to be used with either the Optimism Proxy
+///      implementation or the Optimism ResolvedDelegateProxy implementation. It is not safe to use
+///      this contract with any other proxy implementation.
 abstract contract ProxyAdminOwnedBase {
     /// @notice Thrown when the ProxyAdmin owner of the current contract is not the same as the
     ///         ProxyAdmin owner of the other Proxy address provided.
