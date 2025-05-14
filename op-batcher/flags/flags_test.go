146f03d0cd1c7ad510b93d4a584fab6585736718
geoknee <georgeknee@googlemail.com>
2025-05-14 07:41:57 +0100

    add test

--- op-batcher/flags/flags_test.go
+++ op-batcher/flags/flags_test.go
@@ -97,3 +97,12 @@ func TestEnvVarFormat(t *testing.T) {
 		})
 	}
 }
+
+func TestThrottlingEndpoints(t *testing.T) {
+	for _, f := range Flags {
+		if f.Names()[0] == AdditionalThrottlingEndpointsFlag.Name {
+			return
+		}
+	}
+	t.Error("Throttling endpoints flag not found")
+}
