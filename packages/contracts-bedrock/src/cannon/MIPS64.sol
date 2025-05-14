f836464619b0ac777752118097e7093eb0c1e5ab
inphi <mlaw2501@gmail.com>
2025-05-14 17:33:30 -0400

    contracts: Prepare MIPS64 semver for release

--- packages/contracts-bedrock/src/cannon/MIPS64.sol
+++ packages/contracts-bedrock/src/cannon/MIPS64.sol
@@ -66,8 +66,8 @@ contract MIPS64 is ISemver {
     }
 
     /// @notice The semantic version of the MIPS64 contract.
-    /// @custom:semver 1.3.1
-    string public constant version = "1.3.1";
+    /// @custom:semver 1.4.0
+    string public constant version = "1.4.0";
 
     /// @notice The preimage oracle contract.
     IPreimageOracle internal immutable ORACLE;
