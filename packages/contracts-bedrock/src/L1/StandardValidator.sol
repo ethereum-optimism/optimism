6638e51f8cd29be3e886b805c6cb34ecb61f9eed
Maurelian <maurelian@protonmail.ch>
2025-05-15 14:01:44 -0400

    Remove @return comments, we don't use them in most other contracts

--- packages/contracts-bedrock/src/L1/StandardValidator.sol
+++ packages/contracts-bedrock/src/L1/StandardValidator.sol
@@ -143,7 +143,6 @@ contract StandardValidator {
 
     /// @notice Returns a string representing the overrides that are set.
     /// @param _overrides The overrides to check.
-    /// @return A string representing the overrides that are set.
     function getOverridesString(ValidationOverrides memory _overrides) private pure returns (string memory) {
         string memory overridesError;
 
@@ -161,7 +160,6 @@ contract StandardValidator {
 
     /// @notice Returns the expected L1 PAO multisig address.
     /// @param _overrides The overrides to check.
-    /// @return The expected L1 PAO multisig address.
     function expectedL1PAOMultisig(ValidationOverrides memory _overrides) internal view returns (address) {
         if (_overrides.l1PAOMultisig != address(0)) {
             return _overrides.l1PAOMultisig;
@@ -171,7 +169,6 @@ contract StandardValidator {
 
     /// @notice Returns the expected challenger address.
     /// @param _overrides The overrides to check.
-    /// @return The expected challenger address.
     function expectedChallenger(ValidationOverrides memory _overrides) internal view returns (address) {
         if (_overrides.challenger != address(0)) {
             return _overrides.challenger;
@@ -180,86 +177,72 @@ contract StandardValidator {
     }
 
     /// @notice Returns the expected SystemConfig version.
-    /// @return The expected SystemConfig version.
     function systemConfigVersion() public pure returns (string memory) {
         return "3.3.0";
     }
 
     /// @notice Returns the expected OptimismPortal version.
-    /// @return The expected OptimismPortal version.
     function optimismPortalVersion() public pure returns (string memory) {
         return "4.6.0";
     }
 
     /// @notice Returns the expected L1CrossDomainMessenger version.
-    /// @return The expected L1CrossDomainMessenger version.
     function l1CrossDomainMessengerVersion() public pure returns (string memory) {
         return "2.9.0";
     }
 
     /// @notice Returns the expected L1ERC721Bridge version.
-    /// @return The expected L1ERC721Bridge version.
     function l1ERC721BridgeVersion() public pure returns (string memory) {
         return "2.7.0";
     }
 
     /// @notice Returns the expected L1StandardBridge version.
-    /// @return The expected L1StandardBridge version.
     function l1StandardBridgeVersion() public pure returns (string memory) {
         return "2.6.0";
     }
 
     /// @notice Returns the expected MIPS version.
-    /// @return The expected MIPS version.
     function mipsVersion() public pure returns (string memory) {
         return "1.3.0";
     }
 
     /// @notice Returns the expected OptimismMintableERC20Factory version.
-    /// @return The expected OptimismMintableERC20Factory version.
     function optimismMintableERC20FactoryVersion() public pure returns (string memory) {
         return "1.10.1";
     }
 
     /// @notice Returns the expected DisputeGameFactory version.
-    /// @return The expected DisputeGameFactory version.
     function disputeGameFactoryVersion() public pure returns (string memory) {
         return "1.2.0";
     }
 
     /// @notice Returns the expected AnchorStateRegistry version.
-    /// @return The expected AnchorStateRegistry version.
     function anchorStateRegistryVersion() public pure returns (string memory) {
         return "3.5.0";
     }
 
     /// @notice Returns the expected DelayedWETH version.
-    /// @return The expected DelayedWETH version.
     function delayedWETHVersion() public pure returns (string memory) {
         return "1.5.0";
     }
 
     /// @notice Returns the expected PermissionedDisputeGame version.
-    /// @return The expected PermissionedDisputeGame version.
     function permissionedDisputeGameVersion() public pure returns (string memory) {
         return "1.7.0";
     }
 
     /// @notice Returns the expected PreimageOracle version.
-    /// @return The expected PreimageOracle version.
     function preimageOracleVersion() public pure returns (string memory) {
         return "1.1.4";
     }
 
     /// @notice Returns the expected ETHLockbox version.
-    /// @return The expected ETHLockbox version.
     function ethLockboxVersion() public pure returns (string memory) {
         return "1.2.0";
     }
 
     /// @notice Asserts that the SuperchainConfig contract is valid.
     /// @param _errors The current error string.
-    /// @return The updated error string.
     function assertValidSuperchainConfig(string memory _errors) internal view returns (string memory) {
         _errors = internalRequire(!superchainConfig.paused(address(0)), "SPRCFG-10", _errors);
         return _errors;
@@ -269,7 +252,6 @@ contract StandardValidator {
     /// @param _errors The current error string.
     /// @param _admin The ProxyAdmin contract.
     /// @param _overrides The validation overrides.
-    /// @return The updated error string.
     function assertValidProxyAdmin(
         string memory _errors,
         IProxyAdmin _admin,
@@ -288,7 +270,6 @@ contract StandardValidator {
     /// @param _errors The current error string.
     /// @param _sysCfg The SystemConfig contract.
     /// @param _admin The ProxyAdmin contract.
-    /// @return The updated error string.
     function assertValidSystemConfig(
         string memory _errors,
         ISystemConfig _sysCfg,
@@ -324,7 +305,6 @@ contract StandardValidator {
     /// @param _errors The current error string.
     /// @param _sysCfg The SystemConfig contract.
     /// @param _admin The ProxyAdmin contract.
-    /// @return The updated error string.
     function assertValidL1CrossDomainMessenger(
         string memory _errors,
         ISystemConfig _sysCfg,
@@ -360,7 +340,6 @@ contract StandardValidator {
     /// @param _errors The current error string.
     /// @param _sysCfg The SystemConfig contract.
     /// @param _admin The ProxyAdmin contract.
-    /// @return The updated error string.
     function assertValidL1StandardBridge(
         string memory _errors,
         ISystemConfig _sysCfg,
@@ -390,7 +369,6 @@ contract StandardValidator {
     /// @param _errors The current error string.
     /// @param _sysCfg The SystemConfig contract.
     /// @param _admin The ProxyAdmin contract.
-    /// @return The updated error string.
     function assertValidOptimismMintableERC20Factory(
         string memory _errors,
         ISystemConfig _sysCfg,
@@ -418,7 +396,6 @@ contract StandardValidator {
     /// @param _errors The current error string.
     /// @param _sysCfg The SystemConfig contract.
     /// @param _admin The ProxyAdmin contract.
-    /// @return The updated error string.
     function assertValidL1ERC721Bridge(
         string memory _errors,
         ISystemConfig _sysCfg,
@@ -447,7 +424,6 @@ contract StandardValidator {
     /// @param _errors The current error string.
     /// @param _sysCfg The SystemConfig contract.
     /// @param _admin The ProxyAdmin contract.
-    /// @return The updated error string.
     function assertValidOptimismPortal(
         string memory _errors,
         ISystemConfig _sysCfg,
@@ -474,7 +450,6 @@ contract StandardValidator {
     /// @param _errors The current error string.
     /// @param _sysCfg The SystemConfig contract.
     /// @param _admin The ProxyAdmin contract.
-    /// @return The updated error string.
     function assertValidETHLockbox(
         string memory _errors,
         ISystemConfig _sysCfg,
@@ -501,7 +476,6 @@ contract StandardValidator {
     /// @param _sysCfg The SystemConfig contract.
     /// @param _admin The ProxyAdmin contract.
     /// @param _overrides The validation overrides.
-    /// @return The updated error string.
     function assertValidDisputeGameFactory(
         string memory _errors,
         ISystemConfig _sysCfg,
@@ -530,7 +504,6 @@ contract StandardValidator {
     /// @param _l2ChainID The L2 chain ID.
     /// @param _admin The ProxyAdmin contract.
     /// @param _overrides The validation overrides.
-    /// @return The updated error string.
     function assertValidPermissionedDisputeGame(
         string memory _errors,
         ISystemConfig _sysCfg,
@@ -589,7 +562,6 @@ contract StandardValidator {
     /// @param _l2ChainID The L2 chain ID.
     /// @param _admin The ProxyAdmin contract.
     /// @param _overrides The validation overrides.
-    /// @return The updated error string.
     function assertValidPermissionlessDisputeGame(
         string memory _errors,
         ISystemConfig _sysCfg,
@@ -645,7 +617,6 @@ contract StandardValidator {
     /// @param _gameType The type of the dispute game.
     /// @param _overrides The validation overrides.
     /// @param _errorPrefix The prefix for error messages.
-    /// @return The updated error string.
     function assertValidDisputeGame(
         string memory _errors,
         ISystemConfig _sysCfg,
@@ -705,7 +676,6 @@ contract StandardValidator {
     /// @param _admin The ProxyAdmin contract.
     /// @param _overrides The validation overrides.
     /// @param _errorPrefix The prefix for error messages.
-    /// @return The updated error string.
     function assertValidDelayedWETH(
         string memory _errors,
         ISystemConfig _sysCfg,
@@ -743,7 +713,6 @@ contract StandardValidator {
     /// @param _asr The AnchorStateRegistry contract.
     /// @param _admin The ProxyAdmin contract.
     /// @param _errorPrefix The prefix for error messages.
-    /// @return The updated error string.
     function assertValidAnchorStateRegistry(
         string memory _errors,
         ISystemConfig _sysCfg,
@@ -779,7 +748,6 @@ contract StandardValidator {
     /// @param _errors The current error string.
     /// @param _oracle The PreimageOracle contract.
     /// @param _errorPrefix The prefix for error messages.
-    /// @return The updated error string.
     function assertValidPreimageOracle(
         string memory _errors,
         IPreimageOracle _oracle,
@@ -803,7 +771,6 @@ contract StandardValidator {
     /// @param _condition The condition to check.
     /// @param _message The error message to append if the condition is false.
     /// @param _errors The current error string.
-    /// @return The updated error string.
     function internalRequire(
         bool _condition,
         string memory _message,
@@ -827,7 +794,6 @@ contract StandardValidator {
     /// @notice Compares two strings and returns true if they are equal.
     /// @param _a The first string.
     /// @param _b The second string.
-    /// @return True if the strings are equal, false otherwise.
     function stringEq(string memory _a, string memory _b) internal pure returns (bool) {
         return keccak256(bytes(_a)) == keccak256(bytes(_b));
     }
@@ -835,7 +801,6 @@ contract StandardValidator {
     /// @notice Validates the configuration of the L1 contracts.
     /// @param _input The input parameters for the validation process.
     /// @param _allowFailure Whether to allow the validation to fail without reverting.
-    /// @return A string containing any errors found during validation.
     function validate(ValidationInput memory _input, bool _allowFailure) external view returns (string memory) {
         return
             validate(_input, _allowFailure, ValidationOverrides({ l1PAOMultisig: address(0), challenger: address(0) }));
@@ -845,7 +810,6 @@ contract StandardValidator {
     /// @param _input The input parameters for the validation process.
     /// @param _allowFailure Whether to allow the validation to fail without reverting.
     /// @param _overrides The validation overrides.
-    /// @return A string containing any errors found during validation.
     function validate(
         ValidationInput memory _input,
         bool _allowFailure,
