359a3206a1d9611b80cf93fe928e32c576be1f5d
Shourya Chaudhry <149008800+18aaddy@users.noreply.github.com>
2025-05-18 16:21:23 +0530

    feat: implement IsTyped2781 for reth types (paradigmxyz/reth#16325)
    Co-authored-by: Matthias Seitz <matthias.seitz@outlook.de>

--- primitives/src/receipt.rs
+++ primitives/src/receipt.rs
@@ -2,7 +2,10 @@ use alloy_consensus::{
     Eip2718EncodableReceipt, Eip658Value, Receipt, ReceiptWithBloom, RlpDecodableReceipt,
     RlpEncodableReceipt, TxReceipt, Typed2718,
 };
-use alloy_eips::{eip2718::Eip2718Result, Decodable2718, Encodable2718};
+use alloy_eips::{
+    eip2718::{Eip2718Result, IsTyped2718},
+    Decodable2718, Encodable2718,
+};
 use alloy_primitives::{Bloom, Log};
 use alloy_rlp::{BufMut, Decodable, Encodable, Header};
 use op_alloy_consensus::{OpDepositReceipt, OpTxType};
@@ -359,6 +362,12 @@ impl TxReceipt for OpReceipt {
 impl Typed2718 for OpReceipt {
     fn ty(&self) -> u8 {
         self.tx_type().into()
+    }
+}
+
+impl IsTyped2718 for OpReceipt {
+    fn is_type(type_id: u8) -> bool {
+        <OpTxType as IsTyped2718>::is_type(type_id)
     }
 }
 
