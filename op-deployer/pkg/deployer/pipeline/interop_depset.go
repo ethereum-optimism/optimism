ebcf4c97e2596f8d787628d7d936fe50887a8f63
Adrian Sutton <adrian@oplabs.co>
2025-05-23 07:34:19 +1000

    op-deployer: Always generate a dependency set

--- op-deployer/pkg/deployer/pipeline/interop_depset.go
+++ op-deployer/pkg/deployer/pipeline/interop_depset.go
@@ -13,26 +13,13 @@ import (
 func GenerateInteropDepset(_ context.Context, pEnv *Env, globalIntent *state.Intent, st *state.State) error {
 	lgr := pEnv.Logger.New("stage", "generate-interop-depset")
 
-	lgr.Info("creating interop dependency set...")
+	lgr.Info("Creating interop dependency set...")
 	deps := make(map[eth.ChainID]*depset.StaticConfigDependency)
 	for i, chain := range globalIntent.Chains {
 		id := eth.ChainIDFromBytes32(chain.ID)
-		_, config, err := calculateL2GenesisOverrides(globalIntent, chain)
-		if err != nil {
-			return fmt.Errorf("failed to calculate L2 genesis overrides for chain %v: %w", chain.ID, err)
-		}
-		if config.L2GenesisInteropTimeOffset == nil {
-			// Don't add chains to the dep set if they don't have interop scheduled.
-			continue
-		}
-
 		deps[id] = &depset.StaticConfigDependency{ChainIndex: types.ChainIndex(i)}
 	}
 
-	if len(deps) == 0 {
-		lgr.Info("No interop chains found, skipping dependency set generation")
-		return nil
-	}
 	interopDepSet, err := depset.NewStaticConfigDependencySet(deps)
 	if err != nil {
 		return fmt.Errorf("failed to create interop dependency set: %w", err)
