639744b937d7082bdb92224d6062ef1b4dd71a30
Ferran Borreguero <ferran.borreguero@gmail.com>
2025-05-22 14:34:51 +0100

    Update contender args on CI (flashbots/rollup-boost#247)
    * Update contender
    
    * Update again
    
    * Update

--- scripts/ci/stress.sh
+++ scripts/ci/stress.sh
@@ -58,7 +58,7 @@ run() {
     contender setup -p $PREFUNDED_PRIV_KEY "/tmp/scenario.toml" -r $ROLLUP_BOOST_SOCKET --optimism
 
     # Run the scenario on the builder
-    contender run -p $PREFUNDED_PRIV_KEY fill-block -r $OP_RETH_BUILDER_SOCKET --optimism
+    contender spam --tps 50 -p $PREFUNDED_PRIV_KEY -r $OP_RETH_BUILDER_SOCKET --optimism fill-block
 }
 
 clean() {
