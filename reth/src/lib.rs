d15e754007f516b32286bd8345bd9a53b2c1ec70
Veer Chaurasia <142890355+VeerChaurasia@users.noreply.github.com>
2025-05-24 14:05:40 +0530

    refactor: remove reth dependencies and instead use reth_ethereum (paradigmxyz/reth#16416)
    Co-authored-by: Matthias Seitz <matthias.seitz@outlook.de>

--- reth/src/lib.rs
+++ reth/src/lib.rs
@@ -63,6 +63,12 @@ pub mod evm {
     pub use reth_revm as revm;
 }
 
+/// Re-exported from `tasks`.
+#[cfg(feature = "tasks")]
+pub mod tasks {
+    pub use reth_tasks::*;
+}
+
 /// Re-exported reth network types
 #[cfg(feature = "network")]
 pub mod network {
