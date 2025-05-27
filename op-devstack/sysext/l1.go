c0f6366b5bb3c8acd48ae6416dff3ab23231bb78
serpixel <5087962+serpixel@users.noreply.github.com>
2025-05-27 20:01:05 +0200

    fix(op-acceptance-tests): formatting comments
    Co-authored-by: protolambda <proto@protolambda.com>

--- op-devstack/sysext/l1.go
+++ op-devstack/sysext/l1.go
@@ -29,12 +29,12 @@ func (o *Orchestrator) hydrateL1(system stack.ExtensibleSystem) {
 	for idx, node := range env.Env.L1.Nodes {
 		elService, ok := node.Services[ELServiceName]
 
-		// TODO: This is a hack to force direct connection to the L1 node. Tracked in #16127.
+		// TODO(#16127): This is a hack to force direct connection to the L1 node.
 		// Only enable this for kurtosis devnet.
 		if strings.HasPrefix(os.Getenv("DEVNET_ENV_URL"), "kt://") {
 			elService.Endpoints[RPCProtocol].ReverseProxyHeader = map[string][]string{}
 		}
-		// TODO: Remove this once we have a proper reverse proxy.
+		// TODO(#16127): Remove this once we have a proper reverse proxy.
 
 		require.True(ok, "need L1 EL service %d", idx)
 
