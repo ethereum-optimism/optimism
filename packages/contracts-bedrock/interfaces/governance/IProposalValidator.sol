ec4369c4714de27fa127f9d148ae7e6e5d17c6b2
Chiin <77933451+0xChin@users.noreply.github.com>
2025-05-28 11:57:38 -0300

    feat: admin functions (#393)
    * feat: add upgradeability to ProposalValidator contract
    
    * chore: fix styling
    
    * feat: add admin functions and tests
    
    * chore: more descriptive variables naming
    
    * test: expect event emissions
    
    * test: use fuzzing for setter functions
    
    * chore: run pre-pr
    
    * docs: use correct natspec in ProosalValidator contract
    
    * feat: add semver
    
    * feat: add reinitializable base
    
    * chore: run pre-pr
    
    * chore: run pre-pr
    
    ---------
    
    Signed-off-by: Chiin <77933451+0xChin@users.noreply.github.com>

--- packages/contracts-bedrock/interfaces/governance/IProposalValidator.sol
+++ packages/contracts-bedrock/interfaces/governance/IProposalValidator.sol
@@ -15,6 +15,7 @@ interface IProposalValidator is ISemver {
     error ProposalValidator_InsufficientVotingPower();
     error ProposalValidator_InvalidAttestation();
     error ProposalValidator_ProposalDoesNotExist();
+    error ProposalValidator_VotingCycleAlreadySet();
     error ReinitializableBase_ZeroInitVersion();
 
     struct ProposalData {
@@ -61,8 +62,15 @@ interface IProposalValidator is ISemver {
 
     event DistributionThresholdSet(uint256 newDistributionThreshold);
 
-    event ProposalApprovalThresholdSet(ProposalType proposalType, uint256 newApprovalThreshold);
-
+    event ProposalTypeApprovalThresholdSet(ProposalType proposalType, uint256 newApprovalThreshold);
+    
+    event VotingCycleDataSet(
+        uint256 cycleNumber, 
+        uint256 startBlock, 
+        uint256 duration, 
+        uint256 votingCycleDistributionLimit
+    );
+    
     event Initialized(uint8 version);
 
     function submitProposal(
@@ -87,8 +95,27 @@ interface IProposalValidator is ISemver {
     function setMinimumVotingPower(uint256 _minimumVotingPower) external;
 
     function setDistributionThreshold(uint256 _distributionThreshold) external;
-
-    function setProposalRequiredApprovals(ProposalType _proposalType, uint256 _requiredApprovals) external;
+    
+    function setProposalTypeApprovalThreshold(ProposalType _proposalType, uint256 _requiredApprovals) external;
+    
+    function setVotingCycleData(
+        uint256 _cycleNumber,
+        uint256 _startBlock,
+        uint256 _duration,
+        uint256 _votingCycleDistributionLimit
+    ) external;
+    
+    function initialize(
+        address _owner,
+        uint256 _minimumVotingPower,
+        uint256 _cycleNumber,
+        uint256 _startBlock,
+        uint256 _duration,
+        uint256 _votingCycleDistributionLimit,
+        uint256 _distributionThreshold,
+        ProposalType[] memory _proposalTypes,
+        uint256[] memory _requiredApprovals
+    ) external;
     
     function renounceOwnership() external;
     
@@ -109,14 +136,14 @@ interface IProposalValidator is ISemver {
     function initVersion() external view returns (uint8);
 
     function ATTESTATION_SCHEMA_UID() external view returns (bytes32);
-
-    function initialize(
-        address _owner,
-        uint256 _minimumVotingPower,
-        uint256 _distributionThreshold,
-        IProposalValidator.ProposalType[] memory _proposalTypes,
-        uint256[] memory _requiredApprovals
-    ) external;
+    
+    function proposalRequiredApprovals(ProposalType) external view returns (uint256);
+    
+    function votingCycles(uint256) external view returns (
+        uint256 startingBlock, 
+        uint256 duration, 
+        uint256 votingCycleDistributionLimit
+    );
 
     function __constructor__(
         bytes32 _attestationSchemaUid,
