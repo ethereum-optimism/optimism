cec5fdbb80607e53ba49456b6e9b41716392bfe2
Haardik <hhaardik@uwaterloo.ca>
2025-05-28 17:36:36 +0530

    feat(optimism): add metrics to miner to track max DA size throttle values (paradigmxyz/reth#16514)
    Co-authored-by: Matthias Seitz <matthias.seitz@outlook.de>

--- rpc/src/miner.rs
+++ rpc/src/miner.rs
@@ -3,6 +3,7 @@
 use alloy_primitives::U64;
 use jsonrpsee_core::{async_trait, RpcResult};
 pub use op_alloy_rpc_jsonrpsee::traits::MinerApiExtServer;
+use reth_metrics::{metrics::Gauge, Metrics};
 use reth_optimism_payload_builder::config::OpDAConfig;
 use tracing::debug;
 
@@ -11,13 +12,14 @@ use tracing::debug;
 #[derive(Debug, Clone)]
 pub struct OpMinerExtApi {
     da_config: OpDAConfig,
+    metrics: OpMinerMetrics,
 }
 
 impl OpMinerExtApi {
     /// Instantiate the miner API extension with the given, sharable data availability
     /// configuration.
-    pub const fn new(da_config: OpDAConfig) -> Self {
-        Self { da_config }
+    pub fn new(da_config: OpDAConfig) -> Self {
+        Self { da_config, metrics: OpMinerMetrics::default() }
     }
 }
 
@@ -27,6 +29,34 @@ impl MinerApiExtServer for OpMinerExtApi {
     async fn set_max_da_size(&self, max_tx_size: U64, max_block_size: U64) -> RpcResult<bool> {
         debug!(target: "rpc", "Setting max DA size: tx={}, block={}", max_tx_size, max_block_size);
         self.da_config.set_max_da_size(max_tx_size.to(), max_block_size.to());
+
+        self.metrics.set_max_da_tx_size(max_tx_size.to());
+        self.metrics.set_max_da_block_size(max_block_size.to());
+
         Ok(true)
+    }
+}
+
+/// Optimism miner metrics
+#[derive(Metrics, Clone)]
+#[metrics(scope = "optimism_rpc.miner")]
+pub struct OpMinerMetrics {
+    /// Max DA tx size set on the miner
+    max_da_tx_size: Gauge,
+    /// Max DA block size set on the miner
+    max_da_block_size: Gauge,
+}
+
+impl OpMinerMetrics {
+    /// Sets the max DA tx size gauge value
+    #[inline]
+    pub fn set_max_da_tx_size(&self, size: u64) {
+        self.max_da_tx_size.set(size as f64);
+    }
+
+    /// Sets the max DA block size gauge value
+    #[inline]
+    pub fn set_max_da_block_size(&self, size: u64) {
+        self.max_da_block_size.set(size as f64);
     }
 }
