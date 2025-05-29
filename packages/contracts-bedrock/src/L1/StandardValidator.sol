566845404ed72168e5bff2c956be2c0654b2c0e9
Maurelian <maurelian@protonmail.ch>
2025-05-29 15:09:40 -0400

    Add ISemver to StandardValidator

--- packages/contracts-bedrock/src/L1/StandardValidator.sol
+++ packages/contracts-bedrock/src/L1/StandardValidator.sol
@@ -33,7 +33,12 @@ import { IMIPS64 } from "interfaces/cannon/IMIPS64.sol";
 /// It is a stateless contract that can be used to ensure that the L1 contracts are configured correctly.
 /// It is intended to be used by the L1 PAO multisig to validate the configuration of the L1 contracts
 /// before and after an upgrade.
-contract StandardValidator {
+contract StandardValidator is ISemver {
+
+    /// @notice The semantic version of the StandardValidator contract.
+    /// @custom:semver 1.0.0
+    string public constant version = "1.0.0";
+
     /// @notice The SuperchainConfig contract.
     ISuperchainConfig public superchainConfig;
 
