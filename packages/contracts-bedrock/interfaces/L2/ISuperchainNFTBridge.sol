f4c203cab65a9657b3bfe9dec23b9c9df7e983af
niha <205694301+0xniha@users.noreply.github.com>
2025-06-06 15:12:58 -0300

    feat: add superchain nft bridge interface

---
+++ packages/contracts-bedrock/interfaces/L2/ISuperchainNFTBridge.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import { ISemver } from "interfaces/universal/ISemver.sol";
+
+/// @title ISuperchainNFTBridge
+/// @notice Interface for the SuperchainNFTBridge contract.
+interface ISuperchainNFTBridge is ISemver {
+    error ZeroAddress();
+    error Unauthorized();
+    error InvalidCrossDomainSender();
+    error InvalidCrosschainERC721();
+
+    event SendERC721(
+        address indexed token, address indexed from, address indexed to, uint256 tokenId, uint256 destination
+    );
+
+    event RelayERC721(address indexed token, address indexed from, address indexed to, uint256 tokenId, uint256 source);
+
+    function sendERC721(
+        address _token,
+        address _to,
+        uint256 _tokenId,
+        uint256 _chainId
+    )
+        external
+        returns (bytes32 msgHash_);
+
+    function relayERC721(address _token, address _from, address _to, uint256 _tokenId) external;
+
+    function __constructor__() external;
+}
