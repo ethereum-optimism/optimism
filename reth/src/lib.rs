b9915be648ab76ffdee5c94a1a3bf998493a72fa
Matthias Seitz <matthias.seitz@outlook.de>
2025-06-07 09:25:44 +0200

    chore: re-export cli-util crate (paradigmxyz/reth#16711)

--- reth/src/lib.rs
+++ reth/src/lib.rs
@@ -22,7 +22,12 @@ pub mod primitives {
 
 /// Re-exported cli types
 #[cfg(feature = "cli")]
-pub use reth_optimism_cli as cli;
+pub mod cli {
+    #[doc(inline)]
+    pub use reth_cli_util::*;
+    #[doc(inline)]
+    pub use reth_optimism_cli::*;
+}
 
 /// Re-exported pool types
 #[cfg(feature = "pool")]
