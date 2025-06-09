8583ebf7851964a69abe3f400e01db301b4b9c28
steven <steven.nevins.js@gmail.com>
2025-06-09 15:37:32 -0400

    feat: add implArgs to migrate function

--- packages/contracts-bedrock/src/L1/OPContractsManager.sol
+++ packages/contracts-bedrock/src/L1/OPContractsManager.sol
@@ -1602,9 +1602,10 @@ contract OPContractsManagerInteropMigrator is OPContractsManagerBase {
                 )
             );
 
+            bytes memory implArgs = abi.encodePacked(_input.opChainConfigs[0].absolutePrestate,_input.opChainConfigs[0].absolutePrestate , newAnchorStateRegistry);
             // Register the new SuperPermissionedDisputeGame.
             newDisputeGameFactory.setImplementation(
-                GameTypes.SUPER_PERMISSIONED_CANNON, IDisputeGame(address(newSuperPDG)), "" // TODO: snevins - validate correct parameters later
+                GameTypes.SUPER_PERMISSIONED_CANNON, IDisputeGame(address(newSuperPDG)), implArgs // TODO: snevins - validate correct parameters later
             );
             newDisputeGameFactory.setInitBond(GameTypes.SUPER_PERMISSIONED_CANNON, _input.gameParameters.initBond);
         }
@@ -1651,8 +1652,9 @@ contract OPContractsManagerInteropMigrator is OPContractsManagerBase {
                 )
             );
 
+            bytes memory implArgs = abi.encodePacked(_input.opChainConfigs[0].absolutePrestate,_input.opChainConfigs[0].absolutePrestate , newAnchorStateRegistry);
             // Register the new SuperFaultDisputeGame.
-            newDisputeGameFactory.setImplementation(GameTypes.SUPER_CANNON, IDisputeGame(address(newSuperFDG)), ""); // TODO: snevins - validate correct parameters later
+            newDisputeGameFactory.setImplementation(GameTypes.SUPER_CANNON, IDisputeGame(address(newSuperFDG)), implArgs); // TODO: snevins - validate correct parameters later
             newDisputeGameFactory.setInitBond(GameTypes.SUPER_CANNON, _input.gameParameters.initBond);
         }
     }
