e5c77c6da56b0e17191f93f29a4cbd118596b68d
Adrian Sutton <adrian@oplabs.co>
2025-06-10 10:02:08 +1000

    contracts: Update expected mipsVersion

--- packages/contracts-bedrock/src/L1/StandardValidator.sol
+++ packages/contracts-bedrock/src/L1/StandardValidator.sol
@@ -200,7 +200,7 @@ contract StandardValidator is ISemver {
 
     /// @notice Returns the expected MIPS version.
     function mipsVersion() public pure returns (string memory) {
-        return "1.5.0";
+        return "1.6.0";
     }
 
     /// @notice Returns the expected OptimismMintableERC20Factory version.
