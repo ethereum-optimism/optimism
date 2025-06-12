2ad7ba75da32342152110726c6309a90fa9c9a25
Katie Clay <katie@platonic.io>
2025-06-12 09:52:46 -0600

    Added new boba branding card

--- boba-docs/docusaurus.config.js
+++ boba-docs/docusaurus.config.js
@@ -78,7 +78,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/boba_B.ico',
+      image: 'img/boba-brandmark-light.png',
       navbar: {
         title: 'Boba Developer Docs',
         logo: {
