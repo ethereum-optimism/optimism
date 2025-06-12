14e7c53000e6d04feab9777497b59298875e473d
Katie Clay <katie@platonic.io>
2025-06-12 12:26:50 -0600

    Added a resized image

--- boba-docs/docusaurus.config.js
+++ boba-docs/docusaurus.config.js
@@ -78,7 +78,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/boba_B.png',
+      image: 'img/boba_preview.png',
       navbar: {
         title: 'Boba Developer Docs',
         logo: {
