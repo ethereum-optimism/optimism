6e2a05f3c98747c86f0e91a4169875db8019cac2
Matthias Seitz <matthias.seitz@outlook.de>
2025-06-16 16:18:58 +0200

    chore: update op-alloy deps to 0.18.2 (paradigmxyz/reth#16827)

--- primitives/src/transaction/mod.rs
+++ primitives/src/transaction/mod.rs
@@ -6,34 +6,7 @@ mod tx_type;
 #[cfg(test)]
 mod signed;
 
-pub use op_alloy_consensus::{OpTxType, OpTypedTransaction};
-use reth_primitives_traits::Extended;
+pub use op_alloy_consensus::{OpTransaction, OpTxType, OpTypedTransaction};
 
 /// Signed transaction.
 pub type OpTransactionSigned = op_alloy_consensus::OpTxEnvelope;
-
-/// A trait that represents an optimism transaction, mainly used to indicate whether or not the
-/// transaction is a deposit transaction.
-pub trait OpTransaction {
-    /// Whether or not the transaction is a dpeosit transaction.
-    fn is_deposit(&self) -> bool;
-}
-
-impl OpTransaction for op_alloy_consensus::OpTxEnvelope {
-    fn is_deposit(&self) -> bool {
-        Self::is_deposit(self)
-    }
-}
-
-impl<B, T> OpTransaction for Extended<B, T>
-where
-    B: OpTransaction,
-    T: OpTransaction,
-{
-    fn is_deposit(&self) -> bool {
-        match self {
-            Self::BuiltIn(b) => b.is_deposit(),
-            Self::Other(t) => t.is_deposit(),
-        }
-    }
-}
