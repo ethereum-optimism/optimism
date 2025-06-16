cc38223866736133ba3d27b09f1126a98d7269c0
Jan Nanista <jan.jakub.nanista@gmail.com>
2025-06-15 21:14:13 -0700

    fix: Protocols

--- op-devstack/sysext/l2.go
+++ op-devstack/sysext/l2.go
@@ -97,7 +97,7 @@ func (o *Orchestrator) hydrateL2ELCL(node *descriptors.Node, l2Net stack.Extensi
 	require.True(ok, "need L2 CL service for chain", l2ID)
 
 	// it's an RPC, but 'http' in kurtosis descriptor
-	clClient := o.rpcClient(l2Net.T(), clService, HTTPProtocol, "/")
+	clClient := o.rpcClient(l2Net.T(), clService, RPCProtocol, "/")
 	l2CL := shim.NewL2CLNode(shim.L2CLNodeConfig{
 		ID:           stack.NewL2CLNodeID(clService.Name, l2ID.ChainID()),
 		CommonConfig: shim.NewCommonConfig(l2Net.T()),
