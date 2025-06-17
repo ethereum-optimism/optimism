8ad10a75e1a203af78f47f107bc486836babe0a7
Katie Clay <katie@platonic.io>
2025-06-17 12:56:30 -0600

    Trying a jpg

--- boba-docs/docusaurus.config.js
+++ boba-docs/docusaurus.config.js
@@ -78,7 +78,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/b_preview.png', 
+      image: 'img/boba_preview.jpg', 
       navbar: {
         title: 'Boba Developer Docs',
         logo: {
