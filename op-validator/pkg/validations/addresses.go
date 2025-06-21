Necter-Network/necter-chain

op-deployer: Add support for upgrading and validating v4.0.0 (#16048)
* op-deployer: Add support for upgrading to v4.0.0

* simplify versions

* rebase, add validator v4 support

* update address test

* lint

op-validator/pkg/validations/addresses.go
@@ -3,36 +3,31 @@ package validations
 import (
 	"fmt"
 
-	"github.com/ethereum/go-ethereum/common"
-)
+	"github.com/ethereum-optimism/optimism/op-deployer/pkg/deployer/standard"
 
-const (
-	VersionV180 = "v1.8.0"
-	VersionV200 = "v2.0.0"
-	VersionV300 = "v3.0.0"
-	VersionV400 = "v4.0.0"
+	"github.com/ethereum/go-ethereum/common"
 )
 
 var addresses = map[uint64]map[string]common.Address{
 	1: {
 		// Bootstrapped on 03/07/2025 using OP Deployer.
-		VersionV180: common.HexToAddress("0x37fb5b21750d0e08a992350574bd1c24f4bcedf9"),
+		standard.ContractsV180Tag: common.HexToAddress("0x37fb5b21750d0e08a992350574bd1c24f4bcedf9"),
 		// Bootstrapped on 03/07/2025 using OP Deployer.
-		VersionV200: common.HexToAddress("0x12a9e38628e5a5b24d18b1956ed68a24fe4e3dc0"),
+		standard.ContractsV200Tag: common.HexToAddress("0x12a9e38628e5a5b24d18b1956ed68a24fe4e3dc0"),
 		// Bootstrapped on 04/16/2025 using OP Deployer.
-		VersionV300: common.HexToAddress("0xf989Df70FB46c581ba6157Ab335c0833bA60e1f0"),
+		standard.ContractsV300Tag: common.HexToAddress("0xf989Df70FB46c581ba6157Ab335c0833bA60e1f0"),
 		// Bootstrapped on 06/03/2025 using OP Deployer.
-		VersionV400: common.HexToAddress("0x3dfc5e44043DC5998928E0b8280136b7352d3F70"),
+		standard.ContractsV400Tag: common.HexToAddress("0x3dfc5e44043DC5998928E0b8280136b7352d3F70"),
 	},
 	11155111: {
 		// Bootstrapped on 03/02/2025 using OP Deployer.
-		VersionV180: common.HexToAddress("0x0a5bf8ebb4b177b2dcc6eba933db726a2e2e2b4d"),
+		standard.ContractsV180Tag: common.HexToAddress("0x0a5bf8ebb4b177b2dcc6eba933db726a2e2e2b4d"),
 		// Bootstrapped on 03/02/2025 using OP Deployer.
-		VersionV200: common.HexToAddress("0x37739a6b0a3f1e7429499a4ec4a0685439daff5c"),
+		standard.ContractsV200Tag: common.HexToAddress("0x37739a6b0a3f1e7429499a4ec4a0685439daff5c"),
 		// Bootstrapped on 04/03/2025 using OP Deployer.
-		VersionV300: common.HexToAddress("0x2d56022cb84ce6b961c3b4288ca36386bcd9024c"),
+		standard.ContractsV300Tag: common.HexToAddress("0x2d56022cb84ce6b961c3b4288ca36386bcd9024c"),
 		// Bootstrapped on 06/03/2025 using OP Deployer.
-		VersionV400: common.HexToAddress("0xA8a1529547306FEC7A32a001705160f2110451aE"),
+		standard.ContractsV400Tag: common.HexToAddress("0xA8a1529547306FEC7A32a001705160f2110451aE"),
 	},
 }
 

ethereum-optimism/optimism

op-validator/pkg/validations/addresses.go
@@ -3,36 +3,31 @@ package validations
 import (
 	"fmt"
 
-	"github.com/ethereum/go-ethereum/common"
-)
+	"github.com/ethereum-optimism/optimism/op-deployer/pkg/deployer/standard"
 
-const (
-	VersionV180 = "v1.8.0"
-	VersionV200 = "v2.0.0"
-	VersionV300 = "v3.0.0"
-	VersionV400 = "v4.0.0"
+	"github.com/ethereum/go-ethereum/common"
 )
 
 var addresses = map[uint64]map[string]common.Address{
 	1: {
 		// Bootstrapped on 03/07/2025 using OP Deployer.
-		VersionV180: common.HexToAddress("0x37fb5b21750d0e08a992350574bd1c24f4bcedf9"),
+		standard.ContractsV180Tag: common.HexToAddress("0x37fb5b21750d0e08a992350574bd1c24f4bcedf9"),
 		// Bootstrapped on 03/07/2025 using OP Deployer.
-		VersionV200: common.HexToAddress("0x12a9e38628e5a5b24d18b1956ed68a24fe4e3dc0"),
+		standard.ContractsV200Tag: common.HexToAddress("0x12a9e38628e5a5b24d18b1956ed68a24fe4e3dc0"),
 		// Bootstrapped on 04/16/2025 using OP Deployer.
-		VersionV300: common.HexToAddress("0xf989Df70FB46c581ba6157Ab335c0833bA60e1f0"),
+		standard.ContractsV300Tag: common.HexToAddress("0xf989Df70FB46c581ba6157Ab335c0833bA60e1f0"),
 		// Bootstrapped on 06/19/2025 using OP Deployer.
-		VersionV400: common.HexToAddress("0xbb43313d206a9b02032c749ca0828a07c962b4b5"),
+		standard.ContractsV400Tag: common.HexToAddress("0xbb43313d206a9b02032c749ca0828a07c962b4b5"),
 	},
 	11155111: {
 		// Bootstrapped on 03/02/2025 using OP Deployer.
-		VersionV180: common.HexToAddress("0x0a5bf8ebb4b177b2dcc6eba933db726a2e2e2b4d"),
+		standard.ContractsV180Tag: common.HexToAddress("0x0a5bf8ebb4b177b2dcc6eba933db726a2e2e2b4d"),
 		// Bootstrapped on 03/02/2025 using OP Deployer.
-		VersionV200: common.HexToAddress("0x37739a6b0a3f1e7429499a4ec4a0685439daff5c"),
+		standard.ContractsV200Tag: common.HexToAddress("0x37739a6b0a3f1e7429499a4ec4a0685439daff5c"),
 		// Bootstrapped on 04/03/2025 using OP Deployer.
-		VersionV300: common.HexToAddress("0x2d56022cb84ce6b961c3b4288ca36386bcd9024c"),
+		standard.ContractsV300Tag: common.HexToAddress("0x2d56022cb84ce6b961c3b4288ca36386bcd9024c"),
 		// Bootstrapped on 06/19/2025 using OP Deployer.
-		VersionV400: common.HexToAddress("0xaaabe70a4198ab9e99e1a22b1afa0a43cc7f2c79"),
+		standard.ContractsV400Tag: common.HexToAddress("0xaaabe70a4198ab9e99e1a22b1afa0a43cc7f2c79"),
 	},
 }
 