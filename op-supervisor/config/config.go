6d29d8d317fbc918932d3a99971f93d2ae5e1b96
axelKingsley <axel.kingsley@gmail.com>
2025-06-27 09:08:20 -0500

    unit test fix

--- op-supervisor/config/config.go
+++ op-supervisor/config/config.go
@@ -75,14 +75,16 @@ func (c *Config) Check() error {
 // Required options with no suitable default are passed as parameters.
 func NewConfig(l1RPC string, syncSrcs syncnode.SyncNodeCollection, fullCfgSet depset.FullConfigSetSource, datadir string) *Config {
 	return &Config{
-		LogConfig:           oplog.DefaultCLIConfig(),
-		MetricsConfig:       opmetrics.DefaultCLIConfig(),
-		PprofConfig:         oppprof.DefaultCLIConfig(),
-		RPC:                 oprpc.DefaultCLIConfig(),
-		FullConfigSetSource: fullCfgSet,
-		MockRun:             false,
-		L1RPC:               l1RPC,
-		SyncSources:         syncSrcs,
-		Datadir:             datadir,
+		LogConfig:              oplog.DefaultCLIConfig(),
+		MetricsConfig:          opmetrics.DefaultCLIConfig(),
+		PprofConfig:            oppprof.DefaultCLIConfig(),
+		RPC:                    oprpc.DefaultCLIConfig(),
+		FullConfigSetSource:    fullCfgSet,
+		MockRun:                false,
+		L1RPC:                  l1RPC,
+		SyncSources:            syncSrcs,
+		Datadir:                datadir,
+		FailsafeEnabled:        false,
+		FailsafeOnInvalidation: true,
 	}
 }
