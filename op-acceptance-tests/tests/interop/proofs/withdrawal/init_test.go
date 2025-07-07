54dcc7d4abcdd22b3e7e107c7fdca7a473546017
Adrian Sutton <adrian@oplabs.co>
2025-07-07 14:12:12 +1000

    op-acceptance: Move withdrawal test to own package.
    Time travelling L1 a week into the future makes the L2 lag behind for a long time after the test completes. That causes unexpected flakiness.
    Ultimately time travel tests need to have a disposable system.

--- op-acceptance-tests/tests/interop/proofs/withdrawal/init_test.go
+++ op-acceptance-tests/tests/interop/proofs/withdrawal/init_test.go
@@ -1,4 +1,4 @@
-package proofs
+package withdrawal
 
 import (
 	"testing"
@@ -7,5 +7,5 @@ import (
 )
 
 func TestMain(m *testing.M) {
-	presets.DoMain(m, presets.WithSuperInterop())
+	presets.DoMain(m, presets.WithSuperInterop(), presets.WithTimeTravel())
 }
