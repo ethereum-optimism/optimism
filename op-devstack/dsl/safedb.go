0df9aae6caacf5188c71176fd7295fbd8909fc41
Adrian Sutton <adrian@oplabs.co>
2025-07-10 07:59:40 +1000

    Remove printf

--- op-devstack/dsl/safedb.go
+++ op-devstack/dsl/safedb.go
@@ -1,8 +1,7 @@
 package dsl
 
 import (
-	"fmt"
-
+	"github.com/ethereum-optimism/optimism/op-devstack/devtest"
 	"github.com/ethereum-optimism/optimism/op-service/eth"
 	"github.com/ethereum-optimism/optimism/op-service/testreq"
 )
@@ -11,7 +10,7 @@ type safeHeadDBProvider interface {
 	safeHeadAtL1Block(l1BlockNum uint64) *eth.SafeHeadResponse
 }
 
-func checkSafeHeadConsistent(t testreq.TestingT, maxL1BlockNum uint64, checkNode, sourceOfTruth safeHeadDBProvider, minRequiredL2Block *uint64) {
+func checkSafeHeadConsistent(t devtest.T, maxL1BlockNum uint64, checkNode, sourceOfTruth safeHeadDBProvider, minRequiredL2Block *uint64) {
 	require := testreq.New(t)
 	l1BlockNum := maxL1BlockNum
 	var minL2BlockRecorded *uint64
@@ -21,7 +20,6 @@ func checkSafeHeadConsistent(t testreq.TestingT, maxL1BlockNum uint64, checkNode
 			// No further safe head data available
 			// Stop iterating as long as we found _some_ data
 			require.NotNil(minL2BlockRecorded, "no safe head data available at L1 block %v", l1BlockNum)
-			fmt.Printf("Require min block %v and got back to %v\n", minRequiredL2Block, minL2BlockRecorded)
 			if minRequiredL2Block != nil {
 				// Ensure we had data back at least as far as minRequiredL2Block
 				require.LessOrEqual(*minL2BlockRecorded, *minRequiredL2Block, "safe head db did not go back far enough")
