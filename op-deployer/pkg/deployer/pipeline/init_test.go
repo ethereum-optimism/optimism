34bd5ce3e845234bbed8220cb0b07a8deaca31c4
Michael Amadi <amadimichaeld@gmail.com>
2025-07-09 21:33:54 +0100

    fixes

--- op-deployer/pkg/deployer/pipeline/init_test.go
+++ op-deployer/pkg/deployer/pipeline/init_test.go
@@ -241,7 +241,7 @@ func TestPopulateSuperchainState(t *testing.T) {
 	require.Equal(t, addresses.SuperchainContracts{
 		SuperchainProxyAdminImpl: common.HexToAddress("0x189aBAAaa82DfC015A588A7dbaD6F13b1D3485Bc"),
 		SuperchainConfigProxy:    superchain.SuperchainConfigAddr,
-		SuperchainConfigImpl:     common.HexToAddress("0xce28685eb204186b557133766eca00334eb441e4"),
+		SuperchainConfigImpl:     common.HexToAddress("0x4da82a327773965b8d4D85Fa3dB8249b387458E7"),
 		ProtocolVersionsProxy:    superchain.ProtocolVersionsAddr,
 		ProtocolVersionsImpl:     common.HexToAddress("0x37E15e4d6DFFa9e5E320Ee1eC036922E563CB76C"),
 	}, *dep)
