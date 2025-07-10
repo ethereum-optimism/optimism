3f301b9b055300052192eb1a021d51cc65e681fd
Park Changwan <pcw109550@gmail.com>
2025-07-10 22:56:43 +0900

    linter

--- op-devstack/sysgo/sync_tester.go
+++ op-devstack/sysgo/sync_tester.go
@@ -2,11 +2,9 @@ package sysgo
 
 import (
 	"github.com/ethereum-optimism/optimism/op-devstack/stack"
-	"github.com/ethereum-optimism/optimism/op-sync-tester/synctester"
 )
 
 type SyncTesterService struct {
-	service *synctester.Service
 }
 
 func (s *SyncTesterService) hydrate(system stack.ExtensibleSystem) {
