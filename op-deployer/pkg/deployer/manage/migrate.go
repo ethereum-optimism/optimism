c8f5ef68d20e9e7c4e8c2460f3ecd60fb914ce73
Jan Nanista <jan.jakub.nanista@gmail.com>
2025-07-15 11:03:47 -0700

    chore: Move interop migration under manage package

---
+++ op-deployer/pkg/deployer/manage/migrate.go
@@ -0,0 +1,153 @@
+package manage
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"math/big"
+	"os"
+
+	"github.com/ethereum-optimism/optimism/op-chain-ops/script"
+	"github.com/ethereum-optimism/optimism/op-deployer/pkg/deployer"
+	"github.com/ethereum-optimism/optimism/op-deployer/pkg/deployer/artifacts"
+	"github.com/ethereum-optimism/optimism/op-deployer/pkg/deployer/broadcaster"
+	"github.com/ethereum-optimism/optimism/op-deployer/pkg/deployer/opcm"
+	"github.com/ethereum-optimism/optimism/op-deployer/pkg/env"
+	oplog "github.com/ethereum-optimism/optimism/op-service/log"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/rpc"
+	"github.com/lmittmann/w3"
+	"github.com/urfave/cli/v2"
+)
+
+type InteropMigrationInput struct {
+	Prank common.Address `json:"prank"`
+	Opcm  common.Address `json:"opcm"`
+
+	UsePermissionlessGame          bool           `json:"usePermissionlessGame"`
+	StartingAnchorRoot             common.Hash    `json:"startingAnchorRoot"`
+	StartingAnchorL2SequenceNumber *big.Int       `json:"startingAnchorL2SequenceNumber"`
+	Proposer                       common.Address `json:"proposer"`
+	Challenger                     common.Address `json:"challenger"`
+	MaxGameDepth                   uint64         `json:"maxGameDepth"`
+	SplitDepth                     uint64         `json:"splitDepth"`
+	InitBond                       *big.Int       `json:"initBond"`
+	ClockExtension                 uint64         `json:"clockExtension"`
+	MaxClockDuration               uint64         `json:"maxClockDuration"`
+
+	EncodedChainConfigs []OPChainConfig `evm:"-" json:"chainConfigs"`
+}
+
+func (u *InteropMigrationInput) OpChainConfigs() ([]byte, error) {
+	data, err := opChainConfigEncoder.EncodeArgs(u.EncodedChainConfigs)
+	if err != nil {
+		return nil, fmt.Errorf("failed to encode chain configs: %w", err)
+	}
+	return data[4:], nil
+}
+
+type OPChainConfig struct {
+	SystemConfigProxy common.Address `json:"systemConfigProxy"`
+	ProxyAdmin        common.Address `json:"proxyAdmin"`
+	AbsolutePrestate  common.Hash    `json:"absolutePrestate"`
+}
+
+type InteropMigrationOutput struct {
+	DisputeGameFactory common.Address `json:"disputeGameFactory"`
+}
+
+func (output *InteropMigrationOutput) CheckOutput(input common.Address) error {
+	return nil
+}
+
+var opChainConfigEncoder = w3.MustNewFunc("dummy((address systemConfigProxy,address proxyAdmin,bytes32 absolutePrestate)[])", "")
+
+type InteropMigration struct {
+	Run func(input common.Address)
+}
+
+func Migrate(host *script.Host, input InteropMigrationInput) (InteropMigrationOutput, error) {
+	return opcm.RunScriptSingle[InteropMigrationInput, InteropMigrationOutput](host, input, "InteropMigration.s.sol", "InteropMigration")
+}
+
+func MigrateCLI(cliCtx *cli.Context) error {
+	logCfg := oplog.ReadCLIConfig(cliCtx)
+	lgr := oplog.NewLogger(oplog.AppOut(cliCtx), logCfg)
+	oplog.SetGlobalLogHandler(lgr.Handler())
+
+	ctx, cancel := context.WithCancel(cliCtx.Context)
+	defer cancel()
+
+	cacheDir := cliCtx.String(deployer.CacheDirFlag.Name)
+
+	l1RPCUrl := cliCtx.String(deployer.L1RPCURLFlag.Name)
+	if l1RPCUrl == "" {
+		return fmt.Errorf("missing required flag: %s", deployer.L1RPCURLFlag.Name)
+	}
+
+	input := InteropMigrationInput{
+		Prank:                          common.HexToAddress(cliCtx.String(PrankFlag.Name)),
+		Opcm:                           common.HexToAddress(cliCtx.String(OPCMImplFlag.Name)),
+		UsePermissionlessGame:          cliCtx.Bool(PermissionlessFlag.Name),
+		StartingAnchorRoot:             common.HexToHash(cliCtx.String(StartingAnchorRootFlag.Name)),
+		StartingAnchorL2SequenceNumber: new(big.Int).SetUint64(cliCtx.Uint64(StartingAnchorL2SequenceNumberFlag.Name)),
+		Proposer:                       common.HexToAddress(cliCtx.String(ProposerFlag.Name)),
+		Challenger:                     common.HexToAddress(cliCtx.String(ChallengerFlag.Name)),
+		MaxGameDepth:                   cliCtx.Uint64(DisputeMaxGameDepthFlag.Name),
+		SplitDepth:                     cliCtx.Uint64(DisputeSplitDepthFlag.Name),
+		InitBond:                       big.NewInt(int64(cliCtx.Uint64(InitialBondFlag.Name))),
+		ClockExtension:                 cliCtx.Uint64(DisputeClockExtensionFlag.Name),
+		MaxClockDuration:               cliCtx.Uint64(DisputeMaxClockDurationFlag.Name),
+		// At the moment we only support a single chain config
+		EncodedChainConfigs: []OPChainConfig{
+			{
+				SystemConfigProxy: common.HexToAddress(cliCtx.String(SystemConfigProxyFlag.Name)),
+				ProxyAdmin:        common.HexToAddress(cliCtx.String(OPChainProxyAdminFlag.Name)),
+				AbsolutePrestate:  common.HexToHash(cliCtx.String(DisputeAbsolutePrestateFlag.Name)),
+			},
+		},
+	}
+
+	artifactsLocatorStr := cliCtx.String(deployer.ArtifactsLocatorFlag.Name)
+	artifactsLocator := new(artifacts.Locator)
+	if err := artifactsLocator.UnmarshalText([]byte(artifactsLocatorStr)); err != nil {
+		return fmt.Errorf("failed to parse artifacts locator: %w", err)
+	}
+
+	artifactsFS, err := artifacts.Download(ctx, artifactsLocator, artifacts.BarProgressor(), cacheDir)
+	if err != nil {
+		return fmt.Errorf("failed to download artifacts: %w", err)
+	}
+
+	bcaster := new(broadcaster.CalldataBroadcaster)
+
+	l1RPC, err := rpc.Dial(l1RPCUrl)
+	if err != nil {
+		return fmt.Errorf("failed to dial RPC %s: %w", l1RPCUrl, err)
+	}
+
+	l1Host, err := env.DefaultForkedScriptHost(
+		ctx,
+		bcaster,
+		lgr,
+		common.Address{'D'},
+		artifactsFS,
+		l1RPC,
+	)
+	if err != nil {
+		return fmt.Errorf("failed to create script host: %w", err)
+	}
+
+	output, err := Migrate(l1Host, input)
+	if err != nil {
+		return fmt.Errorf("failed to run interop migration: %w", err)
+	}
+
+	enc := json.NewEncoder(os.Stdout)
+	enc.SetIndent("", "  ")
+	if err := enc.Encode(output); err != nil {
+		return fmt.Errorf("failed to encode interop migration output: %w", err)
+	}
+
+	return nil
+}
