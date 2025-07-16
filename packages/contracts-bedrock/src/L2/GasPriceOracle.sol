ccde0d02639b70d49fa697b82f203fedbde8d76e
geoknee <georgeknee@googlemail.com>
2025-07-16 11:42:09 +0100

    fix!(contracts): multiply operatorFeeScalar by 1e3 instead of dividing by 1e6
    This is a consensus breaking change.

--- packages/contracts-bedrock/src/L2/GasPriceOracle.sol
+++ packages/contracts-bedrock/src/L2/GasPriceOracle.sol
@@ -30,8 +30,8 @@ contract GasPriceOracle is ISemver {
     uint256 public constant DECIMALS = 6;
 
     /// @notice Semantic version.
-    /// @custom:semver 1.4.0
-    string public constant version = "1.4.0";
+    /// @custom:semver 1.4.1
+    string public constant version = "1.4.1";
 
     /// @notice This is the intercept value for the linear regression used to estimate the final size of the
     ///         compressed transaction.
@@ -200,7 +200,7 @@ contract GasPriceOracle is ISemver {
         }
 
         return Arithmetic.saturatingAdd(
-            Arithmetic.saturatingMul(_gasUsed, IL1Block(Predeploys.L1_BLOCK_ATTRIBUTES).operatorFeeScalar()) / 1e6,
+            Arithmetic.saturatingMul(_gasUsed, IL1Block(Predeploys.L1_BLOCK_ATTRIBUTES).operatorFeeScalar()) * 1e3,
             IL1Block(Predeploys.L1_BLOCK_ATTRIBUTES).operatorFeeConstant()
         );
     }
