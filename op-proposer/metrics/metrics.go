468eabc6b244192f12030dba3e929abf489e1f36
jtriley <jtriley15@gmail.com>
2025-07-29 08:51:32 -0700

    add metrics

--- op-proposer/metrics/metrics.go
+++ op-proposer/metrics/metrics.go
@@ -16,9 +16,10 @@ import (
 
 const Namespace = "op_proposer"
 
-// implements the Registry getter, for metrics HTTP server to hook into
+// implements the Registry getter, for metrics HTTP server to hook into.
 var _ opmetrics.RegistryMetricer = (*Metrics)(nil)
 
+// Metricer is the interface that wraps the basic methods for recording metrics.
 type Metricer interface {
 	RecordInfo(version string)
 	RecordUp()
@@ -37,6 +38,7 @@ type Metricer interface {
 	RecordL2BlocksProposed(l2ref eth.L2BlockRef)
 }
 
+// Metrics implements the Metricer interface.
 type Metrics struct {
 	ns       string
 	registry *prometheus.Registry
@@ -54,6 +56,7 @@ type Metrics struct {
 
 var _ Metricer = (*Metrics)(nil)
 
+// NewMetrics constructs a new Metrics instance.
 func NewMetrics(procName string) *Metrics {
 	if procName == "" {
 		procName = "default"
@@ -92,21 +95,22 @@ func NewMetrics(procName string) *Metrics {
 	}
 }
 
+// Registry returns the prometheus registry.
 func (m *Metrics) Registry() *prometheus.Registry {
 	return m.registry
 }
 
+// StartBalanceMetrics starts the balance metrics.
 func (m *Metrics) StartBalanceMetrics(l log.Logger, client *ethclient.Client, account common.Address) io.Closer {
 	return opmetrics.LaunchBalanceMetrics(l, m.registry, m.ns, client, account)
 }
 
-// RecordInfo sets a pseudo-metric that contains versioning and
-// config info for the op-proposer.
+// RecordInfo sets a pseudo-metric that contains versioning and config info for the op-proposer.
 func (m *Metrics) RecordInfo(version string) {
 	m.info.WithLabelValues(version).Set(1)
 }
 
-// RecordUp sets the up metric to 1.
+// RecordUp sets the `up` metric to 1.
 func (m *Metrics) RecordUp() {
 	m.up.Set(1)
 }
@@ -115,15 +119,17 @@ const (
 	BlockProposed = "proposed"
 )
 
-// RecordL2BlocksProposed should be called when new L2 block is proposed
+// RecordL2BlocksProposed records when new L2 block is proposed.
 func (m *Metrics) RecordL2BlocksProposed(l2ref eth.L2BlockRef) {
 	m.RecordL2Ref(BlockProposed, l2ref)
 }
 
+// RecordL2Proposal records when a new L2 proposal is created.
 func (m *Metrics) RecordL2Proposal(seqNum uint64) {
 	m.proposalSequenceNum.Set(float64(seqNum))
 }
 
+// Document returns the documented metrics.
 func (m *Metrics) Document() []opmetrics.DocumentedMetric {
 	return m.factory.Document()
 }
