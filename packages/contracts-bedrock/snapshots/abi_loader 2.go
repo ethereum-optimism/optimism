717ceca8f3b8c8e39e57553c7f896b976e6f3c2a
steven <steven.nevins.js@gmail.com>
2025-07-31 15:30:53 -0400

    chore: run pre pr snapshots and rename interface to fix ci

---
+++ packages/contracts-bedrock/snapshots/abi_loader 2.go
@@ -0,0 +1,67 @@
+package snapshots
+
+import (
+	"bytes"
+	_ "embed"
+
+	"github.com/ethereum/go-ethereum/accounts/abi"
+)
+
+//go:embed abi/DisputeGameFactory.json
+var disputeGameFactory []byte
+
+//go:embed abi/SuperFaultDisputeGame.json
+var superFaultDisputeGame []byte
+
+//go:embed abi/FaultDisputeGame.json
+var faultDisputeGame []byte
+
+//go:embed abi/PreimageOracle.json
+var preimageOracle []byte
+
+//go:embed abi/MIPS64.json
+var mips []byte
+
+//go:embed abi/DelayedWETH.json
+var delayedWETH []byte
+
+//go:embed abi/SystemConfig.json
+var systemConfig []byte
+
+//go:embed abi/CrossL2Inbox.json
+var crossL2Inbox []byte
+
+func LoadDisputeGameFactoryABI() *abi.ABI {
+	return loadABI(disputeGameFactory)
+}
+func LoadSuperFaultDisputeGameABI() *abi.ABI {
+	return loadABI(superFaultDisputeGame)
+}
+func LoadFaultDisputeGameABI() *abi.ABI {
+	return loadABI(faultDisputeGame)
+}
+func LoadPreimageOracleABI() *abi.ABI {
+	return loadABI(preimageOracle)
+}
+func LoadMIPSABI() *abi.ABI {
+	return loadABI(mips)
+}
+func LoadDelayedWETHABI() *abi.ABI {
+	return loadABI(delayedWETH)
+}
+
+func LoadSystemConfigABI() *abi.ABI {
+	return loadABI(systemConfig)
+}
+
+func LoadCrossL2InboxABI() *abi.ABI {
+	return loadABI(crossL2Inbox)
+}
+
+func loadABI(json []byte) *abi.ABI {
+	if parsed, err := abi.JSON(bytes.NewReader(json)); err != nil {
+		panic(err)
+	} else {
+		return &parsed
+	}
+}
