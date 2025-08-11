43a876f34df271d81141584ed3df5f703125db10
Adrian Sutton <adrian@oplabs.co>
2025-08-11 15:27:44 +1000

    Fix contract checks.

--- packages/contracts-bedrock/src/integration/GameHelper.sol
+++ packages/contracts-bedrock/src/integration/GameHelper.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import {IDisputeGameFactory} from "../../interfaces/dispute/IDisputeGameFactory.sol";
-import {IFaultDisputeGame} from "../../interfaces/dispute/IFaultDisputeGame.sol";
+import { IDisputeGameFactory } from "../../interfaces/dispute/IDisputeGameFactory.sol";
+import { IFaultDisputeGame } from "../../interfaces/dispute/IFaultDisputeGame.sol";
 
 // Libraries
 import { Claim, Position, GameType } from "src/dispute/lib/Types.sol";
@@ -14,25 +14,13 @@ contract GameHelper {
         bool attack;
     }
 
-    function performMoves(
-        IFaultDisputeGame _game,
-        Move[] calldata _moves
-    ) public payable {
+    function performMoves(IFaultDisputeGame _game, Move[] calldata _moves) public payable {
         uint256 movesLen = _moves.length;
         for (uint256 i = 0; i < movesLen; i++) {
             Move memory move = _moves[i];
-            (, , , , Claim pClaim, Position pPosition, ) = _game.claimData(
-                move.parentIdx
-            );
-            uint256 requiredBond = _game.getRequiredBond(
-                pPosition.move(move.attack)
-            );
-            _game.move{value: requiredBond}(
-                pClaim,
-                move.parentIdx,
-                move.claim,
-                move.attack
-            );
+            (,,,, Claim pClaim, Position pPosition,) = _game.claimData(move.parentIdx);
+            uint256 requiredBond = _game.getRequiredBond(pPosition.move(move.attack));
+            _game.move{ value: requiredBond }(pClaim, move.parentIdx, move.claim, move.attack);
         }
     }
 
@@ -42,13 +30,14 @@ contract GameHelper {
         Claim _rootClaim,
         bytes memory _extraData,
         Move[] calldata _moves
-    ) external payable returns (address) {
+    )
+        external
+        payable
+        returns (address)
+    {
         uint256 initBond = _dgf.initBonds(_gameType);
-        IFaultDisputeGame game = IFaultDisputeGame(
-            address(
-                _dgf.create{value: initBond}(_gameType, _rootClaim, _extraData)
-            )
-        );
+        IFaultDisputeGame game =
+            IFaultDisputeGame(address(_dgf.create{ value: initBond }(_gameType, _rootClaim, _extraData)));
         performMoves(game, _moves);
         return address(game);
     }
