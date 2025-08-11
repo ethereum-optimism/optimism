6a647cbe9dec970d3238fb31b95332b0e2c6236c
William Law <williamlaw.wtl@gmail.com>
2025-08-11 11:32:54 -0400

    feedback on e2e and acceptance tests

--- op-program/client/l2/engineapi/l2_engine_api_test.go
+++ op-program/client/l2/engineapi/l2_engine_api_test.go
@@ -200,6 +200,7 @@ func createGenesisWithForkTimeOffset(forkTimeOffset uint64) *core.Genesis {
 	deployConfig.L2GenesisHoloceneTimeOffset = &ts
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 	// Set fork time for latest forks
 	offset := hexutil.Uint64(forkTimeOffset)
 	deployConfig.L2GenesisIsthmusTimeOffset = &offset
@@ -208,6 +209,17 @@ func createGenesisWithForkTimeOffset(forkTimeOffset uint64) *core.Genesis {
 	// Set isthmus time and subsequent forks
 	isthmusTimeOffset := hexutil.Uint64(isthmusTime)
 	deployConfig.L2GenesisIsthmusTimeOffset = &isthmusTimeOffset
+=======
+	// Set fork time for latest forks
+	forkTimeOffset := hexutil.Uint64(forkTime)
+	deployConfig.L2GenesisIsthmusTimeOffset = &forkTimeOffset
+	deployConfig.L2GenesisInteropTimeOffset = &forkTimeOffset
+	deployConfig.L2GenesisJovianTimeOffset = &forkTimeOffset
+
+	l1Genesis, err := genesis.NewL1Genesis(deployConfig)
+	if err != nil {
+		panic(err)
+>>>>>>> 7bbf76e6f6 (feedback on e2e and acceptance tests)
 	}
 	l2Genesis, err := genesis.NewL2Genesis(deployConfig, eth.BlockRefFromHeader(l1Genesis.ToBlock().Header()))
 	if err != nil {
