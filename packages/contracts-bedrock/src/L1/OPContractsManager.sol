1ef2692ab02fb86596f1b3098f354d024f17b902
Matthew Slipper <me@matthewslipper.com>
2025-08-12 10:15:29 -0600

    bindings, remove usages

--- packages/contracts-bedrock/src/L1/OPContractsManager.sol
+++ packages/contracts-bedrock/src/L1/OPContractsManager.sol
@@ -1760,9 +1760,9 @@ contract OPContractsManager is ISemver {
 
     // -------- Constants and Variables --------
 
-    /// @custom:semver 2.6.0
+    /// @custom:semver 3.0.0
     function version() public pure virtual returns (string memory) {
-        return "2.6.0";
+        return "3.0.0";
     }
 
     OPContractsManagerGameTypeAdder public immutable opcmGameTypeAdder;
