3836fffd8b3cf10e867eee09faf5f114406d9017
Anton Evangelatov <anton.evangelatov@oplabs.co>
2025-08-12 15:53:36 +0300

    op-node: remove comment

--- op-node/node/tracer/comms.go
+++ op-node/node/tracer/comms.go
@@ -8,7 +8,6 @@ import (
 	"github.com/ethereum-optimism/optimism/op-service/eth"
 )
 
-// Tracer configures the OpNode to share events
 type Tracer interface {
 	OnNewL1Head(ctx context.Context, sig eth.L1BlockRef)
 	OnUnsafeL2Payload(ctx context.Context, from peer.ID, payload *eth.ExecutionPayloadEnvelope)
