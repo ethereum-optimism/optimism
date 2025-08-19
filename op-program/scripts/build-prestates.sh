3855fef7fa8ee3c600c44344d67fd08aa5c87bce
inphi <mlaw2501@gmail.com>
2025-08-19 13:52:36 -0400

    remove version filter

--- op-program/scripts/build-prestates.sh
+++ op-program/scripts/build-prestates.sh
@@ -26,8 +26,9 @@ mkdir -p "${STATES_DIR}" "${LOGS_DIR}"
 cd "${REPO_DIR}"
 
 VERSIONS_JSON="[]"
-VERSIONS=$(git tag --list 'op-program/v*' --sort taggerdate | grep v1.4.0-rc.3)
+#VERSIONS=$(git tag --list 'op-program/v*' --sort taggerdate | grep v1.4.0-rc.3)
 #VERSIONS=$(git tag --list 'op-program/v*' --sort taggerdate | grep v1.6.0-rc.2)
+VERSIONS=$(git tag --list 'op-program/v*' --sort taggerdate)
 
 for VERSION in ${VERSIONS}
 do
