2b8febda8176869c2021b867ac1a6d270c9c9f83
mbaxter <meredith@oplabs.co>
2025-08-21 11:06:44 -0400

    Bump StandardValidator contract version

--- packages/contracts-bedrock/src/L1/OPContractsManagerStandardValidator.sol
+++ packages/contracts-bedrock/src/L1/OPContractsManagerStandardValidator.sol
@@ -36,8 +36,8 @@ import { IProxyAdminOwnedBase } from "interfaces/L1/IProxyAdminOwnedBase.sol";
 /// before and after an upgrade.
 contract OPContractsManagerStandardValidator is ISemver {
     /// @notice The semantic version of the OPContractsManagerStandardValidator contract.
-    /// @custom:semver 1.6.0
-    string public constant version = "1.6.0";
+    /// @custom:semver 1.7.0
+    string public constant version = "1.7.0";
 
     /// @notice The SuperchainConfig contract.
     ISuperchainConfig public superchainConfig;
