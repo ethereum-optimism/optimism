1322c6981c501b52af2130e3a8f671687b12e09a
mbaxter <meredith@oplabs.co>
2025-08-29 17:45:38 -0400

    Update PDG v2 version

--- packages/contracts-bedrock/src/dispute/v2/PermissionedDisputeGameV2.sol
+++ packages/contracts-bedrock/src/dispute/v2/PermissionedDisputeGameV2.sol
@@ -32,9 +32,9 @@ contract PermissionedDisputeGameV2 is FaultDisputeGameV2 {
     }
 
     /// @notice Semantic version.
-    /// @custom:semver 1.8.0
+    /// @custom:semver 2.0.0
     function version() public pure override returns (string memory) {
-        return "1.8.0";
+        return "2.0.0";
     }
 
     /// @param _params Parameters for creating a new FaultDisputeGame.
