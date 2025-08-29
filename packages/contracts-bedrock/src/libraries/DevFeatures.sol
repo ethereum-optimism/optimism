f6c583dd8eeef047e1c97cb4dfaadd86e58db1fa
Kelvin Fichter <kelvinfichter@gmail.com>
2025-08-29 09:42:29 -0400

    feat: small ergonomic cleanup

--- packages/contracts-bedrock/src/libraries/DevFeatures.sol
+++ packages/contracts-bedrock/src/libraries/DevFeatures.sol
@@ -6,4 +6,15 @@ library DevFeatures {
     /// @notice The feature that enables the OptimismPortalInterop contract.
     bytes32 public constant OPTIMISM_PORTAL_INTEROP =
         bytes32(0x0000000000000000000000000000000000000000000000000000000000000001);
+
+    /// @notice Checks if a feature is enabled in a bitmap. Note that this function does not check
+    ///         that the input feature represents a single feature and the bitwise AND operation
+    ///         allows for multiple features to be enabled at once. Users should generally check
+    ///         for only a single feature at a time.
+    /// @param _bitmap The bitmap to check.
+    /// @param _feature The feature to check.
+    /// @return True if the feature is enabled, false otherwise.
+    function isFeatureEnabled(bytes32 _bitmap, bytes32 _feature) internal pure returns (bool) {
+        return (_bitmap & _feature) != 0;
+    }
 }
