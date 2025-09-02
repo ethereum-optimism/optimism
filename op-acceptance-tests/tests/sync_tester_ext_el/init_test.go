c7c5cb13a732395ef73b5d67e507fc0be1197965
Anton Evangelatov <anton.evangelatov@oplabs.co>
2025-09-02 15:40:57 +0200

    op-acceptance-tests: skip tests until CI allows tests using external endpoints

--- op-acceptance-tests/tests/sync_tester_ext_el/init_test.go
+++ op-acceptance-tests/tests/sync_tester_ext_el/init_test.go
@@ -1,6 +1,7 @@
 package sync_tester_ext_el
 
 import (
+	"os"
 	"testing"
 
 	"github.com/ethereum-optimism/optimism/op-devstack/compat"
@@ -9,14 +10,18 @@ import (
 )
 
 func TestMain(m *testing.M) {
+	if os.Getenv("NIGHTLY_CI_TAILSCALE_JOB") != "true" {
+		// Skipping tests because NIGHTLY_CI_TAILSCALE_JOB is not set
+		return
+	}
+
 	L2NetworkName := "sepolia"
 	L1ChainID := eth.ChainIDFromUInt64(11155111)
 
 	// tailscale
 	L2ELEndpoint := "https://sepolia.optimism.io/"
 	L1CLBeaconEndpoint := "https://beacon-api-proxy-sepolia.primary.client.dev.oplabs.cloud"
-	// L1ELEndpoint := "https://proxyd-l1-sepolia.primary.client.dev.oplabs.cloud"
-	L1ELEndpoint := "https://ci-sepolia-l1.optimism.io"
+	L1ELEndpoint := "https://proxyd-l1-sepolia.primary.client.dev.oplabs.cloud"
 
 	presets.DoMain(m, presets.WithMinimalExternalELWithSuperchainRegistry(L1CLBeaconEndpoint, L1ELEndpoint, L2ELEndpoint, L1ChainID, L2NetworkName, eth.FCUState{
 		Latest:    32012748,
