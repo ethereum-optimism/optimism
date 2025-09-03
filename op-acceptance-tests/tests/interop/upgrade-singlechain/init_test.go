3cbf02812f1593a5a6870ecef184f64dc2a0e91c
Adrian <hawkadrian10@gmail.com>
2025-09-03 02:04:36 +0200

    Update init_test.go

--- op-acceptance-tests/tests/interop/upgrade-singlechain/init_test.go
+++ op-acceptance-tests/tests/interop/upgrade-singlechain/init_test.go
@@ -9,7 +9,7 @@ import (
 func TestMain(m *testing.M) {
 	presets.DoMain(m,
 		presets.WithSingleChainInterop(),
-		presets.WithSuggestedInteropActivationOffset(30),
+		presets.WithSuggestedInteropActivationOffset(120), // Increased from 30 to 120 seconds for consistency
 		presets.WithInteropNotAtGenesis(),
 		presets.WithL2NetworkCount(1), // Specifically testing dependency set of 1 upgrade
 	)
