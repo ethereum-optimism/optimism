425d3cc4508c41ee86fc1749c0ff85039cac0339
geoknee <georgeknee@googlemail.com>
2025-09-04 13:07:35 +0100

    use eip1559.Validate/DecodeOptimismExtraData in PayloadToSystemConfig

--- op-node/rollup/derive/payload_util.go
+++ op-node/rollup/derive/payload_util.go
@@ -90,20 +90,13 @@ func PayloadToSystemConfig(rollupCfg *rollup.Config, payload *eth.ExecutionPaylo
 		Scalar:      info.L1FeeScalar,
 		GasLimit:    uint64(payload.GasLimit),
 	}
-	if rollupCfg.IsJovian(uint64(payload.Timestamp)) {
-		if err := eip1559.ValidateJovianExtraData(payload.ExtraData); err != nil {
-			return eth.SystemConfig{}, err
-		}
-		d, e, _ := eip1559.DecodeJovianExtraData(payload.ExtraData)
-		copy(r.EIP1559Params[:], eip1559.EncodeHolocene1559Params(d, e))
-		// TODO r.MinBaseFee = m
-	} else if rollupCfg.IsHolocene(uint64(payload.Timestamp)) {
-		if err := eip1559.ValidateHoloceneExtraData(payload.ExtraData); err != nil {
-			return eth.SystemConfig{}, err
-		}
-		d, e := eip1559.DecodeHoloceneExtraData(payload.ExtraData)
-		copy(r.EIP1559Params[:], eip1559.EncodeHolocene1559Params(d, e))
+	err = eip1559.ValidateOptimismExtraData(rollupCfg, uint64(payload.Timestamp), payload.ExtraData)
+	if err != nil {
+		return eth.SystemConfig{}, err
 	}
+	d, e, _ := eip1559.DecodeOptimismExtraData(rollupCfg, uint64(payload.Timestamp), payload.ExtraData)
+	copy(r.EIP1559Params[:], eip1559.EncodeHolocene1559Params(d, e))
+	// TODO r.MinBaseFee = m
 
 	if rollupCfg.IsIsthmus(uint64(payload.Timestamp)) {
 		r.OperatorFeeParams = eth.EncodeOperatorFeeParams(eth.OperatorFeeParams{
