bbdf01f99b0569f84e1485fc018aa069ef0b4315
steven <steven.nevins.js@gmail.com>
2025-09-08 11:19:02 -0400

    fix: incoming change added exclusion as well

--- packages/contracts-bedrock/test/vendor/Initializable.t.sol
+++ packages/contracts-bedrock/test/vendor/Initializable.t.sol
@@ -378,7 +378,7 @@ contract Initializer_Test is CommonTest {
     function test_cannotReinitialize_succeeds() public {
         // Collect exclusions.
         uint256 j;
-        string[] memory excludes = new string[](10);
+        string[] memory excludes = new string[](11);
         // Contract is currently not being deployed as part of the standard deployment script.
         excludes[j++] = "src/L2/OptimismSuperchainERC20.sol";
         // Periphery contracts don't get deployed as part of the standard deployment script.
