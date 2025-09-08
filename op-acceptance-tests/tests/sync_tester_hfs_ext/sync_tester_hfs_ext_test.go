2fb200fe1c3367372464c005e0430f1a13985235
Anton Evangelatov <anton.evangelatov@oplabs.co>
2025-09-08 11:33:13 +0200

    op-acceptance-tests: hfs ext package

---
+++ op-acceptance-tests/tests/sync_tester_hfs_ext/sync_tester_hfs_ext_test.go
@@ -0,0 +1,235 @@
+package sync_tester_hfs_ext
+
+import (
+	"context"
+	"os"
+	"strconv"
+	"testing"
+
+	"github.com/ethereum/go-ethereum/log"
+
+	"github.com/ethereum-optimism/optimism/op-devstack/devtest"
+	"github.com/ethereum-optimism/optimism/op-devstack/dsl"
+	"github.com/ethereum-optimism/optimism/op-devstack/shim"
+	"github.com/ethereum-optimism/optimism/op-devstack/stack"
+	"github.com/ethereum-optimism/optimism/op-devstack/stack/match"
+	"github.com/ethereum-optimism/optimism/op-devstack/sysgo"
+	"github.com/ethereum-optimism/optimism/op-service/eth"
+	"github.com/ethereum-optimism/optimism/op-service/testlog"
+	"github.com/ethereum-optimism/optimism/op-supervisor/supervisor/types"
+)
+
+// Configuration defaults for op-sepolia
+const (
+	DefaultL2NetworkName      = "op-sepolia"
+	DefaultL1ChainID          = 11155111
+	DefaultL2ELEndpoint       = "https://ci-sepolia-l2.optimism.io"
+	DefaultL1CLBeaconEndpoint = "https://ci-sepolia-beacon.optimism.io"
+	DefaultL1ELEndpoint       = "https://ci-sepolia-l1.optimism.io"
+
+	// Tailscale networking endpoints
+	DefaultL2ELEndpointTailscale       = "https://proxyd-l2-sepolia.primary.client.dev.oplabs.cloud"
+	DefaultL1CLBeaconEndpointTailscale = "https://beacon-api-proxy-sepolia.primary.client.dev.oplabs.cloud"
+	DefaultL1ELEndpointTailscale       = "https://proxyd-l1-sepolia.primary.client.dev.oplabs.cloud"
+)
+
+// Network upgrade block numbers for op-sepolia
+var networkUpgradeBlocks = map[string]uint64{
+	"Canyon":   4089330,
+	"Delta":    5700330,
+	"Ecotone":  8366130,
+	"Fjord":    12597930,
+	"Granite":  15837930,
+	"Holocene": 20415330,
+	"Isthmus":  26551530,
+}
+
+var (
+	// Load configuration from environment variables with defaults
+	L2NetworkName = getEnvOrDefault("L2_NETWORK_NAME", DefaultL2NetworkName)
+	L1ChainID     = eth.ChainIDFromUInt64(getEnvUint64OrDefault("L1_CHAIN_ID", DefaultL1ChainID))
+
+	// Default endpoints
+	L2ELEndpoint       = getEnvOrDefault("L2_EL_ENDPOINT", DefaultL2ELEndpoint)
+	L1CLBeaconEndpoint = getEnvOrDefault("L1_CL_BEACON_ENDPOINT", DefaultL1CLBeaconEndpoint)
+	L1ELEndpoint       = getEnvOrDefault("L1_EL_ENDPOINT", DefaultL1ELEndpoint)
+)
+
+// getEnvOrDefault returns the environment variable value or the default if not set
+func getEnvOrDefault(envVar, defaultValue string) string {
+	if value := os.Getenv(envVar); value != "" {
+		return value
+	}
+	return defaultValue
+}
+
+// getEnvUint64OrDefault returns the environment variable value as uint64 or the default if not set
+func getEnvUint64OrDefault(envVar string, defaultValue uint64) uint64 {
+	if value := os.Getenv(envVar); value != "" {
+		if parsed, err := strconv.ParseUint(value, 10, 64); err == nil {
+			return parsed
+		}
+	}
+	return defaultValue
+}
+
+// setupOrchestrator initializes and configures the orchestrator for the test
+func setupOrchestrator(gt *testing.T, t devtest.T, blk uint64) *sysgo.Orchestrator {
+	l := t.Logger()
+
+	// Override configuration with Tailscale endpoints if Tailscale networking is enabled
+	l2ELEndpoint := L2ELEndpoint
+	l1CLBeaconEndpoint := L1CLBeaconEndpoint
+	l1ELEndpoint := L1ELEndpoint
+
+	if os.Getenv("TAILSCALE_NETWORKING") == "true" {
+		l2ELEndpoint = getEnvOrDefault("L2_EL_ENDPOINT_TAILSCALE", DefaultL2ELEndpointTailscale)
+		l1CLBeaconEndpoint = getEnvOrDefault("L1_CL_BEACON_ENDPOINT_TAILSCALE", DefaultL1CLBeaconEndpointTailscale)
+		l1ELEndpoint = getEnvOrDefault("L1_EL_ENDPOINT_TAILSCALE", DefaultL1ELEndpointTailscale)
+	}
+
+	// Setup orchestrator directly without TestMain
+	logger := testlog.Logger(gt, log.LevelInfo)
+	onFail := func(now bool) {
+		if now {
+			gt.FailNow()
+		} else {
+			gt.Fail()
+		}
+	}
+	onSkipNow := func() {
+		gt.SkipNow()
+	}
+	p := devtest.NewP(context.Background(), logger, onFail, onSkipNow)
+	gt.Cleanup(p.Close)
+
+	// Runtime configuration values
+	l.Info("Runtime configuration values for TestSyncTesterExtEL")
+	l.Info("L2_NETWORK_NAME", "value", L2NetworkName)
+	l.Info("L1_CHAIN_ID", "value", L1ChainID)
+	l.Info("L2_EL_ENDPOINT", "value", L2ELEndpoint)
+	l.Info("L1_CL_BEACON_ENDPOINT", "value", L1CLBeaconEndpoint)
+	l.Info("L1_EL_ENDPOINT", "value", L1ELEndpoint)
+	l.Info("TAILSCALE_NETWORKING", "value", os.Getenv("TAILSCALE_NETWORKING"))
+
+	// Create orchestrator with the same configuration that was in TestMain
+	opt := sysgo.DefaultMinimalExternalELSystemWithEndpointAndSuperchainRegistry(&sysgo.DefaultMinimalExternalELSystemIDs{}, l1CLBeaconEndpoint, l1ELEndpoint, l2ELEndpoint, L1ChainID, L2NetworkName, eth.FCUState{
+		Latest:    blk,
+		Safe:      blk,
+		Finalized: blk,
+	})
+
+	orch := sysgo.NewOrchestrator(p, stack.SystemHook(opt))
+	stack.ApplyOptionLifecycle[*sysgo.Orchestrator](opt, orch)
+
+	return orch
+}
+
+func TestSyncTesterHFS_Canyon(gt *testing.T) {
+	forkTimestamp := func(net *dsl.L2Network) *uint64 {
+		return net.Escape().ChainConfig().CanyonTime
+	}
+	testSyncTesterHFSExt(gt, "Canyon", forkTimestamp)
+}
+
+func TestSyncTesterHFS_Ecotone(gt *testing.T) {
+	forkTimestamp := func(net *dsl.L2Network) *uint64 {
+		return net.Escape().ChainConfig().EcotoneTime
+	}
+	testSyncTesterHFSExt(gt, "Ecotone", forkTimestamp)
+}
+
+func TestSyncTesterHFS_Fjord(gt *testing.T) {
+	forkTimestamp := func(net *dsl.L2Network) *uint64 {
+		return net.Escape().ChainConfig().FjordTime
+	}
+	testSyncTesterHFSExt(gt, "Fjord", forkTimestamp)
+}
+
+func TestSyncTesterHFS_Granite(gt *testing.T) {
+	forkTimestamp := func(net *dsl.L2Network) *uint64 {
+		return net.Escape().ChainConfig().GraniteTime
+	}
+	testSyncTesterHFSExt(gt, "Granite", forkTimestamp)
+}
+
+func TestSyncTesterHFS_Holocene(gt *testing.T) {
+	forkTimestamp := func(net *dsl.L2Network) *uint64 {
+		return net.Escape().ChainConfig().HoloceneTime
+	}
+	testSyncTesterHFSExt(gt, "Holocene", forkTimestamp)
+}
+
+func TestSyncTesterHFS_Isthmus(gt *testing.T) {
+	forkTimestamp := func(net *dsl.L2Network) *uint64 {
+		return net.Escape().ChainConfig().IsthmusTime
+	}
+	testSyncTesterHFSExt(gt, "Isthmus", forkTimestamp)
+}
+
+func testSyncTesterHFSExt(gt *testing.T, upgradeName string, forkTimestamp func(net *dsl.L2Network) *uint64) {
+	t := devtest.SerialT(gt)
+	l := t.Logger()
+
+	// Initial block number to sync from before the upgrade
+	blk := networkUpgradeBlocks[upgradeName] - 5
+
+	// Initialize orchestrator
+	orch := setupOrchestrator(gt, t, blk)
+	system := shim.NewSystem(t)
+	orch.Hydrate(system)
+
+	l2 := system.L2Network(match.Assume(t, match.L2ChainA))
+	verifierCL := l2.L2CLNode(match.FirstL2CL)
+	syncTester := l2.SyncTester(match.Assume(t, match.FirstSyncTester))
+
+	sys := &struct {
+		L2CL       *dsl.L2CLNode
+		L2EL       *dsl.L2ELNode
+		SyncTester *dsl.SyncTester
+		L2         *dsl.L2Network
+	}{
+		L2CL:       dsl.NewL2CLNode(verifierCL, orch.ControlPlane()),
+		L2EL:       dsl.NewL2ELNode(l2.L2ELNode(match.FirstL2EL), orch.ControlPlane()),
+		SyncTester: dsl.NewSyncTester(syncTester),
+		L2:         dsl.NewL2Network(l2, orch.ControlPlane()),
+	}
+	require := t.Require()
+
+	ft := forkTimestamp(sys.L2)
+	require.NotNil(ft, "fork timestamp should be set")
+
+	// var l2CLSyncStatus *eth.SyncStatus
+	// err := retry.Do0(t.Ctx(), 10, retry.Fixed(2*time.Second), func() error {
+	// 	l2CLSyncStatus = sys.L2CL.SyncStatus()
+	// 	require.NotNil(l2CLSyncStatus, "L2CL should have sync status")
+	// 	if l2CLSyncStatus.UnsafeL2.Time == 0 {
+	// 		return fmt.Errorf("L2CL unsafe time is still zero")
+	// 	}
+	// 	return nil
+	// })
+	// require.NoError(err, "L2CL unsafe time should be set within retry limit")
+	// require.NotZero(l2CLSyncStatus.UnsafeL2.Time, "L2CL unsafe time should not be zero")
+	// require.Less(l2CLSyncStatus.UnsafeL2.Time, *ft, "L2CL unsafe time should be less than fork timestamp before upgrade")
+
+	blocksToSync := uint64(10)
+	targetBlock := blk + blocksToSync
+	sys.L2CL.Reached(types.LocalUnsafe, targetBlock, 500)
+
+	l2CLSyncStatus := sys.L2CL.SyncStatus()
+	require.NotNil(l2CLSyncStatus, "L2CL should have sync status")
+	require.Greater(l2CLSyncStatus.UnsafeL2.Time, *ft, "L2CL unsafe time should be greater than fork timestamp after upgrade")
+
+	unsafeL2Ref := l2CLSyncStatus.UnsafeL2
+	ref := sys.L2EL.BlockRefByNumber(unsafeL2Ref.Number)
+	require.Equal(unsafeL2Ref.Hash, ref.Hash, "L2EL should be on the same block as L2CL")
+
+	stSessions := sys.SyncTester.ListSessions()
+	require.Equal(len(stSessions), 1, "expect exactly one session")
+
+	stSession := sys.SyncTester.GetSession(stSessions[0])
+	require.GreaterOrEqual(stSession.CurrentState.Latest, stSession.InitialState.Latest+blocksToSync, "SyncTester session Latest should be on the same block as L2CL")
+	require.GreaterOrEqual(stSession.CurrentState.Safe, stSession.InitialState.Safe+blocksToSync, "SyncTester session Safe should be on the same block as L2CL")
+
+	l.Info("SyncTester HFS Ext test completed successfully", "l2cl_chain_id", sys.L2CL.ID().ChainID(), "l2cl_sync_status", l2CLSyncStatus, "upgrade_name", upgradeName)
+}
