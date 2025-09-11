ec9db14790579f9f821bea821c1c563c1f9d9fa0
William Law <williamlaw.wtl@gmail.com>
2025-09-11 12:12:24 -0400

    remove unused code/funcs in l2_sequencer and verifier

--- op-e2e/actions/helpers/l2_verifier.go
+++ op-e2e/actions/helpers/l2_verifier.go
@@ -133,10 +133,7 @@ func NewL2Verifier(t Testing, log log.Logger, l1 derive.L1Fetcher,
 	)
 
 	var interopSys interop.SubSystem
-	// Only enable supervisor management for multi-chain setups
-	// For single-chain tests with interop/jovian, we provide the depSet but don't use supervisor management
-	enableSupervisorManagement := cfg.InteropTime != nil && depSet != nil && len(depSet.Chains()) > 1
-	if enableSupervisorManagement {
+	if cfg.InteropTime != nil {
 		mm := indexing.NewIndexingMode(log, cfg, "127.0.0.1", 0, interopJWTSecret, l1, eng, &opmetrics.NoopRPCMetrics{})
 		mm.TestDisableEventDeduplication()
 		interopSys = mm
