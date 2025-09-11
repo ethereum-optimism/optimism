6f3b86f6a3acd671d451d1b6ccb94c243ed55021
geoknee <georgeknee@googlemail.com>
2025-09-11 10:48:48 +0100

    add comment

--- op-node/rollup/toggles.go
+++ op-node/rollup/toggles.go
@@ -1,5 +1,8 @@
 package rollup
 
+// This file contains ephemeral feature toggles which should be removed
+// after the fork scope is locked.
+
 func (c *Config) IsMinBaseFee(time uint64) bool {
 	return c.IsJovian(time) // Replace with return false to disable
 }
