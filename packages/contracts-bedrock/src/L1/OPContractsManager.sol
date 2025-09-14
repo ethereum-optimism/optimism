4f5312f22799e1cec6cd29102a32a57a4a54216d
Adrian Sutton <adrian@oplabs.co>
2025-09-15 06:54:59 +1000

    opcm: Update version

--- packages/contracts-bedrock/src/L1/OPContractsManager.sol
+++ packages/contracts-bedrock/src/L1/OPContractsManager.sol
@@ -1859,9 +1859,9 @@ contract OPContractsManager is ISemver {
 
     // -------- Constants and Variables --------
 
-    /// @custom:semver 3.3.0
+    /// @custom:semver 3.4.0
     function version() public pure virtual returns (string memory) {
-        return "3.3.0";
+        return "3.4.0";
     }
 
     OPContractsManagerGameTypeAdder public immutable opcmGameTypeAdder;
