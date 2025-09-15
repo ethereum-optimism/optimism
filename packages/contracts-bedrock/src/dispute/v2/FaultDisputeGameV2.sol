3a6853c2e6401ca3e43b4ae277d0df96a5a2a151
mbaxter <meredith@oplabs.co>
2025-09-15 11:57:03 -0400

    Make calldata length helpers more granular

--- packages/contracts-bedrock/src/dispute/v2/FaultDisputeGameV2.sol
+++ packages/contracts-bedrock/src/dispute/v2/FaultDisputeGameV2.sol
@@ -268,7 +268,7 @@ contract FaultDisputeGameV2 is Clone, ISemver {
         // This is to prevent adding extra or omitting bytes from to `extraData` that result in a different game UUID
         // in the factory, but are not used by the game, which would allow for multiple dispute games for the same
         // output proposal to be created.
-        if (msg.data.length != expectedCallDataLength()) revert BadExtraData();
+        if (msg.data.length != expectedInitCallDataLength()) revert BadExtraData();
 
         // Grab the latest anchor root.
         (Hash root, uint256 rootBlockNumber) = anchorStateRegistry().getAnchorRoot();
@@ -327,10 +327,16 @@ contract FaultDisputeGameV2 is Clone, ISemver {
             GameType.unwrap(anchorStateRegistry().respectedGameType()) == GameType.unwrap(GAME_TYPE);
     }
 
-    function expectedCallDataLength() internal virtual {
-        // Expected length: 246 bytes
+    function expectedInitCallDataLength() internal returns (uint256) {
+        // Expected length: 6 bytes + CWIA data length
         // - 4 bytes: selector
         // - 2 bytes: CWIA length prefix
+        // - n bytes: Remaining CWIA data
+        return 6 + expectedCWIADataLength();
+    }
+
+    function expectedCWIADataLength() internal virtual returns (uint256) {
+        // Expected length: 240 bytes
         // - 20 bytes: creator address
         // - 32 bytes: root claim
         // - 32 bytes: l1 head
@@ -340,7 +346,7 @@ contract FaultDisputeGameV2 is Clone, ISemver {
         // - 20 bytes: anchorStateRegistry address
         // - 20 bytes: weth address
         // - 32 bytes: l2ChainId
-        return 246;
+        return 240;
     }
 
     ////////////////////////////////////////////////////////////////
