37777cf907ce2fec271fe76ac19c3ef3a26c793d
Kelvin Fichter <kelvinfichter@gmail.com>
2025-09-16 15:52:52 -0400

    fix: bug blocking op-deployer verify
    Fixes some bugs in op-deployer's verify command. Also fixes a few
    issues with the contracts that causes the deployer to struggle
    to properly verify things.

--- packages/contracts-bedrock/test/setup/ForkLive.s.sol
+++ packages/contracts-bedrock/test/setup/ForkLive.s.sol
@@ -15,7 +15,7 @@ import { Config } from "scripts/libraries/Config.sol";
 // Libraries
 import { GameTypes, Claim } from "src/dispute/lib/Types.sol";
 import { EIP1967Helper } from "test/mocks/EIP1967Helper.sol";
-import { LibString } from "solady/src/utils/LibString.sol";
+import { LibString } from "@solady/utils/LibString.sol";
 
 // Interfaces
 import { IFaultDisputeGame } from "interfaces/dispute/IFaultDisputeGame.sol";
