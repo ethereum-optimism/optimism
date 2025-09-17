2922e3d29506b4aab8b49298527f60df2d468983
Ashitaka <96790496+ashitakah@users.noreply.github.com>
2025-09-17 14:51:59 -0300

    fix: json default zero

--- packages/contracts-bedrock/scripts/deploy/DeployConfig.s.sol
+++ packages/contracts-bedrock/scripts/deploy/DeployConfig.s.sol
@@ -126,7 +126,7 @@ contract DeployConfig is Script {
         useCustomGasToken = _readOr(_json, "$.useCustomGasToken", false);
         gasPayingTokenName = _readOr(_json, "$.gasPayingTokenName", "");
         gasPayingTokenSymbol = _readOr(_json, "$.gasPayingTokenSymbol", "");
-        nativeAssetLiquidityAmount = _readOr(_json, "$.nativeAssetLiquidityAmount", type(uint248).max);
+        nativeAssetLiquidityAmount = _readOr(_json, "$.nativeAssetLiquidityAmount", 0);
 
         enableGovernance = _readOr(_json, "$.enableGovernance", false);
         systemConfigStartBlock = stdJson.readUint(_json, "$.systemConfigStartBlock");
