a21d14d2869742f20491de9058bc6fbb99a20a29
geoknee <georgeknee@googlemail.com>
2025-09-17 11:23:10 +0100

    add operator fee fix toggle

--- op-node/rollup/toggles.go
+++ op-node/rollup/toggles.go
@@ -10,3 +10,7 @@ func (c *Config) IsMinBaseFee(time uint64) bool {
 func (c *Config) IsDAFootprintBlockLimit(time uint64) bool {
 	return c.IsJovian(time) // Replace with return false to disable
 }
+
+func (c *Config) IsOperatorFeeFix(time uint64) bool {
+	return c.IsJovian(time) // Replace with return false to disable
+}
