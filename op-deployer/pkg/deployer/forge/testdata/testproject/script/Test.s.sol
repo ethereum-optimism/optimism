6788cceb52bdf1e32dd2019a5e07d6173b7031dd
Samuel Stokes <sam.adam.stokes@gmail.com>
2025-09-18 15:47:15 -0400

    op-deployer: create generic forge.BytesScriptEncoder

--- op-deployer/pkg/deployer/forge/testdata/testproject/script/Test.s.sol
+++ op-deployer/pkg/deployer/forge/testdata/testproject/script/Test.s.sol
@@ -5,18 +5,20 @@ contract TestScript {
     struct Input {
         uint8 id;
         bytes data;
+        uint32[] slice;
     }
 
     struct Output {
         uint8 id;
         bytes data;
+        uint32[] slice;
     }
 
     function _run(Input memory _input) public pure returns (Output memory) {
-        return Output({ id: 0x02, data: _input.data });
+        return Output({ id: 0x02, data: _input.data, slice: _input.slice });
     }
 
-    function run(bytes memory _input) public pure returns (bytes memory) {
+    function runEncoded(bytes memory _input) public pure returns (bytes memory) {
         Input memory input = abi.decode(_input, (Input));
         Output memory output = _run(input);
         return abi.encode(output);
