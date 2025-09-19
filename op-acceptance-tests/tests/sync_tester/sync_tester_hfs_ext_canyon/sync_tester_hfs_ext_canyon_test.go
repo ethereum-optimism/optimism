c03f7cb0bf51eff0ddf26726c30fb2ef4055015b
Park Changwan <pcw109550@gmail.com>
2025-09-19 22:39:25 +0900

    op-acceptance-tests: Sync Tester External Network HF tests: SyncMode

--- op-acceptance-tests/tests/sync_tester/sync_tester_hfs_ext_canyon/sync_tester_hfs_ext_canyon_test.go
+++ op-acceptance-tests/tests/sync_tester/sync_tester_hfs_ext_canyon/sync_tester_hfs_ext_canyon_test.go
@@ -5,8 +5,13 @@ import (
 
 	"github.com/ethereum-optimism/optimism/op-acceptance-tests/tests/sync_tester/hardforks_ext"
 	"github.com/ethereum-optimism/optimism/op-node/rollup"
+	"github.com/ethereum-optimism/optimism/op-node/rollup/sync"
 )
 
-func TestSyncTesterHFS_Canyon(gt *testing.T) {
-	hardforks_ext.SyncTesterHFSExt(gt, rollup.Canyon)
+func TestSyncTesterHFS_Canyon_CLSync(gt *testing.T) {
+	hardforks_ext.SyncTesterHFSExt(gt, rollup.Canyon, sync.CLSync)
+}
+
+func TestSyncTesterHFS_Canyon_ELSync(gt *testing.T) {
+	hardforks_ext.SyncTesterHFSExt(gt, rollup.Canyon, sync.ELSync)
 }
