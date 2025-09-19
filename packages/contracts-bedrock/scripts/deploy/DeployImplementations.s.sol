ac03fa47eda86ddf3b23457caa0950c2fe05b93a
steven <12021290+stevennevins@users.noreply.github.com>
2025-09-19 14:29:04 -0400

    Update packages/contracts-bedrock/scripts/deploy/DeployImplementations.s.sol
    Co-authored-by: mbaxter <meredith@oplabs.co>

--- packages/contracts-bedrock/scripts/deploy/DeployImplementations.s.sol
+++ packages/contracts-bedrock/scripts/deploy/DeployImplementations.s.sol
@@ -88,8 +88,8 @@ contract DeployImplementations is Script {
         IAnchorStateRegistry anchorStateRegistryImpl;
         ISuperchainConfig superchainConfigImpl;
         IProtocolVersions protocolVersionsImpl;
-        IFaultDisputeGameV2 faultDisputeGameV2Impl;
-        IPermissionedDisputeGameV2 permissionedDisputeGameV2Impl;
+        IFaultDisputeGame faultDisputeGameV2Impl;
+        IPermissionedDisputeGame permissionedDisputeGameV2Impl;
     }
 
     bytes32 internal _salt = DeployUtils.DEFAULT_SALT;
