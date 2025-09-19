e1cd91dbabcb531958cd17bad0bb57629addab30
Park Changwan <pcw109550@gmail.com>
2025-09-19 23:55:53 +0900

    op-devstack: Logging when L2 CL Signaling

--- op-devstack/dsl/l2_cl.go
+++ op-devstack/dsl/l2_cl.go
@@ -368,6 +368,7 @@ func (cl *L2CLNode) WaitForNonZeroUnsafeTime(ctx context.Context) *eth.SyncStatu
 }
 
 func (cl *L2CLNode) SignalTarget(el *L2ELNode, targetNum uint64) {
+	cl.log.Info("Signaling L2CL", "target", targetNum)
 	payload := el.PayloadByNumber(targetNum)
 	err := retry.Do0(cl.ctx, 3, retry.Fixed(2*time.Second), func() error {
 		return cl.inner.RollupAPI().PostUnsafePayload(cl.ctx, payload)
