268f4a0e0827d0bde204151ecf84030583950cdf
inphi <mlaw2501@gmail.com>
2025-09-24 11:58:23 -0400

    fix opcm semver bump

--- packages/contracts-bedrock/src/L1/OPContractsManager.sol
+++ packages/contracts-bedrock/src/L1/OPContractsManager.sol
@@ -1802,9 +1802,9 @@ contract OPContractsManager is ISemver {
 
     // -------- Constants and Variables --------
 
-    /// @custom:semver 3.4.1
+    /// @custom:semver 3.5.0
     function version() public pure virtual returns (string memory) {
-        return "3.4.1";
+        return "3.5.0";
     }
 
     OPContractsManagerGameTypeAdder public immutable opcmGameTypeAdder;
