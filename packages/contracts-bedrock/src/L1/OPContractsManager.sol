a2e436eef8cee19a0d48586228dc9cdc11ba5f32
inphi <mlaw2501@gmail.com>
2025-09-25 10:03:34 -0400

    bump semver after rebase

--- packages/contracts-bedrock/src/L1/OPContractsManager.sol
+++ packages/contracts-bedrock/src/L1/OPContractsManager.sol
@@ -1829,9 +1829,9 @@ contract OPContractsManager is ISemver {
 
     // -------- Constants and Variables --------
 
-    /// @custom:semver 3.5.0
+    /// @custom:semver 3.6.0
     function version() public pure virtual returns (string memory) {
-        return "3.5.0";
+        return "3.6.0";
     }
 
     OPContractsManagerGameTypeAdder public immutable opcmGameTypeAdder;
