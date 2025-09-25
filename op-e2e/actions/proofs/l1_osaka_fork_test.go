6349fd0fa52f5bba30aad7006d7d2da7e2fb329d
geoknee <georgeknee@googlemail.com>
2025-09-25 14:56:28 +0100

    give test teeth

--- op-e2e/actions/proofs/l1_osaka_fork_test.go
+++ op-e2e/actions/proofs/l1_osaka_fork_test.go
@@ -1,13 +1,20 @@
 package proofs_test
 
 import (
+	"math/big"
 	"testing"
 
 	batcherFlags "github.com/ethereum-optimism/optimism/op-batcher/flags"
 	"github.com/ethereum-optimism/optimism/op-chain-ops/genesis"
 	actionsHelpers "github.com/ethereum-optimism/optimism/op-e2e/actions/helpers"
 	"github.com/ethereum-optimism/optimism/op-e2e/actions/proofs/helpers"
+	legacybindings "github.com/ethereum-optimism/optimism/op-e2e/bindings"
+	"github.com/ethereum-optimism/optimism/op-service/eth"
+	"github.com/ethereum-optimism/optimism/op-service/predeploys"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/consensus/misc/eip4844"
 	"github.com/stretchr/testify/require"
 )
 
@@ -19,63 +26,78 @@ func Test_ProgramAction_OsakaForkAfterGenesis(gt *testing.T) {
 		env := helpers.NewL2FaultProofEnv(t, testCfg, helpers.NewTestParams(),
 			helpers.NewBatcherCfg(
 				func(c *actionsHelpers.BatcherCfg) {
-					c.DataAvailabilityType = batcherFlags.CalldataType
+					c.DataAvailabilityType = batcherFlags.BlobsType
 				},
 			),
 			func(dp *genesis.DeployConfig) {
 				dp.L1OsakaTimeOffset = ptr(hexutil.Uint64(24))
+				// TODO add the BPO forks
+				dp.L1GenesisBlockExcessBlobGas = ptr(hexutil.Uint64(1e8)) // Jack up the blob market so we can test the blob fee calculation
 			},
 		)
 
-		miner, _, verifier, sequencer, _ := env.Miner, env.Batcher, env.Sequencer, env.Sequencer, env.Engine
+		miner, _, _, sequencer, _ := env.Miner, env.Batcher, env.Sequencer, env.Sequencer, env.Engine
 
-		// Helper function to check L1 blob base fee consistency between L1 and L2
-		// checkL1BlockBlobBaseFee := func(t actionsHelpers.StatefulTesting, l2Block *types.Block, l1BlockID eth.BlockID) {
-		// 	// TODO get the blob fee via L1 RPC, and also via L2 RPC if possible.
-		// 	// Also query the L1Block contract and check these value are all consistent.
-		// }
+		// Bind to L1Block contract on L2
+		l1BlockContract, err := legacybindings.NewL1Block(predeploys.L1BlockAddr, env.Engine.EthClient())
+		require.NoError(t, err)
 
-		// Start nodes
-		sequencer.ActL2PipelineFull(t)
-		verifier.ActL2PipelineFull(t)
+		atBlockWithHash := func(hash common.Hash) *bind.CallOpts {
+			return &bind.CallOpts{
+				BlockHash: hash,
+			}
+		}
+
+		// requireConsistentBlobBaseFee requires the blob base fee to be consistent between
+		// the L1 Origin block (computed using the excess blob gas and l1 chain config)
+		// and the L1 Block contract on L2 (acessed with a contract method call).
+		requireConsistentBlobBaseFee := func(t actionsHelpers.StatefulTesting, l2Block eth.L2BlockRef, expectL1OriginToBeFusaka bool) {
+			bbfL2, err := l1BlockContract.BlobBaseFee(atBlockWithHash(l2Block.Hash))
+			require.NoError(t, err)
+
+			l1Origin := miner.L1Chain().GetHeaderByHash(l2Block.L1Origin.Hash)
+			if expectL1OriginToBeFusaka {
+				require.True(t, env.Sd.L1Cfg.Config.IsOsaka(l1Origin.Number, l1Origin.Time), "Osaka not active at l1 origin %d, time %d", l1Origin.Number, l1Origin.Time)
+			} else {
+				require.False(t, env.Sd.L1Cfg.Config.IsOsaka(l1Origin.Number, l1Origin.Time), "Osaka should not be active at l1 origin %d, time %d", l1Origin.Number, l1Origin.Time)
+			}
+			bbfL1 := eip4844.CalcBlobFee(env.Sd.L1Cfg.Config, l1Origin)
+
+			require.True(t, bbfL2.Cmp(bbfL1) == 0,
+				"blob base fee does not match, bbfL2=%d, bbfL1=%d, l1BlockNum=%d, l2BlockNum=%d", bbfL2, bbfL1, l1Origin.Number, l2Block.Number)
+
+			require.True(t, bbfL2.Cmp(big.NewInt(1)) > 0,
+				"blob base fee is unrealistically low and doesn't exercise the blob fee calculation")
+		}
 
 		// Build L1 blocks to trigger Fusaka activation
-		miner.ActEmptyBlock(t) // block 1
-		miner.ActEmptyBlock(t) // block 2 - Fusaka activates here
-
-		block := miner.L1Chain().CurrentBlock()
-		require.True(t, env.Sd.L1Cfg.Config.IsOsaka(block.Number, block.Time), "Osaka not active")
+		l1Block := miner.ActBuildToOsaka(t)
+		require.Equal(t, uint64(2), l1Block.Number().Uint64())
 
 		// Build an empty L2 block which has a pre-Fusaka L1 origin, and check the blob fee is correct
 		sequencer.ActL2EmptyBlock(t)
+		l2Block := sequencer.SyncStatus().UnsafeL2
+		require.Equal(t, uint64(1), l2Block.Number)
+		requireConsistentBlobBaseFee(t, l2Block, false)
 
-		// Build L2 unsafe chain and batch it to L1
+		// Advance L2 chain until L1 origin has Fusaka activ
 		sequencer.ActL1HeadSignal(t)
-		sequencer.ActBuildToL1Head(t)
-		env.BatchMineAndSync(t)
-
-		// Advance L2 chain until L1 origin has Fusaka active
-		sequencer.ActBuildToL1Head(t)
+		sequencer.ActBuildToL1HeadUnsafe(t)
 
 		// Check that the L1 origin is now a Fusaka block, and that the blob fee is correct
-		l2Block := verifier.SyncStatus().UnsafeL2
-
-		l1BlockHeader := miner.L1Chain().GetHeaderByHash(l2Block.L1Origin.Hash)
-		require.True(t, env.Sd.L1Cfg.Config.IsOsaka(l1BlockHeader.Number, l1BlockHeader.Time), "Osaka not active at l1 origin")
-
-		// checkL1BlockBlobBaseFee(t, fullL2Block, l2Block.L1Origin) // TODO
+		l2Block = sequencer.L2Unsafe()
+		require.Greater(t, l2Block.Number, uint64(1))
+		requireConsistentBlobBaseFee(t, l2Block, true)
 
 		// Final sync
-		sequencer.ActL1HeadSignal(t)
-		sequencer.ActBuildToL1Head(t)
 		env.BatchMineAndSync(t)
 
 		// Run fault proof program
-		safeL2Head := verifier.SyncStatus().SafeL2
+		safeL2Head := sequencer.L2Safe()
 		env.RunFaultProofProgramFromGenesis(t, safeL2Head.Number, testCfg.CheckResult, testCfg.InputParams...)
 	}
 
 	matrix := helpers.NewMatrix[any]()
 	defer matrix.Run(gt)
-	matrix.AddDefaultTestCases(nil, helpers.NewForkMatrix(helpers.Holocene, helpers.LatestFork), runL1FusakaTest)
+	matrix.AddDefaultTestCases(nil, helpers.NewForkMatrix(helpers.LatestFork), runL1FusakaTest)
 }
