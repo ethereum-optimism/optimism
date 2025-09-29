9213f495831d4aad2e309bda3c10ac1d3dba131b
Michael Amadi <amadimichaeld@gmail.com>
2025-09-29 17:31:23 +0100

    change semver

--- packages/contracts-bedrock/src/L1/OPContractsManager.sol
+++ packages/contracts-bedrock/src/L1/OPContractsManager.sol
@@ -1803,9 +1803,9 @@ contract OPContractsManager is ISemver {
 
     // -------- Constants and Variables --------
 
-    /// @custom:semver 3.6.1
+    /// @custom:semver 3.5.1
     function version() public pure virtual returns (string memory) {
-        return "3.6.1";
+        return "3.5.1";
     }
 
     OPContractsManagerGameTypeAdder public immutable opcmGameTypeAdder;
