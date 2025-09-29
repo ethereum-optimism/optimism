3738717ff788f7a06c8bce46655fb52c8643110b
Anton Evangelatov <anton.evangelatov@gmail.com>
2025-09-29 13:56:11 +0200

    op-node/rollup: remove ReceivedUnsafePayloadEvent event (#17530)
    * op-node/rollup: remove ReceivedUnsafePayloadEvent event
    
    * op-node/rollup: add lock around AddUnsafePayload

--- op-e2e/actions/helpers/l2_verifier.go
+++ op-e2e/actions/helpers/l2_verifier.go
@@ -55,7 +55,8 @@ type L2Verifier struct {
 
 	log log.Logger
 
-	Eng L2API
+	Eng    L2API
+	CLSync *clsync.CLSync
 
 	syncStatus driver.SyncStatusTracker
 
@@ -216,6 +217,7 @@ func NewL2Verifier(t Testing, log log.Logger, l1 derive.L1Fetcher,
 		eventSys:          sys,
 		log:               log,
 		Eng:               eng,
+		CLSync:            clSync,
 		engine:            ec,
 		derivationMetrics: metrics,
 		derivation:        pipeline,
@@ -459,7 +461,7 @@ func (s *L2Verifier) ActL2PipelineFull(t Testing) {
 // ActL2UnsafeGossipReceive creates an action that can receive an unsafe execution payload, like gossipsub
 func (s *L2Verifier) ActL2UnsafeGossipReceive(payload *eth.ExecutionPayloadEnvelope) Action {
 	return func(t Testing) {
-		s.synchronousEvents.Emit(t.Ctx(), clsync.ReceivedUnsafePayloadEvent{Envelope: payload})
+		s.CLSync.AddUnsafePayload(t.Ctx(), payload)
 	}
 }
 
