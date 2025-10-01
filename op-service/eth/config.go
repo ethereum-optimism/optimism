f5f2cdeadbf609d13a996bd1270fb8bd0ac5ba99
Josh Klopfenstein <joshklop@oplabs.co>
2025-10-01 14:51:48 -0700

    fix lints and tests

--- op-service/eth/config.go
+++ op-service/eth/config.go
@@ -1,22 +1,20 @@
 package eth
 
 import (
-	"fmt"
-
 	"github.com/ethereum/go-ethereum/params"
 )
 
-func L1ChainConfigByChainID(chainID ChainID) (*params.ChainConfig, error) {
+func L1ChainConfigByChainID(chainID ChainID) *params.ChainConfig {
 	switch chainID {
 	case ChainIDFromBig(params.MainnetChainConfig.ChainID):
-		return params.MainnetChainConfig, nil
+		return params.MainnetChainConfig
 	case ChainIDFromBig(params.SepoliaChainConfig.ChainID):
-		return params.SepoliaChainConfig, nil
+		return params.SepoliaChainConfig
 	case ChainIDFromBig(params.HoleskyChainConfig.ChainID):
-		return params.HoleskyChainConfig, nil
+		return params.HoleskyChainConfig
 	case ChainIDFromBig(params.HoodiChainConfig.ChainID):
-		return params.HoodiChainConfig, nil
+		return params.HoodiChainConfig
 	default:
-		return nil, fmt.Errorf("unknown chain ID: %v", chainID)
+		return nil
 	}
 }
