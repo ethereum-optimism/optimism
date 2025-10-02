91e55118b8701da4ae2da973caf6f18d74036b6e
Bilog WEB3 <155262265+Bilogweb3@users.noreply.github.com>
2025-10-02 15:43:00 +0200

    chore: remove unused CrossL2Inbox ABI loader

--- packages/contracts-bedrock/snapshots/abi_loader.go
+++ packages/contracts-bedrock/snapshots/abi_loader.go
@@ -28,9 +28,6 @@ var delayedWETH []byte
 //go:embed abi/SystemConfig.json
 var systemConfig []byte
 
-//go:embed abi/CrossL2Inbox.json
-var crossL2Inbox []byte
-
 func LoadDisputeGameFactoryABI() *abi.ABI {
 	return loadABI(disputeGameFactory)
 }
@@ -52,10 +49,6 @@ func LoadDelayedWETHABI() *abi.ABI {
 
 func LoadSystemConfigABI() *abi.ABI {
 	return loadABI(systemConfig)
-}
-
-func LoadCrossL2InboxABI() *abi.ABI {
-	return loadABI(crossL2Inbox)
 }
 
 func loadABI(json []byte) *abi.ABI {
