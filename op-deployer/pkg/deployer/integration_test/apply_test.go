02f8645dca15f165f17bb72673320ff83809341d
Samuel Stokes <sam.adam.stokes@gmail.com>
2025-10-06 13:33:29 -0400

    op-deployer: always run default_tagged_artifacts e2e test

--- op-deployer/pkg/deployer/integration_test/apply_test.go
+++ op-deployer/pkg/deployer/integration_test/apply_test.go
@@ -7,7 +7,6 @@ import (
 	"encoding/hex"
 	"log/slog"
 	"math/big"
-	"regexp"
 	"strings"
 	"testing"
 	"time"
@@ -145,7 +144,6 @@ func TestEndToEndBootstrapApply(t *testing.T) {
 
 	t.Run("default tagged artifacts", func(t *testing.T) {
 		op_e2e.InitParallel(t)
-		testutils.RunOnBranch(t, regexp.MustCompile(`^(backports/op-deployer|proposal/op-contracts)/*`))
 		apply(t, artifacts.DefaultL1ContractsLocator)
 	})
 
