773b47f5315d22b39eb149ec9c9e4006d489230e
geoknee <georgeknee@googlemail.com>
2025-10-10 14:18:55 +0100

    add 12s buffer

--- op-service/txmgr/txmgr.go
+++ op-service/txmgr/txmgr.go
@@ -364,8 +364,12 @@ func (m *SimpleTxManager) craftTx(ctx context.Context, candidate TxCandidate) (*
 		if candidate.To == nil {
 			return nil, errors.New("blob txs cannot deploy contracts")
 		}
-		// Use configuration to determine whether to enable cell proofs
-		useCellProofs := m.cfg.CellProofTime < uint64(time.Now().Unix())
+
+		// Use configuration to determine whether to enable cell proofs.
+		// We add a 12s buffer, because cell proofs are likely _not_
+		// supported before the Fusaka fork and legacy blob proofs
+		// may well be accepted after the Fusaka fork.
+		useCellProofs := m.cfg.CellProofTime+12 < uint64(time.Now().Unix())
 		m.l.Debug("crafting Blob transaction", "useCellProofs", useCellProofs)
 		if sidecar, blobHashes, err = MakeSidecar(candidate.Blobs, useCellProofs); err != nil {
 			return nil, fmt.Errorf("failed to make sidecar: %w", err)
