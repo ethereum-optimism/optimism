d7d1c430b6a63a5d831a79b06c03cf01ee85e4ce
Park Changwan <pcw109550@gmail.com>
2025-10-16 21:38:15 +0900

    op-acceptance-tests: ELP2P down but payload appendable till chain tip

--- op-acceptance-tests/tests/sync/elsync/gap_clp2p/sync_test.go
+++ op-acceptance-tests/tests/sync/elsync/gap_clp2p/sync_test.go
@@ -1,11 +1,14 @@
 package gap_clp2p
 
 import (
+	"fmt"
 	"testing"
+	"time"
 
 	"github.com/ethereum-optimism/optimism/op-devstack/devtest"
 	"github.com/ethereum-optimism/optimism/op-devstack/presets"
 	"github.com/ethereum-optimism/optimism/op-service/eth"
+	"github.com/ethereum-optimism/optimism/op-service/retry"
 	"github.com/ethereum-optimism/optimism/op-supervisor/supervisor/types"
 )
 
@@ -19,33 +22,59 @@ func TestSyncAfterInitialELSync(gt *testing.T) {
 	// batcher down so safe not advanced
 	require.Equal(uint64(0), sys.L2CL.HeadBlockRef(types.LocalSafe).Number)
 	require.Equal(uint64(0), sys.L2CLB.HeadBlockRef(types.LocalSafe).Number)
-	// verifier not advanced unsafe head
-	require.Equal(uint64(0), sys.L2CLB.HeadBlockRef(types.LocalUnsafe).Number)
+
+	startNum := sys.L2CLB.HeadBlockRef(types.LocalUnsafe).Number
 
 	// Finish EL sync by supplying the first block
-	// EL Sync finished because underlying EL has states to validate the payload for block 1
-	sys.L2CLB.SignalTarget(sys.L2EL, 1)
+	// EL Sync finished because underlying EL has states to validate the payload for block startNum+1
+	sys.L2CLB.SignalTarget(sys.L2EL, startNum+1)
 
-	// Send payloads for block 3, 4, 5, 7 which will fill in unsafe payload queue, block 2 missed
+	// Send payloads for block startNum+3, startNum+4, startNum+5, startNum+7 which will fill in unsafe payload queue, block startNum+2 missed
 	// Non-canonical payloads will be not sent to L2EL
 	// Order does not matter
-	for _, target := range []uint64{5, 3, 4, 7} {
+	for _, delta := range []uint64{5, 3, 4, 7} {
+		target := startNum + delta
 		sys.L2CLB.SignalTarget(sys.L2EL, target)
 		// Canonical unsafe head never advances because of the gap
-		require.Equal(uint64(1), sys.L2ELB.BlockRefByLabel(eth.Unsafe).Number)
+		require.Equal(startNum+1, sys.L2ELB.BlockRefByLabel(eth.Unsafe).Number)
 	}
 
-	// Send missing gap, payload 2, still not sending FCU since unsafe gap exists
-	sys.L2CLB.SignalTarget(sys.L2EL, 2)
+	// Send missing gap, payload startNum+2, still not sending FCU since unsafe gap exists
+	sys.L2CLB.SignalTarget(sys.L2EL, startNum+2)
 
 	retries := 2
-	// Gap filled and payload 2, 3, 4, 5 became canonical by relaying to ELB.
-	// Payload 7 is still in the unsafe payload queue because of unsafe gap
-	sys.L2ELB.Reached(eth.Unsafe, 5, retries)
+	// Gap filled and payload startNum+2, startNum+3, startNum+4, startNum+5 became canonical by relaying to ELB.
+	// Payload startNum+7 is still in the unsafe payload queue because of unsafe gap
+	sys.L2ELB.Reached(eth.Unsafe, startNum+5, retries)
 
-	// Send missing gap, payload 6
-	sys.L2CLB.SignalTarget(sys.L2EL, 6)
+	// Send missing gap, payload startNum+6
+	sys.L2CLB.SignalTarget(sys.L2EL, startNum+6)
 
-	// Gap filled and block 6, 7 became canonical by relaying to ELB.
-	sys.L2ELB.Reached(eth.Unsafe, 7, retries)
+	// Gap filled and block startNum+6, startNum+7 became canonical by relaying to ELB.
+	sys.L2ELB.Reached(eth.Unsafe, startNum+7, retries)
+}
+
+func TestReachUnsafeTipByAppendingUnsafePayload(gt *testing.T) {
+	t := devtest.SerialT(gt)
+	sys := presets.NewSingleChainMultiNodeWithoutCheck(t)
+	require := t.Require()
+	logger := t.Logger()
+	ctx := t.Ctx()
+
+	sys.L2CL.Advanced(types.LocalUnsafe, 7, 30)
+
+	trial := 0
+	require.NoError(
+		retry.Do0(ctx, 200, &retry.FixedStrategy{Dur: 250 * time.Millisecond}, func() error {
+			verUnsafe := sys.L2ELB.BlockRefByLabel(eth.Unsafe)
+			seqUnsafe := sys.L2EL.BlockRefByLabel(eth.Unsafe)
+			gap := seqUnsafe.Number - verUnsafe.Number
+			logger.Info("Filling in the gap by appending unsafe payload", "gap", gap, "ver", verUnsafe, "seq", seqUnsafe, "trial", trial)
+			if gap == 0 {
+				return nil
+			}
+			trial += 1
+			sys.L2CLB.SignalTarget(sys.L2EL, verUnsafe.Number+1)
+			return fmt.Errorf("gap yet filled: %d", gap)
+		}))
 }
