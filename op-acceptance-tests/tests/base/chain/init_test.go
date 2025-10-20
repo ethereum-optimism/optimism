12526747f7908c661e0b99baf8dc0b9e516ff088
geoknee <georgeknee@googlemail.com>
2025-10-20 18:17:36 +0100

    WIP

--- op-acceptance-tests/tests/base/chain/init_test.go
+++ op-acceptance-tests/tests/base/chain/init_test.go
@@ -8,6 +8,6 @@ import (
 
 func TestMain(m *testing.M) {
 	presets.DoMain(m,
-		presets.WithMinimal(),
+		presets.WithSingleChainMultiNode(),
 	)
 }
---
+++ op-acceptance-tests/tests/base/chain/unsafe_sync_test.go
@@ -0,0 +1,59 @@
+package chain
+
+import (
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/ethereum-optimism/optimism/op-devstack/devtest"
+	"github.com/ethereum-optimism/optimism/op-devstack/presets"
+	"github.com/ethereum-optimism/optimism/op-supervisor/supervisor/types"
+)
+
+// TestUnsafeSync tests that the verifier nodes are able to sync the unsafe head of the chain.
+// It does this by waiting for the sequencer to produce 10 unsafe blocks and requiring the verifier nodes to
+// be no more than 10 blocks behind the unsafe head.
+func TestUnsafeSync(gt *testing.T) {
+	t := devtest.SerialT(gt)
+	sys := presets.NewSingleChainMultiNode(t)
+
+	l2chainID := sys.L2Chain.Escape().ChainID()
+
+	const NUM_UNSAFE_BLOCKS = 20
+	const MAX_LAG = 10
+
+	t.Require().Greater(len(sys.L2Chain.L2ELNodes()), 1, "expected at least 2 L2 EL nodes")
+
+	indexOfSequencer := 0
+	for i, el := range sys.L2Chain.L2ELNodes() {
+		if strings.Contains(el.ID().String(), "sequencer") {
+			indexOfSequencer = i
+			break
+		}
+	}
+
+	initialUnsafeHead := sys.L2Chain.L2ELNodes()[0].ChainSyncStatus(sys.L2Chain.Escape().ChainID(), types.LocalUnsafe).Number
+
+	ticker := time.NewTicker(time.Duration(sys.L2Chain.Escape().RollupConfig().BlockTime) * time.Second / 10)
+	for range ticker.C {
+		sequencerUnsafeHead := sys.L2Chain.L2ELNodes()[indexOfSequencer].ChainSyncStatus(sys.L2Chain.Escape().ChainID(), types.LocalUnsafe)
+		for i, el := range sys.L2Chain.L2ELNodes() {
+			if i == indexOfSequencer {
+				continue
+			}
+			verifierUnsafeHead := el.ChainSyncStatus(l2chainID, types.LocalUnsafe)
+			lag := int64(verifierUnsafeHead.Number) - int64(sequencerUnsafeHead.Number)
+			if lag > 0 {
+				t.Require().Fail("verifier unsafe head is ahead of sequencer unsafe head")
+			}
+			if lag < 0 {
+				t.Require().Greater(lag, -MAX_LAG, "verifier (%s) unsafe head (number %d) is too far behind sequencer unsafe head (number %d)", el.ID(), verifierUnsafeHead.Number, sequencerUnsafeHead.Number)
+			}
+			t.Logf("verifier (%s) unsafe head (number %d) is %d blocks behind sequencer unsafe head (number %d)", el.ID(), verifierUnsafeHead.Number, lag, sequencerUnsafeHead.Number)
+		}
+		if sequencerUnsafeHead.Number >= initialUnsafeHead+NUM_UNSAFE_BLOCKS {
+			break
+		}
+	}
+	t.Logf("Sequencer unsafe head advanced %d blocks and %d verifier nodes stayed within 10 blocks of the unsafe head", initialUnsafeHead+NUM_UNSAFE_BLOCKS, len(sys.L2Chain.L2ELNodes()))
+}
