37be338dc8e36b86e53de1680afc938eea887dc5
Emilia Hane <elsaemiliaevahane@gmail.com>
2025-10-21 18:41:03 +0200

    perf(trie): feature gate `reth-optimism-trie` metrics (op-rs/op-reth#282)
    Closes https://github.com/op-rs/bin/issues/281
    
    - Feature gates `reth-optimism-trie` metrics
    - Moves cursor impls out of proofs module into new module `cursor`
    - Moves cursor factory impls into new module `cursor_factory`
    - Updates cursor factory impls to return cursor types with metrics
    wrapper if metrics feature is enabled

--- rust/op-reth/bin/src/main.rs
+++ rust/op-reth/bin/src/main.rs
@@ -10,7 +10,9 @@ use reth_optimism_cli::{chainspec::OpChainSpecParser, Cli};
 use reth_optimism_exex::OpProofsExEx;
 use reth_optimism_node::{args::RollupArgs, OpNode};
 use reth_optimism_rpc::eth::proofs::{EthApiExt, EthApiOverrideServer};
-use reth_optimism_trie::{db::MdbxProofsStorage, InMemoryProofsStorage, OpProofsStorage};
+use reth_optimism_trie::{
+    db::MdbxProofsStorage, InMemoryProofsStorage, OpProofsStorage, OpProofsStore, StorageMetrics,
+};
 use tracing::info;
 
 use std::{path::PathBuf, sync::Arc};
@@ -72,10 +74,10 @@ struct Args {
 async fn launch_node_with_storage<S>(
     builder: WithLaunchContext<NodeBuilder<Arc<DatabaseEnv>, OpChainSpec>>,
     args: Args,
-    storage: S,
+    storage: OpProofsStorage<S>,
 ) -> eyre::Result<(), ErrReport>
 where
-    S: OpProofsStorage + Clone + 'static,
+    S: OpProofsStore + Clone + 'static,
 {
     let storage_clone = storage.clone();
     let proofs_history_enabled = args.proofs_history;
@@ -110,7 +112,12 @@ fn main() {
         info!(target: "reth::cli", "Launching node");
 
         if args.proofs_history_storage_in_mem {
-            let storage = Arc::new(InMemoryProofsStorage::new());
+            // todo: enable launch without metrics
+            let storage = OpProofsStorage::new(
+                Arc::new(InMemoryProofsStorage::new()),
+                Arc::new(StorageMetrics::default()),
+            );
+
             launch_node_with_storage(builder, args.clone(), storage).await?;
         } else {
             let path = args
@@ -119,10 +126,15 @@ fn main() {
                 .expect("Path must be provided if not using in-memory storage");
             info!(target: "reth::cli", "Using on-disk storage for proofs history");
 
-            let storage = Arc::new(
-                MdbxProofsStorage::new(&path)
-                    .map_err(|e| eyre::eyre!("Failed to create MdbxProofsStorage: {e}"))?,
+            // todo: enable launch without metrics
+            let storage = OpProofsStorage::new(
+                Arc::new(
+                    MdbxProofsStorage::new(&path)
+                        .map_err(|e| eyre::eyre!("Failed to create MdbxProofsStorage: {e}"))?,
+                ),
+                Arc::new(StorageMetrics::default()),
             );
+
             launch_node_with_storage(builder, args.clone(), storage).await?;
         }
 
