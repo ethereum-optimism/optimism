4afb81d9a123d5206b98c4933074cccfed06fc91
einar-oplabs <oplabs@einar.io>
2025-10-21 09:53:36 +0200

    chore(exex): Add missing crate header to `reth-optimism-exex` (#271)
    Resolves #243
    
    ---------
    
    Co-authored-by: Einar Rasmussen <einar@oplabs.co>
    Co-authored-by: Emilia Hane <elsaemiliaevahane@gmail.com>

--- op-reth/crates/exex/src/lib.rs
+++ op-reth/crates/exex/src/lib.rs
@@ -1,5 +1,13 @@
 //! ExEx unique for OP-Reth. See also [`reth_exex`] for more op-reth execution extensions.
 
+#![doc(
+    html_logo_url = "https://raw.githubusercontent.com/paradigmxyz/reth/main/assets/reth-docs.png",
+    html_favicon_url = "https://avatars0.githubusercontent.com/u/97369466?s=256",
+    issue_tracker_base_url = "https://github.com/paradigmxyz/reth/issues/"
+)]
+#![cfg_attr(docsrs, feature(doc_cfg))]
+#![cfg_attr(not(test), warn(unused_crate_dependencies))]
+
 use derive_more::Constructor;
 use futures_util::TryStreamExt;
 use reth_chainspec::ChainInfo;
