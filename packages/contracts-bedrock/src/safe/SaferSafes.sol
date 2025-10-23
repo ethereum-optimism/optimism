e625629066a818f3105fb6231f5eefc48afc7c70
Kelvin Fichter <kelvinfichter@gmail.com>
2025-10-23 10:05:56 -0400

    feat: use a nonce for timelock configuration
    Puts timelock configuration behind a nonce, which allows us to
    clear the timelock configuration with an O(1) operation.

--- packages/contracts-bedrock/src/safe/SaferSafes.sol
+++ packages/contracts-bedrock/src/safe/SaferSafes.sol
@@ -44,7 +44,7 @@ contract SaferSafes is LivenessModule2, TimelockGuard, ISemver {
             return;
         }
 
-        uint256 timelockDelay = _safeState[_safe].timelockDelay;
+        uint256 timelockDelay = _currentSafeState(_safe).timelockDelay;
         uint256 livenessResponsePeriod = _livenessSafeConfiguration[_safe].livenessResponsePeriod;
 
         // If the timelock delay is 0, then the timelock guard is enabled but not configured.
