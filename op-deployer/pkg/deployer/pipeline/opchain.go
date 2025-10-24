d8d14aece073ff169d68097e4030da24a3060ad7
mbaxter <meredith@oplabs.co>
2025-10-24 17:37:04 -0400

    Set v2 contracts on chainstate

--- op-deployer/pkg/deployer/pipeline/opchain.go
+++ op-deployer/pkg/deployer/pipeline/opchain.go
@@ -39,8 +39,6 @@ func DeployOPChain(env *Env, intent *state.Intent, st *state.State, chainID comm
 		return fmt.Errorf("error deploying OP chain: %w", err)
 	}
 
-	st.Chains = append(st.Chains, makeChainState(chainID, dco))
-
 	readInput := opcm.ReadImplementationAddressesInput{
 		AddressManager:                    dco.AddressManager,
 		L1ERC721BridgeProxy:               dco.L1ERC721BridgeProxy,
@@ -62,6 +60,8 @@ func DeployOPChain(env *Env, intent *state.Intent, st *state.State, chainID comm
 	if err != nil {
 		return fmt.Errorf("failed to run ReadImplementationAddresses script: %w", err)
 	}
+
+	st.Chains = append(st.Chains, makeChainState(chainID, impls, dco))
 
 	st.ImplementationsDeployment.DelayedWethImpl = impls.DelayedWETH
 	st.ImplementationsDeployment.OptimismPortalImpl = impls.OptimismPortal
@@ -123,7 +123,7 @@ func makeDCI(intent *state.Intent, thisIntent *state.ChainIntent, chainID common
 	}, nil
 }
 
-func makeChainState(chainID common.Hash, dco opcm.DeployOPChainOutput) *state.ChainState {
+func makeChainState(chainID common.Hash, impls opcm.ReadImplementationAddressesOutput, dco opcm.DeployOPChainOutput) *state.ChainState {
 	opChainContracts := addresses.OpChainContracts{}
 	opChainContracts.OpChainProxyAdminImpl = dco.OpChainProxyAdmin
 	opChainContracts.AddressManagerImpl = dco.AddressManager
@@ -140,6 +140,13 @@ func makeChainState(chainID common.Hash, dco opcm.DeployOPChainOutput) *state.Ch
 	opChainContracts.PermissionedDisputeGameImpl = dco.PermissionedDisputeGame
 	opChainContracts.DelayedWethPermissionedGameProxy = dco.DelayedWETHPermissionedGameProxy
 	opChainContracts.DelayedWethPermissionlessGameProxy = dco.DelayedWETHPermissionlessGameProxy
+
+	if (impls.PermissionedDisputeGameV2 != common.Address{}) {
+		opChainContracts.PermissionedDisputeGameImpl = impls.PermissionedDisputeGameV2
+	}
+	if (impls.FaultDisputeGameV2 != common.Address{}) {
+		opChainContracts.FaultDisputeGameImpl = impls.FaultDisputeGameV2
+	}
 
 	return &state.ChainState{
 		ID:               chainID,
