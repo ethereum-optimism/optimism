64ca94db1bc39e337bf43db39a9abbe966d4b205
op-will <232669456+op-will@users.noreply.github.com>
2025-10-28 15:16:02 -0500

    Fixing broken test

--- op-devstack/sysgo/l2_metrics_dashboard_test.go
+++ op-devstack/sysgo/l2_metrics_dashboard_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/ethereum-optimism/optimism/op-devstack/stack"
+	"github.com/ethereum-optimism/optimism/op-service/eth"
 	"github.com/stretchr/testify/require"
 )
 
@@ -14,14 +15,11 @@ func TestWithL2MetricsDashboard_DefaultDisabled(t *testing.T) {
 }
 
 func TestWithL2MetricsDashboard_DisabledIfEndpointsRegisteredButNotExplicitlyEnabled(t *testing.T) {
+	id := stack.NewL2ELNodeID("test", eth.ChainIDFromUInt64(11111111111))
+	metricsTarget := NewPrometheusMetricsTarget("localhost", "9090", false)
 
 	o := &Orchestrator{}
-	o.RegisterL2MetricsTargets("test", PrometheusMetricsTarget{
-		host:              "localhost",
-		port:              "9090",
-		isLocal:           true,
-		isRunningInDocker: false,
-	})
+	o.RegisterL2MetricsTargets(id, metricsTarget)
 
 	// This should run without error if disabled
 	stack.ApplyOptionLifecycle(WithL2MetricsDashboard(), o)
