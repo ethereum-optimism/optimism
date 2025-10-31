45f6a39c90b4d8f160e200396e56715f03327dcd
Adrian Sutton <adrian@oplabs.co>
2025-10-31 13:18:16 +1000

    Don't need to use permissioned proposals.

--- op-acceptance-tests/tests/proofs/cannon/init_test.go
+++ op-acceptance-tests/tests/proofs/cannon/init_test.go
@@ -3,7 +3,6 @@ package cannon
 import (
 	"testing"
 
-	faultTypes "github.com/ethereum-optimism/optimism/op-challenger/game/fault/types"
 	"github.com/ethereum-optimism/optimism/op-devstack/compat"
 	"github.com/ethereum-optimism/optimism/op-devstack/presets"
 )
@@ -13,8 +12,6 @@ func TestMain(m *testing.M) {
 		presets.WithProofs(),
 		presets.WithJovianAtGenesis(),
 		presets.WithSafeDBEnabled(),
-		// Use a slow game type to be sure the anchor state doesn't update
-		presets.WithProposerGameType(faultTypes.PermissionedGameType),
 		// Requires access to a challenger config which only sysgo provides
 		// These tests would also be exceptionally slow on real L1s
 		presets.WithCompatibleTypes(compat.SysGo))
