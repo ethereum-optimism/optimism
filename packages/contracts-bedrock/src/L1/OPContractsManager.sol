9f6c3a31ec657bad716c5b5dddb96e9e451f2aa0
mbaxter <meredith@oplabs.co>
2025-11-03 14:13:52 -0500

    Bump opcm version again

--- packages/contracts-bedrock/src/L1/OPContractsManager.sol
+++ packages/contracts-bedrock/src/L1/OPContractsManager.sol
@@ -2208,9 +2208,9 @@ contract OPContractsManager is ISemver {
 
     // -------- Constants and Variables --------
 
-    /// @custom:semver 5.5.0
+    /// @custom:semver 5.6.0
     function version() public pure virtual returns (string memory) {
-        return "5.5.0";
+        return "5.6.0";
     }
 
     OPContractsManagerGameTypeAdder public immutable opcmGameTypeAdder;
