3d8dffc4539e95c4287c6ec9d89f0f16f202e445
blockchaindevsh <shore.cloud@gmail.com>
2025-11-09 10:40:20 +0800

    fix getAPIEndpoint

--- op-deployer/pkg/deployer/verify/etherscan.go
+++ op-deployer/pkg/deployer/verify/etherscan.go
@@ -40,9 +40,9 @@ func getAPIEndpoint(l1ChainID uint64) (string, error) {
 	case 1:
 		return "https://api.etherscan.io/v2/api", nil // eth-mainnet
 	case 11155111:
-		return "https://api-sepolia.etherscan.io/v2/api", nil // eth-sepolia
+		return "https://api.etherscan.io/v2/api", nil // eth-sepolia
 	case 84532:
-		return "https://api-sepolia.basescan.org/v2/api", nil // base-sepolia
+		return "https://api.basescan.org/v2/api", nil // base-sepolia
 	default:
 		return "", fmt.Errorf("unsupported L1 chain ID: %d", l1ChainID)
 	}
@@ -120,7 +120,7 @@ func (c *EtherscanClient) verifySourceCode(address common.Address, artifact *con
 		"constructorArguements": {constructorArgs},
 	}
 
-	req, err := http.NewRequest("POST", c.url, strings.NewReader(data.Encode()))
+	req, err := http.NewRequest("POST", fmt.Sprintf("%s?chainid=%d", c.url, c.chainID), strings.NewReader(data.Encode()))
 	if err != nil {
 		return "", fmt.Errorf("failed to create verification request: %w", err)
 	}
