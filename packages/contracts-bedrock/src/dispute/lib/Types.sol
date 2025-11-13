89c0d18ad9092109c4984517b31a4edb20c18c85
steven <steven.nevins.js@gmail.com>
2025-11-13 16:05:18 -0500

    feat: add types and errors for op-succinct contracts

--- packages/contracts-bedrock/src/dispute/lib/Types.sol
+++ packages/contracts-bedrock/src/dispute/lib/Types.sol
@@ -125,3 +125,22 @@ library LocalPreimageKey {
     /// @notice The identifier for the chain ID.
     uint256 internal constant CHAIN_ID = 0x05;
 }
+
+////////////////////////////////////////////////////////////////
+//            `OPSuccinctFaultDisputeGame` Types             //
+////////////////////////////////////////////////////////////////
+
+// The game type for the OP Succinct Fault Dispute Game.
+// Eventually will be enshrined in the game type enum.
+uint32 constant OP_SUCCINCT_FAULT_DISPUTE_GAME_TYPE = 42;
+
+/// @notice The public values committed to for an OP Succinct aggregation program.
+struct AggregationOutputs {
+    bytes32 l1Head;
+    bytes32 l2PreRoot;
+    bytes32 claimRoot;
+    uint256 claimBlockNum;
+    bytes32 rollupConfigHash;
+    bytes32 rangeVkeyCommitment;
+    address proverAddress;
+}
