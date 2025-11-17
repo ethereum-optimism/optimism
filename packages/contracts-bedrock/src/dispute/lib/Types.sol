21d293e650af3ed17c8580125c12bc09acdfe5ad
steven <steven.nevins.js@gmail.com>
2025-11-17 10:26:29 -0500

    fix: build error for types file

--- packages/contracts-bedrock/src/dispute/lib/Types.sol
+++ packages/contracts-bedrock/src/dispute/lib/Types.sol
@@ -130,8 +130,6 @@ library LocalPreimageKey {
 //            `OPSuccinctFaultDisputeGame` Types             //
 ////////////////////////////////////////////////////////////////
 
-/// The game type for the OP Succinct Fault Dispute Game.
-// Eventually will be enshrined in the game type enum.
 uint32 constant OP_SUCCINCT_FAULT_DISPUTE_GAME_TYPE = 42;
 
 /// @notice The public values committed to for an OP Succinct aggregation program.
