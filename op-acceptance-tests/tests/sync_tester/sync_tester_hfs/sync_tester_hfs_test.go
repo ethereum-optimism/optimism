4deb47190cf9b320bd85f664e4cea8c74dba6339
Park Changwan <pcw109550@gmail.com>
2025-11-17 22:07:08 +0900

    op-acceptance-tests: Include Jovian for Sync Tester HF tests

--- op-acceptance-tests/tests/sync_tester/sync_tester_hfs/sync_tester_hfs_test.go
+++ op-acceptance-tests/tests/sync_tester/sync_tester_hfs/sync_tester_hfs_test.go
@@ -15,20 +15,20 @@ func TestSyncTesterHardforks(gt *testing.T) {
 	sys := presets.NewSimpleWithSyncTester(t)
 	require := t.Require()
 
-	// Hardforks will be activated from Bedrock to Isthmus, 9 hardforks with 15 second time delta between.
-	// 15 * 9 = 135s, so we need at least 69 (135 / 2 + 1) L2 blocks with block time 2 to make the CL experience scheduled hardforks.
-	targetNum := 70
+	// Hardforks will be activated from Bedrock to Jovian, 10 hardforks with 6 second time delta between.
+	// 6 * 10 = 60s , so we need at least 30 (60 / 2 + 1) L2 blocks with block time 2 to make the CL experience scheduled hardforks.
+	targetNum := 32
 	dsl.CheckAll(t,
-		sys.L2CL.AdvancedFn(types.LocalUnsafe, uint64(targetNum), targetNum*2+10),
-		sys.L2CL2.AdvancedFn(types.LocalUnsafe, uint64(targetNum), targetNum*2+10),
+		sys.L2CL.AdvancedFn(types.LocalUnsafe, uint64(targetNum), targetNum+10),
+		sys.L2CL2.AdvancedFn(types.LocalUnsafe, uint64(targetNum), targetNum+10),
 	)
 
 	current := sys.L2CL2.HeadBlockRef(types.LocalUnsafe)
 
 	// Check the L2CL passed configured hardforks
-	isthmusTime := sys.L2Chain.Escape().ChainConfig().IsthmusTime
-	require.NotNil(isthmusTime, "isthmus must be activated")
-	require.Greater(current.Time, *isthmusTime, "must pass isthmus block")
+	jovianTime := sys.L2Chain.Escape().ChainConfig().JovianTime
+	require.NotNil(jovianTime, "jovian must be activated")
+	require.Greater(current.Time, *jovianTime, "must pass jovian block")
 	// Check block hash state from L2CL2 which was synced using the sync tester
 	require.Equal(sys.L2EL.BlockRefByNumber(current.Number).Hash, current.Hash, "hash mismatch")
 }
