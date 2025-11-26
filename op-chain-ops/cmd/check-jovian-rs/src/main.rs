bb070ce37618d1383a5d18fa83cbde1fc6e172e3
geoknee <georgeknee@googlemail.com>
2025-11-26 11:33:56 +0000

    Add CLI argument parsing with clap

--- op-chain-ops/cmd/check-jovian-rs/src/main.rs
+++ op-chain-ops/cmd/check-jovian-rs/src/main.rs
@@ -1,3 +1,17 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long, default_value = "http://localhost:9545")]
+    l2: String,
+
+    #[arg(short, long)]
+    secret_key: Option<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    println!("l2: {}", args.l2);
+    println!("secret_key: {:?}", args.secret_key);
 }
