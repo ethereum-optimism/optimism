03d4975dffb6f94f92cbfeeb81f64a790ab92008
geoknee <georgeknee@googlemail.com>
2025-12-03 20:45:00 +0000

    Add op-alloy deps and decode Jovian extra data

--- op-chain-ops/cmd/check-jovian-rs/src/block_header.rs
+++ op-chain-ops/cmd/check-jovian-rs/src/block_header.rs
@@ -1,5 +1,7 @@
 use alloy::{consensus::BlockHeader, network::Ethereum, providers::Provider};
-use log::info;
+use log::{info, warn};
+use op_alloy_consensus::decode_jovian_extra_data;
+
 use std::error::Error;
 
 pub async fn check<T: Provider<Ethereum>>(provider: &T) -> Result<(), Box<dyn Error>> {
@@ -10,17 +12,27 @@ pub async fn check<T: Provider<Ethereum>>(provider: &T) -> Result<(), Box<dyn Er
     let extra_data = block.header.extra_data();
     info!("Extra data: {:?}", extra_data);
 
-    // TODO check validity of extra data
-    //
+    match decode_jovian_extra_data(&extra_data) {
+        Ok(data) => {
+            info!(
+                "Decoded extra data: elasticity:{}, denominator:{}, minBaseFee:{}",
+                data.0, data.1, data.2
+            );
+        }
+        Err(err) => {
+            return Err(Box::new(err));
+        }
+    }
 
     match block.header.blob_gas_used() {
-        Some(data) => {
-            info!("Blob gas used: {}", data);
-            // if nonzero, success
-            // if zero, warn that it is inconclusive, or check transaction list
+        Some(0) => {
+            warn!("Zero Blob gas used, inconclusive");
+        }
+        Some(d) => {
+            info!("Nonzero Blob gas used: {}", d);
         }
         None => {
-            info!("No extra data");
+            return Err(Box::<dyn Error>::from("Nil blobGasUsed".to_string()));
         }
     }
 
