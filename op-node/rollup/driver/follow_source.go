4cd424764dde9a6b3588e3e63878d5b8a7a9e3cc
Park Changwan <pcw109550@gmail.com>
2025-12-06 03:20:45 +0900

    op-node: Follow Source: EL / CL support

--- op-node/rollup/driver/follow_source.go
+++ op-node/rollup/driver/follow_source.go
@@ -15,44 +15,55 @@ type L1FollowSource interface {
 	L1BlockRefByNumber(ctx context.Context, num uint64) (eth.L1BlockRef, error)
 }
 
-// L2FollowSource provides access to L2 block references for upstream following.
-type L2FollowSource interface {
-	L2BlockRefByLabel(ctx context.Context, label eth.BlockLabel) (eth.L2BlockRef, error)
-}
-
 // UpstreamFollowSource combines L1 and L2 follow sources.
 // L2 following may be optionally disabled.
 type UpstreamFollowSource interface {
-	L2FollowSource
 	L1FollowSource
 	CanFollowL2() bool
+	SafeL2(ctx context.Context) (eth.L2BlockRef, error)
+	FinalizedL2(ctx context.Context) (eth.L2BlockRef, error)
+	CurrentL1(ctx context.Context) (eth.L1BlockRef, error)
 }
 
-type L2ELFollowSource struct {
-	l2Source *sources.L2Client
+type L2FollowSource struct {
+	l2Source *sources.FollowClient
 	l1Source L1FollowSource
 }
 
-var _ UpstreamFollowSource = (*L2ELFollowSource)(nil)
+var _ UpstreamFollowSource = (*L2FollowSource)(nil)
 
-func NewL2ELFollowSource(client *sources.L2Client, l1Source L1FollowSource) *L2ELFollowSource {
+func NewL2FollowSource(client *sources.FollowClient, l1Source L1FollowSource) *L2FollowSource {
 	if l1Source == nil {
-		panic("NewL2ELFollowSource: l1Source must not be nil")
+		panic("NewL2FollowSource: l1Source must not be nil")
 	}
-	return &L2ELFollowSource{l2Source: client, l1Source: l1Source}
+	return &L2FollowSource{l2Source: client, l1Source: l1Source}
 }
 
-func (fs *L2ELFollowSource) CanFollowL2() bool {
+func (fs *L2FollowSource) CanFollowL2() bool {
 	return fs.l2Source != nil
 }
 
-func (fs *L2ELFollowSource) L2BlockRefByLabel(ctx context.Context, label eth.BlockLabel) (eth.L2BlockRef, error) {
+func (fs *L2FollowSource) SafeL2(ctx context.Context) (eth.L2BlockRef, error) {
 	if fs.l2Source == nil {
 		return eth.L2BlockRef{}, errL2FollowSourceNotEnabled
 	}
-	return fs.l2Source.L2BlockRefByLabel(ctx, label)
+	return fs.l2Source.SafeL2(ctx)
 }
 
-func (fs *L2ELFollowSource) L1BlockRefByNumber(ctx context.Context, num uint64) (eth.L1BlockRef, error) {
+func (fs *L2FollowSource) FinalizedL2(ctx context.Context) (eth.L2BlockRef, error) {
+	if fs.l2Source == nil {
+		return eth.L2BlockRef{}, errL2FollowSourceNotEnabled
+	}
+	return fs.l2Source.FinalizedL2(ctx)
+}
+
+func (fs *L2FollowSource) CurrentL1(ctx context.Context) (eth.L1BlockRef, error) {
+	if fs.l2Source == nil {
+		return eth.L1BlockRef{}, errL2FollowSourceNotEnabled
+	}
+	return fs.l2Source.CurrentL1(ctx)
+}
+
+func (fs *L2FollowSource) L1BlockRefByNumber(ctx context.Context, num uint64) (eth.L1BlockRef, error) {
 	return fs.l1Source.L1BlockRefByNumber(ctx, num)
 }
