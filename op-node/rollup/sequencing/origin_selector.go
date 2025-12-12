08dd4d3332e9d549c505e12a6b465e400565b5ef
geoknee <georgeknee@googlemail.com>
2025-12-12 15:39:47 +0000

    move new pure function into a method on los

--- op-node/rollup/sequencing/origin_selector.go
+++ op-node/rollup/sequencing/origin_selector.go
@@ -3,6 +3,7 @@ package sequencing
 import (
 	"context"
 	"errors"
+	"fmt"
 	"sync"
 	"sync/atomic"
 	"time"
@@ -78,7 +79,7 @@ func (los *L1OriginSelector) FindL1Origin(ctx context.Context, l2Head eth.L2Bloc
 		return eth.L1BlockRef{}, err
 	}
 	// Try to find the L1 origin given the current data in cache
-	o, err := FindL1OriginOfNextL2Block(los.cfg,
+	o, err := los.findL1OriginOfNextL2Block(
 		l2Head,
 		currentOrigin,
 		nextOrigin,
@@ -92,7 +93,7 @@ func (los *L1OriginSelector) FindL1Origin(ctx context.Context, l2Head eth.L2Bloc
 			// If we got the origin, or we are in recover mode and the origin is not found
 			// (because we recovered the l1 origin up to the l1 tip)
 			// try again with matchAutoDerivation = false.
-			return FindL1OriginOfNextL2Block(los.cfg,
+			return los.findL1OriginOfNextL2Block(
 				l2Head,
 				currentOrigin,
 				nextOrigin,
@@ -204,4 +205,64 @@ func (los *L1OriginSelector) reset() {
 
 	los.currentOrigin = eth.L1BlockRef{}
 	los.nextOrigin = eth.L1BlockRef{}
+}
+
+var (
+	ErrInvalidL1Origin      = fmt.Errorf("origin-selector: currentL1Origin.Hash != l2Head.L1Origin.Hash")
+	ErrNextL1OriginOrphaned = fmt.Errorf("origin-selector: nextL1Origin.ParentHash != currentL1Origin.Hash")
+	ErrNextL1OriginRequired = fmt.Errorf("origin-selector: nextL1Origin not supplied but required to satisfy constraints")
+)
+
+// FindL1OriginOfNextL2Block finds the L1 origin of the next L2 block.
+// It returns an error if there is no way to build a block satisfying
+// derivation constraints with the supplied data.
+// You can pass a nil pointer for l1OriginChild if it is not yet available
+// removing the need for block building to wait on the result of network calls
+// This method is designed to be pure (it only reads the cfg property of the receiver)
+// and should not have any side effects.
+func (los *L1OriginSelector) findL1OriginOfNextL2Block(
+	l2Head eth.L2BlockRef,
+	currentL1Origin eth.L1BlockRef, nextL1Origin eth.L1BlockRef,
+	matchAutoDerivation bool) (eth.L1BlockRef, error) {
+
+	if (currentL1Origin == eth.L1BlockRef{}) {
+		panic("origin-selector: currentl1Origin is nil")
+	}
+	if l2Head.L1Origin.Hash != currentL1Origin.Hash {
+		return eth.L1BlockRef{}, ErrInvalidL1Origin
+	}
+	if (nextL1Origin != eth.L1BlockRef{} && nextL1Origin.ParentHash != currentL1Origin.Hash) {
+		return eth.L1BlockRef{}, ErrNextL1OriginOrphaned
+	}
+
+	l2BlockTime := los.cfg.BlockTime
+	maxDrift := rollup.NewChainSpec(los.cfg).MaxSequencerDrift(currentL1Origin.Time)
+	nextL2BlockTime := l2Head.Time + l2BlockTime
+	driftCurrent := nextL2BlockTime - currentL1Origin.Time
+
+	if (nextL1Origin == eth.L1BlockRef{}) {
+		if matchAutoDerivation {
+			// This can cause unsafe block production to slow to the rate of L1 block production, if the L1 origin is caught up to the L1 Head.
+			// Code higher up the call stack should ensure that matchAutoDerivation is false under such conditions.
+			return eth.L1BlockRef{}, ErrNextL1OriginRequired
+		} else {
+			// If we don't yet have the nextL1Origin, stick with the current L1 origin unless doing so would exceed the maximum drift.
+			if driftCurrent > maxDrift {
+				// Return an error so the caller knows it needs to fetch the next l1 origin now.
+				return eth.L1BlockRef{}, fmt.Errorf("%w: drift of next L2 block would exceed maximum %d unless nextl1Origin is adopted", ErrNextL1OriginRequired, maxDrift)
+			}
+			return currentL1Origin, nil
+		}
+	}
+
+	driftNext := int64(nextL2BlockTime) - int64(nextL1Origin.Time)
+
+	// Progress to l1OriginChild if doing so would respect the requirement
+	// that L2 blocks cannot point to a future L1 block (negative drift).
+	if driftNext >= 0 {
+		return nextL1Origin, nil
+	} else {
+		// If we cannot adopt the l1OriginChild, use the current l1 origin.
+		return currentL1Origin, nil
+	}
 }
--- op-node/rollup/sequencing/origin_selector_test.go
+++ op-node/rollup/sequencing/origin_selector_test.go
@@ -810,3 +810,194 @@ func TestOriginSelectorMiscEvent(t *testing.T) {
 	handled := s.OnEvent(context.Background(), rollup.L1TemporaryErrorEvent{})
 	require.False(t, handled)
 }
+
+func TestFindL1OriginOfNextL2Block(t *testing.T) {
+	cfg := &rollup.Config{
+		MaxSequencerDrift: 1800, // Use Fjord constant value
+		BlockTime:         2,
+	}
+
+	los := NewL1OriginSelector(context.Background(), testlog.Logger(t, log.LevelDebug), cfg, &testutils.MockL1Source{})
+
+	require.Panics(t, func() {
+		_, _ = los.findL1OriginOfNextL2Block(
+			eth.L2BlockRef{},
+			eth.L1BlockRef{},
+			eth.L1BlockRef{},
+			false)
+	})
+
+	type testCase struct {
+		name                string
+		l2Head              eth.L2BlockRef
+		currentL1Origin     eth.L1BlockRef
+		nextL1Origin        eth.L1BlockRef
+		matchAutoderivation bool
+		expectedResult      eth.L1BlockRef
+		expectedError       error
+	}
+
+	tcs := []testCase{}
+
+	// Scenarios with valid data, no drift concerns
+	// but availability of next l1 origin is modulated.
+	//
+	// L1 chain: a100(1200) <- [ a101(1212) ]
+	//            /\
+	// L2 chain    \_ b1000(1220)
+	a100 := eth.L1BlockRef{
+		Number: 100,
+		Hash:   common.Hash{'a', '1', '0', '0'},
+		Time:   1200,
+	}
+	a101 := eth.L1BlockRef{
+		Number:     101,
+		ParentHash: a100.Hash,
+		Hash:       common.Hash{'a', '0', '0'},
+		Time:       1212,
+	}
+	b1000 := eth.L2BlockRef{
+		Number:   1000,
+		Hash:     common.Hash{'b', '1', '0', '0', '0'},
+		L1Origin: a100.ID(),
+		Time:     1220,
+	}
+
+	tcs = append(tcs,
+		testCase{
+			name:            "normal operation, progress because we can",
+			l2Head:          b1000,
+			currentL1Origin: a100,
+			nextL1Origin:    a101,
+			expectedResult:  a101,
+		},
+		testCase{
+			name:                "recover mode, progress because we can",
+			l2Head:              b1000,
+			currentL1Origin:     a100,
+			nextL1Origin:        a101,
+			expectedResult:      a101,
+			matchAutoderivation: true,
+		},
+		testCase{
+			name:            "normal operation, don't need to progress",
+			l2Head:          b1000,
+			currentL1Origin: a100,
+			expectedResult:  a100,
+		},
+		testCase{
+			name:                "recover mode, need to progress but can't",
+			l2Head:              b1000,
+			currentL1Origin:     a100,
+			matchAutoderivation: true,
+			expectedError:       ErrNextL1OriginRequired,
+		},
+	)
+
+	// Bad input data / reorg scenarios
+	// L1 chain: c100(1200) <-[x]- c101(1212)
+	//            /\
+	// L2 chain    \_[x]- d/e1000(1220)
+	c100 := eth.L1BlockRef{
+		Number: 100,
+		Hash:   common.Hash{'a', '1', '0', '0'},
+		Time:   1200,
+	}
+	c101 := eth.L1BlockRef{
+		Number:     101,
+		ParentHash: common.Hash{}, // does not point to c100
+		Hash:       common.Hash{'a', '0', '0'},
+		Time:       1212,
+	}
+	d1000 := eth.L2BlockRef{
+		Number:   1000,
+		L1Origin: c100.ID(),
+		Hash:     common.Hash{'d', '1', '0', '0', '0'},
+		Time:     1220,
+	}
+	e1000 := eth.L2BlockRef{
+		Number:   1000,
+		L1Origin: eth.BlockID{}, // does not point to c100
+		Hash:     common.Hash{'d', '1', '0', '0', '0'},
+		Time:     1220,
+	}
+	tcs = append(tcs,
+		testCase{
+			name:            "L1 reorg",
+			currentL1Origin: c100,
+			nextL1Origin:    c101,
+			l2Head:          d1000,
+			expectedError:   ErrNextL1OriginOrphaned,
+		},
+		testCase{
+			name:            "Invalid l1 origin",
+			currentL1Origin: c100,
+			nextL1Origin:    c101,
+			l2Head:          e1000,
+			expectedError:   ErrInvalidL1Origin,
+		})
+
+	// Drift at maximum,
+	// L1 chain: a100(1200) <- [ a101(1212) ]
+	//            /\
+	// L2 chain    \_ f1000(3000)
+	f1000 := eth.L2BlockRef{
+		Number:   1000,
+		L1Origin: a100.ID(),
+		Hash:     common.Hash{'f', '1', '0', '0', '0'},
+		Time:     3000,
+	}
+	tcs = append(tcs,
+		testCase{
+			name:            "Drift at maximum, nextL1Origin available",
+			currentL1Origin: a100,
+			nextL1Origin:    a101,
+			l2Head:          f1000,
+			expectedResult:  a101,
+		},
+		testCase{
+			name:            "Drift at maximum, nextL1Origin unavailable",
+			currentL1Origin: a100,
+			l2Head:          f1000,
+			expectedError:   ErrNextL1OriginRequired,
+		})
+
+	// Negative drift,
+	// L1 chain: a100(1200) <- a101(1212)
+	//            /\
+	// L2 chain    \_ g1000(1200)
+	// Current drift is 0
+	// adopting the nextLOrigin would make it negative (add 2 subtract 12)
+	g1000 := eth.L2BlockRef{
+		Number:   1000,
+		L1Origin: a100.ID(),
+		Hash:     common.Hash{'g', '1', '0', '0', '0'},
+		Time:     1200,
+	}
+	tcs = append(tcs,
+		testCase{
+			name:            "Negative drift",
+			currentL1Origin: a100,
+			nextL1Origin:    a101,
+			l2Head:          g1000,
+			expectedResult:  a100,
+		})
+
+	for _, tc := range tcs {
+		t.Run(tc.name, func(t *testing.T) {
+			result, err := los.findL1OriginOfNextL2Block(
+				tc.l2Head,
+				tc.currentL1Origin,
+				tc.nextL1Origin,
+				tc.matchAutoderivation)
+			if tc.expectedError != nil {
+				require.ErrorIs(t, err, tc.expectedError)
+			} else {
+				require.NoError(t, err)
+			}
+			if result != tc.expectedResult {
+				t.Errorf("expected result %v, got %v", tc.expectedResult, result)
+			}
+		})
+	}
+}
