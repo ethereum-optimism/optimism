d7b6a5e5e906fc554448917d4ef2ab24f6dbaa27
steven <steven.nevins.js@gmail.com>
2025-12-15 15:59:20 -0500

    fix: undeclared identifier error

--- packages/contracts-bedrock/test/dispute/AnchorStateRegistry.t.sol
+++ packages/contracts-bedrock/test/dispute/AnchorStateRegistry.t.sol
@@ -466,7 +466,9 @@ contract AnchorStateRegistry_GetStartingAnchorRoot_Test is AnchorStateRegistry_T
 
         // Mock the game's anchor root to be different from the starting anchor root.
         vm.mockCall(
-            address(gameProxy), abi.encodeCall(IDisputeGame.rootClaim, ()), abi.encode(Claim.wrap(keccak256(abi.encode(123))))
+            address(gameProxy),
+            abi.encodeCall(IDisputeGame.rootClaim, ()),
+            abi.encode(Claim.wrap(keccak256(abi.encode(123))))
         );
 
         // Set the anchor game to the game proxy.
