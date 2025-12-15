416feae4f6ae078a8d7bb554ea82abef32d14c15
IamFlux <175354924+0xiamflux@users.noreply.github.com>
2025-12-15 11:10:12 -0500

    chore: pre-pr ready (#758)

--- packages/contracts-bedrock/src/L1/OPContractsManager.sol
+++ packages/contracts-bedrock/src/L1/OPContractsManager.sol
@@ -1977,9 +1977,9 @@ contract OPContractsManager is ISemver {
 
     // -------- Constants and Variables --------
 
-    /// @custom:semver 6.0.0
+    /// @custom:semver 6.0.1
     function version() public pure virtual returns (string memory) {
-        return "6.0.0";
+        return "6.0.1";
     }
 
     OPContractsManagerGameTypeAdder public immutable opcmGameTypeAdder;
