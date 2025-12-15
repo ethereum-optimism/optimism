3bfc87f8a84e84629acede78e229788e5abdfa26
steven <steven.nevins.js@gmail.com>
2025-12-15 16:18:23 -0500

    fix: interfaces

--- packages/contracts-bedrock/interfaces/dispute/IPermissionedDisputeGame.sol
+++ packages/contracts-bedrock/interfaces/dispute/IPermissionedDisputeGame.sol
@@ -63,6 +63,7 @@ interface IPermissionedDisputeGame is IDisputeGame {
     error UnexpectedList();
     error UnexpectedRootClaim(Claim rootClaim);
     error UnexpectedString();
+    error UnknownChainId();
     error ValidStep();
     error InvalidBondDistributionMode();
     error GameNotFinalized();
@@ -111,7 +112,7 @@ interface IPermissionedDisputeGame is IDisputeGame {
     function move(Claim _disputed, uint256 _challengeIndex, Claim _claim, bool _isAttack) external payable;
     function normalModeCredit(address) external view returns (uint256);
     function refundModeCredit(address) external view returns (uint256);
-    function rootClaimByChainId(uint256) external pure returns (Claim rootClaim_);
+    function rootClaimByChainId(uint256 _chainId) external view returns (Claim rootClaim_);
     function resolutionCheckpoints(uint256)
         external
         view
