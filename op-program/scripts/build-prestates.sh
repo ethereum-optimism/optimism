2241d4431ddfb01c9b1d53f17d76a7b52cda36cf
inphi <mlaw2501@gmail.com>
2025-12-17 15:30:05 -0500

    log disk usage

--- op-program/scripts/build-prestates.sh
+++ op-program/scripts/build-prestates.sh
@@ -69,15 +69,6 @@ function build_kona_prestate() {
 
 EXCLUDED_OP_PROGRAM_VERSIONS=(
   "op-program/v0.0.1"
-  "op-program/v0.1.0"
-  "op-program/v0.3.0"
-  "op-program/v0.3.1"
-  "op-program/v1.0.0"
-  "op-program/v1.0.1"
-  "op-program/v1.2.0"
-  "op-program/v1.3.0-rc.1"
-  "op-program/v1.3.0-rc.2"
-  "op-program/v1.3.0-rc.3"
 )
 printf "%s\n" "${EXCLUDED_OP_PROGRAM_VERSIONS[@]}" > excluded_op_program_versions.txt
 
@@ -160,8 +151,6 @@ EXCLUDED=(
   "kona-client/v1.1.0-rc.1"
   "kona-client/v1.1.0-rc.3"
   "kona-client/v1.1.3"
-  "kona-client/v1.1.6"
-  "kona-client/v1.1.7"
 )
 printf "%s\n" "${EXCLUDED[@]}" > excluded.txt
 
