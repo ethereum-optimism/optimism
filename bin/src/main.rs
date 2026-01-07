4aed255a7ff022d7855ed890391c499e0a916afd
Alexey Shekhirin <github@shekhirin.com>
2026-01-07 19:36:08 +0000

    feat(metrics): jemalloc heap dump endpoint (paradigmxyz/reth#20811)

--- bin/src/main.rs
+++ bin/src/main.rs
@@ -8,6 +8,10 @@ use tracing::info;
 #[global_allocator]
 static ALLOC: reth_cli_util::allocator::Allocator = reth_cli_util::allocator::new_allocator();
 
+#[cfg(all(feature = "jemalloc-prof", unix))]
+#[unsafe(export_name = "_rjem_malloc_conf")]
+static MALLOC_CONF: &[u8] = b"prof:true,prof_active:true,lg_prof_sample:19\0";
+
 fn main() {
     reth_cli_util::sigsegv_handler::install();
 
