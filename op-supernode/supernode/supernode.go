8a3b39a117e55fa1db98baa6eaca03b5a3b6c37d
geoknee <georgeknee@googlemail.com>
2026-01-15 14:05:49 +0000

    temp: Add Rewind RPC activity for chains
    Expose RPC namespace "rewind" with ChainTo(chainID, timestamp) which
    invokes ChainContainer.RewindEngine to rewind a chain to a timestamp.
    Register the new activity in the supernode activities list.

--- op-supernode/supernode/supernode.go
+++ op-supernode/supernode/supernode.go
@@ -17,6 +17,7 @@ import (
 	"github.com/ethereum-optimism/optimism/op-service/sources"
 	"github.com/ethereum-optimism/optimism/op-supernode/supernode/activity"
 	"github.com/ethereum-optimism/optimism/op-supernode/supernode/activity/heartbeat"
+	"github.com/ethereum-optimism/optimism/op-supernode/supernode/activity/rewind-chains"
 	"github.com/ethereum-optimism/optimism/op-supernode/supernode/activity/superroot"
 	cc "github.com/ethereum-optimism/optimism/op-supernode/supernode/chain_container"
 	"github.com/ethereum-optimism/optimism/op-supernode/supernode/resources"
@@ -86,6 +87,7 @@ func New(ctx context.Context, log gethlog.Logger, version string, requestStop co
 	s.activities = []activity.Activity{
 		heartbeat.New(log.New("activity", "heartbeat"), 10*time.Second),
 		superroot.New(log.New("activity", "superroot"), s.chains),
+		rewind.New(log.New("activity", "rewind"), s.chains),
 	}
 	addr := net.JoinHostPort(cfg.RPCConfig.ListenAddr, strconv.Itoa(cfg.RPCConfig.ListenPort))
 	s.httpServer = httputil.NewHTTPServer(addr, s.rpcRouter)
