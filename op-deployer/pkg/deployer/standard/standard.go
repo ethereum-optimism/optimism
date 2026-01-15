57e90f96cc640e6ef57a2b4f065890c4a906a003
Maurelian <john@oplabs.co>
2026-01-15 15:01:35 -0500

    feat(op-deployer): Update for op-contracts/v6.0.0-rc.2 (#18809)
    * feat(op-deployer): Update for op-contracts/v6.0.0-rc.2
    
    * fix registry hash

--- op-deployer/pkg/deployer/standard/standard.go
+++ op-deployer/pkg/deployer/standard/standard.go
@@ -43,7 +43,7 @@ const (
 	ContractsV400Tag        = "op-contracts/v4.0.0-rc.7"
 	ContractsV410Tag        = "op-contracts/v4.1.0"
 	ContractsV500Tag        = "op-contracts/v5.0.0"
-	ContractsV600Tag        = "op-contracts/v6.0.0-rc.1"
+	ContractsV600Tag        = "op-contracts/v6.0.0-rc.2"
 	CurrentTag              = ContractsV600Tag
 )
 
