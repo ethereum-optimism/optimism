7a9d7f4a35233628c5835b094963e4d4903f8e43
niha <205694301+0xniha@users.noreply.github.com>
2026-01-21 15:49:11 -0300

    refactor: standarize opcmv2 version check
    * refactor: replace dev feature with opcm version check in DeployOPChain script
    
    * refactor: replace dev feature with opcm version check in ReadImplementationAddresses script
    
    * refactor: replace dev feature with opcm version check in UpgradeOPChain script
    
    * refactor: replace dev feature with opcm version check in UpgradeSuperchainConfig script
    
    * refactor: standarize isOPCMv2 variable
    
    * feat: add opcmv2 version constant
    
    * fix: rename constant to OPCM_V2_MIN_VERSION

--- packages/contracts-bedrock/scripts/deploy/ReadSuperchainDeployment.s.sol
+++ packages/contracts-bedrock/scripts/deploy/ReadSuperchainDeployment.s.sol
@@ -10,6 +10,7 @@ import { IProxy } from "interfaces/universal/IProxy.sol";
 import { IOPContractsManager } from "interfaces/L1/IOPContractsManager.sol";
 import { EIP1967Helper } from "test/mocks/EIP1967Helper.sol";
 import { SemverComp } from "src/libraries/SemverComp.sol";
+import { Constants } from "src/libraries/Constants.sol";
 
 contract ReadSuperchainDeployment is Script {
     struct Input {
@@ -40,7 +41,7 @@ contract ReadSuperchainDeployment is Script {
             isOPCMV2 = true;
         } else {
             require(address(opcm).code.length > 0, "ReadSuperchainDeployment: OPCM address has no code");
-            isOPCMV2 = SemverComp.gte(opcm.version(), "7.0.0");
+            isOPCMV2 = SemverComp.gte(opcm.version(), Constants.OPCM_V2_MIN_VERSION);
         }
 
         if (isOPCMV2) {
