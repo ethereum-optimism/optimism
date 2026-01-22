16e575a8c207ab64955828682d3551e86dec0ac7
axelKingsley <axel.kingsley@gmail.com>
2026-01-22 09:53:40 -0600

    only create interop activity if enabled

--- op-supernode/supernode/supernode.go
+++ op-supernode/supernode/supernode.go
@@ -84,21 +84,23 @@ func New(ctx context.Context, log gethlog.Logger, version string, requestStop co
 		s.chains[chainID] = cc.NewChainContainer(chainID, vnCfgs[chainID], log, *cfg, initOverrides, nil, s.rpcRouter.SetHandler, s.metricsFanIn.SetMetricsRegistry)
 	}
 
-	// Initialize activities
-	interopActivationTimestamp := cfg.RawCtx.Uint64(interop.InteropActivationTimestampFlag.Name)
-	interopActivity := interop.New(log.New("activity", "interop"), interopActivationTimestamp, s.chains, cfg.DataDir, s.l1Client)
+	// Initialize general activities
 	s.activities = []activity.Activity{
-		interopActivity,
 		heartbeat.New(log.New("activity", "heartbeat"), 10*time.Second),
 		superroot.New(log.New("activity", "superroot"), s.chains),
 	}
 
-	// Cross-register verification activities with chains
-	for _, chain := range s.chains {
-		chain.RegisterVerifier(interopActivity)
+	// Initialize interop activity if the activation timestamp is set
+	if cfg.RawCtx.IsSet(interop.InteropActivationTimestampFlag.Name) {
+		interopActivationTimestamp := cfg.RawCtx.Uint64(interop.InteropActivationTimestampFlag.Name)
+		interopActivity := interop.New(log.New("activity", "interop"), interopActivationTimestamp, s.chains, cfg.DataDir, s.l1Client)
+		s.activities = append(s.activities, interopActivity)
+		for _, chain := range s.chains {
+			chain.RegisterVerifier(interopActivity)
+		}
 	}
 
-	// set up http server
+	// Set up http server
 	addr := net.JoinHostPort(cfg.RPCConfig.ListenAddr, strconv.Itoa(cfg.RPCConfig.ListenPort))
 	s.httpServer = httputil.NewHTTPServer(addr, s.rpcRouter)
 
