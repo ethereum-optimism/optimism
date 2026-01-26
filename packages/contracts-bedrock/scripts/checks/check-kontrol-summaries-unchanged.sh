fd374278c6fb09efee02e89e7c8f20a33b58074e
Michael Amadi <amadimichaeld@gmail.com>
2026-01-26 16:01:13 +0100

    fix checks and tests

--- packages/contracts-bedrock/scripts/checks/check-kontrol-summaries-unchanged.sh
+++ packages/contracts-bedrock/scripts/checks/check-kontrol-summaries-unchanged.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 # Runs the file unchanged script for each of the Kontrol summary files.
 # Update these hashes if you have changed the summary files deliberately.
 # Use `openssl dgst -sha256` to generate the hash for a file.
-./scripts/checks/check-file-unchanged.sh ./test/kontrol/proofs/utils/DeploymentSummary.sol 73e1ed1f3c8bd9e46bb348b774443458206b2399e4eac303e2a92ac987eeefc6
-./scripts/checks/check-file-unchanged.sh ./test/kontrol/proofs/utils/DeploymentSummaryCode.sol ffe7298024464a6a282ceb549db401cce4b36fe3342113250a6c674a3b4203ba
-./scripts/checks/check-file-unchanged.sh ./test/kontrol/proofs/utils/DeploymentSummaryFaultProofs.sol 5ec8b19a00b47f15a2a8c8d632ab83933e19b0c2bbb04c905a2cda926e56038f
-./scripts/checks/check-file-unchanged.sh ./test/kontrol/proofs/utils/DeploymentSummaryFaultProofsCode.sol 1215a561a2bffb5af62b532a2608c4048a22368e505f7ae1d237c16b4ef6a45e
+./scripts/checks/check-file-unchanged.sh ./test/kontrol/proofs/utils/DeploymentSummary.sol cb2f1d7ff9a6fb878328650114af6a6864ed6e353e1d031b5704dee75270a4b5
+./scripts/checks/check-file-unchanged.sh ./test/kontrol/proofs/utils/DeploymentSummaryCode.sol 8cef371441261e54161645036717d3ee21f66bd638a961da698c6858250e41bc
+./scripts/checks/check-file-unchanged.sh ./test/kontrol/proofs/utils/DeploymentSummaryFaultProofs.sol 80e9caac4deba370d01dd642401ec2e8145a1e2ae981b7e423d4de188266f87b
+./scripts/checks/check-file-unchanged.sh ./test/kontrol/proofs/utils/DeploymentSummaryFaultProofsCode.sol c4379d2232fab24bea2f55d25cef5de4d0629b8e04592f074f680a3141861d9e
