e4244553d453e9bba888b531d1bbac942fb711b2
theochap <theodore.chap@gmail.com>
2026-01-28 15:04:19 -0500

    chore(tests): restructure integration testing

---
+++ kona/tests/integration/derivation/mod.rs
@@ -0,0 +1,4 @@
+//! Derivation tests for kona written in Rust.
+
+/// Test re-export of the derivation pipeline.
+pub use kona_derive::DerivationPipeline;
---
+++ kona/tests/integration/mod.rs
@@ -0,0 +1,3 @@
+//! Integration tests for kona written in Rust. Contains derivation tests.
+
+mod derivation;
