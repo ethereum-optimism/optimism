8ae40af15d80beeb1bc25b6dcb13fafdd15a58c5
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2026-02-01 21:43:25 +0100

    refactor: add with_* compressor utility methods (paradigmxyz/reth#21680)

--- primitives/src/transaction/signed.rs
+++ primitives/src/transaction/signed.rs
@@ -435,19 +435,11 @@ impl reth_codecs::Compact for OpTransactionSigned {
 
         let tx_bits = if zstd_bit {
             let mut tmp = Vec::with_capacity(256);
-            if cfg!(feature = "std") {
-                reth_zstd_compressors::TRANSACTION_COMPRESSOR.with(|compressor| {
-                    let mut compressor = compressor.borrow_mut();
-                    let tx_bits = self.transaction.to_compact(&mut tmp);
-                    buf.put_slice(&compressor.compress(&tmp).expect("Failed to compress"));
-                    tx_bits as u8
-                })
-            } else {
-                let mut compressor = reth_zstd_compressors::create_tx_compressor();
+            reth_zstd_compressors::with_tx_compressor(|compressor| {
                 let tx_bits = self.transaction.to_compact(&mut tmp);
                 buf.put_slice(&compressor.compress(&tmp).expect("Failed to compress"));
                 tx_bits as u8
-            }
+            })
         } else {
             self.transaction.to_compact(buf) as u8
         };
@@ -469,29 +461,15 @@ impl reth_codecs::Compact for OpTransactionSigned {
 
         let zstd_bit = bitflags >> 3;
         let (transaction, buf) = if zstd_bit != 0 {
-            if cfg!(feature = "std") {
-                reth_zstd_compressors::TRANSACTION_DECOMPRESSOR.with(|decompressor| {
-                    let mut decompressor = decompressor.borrow_mut();
-
-                    // TODO: enforce that zstd is only present at a "top" level type
-                    let transaction_type = (bitflags & 0b110) >> 1;
-                    let (transaction, _) = OpTypedTransaction::from_compact(
-                        decompressor.decompress(buf),
-                        transaction_type,
-                    );
-
-                    (transaction, buf)
-                })
-            } else {
-                let mut decompressor = reth_zstd_compressors::create_tx_decompressor();
+            reth_zstd_compressors::with_tx_decompressor(|decompressor| {
+                // TODO: enforce that zstd is only present at a "top" level type
                 let transaction_type = (bitflags & 0b110) >> 1;
                 let (transaction, _) = OpTypedTransaction::from_compact(
                     decompressor.decompress(buf),
                     transaction_type,
                 );
-
                 (transaction, buf)
-            }
+            })
         } else {
             let transaction_type = bitflags >> 1;
             OpTypedTransaction::from_compact(buf, transaction_type)
