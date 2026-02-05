82b4d603ed23a3214e2ca37ad588ece53801fb7e
geoknee <georgeknee@googlemail.com>
2026-02-05 21:34:51 +0000

    Refactor BlockAtTimestamp to use TargetBlockNumber and L2BlockRefByNumber

--- op-supernode/supernode/chain_container/chain_container_test.go
+++ op-supernode/supernode/chain_container/chain_container_test.go
@@ -119,12 +119,12 @@ func (m *mockVirtualNode) SyncStatus(ctx context.Context) (*eth.SyncStatus, erro
 
 // mockEngineController is a mock implementation of engine_controller.EngineController
 type mockEngineController struct {
-	blockAtTimestampResult eth.L2BlockRef
-	blockAtTimestampErr    error
+	l2BlockRefByNumberResult eth.L2BlockRef
+	l2BlockRefByNumberErr    error
 }
 
-func (m *mockEngineController) BlockAtTimestamp(ctx context.Context, ts uint64, label eth.BlockLabel) (eth.L2BlockRef, error) {
-	return m.blockAtTimestampResult, m.blockAtTimestampErr
+func (m *mockEngineController) L2BlockRefByNumber(ctx context.Context, num uint64) (eth.L2BlockRef, error) {
+	return m.l2BlockRefByNumberResult, m.l2BlockRefByNumberErr
 }
 
 func (m *mockEngineController) OutputV0AtBlockNumber(ctx context.Context, num uint64) (*eth.OutputV0, error) {
@@ -718,11 +718,11 @@ func TestChainContainer_VerifiedAt(t *testing.T) {
 
 		// Set up mock engine controller
 		mockEngine := &mockEngineController{
-			blockAtTimestampResult: eth.L2BlockRef{
+			l2BlockRefByNumberResult: eth.L2BlockRef{
 				Hash:   [32]byte{1},
 				Number: 100,
 			},
-			blockAtTimestampErr: nil,
+			l2BlockRefByNumberErr: nil,
 		}
 		impl.engine = mockEngine
 
