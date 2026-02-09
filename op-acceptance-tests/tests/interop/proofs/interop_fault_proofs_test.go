f81b9cd8fafa7c76254f5dadbf0e5af582f96b10
inphi <mlaw2501@gmail.com>
2026-02-09 15:16:12 -0500

    proofs: port TestInteropFaultProofs action test to devstack

---
+++ op-acceptance-tests/tests/interop/proofs/interop_fault_proofs_test.go
@@ -0,0 +1,15 @@
+package proofs
+
+import (
+	"testing"
+
+	sfp "github.com/ethereum-optimism/optimism/op-acceptance-tests/tests/superfaultproofs"
+	"github.com/ethereum-optimism/optimism/op-devstack/devtest"
+	"github.com/ethereum-optimism/optimism/op-devstack/presets"
+)
+
+func TestInteropFaultProofs(gt *testing.T) {
+	t := devtest.SerialT(gt)
+	sys := presets.NewSimpleInterop(t)
+	sfp.RunSuperFaultProofTest(t, sys)
+}
