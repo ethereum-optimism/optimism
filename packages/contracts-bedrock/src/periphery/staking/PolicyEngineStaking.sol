16dbfbd3c1aa5766cb78ba99f0ab672835a58d56
Ashitaka <ashitaka@defi.sucks>
2026-02-11 12:30:51 -0300

    fix: comments

--- packages/contracts-bedrock/src/periphery/staking/PolicyEngineStaking.sol
+++ packages/contracts-bedrock/src/periphery/staking/PolicyEngineStaking.sol
@@ -2,8 +2,6 @@
 pragma solidity 0.8.15;
 
 // Interfaces
-
-// Interfaces
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { ISemver } from "interfaces/universal/ISemver.sol";
 
