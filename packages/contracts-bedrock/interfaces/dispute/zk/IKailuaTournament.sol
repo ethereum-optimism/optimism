2e5ef3ad95d8ee1b3655e1a9da97a22bc730c985
Rami Khalil <the.rami.khalil@gmail.com>
2026-02-13 13:48:58 +0200

    licenses

--- packages/contracts-bedrock/interfaces/dispute/zk/IKailuaTournament.sol
+++ packages/contracts-bedrock/interfaces/dispute/zk/IKailuaTournament.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: MIT
 pragma solidity 0.8.24;
 
 import { IOptimismPortal2 } from "interfaces/L1/IOptimismPortal2.sol";
--- packages/contracts-bedrock/interfaces/dispute/zk/IKailuaTreasury.sol
+++ packages/contracts-bedrock/interfaces/dispute/zk/IKailuaTreasury.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: MIT
 pragma solidity 0.8.24;
 
 import { IKailuaTournament } from "interfaces/dispute/zk/IKailuaTournament.sol";
