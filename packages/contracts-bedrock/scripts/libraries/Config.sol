6c8993ef253d1a2773d4a42261bb7e9945f30206
IamFlux <175354924+0xiamflux@users.noreply.github.com>
2026-02-25 16:38:21 -0600

    feat: add L2 Interop flag

--- packages/contracts-bedrock/scripts/libraries/Config.sol
+++ packages/contracts-bedrock/scripts/libraries/Config.sol
@@ -291,6 +291,11 @@ library Config {
         return vm.envOr("DEV_FEATURE__L2CM", false);
     }
 
+    /// @notice Returns true if the development feature l2_interop is enabled.
+    function devFeatureL2Interop() internal view returns (bool) {
+        return vm.envOr("DEV_FEATURE__L2_INTEROP", false);
+    }
+
     /// @notice Returns true if the system feature custom_gas_token is enabled.
     function sysFeatureCustomGasToken() internal view returns (bool) {
         return vm.envOr("SYS_FEATURE__CUSTOM_GAS_TOKEN", false);
--- packages/contracts-bedrock/test/setup/FeatureFlags.sol
+++ packages/contracts-bedrock/test/setup/FeatureFlags.sol
@@ -49,6 +49,10 @@ abstract contract FeatureFlags {
             console.log("Setup: DEV_FEATURE__L2CM is enabled");
             devFeatureBitmap |= DevFeatures.L2CM;
         }
+        if (Config.devFeatureL2Interop()) {
+            console.log("Setup: DEV_FEATURE__L2_INTEROP is enabled");
+            devFeatureBitmap |= DevFeatures.L2_INTEROP;
+        }
     }
 
     /// @notice Returns the string name of a feature.
@@ -61,6 +65,8 @@ abstract contract FeatureFlags {
             return "DEV_FEATURE__OPCM_V2";
         } else if (_feature == DevFeatures.L2CM) {
             return "DEV_FEATURE__L2CM";
+        } else if (_feature == DevFeatures.L2_INTEROP) {
+            return "DEV_FEATURE__L2_INTEROP";
         } else if (_feature == Features.CUSTOM_GAS_TOKEN) {
             return "SYS_FEATURE__CUSTOM_GAS_TOKEN";
         } else if (_feature == Features.ETH_LOCKBOX) {
