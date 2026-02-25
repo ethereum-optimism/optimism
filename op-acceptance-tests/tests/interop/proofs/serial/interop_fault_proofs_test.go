78850274336b5c008ba838bc47ba12cd9ea13ce7
Adrian Sutton <adrian@oplabs.co>
2026-02-25 08:11:51 -0500

    Skip one more test.

--- op-acceptance-tests/tests/interop/proofs/serial/interop_fault_proofs_test.go
+++ op-acceptance-tests/tests/interop/proofs/serial/interop_fault_proofs_test.go
@@ -10,6 +10,8 @@ import (
 
 func TestInteropFaultProofs(gt *testing.T) {
 	t := devtest.SerialT(gt)
+	// TODO(#19180): Unskip this once supernode is updated.
+	t.Skip("Supernode does not yet return optimistic blocks until blocks are fully validated")
 	sys := presets.NewSimpleInterop(t)
 	sfp.RunSuperFaultProofTest(t, sys)
 }
