18856fa9cbd13a98fed275b0ca2d696c8e111935
Karl Floersch <karl@oplabs.co>
2026-04-28 17:53:49 -0400

    fix(kona): format registry test cases

--- rust/kona/crates/protocol/registry/src/lib.rs
+++ rust/kona/crates/protocol/registry/src/lib.rs
@@ -64,11 +64,9 @@ mod tests {
 
     #[test]
     fn test_hardcoded_rollup_configs() {
-        let test_cases = [
-            (10, test_utils::OP_MAINNET_CONFIG),
-            (11155420, test_utils::OP_SEPOLIA_CONFIG),
-        ]
-        .to_vec();
+        let test_cases =
+            [(10, test_utils::OP_MAINNET_CONFIG), (11155420, test_utils::OP_SEPOLIA_CONFIG)]
+                .to_vec();
 
         for (chain_id, expected) in test_cases {
             let derived = super::ROLLUP_CONFIGS.get(&chain_id).unwrap();
