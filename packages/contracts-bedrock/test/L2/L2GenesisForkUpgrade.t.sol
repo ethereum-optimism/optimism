4aa30858b5dca92c77522484d4b74afa2584df0a
IamFlux <175354924+0xiamflux@users.noreply.github.com>
2026-05-12 13:34:09 -0600

    test: fix override for _executeCurrentBundle

--- packages/contracts-bedrock/test/L2/L2GenesisForkUpgrade.t.sol
+++ packages/contracts-bedrock/test/L2/L2GenesisForkUpgrade.t.sol
@@ -127,6 +127,10 @@ contract L2GenesisForkUpgrade_GasProfile_Test is L2GenesisForkUpgrade_TestInit, 
     function setUp() public override(L2GenesisForkUpgrade_TestInit, L2ForkUpgrade_TestInit) {
         L2GenesisForkUpgrade_TestInit.setUp();
     }
+
+    function _executeCurrentBundle() internal override(L2GenesisForkUpgrade_TestInit, L2ForkUpgrade_TestInit) {
+        L2GenesisForkUpgrade_TestInit._executeCurrentBundle();
+    }
 }
 
 // ============================================================
@@ -142,6 +146,10 @@ abstract contract L2GenesisForkUpgrade_Interop_TestInit is L2GenesisForkUpgrade_
         super.enableInterop();
         L2GenesisForkUpgrade_TestInit.setUp();
     }
+
+    function _executeCurrentBundle() internal virtual override {
+        L2GenesisForkUpgrade_TestInit._executeCurrentBundle();
+    }
 }
 
 /// @title L2GenesisForkUpgrade_Interop_Versions_Test
@@ -151,6 +159,10 @@ contract L2GenesisForkUpgrade_Interop_Versions_Test is
 {
     function setUp() public override(L2GenesisForkUpgrade_Interop_TestInit, L2ForkUpgrade_TestInit) {
         L2GenesisForkUpgrade_Interop_TestInit.setUp();
+    }
+
+    function _executeCurrentBundle() internal override(L2GenesisForkUpgrade_Interop_TestInit, L2ForkUpgrade_TestInit) {
+        L2GenesisForkUpgrade_Interop_TestInit._executeCurrentBundle();
     }
 }
 
@@ -162,6 +174,10 @@ contract L2GenesisForkUpgrade_Interop_Initialization_Test is
     function setUp() public override(L2GenesisForkUpgrade_Interop_TestInit, L2ForkUpgrade_TestInit) {
         L2GenesisForkUpgrade_Interop_TestInit.setUp();
     }
+
+    function _executeCurrentBundle() internal override(L2GenesisForkUpgrade_Interop_TestInit, L2ForkUpgrade_TestInit) {
+        L2GenesisForkUpgrade_Interop_TestInit._executeCurrentBundle();
+    }
 }
 
 /// @title L2GenesisForkUpgrade_Interop_Implementations_Test
@@ -171,6 +187,10 @@ contract L2GenesisForkUpgrade_Interop_Implementations_Test is
 {
     function setUp() public override(L2GenesisForkUpgrade_Interop_TestInit, L2ForkUpgrade_TestInit) {
         L2GenesisForkUpgrade_Interop_TestInit.setUp();
+    }
+
+    function _executeCurrentBundle() internal override(L2GenesisForkUpgrade_Interop_TestInit, L2ForkUpgrade_TestInit) {
+        L2GenesisForkUpgrade_Interop_TestInit._executeCurrentBundle();
     }
 }
 
@@ -182,6 +202,10 @@ contract L2GenesisForkUpgrade_Interop_Events_Test is
     function setUp() public override(L2GenesisForkUpgrade_Interop_TestInit, L2ForkUpgrade_TestInit) {
         L2GenesisForkUpgrade_Interop_TestInit.setUp();
     }
+
+    function _executeCurrentBundle() internal override(L2GenesisForkUpgrade_Interop_TestInit, L2ForkUpgrade_TestInit) {
+        L2GenesisForkUpgrade_Interop_TestInit._executeCurrentBundle();
+    }
 }
 
 /// @title L2GenesisForkUpgrade_Interop_GasProfile_Test
@@ -191,6 +215,10 @@ contract L2GenesisForkUpgrade_Interop_GasProfile_Test is
 {
     function setUp() public override(L2GenesisForkUpgrade_Interop_TestInit, L2ForkUpgrade_TestInit) {
         L2GenesisForkUpgrade_Interop_TestInit.setUp();
+    }
+
+    function _executeCurrentBundle() internal override(L2GenesisForkUpgrade_Interop_TestInit, L2ForkUpgrade_TestInit) {
+        L2GenesisForkUpgrade_Interop_TestInit._executeCurrentBundle();
     }
 }
 
@@ -214,6 +242,10 @@ contract L2GenesisForkUpgrade_CGT_Versions_Test is L2GenesisForkUpgrade_CGT_Test
     function setUp() public override(L2GenesisForkUpgrade_CGT_TestInit, L2ForkUpgrade_TestInit) {
         L2GenesisForkUpgrade_CGT_TestInit.setUp();
     }
+
+    function _executeCurrentBundle() internal override(L2GenesisForkUpgrade_TestInit, L2ForkUpgrade_TestInit) {
+        L2GenesisForkUpgrade_TestInit._executeCurrentBundle();
+    }
 }
 
 /// @title L2GenesisForkUpgrade_CGT_Initialization_Test
@@ -223,6 +255,10 @@ contract L2GenesisForkUpgrade_CGT_Initialization_Test is
 {
     function setUp() public override(L2GenesisForkUpgrade_CGT_TestInit, L2ForkUpgrade_TestInit) {
         L2GenesisForkUpgrade_CGT_TestInit.setUp();
+    }
+
+    function _executeCurrentBundle() internal override(L2GenesisForkUpgrade_TestInit, L2ForkUpgrade_TestInit) {
+        L2GenesisForkUpgrade_TestInit._executeCurrentBundle();
     }
 }
 
@@ -234,12 +270,20 @@ contract L2GenesisForkUpgrade_CGT_Implementations_Test is
     function setUp() public override(L2GenesisForkUpgrade_CGT_TestInit, L2ForkUpgrade_TestInit) {
         L2GenesisForkUpgrade_CGT_TestInit.setUp();
     }
+
+    function _executeCurrentBundle() internal override(L2GenesisForkUpgrade_TestInit, L2ForkUpgrade_TestInit) {
+        L2GenesisForkUpgrade_TestInit._executeCurrentBundle();
+    }
 }
 
 /// @title L2GenesisForkUpgrade_CGT_Events_Test
 contract L2GenesisForkUpgrade_CGT_Events_Test is L2GenesisForkUpgrade_CGT_TestInit, L2ForkUpgrade_Events_Test {
     function setUp() public override(L2GenesisForkUpgrade_CGT_TestInit, L2ForkUpgrade_TestInit) {
         L2GenesisForkUpgrade_CGT_TestInit.setUp();
+    }
+
+    function _executeCurrentBundle() internal override(L2GenesisForkUpgrade_TestInit, L2ForkUpgrade_TestInit) {
+        L2GenesisForkUpgrade_TestInit._executeCurrentBundle();
     }
 }
 
@@ -250,5 +294,9 @@ contract L2GenesisForkUpgrade_CGT_GasProfile_Test is
 {
     function setUp() public override(L2GenesisForkUpgrade_CGT_TestInit, L2ForkUpgrade_TestInit) {
         L2GenesisForkUpgrade_CGT_TestInit.setUp();
+    }
+
+    function _executeCurrentBundle() internal override(L2GenesisForkUpgrade_TestInit, L2ForkUpgrade_TestInit) {
+        L2GenesisForkUpgrade_TestInit._executeCurrentBundle();
     }
 }
