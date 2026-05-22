1464ec2fb0f86b91f4a899d08ef96be7ebf510cd
Maurelian <maurelian@protonmail.ch>
2026-05-22 16:20:21 -0400

    chore(opd): update current tag

--- op-deployer/pkg/deployer/standard/standard.go
+++ op-deployer/pkg/deployer/standard/standard.go
@@ -42,7 +42,8 @@ const (
 	ContractsV410Tag        = "op-contracts/v4.1.0"
 	ContractsV500Tag        = "op-contracts/v5.0.0"
 	ContractsV600Tag        = "op-contracts/v6.0.0-rc.2"
-	CurrentTag              = ContractsV600Tag
+	ContractsV700Tag        = "op-contracts/v7.0.0-rc.3"
+	CurrentTag              = ContractsV700Tag
 )
 
 var DisputeAbsolutePrestate = common.HexToHash("0x038512e02c4c3f7bdaec27d00edf55b7155e0905301e1a88083e4e0a6764d54c")
