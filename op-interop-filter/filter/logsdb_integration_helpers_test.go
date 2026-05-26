3a68ea647f8d28d1399fd91c93d861219ebfbce4
Jacob Elias <jacobelias.gv@gmail.com>
2026-05-26 16:17:20 -0400

    fix(op-interop-filter): fix goimports formatting

--- op-interop-filter/filter/logsdb_integration_helpers_test.go
+++ op-interop-filter/filter/logsdb_integration_helpers_test.go
@@ -537,7 +537,7 @@ func (m *capturingMetrics) RecordChainTip(chainID uint64, blockNum uint64)      
 func (m *capturingMetrics) RecordTipLagBlocks(chainID uint64, lag uint64)       {}
 func (m *capturingMetrics) RecordIngestionLagSeconds(chainID uint64, _ float64) {}
 func (m *capturingMetrics) RecordCheckAccessList(success bool)                  {}
-func (m *capturingMetrics) RecordCheckAccessListDuration(duration float64) {}
+func (m *capturingMetrics) RecordCheckAccessListDuration(duration float64)      {}
 func (m *capturingMetrics) RecordCheckAccessListRejection(reason string) {
 	m.locked(func() { m.rejections[reason]++ })
 }
