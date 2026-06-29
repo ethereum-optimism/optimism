20302cab26a30735af3ab302385146ee7c118028
lumoswiz <lumoswiz@protonmail.com>
2026-06-29 16:52:58 +0200

    test: use nonzero cannon prestate in validator fixtures

--- packages/contracts-bedrock/test/L1/OPContractsManagerStandardValidator.t.sol
+++ packages/contracts-bedrock/test/L1/OPContractsManagerStandardValidator.t.sol
@@ -227,7 +227,7 @@ abstract contract OPContractsManagerStandardValidator_TestInit is CommonTest {
             );
         } else {
             l2ChainId = deploy.cfg().l2ChainID();
-            cannonPrestate = Claim.wrap(bytes32(deploy.cfg().faultGameAbsolutePrestate()));
+            cannonPrestate = Claim.wrap(keccak256("cannonPrestate"));
             proposer = deploy.cfg().l2OutputOracleProposer();
             challenger = deploy.cfg().l2OutputOracleChallenger();
         }
@@ -2178,7 +2178,7 @@ abstract contract OPContractsManagerStandardValidator_ZKMode_TestInit is CommonT
             );
         } else {
             l2ChainId = deploy.cfg().l2ChainID();
-            cannonPrestate = Claim.wrap(bytes32(deploy.cfg().faultGameAbsolutePrestate()));
+            cannonPrestate = Claim.wrap(keccak256("cannonPrestate"));
             proposer = deploy.cfg().l2OutputOracleProposer();
             challenger = deploy.cfg().l2OutputOracleChallenger();
 
