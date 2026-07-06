315ba663d4d566857df6a6a683ba0d622e29cdf3
Matt Solomon <matt@mattsolomon.dev>
2026-07-06 11:17:04 -0700

    ci: move scheduled tests to daily (#21595)

--- .circleci/scripts/test-decision-tree.sh
+++ .circleci/scripts/test-decision-tree.sh
@@ -167,13 +167,13 @@ run_scenario \
   "Scheduled: build_daily" \
   "scheduled_pipeline" "" "" "build_daily" \
   '{}' \
-  scheduled_preimage_reproducibility scheduled_stale_check scheduled_heavy_fuzz_tests
+  scheduled_preimage_reproducibility scheduled_stale_check scheduled_heavy_fuzz_tests scheduled_daily_tests
 
 run_scenario \
   "Scheduled: build_weekly" \
   "scheduled_pipeline" "" "" "build_weekly" \
   '{}' \
-  scheduled_weekly_tests scheduled_kona_link_checker
+  scheduled_kona_link_checker
 
 run_scenario \
   "API: main_dispatch (no github event)" \
