27bc568da1661bedb9cc18aedf03a1ae88d56131
Adrian Sutton <adrian@oplabs.co>
2026-07-13 08:11:48 +1000

    Bump OPCM V2 minor version

--- packages/contracts-bedrock/src/L1/opcm/OPContractsManagerV2.sol
+++ packages/contracts-bedrock/src/L1/opcm/OPContractsManagerV2.sol
@@ -158,9 +158,9 @@ contract OPContractsManagerV2 is ISemver, OPContractsManagerUtilsCaller {
     ///         - Major bump: New required sequential upgrade
     ///         - Minor bump: Replacement OPCM for same upgrade
     ///         - Patch bump: Development changes (expected for normal dev work)
-    /// @custom:semver 7.1.24
+    /// @custom:semver 7.2.0
     function version() public pure returns (string memory) {
-        return "7.1.24";
+        return "7.2.0";
     }
 
     /// @param _standardValidator The standard validator for this OPCM release.
