58e7bb29578771ff4a995866b5a76a1bca7fa3ca
Josh Klopfenstein <joshklop@oplabs.co>
2026-08-05 06:15:45 -0500

    op-acceptance-tests: enforce multi EL sync on kona (#22249)

--- op-acceptance-tests/tests/sync_tester/sync_tester_elsync_multi/sync_test.go
+++ op-acceptance-tests/tests/sync_tester/sync_tester_elsync_multi/sync_test.go
@@ -30,7 +30,6 @@ func simpleWithSyncTesterOpts() []presets.Option {
 
 func TestMultiELSync(gt *testing.T) {
 	t := devtest.ParallelT(gt)
-	sysgo.FlakyOnKonaNode(t, "fails in ci but passes locally")
 	sys := presets.NewSimpleWithSyncTester(t, simpleWithSyncTesterOpts()...)
 	require := t.Require()
 
