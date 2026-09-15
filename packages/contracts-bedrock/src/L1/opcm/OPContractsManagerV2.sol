77b798db45a12c5cffb1993f16f47e7d41abf2db
lumoswiz <lumoswiz@protonmail.com>
2026-09-15 15:17:30 +0200

    contracts-bedrock: bump OPCM contract versions

--- packages/contracts-bedrock/src/L1/opcm/OPContractsManagerV2.sol
+++ packages/contracts-bedrock/src/L1/opcm/OPContractsManagerV2.sol
@@ -158,9 +158,9 @@ contract OPContractsManagerV2 is ISemver, OPContractsManagerUtilsCaller {
     ///         - Major bump: New required sequential upgrade
     ///         - Minor bump: Replacement OPCM for same upgrade
     ///         - Patch bump: Development changes (expected for normal dev work)
-    /// @custom:semver 8.0.4
+    /// @custom:semver 8.0.5
     function version() public pure returns (string memory) {
-        return "8.0.4";
+        return "8.0.5";
     }
 
     /// @param _standardValidator The standard validator for this OPCM release.
