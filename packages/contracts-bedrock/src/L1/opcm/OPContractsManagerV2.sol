c92cbcdd311e9883986dfe815cf219d2a1da4843
IamFlux <175354924+0xiamflux@users.noreply.github.com>
2026-09-18 10:27:07 -0600

    contracts-bedrock: OPCM version bump

--- packages/contracts-bedrock/src/L1/opcm/OPContractsManagerV2.sol
+++ packages/contracts-bedrock/src/L1/opcm/OPContractsManagerV2.sol
@@ -166,9 +166,9 @@ contract OPContractsManagerV2 is ISemver, OPContractsManagerUtilsCaller {
     ///         - Major bump: New required sequential upgrade
     ///         - Minor bump: Replacement OPCM for same upgrade
     ///         - Patch bump: Development changes (expected for normal dev work)
-    /// @custom:semver 9.0.0
+    /// @custom:semver 9.0.1
     function version() public pure returns (string memory) {
-        return "9.0.0";
+        return "9.0.1";
     }
 
     /// @param _standardValidator The standard validator for this OPCM release.
